03aa7e9c34937a4c5a18e37788c23f8ba418eac2
mloning <markus.loning.17@ucl.ac.uk>
2019-01-14 11:56:00 +0000

    added classifier and regressor using xpandas df for feature extraction before passing it to sklearn estimator

--- sktime/_regressors.py
+++ sktime/_regressors.py
@@ -6,6 +6,7 @@ import pandas as pd
 from xpandas.data_container import XSeries, XDataFrame
 from sklearn.base import BaseEstimator
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
+from sklearn.ensemble import RandomForestRegressor
 
 
 class TSDummyRegressor(BaseEstimator):
@@ -80,3 +81,77 @@ class TSDummyRegressor(BaseEstimator):
             X = check_array(X)
         check_is_fitted(self, 'is_fitted_')
         return np.ones(X.shape[0], dtype=np.int64) * self.theta_
+
+
+class TSExampleRegressor(BaseEstimator):
+    """ An example regressor that makes use of the xpandas input.
+    """
+
+    def __init__(self, func=np.mean, columns=None, **kwargs):
+        self.func = func
+        self.columns = columns
+        self.estimator = RandomForestRegressor(**kwargs)
+
+    def fit(self, X, y):
+        """ A reference implementation of a fitting function.
+
+        Parameters
+        ----------
+        X : array-like, xpandas XDataFrame or XSeries, shape (n_samples, ...)
+            The training input samples.
+        y : array-like, xpandas XdataFrame Xseries, shape (n_samples,)
+            The target values (class labels in classification)
+
+        Returns
+        -------
+        self : object
+            Returns self.
+        """
+
+        # simple feature extraction
+        X = pd.DataFrame([X[col].apply(self.func) for col in self.columns]).T
+
+        # input checks
+        if isinstance(X, (XSeries, XDataFrame, pd.Series, pd.DataFrame)) or \
+                isinstance(y, (XSeries, XDataFrame, pd.Series, pd.DataFrame)):
+            # do custom checks as per fit() implementation requirements
+            # checking that the first dimensions are equal and other basic stuff as per sklearn req.
+            X, y = check_X_y(X, y, dtype=None, ensure_2d=False)
+        else:
+            # check as per sklearn default requirements
+            X, y = check_X_y(X, y)
+        # fitting (finding the value of dummy prediction theta_) the model based on strategy
+
+        # fitting
+        self.estimator.fit(X, y)
+
+        # let the model know that it is fitted
+        self.is_fitted_ = True
+        # `fit` should always return `self`
+        return self
+
+    def predict(self, X):
+        """ A reference implementation of a predicting function.
+
+        Parameters
+        ----------
+        X : array-like, xpandas XDataFrame or XSeries, shape (n_samples, ...)
+            The training input samples.
+        Returns
+        -------
+        y : ndarray, shape (n_samples,)
+            Returns the dummy predictions
+        """
+        X = pd.DataFrame([X[col].apply(self.func) for col in self.columns]).T
+
+        if isinstance(X, (XSeries, XDataFrame, pd.Series, pd.DataFrame)):
+            # do custom checks as per predict() implementation requirements
+            # basic stuff as per sklearn req.
+            X = check_array(X, dtype=None, ensure_2d=False)
+        else:
+            # check as per sklearn default requirements
+            X = check_array(X)
+
+        check_is_fitted(self, 'is_fitted_')
+
+        return self.estimator.predict(X)
