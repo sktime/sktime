466e16a4d3cddddd0321ffcae799e298dc362008
Sajaysurya Ganesh <sajay.surya@gmail.com>
2019-01-26 21:47:07 +0000

    Added sktime to path and napoleon to extensions
    Signed-off-by: Sajaysurya Ganesh <sajay.surya@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,9 +12,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
@@ -45,6 +45,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
+    'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
