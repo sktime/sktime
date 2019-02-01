4ae379fd4e6b3cbab33c07c568aaa24b248c103e
Sajaysurya Ganesh <sajay.surya@gmail.com>
2019-02-01 17:08:21 +0000

    Added additional tests for default scorers in the wrapped GridSearchCV
    Signed-off-by: Sajaysurya Ganesh <sajay.surya@gmail.com>

--- sktime/tests/test_TSExampleClassifier.py
+++ sktime/tests/test_TSExampleClassifier.py
@@ -11,6 +11,9 @@ from sklearn.utils.testing import assert_array_equal
 
 from sktime.classifiers import TSExampleClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sktime.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score, make_scorer
 
 
 def read_data(file):
@@ -53,3 +56,41 @@ def test_set_get_param():
     model = TSExampleClassifier(func=np.mean, columns=X.columns, estimator=RandomForestClassifier(random_state=123, n_estimators=10))
     model.set_params(estimator__random_state=42)
     assert model.get_params()['estimator__random_state'] == 42
+
+def test_grid_search_cv():
+    X = Xdf_train
+    y = y_train
+    model = TSExampleClassifier(func=np.mean,
+                                columns=X.columns,
+                                estimator=LogisticRegression(fit_intercept=False,
+                                                             solver='lbfgs'))
+    model.fit(X, y)
+    expected = model.predict(X)
+
+    # give (deep) parameter tuning details
+    parameters = {'estimator__fit_intercept': (True, False)}
+    # as we are not using a mixin, we need an external scorer
+    external_scorer = make_scorer(accuracy_score)
+    # fit and predict GridSearchCV
+    clf = GridSearchCV(model, parameters, scoring=external_scorer, cv=5)
+    clf.fit(X, y)
+    got = clf.predict(X)
+    assert_array_equal(expected, got)
+
+def test_grid_search_cv_default_scorer():
+    X = Xdf_train
+    y = y_train
+    model = TSExampleClassifier(func=np.mean,
+                                columns=X.columns,
+                                estimator=LogisticRegression(fit_intercept=False,
+                                                             solver='lbfgs'))
+    model.fit(X, y)
+    expected = model.predict(X)
+
+    # give (deep) parameter tuning details
+    parameters = {'estimator__fit_intercept': (True, False)}
+    # fit and predict GridSearchCV without external scorer
+    clf = GridSearchCV(model, parameters, cv=5)
+    clf.fit(X, y)
+    got = clf.predict(X)
+    assert_array_equal(expected, got)
