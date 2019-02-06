d92b993fc5c20364cf1f9a05597fdc3d0743b8ff
mloning <markus.loning.17@ucl.ac.uk>
2019-02-06 09:55:50 +0000

    added docstring for base classes

--- sktime/base.py
+++ sktime/base.py
@@ -2,9 +2,15 @@ from sklearn.base import BaseEstimator
 
 
 class BaseClassifier(BaseEstimator):
+    """
+    Base class for classifiers, necessary for default scoring behaviour.
+    """
     pass
 
 
 class BaseRegressor(BaseEstimator):
+    """
+    Base class for regressors, necessary for default scoring behaviour.
+    """
     pass
 
