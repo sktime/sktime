ad38bebdf32e3a0a114e5d416afdae1ec8e3c156
mloning <markus.loning.17@ucl.ac.uk>
2019-02-07 14:05:46 +0000

    added function to check if all instances have time-series with the same index in same column

--- sktime/utils/validation.py
+++ sktime/utils/validation.py
@@ -2,7 +2,8 @@
 validation utilities for sktime
 '''
 # build on top of sklearn
-from sklearn.utils.validation import *
+from sklearn.utils.validation import check_X_y
+
 
 def check_ts_X_y(X, y):
     '''
@@ -10,3 +11,20 @@ def check_ts_X_y(X, y):
     '''
     # TODO: add proper checks (e.g. check if input stuff is xpandas)
     return check_X_y(X, y, dtype=None, ensure_2d=False)
+
+
+def check_equal_index(X):
+    """
+    Check if all series in the same column have the same index.
+    """
+    n_rows, n_cols = X.shape
+    indexes = []
+    for c in range(n_cols):
+        first_index = X.iloc[0, c].index
+        for i in range(1, n_rows):
+            if not first_index.equals(X.iloc[i, c].index):
+                raise ValueError(f'Found time series with unequal index in column {c}. '
+                                 'Input time-series must have the same index.')
+        indexes.append(first_index)
+    return indexes
+
