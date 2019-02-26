61a394dec76e46768a695d76657f2ab7c9841bb5
mloning <markus.loning.17@ucl.ac.uk>
2019-02-26 16:55:40 +0000

    minor changes

--- sktime/transformers/series_to_tabular.py
+++ sktime/transformers/series_to_tabular.py
@@ -9,35 +9,37 @@ __all__ = ["RandomIntervalFeatureExtractor"]
 
 class RandomIntervalFeatureExtractor(RandomIntervalSegmenter):
     """
-    Splits time-series into random intervals and extracts features from each interval.
-    Series-to-tabular transformer.
+    RandomIntervalFeatureExtractor, a transformer that segments time-series into random intervals
+    and extracts features from each interval.
+
+    :param n_intervals: str or int
+        - If "fixed", sqrt of length of time-series is used.
+        - If "random", random number of intervals is generated.
+        - If integer, integer gives (fixed) number of intervals to generate.
+
+        Default is "sqrt".
+    :param features: None or list of functions
+        - If list of function, applies each function to random intervals to extract features.
+        - If None, only the mean is extracted.
+        Default is None.
+
+    :param random_state: : int, RandomState instance or None, optional (default=None)
+        If int, random_state is the seed used by the random number generator;
+        If RandomState instance, random_state is the random number generator;
+        If None, the random number generator is the RandomState instance used
+        by `np.random`.
+
+    :param check_input: bool, optional (default=True)
+        When set to ``True``, inputs will be validated, otherwise inputs are assumed to be valid
+        and no checks are performed. Use with caution.
     """
 
     def __init__(self, n_intervals='sqrt', features=None, random_state=None, check_input=True):
-        """
-        Creates instance of RandomIntervalFeatureExtractor transformer.
-
-        :param n_intervals: str or int
-            - If "fixed", sqrt of length of time-series is used.
-            - If "random", random number of intervals is generated.
-            - If integer, integer gives (fixed) number of intervals to generate.
-
-            Default is "sqrt".
-        :param features: None or list of functions
-            - If list of function, applies each function to random intervals to extract features.
-            - If None, the mean is used.
-
-            Default is None.
-
-        :param random_state:
-        :param check_input:
-        """
         super(RandomIntervalFeatureExtractor, self).__init__(
             n_intervals=n_intervals,
             random_state=random_state,
             check_input=check_input
         )
-        self.feature_names_ = []
 
         # Check input of feature calculators, i.e list of functions to be applied to time-series
         if features is None:
@@ -45,7 +47,7 @@ class RandomIntervalFeatureExtractor(RandomIntervalSegmenter):
         elif isinstance(features, list) and all([callable(func) for func in features]):
             self.features = features
         else:
-            raise ValueError('Features must be list containing only functions (callable) to be '
+            raise ValueError('Features must be list containing only functions (callables) to be '
                              'applied to the data columns')
 
     def transform(self, X, y=None):
@@ -54,7 +56,7 @@ class RandomIntervalFeatureExtractor(RandomIntervalSegmenter):
         """
 
         # Check is fit had been called
-        check_is_fitted(self, 'is_fitted_')
+        check_is_fitted(self, 'intervals_')
 
         # check inputs
         if self.check_input:
@@ -75,7 +77,7 @@ class RandomIntervalFeatureExtractor(RandomIntervalSegmenter):
         # Pre-allocate arrays.
         n_rows, n_cols = X.shape
         n_features = len(self.features)
-        n_cols_intervals = sum([intervals.shape[0] for intervals in self.intervals_])  # total number of intervals of all columns
+        n_cols_intervals = sum([intervals.shape[0] for intervals in self.intervals_])
         Xt = np.zeros((n_rows, n_features * n_cols_intervals))  # Allocate output array for transformed data
         feature_names_ = []
 
