02e1b683323241c083cbebade581a494d3baa3fd
mloning <markus.loning.17@ucl.ac.uk>
2019-03-05 22:51:03 +0000

    added option for return numpy array

--- sktime/utils/transformations.py
+++ sktime/utils/transformations.py
@@ -2,13 +2,21 @@ import pandas as pd
 import numpy as np
 
 
-def tabularize(X):
+def tabularize(X, return_array=False):
     """
     Helper function to turn nested pandas dataframes into tabular data.
     :param X: nested pandas dataframe
+    :param return_array : bool
+        If True, returns a numpy array of the tabular data.
+        If False, returns a pandas dataframe with row and column names.
     :return: tabular pandas dataframe
     """
-    Xt = pd.DataFrame(np.hstack([col.tolist() for _, col in X.items()]))
+
+    Xt = np.hstack([col.tolist() for _, col in X.items()])
+    if return_array:
+        return Xt
+
+    Xt = pd.DataFrame(Xt)
     Xt.index = X.index
     columns = []
     for colname, col in X.items():
