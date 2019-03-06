47057c0ad9025da59dae2fcf6e9dbb1ea9dd4d4b
mloning <markus.loning.17@ucl.ac.uk>
2019-03-06 10:21:13 +0000

    changes to handle pd.Series or np.arrays

--- sktime/utils/validation.py
+++ sktime/utils/validation.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 
 def check_ts_X_y(X, y):
@@ -22,20 +23,33 @@ def check_ts_array(X):
 
 def check_equal_index(X):
     """
-    Check if all series in the same column have the same index.
-    """
-    n_rows, n_cols = X.shape
-    indexes = []
-    for c, col in enumerate(X.columns):
-        first_index = X.iloc[0, c].index
+    Check for each column in dataframe if all series have the same index.
 
+    :param X:
+    :return:
+    """
+
+    # TODO assumes columns are typed (i.e. all rows for a given column have the same type)
+    # TODO only handles series columns, raises error for columns with primitives
+
+    indexes = []
+    # Check index for each column separately.
+    for c, col in enumerate(X.columns):
+
+        # Get index from first row, can be either pd.Series or np.array.
+        first_index = X.iloc[0, c].index if hasattr(X.iloc[0, c], 'index') else np.arange(X.iloc[c, 0].shape[0])
+
+        # Series must contain at least 2 observations, otherwise should be primitive.
         if first_index.size < 2:
             raise ValueError(f'Time series must contain at least 2 observations, '
                              f'found time series in column {col} with less than 2 observations')
 
-        for i in range(1, n_rows):
-            if not first_index.equals(X.iloc[i, c].index):
+        # Check index for all rows.
+        for i in range(1, X.shape[0]):
+            index = X.iloc[i, c].index if hasattr(X.iloc[i, c], 'index') else np.arange(X.iloc[c, 0].shape[0])
+            if not np.array_equal(first_index, index):
                 raise ValueError(f'Found time series with unequal index in column {col}. '
                                  f'Input time-series must have the same index.')
         indexes.append(first_index)
+
     return indexes
