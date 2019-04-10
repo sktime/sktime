9c8c3ebc5041eb031c3a89863b8f48965989eb14
George Oastler <goastler4@gmail.com>
2019-04-10 17:38:35 +0100

    Revert "distance measure parameter ranges"
    This reverts commit b412254579ab0c14fa6239bac847719790128a05.

--- sktime/classifiers/proximity_forest/proximity_forest.py
+++ sktime/classifiers/proximity_forest/proximity_forest.py
@@ -8,12 +8,13 @@ from sklearn.utils.validation import check_is_fitted, check_array
 
 from datasets import load_gunpoint
 
+
 class ProximityForest:
-    'Proximity Forest - a distance-based time-series classifier. https://arxiv.org/pdf/1808.10594.pdf'
+    'Proximity Forest - a distance-based time-series classifier'
 
     _seed_key = 'seed'
     _verbose_key = 'verbose'
-    _distance_measures_key = 'dms'
+    _distance_measures_key = 'distance_measures'
     _default_distance_measures = ('dtw', 'lcss', 'msm')
 
     def __init__(self, **parameters):
@@ -56,17 +57,16 @@ class ProximityForest:
         return 'ProximityForest'
 
 
-
 # from sklearn.utils.estimator_checks import check_estimator
 #
-# pf = ProximityForest(seed=5, verbose=True, dms=('dtw', 'lcss', 'msm'))
-# x_train, y_train = load_gunpoint(return_X_y=True)
-# x_test, y_test = load_gunpoint(split='TEST', return_X_y=True)
-# pf.fit(x_train, y_train)
-# predictions = pf.predict_proba(x_test)
-#
-# score = accuracy_score(y_test, predictions)
-# print(score)
+pf = ProximityForest(seed=5, verbose=True, distance_measures=('dtw', 'lcss', 'msm'))
+x_train, y_train = load_gunpoint(return_X_y=True)
+x_test, y_test = load_gunpoint(split='TEST', return_X_y=True)
+pf.fit(x_train, y_train)
+predictions = pf.predict_proba(x_test)
+
+score = accuracy_score(y_test, predictions)
+print(score)
 
 # nb = GaussianNB()
 # iris = datasets.load_iris()
