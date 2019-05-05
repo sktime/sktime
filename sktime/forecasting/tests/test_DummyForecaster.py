818eff7b34de6ff6c35174dd21880a90f028f1de
mloning <markus.loning.17@ucl.ac.uk>
2019-05-05 14:10:43 +0100

    added tests for forecasting

--- sktime/forecasting/tests/test_DummyForecaster.py
+++ sktime/forecasting/tests/test_DummyForecaster.py
@@ -1,21 +1,38 @@
-import numpy as np
 import pytest
+import numpy as np
+from numpy.testing import assert_array_equal
+from numpy.testing import assert_array_almost_equal
+
 from sktime.forecasting.forecasters import DummyForecaster
+from sktime.forecasting.forecasters import ExponentialSmoothing
+from sktime.forecasting.forecasters import ARIMAForecaster
+from sktime.forecasting.forecasters import EnsembleForecaster
 from sktime.datasets import load_shampoo_sales
 
+__author__ = "Markus Löning"
+
+
+# forecasters
+FORECASTERS = (DummyForecaster, ExponentialSmoothing, ARIMAForecaster, EnsembleForecaster)
+
+# forecast horizons
+FHS = (None, [1], [1, 3], np.array([1]), np.array([1, 3]), np.arange(1, 5))
+
+# load test data
 y = load_shampoo_sales()
 
 
+# test strategies of dummy forecaster
 # TODO add test for linear strategy
-@pytest.mark.parametrize(
-    "strategy, expected", [
-        ("mean", y.iloc[0].mean()),
-        ("last", y.iloc[0].iloc[-1])
-    ])
-def test_DummyForecaster_strategies(strategy, expected):
-    fh = np.array([1, 2])
+@pytest.mark.filterwarnings('ignore::FutureWarning')
+@pytest.mark.parametrize("fh", FHS)
+@pytest.mark.parametrize("strategy, expected", [("mean", y.iloc[0].mean()), ("last", y.iloc[0].iloc[-1])])
+def test_DummyForecaster_strategies(fh, strategy, expected):
     m = DummyForecaster(strategy=strategy)
     m.fit(y)
     y_pred = m.predict(fh=fh)
-    np.testing.assert_array_almost_equal(y_pred, expected, decimal=4)
+    assert_array_almost_equal(y_pred, expected, decimal=4)
+
+
+
 
---
+++ sktime/forecasting/tests/test_forecasters.py
@@ -0,0 +1,59 @@
+import pytest
+import numpy as np
+from numpy.testing import assert_array_equal
+
+from sktime.forecasting.forecasters import DummyForecaster
+from sktime.forecasting.forecasters import ExpSmoothingForecaster
+from sktime.forecasting.forecasters import ARIMAForecaster
+from sktime.datasets import load_shampoo_sales
+
+
+# forecasters
+FORECASTERS = (DummyForecaster, ExpSmoothingForecaster, ARIMAForecaster)
+FORECASTER_PARAMS = {DummyForecaster: {"strategy": "last"},
+                     ExpSmoothingForecaster: {"trend": "additive"},
+                     ARIMAForecaster: {"order": (1, 1, 0)}}
+
+# forecast horizons
+FHS = (None, [1], [1, 3], np.array([1]), np.array([1, 3]), np.arange(5))
+
+# load test data
+y = load_shampoo_sales()
+
+
+# test default forecasters output for different forecasting horizons
+@pytest.mark.filterwarnings('ignore::FutureWarning')
+@pytest.mark.parametrize("forecaster", FORECASTERS)
+@pytest.mark.parametrize("fh", FHS)
+def test_fhs(forecaster, fh):
+    m = forecaster()
+
+    m.fit(y)
+    y_pred = m.predict(fh=fh)
+
+    # adjust for default value
+    if fh is None:
+        fh = np.array([1])
+    elif isinstance(fh, list):
+        fh = np.asarray(fh)
+
+    # test length of output
+    assert len(y_pred) == len(fh)
+
+    # test index
+    assert_array_equal(y_pred.index.values, y.iloc[0].index[-1] + fh)
+
+
+@pytest.mark.filterwarnings('ignore::FutureWarning')
+@pytest.mark.parametrize("forecaster, params", FORECASTER_PARAMS.items())
+def test_set_params(forecaster, params):
+    m = forecaster(**params)
+    m.fit(y)
+    expected = m.predict()
+
+    m = forecaster()
+    m.set_params(**params)
+    m.fit(y)
+    y_pred = m.predict()
+
+    assert_array_equal(y_pred, expected)
