1a76f8e7ac7610c24ab71c32f67c9a29b141ca75
mloning <markus.loning.17@ucl.ac.uk>
2019-05-07 14:47:01 +0100

    reorganised unit/component tests

--- sktime/transformers/tests/test_compose.py
+++ sktime/transformers/tests/test_compose.py
@@ -3,14 +3,11 @@ import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import FunctionTransformer
-from sktime.pipeline import TSPipeline
-from sktime.transformers.compose import TSColumnTransformer
-from sktime.transformers.compose import RowwiseTransformer
+
+from sktime.pipeline import Pipeline
+from sktime.tests.test_pipeline import X_train, y_train, X_test, y_test
+from sktime.transformers.compose import TSColumnTransformer, Tabulariser, RowwiseTransformer
 from sktime.datasets import load_gunpoint
-from sktime.transformers.compose import Tabulariser
-from sktime.pipeline import TSFeatureUnion
-from sklearn.tree import DecisionTreeClassifier
-from sktime.transformers.series_to_series import RandomIntervalSegmenter
 
 # load data
 X_train, y_train = load_gunpoint("TRAIN", return_X_y=True)
@@ -22,10 +19,7 @@ X_test = pd.concat([X_test, X_test], axis=1)
 X_test.columns = ['ts', 'ts_copy']
 
 
-def test_TSColumnTransformer_pipeline():
-    """
-    there is a series to series transformer tested in here
-    """
+def test_ColumnTransformer_pipeline():
     # using Identity function transformers (transform series to series)
     id_func = lambda X: X
     column_transformer = TSColumnTransformer(
@@ -35,7 +29,7 @@ def test_TSColumnTransformer_pipeline():
         ('feature_extract', column_transformer),
         ('tabularise', Tabulariser()),
         ('rfestimator', RandomForestClassifier(n_estimators=2))]
-    model = TSPipeline(steps=steps)
+    model = Pipeline(steps=steps)
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
     assert y_pred.shape[0] == y_test.shape[0]
@@ -53,7 +47,7 @@ def test_RowwiseTransformer_pipeline():
     strategy = [
         ('feature_extract', column_transformer),
         ('rfestimator', estimator)]
-    model = TSPipeline(steps=strategy)
+    model = Pipeline(steps=strategy)
     model.fit(X_train, y_train)
     expected = model.predict(X_test)
 
@@ -66,26 +60,7 @@ def test_RowwiseTransformer_pipeline():
     strategy = [
         ('feature_extract', column_transformer),
         ('rfestimator', estimator)]
-    model = TSPipeline(steps=strategy)
+    model = Pipeline(steps=strategy)
     model.fit(X_train, y_train)
     got = model.predict(X_test)
     np.testing.assert_array_equal(expected, got)
-
-
-def test_TSFeatureUnion_pipeline():
-    # pipeline with segmentation plus multiple feature extraction
-    steps = [
-        ('segment', RandomIntervalSegmenter(n_intervals=3, check_input=False)),
-        ('transform', TSFeatureUnion([
-            ('mean', RowwiseTransformer(FunctionTransformer(func=np.mean, validate=False))),
-            ('std', RowwiseTransformer(FunctionTransformer(func=np.std, validate=False)))
-        ])),
-        ('clf', DecisionTreeClassifier())
-    ]
-    clf = TSPipeline(steps)
-
-    clf.fit(X_train, y_train)
-    y_pred = clf.predict(X_test)
-
-    assert y_pred.shape[0] == y_test.shape[0]
-    np.testing.assert_array_equal(np.unique(y_pred), np.unique(y_test))
