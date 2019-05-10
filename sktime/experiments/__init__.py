f8e3e225bcb1114dc2259fe187167086f740b88f
mloning <markus.loning.17@ucl.ac.uk>
2019-05-10 16:08:53 +0100

    cleaned up init file

--- sktime/experiments/__init__.py
+++
@@ -1,9 +0,0 @@
-from .analysis import AnalyseResults
-from .orchestrator import Orchestrator
-
-from .data import DatasetLoadFromDir
-from .data import Result
-from .data import ResultHDD
-from .data import DatasetHDD
-
-
