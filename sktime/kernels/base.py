3cef634f037944740c2b96a9ba82e2836c1f019d
George Oastler <goastler4@gmail.com>
2019-05-16 16:27:50 +0100

    Distance fix

--- sktime/kernels/base.py
+++ sktime/kernels/base.py
@@ -20,7 +20,8 @@ def distance_matrix(distance_measure, **kwargs):
     def distance(a, b):
         a = to_time_series(a)
         b = to_time_series(b)
-        return distance_measure(a, b, **kwargs)
+        dist = distance_measure(a, b, **kwargs)
+        return np.exp(-dist)
 
     def build_matrix(X, Y):
         matrix = cdist(X, Y, metric=distance)
