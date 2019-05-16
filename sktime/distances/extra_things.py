82ecb843c2d589426c6dc0c7054238b6cfec7919
abandus <amaiabanda@gmail.com>
2019-05-16 17:34:21 +0200

    Training svm with dtw kernel works wrong

---
+++ sktime/distances/extra_things.py
@@ -0,0 +1,10 @@
+#if __name__ == "__main__":
+#    a = np.array([1.0, 2.0, 3.0, 4.0, 5.0]).reshape((1, 5))
+#    b = np.array([3.0,4.0,3.0,6.0,1.0]).reshape((1, 5))
+#    dist = GDS(a, b, w=5)
+#   print(dist)
+
+
+# clf1 = KNeighborsClassifier(n_neighbors=1, metric=GDS_pairs)
+# clf1.fit(X_train2,y_train)
+# print("Correct classification rate:", clf1.score(X_test2, y_test))
