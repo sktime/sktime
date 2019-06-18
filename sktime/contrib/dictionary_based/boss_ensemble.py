3ed5663f7562303b58dd8fff3b3077a28892d2af
MatthewMiddlehurst <pfm15hbu@uea.ac.uk>
2019-06-18 16:59:37 +0100

    BOSS fix and start of SFA integration

--- sktime/contrib/dictionary_based/boss_ensemble.py
+++ sktime/contrib/dictionary_based/boss_ensemble.py
@@ -6,6 +6,8 @@ import math
 from sklearn.base import BaseEstimator
 from sklearn.utils.multiclass import class_distribution
 
+from sktime.transformers.SFA import SFA
+
 all__ = ["BOSSEnsemble", "BOSSIndividual", "BOSSTransform"]
 
 
@@ -216,10 +218,10 @@ class BOSSEnsemble(BaseEstimator):
 
         sums = np.zeros((X.shape[0], self.num_classes))
 
-        for i, clf in enumerate(self.classifiers):
+        for n, clf in enumerate(self.classifiers):
             preds = clf.predict(X)
-            for n in range(0, X.shape[0]):
-                sums[n, self.class_dictionary.get(preds[i])] += 1
+            for i in range(0, X.shape[0]):
+                sums[i, self.class_dictionary.get(preds[i])] += 1
 
         dists = sums / (np.ones(self.num_classes) * self.num_classifiers)
 
@@ -298,7 +300,7 @@ class BOSSIndividual:
         self.alphabet_size = alphabet_size
         self.norm = norm
 
-        self.transform = BOSSTransform(window_size, word_length, alphabet_size, norm)
+        self.transform = SFA(word_length, alphabet_size, window_size=window_size, norm=norm, remove_repeat_words=True)
         self.transformed_data = []
         self.class_vals = []
         self.accuracy = 0
@@ -339,7 +341,7 @@ class BOSSIndividual:
             dist = self.BOSSDistance(testBag, bag, best_dist)
 
             if dist < best_dist:
-                best_dist = dist;
+                best_dist = dist
                 nn = self.class_vals[n]
 
         return nn
@@ -367,296 +369,6 @@ class BOSSIndividual:
     def clean(self):
         self.transform.words = None
 
-    def setWordLen(self, wordLen):
-        self.word_length = wordLen
-        self.transform.word_length = wordLen
-
-class BOSSTransform():
-    """ Boss Transform for a fixed set of parameters
-    An internal class not to be used outside of the BOSS transform
-    """
-    def __init__(self, window_size, word_length, alphabet_size, norm):
-        self.words = []
-        self.breakpoints = []
-
-        self.inverse_sqrt_win_size = 1 / math.sqrt(window_size)
-        self.window_size = window_size
-        self.word_length = word_length
-        self.alphabet_size = alphabet_size
-        self.norm = norm
-
-        self.num_insts = 0
-        self.num_atts = 0
-
-    def fit(self, X):
-        """Build a histogram
-
-        Parameters
-        ----------
-        X : array-like or sparse matrix of shape = [n_samps, num_atts]
-
-        Returns
-        -------
-        self : object
-         """
-
-        self.num_insts, self.num_atts = X.shape
-        self.breakpoints = self.MCB(X)
-
-        bags = []
-
-        for i in range(self.num_insts):
-            dfts = self.MFT(X[i, :])
-            bag = {}
-            lastWord = -1
-
-            words = []
-
-            for window in range(dfts.shape[0]):
-                word = self.createWord(dfts[window])
-                words.append(word)
-                lastWord = self.addToBag(bag, word, lastWord)
-
-            self.words.append(words)
-            bags.append(bag)
-
-        return bags
-
-    def transform_single(self, series):
-        dfts = self.MFT(series)
-        bag = {}
-        lastWord = -1
-
-        for window in range(dfts.shape[0]):
-            word = self.createWord(dfts[window])
-            lastWord = self.addToBag(bag, word, lastWord)
-
-        return bag
-
-    def MCB(self, X):
-        """
-
-
-        :param X: the training data
-        :return:
-        """
-        num_windows_per_inst = math.ceil(self.num_atts / self.window_size)
-        dft = np.zeros((self.num_insts, num_windows_per_inst, int((self.word_length / 2) * 2)))
-
-        for i in range(X.shape[0]):
-            split = np.split(X[i, :], np.linspace(self.window_size, self.window_size * (num_windows_per_inst - 1),
-                                                  num_windows_per_inst - 1, dtype=np.int_))
-            split[-1] = X[i, self.num_atts - self.window_size:self.num_atts]
-
-            for n, row in enumerate(split):
-                dft[i, n] = self.discrete_fourier_transform(row)
-
-        total_num_windows = self.num_insts * num_windows_per_inst
-        breakpoints = np.zeros((self.word_length, self.alphabet_size))
-
-        for letter in range(self.word_length):
-            column = np.zeros(total_num_windows)
-
-            for inst in range(self.num_insts):
-                for window in range(num_windows_per_inst):
-                    column[(inst * num_windows_per_inst) + window] = round(dft[inst][window][letter] * 100) / 100
-
-            column = np.sort(column)
-
-            bin_index = 0
-            target_bin_depth = total_num_windows / self.alphabet_size
-
-            for bp in range(self.alphabet_size - 1):
-                bin_index += target_bin_depth
-                breakpoints[letter][bp] = column[int(bin_index)]
-
-            breakpoints[letter][self.alphabet_size - 1] = sys.float_info.max
-
-        return breakpoints
-
-    def discrete_fourier_transform(self, series):
-        """ Performs a discrete fourier transform using standard O(n^2) transform
-        if self.norm is True, then the first term of the DFT is ignored
-
-        TO DO: Use a fast fourier transform
-        Input
-        -------
-        X : The training input samples.  array-like or sparse matrix of shape = [n_samps, num_atts]
-
-        Returns
-        -------
-        1D array of fourier term, real_0,imag_0, real_1, imag_1 etc, length num_atts or
-        num_atts-2 if if self.norm is True
-        """
-
-        length = len(series)
-        outputLength = int(self.word_length / 2)
-        start = 1 if self.norm else 0
-
-        std = np.std(series)
-        if std == 0:
-            std = 1
-        normalising_factor = self.inverse_sqrt_win_size / std
-
-        #dft2 = np.array([np.sum([series[n] * math.cos(2 * math.pi * n * i / length) for n in range(length)]) for i in
-                         #range(start, start + outputLength)])
-        #print(dft2)
-
-        dft = np.zeros(outputLength * 2)
-
-        for i in range(start, start + outputLength):
-            idx = (i - start) * 2
-
-            for n in range(length):
-                dft[idx] += series[n] * math.cos(2 * math.pi * n * i / length)
-                dft[idx + 1] += -series[n] * math.sin(2 * math.pi * n * i / length)
-
-        #print(dft)
-
-        dft *= normalising_factor
-
-        return dft
-
-    def DFTunnormed(self, series):
-        length = len(series)
-        outputLength = int(self.word_length / 2)
-        start = 1 if self.norm else 0
-
-        dft = np.zeros(outputLength * 2)
-
-        for i in range(start, start + outputLength):
-            idx = (i - start) * 2
-
-            for n in range(length):
-                dft[idx] += series[n] * math.cos(2 * math.pi * n * i / length)
-                dft[idx + 1] += -series[n] * math.sin(2 * math.pi * n * i / length)
-
-        return dft
-
-    def MFT(self, series):
-        """
-
-        :param series:
-        :return:
-        """
-        startOffset = 2 if self.norm else 0
-        l = self.word_length + self.word_length % 2
-        phis = np.zeros(l)
-
-        for i in range(0, l, 2):
-            half = -(i + startOffset)/2
-            phis[i] = math.cos(2 * math.pi * half / self.window_size);
-            phis[i+1] = -math.sin(2 * math.pi * half / self.window_size)
-
-        end = max(1, len(series) - self.window_size + 1)
-        stds = self.calcIncrementalMeanStd(series, end)
-        transformed = np.zeros((end, l))
-        mftData = None
-
-        for i in range(end):
-            if i > 0:
-                for n in range(0, l, 2):
-                    real1 = mftData[n] + series[i + self.window_size - 1] - series[i - 1]
-                    imag1 = mftData[n + 1]
-                    real = real1 * phis[n] - imag1 * phis[n + 1]
-                    imag = real1 * phis[n + 1] + phis[n] * imag1
-                    mftData[n] = real
-                    mftData[n + 1] = imag
-            else:
-                mftData = self.DFTunnormed(series[0:self.window_size])
-
-            normalisingFactor = (1 / stds[i] if stds[i] > 0 else 1) * self.inverse_sqrt_win_size;
-            transformed[i] = mftData * normalisingFactor;
-
-        return transformed
-
-    def calcIncrementalMeanStd(self, series, end):
-        means = np.zeros(end)
-        stds = np.zeros(end)
-
-        sum = 0
-        squareSum = 0
-
-        for ww in range(self.window_size):
-            sum += series[ww]
-            squareSum += series[ww] * series[ww]
-
-        rWindowLength = 1 / self.window_size
-        means[0] = sum * rWindowLength
-        buf = squareSum * rWindowLength - means[0] * means[0]
-        stds[0] = math.sqrt(buf) if buf > 0 else 0
-
-        for w in range(1, end):
-            sum += series[w + self.window_size - 1] - series[w - 1]
-            means[w] = sum * rWindowLength
-            squareSum += series[w + self.window_size - 1] * series[w + self.window_size - 1] - series[w - 1] * series[w - 1]
-            buf = squareSum * rWindowLength - means[w] * means[w]
-            stds[w] = math.sqrt(buf) if buf > 0 else 0
-
-        return stds
-
-    def createWord(self, dft):
-        word = BitWord()
-
-        for i in range(self.word_length):
-            for bp in range(self.alphabet_size):
-                if dft[i] <= self.breakpoints[i][bp]:
-                    word.push(bp)
-                    break
-
-        return word
-
-    def shorten_bags(self, wordLen):
-        newBags = []
-
-        for i in range(self.num_insts):
-            bag = {}
-            lastWord = -1
-
-            for n, word in enumerate(self.words[i]):
-                newWord = BitWord(word = word.word, length = word.length)
-                newWord.shorten(16 - wordLen)
-                lastWord = self.addToBag(bag, newWord, lastWord)
-
-            newBags.append(bag)
-
-        return newBags;
-
-    def addToBag(self, bag, word, lastWord):
-        if word.word == lastWord:
-            return lastWord
-
-        if word.word in bag:
-            bag[word.word] += 1
-        else:
-            bag[word.word] = 1
-
-        return word.word
-
-
-class BitWord:
-
-    def __init__(self, word = np.int_(0), length = 0):
-        self.word = word
-        self.length = length
-
-    def push(self, letter):
-        self.word = (self.word << 2) | letter
-        self.length += 1
-
-    def shorten(self, amount):
-        self.word = self.rightShift(self.word,amount*2)
-        self.length -= amount
-
-    def wordList(self):
-        wordList = []
-        shift = 32-(self.length*2)
-
-        for i in range(self.length-1, -1, -1):
-            wordList.append(self.rightShift(self.word << shift, 32-2))
-            shift += 2
-
-        return wordList
-
-    def rightShift(self, left, right):
-        return (left % 0x100000000) >> right
+    def set_word_len(self, word_len):
+        self.word_length = word_len
+        self.transform.word_length = word_len
