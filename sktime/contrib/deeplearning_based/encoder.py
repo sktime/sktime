b33120f6016f277dc08b906385f482966c6c1ac3
James Large <james.large@uea.ac.uk>
2019-06-18 14:43:03 +0100

    encoder implemented, NOT TESTED

---
+++ sktime/contrib/deeplearning_based/encoder.py
@@ -0,0 +1,107 @@
+# Encoder
+import keras
+import keras_contrib
+import numpy as np
+import pandas as pd
+
+from sktime.utils.validation import check_X_y
+from sktime.contrib.deeplearning_based.basenetwork import BaseDeepLearner
+from sktime.contrib.deeplearning_based.basenetwork import networkTests
+
+
+class Encoder(BaseDeepLearner):
+
+    def __init__(self, dim_to_use=0, rand_seed=0, verbose=False):
+        self.verbose = verbose
+        self.dim_to_use = dim_to_use
+
+        # calced in fit
+        self.classes = None
+        self.nb_classes = -1
+        self.input_shape = None
+        self.model = None
+        self.history = None
+
+        # predefined
+        self.nb_epochs = 100
+        self.batch_size = 12
+        self.callbacks = None
+
+        self.rand_seed = rand_seed
+        self.random_state = np.random.RandomState(self.rand_seed)
+
+    def build_model(self, input_shape, nb_classes, **kwargs):
+        input_layer = keras.layers.Input(input_shape)
+
+        # conv block -1
+        conv1 = keras.layers.Conv1D(filters=128,kernel_size=5,strides=1,padding='same')(input_layer)
+        conv1 = keras_contrib.layers.InstanceNormalization()(conv1)
+        conv1 = keras.layers.PReLU(shared_axes=[1])(conv1)
+        conv1 = keras.layers.Dropout(rate=0.2)(conv1)
+        conv1 = keras.layers.MaxPooling1D(pool_size=2)(conv1)
+        # conv block -2
+        conv2 = keras.layers.Conv1D(filters=256,kernel_size=11,strides=1,padding='same')(conv1)
+        conv2 = keras_contrib.layers.InstanceNormalization()(conv2)
+        conv2 = keras.layers.PReLU(shared_axes=[1])(conv2)
+        conv2 = keras.layers.Dropout(rate=0.2)(conv2)
+        conv2 = keras.layers.MaxPooling1D(pool_size=2)(conv2)
+        # conv block -3
+        conv3 = keras.layers.Conv1D(filters=512,kernel_size=21,strides=1,padding='same')(conv2)
+        conv3 = keras_contrib.layers.InstanceNormalization()(conv3)
+        conv3 = keras.layers.PReLU(shared_axes=[1])(conv3)
+        conv3 = keras.layers.Dropout(rate=0.2)(conv3)
+        # split for attention
+        attention_data = keras.layers.Lambda(lambda x: x[:,:,:256])(conv3)
+        attention_softmax = keras.layers.Lambda(lambda x: x[:,:,256:])(conv3)
+        # attention mechanism
+        attention_softmax = keras.layers.Softmax()(attention_softmax)
+        multiply_layer = keras.layers.Multiply()([attention_softmax,attention_data])
+        # last layer
+        dense_layer = keras.layers.Dense(units=256,activation='sigmoid')(multiply_layer)
+        dense_layer = keras_contrib.layers.InstanceNormalization()(dense_layer)
+        # output layer
+        flatten_layer = keras.layers.Flatten()(dense_layer)
+        output_layer = keras.layers.Dense(units=nb_classes,activation='softmax')(flatten_layer)
+
+        model = keras.models.Model(inputs=input_layer, outputs=output_layer)
+
+        model.compile(loss='categorical_crossentropy', optimizer=keras.optimizers.Adam(0.00001),
+                      metrics=['accuracy'])
+
+        #file_path = self.output_directory + 'best_model.hdf5'
+        #model_checkpoint = keras.callbacks.ModelCheckpoint(filepath=file_path,
+        #                                                   monitor='loss', save_best_only=True)
+        #self.callbacks = [model_checkpoint]
+        self.callbacks = []
+
+        return model
+
+    def fit(self, X, y, input_checks=True, **kwargs):
+        if input_checks:
+            check_X_y(X, y)
+
+        if isinstance(X, pd.DataFrame):
+            if isinstance(X.iloc[0, self.dim_to_use], pd.Series):
+                X = np.asarray([a.values for a in X.iloc[:, 0]])
+            else:
+                raise TypeError(
+                    "Input should either be a 2d numpy array, or a pandas dataframe containing Series objects")
+
+        if len(X.shape) == 2:
+            # add a dimension to make it multivariate with one dimension
+            X = X.reshape((X.shape[0], X.shape[1], 1))
+
+        y_onehot = self.convert_y(y)
+        self.input_shape = X.shape[1:]
+
+        self.model = self.build_model(self.input_shape, self.nb_classes)
+
+        if self.verbose:
+            self.model.summary()
+
+        self.history = self.model.fit(X, y_onehot, batch_size=self.batch_size, epochs=self.nb_epochs,
+                              verbose=self.verbose, callbacks=self.callbacks)
+
+
+if __name__ == '__main__':
+    networkTests(Encoder())
