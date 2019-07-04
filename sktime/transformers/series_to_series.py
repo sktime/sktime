8c70bfd27cbf51242e91140090f7be5a7096e792
mloning <markus.loning.17@ucl.ac.uk>
2019-07-04 10:35:46 +0100

    updated transformer for deseasonalising time series

--- sktime/transformers/series_to_series.py
+++ sktime/transformers/series_to_series.py
@@ -1,9 +1,10 @@
 from sklearn.utils.validation import check_is_fitted
 from sklearn.utils.validation import check_random_state
+from statsmodels.tsa.seasonal import seasonal_decompose
 import numpy as np
 import pandas as pd
 
-from sktime.utils.validation import check_equal_index
+from sktime.utils.validation import check_equal_index, validate_sp, check_ts_array
 from sktime.utils.transformations import tabularize
 from sktime.utils.transformations import detabularize
 from sktime.utils.transformations import concat_nested_arrays
@@ -11,8 +12,6 @@ from sktime.utils.transformations import fit_trend
 from sktime.utils.transformations import remove_trend
 from sktime.utils.transformations import add_trend
 from sktime.transformers.base import BaseTransformer
-from sktime.utils.seasonality import remove_seasonality
-from sktime.utils.seasonality import add_seasonality
 
 
 __all__ = ['RandomIntervalSegmenter',
@@ -378,6 +377,7 @@ class Detrender(BaseTransformer):
     # TODO make it more efficient/vectorise to work on multiple rows simultaneously
     # TODO time index is lost and needs to be added the same after transformation
     # TODO separate fit/transform steps currently confounded in transform, fitting polynomials to data should be in fit
+    # TODO factor out getting of time series index of dataframe, also used in all forecasters and Deseasonaliser below
 
     def __init__(self, order=0, check_input=True):
 
@@ -488,7 +488,7 @@ class Detrender(BaseTransformer):
 
 
 class Deseasonaliser(BaseTransformer):
-    """A transformer that removes trend of given polynomial order from time series/panel data
+    """A transformer that removes a seasonal component from time series/panel data
 
     Parameters
     ----------
@@ -506,10 +506,17 @@ class Deseasonaliser(BaseTransformer):
     # TODO time index is lost and needs to be added the same after transformation
 
     def __init__(self, sp=1, model='additive', check_input=True):
-        self.sp = sp
-        self.model = model
+        self.sp = validate_sp(sp)
+        allowed_models = ('additive', 'multiplicative')
+        if model in allowed_models:
+            self.model = model
+        else:
+            raise ValueError(f"Allowed models are {allowed_models}, but found: {model}")
         self.check_input = check_input
-        self.si = None
+
+        self.seasonal_components_ = None
+        self._time_index = None
+        self._input_shape = None
 
     def transform(self, X, y=None):
         """Transform X.
@@ -525,27 +532,41 @@ class Deseasonaliser(BaseTransformer):
           Transformed pandas DataFrame with same number of rows and one column for each generated interval.
         """
         if self.check_input:
-            if not isinstance(X, pd.DataFrame):
-                raise ValueError(f"Input must be pandas DataFrame, but found: {type(X)}")
+            check_ts_array(X)
+            if X.shape[1] > 1:
+                raise NotImplementedError(f"Currently does not work on multiple columns, make use of ColumnTransformer "
+                                          f"instead")
 
-        if X.shape[1] > 1:
-            raise NotImplementedError(f"Currently does not work on multiple columns, make use of ColumnTransformer "
-                                      f"instead")
+        self._input_shape = X.shape
 
-        # convert into list of series/rows
-        xs = X.iloc[:, 0].tolist()
+        # when seasonal periodicity is equal to 1 return X unchanged
+        if self.sp == 1:
+            return X
 
-        n = X.shape[0]  # number of rows
-        m = X.iloc[0, 0].shape[0]  # number of time series observations
-        xt = np.zeros((n, m))
-        self.si = np.zeros((n, self.sp))
+        # keep time index as transform/inverse transform depends on it, e.g. to carry forward trend in inverse_transform
+        time_index = X.iloc[0, 0].index if hasattr(X.iloc[0, 0], 'index') else pd.RangeIndex(X.iloc[0, 0].shape[0])
+        if isinstance(time_index, (pd.PeriodIndex, pd.DatetimeIndex)):
+            raise NotImplementedError()
+        self._time_index = time_index
 
-        # remove seasonality from each series/row
-        for i, x in enumerate(xs):
-            xt[i, :], self.si[i, :] = remove_seasonality(x, sp=self.sp, model=self.model)
+        # tabularise data, assuming equal length time series
+        Xs = tabularize(X.iloc[:, 0])
+
+        # fit seasonal decomposition model
+        seasonal_components = self._fit_seasonal_decomposition_model(Xs)
+
+        # remove seasonal components from data
+        if self.model == 'additive':
+            Xt = Xs - seasonal_components
+        else:
+            Xt = Xs / seasonal_components
+
+        # keep fitted seasonal components for inverse transform, they are repeated after the first seasonal
+        # period so we only keep the components for the first seasonal period
+        self.seasonal_components_ = seasonal_components[:, :self.sp]
 
         # convert back into nested format
-        Xt = detabularize(pd.DataFrame(xt))
+        Xt = detabularize(pd.DataFrame(Xt))
         Xt.columns = X.columns
         return Xt
 
@@ -563,27 +584,71 @@ class Deseasonaliser(BaseTransformer):
           Transformed pandas DataFrame with same number of rows and one column for each generated interval.
         """
         if self.check_input:
-            if not isinstance(X, pd.DataFrame):
-                raise ValueError(f"Input must be pandas DataFrame, but found: {type(X)}")
+            check_ts_array(X)
+            if X.shape[1] > 1:
+                raise NotImplementedError(f"Currently does not work on multiple columns")
 
-        if X.shape[1] > 1:
-            raise NotImplementedError(f"Currently does not work on multiple columns")
+        # check that number of samples are the same, inverse transform depends on parameters fitted in transform and
+        # hence only works on data with the same (number of) rows
+        if not X.shape == self._input_shape:
+            raise ValueError(f"Inverse transform only works on data with the same samples as seen during transform, but"
+                             f"found: {X.shape[0]} samples != {self._input_shape[0]} samples (seen during transform)")
 
-        # convert into list of series/rows
-        xs = X.iloc[:, 0].tolist()
+        # if the seasonal periodicity is 1, return unchanged X
+        sp = self.sp
+        if sp == 1:
+            return X
 
-        n = X.shape[0]  # number of rows
-        m = X.iloc[0, 0].shape[0]  # number of time series observations
-        xit = np.zeros((n, m))
+        # check if time index is aligned with time index seen during transform
+        time_index = X.iloc[0, 0].index if hasattr(X.iloc[0, 0], 'index') else pd.RangeIndex(X.iloc[0, 0].shape[0])
+        if isinstance(time_index, (pd.PeriodIndex, pd.DatetimeIndex)):
+            raise NotImplementedError()
 
-        # remove seasonality from each series/row
-        for i, x in enumerate(xs):
-            xit[i, :] = add_seasonality(x, self.si[i, :])
+        # align seasonal components with index of X
+        if self._time_index.equals(time_index):
+            # if time index is the same as used for fitting seasonal components, simply expand it to the size of X
+            seasonal_components = self.seasonal_components_
+
+        else:
+            # if time index is not aligned, make sure to align fitted seasonal components to new index
+            seasonal_components = self._align_seasonal_components_to_index(time_index)
+
+        # expand or shorten aligned seasonal components to same size as X
+        n_obs = len(time_index)
+        if n_obs > sp:
+            n_tiles = np.int(np.ceil(n_obs / sp))
+            seasonal_components = np.tile(seasonal_components, n_tiles)
+        seasonal_components = seasonal_components[:, :n_obs]
+
+        # tabularise data for inverse transform, assuming equal length time series
+        Xs = tabularize(X.iloc[:, 0])
+
+        # inverse transform data
+        if self.model == 'additive':
+            Xit = Xs + seasonal_components
+        else:
+            Xit = Xs * seasonal_components
 
         # convert back into nested format
-        Xit = detabularize(pd.DataFrame(xit))
+        Xit = detabularize(pd.DataFrame(Xit))
         Xit.columns = X.columns
         return Xit
+
+    def _align_seasonal_components_to_index(self, time_index):
+        """Helper function to align seasonal components with new time series index"""
+        # find out by how much we have to shift seasonal_components to align with new index
+        shift = -time_index[0] % self.sp
+
+        # align seasonal components with new starting point of new time_index
+        return np.roll(self.seasonal_components_, shift=shift, axis=1)
+
+    def _fit_seasonal_decomposition_model(self, X):
+        """Fit seasonal decopmosition model and return fitted seasonal components"""
+        # statsmodels `seasonal_decompose` expects time series to be in columns, rather than rows, we therefore need to
+        # transpose X here
+        res = seasonal_decompose(X.T, model=self.model, freq=self.sp, filt=None, two_sided=True, extrapolate_trend=0)
+        seasonal_components = res.seasonal.T
+        return np.atleast_2d(seasonal_components)
 
 
 Deseasonalizer = Deseasonaliser
--- sktime/utils/testing.py
+++ sktime/utils/testing.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
+from sktime.utils.transformations import detabularise
 
 
 def generate_df_from_array(array, n_rows=10, n_cols=1):
-    """Helper function to generate nested pandas DataFrame from array"""
     return pd.DataFrame([[pd.Series(array) for _ in range(n_cols)] for _ in range(n_rows)],
                         columns=[f'col{c}' for c in range(n_cols)])
 
@@ -14,4 +14,44 @@ def generate_polynomial_series(n, order, coefs=None):
         coefs = np.ones((order + 1, 1))
 
     x = np.vander(np.arange(n), N=order + 1).dot(coefs)
-    return x
+    return x.ravel()
+
+
+def generate_time_series_data_with_trend(n_samples=1, n_obs=100, order=0, coefs=None):
+    """Helper function to generate time series/panel data with polynomial trend"""
+    samples = []
+    for i in range(n_samples):
+        s = generate_polynomial_series(n_obs, order=order, coefs=coefs)
+
+        index = np.arange(n_obs)
+        y = pd.Series(s, index=index)
+        samples.append(y)
+
+    X = pd.DataFrame(samples)
+    assert X.shape == (n_samples, n_obs)
+    return detabularise(X)
+
+
+def generate_seasonal_time_series_data_with_trend(n_samples=1, n_obs=100, order=0, sp=1, model='additive'):
+    """Helper function to generate time series/panel data with polynomial trend and seasonal component"""
+    if sp == 1:
+        return generate_time_series_data_with_trend(n_samples=n_samples, n_obs=n_obs, order=order)
+
+    samples = []
+    for i in range(n_samples):
+        # coefs = np.random.normal(scale=0.01, size=(order + 1, 1))
+        s = generate_polynomial_series(n_obs, order)
+
+        if model == 'additive':
+            s[::sp] = s[::sp] + 0.1
+        else:
+            s[::sp] = s[::sp] * 1.1
+
+        index = np.arange(n_obs)
+        y = pd.Series(s, index=index)
+        samples.append(y)
+
+    X = pd.DataFrame(samples)
+    assert X.shape == (n_samples, n_obs)
+    return detabularise(X)
+
