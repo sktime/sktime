011381f9b9ef7da480c37c7ac4700b981a3174a0
mloning <markus.loning.17@ucl.ac.uk>
2019-08-11 17:04:28 +0100

    removed changes on dev from branch

--- sktime/experiments/analysis.py
+++ sktime/experiments/analysis.py
@@ -3,8 +3,7 @@ from scipy import stats
 from scipy.stats import ranksums
 import scikit_posthocs as sp
 import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
+
 from .losses import Losses
 
 
