6de2dfcfb8abac4b3b7861332beb0651f294e75f
TonyBagnall <ajb@uea.ac.uk>
2019-09-11 10:42:07 +0100

    benchmarking

--- sktime/contrib/basic_benchmarking.py
+++ sktime/contrib/basic_benchmarking.py
@@ -188,7 +188,7 @@ def boss_benchmarking():
     for i in range(0, int(len(benchmark_datasets))):
         dataset = benchmark_datasets[i]
         print(str(i)+" problem = "+dataset+ " writing to "+ results_dir)
-        boss = db.BOSSEnsemble(max_ensemble_size=10)
+        boss = db.BOSSEnsemble()
         exp.run_experiment(overwrite=False, problem_path=data_dir, results_path=results_dir, cls_name="PythonBOSS",
                            classifier=boss,dataset=dataset, train_file=False)
 
