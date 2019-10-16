476401e54207d3a7340ce52fb61cda08efe1af07
Claudia Rincon Sanches (CMP - Visitor) <nan19nru@UEA.AC.UK>
2019-10-16 13:16:58 +0100

    Update matrix profile transformer

--- sktime/transformers/matrix_profile.py
+++ sktime/transformers/matrix_profile.py
@@ -178,13 +178,17 @@ class MatrixProfile(BaseTransformer):
         index profile for each time series of the dataset.
 
         Example of use:
-        # mp, ip = MatrixProfile().transform(X, m)
-        where X, the dataset, and m, the desired subsequence length to be used,
-        are the inputs and mp and ip, both ndarrays with the matrix profile and
-        the index profile, respectively, of each time series, are the outputs.
+        # Xt = MatrixProfile(m).transform(X)
+        X, a pandas DataFrame, is the the dataset.
+        m, an integer, is the desired subsequence length to be used.
+        Xt is the transformed X, i.e., a pandas DataFrame with the same number
+        of rows as X, but each row has the matrix profile for the corresponding time series.
     """
 
-    def transform(self, X, m):
+    def __init__(self, subs_len):
+        self.m = subs_len  # subsequence length
+
+    def transform(self, X):
         """
             Takes as input a time series dataset and returns the matrix profile
             for each single time series of the dataset.
@@ -193,8 +197,6 @@ class MatrixProfile(BaseTransformer):
             ----------
                 X: pandas.DataFrame
                    Time series dataset.
-                m: int
-                   Length of the subsequences.
 
             Output
             ------
@@ -214,8 +216,8 @@ class MatrixProfile(BaseTransformer):
         tabulariser = Tabulariser()
         X = tabulariser.transform(X)
 
-        n_subs = X.shape[1]-m+1
+        n_subs = X.shape[1]-self.m+1
 
-        Xt = pd.DataFrame(stomp_self(np.array([X.iloc[i]]), m) for i in range(0, n_instances))
+        Xt = pd.DataFrame(stomp_self(np.array([X.iloc[i]]), self.m) for i in range(0, n_instances))
 
         return Xt
