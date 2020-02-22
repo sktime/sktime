66d8d104a782d21babf0abda620d4e839c11d325
mloning <markus.loning.17@ucl.ac.uk>
2020-02-22 17:11:34 +0000

    fixed pmdarima version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ PANDAS_MIN_VERSION = "0.23.0"
 SKLEARN_MIN_VERSION = "0.21.0"
 STATSMODELS_MIN_VERSION = "0.9.0"
 SCIKIT_POSTHOCS_MIN_VERSION = "0.5.0"
-PMDARIMA_VERSION = "1.5.*"
+PMDARIMA_VERSION_MIN_VERSION = "1.5.0"
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 
@@ -67,7 +67,7 @@ INSTALL_REQUIRES = (
     'joblib>={}'.format(JOBLIB_MIN_VERSION),
     'scikit-posthocs>={}'.format(SCIKIT_POSTHOCS_MIN_VERSION),
     'statsmodels>={}'.format(STATSMODELS_MIN_VERSION),
-    'pmdarima>={}'.format(PMDARIMA_VERSION)
+    'pmdarima>={}'.format(PMDARIMA_VERSION_MIN_VERSION)
 )
 CLASSIFIERS = [
     'Intended Audience :: Science/Research',
