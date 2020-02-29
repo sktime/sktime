f14f2c647e2e464dd6bb17fce18f023ba4d4e16b
Ayushmaan <ayushmaanseth@gmail.com>
2020-02-29 14:11:17 +0000

    added feature selector and example notebook

--- sktime/transformers/summarise.py
+++ sktime/transformers/summarise.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
+from functools import partial
 from sklearn.utils.validation import check_is_fitted
+
 
 from sktime.transformers.base import BaseTransformer
 from sktime.transformers.segment import RandomIntervalSegmenter
@@ -11,6 +13,9 @@ import tsfresh
 from tsfresh import extract_features, extract_relevant_features, select_features,defaults
 from tsfresh.feature_extraction import ComprehensiveFCParameters,MinimalFCParameters,EfficientFCParameters
 from tsfresh.utilities.dataframe_functions import impute
+from tsfresh.feature_extraction.settings import from_columns
+from tsfresh.transformers.feature_selector import FeatureSelector
+from tsfresh.utilities.dataframe_functions import impute_dataframe_range, get_range_values_per_column
 
 COMPREHENSIVE = "COMPREHENSIVE"
 MINIMAL = "MINIMAL"
@@ -239,6 +244,7 @@ class TsFreshFeatureExtractor(BaseTransformer):
         self.default_fc_parameters = default_fc_parameters
         self.kind_to_fc_parameters = kind_to_fc_parameters
         self.n_jobs = n_jobs
+        self.chunksize = chunksize
         self.show_warnings = show_warnings
         self.disable_progressbar = disable_progressbar
         self.impute_function = impute_function
@@ -247,7 +253,13 @@ class TsFreshFeatureExtractor(BaseTransformer):
         self.profiling_filename = profiling_filename
         self.distributor = distributor
         self.passed_default_fc_params = None
-        self.passed_kind_to_fc_params = None
+        self.passed_kind_to_fc_params = kind_to_fc_parameters
+
+        #time series attribute
+        self.timeseries_container = None
+
+    def set_timeseries_container(self, timeseries_container):
+        self.timeseries_container = timeseries_container    
 
     def _check_default_rc_parameters(self):
 
@@ -269,7 +281,7 @@ class TsFreshFeatureExtractor(BaseTransformer):
             elif self.default_fc_parameters == EFFICIENT:
                 self.passed_default_fc_params = tsfresh.feature_extraction.settings.EfficientFCParameters()
             else:
-                raise ValueError("Unsupposrted default_fc_parameters, choose one of COMPREHENSIVE,MINIMAL or EFFICIENT")
+                raise ValueError("Unsupported default_fc_parameters, choose one of COMPREHENSIVE,MINIMAL or EFFICIENT")
 
 
         elif isinstance(self.default_fc_parameters,
@@ -280,7 +292,10 @@ class TsFreshFeatureExtractor(BaseTransformer):
 
             # TODO checks to be performed over custom parameters
         elif isinstance(self.default_fc_parameters,dict):
-            self.passed_default_fc_params = self.default_fc_parameters
+            if len(self.default_fc_parameters) == 0:
+                self.passed_default_fc_params = None
+            else:
+                self.passed_default_fc_params = self.default_fc_parameters
 
         else:
             raise ValueError("Invalid type of default_fc_parameters")
@@ -326,4 +341,173 @@ class TsFreshFeatureExtractor(BaseTransformer):
                     profiling_sorting=self.profiling_sorting,
                     distributor=self.distributor)
 
-        return impute(Xt)
+        return Xt
+
+
+    def transform_with_input(self,X,y=None):
+        #input checks
+        validate_X(X)
+
+        # set timeseries_container
+        if self.timeseries_container is None:
+            raise RuntimeError("You have to provide a time series using the set_timeseries_container function before.")
+
+        # check_X_is_univariate(X)
+        timeseries_container_converted = convert_data(self.timeseries_container)
+        
+        self._check_default_rc_parameters()
+        extracted_features = extract_features(timeseries_container_converted,
+                                                column_id="index", column_value="value", 
+                                                column_kind="column", column_sort="time_index",
+                                                default_fc_parameters=self.passed_default_fc_params,
+                                                kind_to_fc_parameters=self.passed_kind_to_fc_params,
+                                                n_jobs=self.n_jobs, show_warnings=self.show_warnings,
+                                                disable_progressbar=self.disable_progressbar,
+                                                impute_function=self.impute_function,
+                                                profile=self.profile,
+                                                profiling_filename=self.profiling_filename,
+                                                profiling_sorting=self.profiling_sorting,
+                                                distributor=self.distributor)
+
+        X_merged = pd.merge(X, extracted_features, left_index=True, right_index=True, how="left")
+
+        return X_merged
+
+
+
+class TsFreshFeatureSelector(BaseTransformer):
+    def __init__(self,
+                 filter_only_tsfresh_features=True,
+                 default_fc_parameters=None,
+                 kind_to_fc_parameters=None,
+                 chunksize=defaults.CHUNKSIZE,
+                 n_jobs=defaults.N_PROCESSES,
+                 show_warnings=defaults.SHOW_WARNINGS,
+                 disable_progressbar=defaults.DISABLE_PROGRESSBAR,
+                 profile=defaults.PROFILING,
+                 profiling_filename=defaults.PROFILING_FILENAME,
+                 profiling_sorting=defaults.PROFILING_SORTING,
+                 test_for_binary_target_binary_feature=defaults.TEST_FOR_BINARY_TARGET_BINARY_FEATURE,
+                 test_for_binary_target_real_feature=defaults.TEST_FOR_BINARY_TARGET_REAL_FEATURE,
+                 test_for_real_target_binary_feature=defaults.TEST_FOR_REAL_TARGET_BINARY_FEATURE,
+                 test_for_real_target_real_feature=defaults.TEST_FOR_REAL_TARGET_REAL_FEATURE,
+                 fdr_level=defaults.FDR_LEVEL,
+                 hypotheses_independent=defaults.HYPOTHESES_INDEPENDENT,
+                 ml_task='auto'):
+        self.filter_only_tsfresh_features = filter_only_tsfresh_features
+        self.default_fc_parameters = default_fc_parameters
+        self.kind_to_fc_parameters = kind_to_fc_parameters
+        # self.column_id = column_id
+        # self.column_sort = column_sort
+        # self.column_kind = column_kind
+        # self.column_value = column_value
+        # self.timeseries_container = timeseries_container
+        self.chunksize = chunksize
+        self.n_jobs = n_jobs
+        self.show_warnings = show_warnings
+        self.disable_progressbar = disable_progressbar
+        self.profile = profile
+        self.profiling_filename = profiling_filename
+        self.profiling_sorting = profiling_sorting
+        self.test_for_binary_target_binary_feature = test_for_binary_target_binary_feature
+        self.test_for_binary_target_real_feature = test_for_binary_target_real_feature
+        self.test_for_real_target_binary_feature = test_for_real_target_binary_feature
+        self.test_for_real_target_real_feature = test_for_real_target_real_feature
+        self.fdr_level = fdr_level
+        self.hypotheses_independent = hypotheses_independent
+        self.ml_task = ml_task
+
+
+        # extractor and selector
+        self.feature_extractor = None
+        self.feature_selector = None
+    
+    def fit(self,X,y):
+
+        #input checks
+        validate_X(X)
+
+        # if self.timeseries_container is None:
+        #     raise RuntimeError("You have to provide a time series using the set_timeseries_container function before.")
+
+        self.feature_extractor = TsFreshFeatureExtractor(
+            default_fc_parameters=self.default_fc_parameters,
+            kind_to_fc_parameters=self.kind_to_fc_parameters,
+            chunksize=self.chunksize,
+            n_jobs=self.n_jobs,
+            show_warnings=self.show_warnings,
+            disable_progressbar=self.disable_progressbar,
+            profile=self.profile,
+            profiling_filename=self.profiling_filename,
+            profiling_sorting=self.profiling_sorting)
+
+
+        self.feature_selector = FeatureSelector(
+            test_for_binary_target_binary_feature=self.test_for_binary_target_binary_feature,
+            test_for_binary_target_real_feature=self.test_for_binary_target_real_feature,
+            test_for_real_target_binary_feature=self.test_for_real_target_binary_feature,
+            test_for_real_target_real_feature=self.test_for_real_target_real_feature,
+            fdr_level=self.fdr_level,
+            hypotheses_independent=self.hypotheses_independent,
+            n_jobs=self.n_jobs,
+            chunksize=self.chunksize,
+            ml_task=self.ml_task
+        )
+
+        # Check to include old features
+        X_old = X
+
+        #Transform timseries data
+        X_transformed = self.feature_extractor.transform(X_old)
+        print(X_transformed.head())
+
+        self.col_to_max, self.col_to_min, self.col_to_median = get_range_values_per_column(X_transformed)
+
+        #Immpute timeseries data
+        X_transformed = impute_dataframe_range(X_transformed, col_to_max=self.col_to_max, col_to_median=self.col_to_median,
+                                             col_to_min=self.col_to_min)
+
+        # print("Imputed X is",X_transformed.head())
+        # Fit using transformed timeseries data and y
+        self.feature_selector.fit(X_transformed, y)
+
+        return self
+
+
+    def transform(self,X):
+        if self.feature_selector is None:
+            raise RuntimeError("You have to call fit before calling transform.")
+
+        if self.feature_selector.relevant_features is None:
+            raise RuntimeError("You have to call fit before calling transform.")
+
+        relevant_time_series_features = set(self.feature_selector.relevant_features) - set(pd.DataFrame(X).columns)
+        relevant_extraction_settings = from_columns(relevant_time_series_features)
+
+        # Set imputing strategy
+        #TODO keeo the impute strategy defaults.IMPUTE_FUNCTION or set it customised?
+        impute_function = partial(impute_dataframe_range, col_to_max=self.col_to_max,
+                                  col_to_min=self.col_to_min, col_to_median=self.col_to_median)
+        relevant_feature_extractor = TsFreshFeatureExtractor(kind_to_fc_parameters=relevant_extraction_settings,
+                                                      chunksize=self.feature_extractor.chunksize,
+                                                      n_jobs=self.feature_extractor.n_jobs,
+                                                      show_warnings=self.feature_extractor.show_warnings,
+                                                      disable_progressbar=self.feature_extractor.disable_progressbar,
+                                                      impute_function=impute_function,
+                                                      profile=self.feature_extractor.profile,
+                                                      profiling_filename=self.feature_extractor.profiling_filename,
+                                                      profiling_sorting=self.feature_extractor.profiling_sorting)
+        
+        Xt = relevant_feature_extractor.transform(X)
+
+        return Xt 
+
+
+    def fit_transform(self, X, y):
+        if y is None:
+            raise RuntimeError("You have to pass in y")
+        
+        #input checks
+        validate_X(X)
+
+        return self.fit(X, y).transform(X)
