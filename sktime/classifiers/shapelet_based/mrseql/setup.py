30d07d1c9c8637d0e3a69985761d2a1165ff1d66
Thach Le Nguyen <lenguyenthach141@gmail.com>
2020-03-18 11:52:34 +0000

    Add Mr-SEQL time series classifier (#211)
    * add mrseql classifier
    
    * fix windows installation, remove files,add input checking
    
    * small fix
    
    * fix method to check multiclass
    
    * add oneversusall class
    
    * add mrseql unit test
    
    * Fix unit test
    
    * edit ESTIMATOR
    
    * format code, add docstring, edit notebook example
    
    * minor clean

---
+++ sktime/classifiers/shapelet_based/mrseql/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3 -u
+# coding: utf-8
+
+__author__ = "Thach Le Nguyen"
+
+import numpy
+from numpy.distutils.misc_util import Configuration
+
+
+def configuration(parent_package="", top_path=None):
+    config = Configuration("mrseql", parent_package, top_path)
+
+    config.add_extension(
+        name="mrseql",
+        sources=["mrseql.pyx","seql_learn.cpp","SNode.cpp"],
+        # sources=["mrseql_wrapper.cpp"],
+        extra_compile_args=['-std=c++11'],
+        language="c++",
+        #include_dirs=[numpy.get_include()]
+    )
+    return config
+
+
+if __name__ == "__main__":
+    from numpy.distutils.core import setup
+    setup(**configuration().todict())
