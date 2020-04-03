685eee02cb26b454dd303265af189837b1dcfe3c
Ayushmaan <ayushmaanseth@gmail.com>
2020-04-03 19:35:11 +0530

    created BaseTSFRegressor and BaseTSFClassifier

---
+++ sktime/base/_classifier.py
@@ -0,0 +1,297 @@
+__author__ = ["Markus Löning","Ayushmaan Seth"]
+__all__ = ["BaseTSFRegressor"]
+
+__author__ = ["Markus Löning","Ayushmaan Seth"]
+__all__ = ["BaseTSFRegressor"]
+
+from warnings import warn
+
+import numpy as np
+from scipy.sparse import issparse
+from scipy.sparse import hstack as sparse_hstack
+
+from sklearn.utils._joblib import Parallel, delayed
+from sklearn.utils.fixes import _joblib_parallel_args
+from sklearn.ensemble.forest import MAX_INT
+from sklearn.exceptions import DataConversionWarning
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.tree._tree import DOUBLE,DTYPE
+from sklearn.utils import check_array
+from sklearn.utils import check_random_state
+from sklearn.utils._joblib import Parallel, delayed
+from sklearn.utils import check_random_state, check_array, compute_sample_weight
+from sklearn.utils.multiclass import check_classification_targets
+
+from sktime.classifiers.compose.ensemble import _parallel_build_trees
+from sktime.base import BaseEstimator,_partition_estimators
+from sktime.utils.validation import check_is_fitted
+from sktime.utils.validation.supervised import validate_X_y, check_X_is_univariate, validate_X
+
+class BaseTSFClassifier(BaseEstimator):
+
+    #TODO - Include _validate_estimator and _make_estimator
+    """
+    Base class for forests of trees.
+    """
+
+    @abstractmethod
+    def __init__(self,
+                 base_estimator,
+                 n_estimators=100,
+                 estimator_params=tuple(),
+                 bootstrap=False,
+                 oob_score=False,
+                 n_jobs=None,
+                 random_state=None,
+                 verbose=0,
+                 warm_start=False,
+                 class_weight=None,
+                 max_samples=None):
+        
+        #TODO - Is this structure correct or we need Pipelines here?
+        self.base_estimator = base_estimator
+        self.n_estimators = n_estimators
+        self.estimator_params = estimator_params
+        self.bootstrap = bootstrap
+        self.oob_score = oob_score
+        self.n_jobs = n_jobs
+        self.random_state = random_state
+        self.verbose = verbose
+        self.warm_start = warm_start
+        self.class_weight = class_weight
+        self.max_samples = max_samples
+
+
+    def fit(self, X, y, sample_weight=None):
+        """
+        Build a forest of trees from the training set (X, y).
+        Parameters
+        ----------
+        X : array-like or sparse matrix of shape (n_samples, n_features)
+            The training input samples. Internally, its dtype will be converted
+            to ``dtype=np.float32``. If a sparse matrix is provided, it will be
+            converted into a sparse ``csc_matrix``.
+        y : array-like of shape (n_samples,) or (n_samples, n_outputs)
+            The target values (class labels in classification, real numbers in
+            regression).
+        sample_weight : array-like of shape (n_samples,), default=None
+            Sample weights. If None, then samples are equally weighted. Splits
+            that would create child nodes with net zero or negative weight are
+            ignored while searching for a split in each node. In the case of
+            classification, splits are also ignored if they would result in any
+            single class carrying a negative weight in either child node.
+        Returns
+        -------
+        self : object
+        """
+        validate_X_y(X, y)
+
+        # Validate or convert input data
+        if sample_weight is not None:
+            sample_weight = check_array(sample_weight, ensure_2d=False)
+        if issparse(X):
+            # Pre-sort indices to avoid that each individual tree of the
+            # ensemble sorts the indices.
+            X.sort_indices()
+
+        # Remap output
+        self.n_features_ = X.shape[1] if X.ndim == 2 else 1
+
+        y = np.atleast_1d(y)
+        if y.ndim == 2 and y.shape[1] == 1:
+            warn("A column-vector y was passed when a 1d array was"
+                 " expected. Please change the shape of y to "
+                 "(n_samples,), for example using ravel().",
+                 DataConversionWarning, stacklevel=2)
+
+        if y.ndim == 1:
+            # reshape is necessary to preserve the data contiguity against vs
+            # [:, np.newaxis] that does not.
+            y = np.reshape(y, (-1, 1))
+
+        self.n_outputs_ = y.shape[1]
+
+        y, expanded_class_weight = self._validate_y_class_weight(y)
+
+        if getattr(y, "dtype", None) != DOUBLE or not y.flags.contiguous:
+            y = np.ascontiguousarray(y, dtype=DOUBLE)
+
+        if expanded_class_weight is not None:
+            if sample_weight is not None:
+                sample_weight = sample_weight * expanded_class_weight
+            else:
+                sample_weight = expanded_class_weight
+
+        # Check parameters
+        self._validate_estimator()
+
+        if not self.bootstrap and self.oob_score:
+            raise ValueError("Out of bag estimation only available"
+                             " if bootstrap=True")
+
+        random_state = check_random_state(self.random_state)
+
+        if not self.warm_start or not hasattr(self, "estimators_"):
+            # Free allocated memory, if any
+            self.estimators_ = []
+
+        n_more_estimators = self.n_estimators - len(self.estimators_)
+
+        if n_more_estimators < 0:
+            raise ValueError('n_estimators=%d must be larger or equal to '
+                             'len(estimators_)=%d when warm_start==True'
+                             % (self.n_estimators, len(self.estimators_)))
+
+        elif n_more_estimators == 0:
+            warn("Warm-start fitting without increasing n_estimators does not "
+                 "fit new trees.")
+        else:
+            if self.warm_start and len(self.estimators_) > 0:
+                # We draw from the random state to get the random state we
+                # would have got if we hadn't used a warm_start.
+                random_state.randint(MAX_INT, size=len(self.estimators_))
+
+            trees = [self._make_estimator(append=False,
+                                          random_state=random_state)
+                     for i in range(n_more_estimators)]
+
+            # Parallel loop: for standard random forests, the threading
+            # backend is preferred as the Cython code for fitting the trees
+            # is internally releasing the Python GIL making threading more
+            # efficient than multiprocessing in that case. However, in this case,
+            # for fitting pipelines in parallel, multiprocessing is more efficient.
+            trees = Parallel(n_jobs=self.n_jobs, verbose=self.verbose)(
+                delayed(_parallel_build_trees)(
+                    t, self, X, y, sample_weight, i, len(trees),
+                    verbose=self.verbose, class_weight=self.class_weight)
+                for i, t in enumerate(trees))
+
+            # Collect newly grown trees
+            self.estimators_.extend(trees)
+
+        if self.oob_score:
+            self._set_oob_score(X, y)
+
+        return self
+
+
+    def predict_proba(self, X):
+        """Predict class probabilities for X.
+        The predicted class probabilities of an input sample are computed as
+        the mean predicted class probabilities of the trees in the forest. The
+        class probability of a single tree is the fraction of samples of the same
+        class in a leaf.
+        Parameters
+        ----------
+        X : array-like or sparse matrix of shape = [n_samples, n_features]
+            The input samples. Internally, its dtype will be converted to
+            ``dtype=np.float32``. If a sparse matrix is provided, it will be
+            converted into a sparse ``csr_matrix``.
+        Returns
+        -------
+        p : array of shape = [n_samples, n_classes], or a list of n_outputs
+            such arrays if n_outputs > 1.
+            The class probabilities of the input samples. The order of the
+            classes corresponds to that in the attribute `classes_`.
+        """
+        check_is_fitted(self, 'estimators_')
+
+        # Check data
+        validate_X(X)
+        check_X_is_univariate(X)
+        X = self._validate_X_predict(X)
+
+        # Assign chunk of trees to jobs
+        n_jobs, _, _ = _partition_estimators(self.n_estimators, self.n_jobs)
+
+        all_proba = Parallel(n_jobs=n_jobs, verbose=self.verbose)(delayed(e.predict_proba)(X) for e in self.estimators_)
+
+        return np.sum(all_proba, axis=0) / len(self.estimators_)
+
+    def apply(self, X):
+        """
+        Apply trees in the forest to X, return leaf indices.
+        Parameters
+        ----------
+        X : {array-like or sparse matrix} of shape (n_samples, n_features)
+            The input samples. Internally, its dtype will be converted to
+            ``dtype=np.float32``. If a sparse matrix is provided, it will be
+            converted into a sparse ``csr_matrix``.
+        Returns
+        -------
+        X_leaves : array_like, shape = [n_samples, n_estimators]
+            For each datapoint x in X and for each tree in the forest,
+            return the index of the leaf x ends up in.
+        """
+        raise NotImplementedError()
+
+    def decision_path(self, X):
+        """
+        Return the decision path in the forest.
+        .. versionadded:: 0.18
+        Parameters
+        ----------
+        X : {array-like or sparse matrix} of shape (n_samples, n_features)
+            The input samples. Internally, its dtype will be converted to
+            ``dtype=np.float32``. If a sparse matrix is provided, it will be
+            converted into a sparse ``csr_matrix``.
+        Returns
+        -------
+        indicator : sparse csr array, shape = [n_samples, n_nodes]
+            Return a node indicator matrix where non zero elements
+            indicates that the samples goes through the nodes.
+        n_nodes_ptr : array of size (n_estimators + 1, )
+            The columns from indicator[n_nodes_ptr[i]:n_nodes_ptr[i+1]]
+            gives the indicator value for the i-th estimator.
+        """
+        X = self._validate_X_predict(X)
+        indicators = Parallel(n_jobs=self.n_jobs, verbose=self.verbose,
+                              **_joblib_parallel_args(prefer='threads'))(
+            delayed(tree.decision_path)(X, check_input=False)
+            for tree in self.estimators_)
+
+        n_nodes = [0]
+        n_nodes.extend([i.shape[1] for i in indicators])
+        n_nodes_ptr = np.array(n_nodes).cumsum()
+
+        return sparse_hstack(indicators).tocsr(), n_nodes_ptr
+
+    @abstractmethod
+    def _set_oob_score(self, X, y):
+        """
+        Calculate out of bag predictions and score."""
+
+    def _validate_y_class_weight(self, y):
+        raise NotImplementedError()
+
+    def _validate_X_predict(self, X):
+        """
+        Validate X whenever one tries to predict, apply, predict_proba."""
+        check_is_fitted(self)
+        return self.estimators_[0]._validate_X_predict(X, check_input=True)
+
+    @property
+    def feature_importances_(self):
+        """
+        Return the feature importances (the higher, the more important the
+           feature).
+        Returns
+        -------
+        feature_importances_ : array, shape = [n_features]
+            The values of this array sum to 1, unless all trees are single node
+            trees consisting of only the root node, in which case it will be an
+            array of zeros.
+        """
+        check_is_fitted(self)
+
+        all_importances = Parallel(n_jobs=self.n_jobs,
+                                   **_joblib_parallel_args(prefer='threads'))(
+            delayed(getattr)(tree, 'feature_importances_')
+            for tree in self.estimators_ if tree.tree_.node_count > 1)
+
+        if not all_importances:
+            return np.zeros(self.n_features_, dtype=np.float64)
+
+        all_importances = np.mean(all_importances,
+                                  axis=0, dtype=np.float64)
+        return all_importances / np.sum(all_importances)
