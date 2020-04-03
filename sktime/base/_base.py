3995eae06ec098ad0f7dd93cedd3ff624e5be3d0
Ayushmaan <ayushmaanseth@gmail.com>
2020-04-03 19:35:11 +0530

    created BaseTSFRegressor and BaseTSFClassifier

--- sktime/base/_base.py
+++ sktime/base/_base.py
@@ -1,21 +1,11 @@
-#!/usr/bin/env python3 -u
-# coding: utf-8
-
 __author__ = ["Markus Löning"]
 __all__ = ["BaseEstimator"]
 
 from sklearn.base import BaseEstimator as _BaseEstimator
-from sktime.exceptions import NotFittedError
+from sktime.utils.exceptions import NotFittedError
 
 
 class BaseEstimator(_BaseEstimator):
-
-    # def __init__(self, *args, **kwargs):
-    #     # Including args and kwargs make the class cooperative, so that args
-    #     # and kwargs are passed on to other parent classes when using
-    #     # multiple inheritance
-    #     self._is_fitted = False
-    #     super(BaseEstimator, self).__init__(*args, **kwargs)
 
     def __init__(self):
         self._is_fitted = False
@@ -26,14 +16,13 @@ class BaseEstimator(_BaseEstimator):
         return self._is_fitted
 
     def check_is_fitted(self):
-        """Check if the estimator has been fitted.
-
+        """Check if the forecaster has been fitted.
         Raises
         ------
         NotFittedError
-            If the estimator has not been fitted yet.
+            if the forecaster has not been fitted yet.
         """
         if not self.is_fitted:
-            raise NotFittedError(
-                f"This instance of {self.__class__.__name__} has not "
-                f"been fitted yet; please call `fit` first.")
+            raise NotFittedError(f"This instance of {self.__class__.__name__} \
+                                has not "f"been fitted yet; please call `fit` \
+                                    first.")
