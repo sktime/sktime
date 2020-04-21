93aef55fdd70296d9938963796f2c2e74affba7d
mloning <markus.loning.17@ucl.ac.uk>
2020-04-21 08:43:11 +0100

    more changes

--- maint_tools/linting.sh
+++ maint_tools/linting.sh
@@ -3,7 +3,9 @@
 set -e -x
 set -o pipefail
 
-flake8 --verbose --filename=*.py sktime/
-if [ $? -ne 0 ]; then
-        exit 1
+ls -lh sktime/
+
+if ! flake8 --verbose --filename=*.py sktime/; then
+  echo 'Linting failed.'
+  exit 1
 fi
