c56c7131dfde996c3c517b28eb9560049e810294
Ayushmaan <ayushmaanseth@gmail.com>
2020-05-19 01:20:59 +0530

    fixed __base error

---
+++ sktime/base/_base.py
@@ -0,0 +1,27 @@
+__author__ = ["Markus Löning"]
+__all__ = ["BaseEstimator"]
+
+from sklearn.base import BaseEstimator as _BaseEstimator
+from sktime.exceptions import NotFittedError
+
+
+class BaseEstimator(_BaseEstimator):
+
+    def __init__(self):
+        self._is_fitted = False
+
+    @property
+    def is_fitted(self):
+        """Has `fit` been called?"""
+        return self._is_fitted
+
+    def check_is_fitted(self):
+        """Check if the forecaster has been fitted.
+        Raises
+        ------
+        NotFittedError
+            if the forecaster has not been fitted yet.
+        """
+        if not self.is_fitted:
+            raise NotFittedError(f"This instance of {self.__class__.__name__} \
+                has not "f"been fitted yet; please call `fit` first.")
