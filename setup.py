278541b9afce552c20c6aba5bd94acb5cb7cd8e1
mloning <markus.loning.17@ucl.ac.uk>
2020-05-28 13:28:24 +0100

    minor changes

--- setup.py
+++ setup.py
@@ -97,7 +97,8 @@ EXTRAS_REQUIRE = {
         'sphinx_rtd_theme',
         'numpydoc',
         'matplotlib',
-        'sphinx_bootstrap_theme'
+        'sphinx_bootstrap_theme',
+        'm2r @ git+https://github.com/crossnox/m2r@dev#egg=m2r'
     ]
 }
 SETUP_REQUIRES = [
--- sktime/series_as_features/compose/__init__.py
+++ sktime/series_as_features/compose/__init__.py
@@ -3,10 +3,7 @@
 
 __author__ = ["Markus Löning"]
 __all__ = [
-    "ColumnEnsembleClassifier",
     "FeatureUnion"
 ]
 
-from sktime.series_as_features.compose._column_ensemble import \
-    ColumnEnsembleClassifier
 from sktime.series_as_features.compose._pipeline import FeatureUnion
