61fe13402c5a45d0a54e66f9adb054aadab238e4
DmitriyValetov <dmitriy.valetov@phystech.edu>
2020-05-29 12:30:38 +0300

    reformat

--- sktime/transformers/series_as_features/interpolate.py
+++ sktime/transformers/series_as_features/interpolate.py
@@ -1,7 +1,8 @@
 import numpy as np
 from scipy import interpolate
 
-from sktime.transformers.series_as_features.base import BaseSeriesAsFeaturesTransformer
+from sktime.transformers.series_as_features.base \
+    import BaseSeriesAsFeaturesTransformer
 from sktime.utils.validation.series_as_features import check_X
 
 
