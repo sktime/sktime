bd050d986e2c38c193fd30056060f33c7cdca2e3
vrq16kku <vrq16kku@uea.ac.uk>
2020-06-19 17:58:12 +0100

    implemented weighting_factor

--- sktime/contrib/shape_dtw/shape_dtw.py
+++ sktime/contrib/shape_dtw/shape_dtw.py
@@ -4,7 +4,11 @@ import pandas as pd
 import math
 from sktime.utils.validation.series_as_features import check_X,check_X_y
 from sktime.utils.load_data import load_from_tsfile_to_dataframe as load_ts
+from sktime.utils.data_container import tabularize
 
+#Tuning
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import KFold
 
 #Transforms
 from sktime.contrib.shape_dtw.transformers.SubsequenceTransformer import SubsequenceTransformer
@@ -54,8 +58,10 @@ class ShapeDTW(BaseClassifier):
                                          = scaling_factor_hog1d (default=0.1)
         
         - 'compound'            : use a combination of two shape descriptors simultaneously.
-                                - params = weighting_factor (default=1)
+                                - params = weighting_factor (default=None)
                                            Defines how to adjust values of a shape descriptor such that the final vectors become (shape_desc1,weighting_factor*shape_desc2)
+                                           If a value is not given, then the appropriate weighting_factor value is tuned by 10-fold cross-validation
+                                           on the training data.
                                 
         
     shape_descriptor_functions  : string list, only applicable when the shape_descriptor_function is set to 'compound'. Use a list of
@@ -63,15 +69,11 @@ class ShapeDTW(BaseClassifier):
                                   
     metric_params               : dictionary for metric parameters (default = None).
     """
-    def __init__(self,n_neighbours=1,subsequence_length=None,shape_descriptor_function='raw',shape_descriptor_functions=None,metric_params=None):
-        self.n_neighbours=n_neighbours
+    def __init__(self,n_neighbors=1,subsequence_length=None,shape_descriptor_function='raw',shape_descriptor_functions=None,metric_params=None):
+        self.n_neighbors=n_neighbors
         self.subsequence_length=subsequence_length
-        self.shape_descriptor_function=shape_descriptor_function.lower()
-        #Convert all strings in list to lowercase.
-        if shape_descriptor_functions is not None:
-            self.shape_descriptor_functions=[x.lower() for x in shape_descriptor_functions]
-        else:
-            self.shape_descriptor_functions = None
+        self.shape_descriptor_function=shape_descriptor_function
+        self.shape_descriptor_functions=shape_descriptor_functions
         self.metric_params=metric_params
         super(ShapeDTW, self).__init__()
         
@@ -86,8 +88,20 @@ class ShapeDTW(BaseClassifier):
     self : object
     """
     def fit(self, X, y):
+        #Perform preprocessing on params.
+        self.shape_descriptor_function=self.shape_descriptor_function.lower()
+        #Convert all strings in list to lowercase.
+        if self.shape_descriptor_functions is not None:
+            self.shape_descriptor_functions=[x.lower() for x in self.shape_descriptor_functions]
+        else:
+            self.shape_descriptor_functions = None
+        
         X,y= check_X_y(X,y,enforce_univariate=False)
-            
+        
+        #If the shape descriptor is 'compound', calculate the appropriate weighting_factor
+        if self.shape_descriptor_function == "compound":
+            self.calculateWeightingFactorValue(X,y)
+        
         self.trainData = X
         self.trainDataClasses = y
         
@@ -110,11 +124,55 @@ class ShapeDTW(BaseClassifier):
         self.trainData = self.generateShapeDescriptors(self.sequences,num_insts,num_atts)
         
         #Fit the kNN classifier
-        self.knn = KNeighborsTimeSeriesClassifier(self.n_neighbours)
+        self.knn = KNeighborsTimeSeriesClassifier(self.n_neighbors)
         self.knn.fit(self.trainData,self.trainDataClasses)
         self.classes_ = self.knn.classes_
         
         return self 
+        
+    """
+    Helper function for calculating the appropriate weighting_factor for the compound shape descriptor.
+    If a value is given, the weighting_factor is set as the given value. If not, its tuned via
+    a 10-fold cross-validation on the training data.
+    """
+    def calculateWeightingFactorValue(self,X,y):
+    
+        if self.metric_params is None:
+            self.metric_params={}
+        
+        self.metric_params = {k.lower(): v for k, v in self.metric_params.items()}
+            
+        #Get the weighting_factor if one is provided
+        if self.metric_params.get("weighting_factor") is not None:
+            self.weighting_factor=self.metric_params.get("weighting_factor")
+        else:
+            #Tune it otherwise
+            self._param_matrix = {
+                'metric_params': [{'weighting_factor': 0.1},
+                                  {'weighting_factor': 0.125},
+                                  {'weighting_factor': (1/6)},
+                                  {'weighting_factor': 0.25},
+                                  {'weighting_factor': 0.5},
+                                  {'weighting_factor': 1},
+                                  {'weighting_factor': 2},
+                                  {'weighting_factor': 4},
+                                  {'weighting_factor': 6},
+                                  {'weighting_factor': 8},
+                                  {'weighting_factor': 10}]}
+                                  
+            grid = GridSearchCV(
+                estimator=ShapeDTW(n_neighbors=self.n_neighbors,
+                                   subsequence_length=self.subsequence_length,
+                                   shape_descriptor_function=self.shape_descriptor_function,
+                                   shape_descriptor_functions=self.shape_descriptor_functions,
+                                   metric_params=self.metric_params),
+                param_grid=self._param_matrix,
+                cv=KFold(n_splits=10,shuffle=True),
+                scoring='accuracy'
+            )
+            grid.fit(X,y)
+            self.weighting_factor = grid.best_params_['metric_params']['weighting_factor']
+            
         
 
     """
@@ -168,7 +226,6 @@ class ShapeDTW(BaseClassifier):
     """
     def predict(self, X):
         X = check_X(X,enforce_univariate=False)
-        
         self.testData = X
         
         #get the number of attributes and instances
@@ -188,7 +245,6 @@ class ShapeDTW(BaseClassifier):
         #Classify the test data
         return self.knn.predict(self.testData)
         
-        
     """
     This function is used to convert a list of subsequences into a list of shape descriptors to be used for classification.
     """
@@ -205,8 +261,9 @@ class ShapeDTW(BaseClassifier):
             if not (len(self.transformer)==2):
                 raise ValueError("When using 'compound', shape_descriptor_functions must be a string array of length 2.")
         
+        #To hold the result of each transformer
         dataFrames = []
-        col_names = [x for x in range(len(data.columns)*len(self.transformer))]
+        col_names = [x for x in range(len(data.columns))]
         
         #Apply each transformer on the set of subsequences
         for t in self.transformer:
@@ -219,9 +276,12 @@ class ShapeDTW(BaseClassifier):
                 newData = t.transform(data)
                 dataFrames.append(newData)
                 
-        #Combine the dataframes together
-        result = pd.concat(dataFrames, axis=1, sort=False)
-        result.columns=col_names
+        #Combine the arrays into one dataframe
+        if self.shape_descriptor_function == "compound":
+            result = combineDataFrames(dataFrames,self.weighting_factor,col_names)
+        else:    
+            result = dataFrames[0]
+            result.columns=col_names
             
         return result
         
@@ -238,14 +298,7 @@ class ShapeDTW(BaseClassifier):
     """
     def getTransformer(self,tName):
         parameters = self.metric_params
-        
-        if parameters is None:
-            parameters={}
-        
-        parameters = {k.lower(): v for k, v in parameters.items()}
-        #Get the weighting_factor if one is provided
-        self.weighting_factor=parameters.get("weighting_factor")
-        
+    
         if tName == "raw":
             return None
         elif tName == "paa":
@@ -295,8 +348,53 @@ class ShapeDTW(BaseClassifier):
         else:
             raise ValueError("Invalid shape desciptor function.")
     
+"""
+Helper function for the shape_dtw class to combine two dataframes together into a single dataframe.
+Used when the shape_descriptor_function is set to "compound".
+"""
+def combineDataFrames(dataFrames,weighting_factor,col_names):
+    first_desc=dataFrames[0]
+    second_desc=dataFrames[1]
+    
+    first_desc_array = []
+    second_desc_array = []
+    
+    # Convert the dataframes into arrays
+    for x in first_desc.columns:
+        first_desc_array.append(tabularize(first_desc[x],return_array=True))
+        
+    for x in second_desc.columns:
+        second_desc_array.append(tabularize(second_desc[x],return_array=True))
+    
+    # Concatenate the arrays together
+    res = []
+    for x in range(len(first_desc_array)):
+        dim1 = []
+        for y in range(len(first_desc_array[x])):
+            dim2 = []
+            dim2.extend(first_desc_array[x][y])
+            dim2.extend(second_desc_array[x][y]*weighting_factor)
+            dim1.append(dim2)
+        res.append(dim1)
+        
+    res = np.asarray(res)
+        
+    # Convert to pandas dataframe
+    df = pd.DataFrame()
+    
+    for col in col_names:
+        colToAdd = []
+        for row in range(len(res[col])):
+            inst = res[col][row]
+            colToAdd.append(pd.Series(inst)) 
+        df[col] = colToAdd
+        
+    return df
  
 if __name__ == "__main__":
+    #trainPath="C:\\Users\\Vince\\Documents\\Dissertation Repositories\\datasets\\Test\\Test\\Test_TRAIN.ts"
+    #testPath="C:\\Users\\Vince\\Documents\\Dissertation Repositories\\datasets\\Test\\Test\\Test_TEST.ts"
+    
     trainPath="C:\\Users\\Vince\\Documents\\Dissertation Repositories\\datasets\\Univariate2018_ts\\Chinatown\\Chinatown_TRAIN.ts"
     testPath="C:\\Users\\Vince\\Documents\\Dissertation Repositories\\datasets\\Univariate2018_ts\\Chinatown\\Chinatown_TEST.ts"
     
@@ -306,6 +404,6 @@ if __name__ == "__main__":
     trainData,trainDataClasses =  load_ts(trainPath)
     testData,testDataClasses =  load_ts(testPath)
     
-    shp = ShapeDTW(n_neighbours=1,subsequence_length=30,shape_descriptor_function="dwt",shape_descriptor_functions=["raw","hog1d"],metric_params={"num_intervals_hog1d":2,"num_bins_hog1d":8,"scaling_factor_hog1d":0.1,"num_levels_dwt":3,"weighting_factor":1})
+    shp = ShapeDTW(n_neighbors=1,subsequence_length=10,shape_descriptor_function="compound",shape_descriptor_functions=["raw","hog1d"],metric_params={"num_intervals_hog1d":2,"num_bins_hog1d":8,"scaling_factor_hog1d":0.1,"num_levels_dwt":3,"weighting_factor":None})
     shp.fit(trainData,trainDataClasses)
     print(shp.score(testData,testDataClasses))
