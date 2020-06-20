2d78aced0d9687d20e5d9d2da5c49557f7b19380
dicecko <dicecko@gmail.com>
2020-06-20 14:58:51 +0200

    Fixed formatting docstring to AutoARIMA class in Arima.py

--- sktime/forecasting/arima.py
+++ sktime/forecasting/arima.py
@@ -48,97 +48,91 @@ class AutoARIMA(OptionalForecastingHorizonMixin, BaseSktimeForecaster):
     ----------
     start_p : int, optional (default=2)
         The starting value of p, the order (or number of time lags)
-            of the auto-regressive (“AR”) model. Must be a positive integer.
-    d : int, optional
+        of the auto-regressive (“AR”) model. Must be a positive integer.
+    d : int, optional (default=None)
         The order of first-differencing. If None (by default), the value will
         automatically be selected based on the results of the test (i.e.,
         either the Kwiatkowski–Phillips–Schmidt–Shin, Augmented Dickey-Fuller
         or the Phillips–Perron test will be conducted to find the most probable
         value). Must be a positive integer or None. Note that if d is None,
-        the runtime could be significantly longer., by default None
-    start_q : int, optional
+        the runtime could be significantly longer.
+    start_q : int, optional (default=2)
         The starting value of q, the order of the moving-average (“MA”) model.
-        Must be a positive integer., by default 2
-    max_p : int, optional
+        Must be a positive integer.
+    max_p : int, optional (default=5)
         The maximum value of p, inclusive. Must be a positive integer greater
-        than or equal to start_p., by default 5
-    max_d : int, optional
+        than or equal to start_p.
+    max_d : int, optional (default=2)
         The maximum value of d, or the maximum number of non-seasonal
-        differences. Must be a positive integer greater than or equal to d.,
-        by default 2
-    max_q : int, optional
+        differences. Must be a positive integer greater than or equal to d.
+    max_q : int, optional (default=5)
         he maximum value of q, inclusive. Must be a positive integer greater
-        than start_q., by default 5
-    start_P : int, optional
+        than start_q.
+    start_P : int, optional (default=1)
         The starting value of P, the order of the auto-regressive portion of
-        the seasonal model., by default 1
-    D : int, optional
+        the seasonal model.
+    D : int, optional (default=None)
         The order of the seasonal differencing. If None (by default, the value
         will automatically be selected based on the results of the
-        seasonal_test. Must be a positive integer or None., by default None
-    start_Q : int, optional
+        seasonal_test. Must be a positive integer or None.
+    start_Q : int, optional (default=1)
         The starting value of Q, the order of the moving-average portion of
-        the seasonal model., by default 1
-    max_P : int, optional
+        the seasonal model.
+    max_P : int, optional (default=2)
         The maximum value of P, inclusive. Must be a positive integer greater
-        than start_P., by default 2
-    max_D : int, optional
-        The maximum value of D. Must be a positive integer greater than D.,
-        by default 1
-    max_Q : int, optional
+        than start_P. 
+    max_D : int, optional (default=1)
+        The maximum value of D. Must be a positive integer greater than D.
+    max_Q : int, optional (default=2)
         The maximum value of Q, inclusive. Must be a positive integer greater
-        than start_Q., by default 2
-    max_order : int, optional
+        than start_Q.
+    max_order : int, optional (default=5)
         Maximum value of p+q+P+Q if model selection is not stepwise. If the
         sum of p and q is >= max_order, a model will not be fit with those
         parameters, but will progress to the next combination. Default is 5.
         If max_order is None, it means there are no constraints on maximum
-        order., by default 5
-    sp : int, optional
+        order.
+    sp : int, optional (default=1)
         The period for seasonal differencing, sp refers to the number of
         periods in each season. For example, sp is 4 for quarterly data, 12
         for monthly data, or 1 for annual (non-seasonal) data. Default is 1.
         Note that if sp == 1 (i.e., is non-seasonal), seasonal will be set to
         False. For more information on setting this parameter, see Setting sp.
-        (link to http://alkaline-ml.com/pmdarima/tips_and_tricks.html#period),
-        by default 1
-    seasonal : bool, optional
+        (link to http://alkaline-ml.com/pmdarima/tips_and_tricks.html#period)
+    seasonal : bool, optional (default=True)
         Whether to fit a seasonal ARIMA. Default is True. Note that if
-        seasonal is True and m == 1, seasonal will be set to False., by
-        default True
-    stationary : bool, optional
-        Whether the time-series is stationary and d should be set to zero., by
-        default False
-    information_criterion : str, optional
+        seasonal is True and sp == 1, seasonal will be set to False.
+    stationary : bool, optional (default=False)
+        Whether the time-series is stationary and d should be set to zero.
+    information_criterion : str, optional (default='aic')
         The information criterion used to select the best ARIMA model. One of
-        pmdarima.arima.auto_arima.VALID_CRITERIA, (‘aic’, ‘bic’, ‘hqic’, ‘oob’)
-        , by default 'aic'
-    alpha : float, optional
-        Level of the test for testing significance., by default 0.05
-    test : str, optional
+        pmdarima.arima.auto_arima.VALID_CRITERIA, (‘aic’, ‘bic’, ‘hqic’, ‘oob’).
+    alpha : float, optional (default=0.05)
+        Level of the test for testing significance.
+    test : str, optional (default='kpss')
         Type of unit root test to use in order to detect stationarity if
-        stationary is False and d is None., by default 'kpss'
-    seasonal_test : str, optional
+        stationary is False and d is None.
+    seasonal_test : str, optional (default='ocsb')
         This determines which seasonal unit root test is used if seasonal is
-        True and D is None., by default 'ocsb'
-    stepwise : bool, optional
+        True and D is None.
+    stepwise : bool, optional (default=True)
         Whether to use the stepwise algorithm outlined in Hyndman and
         Khandakar (2008) to identify the optimal model parameters. The
         stepwise algorithm can be significantly faster than fitting all (or a
         random subset of) hyper-parameter combinations and is less likely to
-        over-fit the model., by default True
-    n_jobs : int, optional
+        over-fit the model.
+    n_jobs : int, optional (default=1)
         The number of models to fit in parallel in the case of a grid search
         (stepwise=False). Default is 1, but -1 can be used to designate “as
-        many as possible”., by default 1
-    start_params : array-like, optional
+        many as possible”.
+    start_params : array-like, optional (default=None)
         Starting parameters for ARMA(p,q). If None, the default is given by
-        ARMA._fit_start_params., by default None
-    trend : str, optional
+        ARMA._fit_start_params.
+    trend : str, optional (default=None)
         The trend parameter. If with_intercept is True, trend will be used. If
         with_intercept is False, the trend will be set to a no- intercept
-        value., by default None
-    method : str, optional
+        value.
+    method : str, optional (default='lbfgs')
         The method determines which solver from scipy.optimize is used, and it
         can be chosen from among the following strings:
 
@@ -153,58 +147,51 @@ class AutoARIMA(OptionalForecastingHorizonMixin, BaseSktimeForecaster):
         The explicit arguments in fit are passed to the solver, with the
         exception of the basin-hopping solver. Each solver has several
         optional arguments that are not the same across solvers. These can be
-        passed as **fit_kwargs, by default 'lbfgs'
-                maxiter : int, optional
-                    The maximum number of function evaluations., by default 50
-                offset_test_args : dict, optional
-                    The args to pass to the constructor of the offset (d) test.
-                    See pmdarima.arima.stationarity for more details., by
-                    default None
-                seasonal_test_args : [type], optional
-                    The args to pass to the constructor of the seasonal offset
-                    (D) test. See pmdarima.arima.seasonality for more details.
-                    , by default None
-                suppress_warnings : bool, optional
-                    Many warnings might be thrown inside of statsmodels. If
-                    suppress_warnings is True, all of the warnings coming from
-                    ARIMA will be squelched., by default False
-                error_action : str, optional
-                    If unable to fit an ARIMA due to stationarity issues,
-                    whether to warn (‘warn’), raise the ValueError (‘raise’)
-                    or ignore (‘ignore’). Note that the default behavior is to
-                    warn, and fits that fail will be returned as None. This is
-                    the recommended behavior, as statsmodels ARIMA and SARIMAX
-                    models hit bugs periodically that can cause an otherwise
-                    healthy parameter combination to fail for reasons not
-                    related to pmdarima., by default 'warn'
-                trace : bool, optional
-                    Whether to print status on the fits. A value of False will
-                    print no debugging information. A value of True will print
-                    some. Integer values exceeding 1 will print increasing
-                    amounts of debug information at each fit., by default False
-                random : bool, optional
-                    Similar to grid searches, auto_arima provides the
-                    capability to perform a “random search” over a
-                    hyper-parameter space. If random is True, rather than
-                    perform an exhaustive search or stepwise search, only
-                    n_fits ARIMA models will be fit (stepwise must be False
-                    for this option to do anything)., by default False
-                random_state : int, long or numpy RandomState, optional
-                    The PRNG for when random=True. Ensures replicable testing
-                    and results., by default None
-                n_fits : int, optional
-                    If random is True and a “random search” is going to be
-                    performed, n_iter is the number of ARIMA models to be fit.
-                    , by default 10
-                out_of_sample_size : int, optional
-                    The ARIMA class can fit only a portion of the data if
-                    specified, in order to retain an “out of bag” sample score.
-                    This is the number of examples from the tail of the time
-                    series to hold out and use as validation examples. The
-                    model will not be fit on these samples, but the
-                    observations will be added into the model’s endog and exog
-                    arrays so that future forecast values originate from the
-                    end of the endogenous vector.
+        passed as **fit_kwargs
+    maxiter : int, optional (default=50)
+        The maximum number of function evaluations.
+    offset_test_args : dict, optional (default=None)
+        The args to pass to the constructor of the offset (d) test.
+        See pmdarima.arima.stationarity for more details.
+    seasonal_test_args : dict, optional (default=None)
+        The args to pass to the constructor of the seasonal offset (D) test.
+        See pmdarima.arima.seasonality for more details.
+    suppress_warnings : bool, optional (default=False)
+        Many warnings might be thrown inside of statsmodels. If
+        suppress_warnings is True, all of the warnings coming from ARIMA will
+        be squelched.
+    error_action : str, optional (default='warn')
+        If unable to fit an ARIMA due to stationarity issues, whether to warn
+        (‘warn’), raise the ValueError (‘raise’) or ignore (‘ignore’). Note
+        that the default behavior is to warn, and fits that fail will be
+        returned as None. This is the recommended behavior, as statsmodels
+        ARIMA and SARIMAX models hit bugs periodically that can cause an
+        otherwise healthy parameter combination to fail for reasons not
+        related to pmdarima.
+    trace : bool, optional (default=False)
+        Whether to print status on the fits. A value of False will print no
+        debugging information. A value of True will print some. Integer values
+        exceeding 1 will print increasing amounts of debug information at each
+        fit.
+    random : bool, optional (default='False')
+        Similar to grid searches, auto_arima provides the capability to
+        perform a “random search” over a hyper-parameter space. If random is
+        True, rather than perform an exhaustive search or stepwise search,
+        only n_fits ARIMA models will be fit (stepwise must be False for this
+        option to do anything).
+    random_state : int, long or numpy RandomState, optional (default=None)
+        The PRNG for when random=True. Ensures replicable testingand results.
+    n_fits : int, optional (default=10)
+        If random is True and a “random search” is going to be performed,
+        n_iter is the number of ARIMA models to be fit.
+    out_of_sample_size : int, optional (default=0)
+        The ARIMA class can fit only a portion of the data if specified, in
+        order to retain an “out of bag” sample score. This is the number of
+        examples from the tail of the time series to hold out and use as
+        validation examples. The model will not be fit on these samples, but
+        the observations will be added into the model’s endog and exog arrays
+        so that future forecast values originate from the end of the
+        endogenous vector.
 
         # For instance:
 
@@ -214,16 +201,13 @@ class AutoARIMA(OptionalForecastingHorizonMixin, BaseSktimeForecaster):
         # > Fit on: [0, 1, 2, 3, 4]
         # > Score on: [5, 6]
         # > Append [5, 6] to end of self.arima_res_.data.endog values,
-        # by default 0
-    scoring : str, optional
+    scoring : str, optional (default='mse')
         If performing validation (i.e., if out_of_sample_size > 0), the metric
-        to use for scoring the out-of-sample data. One of (‘mse’, ‘mae’), by
-        default 'mse'
-    scoring_args : dict, optional
-        A dictionary of key-word arguments to be passed to the scoring metric.,
-        by default None
-    with_intercept : bool, optional
-        Whether to include an intercept term. Default is True., by default True
+        to use for scoring the out-of-sample data. One of (‘mse’, ‘mae’)
+    scoring_args : dict, optional (default=None)
+        A dictionary of key-word arguments to be passed to the scoring metric.
+    with_intercept : bool, optional (default=True)
+        Whether to include an intercept term.
     """
 
     def __init__(self, start_p=2, d=None, start_q=2, max_p=5,
