6486cc4fff65e5f21d5f8d9cef755a8ac476ad5e
Piyush1729 <64950012+Piyush1729@users.noreply.github.com>
2020-08-06 17:55:23 +0530

    Add seasonal strategies to NaiveForecaster (#335)
    * Refactored seasonal last and implemented seasonal mean with help of sp param
    
    * Refactored seasonal last in example ipython
    
    * Added unit test for seasonal mean strategy and refactored unit test for seasonal last
    
    * Styled to pep8 conventions
    
    * Corrected unit test for seasonal mean in test_naive.py and added self.sp_ param for seasonal mean
    
    * Modified last_window when fh<sp
    
    * Styled to pep8
    
    * Default value for sp made 1
    
    * Implemented seasonal mean without pandas
    
    * Refactored unit test for seasonal mean
    
    * Modified documentation
    
    * Refactored unit test for seasonal mean
    
    * Refactored if condition of unit test for seasonal mean
    
    * Verified for pep8
    
    * clean up as discussed
    
    * Modified doc string
    
    * Removed local files
    
    Co-authored-by: Markus Löning <markus.loning.17@ucl.ac.uk>

--- sktime/forecasting/tests/test_naive.py
+++ sktime/forecasting/tests/test_naive.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 # copyright: sktime developers, BSD-3-Clause License (see LICENSE file)
 
-__author__ = "Markus Löning"
+__author__ = ["Markus Löning", "Piyush Gade"]
 
 import numpy as np
 import pandas as pd
@@ -45,17 +45,67 @@ def test_strategy_mean(fh, window_length):
 
 @pytest.mark.parametrize("fh", TEST_OOS_FHS)
 @pytest.mark.parametrize("sp", TEST_SPS)
-def test_strategy_seasonal_last(fh, sp):
-    f = NaiveForecaster(strategy="seasonal_last", sp=sp)
+def test_strategy_last_seasonal(fh, sp):
+    f = NaiveForecaster(strategy="last", sp=sp)
     f.fit(y_train)
     y_pred = f.predict(fh)
 
     # check predicted index
     np.testing.assert_array_equal(y_train.index[-1] + check_fh(fh),
                                   y_pred.index)
-
     # check values
     fh = check_fh(fh)  # get well formatted fh
     reps = np.int(np.ceil(max(fh) / sp))
     expected = np.tile(y_train.iloc[-sp:], reps=reps)[fh - 1]
+    np.testing.assert_array_equal(y_pred, expected)
+
+
+@pytest.mark.parametrize("fh", TEST_OOS_FHS)
+@pytest.mark.parametrize("sp", TEST_SPS)
+@pytest.mark.parametrize("window_length", [*TEST_WINDOW_LENGTHS, None])
+def test_strategy_mean_seasonal(fh, sp, window_length):
+    if ((window_length is not None and window_length > sp) or
+            (window_length is None)):
+        f = NaiveForecaster(strategy="mean", sp=sp,
+                            window_length=window_length)
+        f.fit(y_train)
+        y_pred = f.predict(fh)
+
+        # check predicted index
+        np.testing.assert_array_equal(y_train.index[-1] + check_fh(fh),
+                                      y_pred.index)
+
+        if window_length is None:
+            window_length = len(y_train)
+
+        # check values
+        fh = check_fh(fh)  # get well formatted fh
+        reps = np.int(np.ceil(max(fh) / sp))
+        last_window = y_train.iloc[-window_length:].values
+        last_window = np.pad(last_window, (0, sp - len(last_window) % sp),
+                             'constant', constant_values=np.nan)
+
+        last_window = last_window.reshape(np.int(np.ceil(len(last_window) /
+                                                         sp)), sp)
+        expected = np.tile(np.nanmean(last_window, axis=0), reps=reps)[fh - 1]
+        np.testing.assert_array_equal(y_pred, expected)
+
+
+@pytest.mark.parametrize("n_seasons", [1, 3])
+@pytest.mark.parametrize("sp", TEST_SPS)
+def test_strategy_mean_seasonal_simple(n_seasons, sp):
+
+    # create 2d matrix, rows are different seasons, columns time points of
+    # each season
+    values = np.random.normal(size=(n_seasons, sp))
+    y = pd.Series(values.ravel())
+
+    expected = values.mean(axis=0)
+    assert expected.shape == (sp,)
+
+    f = NaiveForecaster(strategy="mean", sp=sp)
+    f.fit(y)
+    fh = np.arange(1, sp + 1)
+    y_pred = f.predict(fh)
+
     np.testing.assert_array_equal(y_pred, expected)
