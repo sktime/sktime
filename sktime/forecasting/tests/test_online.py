09c4d3dab97c219c15661daacb6febfb43469033
Magittan <memo@ekalvaya.c.innate-solution-180816.internal>
2020-08-20 04:14:22 +0000

    Averaging forecaster for tests

--- sktime/forecasting/tests/test_online.py
+++ sktime/forecasting/tests/test_online.py
@@ -13,6 +13,7 @@ from sktime.forecasting.ensemble_algorithms import HedgeExpertEnsemble,\
 from sktime.forecasting.online_experts import NormalHedge, se
 from sktime.forecasting.online_ensemble import OnlineEnsembleForecaster
 from sktime.forecasting.exp_smoothing import ExponentialSmoothing
+from sktime.forecasting.naive import NaiveForecaster
 
 
 def test_weights_for_airline_averaging():
@@ -40,19 +41,17 @@ def test_weights_for_airline_normal_hedge():
     hedge_expert = HedgeExpertEnsemble(3, 80, NormalHedge, loss_func=se)
 
     forecaster = OnlineEnsembleForecaster([
-        ("ses", ExponentialSmoothing(seasonal="multiplicative", sp=12)),
-        ("holt", ExponentialSmoothing(trend="add", damped=False,
-                                      seasonal="multiplicative", sp=12)),
-        ("damped", ExponentialSmoothing(trend="add", damped=True,
-                                        seasonal="multiplicative", sp=12))
-    ], ensemble_algorithm=hedge_expert)
+        ("av5",  NaiveForecaster(strategy="mean", window_length=5)),
+        ("av10", NaiveForecaster(strategy="mean", window_length=10)),
+        ("av20", NaiveForecaster(strategy="mean", window_length=20))],
+        ensemble_algorithm=hedge_expert)
 
     forecaster.fit(y_train)
     results = forecaster.update_predict(y_test)
     print(results)
 
-    expected = np.array([0, 0.55132043, 0.44867957])
-    np.testing.assert_allclose(forecaster.weights, expected, rtol=1e-8)
+    expected = np.array([0.17077154, 0.48156709, 0.34766137])
+    np.testing.assert_allclose(forecaster.weights, expected, atol=1e-8)
 
 
 def test_weights_for_airline_nnls():
@@ -62,17 +61,14 @@ def test_weights_for_airline_nnls():
     hedge_expert = NNLSEnsemble(3, loss_func=se)
 
     forecaster = OnlineEnsembleForecaster([
-        ("ses", ExponentialSmoothing(seasonal="multiplicative", sp=12)),
-        ("holt", ExponentialSmoothing(trend="add", damped=False,
-                                      seasonal="multiplicative", sp=12)),
-        ("damped", ExponentialSmoothing(trend="add", damped=True,
-                                        seasonal="multiplicative", sp=12))
-    ], ensemble_algorithm=hedge_expert)
+        ("av5",  NaiveForecaster(strategy="mean", window_length=5)),
+        ("av10", NaiveForecaster(strategy="mean", window_length=10)),
+        ("av20", NaiveForecaster(strategy="mean", window_length=20))],
+        ensemble_algorithm=hedge_expert)
 
     forecaster.fit(y_train)
     results = forecaster.update_predict(y_test)
     print(results)
-    forecaster.update_predict(y_test)
 
-    expected = np.array([0, 1.02354436, 0])
-    np.testing.assert_allclose(forecaster.weights, expected, rtol=1e-8)
+    expected = np.array([0.04720766, 0, 1.03410876])
+    np.testing.assert_allclose(forecaster.weights, expected, atol=1e-8)
