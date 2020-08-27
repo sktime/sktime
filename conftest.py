27d29bf76397cd0124e220a68fcea632c0cc6e47
big-o <big-o@users.noreply.github.com>
2020-08-28 00:42:55 +0100

    removed local conftest

--- conftest.py
+++
@@ -1,5 +0,0 @@
-# Even if empty this file is useful so that when running from the root folder
-# the local codebase is added to sys.path by pytest. See
-# https://docs.pytest.org/en/latest/pythonpath.html for more details.  For
-# example, this allows us to build extensions in place and run pytest using the package
-# from the local folder rather than the one from site-packages.
