2968504a92fa20d32ed45cd7b379c801d3b7a835
dicecko <dicecko@gmail.com>
2020-09-03 13:05:19 +0200

    removed pickle files from test, added general dataset test

--- sktime/datasets/tests/test_uschange.py
+++ sktime/datasets/tests/test_uschange.py
@@ -9,11 +9,46 @@ import pandas as pd
 from sktime.datasets import load_uschange
 
 
+def check_dataset(checks, y=None, X=None):
+    """
+    asserts if datasets are loaded correctly
+    ----------
+    checks: dictionary with values to assert against should contain:
+        'columns' : list with column names in correct order,
+        'len_y'   : lenght of the y series (int),
+        'len_X'   : lenght of the X series/dataframe (int),
+        'data_types_X' : dictionary with column name keys and dtype as value,
+        'data_type_y'  : dtype if y column (string)
+    y:  y series of the dataset
+    X:  X dataframe or series from dataset
+    """
+    if y is not None:
+        assert(isinstance(y, pd.Series))
+        assert(len(y) == checks['len_y'])
+        assert(y.dtype == checks['data_type_y'])
+
+    if X is not None:
+        if len(checks['data_types_X']) > 1:
+            assert(isinstance(X, pd.DataFrame))
+        else:
+            assert(isinstance(X, pd.Series))
+
+        assert(X.columns.values.tolist() == checks['columns'])
+
+        for col, dt in checks['data_types_X'].items():
+            assert(X[col].dtype == dt)
+
+        assert(len(X) == checks['len_X'])
+
+
 def test_uschange():
     X, y = load_uschange()
+    checks = {
+        'columns': ['Income', 'Production', 'Savings', 'Unemployment'],
+        'len_y': 187,
+        'len_X': 187,
+        'data_types_X': {'Income': 'float64', 'Production': 'float64', 'Savings': 'float64', 'Unemployment': 'float64'},
+        'data_type_y': 'float64'
+    }
 
-    X_test = pd.read_pickle('uschange_X.pkl')
-    y_test = pd.read_pickle('uschange_y.pkl')
-
-    pd.testing.assert_frame_equal(X, X_test)
-    pd.testing.assert_series_equal(y, y_test)
+    check_dataset(checks, y, X)
