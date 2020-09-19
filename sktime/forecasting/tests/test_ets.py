067d35abc493aae3509239cfc79f1df90c12b0c8
o2redshift <44179303+HYang1996@users.noreply.github.com>
2020-09-19 03:53:50 +0100

    Remove the unit test that imports the dataset from R database

--- sktime/forecasting/tests/test_ets.py
+++ sktime/forecasting/tests/test_ets.py
@@ -3,9 +3,6 @@ __author__ = ["Hongyi Yang"]
 from numpy.testing import assert_array_equal
 from sktime.forecasting.ets import AutoETS
 from sktime.datasets import load_airline
-from rpy2.robjects import r, pandas2ri
-from rpy2.robjects.packages import importr
-import pandas as pd
 
 
 # test results against R implementation on airline dataset
@@ -29,45 +26,6 @@ def test_airline():
 
     # Allow multiplicative trend
     # fit <- ets(AirPassengers, model = "ZZZ",
-    # allow.multiplicative.trend = TRUE)
-    # components: "M" "M" "M" "TRUE"
-    # discrepancy lies in damped (True in R but False in statsmodels)
-    fit_result_R = ['mul', 'mul', 'mul']
-
-    forecaster = AutoETS(autofitting=True, sp=12, n_jobs=-1,
-                         allow_multiplicative_trend=True)
-    forecaster.fit(y)
-    fitted_forecaster = forecaster._fitted_forecaster
-    fit_result = [fitted_forecaster.error,
-                  fitted_forecaster.trend,
-                  fitted_forecaster.seasonal]
-
-    assert_array_equal(fit_result_R, fit_result)
-
-
-# test results against R implementation on gas dataset (forecast package)
-def test_gas():
-    importr('forecast')
-    pandas2ri.activate()
-    y = pd.Series(r['gas']).astype('float64')
-
-    # Default condition
-    # fit <- ets(gas, model = 'ZZZ')
-    # components: "M" "A" "M" "TRUE" (error, trend, season, damped)
-    # discrepancy lies in damped (True in R but False in statsmodels)
-    fit_result_R = ['mul', 'add', 'mul']
-
-    forecaster = AutoETS(autofitting=True, sp=12, n_jobs=-1)
-    forecaster.fit(y)
-    fitted_forecaster = forecaster._fitted_forecaster
-    fit_result = [fitted_forecaster.error,
-                  fitted_forecaster.trend,
-                  fitted_forecaster.seasonal]
-
-    assert_array_equal(fit_result_R, fit_result)
-
-    # Allow multiplicative trend
-    # fit <- ets(gas, model = "ZZZ",
     # allow.multiplicative.trend = TRUE)
     # components: "M" "M" "M" "TRUE"
     # discrepancy lies in damped (True in R but False in statsmodels)
