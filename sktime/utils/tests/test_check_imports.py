cba1b4e2e99578131eb8b6af73712b437dbd7aca
AlwinW <16846521+AlwinW@users.noreply.github.com>
2020-10-03 12:45:28 +1000

    Fixed test for check_imports

--- sktime/utils/tests/test_check_imports.py
+++ sktime/utils/tests/test_check_imports.py
@@ -6,5 +6,5 @@ from sktime.utils.check_imports import _check_imports
 
 def test_check_imports():
     """Test the _check_imports() function."""
-    with pytest.raises(ModuleNotFoundError, match=r".* soft dependency .*"):
+    with pytest.raises(Exception, match=r".* soft dependency .*"):
         _check_imports("unavailable_module")
