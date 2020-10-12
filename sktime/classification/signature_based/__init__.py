697491ec1316090a76c2ae82a5af59e951323345
James Morrill <james.morrill.6@gmail.com>
2020-10-12 13:47:20 +0100

    Updated pytorch and signatory for readthedocs.

--- sktime/classification/signature_based/__init__.py
+++ sktime/classification/signature_based/__init__.py
@@ -1,6 +1,6 @@
-__all__ = [
-    "SignatureClassifier", "basic_signature_hyperopt"
-]
+__all__ = ["SignatureClassifier", "basic_signature_hyperopt"]
 
-from sktime.classification.signature_based._signature_classifier import \
-    SignatureClassifier, basic_signature_hyperopt
+from sktime.classification.signature_based._signature_classifier import (
+    SignatureClassifier,
+    basic_signature_hyperopt
+)
--- sktime/transformers/series_as_features/signature_based/__init__.py
+++ sktime/transformers/series_as_features/signature_based/__init__.py
@@ -1,5 +1,3 @@
-__all__ = [
-    "GeneralisedSignatureMethod",
-]
+__all__ = ["GeneralisedSignatureMethod"]
 from sktime.transformers.series_as_features.signature_based._signature_method \
     import GeneralisedSignatureMethod
