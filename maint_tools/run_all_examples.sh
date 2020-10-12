5460db64e0d7cc1c061561de6034607d5274aff2
mloning <markus.loning.17@ucl.ac.uk>
2020-10-12 18:46:09 +0100

    Fix notebooks

---
+++ maint_tools/run_all_examples.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Script to run all example notebooks.
+#set -e  # Exit on error
+
+CMD="jupyter nbconvert --to notebook --inplace --execute --ExecutePreprocessor.timeout=600"
+
+for notebook in examples/*.ipynb; do
+  echo "Running: $notebook"
+  $CMD "$notebook"
+done
