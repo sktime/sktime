01a41bca53b795fe57fb2f3169c0630fa0d9b6a5
mloning <markus.loning.17@ucl.ac.uk>
2020-10-24 18:49:10 +0100

    Fix numba version

--- build_tools/azure/check_soft_dependencies.py
+++ build_tools/azure/check_soft_dependencies.py
@@ -7,6 +7,7 @@
 # dependencies have been properly isolated and are not required to run other
 # modules.
 import pkgutil
+import re
 from importlib import import_module
 
 SOFT_DEPENDENCIES = {
@@ -19,6 +20,15 @@ SOFT_DEPENDENCIES = {
 }
 MODULES_TO_IGNORE = ("tests", "contrib")
 
+
+def _extract_dependency_from_error_msg(msg):
+    match = re.search(r"\'(.+?)\'", msg)
+    if match:
+        return match.group(1)
+    else:
+        raise ValueError("No dependency found in error msg.")
+
+
 for _, modname, _ in pkgutil.walk_packages(path=["./sktime/"], prefix="sktime."):
     mod_parts = modname.split(".")
     if any(part in MODULES_TO_IGNORE for part in mod_parts):
@@ -27,10 +37,14 @@ for _, modname, _ in pkgutil.walk_packages(path=["./sktime/"], prefix="sktime.")
     try:
         import_module(modname)
     except ModuleNotFoundError as e:
+        error_msg = str(e)
+
         soft_dependencies = SOFT_DEPENDENCIES.get(modname, [])
-        if any(soft_dependency in str(e) for soft_dependency in soft_dependencies):
+        if any(soft_dependency in error_msg for soft_dependency in soft_dependencies):
             continue
-        raise RuntimeError(
+
+        dependency = _extract_dependency_from_error_msg(error_msg)
+        raise ModuleNotFoundError(
             f"{modname} should not require any soft dependencies, "
-            f"but tried importing them: {soft_dependencies}"
+            f"but tried importing: {dependency}"
         ) from e
