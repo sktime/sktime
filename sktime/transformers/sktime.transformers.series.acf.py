5cc3399ab15f1c76f678ec90419221ea0921f912
Afzal <afzal442@gmail.com>
2020-11-28 17:15:20 +0530

    Update sktime.transformers.series.acf.py

--- sktime/transformers/sktime.transformers.series.acf.py
+++ sktime/transformers/sktime.transformers.series.acf.py
@@ -1,16 +1,16 @@
+#!/usr/bin/env python3 -u
 # -*- coding: utf-8 -*-
 # import numpy as np
 # import pandas as pd
 
-# from statsmodels.tsa.stattools import acf
+from statsmodels.tsa.stattools import acf
 from sktime.transformers.base import _SeriesToSeriesTransformer
 from sktime.utils.validation.series import check_series
 
 
 class AutoCorrelationFunctionTransformer(_SeriesToSeriesTransformer):
-    def transform(
+    def __init__(
         self,
-        x,
         unbiased=False,
         nlags=40,
         qstat=False,
@@ -25,7 +25,14 @@ class AutoCorrelationFunctionTransformer(_SeriesToSeriesTransformer):
         self.fft = fft
         self.alpha = alpha
         self.missing = missing
-
+        super(AutoCorrelationFunctionTransformer, self).__init__()
+        
+        
+    def transform(self, Z, X=None):
+        
         self.check_is_fitted()
-        x = check_series(x)
-        return x
+        x = check_series(Z)
+        zt = acf(x, self.unbiased, self.nlags, self.qstat, self.fft, self.alpha, self.missing)
+        return zt
+    
+    
