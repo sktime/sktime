6a4d7d029d67855106c0e400ce0b1fc70880f800
angus924 <55837131+angus924@users.noreply.github.com>
2020-12-23 04:40:36 +1100

    Add MiniRocket and MiniRocketMultivariate (#542)
    * Add MiniRocket and MiniRocketMultivariate
    
    * Fix code style
    
    * Fix code style
    
    * Fix code style
    
    * Fix code style
    
    * Fix code style
    
    * Fix code style
    
    * Update CODEOWNERS
    
    * Update CODEOWNERS
    
    * Add univariate-only tag to MiniRocket
    
    * Move Rocket, MiniRocket, and MiniRocketMultivariate to submodule
    
    * Fix style
    
    * Update __init__.py

---
+++ sktime/transformations/panel/rocket/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+__all__ = ["Rocket", "MiniRocket", "MiniRocketMultivariate"]
+
+from ._rocket import Rocket
+from ._minirocket import MiniRocket
+from ._minirocket_multivariate import MiniRocketMultivariate
