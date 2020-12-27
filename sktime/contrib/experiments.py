01e09e2d1075cc03168e709deacd2e8130344151
Tony Bagnall <ajb@uea.ac.uk>
2020-12-27 15:29:27 +0000

    line breaks in comments

--- sktime/contrib/experiments.py
+++ sktime/contrib/experiments.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 """ experiments.py: code to run experiments as an alternative to orchestration.
 
-This file is configured for runs of the main method with command line arguments, or for single debugging runs. Results
+This file is configured for runs of the main method with command line arguments, or for
+single debugging runs. Results
 are written in a standard format
 TO DO: Tidy up this file!
 """
@@ -167,19 +168,25 @@ def run_experiment(
     train_file=False,
 ):
     """
-    Method to run a basic experiment and write the results to files called testFold<resampleID>.csv and, if required,
-    trainFold<resampleID>.csv.
+    Method to run a basic experiment and write the results to files called
+    testFold<resampleID>.csv and, if required, trainFold<resampleID>.csv.
     :param problem_path: Location of problem files, full path.
-    :param results_path: Location of where to write results. Any required directories will be created
-    :param cls_name: determines which classifier to use, as defined in set_classifier. This assumes predict_proba is
+    :param results_path: Location of where to write results. Any required directories
+        will be created
+    :param cls_name: determines which classifier to use, as defined in set_classifier.
+        This assumes predict_proba is
     implemented, to avoid predicting twice. May break some classifiers though
-    :param dataset: Name of problem. Files must be  <problem_path>/<dataset>/<dataset>+"_TRAIN"+format, same for "_TEST"
-    :param resampleID: Seed for resampling. If set to 0, the default train/test split from file is used. Also used in output file name.
-    :param overwrite: if set to False, this will only build results if there is not a result file already present. If
+    :param dataset: Name of problem. Files must be  <problem_path>/<dataset>/<dataset>+
+                "_TRAIN"+format, same for "_TEST"
+    :param resampleID: Seed for resampling. If set to 0, the default train/test split
+                from file is used. Also used in output file name.
+    :param overwrite: if set to False, this will only build results if there is not a
+                result file already present. If
     True, it will overwrite anything already there
-    :param format: Valid formats are ".ts", ".arff" and ".long". For more info on format, see
-    https://github.com/alan-turing-institute/sktime/blob/master/examples/Loading%20Data%20Examples.ipynb
-    :param train_file: whether to generate train files or not. If true, it performs a 10xCV on the train and saves
+    :param format: Valid formats are ".ts", ".arff" and ".long".
+    For more info on format, see   examples/Loading%20Data%20Examples.ipynb
+    :param train_file: whether to generate train files or not. If true, it performs a
+                10xCV on the train and saves
     :return:
     """
 
@@ -221,16 +228,18 @@ def run_experiment(
         if train_file == False and build_test == False:
             return
 
-    # TO DO: Automatically differentiate between problem types, currently only works with .ts
+    # TO DO: Automatically differentiate between problem types,
+    # currently only works with .ts
     trainX, trainY = load_ts(problem_path + dataset + "/" + dataset + "_TRAIN" + format)
     testX, testY = load_ts(problem_path + dataset + "/" + dataset + "_TEST" + format)
     if resampleID != 0:
         # allLabels = np.concatenate((trainY, testY), axis = None)
         # allData = pd.concat([trainX, testX])
         # train_size = len(trainY) / (len(trainY) + len(testY))
-        # trainX, testX, trainY, testY = train_test_split(allData, allLabels, train_size=train_size,
-        #                                                                random_state=resampleID, shuffle=True,
-        #                                                                stratify=allLabels)
+        # trainX, testX, trainY, testY = train_test_split(allData, allLabels,
+        # train_size=train_size,
+        # random_state=resampleID, shuffle=True,
+        # stratify=allLabels)
         trainX, trainY, testX, testY = stratified_resample(
             trainX, trainY, testX, testY, resampleID
         )
@@ -299,7 +308,7 @@ def run_experiment(
         start = int(round(time.time() * 1000))
         if build_test and hasattr(
             classifier, "_get_train_probs"
-        ):  # Normally Can only do this if test has been built ... well not necessarily true, but will do for now
+        ):  # Normally Can only do this if test has been built
             train_probs = classifier._get_train_probs(trainX)
         else:
             train_probs = cross_val_predict(
@@ -361,10 +370,10 @@ def write_results_to_uea_format(
     class_labels=None,
 ):
     """
-    This is very alpha and I will probably completely change the structure once train fold is sorted, as that internally
+    This is very alpha and I will probably completely change the structure once train
+    fold is sorted, as that internally
     does all this I think!
     Output mirrors that produced by this Java
-    https://github.com/TonyBagnall/uea-tsc/blob/master/src/main/java/experiments/Experiments.java
     :param output_path:
     :param classifier_name:
     :param dataset_name:
@@ -380,7 +389,8 @@ def write_results_to_uea_format(
     """
     if len(actual_class_vals) != len(predicted_class_vals):
         raise IndexError(
-            "The number of predicted class values is not the same as the number of actual class values"
+            "The number of predicted class values is not the same as the "
+            + "number of actual class values"
         )
 
     try:
@@ -416,8 +426,6 @@ def write_results_to_uea_format(
         "w",
     )
 
-    # print(classifier_name+" on "+dataset_name+" for resample "+str(resample_seed)+"   "+train_or_test+" data has line three "+third_line)
-    # the first line of the output file is in the form of:
     # <classifierName>,<datasetName>,<train/test>,<Class Labels>
     file.write(
         str(dataset_name)
@@ -431,23 +439,25 @@ def write_results_to_uea_format(
     )
     file.write("\n")
 
-    # the second line of the output is free form and classifier-specific; usually this will record info
+    # the second line of the output is free form and classifier-specific;
+    # usually this will record info
     # such as parameter options used, any constituent model names for ensembles, etc.
     file.write(str(second_line) + "\n")
 
-    # the third line of the file is the accuracy (should be between 0 and 1 inclusive). If this is a train
-    # output file then it will be a training estimate of the classifier on the training data only (e.g.
-    # 10-fold cv, leave-one-out cv, etc.). If this is a test output file, it should be the output
-    # of the estimator on the test data (likely trained on the training data for a-priori parameter optimisation)
+    # the third line of the file is the accuracy (should be between 0 and 1 inclusive).
+    # If this is a train output file then it will be a training estimate of the
+    # classifier on the training data only (e.g. #10-fold cv, leave-one-out cv, etc.).
+    # If this is a test output file, it should be the output of the estimator on the
+    # test data (likely trained on the training data for a-priori para optimisation)
     file.write(str(third_line))
     file.write("\n")
 
-    # from line 4 onwards each line should include the actual and predicted class labels (comma-separated). If
-    # present, for each case, the probabilities of predicting every class value for this case should also be
-    # appended to the line (a space is also included between the predicted value and the predict_proba). E.g.:
-    #
+    # from line 4 onwards each line should include the actual and predicted class labels
+    # (comma-separated). If present, for each case, the probabilities of predicting
+    # every class value for this case should also be appended to the line (a space is
+    # also included between the predicted value and the predict_proba). E.g.:
     # if predict_proba data IS provided for case i:
-    #   actual_class_val[i], predicted_class_val[i],,prob_class_0[i],prob_class_1[i],...,prob_class_c[i]
+    #   actual_class_val[i], predicted_class_val[i],,
     #
     # if predict_proba data IS NOT provided for case i:
     #   actual_class_val[i], predicted_class_val[i]
@@ -505,7 +515,8 @@ if __name__ == "__main__":
     #    test_loading()
     #    sys.exit()
     #    print('experimenting...')
-    # Input args -dp=${dataDir} -rp=${resultsDir} -cn=${classifier} -dn=${dataset} -f=\$LSB_JOBINDEX
+    # Input args -dp=${dataDir} -rp=${resultsDir} -cn=${classifier} -dn=${dataset}
+    # -f=\$LSB_JOBINDEX
     if sys.argv.__len__() > 1:  # cluster run, this is fragile
         print(sys.argv)
         data_dir = sys.argv[1]
