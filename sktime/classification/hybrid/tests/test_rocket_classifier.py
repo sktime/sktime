f222b9cf108a93817ac5b6a5a2a00eefe7eeca10
Matthew Middlehurst <pfm15hbu@uea.ac.uk>
2020-12-29 21:02:15 +0000

    rocket classifier tests

--- sktime/classification/hybrid/tests/test_rocket_classifier.py
+++ sktime/classification/hybrid/tests/test_rocket_classifier.py
@@ -36,7 +36,7 @@ def test_rocket_ensemble_on_gunpoint():
     rocket_e.fit(X_train.iloc[indices], y_train[indices])
 
     # assert probabilities are the same
-    probas = rocket_E.predict_proba(X_test.iloc[indices])
+    probas = rocket_e.predict_proba(X_test.iloc[indices])
     testing.assert_array_equal(probas, rocket_e_gunpoint_probas)
 
 
@@ -67,3 +67,56 @@ def test_tde_on_basic_motions():
     # assert probabilities are the same
     probas = rocket.predict_proba(X_test.iloc[indices])
     testing.assert_array_equal(probas, rocket_basic_motions_probas)
+
+
+rocket_gunpoint_probas = np.array(
+
+)
+rocket_e_gunpoint_probas = np.array(
+
+)
+rocket_basic_motions_probas = np.array(
+
+)
+
+
+def print_array(array):
+    print('[')
+    for sub_array in array:
+        print('[')
+        for value in sub_array:
+            print(value.astype(str), end='')
+            print(', ')
+        print('],')
+    print(']')
+
+
+if __name__ == "__main__":
+    X_train, y_train = load_gunpoint(split="train", return_X_y=True)
+    X_test, y_test = load_gunpoint(split="test", return_X_y=True)
+    indices = np.random.RandomState(0).permutation(10)
+
+    rocket_u = ROCKETClassifier(random_state=0)
+    rocket_e = ROCKETClassifier(
+        num_kernels=2000,
+        ensemble=True,
+        random_state=0,
+    )
+
+    rocket_u.fit(X_train.iloc[indices], y_train[indices])
+    probas = rocket_u.predict_proba(X_test.iloc[indices])
+    print_array(probas)
+
+    rocket_e.fit(X_train.iloc[indices], y_train[indices])
+    probas = rocket_e.predict_proba(X_test.iloc[indices])
+    print_array(probas)
+
+    X_train, y_train = load_basic_motions(split="train", return_X_y=True)
+    X_test, y_test = load_basic_motions(split="test", return_X_y=True)
+    indices = np.random.RandomState(0).permutation(20)
+
+    rocket_m = ROCKETClassifier(random_state=0)
+
+    rocket_m.fit(X_train.iloc[indices], y_train[indices])
+    probas = rocket_m.predict_proba(X_test.iloc[indices])
+    print_array(probas)
