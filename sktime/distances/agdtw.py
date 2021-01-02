ee3c8ba0af8ce15399ee3793acb838b3e5617db4
Ansgar Asseburg <ansgar@asseburg.de>
2021-01-02 16:39:12 +0100

    refactor to allow better testing, add tests, fix bugs

--- sktime/distances/agdtw.py
+++ sktime/distances/agdtw.py
@@ -36,6 +36,7 @@ def agdtw_distance(first, second, window=1):
       biburl    = {https://dblp.org/rec/conf/iconip/XueZTWL17.bib},
       bibsource = {dblp computer science bibliography, https://dblp.org}
     }
+    
     @param first: numpy array containing the first time series
     @param second: numpy array containing the second time series
     @param window: float, representing the window width as ratio of the window
@@ -43,135 +44,150 @@ def agdtw_distance(first, second, window=1):
     @return: a float containing the kernel distance
     """
 
-    def warping_matrix(series_1, series_2, window=1):
-        """
-        Creates the warping matrix and while respecting a given window
-        @param series_1: numpy array containing the first series
-        @param series_2: numpy array containing the second series
-        @param window: float, representing the window width as ratio of the
-        window and the longer series
-        @return: 2D numpy array containing the minimum squared distances
-        """
-        row_dim = len(series_1)
-        col_dim = len(series_2)
-        warp_matrix = np.full([row_dim, col_dim], np.inf)
-        warp_matrix[0, 0] = 0
-        # 1 >= window >= 0
-        window = min(1.0, abs(window))
-        absolute_window_size = int(max(row_dim, col_dim) * window)
-        for row in range(row_dim):
-            for col in range(col_dim):
-                if abs(row - col) <= absolute_window_size:
-                    min_index = index_of_section_min(warp_matrix, (row, col))
-                    min_dist_to_here = warp_matrix[min_index]
-                    warp_matrix[row, col] = \
-                        (series_1[row] - series_2[col]) ** 2 \
-                        + min_dist_to_here
-        return warp_matrix
-
-    def euclidean_distance(x, y):
-        """
-        Calculates the euclidean distance between of two univariate time series
-        @param x: element from first series
-        @param y: element from second series
-        @return euclidean distance
-        """
-        return abs(x - y)
-
-    def warping_path(matrix, first, second):
-        """
-        Creates the warping path along the minimum total distance between the
-        two series in the matrix
-        @matrix: the warping matrix
-        @first: numpy array containing the first time series
-        @second: numpy array containing the second time series
-        @return: numpy array containing the warping path with element distances
-        """
-
-        matrix_dim = matrix.shape
-        warp_path = np.full((sum(matrix_dim), 3),
-                            np.NAN)  # initialize to max length warping path
-        wp_index = len(warp_path) - 1  # we start at the end
-
-        wm_index = (matrix_dim[0] - 1, matrix_dim[1] - 1)
-        while True:
-            # store the distance, index of Ys, index of Xs
-            warp_path[wp_index] = euclidean_distance(first[wm_index[0]],
-                                                     second[wm_index[1]]), \
-                                  wm_index[0], wm_index[1]
-            wp_index -= 1
-
-            if wm_index == (0, 0):
-                break  # we're finished
-
-            # point to min element
-            wm_index = index_of_section_min(matrix, wm_index)
-
-        # remove the remaining NANs
-        return warp_path[np.logical_not(np.isnan(warp_path).any(axis=1))]
-
-    def index_of_section_min(matrix, current_index=(0, 0)):
-        """
-        Reads the values around the given source cell and returns an index to
-        the minimum of these values. Respecting the boundaries of the
-        original matrix the function evaluates the following values:
-        ... __________________
-        ... | value | value  | ...
-        ... | value | source | ...
-        ... | value | value  | ...
-            __________________
-        ...
-        @param matrix: numpy array containing the warping matrix
-        @param current_index: a tuple with the 2D index pointing to current
-        cell
-        @return: a tuple containing the index for the matrix pointing to the
-                    minimum value within the given section
-        """
-
-        section_org_row = max(current_index[0] - 1, 0)
-        section_org_col = max(current_index[1] - 1, 0)
-        section_end_row = min(current_index[0] + 1, matrix.shape[0])
-        section_end_col = current_index[1]
-        # copy section
-        section = matrix[
-                  section_org_row:section_end_row + 1,
-                  section_org_col:section_end_col + 1
-                  ].copy()
-
-        section_current_row = min(current_index[0], 1)
-        section_current_col = min(current_index[1], 1)
-        # prevent current cell from being found as minimum
-        section[(section_current_row, section_current_col)] = np.inf
-        # find minimum cell in section
-        min_section_index = np.unravel_index(np.argmin(section, axis=None),
-                                             section.shape)
-        # find minimum index
-        min_row = section_org_row + min_section_index[0]
-        min_col = section_org_col + min_section_index[1]
-        return min_row, min_col  # point to minimum element from section
-
-    def variance_of_warping_path(path):
-        """
-        calculate the variance needed for finding the kernel distance
-        @param path: numpy array containing the warping path
-        @return: float containing the variance
-        """
-        mean = sum(path[:, 0]) / len(path)
-        variance = sum((mean - path[:, 0]) ** 2) / len(path)
-        return variance
-
-    def kernel_distance(path):
-        """
-        calculates the kernel distance by processing each individual distance
-        along the warping path
-        @param path: numpy array containing the warping path
-        @return: float containing the kernel distance
-        """
-        normalized_distances = path[:, 0] ** 2 / variance_of_warping_path(path)
-        kernel_distances = np.exp(-normalized_distances)
-        return sum(kernel_distances)
-
     warp_matrix = warping_matrix(first, second, window)
     warp_path = warping_path(warp_matrix, first, second)
 
     return kernel_distance(warp_path)
+
+
+def warping_matrix(series_1, series_2, window=1):
+    """
+    Creates the warping matrix and while respecting a given window
+    @param series_1: numpy array containing the first series
+    @param series_2: numpy array containing the second series
+    @param window: float, representing the window width as ratio of the
+    window and the longer series
+    @return: 2D numpy array containing the minimum squared distances
+    """
+    row_dim = len(series_1)
+    col_dim = len(series_2)
+    warp_matrix = np.full([row_dim, col_dim], np.inf)
+    warp_matrix[0, 0] = 0
+    # 1 >= window >= 0
+    window = min(1.0, abs(window))
+    absolute_window_size = int(max(row_dim, col_dim) * window)
+    for row in range(row_dim):
+        for col in range(col_dim):
+            if abs(row - col) <= absolute_window_size:
+                min_index = index_of_section_min(warp_matrix, (row, col))
+                min_dist_to_here = warp_matrix[min_index]
+                warp_matrix[row, col] = \
+                    (series_1[row] - series_2[col]) ** 2 + min_dist_to_here
+    return warp_matrix
+
+
+def euclidean_distance(x, y):
+    """
+    Calculates the euclidean distance between of two univariate time series
+    @param x: element from first series
+    @param y: element from second series
+    @return euclidean distance
+    """
+    return abs(x - y)
+
+
+def warping_path(matrix, first, second):
+    """
+    Creates the warping path along the minimum total distance between the
+    two series in the matrix
+    @matrix: the warping matrix
+    @first: numpy array containing the first time series
+    @second: numpy array containing the second time series
+    @return: numpy array containing the warping path with element distances
+    """
+
+    matrix_dim = matrix.shape
+    warp_path = np.full((sum(matrix_dim), 3),
+                        np.NAN)  # initialize to max length warping path
+    wp_index = len(warp_path) - 1  # we start at the end
+
+    wm_index = (matrix_dim[0] - 1, matrix_dim[1] - 1)
+    while True:
+        # store the distances, index of Ys, index of Xs
+        warp_path[wp_index] = euclidean_distance(first[wm_index[0]],
+                                                 second[wm_index[1]]), \
+                              wm_index[0], wm_index[1]
+        wp_index -= 1
+
+        if wm_index == (0, 0):
+            break  # we're finished
+
+        # point to min element
+        wm_index = index_of_section_min(matrix, wm_index)
+
+    # remove the remaining NANs
+    return warp_path[np.logical_not(np.isnan(warp_path).any(axis=1))]
+
+
+def dynamic_section(matrix, current_index=(0, 0)):
+    section_org_row = max(current_index[0] - 1, 0)
+    section_org_col = max(current_index[1] - 1, 0)
+    section_end_row = min(current_index[0] + 1, matrix.shape[0])
+    section_end_col = current_index[1]
+    # copy section and make its elements float to for allow np.inf
+    section = matrix[
+              section_org_row:section_end_row + 1,
+              section_org_col:section_end_col + 1
+              ].copy().astype(float)
+    return section
+
+
+def index_of_section_min(matrix, current_index=(0, 0)):
+    """
+    Reads the values around the given source cell and returns an index to
+    the minimum of these values. Respecting the boundaries of the
+    original matrix the function evaluates the following values:
+    ... __________________
+    ... | value | value  | ...
+    ... | value | source | ...
+    ... | value | value  | ...
+        __________________
+    ...
+    @param matrix: numpy array containing the warping matrix
+    @param current_index: a tuple with the 2D index pointing to current
+    cell
+    @return: a tuple containing the index for the matrix pointing to the
+                minimum value within the given section
+    """
+
+    # if at origin of matrix return (0, 0)
+    if current_index == (0, 0):
+        return current_index
+    section = dynamic_section(matrix, current_index)
+
+    section_current_row = min(current_index[0], 1)
+    section_current_col = min(current_index[1], 1)
+    # prevent current cell from being found as minimum
+    section[(section_current_row, section_current_col)] = np.inf
+    # find minimum cell in section
+    min_section_index = np.unravel_index(np.argmin(section, axis=None),
+                                         section.shape)
+    # find minimum index
+    section_org_row = max(current_index[0] - 1, 0)
+    section_org_col = max(current_index[1] - 1, 0)
+    min_row = section_org_row + min_section_index[0]
+    min_col = section_org_col + min_section_index[1]
+    return min_row, min_col  # point to minimum element from section
+
+
+def variance_of_warping_path(path):
+    """
+    calculate the variance needed for finding the kernel distance
+    @param path: numpy array containing the warping path
+    @return: float containing the variance
+    """
+    mean = sum(path[:, 0]) / len(path)
+    variance = sum((mean - path[:, 0]) ** 2) / len(path)
+    return variance
+
+
+def kernel_distance(path):
+    """
+    calculates the kernel distance by processing each individual distance
+    along the warping path
+    @param path: numpy array containing the warping path
+    @return: float containing the kernel distance
+    """
+    normalized_distances = path[:, 0] ** 2 / variance_of_warping_path(path)
+    kernel_distances = np.exp(-normalized_distances)
+    return sum(kernel_distances)
---
+++ sktime/distances/test_agdtw.py
@@ -0,0 +1,163 @@
+__author__ = "Ansgar Asseburg"
+__email__ = "devaa@donnerluetjen.de"
+
+import pytest
+
+
+def pytest_assertrepr_compare(op, left, right):
+    import numpy as np
+    if op == '==' and (
+            isinstance(left, np.array) and isinstance(right, np.array)
+    ) or (
+            isinstance(left, tuple) and isinstance(right, tuple)
+    ):
+        return [f'{left} in {right}']
+
+
+def test_euclidean_distance():
+    import sktime.distances.agdtw as agdtw
+
+    assert agdtw.euclidean_distance(0, 0) == 0
+    assert agdtw.euclidean_distance(1, 1) == 0
+    assert agdtw.euclidean_distance(3, 4) == 1
+    assert agdtw.euclidean_distance(4, 3) == 1
+
+
+def test_dynamic_section():
+    import numpy as np
+    import sktime.distances.agdtw as agdtw
+
+    test_matrix = np.array([
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8]
+    ])
+    test_range = np.array([
+        [[[0], [3]], (0, 0)],
+        [[[0, 1], [3, 4]], (0, 1)],
+        [[[1, 2], [4, 5]], (0, 2)],
+        [[[0], [3], [6]], (1, 0)],
+        [[[0, 1], [3, 4], [6, 7]], (1, 1)],
+        [[[1, 2], [4, 5], [7, 8]], (1, 2)],
+        [[[3], [6]], (2, 0)],
+        [[[3, 4], [6, 7]], (2, 1)],
+        [[[4, 5], [7, 8]], (2, 2)]
+    ])
+    for expected_section, source_indices in test_range:
+        actual_section = agdtw.dynamic_section(test_matrix, source_indices)
+        assert (expected_section == actual_section).all(), \
+            f'source_indices: {tuple(source_indices)}'
+
+
+def test_index_of_section_min():
+    import numpy as np
+    import sktime.distances.agdtw as agdtw
+
+    test_matrix = np.array([
+        [0, 1, 2],
+        [3, 4, 5],
+        [1, 7, 0]
+    ])
+    test_range = np.array([
+        [(0, 0), (0, 0)],
+        [(0, 0), (0, 1)],
+        [(0, 1), (0, 2)],
+        [(0, 0), (1, 0)],
+        [(0, 0), (1, 1)],
+        [(2, 2), (1, 2)],
+        [(1, 0), (2, 0)],
+        [(2, 0), (2, 1)],
+        [(1, 1), (2, 2)]
+    ])
+    for expected_indices, source_indices in test_range:
+        actual_indices = agdtw.index_of_section_min(test_matrix,
+                                                    tuple(source_indices))
+        assert (expected_indices == actual_indices).all(), \
+            f'at source indices: {tuple(source_indices)}'
+
+
+def test_warping_matrix():
+    import numpy as np
+    import sktime.distances.agdtw as agdtw
+
+    series_1 = np.array([1, 2, 3, 4, 5])
+    series_2 = np.array([1, 2, 3, 4, 5])
+    expected_result = np.array([[0, 1, 5, 14, 30],
+                                [1, 0, 1, 5, 14],
+                                [5, 1, 0, 1, 5],
+                                [14, 5, 1, 0, 1],
+                                [30, 14, 5, 1, 0]])
+    actual_result = agdtw.warping_matrix(series_1, series_2)
+
+    assert actual_result.shape == expected_result.shape
+    assert (actual_result == expected_result).all()
+
+
+def test_warping_path_with_symmetric_series():
+    import numpy as np
+    import sktime.distances.agdtw as agdtw
+
+    series_1 = np.array([1, 2, 3, 4, 5])
+    series_2 = np.array([1, 2, 3, 4, 5])
+    expected_result = np.array([
+        [0, 0, 0],
+        [0, 1, 1],
+        [0, 2, 2],
+        [0, 3, 3],
+        [0, 4, 4]
+    ])
+    matrix = agdtw.warping_matrix(series_1, series_2)
+    actual_result = agdtw.warping_path(matrix, series_1, series_2)
+    assert (expected_result == actual_result).all()
+
+
+def test_warping_path_with_asymmetric_series():
+    import numpy as np
+    import sktime.distances.agdtw as agdtw
+
+    series_1 = np.array([1, 2, 3, 2, 2])
+    series_2 = np.array([5, 7, 4, 4, 3, 2])
+    expected_result = np.array([
+        [4, 0, 0],
+        [5, 1, 1],
+        [1, 2, 2],
+        [1, 2, 3],
+        [0, 2, 4],
+        [0, 3, 5],
+        [0, 4, 5]
+    ])
+    matrix = agdtw.warping_matrix(series_1, series_2)
+    actual_result = agdtw.warping_path(matrix, series_1, series_2)
+    assert (expected_result == actual_result).all()
+
+
+def test_variance_of_warping_path():
+    import numpy as np
+    import sktime.distances.agdtw as agdtw
+    sample_path = np.array([
+        [4, 0, 0],
+        [5, 1, 1],
+        [1, 2, 2],
+        [1, 2, 3],
+        [0, 2, 4],
+        [0, 3, 5],
+        [0, 4, 5]
+    ])
+
+    expected_result = np.var(sample_path[:, 0])
+    actual_result = agdtw.variance_of_warping_path(sample_path)
+    assert expected_result == actual_result
+
+
+def test_kernel_distance():
+    import numpy as np
+    import sktime.distances.agdtw as agdtw
+    sample_path = np.array([
+        [1, 0, 0],
+        [3, 1, 1],
+        [3, 2, 2],
+        [5, 2, 3]
+    ])
+    expected_result = 0.62875237944229
+    actual_result = agdtw.kernel_distance(sample_path)
+    assert expected_result == pytest.approx(actual_result)
