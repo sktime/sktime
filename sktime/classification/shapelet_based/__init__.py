f6a807985c37ac3dbb2fc38110f843d9ac5643df
Tony Bagnall <ajb@uea.ac.uk>
2021-01-05 16:14:37 +0000

    all init refactored manually

--- sktime/classification/shapelet_based/__init__.py
+++ sktime/classification/shapelet_based/__init__.py
@@ -5,6 +5,6 @@ __all__ = [
     "ROCKETClassifier",
 ]
 
-from sktime.classification.shapelet_based._rocket_classifier import ROCKETClassifier
 from sktime.classification.shapelet_based._stc import ShapeletTransformClassifier
 from sktime.classification.shapelet_based.mrseql.mrseql import MrSEQLClassifier
+from sktime.classification.shapelet_based._rocket_classifier import ROCKETClassifier
