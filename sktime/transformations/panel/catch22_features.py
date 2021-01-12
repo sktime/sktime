3dbb02368f1b40700755385f94e4394cfd927f75
Matthew Middlehurst <pfm15hbu@uea.ac.uk>
2021-01-12 18:02:32 +0000

    formatting 2

--- sktime/transformations/panel/catch22_features.py
+++ sktime/transformations/panel/catch22_features.py
@@ -95,7 +95,8 @@ class Catch22(_PanelToTabularTransformer):
         Numpy array containing a catch22 feature for each input series
         """
         if isinstance(feature, (int, np.integer)) or isinstance(
-                feature, (float, np.float)):
+            feature, (float, np.float)
+        ):
             if feature > 21 or feature < 0:
                 raise ValueError("Invalid catch22 feature ID")
         elif isinstance(feature, str):
