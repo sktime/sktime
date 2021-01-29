aa598d78f8c47c9cde274bf8b421fb1bf1c18831
Tony Bagnall <ajb@uea.ac.uk>
2021-01-29 11:58:34 +0000

    benchmark

--- sktime/contrib/basic_benchmarking.py
+++ sktime/contrib/basic_benchmarking.py
@@ -23,6 +23,8 @@ import sktime.classification.interval_based._rise as fb
 import sktime.classification.dictionary_based._boss as db
 import sktime.classification.distance_based._time_series_neighbors as dist
 import sktime.contrib.experiments as exp
+from sklearn.ensemble import GradientBoostingClassifier
+
 
 # method 1
 
