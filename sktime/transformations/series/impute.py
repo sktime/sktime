5c25aa33b2b7d4e821cbe4861518bb664496b4b5
Walter <walmar2@emea.corpdir.net>
2021-01-30 17:39:40 +0100

    Added further imputation methods

--- sktime/transformations/series/impute.py
+++ sktime/transformations/series/impute.py
@@ -8,8 +8,8 @@ from sktime.transformations.base import _SeriesToSeriesTransformer
 from sktime.utils.validation.series import check_series
 from sktime.forecasting.trend import PolynomialTrendForecaster
 
-
 import numpy as np
+import pandas as pd
 import random
 
 
@@ -18,25 +18,29 @@ class Imputer(_SeriesToSeriesTransformer):
 
     Parameters
     ----------
-    method : {"backfill", "bfill", "pad", "ffill", "random",
-        "value", "drift", "mean", "median"}
-        Method to of filling values.
-            * backfill/bfill : adapted from pd.Series.fillna()
-            * pad/ffill : adapted from pd.Series.fillna()
-            * random : random values between pd.Series.min() and .max()
-            * value : same value for all NaN
-            * drift : drift (trend) values by sktime.PolynomialTrendForecaster
-            * mean : pd.Series.mean()
-            * median : pd.Series.median()
+    method : Method to of filling values.
+        * "drift" : drift/trend values by sktime.PolynomialTrendForecaster()
+        * "linear" : linear interpolation, by pd.Series.interpolate()
+        * "nearest" : use nearest value, by pd.Series.interpolate()
+        * "constant" : same constant value (given in arg value) for all NaN
+        * "mean" : pd.Series.mean()
+        * "median" : pd.Series.median()
+        * "backfill"/"bfill" : adapted from pd.Series.fillna()
+        * "pad"/"ffill" : adapted from pd.Series.fillna()
+        * "random" : random values between pd.Series.min() and .max()
+    missing_values : int/float/str, optional
+        The placeholder for the missing values. All occurrences of
+        missing_values will be imputed. Default, None (np.nan)
     random_state : int/float/str, optional
-        Value to set random.seed()
+        Value to set random.seed() if method="random", default None
     value : int/float, optional
         Value to fill NaN, by default None
     """
 
-    def __init__(self, method, random_state=None, value=None):
+    def __init__(self, method, missing_values=None, random_state=None, value=None):
 
         self.method = method
+        self.missing_values = missing_values
         self.random_state = random_state
         self.value = value
         super(Imputer, self).__init__()
@@ -46,9 +50,13 @@ class Imputer(_SeriesToSeriesTransformer):
         self._check_method()
         z = check_series(Z, enforce_univariate=True)
 
+        # replace missing_values with np.nan
+        if self.missing_values:
+            z = z.replace(to_replace=self.missing_values, value=np.nan)
+
         if self.method == "random":
             z = z.apply(lambda x: self._get_random(z) if np.isnan(x) else x)
-        elif self.method == "value":
+        elif self.method == "constant":
             z = z.fillna(value=self.value)
         elif self.method in ["backfill", "bfill", "pad", "ffill"]:
             z = z.fillna(method=self.method)
@@ -66,15 +74,17 @@ class Imputer(_SeriesToSeriesTransformer):
             z = z.fillna(value=z.mean())
         elif self.method == "median":
             z = z.fillna(value=z.median())
+        elif self.method in ["nearest", "linear"]:
+            z = z.interpolate(method=self.method)
         else:
             raise ValueError(f"method {self.method} not available")
-        return z
+        return pd.Series(z)
 
     def _check_method(self):
         if (
             self.value is not None
-            and self.method != "value"
-            or self.method == "value"
+            and self.method != "constant"
+            or self.method == "constant"
             and self.value is None
         ):
             raise ValueError(
