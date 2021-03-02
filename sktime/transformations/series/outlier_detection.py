1c99bfaeff93695b815db10995bee1ce653862dc
Walter <walmar2@emea.corpdir.net>
2021-03-02 17:23:48 +0100

    Added nan unittest for transformers

--- sktime/transformations/series/outlier_detection.py
+++ sktime/transformations/series/outlier_detection.py
@@ -38,7 +38,11 @@ class HampelFilter(_SeriesToSeriesTransformer):
     https://github.com/MichaelisTrofficus/hampel_filter
     """
 
-    _tags = {"univariate-only": True, "fit-in-transform": True}
+    _tags = {
+        "univariate-only": True,
+        "fit-in-transform": True,
+        "handles_missing_data": True,
+    }
 
     def __init__(self, window_length, n_sigma=3, k=1.4826, return_bool=False):
 
@@ -65,59 +69,75 @@ class HampelFilter(_SeriesToSeriesTransformer):
         )
         half_window_length = int(self.window_length / 2)
 
-        for i in cv.split(z):
-            cv_window = i[0]
-            cv_median = np.nanmedian(z[cv_window])
-            cv_sigma = self.k * np.nanmedian(np.abs(z[cv_window] - cv_median))
-
-            # find outliers at start and end of z
-            if (
-                cv_window[0] <= half_window_length
-                or cv_window[-1] >= len(z) - half_window_length
-            ) and (cv_window[0] in [0, len(z) - cv.window_length - 1]):
-
-                # first half of the first window
-                if cv_window[0] <= half_window_length:
-                    idx_range = range(cv_window[0], half_window_length + 1)
-
-                # last half of the last window
-                else:
-                    idx_range = range(len(z) - half_window_length - 1, len(z))
-                for j in idx_range:
-                    z.iloc[j] = self._compare(
-                        value=z.iloc[j],
-                        cv_median=cv_median,
-                        cv_sigma=cv_sigma,
-                    )
-            else:
-                idx = cv_window[0] + half_window_length
-                z.iloc[idx] = self._compare(
-                    value=z.iloc[idx],
-                    cv_median=cv_median,
-                    cv_sigma=cv_sigma,
-                )
+        z = _hampel_filter(
+            z=z,
+            cv=cv,
+            n_sigma=self.n_sigma,
+            half_window_length=half_window_length,
+            k=self.k,
+        )
 
         # data post-processing
         if self.return_bool:
             z = z.apply(lambda x: True if np.isnan(x) else False)
+
         return z
 
-    def _compare(self, value, cv_median, cv_sigma):
-        """Function to identify an outlier
 
-        Parameters
-        ----------
-        value : int/float
-        cv_median : int/float
-        cv_sigma : int/float
+def _hampel_filter(z, cv, n_sigma, half_window_length, k):
+    for i in cv.split(z):
+        cv_window = i[0]
+        cv_median = np.nanmedian(z[cv_window])
+        cv_sigma = k * np.nanmedian(np.abs(z[cv_window] - cv_median))
 
-        Returns
-        -------
-        int/float or np.nan
-            Returns value if value it is not an outlier,
-            else np.nan (or True if return_bool==True)
-        """
-        if np.abs(value - cv_median) > self.n_sigma * cv_sigma:
-            return np.nan
+        # find outliers at start and end of z
+        if (
+            cv_window[0] <= half_window_length
+            or cv_window[-1] >= len(z) - half_window_length
+        ) and (cv_window[0] in [0, len(z) - cv.window_length - 1]):
+
+            # first half of the first window
+            if cv_window[0] <= half_window_length:
+                idx_range = range(cv_window[0], half_window_length + 1)
+
+            # last half of the last window
+            else:
+                idx_range = range(len(z) - half_window_length - 1, len(z))
+            for j in idx_range:
+                z.iloc[j] = _compare(
+                    value=z.iloc[j],
+                    cv_median=cv_median,
+                    cv_sigma=cv_sigma,
+                    n_sigma=n_sigma,
+                )
         else:
-            return value
+            idx = cv_window[0] + half_window_length
+            z.iloc[idx] = _compare(
+                value=z.iloc[idx],
+                cv_median=cv_median,
+                cv_sigma=cv_sigma,
+                n_sigma=n_sigma,
+            )
+    return z
+
+
+def _compare(value, cv_median, cv_sigma, n_sigma):
+    """Function to identify an outlier
+
+    Parameters
+    ----------
+    value : int/float
+    cv_median : int/float
+    cv_sigma : int/float
+    n_sigma : int/float
+
+    Returns
+    -------
+    int/float or np.nan
+        Returns value if value it is not an outlier,
+        else np.nan (or True/False if return_bool==True)
+    """
+    if np.abs(value - cv_median) > n_sigma * cv_sigma:
+        return np.nan
+    else:
+        return value
