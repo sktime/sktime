a93975b8c232be0afd04beb6c3244564d6fef8be
Kavin <kavinanand77@gmail.com>
2021-03-02 12:22:41 -0800

    calling BaseTimeSeriesForest in super line

--- sktime/classification/interval_based/_tsf.py
+++ sktime/classification/interval_based/_tsf.py
@@ -3,8 +3,8 @@
     Time Series Forest Regressor (TSF).
 """
 
-__author__ = ["Tony Bagnall", "kkoziara", "luiszugasti"]
-__all__ = ["TimeSeriesForest"]
+__author__ = ["Tony Bagnall", "kkoziara", "luiszugasti", "kanand77"]
+__all__ = ["TimeSeriesForestClassifier"]
 
 from sklearn.ensemble._forest import ForestClassifier
 from sklearn.tree import DecisionTreeClassifier
@@ -15,38 +15,8 @@ from sktime.series_as_features.base.estimators.interval_based._tsf import (
 )
 
 
-class TimeSeriesForest(BaseTimeSeriesForest, ForestClassifier, BaseClassifier):
+class TimeSeriesForestClassifier(
+    BaseTimeSeriesForest, ForestClassifier, BaseClassifier
+):
 
-    # Capabilities: data types this classifier can handle
-    capabilities = {
-        "multivariate": False,
-        "unequal_length": False,
-        "missing_values": False,
-    }
-
-    def __init__(
-        self,
-        min_interval=3,
-        n_estimators=200,
-        n_jobs=1,
-        random_state=None,
-    ):
-        super(TimeSeriesForest, self).__init__(
-            base_estimator=DecisionTreeClassifier(criterion="entropy"),
-            n_estimators=n_estimators,
-        )
-
-        self.random_state = random_state
-        self.n_estimators = n_estimators
-        self.min_interval = min_interval
-        self.n_jobs = n_jobs
-        # The following set in method fit
-        self.n_classes = 0
-        self.series_length = 0
-        self.n_intervals = 0
-        self.estimators_ = []
-        self.intervals_ = []
-        self.classes_ = []
-
-        # We need to add is-fitted state when inheriting from scikit-learn
-        self._is_fitted = False
+    base_estimator = DecisionTreeClassifier(criterion="entropy")
--- sktime/series_as_features/base/estimators/interval_based/_tsf.py
+++ sktime/series_as_features/base/estimators/interval_based/_tsf.py
@@ -4,7 +4,7 @@
     An implementation of Deng's Time Series Forest, with minor changes.
 """
 
-__author__ = ["Tony Badnall", "kkoziara", "luiszugasti"]
+__author__ = ["Tony Badnall", "kkoziara", "luiszugasti", "kanand77"]
 __all__ = [
     "BaseTimeSeriesForest",
     "_transform",
@@ -81,6 +81,40 @@ class BaseTimeSeriesForest(ABC):
      java/tsml/classifiers/interval_based/TSF.java
      Arxiv version of the paper: https://arxiv.org/abs/1302.2277
     """
+
+    # Capabilities: data types this classifier can handle
+    capabilities = {
+        "multivariate": False,
+        "unequal_length": False,
+        "missing_values": False,
+    }
+
+    def __init__(
+        self,
+        min_interval=3,
+        n_estimators=200,
+        n_jobs=1,
+        random_state=None,
+    ):
+        super(BaseTimeSeriesForest, self).__init__(
+            base_estimator=self.base_estimator,
+            n_estimators=n_estimators,
+        )
+
+        self.random_state = random_state
+        self.n_estimators = n_estimators
+        self.min_interval = min_interval
+        self.n_jobs = n_jobs
+        # The following set in method fit
+        self.n_classes = 0
+        self.series_length = 0
+        self.n_intervals = 0
+        self.estimators_ = []
+        self.intervals_ = []
+        self.classes_ = []
+
+        # We need to add is-fitted state when inheriting from scikit-learn
+        self._is_fitted = False
 
     @property
     @abstractmethod
