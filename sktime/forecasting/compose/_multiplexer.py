74e19f75d97ce00ab23f84b0b13a842d766ef8ce
Kutay Koralturk <koralturkk@gmail.com>
2021-03-22 14:00:53 +0300

    multiplexer path changed, get/set params implemented

--- sktime/forecasting/compose/_multiplexer.py
+++ sktime/forecasting/compose/_multiplexer.py
@@ -2,20 +2,23 @@
 # -*- coding: utf-8 -*-
 # copyright: sktime developers, BSD-3-Clause License (see LICENSE file)
 
+from sktime.base import _HeterogenousMetaEstimator
 from sktime.forecasting.base._base import DEFAULT_ALPHA
 from sktime.forecasting.base._sktime import _OptionalForecastingHorizonMixin
 from sktime.forecasting.base._sktime import _SktimeForecaster
-import copy
+from sklearn.base import clone
 
 __author__ = ["Kutay Koralturk"]
-__all__ = ["MultiplexerForecaster"]
+__all__ = ["MultiplexForecaster"]
 
 
-class MultiplexerForecaster(_OptionalForecastingHorizonMixin, _SktimeForecaster):
+class MultiplexForecaster(
+    _OptionalForecastingHorizonMixin, _SktimeForecaster, _HeterogenousMetaEstimator
+):
     def __init__(
         self,
-        component_estimators: list,
-        selected_estimator=None,
+        forecasters: list,
+        selected_forecaster=None,
     ):
 
         """
@@ -24,8 +27,8 @@ class MultiplexerForecaster(_OptionalForecastingHorizonMixin, _SktimeForecaster)
         It should be used in conjunction with ForecastingGridSearchCV
         to get full utilization.
 
-        Single use of MultiplexerForecaster with component_estimators
-        and selected_estimator parameter specified,
+        Single use of MultiplexerForecaster with forecasters
+        and selected_forecaster parameter specified,
         works just like the selected component.
         It does not provide any further use in that case.
 
@@ -33,20 +36,20 @@ class MultiplexerForecaster(_OptionalForecastingHorizonMixin, _SktimeForecaster)
         provides an ability to compare different model class
         performances with each other, just like a model tournament.
         When ForecastingGridSearchCV is fitted with a MultiplexerForecaster,
-        returned value for the selected_estimator argument of best_params_
+        returned value for the selected_forecaster argument of best_params_
         attribute of ForecastingGridSearchCV, gives the best
-        performing model class among given models provided in component_estimators.
+        performing model class among given models provided in forecasters.
 
         Parameters
         ----------
-        component_estimators : list
+        forecasters : list
             List of (forecaster names, forecaster objects)
             MultiplexerForecaster switches between these forecasters
             objects when used with ForecastingGridSearchCV to
             find the optimal model
-        selected_estimator: str
-            An argument to make a selection among component_estimators.
-            MultiplexerForecaster uses selected_estimator
+        selected_forecaster: str
+            An argument to make a selection among forecasters.
+            MultiplexerForecaster uses selected_forecaster
             to choose which component to fit.
             Important for using with ForecastingGridSearchCV as a
             hyperparameter.
@@ -62,35 +65,35 @@ class MultiplexerForecaster(_OptionalForecastingHorizonMixin, _SktimeForecaster)
             _forecaster
         """
 
-        self.component_estimators = component_estimators
-        self.selected_estimator = selected_estimator
+        self.forecasters = forecasters
+        self.selected_forecaster = selected_forecaster
         self._check_components()
-        self._forecaster_fit_params = None
 
-        super(MultiplexerForecaster, self).__init__()
+        super(MultiplexForecaster, self).__init__()
 
     def _check_components(self):
-        if not isinstance(self.component_estimators, list):
+        if not isinstance(self.forecasters, list):
             raise Exception(
-                "Please provide a list " "for component_estimators composed of tuples"
+                "Please provide a list " "for forecasters composed of tuples"
             )
 
-        for component in self.component_estimators:
-            name, estimator = component
-            if not isinstance(estimator, _SktimeForecaster):
+        for component in self.forecasters:
+            name, forecaster = component
+            if not isinstance(forecaster, _SktimeForecaster):
                 raise Exception(
                     "Each component has to be an "
                     "sktime forecaster object. "
-                    "Please check {} with name:{}".format(estimator, name)
+                    "Please check {} with name:{}".format(forecaster, name)
                 )
 
     def _check_fit_params(self, fit_params):
-        if fit_params is None or fit_params == {}:
-            return
+        forecaster_fit_params = {}
 
-        for component in self.component_estimators:
+        if self.selected_forecaster is None or fit_params == {}:
+            return forecaster_fit_params
+
+        for component in self.forecasters:
             name, _ = component
-
             if name not in fit_params.keys():
                 raise KeyError(
                     "If you provide fit_params for models "
@@ -98,34 +101,26 @@ class MultiplexerForecaster(_OptionalForecastingHorizonMixin, _SktimeForecaster)
                     " match the associated component key."
                 )
 
-    def _check_selected_estimator(self):
-        component_names = [name for name, _ in self.component_estimators]
-        if self.selected_estimator not in component_names:
+        if self.selected_forecaster in fit_params.keys():
+            forecaster_fit_params = fit_params[self.selected_forecaster]
+            return forecaster_fit_params
+        else:
+            return forecaster_fit_params
+
+    def _check_selected_forecaster(self):
+        component_names = [name for name, _ in self.forecasters]
+        if self.selected_forecaster not in component_names:
             raise Exception(
-                "Please check the selected_estimator argument provided "
-                " Valid selected_estimator parameters: {}".format(component_names)
+                "Please check the selected_forecaster argument provided "
+                " Valid selected_forecaster parameters: {}".format(component_names)
             )
 
-    def _update_forecaster_fit_params(self, fit_params):
-        self._check_fit_params(fit_params)
-
-        if self.selected_estimator is None or fit_params is None:
-            self._forecaster_fit_params = None
-            return
-
-        if self.selected_estimator in fit_params.keys():
-            self._forecaster_fit_params = self.component_fit_params[
-                self.selected_estimator
-            ]
-        else:
-            self._forecaster_fit_params = None
-
     def _update_forecaster(self):
-        self._check_selected_estimator()
-        if self.selected_estimator is not None:
-            for name, estimator in self.component_estimators:
-                if self.selected_estimator == name:
-                    self._forecaster = clone(estimator)
+        self._check_selected_forecaster()
+        if self.selected_forecaster is not None:
+            for name, forecaster in self.forecasters:
+                if self.selected_forecaster == name:
+                    self._forecaster = clone(forecaster)
 
     def fit(self, y, X=None, fh=None, **fit_params):
         """Fit to training data.
@@ -147,17 +142,37 @@ class MultiplexerForecaster(_OptionalForecastingHorizonMixin, _SktimeForecaster)
         -------
         self : returns an instance of self.
         """
+
         self._set_y_X(y, X)
         self._set_fh(fh)
         self._update_forecaster()
-        self._update_forecaster_fit_params(fit_params=fit_params)
-
-        if fit_params:
-            self._forecaster.fit(y, X=X, fh=fh, **self._forecaster_fit_params)
-        else:
-            self._forecaster.fit(y, X=X, fh=fh)
-
+        forecaster_fit_params = self._check_fit_params(fit_params=fit_params)
+        self._forecaster.fit(y, X=X, fh=fh, **forecaster_fit_params)
         self._is_fitted = True
+        return self
+
+    def get_params(self, deep=True):
+        """Get parameters for the forecaster.
+        Parameters
+        ----------
+        deep : boolean, optional
+            If True, will return the parameters for this forecaster and
+            contained subobjects that are forecaster.
+        Returns
+        -------
+        params : mapping of string to any
+            Parameter names mapped to their values.
+        """
+        return self._get_params("forecasters", deep=deep)
+
+    def set_params(self, **kwargs):
+        """Set the parameters of this estimator.
+        Valid parameter keys can be listed with ``get_params()``.
+        Returns
+        -------
+        self
+        """
+        self._set_params("forecasters", **kwargs)
         return self
 
     def _predict(self, fh, X=None, return_pred_int=False, alpha=DEFAULT_ALPHA):
