71375dfb11d69c85970b4384aa9e2f55bda6fb19
Riyabelle25 <riya_e@es.iitr.ac.in>
2021-03-30 03:07:42 +0530

    Shift training loop to fit()

--- sktime/forecasting/croston.py
+++ sktime/forecasting/croston.py
@@ -5,7 +5,7 @@ from sktime.forecasting.base._sktime import _SktimeForecaster
 from sktime.forecasting.base._sktime import _OptionalForecastingHorizonMixin
 
 
-DEFAULT_ALPHA = 0.05
+DEFAULT_SMOOTHING = 0.05
 
 
 class Croston(_OptionalForecastingHorizonMixin, _SktimeForecaster):
@@ -22,30 +22,19 @@ class Croston(_OptionalForecastingHorizonMixin, _SktimeForecaster):
     -----------
         demand: array-like
             Historical data
-        future_periods: int
+        len_fh: int
             Time period for which predictions are required
-        alpha: float, optional(default=0.1)
-            Smoothing parameter
+        smoothing: float, optional(default=0.1)
+            smoothing parameter
 
     Returns:
     --------
         forecast: array-like
             Forecasted demand (on average per period) diff."""
 
-    def __init__(self, alpha=DEFAULT_ALPHA):
+    def __init__(self, smoothing=DEFAULT_SMOOTHING):
         # hyperparameter
-        self.alpha = alpha
-
-        # training data
-        self._y = None
-        self._X = None
-
-        # forecasting horizon
-        self._fh = None
-        # self.cutoff = None  # reference point for relative fh
-
-        # set _is_fitted to False
-        self._is_fitted = False
+        self.smoothing = smoothing
         super(Croston, self).__init__()
 
     def fit(self, y, X=None, fh=None):
@@ -57,38 +46,25 @@ class Croston(_OptionalForecastingHorizonMixin, _SktimeForecaster):
         self._set_fh(fh)
         self._is_fitted = True
 
-        return self
+        n_timepoints = len(y)  # Historical period: i.e the input array's length
+        smoothing = self.smoothing
 
-    def predict(self, fh=None, X=None, return_pred_int=False, alpha=DEFAULT_ALPHA):
-        if return_pred_int or X is not None:
-            raise NotImplementedError()
-        self.check_is_fitted()
-        self._set_fh(fh)
-
-        future_periods = len(self._fh.to_numpy())
-        alpha = self.alpha
-        d = self._y.to_numpy()  # Transform the input into a numpy array
-
-        cols = len(d)  # Historical period: i.e the demand array's length
-        d = np.append(
-            d, [np.nan] * future_periods
-        )  # Append np.nan into the demand array to cover future periods
-
-        # level(a), periodicity(p) and forecast(f)
-        q, a, f = np.full((3, cols + future_periods), np.nan)
+        y = y.to_numpy()  # Transform the input into a numpy array
+        # Fit the parameters: level(q), periodicity(a) and forecast(f)
+        q, a, f = np.full((3, n_timepoints + 1), np.nan)
         p = 1  # periods since last demand observation
 
         # Initialization:
-        first_occurrence = np.argmax(d[:cols] > 0)
-        q[0] = d[first_occurrence]
+        first_occurrence = np.argmax(y[:n_timepoints] > 0)
+        q[0] = y[first_occurrence]
         a[0] = 1 + first_occurrence
         f[0] = q[0] / a[0]
 
         # Create t+1 forecasts:
-        for t in range(0, cols):
-            if d[t] > 0:
-                q[t + 1] = alpha * d[t] + (1 - alpha) * q[t]
-                a[t + 1] = alpha * p + (1 - alpha) * a[t]
+        for t in range(0, n_timepoints):
+            if y[t] > 0:
+                q[t + 1] = smoothing * y[t] + (1 - smoothing) * q[t]
+                a[t + 1] = smoothing * p + (1 - smoothing) * a[t]
                 f[t + 1] = q[t + 1] / a[t + 1]
                 p = 1
             else:
@@ -96,12 +72,24 @@ class Croston(_OptionalForecastingHorizonMixin, _SktimeForecaster):
                 a[t + 1] = a[t]
                 f[t + 1] = f[t]
                 p += 1
+        self.f = f
 
-        # Future forecasts:
-        q[cols + 1 : cols + future_periods] = q[cols]
-        a[cols + 1 : cols + future_periods] = a[cols]
-        f[cols + 1 : cols + future_periods] = f[cols]
+        return self
+
+    def predict(
+        self, fh=None, X=None, return_pred_int=False, smoothing=DEFAULT_SMOOTHING
+    ):
+        if return_pred_int or X is not None:
+            raise NotImplementedError()
+
+        self.check_is_fitted()
+        self._set_fh(fh)
+        len_fh = len(self._fh.to_numpy())
+        f = self.f
+
+        # Predicting future forecasts:
+        y_pred = np.full(len_fh, f[-1])
+        self.f = np.append(self.f, y_pred)
 
         index = self.fh.to_absolute(self._cutoff)
-
-        return pd.Series(np.array(f[cols:]), index=index)
+        return pd.Series(y_pred, index=index)
