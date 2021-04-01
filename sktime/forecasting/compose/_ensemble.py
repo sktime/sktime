0603f20db955040e321236e6237d98d29c5ebb6f
Ifeanyi30 <liamsanctus30@gmail.com>
2021-04-01 06:36:00 -0700

    ensemble correction
    made more corrections on the code

--- sktime/forecasting/compose/_ensemble.py
+++ sktime/forecasting/compose/_ensemble.py
@@ -31,7 +31,8 @@ class EnsembleForecaster(
 
     def __init__(self, forecasters, n_jobs=None, agg="mean"):
         super(EnsembleForecaster, self).__init__(
-            forecasters=forecasters, n_jobs=n_jobs, agg)
+            forecasters=forecasters, n_jobs=n_jobs, agg=agg
+        )
 
     def fit(self, y, X=None, fh=None):
         """Fit to training data.
