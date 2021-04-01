b8ce205af536bd20947245ef103f128363072bc9
Walter <walmar2@emea.corpdir.net>
2021-04-01 12:42:18 +0200

    Added detailed exception output

--- sktime/forecasting/model_selection/_split.py
+++ sktime/forecasting/model_selection/_split.py
@@ -135,6 +135,8 @@ def _check_window_lengths(y, fh, window_length, initial_window):
         raise ValueError(
             "The `window_length` and the forecasting horizon are "
             "incompatible with the length of `y`."
+            f"Found `window_length`={window_length}, `fh_max`={fh_max},"
+            f"len(y)={n_timepoints}"
         )
 
     if initial_window is not None:
@@ -142,6 +144,8 @@ def _check_window_lengths(y, fh, window_length, initial_window):
             raise ValueError(
                 "The `initial_window` and the forecasting horizon are "
                 "incompatible with the length of `y`."
+                f"Found `initial_window`={initial_window},"
+                f"`fh_max`={fh_max}, len(y)={n_timepoints}"
             )
 
 
