65cd143d210d821e830be84a29103c5e748084c7
Walter <walmar2@emea.corpdir.net>
2021-04-14 19:10:44 +0200

    Fix indentation

--- sktime/forecasting/compose/tests/test_pipeline.py
+++ sktime/forecasting/compose/tests/test_pipeline.py
@@ -53,23 +53,24 @@ def test_pipeline():
     expected = compute_expected_y_pred(y_train, fh)
     np.testing.assert_array_equal(actual, expected)
 
-    def test_skip_inverse_transform():
-        # testing that transformers which have the "skip-inverse-transform" tag
-        # are working in a pipeline
-        y = load_airline()
-        # add nan and outlier
-        y.iloc[3] = np.nan
-        y.iloc[4] = y.iloc[4] * 20
 
-        y_train, y_test = temporal_train_test_split(y)
-        forecaster = TransformedTargetForecaster(
-            [
-                ("t1", HampelFilter(window_length=12)),
-                ("t2", Imputer(method="mean")),
-                ("forecaster", NaiveForecaster()),
-            ]
-        )
-        fh = np.arange(len(y_test)) + 1
-        forecaster.fit(y_train, fh=fh)
-        y_pred = forecaster.predict()
-        assert isinstance(y_pred, pd.Series)
+def test_skip_inverse_transform():
+    # testing that transformers which have the "skip-inverse-transform" tag
+    # are working in a pipeline
+    y = load_airline()
+    # add nan and outlier
+    y.iloc[3] = np.nan
+    y.iloc[4] = y.iloc[4] * 20
+
+    y_train, y_test = temporal_train_test_split(y)
+    forecaster = TransformedTargetForecaster(
+        [
+            ("t1", HampelFilter(window_length=12)),
+            ("t2", Imputer(method="mean")),
+            ("forecaster", NaiveForecaster()),
+        ]
+    )
+    fh = np.arange(len(y_test)) + 1
+    forecaster.fit(y_train, fh=fh)
+    y_pred = forecaster.predict()
+    assert isinstance(y_pred, pd.Series)
