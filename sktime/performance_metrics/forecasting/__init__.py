d10da84b638eb75db61eb73f2f64e85bc2d3d410
SHREYA SINGH <50830709+SinghShreya05@users.noreply.github.com>
2021-04-24 19:08:14 +0530

    Added function names and class names

--- sktime/performance_metrics/forecasting/__init__.py
+++ sktime/performance_metrics/forecasting/__init__.py
@@ -10,11 +10,26 @@ __all__ = [
     "smape_loss",
     "mape_loss",
     "make_forecasting_scorer",
+    "RMSSE", 
+    "MAD", 
+    "GMAE", 
+    "RMSPE"
+    "rmsse_loss", 
+    "mad_loss", 
+    "gmae_loss", 
+    "rmspe_loss"
 ]
 
 from sktime.performance_metrics.forecasting._classes import MASE
 from sktime.performance_metrics.forecasting._classes import make_forecasting_scorer
 from sktime.performance_metrics.forecasting._classes import sMAPE
+from sktime.performance_metrics.forecasting._classes import RMSSE
+from sktime.performance_metrics.forecasting._classes import MAD
+from sktime.performance_metrics.forecasting._classes import GMAE
+from sktime.performance_metrics.forecasting._classes import RMSPE
 from sktime.performance_metrics.forecasting._functions import mase_loss
 from sktime.performance_metrics.forecasting._functions import smape_loss
-from sktime.performance_metrics.forecasting._functions import mape_loss
+from sktime.performance_metrics.forecasting._functions import rmsse_loss 
+from sktime.performance_metrics.forecasting._functions import mad_loss
+from sktime.performance_metrics.forecasting._functions import gmae_loss
+from sktime.performance_metrics.forecasting._functions import rmspe_loss
