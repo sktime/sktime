61e71fdd99a819884061c5e21c2243ea23240b91
Ryan Kuhns <rnkuhns@gmail.com>
2021-04-30 11:38:28 -0400

    Add plot_correlations to plot series and acf/pacf

--- sktime/utils/plotting.py
+++ sktime/utils/plotting.py
@@ -2,16 +2,17 @@
 # -*- coding: utf-8 -*-
 # copyright: sktime developers, BSD-3-Clause License (see LICENSE file)
 
-__all__ = ["plot_series"]
+__all__ = ["plot_series", "plot_correlations"]
 __author__ = ["Markus Löning"]
 
 import numpy as np
 
 from sktime.utils.validation._dependencies import _check_soft_dependencies
 from sktime.utils.validation.forecasting import check_y
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 
 
-def plot_series(*series, labels=None, markers=None):
+def plot_series(*series, labels=None, markers=None, ax=None):
     """Plot one or more time series
 
     Parameters
@@ -76,8 +77,10 @@ def plot_series(*series, labels=None, markers=None):
     # generate integer x-values
     xs = [np.argwhere(index.isin(y.index)).ravel() for y in series]
 
-    # create figure
-    fig, ax = plt.subplots(1, figsize=plt.figaspect(0.25))
+    # create figure if no Axe provided for plotting
+    if ax is None:
+        fig, ax = plt.subplots(1, figsize=plt.figaspect(0.25))
+
     colors = sns.color_palette("colorblind", n_colors=n_series)
 
     # plot series
@@ -107,5 +110,87 @@ def plot_series(*series, labels=None, markers=None):
 
     if legend:
         ax.legend()
+    if ax is None:
+        return fig, ax
+    else:
+        return ax
 
-    return fig, ax
+
+def plot_correlations(
+    series,
+    lags=24,
+    alpha=0.05,
+    zero_lag=True,
+    acf_fft=False,
+    acf_adjusted=True,
+    pacf_method="ywadjusted",
+    suptitle=None,
+    series_title=None,
+    acf_title="Autocorrelation",
+    pacf_title="Partial Autocorrelation",
+):
+    """Plot series and its ACF and PACF values.
+
+    Parameters
+    ----------
+    lags : int, default = 24
+        Number of lags to include in ACF and PACF plots
+
+    alpha : int, default = 0.05
+        Alpha value used to set confidence intervals. Alpha = 0.05 results in
+        95% confidence interval with standard deviation calculated via
+        Bartlett's formula.
+
+    zero_lag : bool, default = True
+        If True, start ACF and PACF plots at 0th lag
+
+    acf_fft : bool,  = False
+        Whether to compute ACF via FFT.
+
+    acf_adjusted : bool, default = True
+        If True, denonimator of ACF calculations uses n-k instead of n, where
+        n is number of observations and k is the lag.
+
+    pacf_method : str, default = 'ywadjusted'
+        Method to use in calculation of PACF.
+
+    suptitle : str, default = None
+        The text to use as the Figure's suptitle.
+
+    series_title : str, default = None
+        Used to set the title of the series plot if provided. Otherwise, series
+        plot has no title.
+
+    acf_title : str, default = 'Autocorrelation'
+        Used to set title of ACF plot.
+
+    pacf_title : str, default = 'Partial Autocorrelation'
+        Used to set title of PACF plot.
+
+    Returns
+    -------
+    fig : matplotlib.figure.Figure
+
+    axes : np.ndarray
+        Array of the figure's Axe objects
+    """
+    _check_soft_dependencies("matplotlib", "seaborn")
+    import matplotlib.pyplot as plt
+
+    # Setup figure for plotting
+    fig = plt.figure(constrained_layout=True, figsize=(12, 8))
+    gs = fig.add_gridspec(2, 2)
+    f_ax1 = fig.add_subplot(gs[0, :])
+    if series_title is not None:
+        f_ax1.set_title(series_title)
+    f_ax2 = fig.add_subplot(gs[1, 0])
+    f_ax3 = fig.add_subplot(gs[1, 1])
+
+    # Create expected plots on their respective Axes
+    plot_series(series, ax=f_ax1)
+    plot_acf(series, ax=f_ax2, lags=lags, zero=zero_lag, alpha=alpha, title=acf_title)
+    plot_pacf(series, ax=f_ax3, lags=lags, zero=zero_lag, alpha=alpha, title=pacf_title)
+    if suptitle is not None:
+        fig.suptitle(suptitle, size="xx-large")
+
+    return fig, np.array(fig.get_axes())
