79645e94fb6ef11a19d35e7177409d9f11057848
thayeylolu <hamdallahhassanah@gmail.com>
2021-04-30 07:35:59 +0100

    refactors test and removes condition for type error

--- sktime/utils/validation/tests/test_check_window_length.py
+++ sktime/utils/validation/tests/test_check_window_length.py
@@ -5,33 +5,19 @@ import pytest
 from sktime.utils.validation import check_window_length
 
 
-def test_check_float_window_length():
-    assert check_window_length(0.2) == 1
+@pytest.mark.parametrize(
+    "arg,expected",
+    [
+        (0.2, 1),
+        (43, 43),
+        (None, None),
+    ],
+)
+def test_check_window_length(arg, expected):
+    assert check_window_length(arg) == expected
 
 
-def test_check_integer_window_length():
-    assert check_window_length(43) == 43
-
-
-def test_check_None_window_length():
-    assert check_window_length(None) is None
-
-
-def test_raises_exception_on_string_arg():
-    with pytest.raises(TypeError):
-        check_window_length("Any String")
-
-
-def test_negative_integer_window_length():
+@pytest.mark.parametrize("arg", ["string", -5, -0.5, 6.1, -2.4])
+def test_window_length_bad_arg(arg):
     with pytest.raises(ValueError):
-        check_window_length(-1)
-
-
-def test_negative_float_window_length():
-    with pytest.raises(ValueError):
-        check_window_length(-0.1)
-
-
-def test_large_float_window_length():
-    with pytest.raises(ValueError):
-        check_window_length(6.1)
+        check_window_length(arg)
