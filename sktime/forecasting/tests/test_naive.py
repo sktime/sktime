df6189da30d3c5a3d9f2cee68073fdbfe2b84a68
Senuka Dinujaya Withana Arachchige <43887812+D3ViL-NK@users.noreply.github.com>
2021-04-30 09:31:50 -0500

    Update test_naive.py

--- sktime/forecasting/tests/test_naive.py
+++ sktime/forecasting/tests/test_naive.py
@@ -180,6 +180,6 @@ def test_return_pred_int(fh):
     f.fit(y_train)
     alpha = 0.8
     y_pred, pred_intervals = f.predict(fh, alpha=alpha, return_pred_int=True)
-    pred_intervals = None  # I will change this with the results from R
-    expected = None
+    pred_intervals = np.array([0, 0, 0])  # will change this with the results from R
+    expected = np.array([0, 0, 0])
     np.testing.assert_array_equal(pred_intervals, expected)
