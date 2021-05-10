d42d4ae10bcfc8247ddf38fabcd7f47722fb200d
Matthew Middlehurst <pfm15hbu@uea.ac.uk>
2021-05-10 03:20:04 +0100

    almost equal for c22 test

--- sktime/transformations/panel/tests/test_catch22.py
+++ sktime/transformations/panel/tests/test_catch22.py
@@ -17,7 +17,7 @@ def test_catch22_on_gunpoint():
 
     # assert transformed data is the same
     data = c22.transform(X_train.iloc[indices])
-    testing.assert_array_equal(data, catch22_gunpoint_data)
+    testing.assert_array_almost_equal(data, catch22_gunpoint_data)
 
 
 def test_catch22_single_feature_on_gunpoint():
@@ -33,7 +33,7 @@ def test_catch22_single_feature_on_gunpoint():
     data = []
     for i in range(22):
         data.append(c22._transform_single_feature(X_train.iloc[indices], i))
-    testing.assert_array_equal(data, catch22_single_feature_gunpoint_data)
+    testing.assert_array_almost_equal(data, catch22_single_feature_gunpoint_data)
 
 
 def test_catch22_on_basic_motions():
@@ -47,7 +47,7 @@ def test_catch22_on_basic_motions():
 
     # assert transformed data is the same
     data = c22.transform(X_train.iloc[indices])
-    testing.assert_array_equal(data, catch22_basic_motions_data)
+    testing.assert_array_almost_equal(data, catch22_basic_motions_data)
 
 
 catch22_gunpoint_data = np.array(
