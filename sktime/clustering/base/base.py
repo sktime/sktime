121f6207d4de8966596ceaec9a1c6ee156df3664
chrisholder <chrisholder987@hotmail.com>
2021-05-19 12:56:24 +0100

    Improved the base code for k means

--- sktime/clustering/base/base.py
+++ sktime/clustering/base/base.py
@@ -1,24 +1,28 @@
 # -*- coding: utf-8 -*-
 from sktime.base import BaseEstimator
+from sktime.clustering.base.base_types import Data_Frame
+from typing import List, Union, Mapping
 
 
 class ClusterMixin(BaseEstimator):
-
-    def fit(self, X):
+    def fit(self, X: Data_Frame) -> None:
         raise NotImplementedError("abstract method")
 
-    def predict(self, X):
+    def predict(self, X: Data_Frame) -> List[List[int]]:
         raise NotImplementedError("abstract method")
 
+    def fit_predict(self, X: Data_Frame) -> List[List[int]]:
+        self.fit(X)
+        return self.predict(X)
 
-class TransformerMixin:
 
-    def fit_transform(self, X, y=None, sample_weight=None):
+class CenterInitializerMixin:
+    @staticmethod
+    def initialize_centers(df: Data_Frame, n_centers: int) -> Data_Frame:
         raise NotImplementedError("abstract method")
 
 
 class AverageMixin:
-
     @staticmethod
     def average(series, iterations=100):
         """
@@ -33,3 +37,7 @@ class AverageMixin:
             The number of iterations
         """
         raise NotImplementedError("abstract method")
+
+
+Init_Algo = Union[str, CenterInitializerMixin]
+Init_Algo_Dict = Mapping[str, CenterInitializerMixin]
