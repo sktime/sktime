8d0b94b4992083f3c1e388afaeaf9f644759e68f
RNKuhns <RNKuhns@gmail.com>
2021-06-03 07:54:09 -0400

    Update performance metrics to handle y_train (#858)
    * Updated forecasting metrics to handle y_train
    
    * Unified interface for metric functions and classes
    
    * Raise NotImplementedError in check_scoring for
    metrics that require y_pred_benchmark
    
    * Switched order of check_scoring checks
    
    * Updated check_scoring handling of y_pred_benchmark
    
    * Changed reference of loss func or class to metric
    
    * Tweaked tags and improved error handling
    
    * Added comment to BaseMetric about _all_tags
    
    * Tweaked _get_kwarg and docstrings
    
    * Tweaked docstrings
    
    * Fixed check_scoring validation
    
    * Added @RNKuhns as performance metric code owner
    
    * Fixed docstrings
    
    * Added config to skip test files in pydocstyle
    
    * Tweaked performance_metric docs
    
    * Switched doc automodule back to prior setup
    
    * Update evaluate to work with new metric interface
    
    * Remove make_forecasting_scorer from test_tune
    
    Test tune was creating scorer from scikit-learn
    mean_squared_error. Using sktime metric class now.
    
    Also updated documentation of
    make_forecasting_scorer to make input function
    signature clear.
    
    * Added comment clarifying tests using MASE
    
    * Removed check on y_train in scaled metrics
    
    * Tweak performance metrics used in test_evaluate
    
    Co-authored-by: Markus Löning <markus.loning@gmail.com>

--- sktime/forecasting/model_evaluation/_functions.py
+++ sktime/forecasting/model_evaluation/_functions.py
@@ -22,40 +22,40 @@ def evaluate(
     fit_params=None,
     return_data=False,
 ):
-    """Evaluate forecaster using cross-validation
+    """Evaluate forecaster using timeseries cross-validation.
 
     Parameters
     ----------
     forecaster : sktime.forecaster
         Any forecaster
-    y : pd.Series
-        Target time series to which to fit the forecaster.
-    X : pd.DataFrame, optional (default=None)
-        Exogenous variables
     cv : Temporal cross-validation splitter
         Splitter of how to split the data into test data and train data
-    strategy : str, optional (default="refit")
+    y : pd.Series
+        Target time series to which to fit the forecaster.
+    X : pd.DataFrame, default=None
+        Exogenous variables
+    strategy : {"refit", "update"}
         Must be "refit" or "update". The strategy defines whether the `forecaster` is
         only fitted on the first train window data and then updated, or always refitted.
-    scoring : object of class MetricFunctionWrapper from
-        sktime.performance_metrics, optional. Example scoring=sMAPE().
-        Used to get a score function that takes y_pred and y_test as arguments,
-        by default None (if None, uses sMAPE)
-    fit_params : dict, optional (default=None)
+    scoring : subclass of sktime.performance_metrics.BaseMetric, default=None.
+        Used to get a score function that takes y_pred and y_test arguments
+        and accept y_train as keyword argument.
+        If None, then uses scoring = MeanAbsolutePercentageError(symmetric=True).
+    fit_params : dict, default=None
         Parameters passed to the `fit` call of the forecaster.
-    return_data : bool, optional
+    return_data : bool, default=False
         Returns three additional columns in the DataFrame, by default False.
         The cells of the columns contain each a pd.Series for y_train,
         y_pred, y_test.
 
     Returns
-    -------
+    ----------
     pd.DataFrame
         DataFrame that contains several columns with information regarding each
         refit/update and prediction of the forecaster.
 
     Example
-    --------
+    -------
     >>> from sktime.datasets import load_airline
     >>> from sktime.forecasting.model_evaluation import evaluate
     >>> from sktime.forecasting.model_selection import ExpandingWindowSplitter
@@ -103,7 +103,7 @@ def evaluate(
         pred_time = time.time() - start_pred
 
         # score
-        score = scoring(y_pred, y_test)
+        score = scoring(y_test, y_pred, y_train=y_train)
 
         # save results
         results = results.append(
@@ -129,7 +129,7 @@ def evaluate(
 
 
 def _split(y, X, train, test, fh):
-    """Split y and X for given train and test set indices"""
+    """Split y and X for given train and test set indices."""
     y_train = y.iloc[train]
     y_test = y.iloc[test]
 
@@ -152,7 +152,7 @@ def _split(y, X, train, test, fh):
 
 
 def _check_strategy(strategy):
-    """Assert strategy value
+    """Assert strategy value.
 
     Parameters
     ----------
