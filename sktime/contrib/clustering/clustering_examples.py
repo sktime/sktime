84ee1669153870c7efb01699baf1d6bf9ff508bc
Tony Bagnall <ajb@uea.ac.uk>
2021-06-04 17:36:19 +0100

    Update clustering_examples.py

--- sktime/contrib/clustering/clustering_examples.py
+++ sktime/contrib/clustering/clustering_examples.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 """Clustering usage tests and examples"""
 
-
-def test_loading():
-    # test multivariate
-    # Test univariate
-    data_dir = "E:/tsc_ts/"
-    dataset = "Gunpoint"
-    trainX, trainY = load_ts(data_dir + dataset + "/" + dataset + "_TRAIN.ts")
-    testX, testY = load_ts(data_dir + dataset + "/" + dataset + "_TEST.ts")
-    print("Loaded " + dataset + " in position " + str(i))
+from sktime.clustering import (
+    TimeSeriesKMeans,
+    TimeSeriesKMedoids,
+)
 
 
 if __name__ == "__main__":
     """
-    Example simple usage, with arguments input via script or hard coded for testing
+    Example simple usage, with arguments input via script or hard coded for testing.
     """
+    clusterer1 = TimeSeriesKMeans()
+    clusterer2 = TimeSeriesKMedoids()
+
+
+
 
