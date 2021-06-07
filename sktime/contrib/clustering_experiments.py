e9c2796f72b2408aea274ffbabfcbcca15e61606
Tony Bagnall <ajb@uea.ac.uk>
2021-06-07 21:29:36 +0100

    remove mystery whitespace comment

--- sktime/contrib/clustering_experiments.py
+++ sktime/contrib/clustering_experiments.py
@@ -34,9 +34,6 @@ from sktime.utils.data_io import load_from_tsfile_to_dataframe as load_ts
 import sktime.datasets.tsc_dataset_names as dataset_lists
 
 __author__ = ["Tony Bagnall"]
-"""Prototype mechanism for testing clusterers on the UCR data, mirroring the 
-classification code.
-"""
 
 
 def set_clusterer(cls, resampleId=None):
