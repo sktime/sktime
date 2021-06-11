233c3553be6ef47405a18f82bd1361ab538549c5
Abhishek Tiwari <55629809+tabhishek432@users.noreply.github.com>
2021-06-11 05:57:28 +0530

    Add example to docstring for each classifier

--- sktime/classification/interval_based/_stsf.py
+++ sktime/classification/interval_based/_stsf.py
@@ -54,6 +54,17 @@ class SupervisedTimeSeriesForest(ForestClassifier, BaseClassifier):
     intervals    : array of shape = [n_estimators][3][7][n_intervals][2] stores
     indexes of all start and end points for all classifiers for each representaion
     and feature
+    
+    Example
+    -------
+    >>> from sktime.classification.interval_based import SupervisedTimeSeriesForest
+    >>> from sklearn.model_selection import train_test_split
+    >>> from sktime.datasets import load_arrow_head
+    >>> X, y = load_arrow_head(return_X_y=True)
+    >>> X_train, X_test, y_train, y_test = train_test_split(X, y)
+    >>> clf = SupervisedTimeSeriesForest(n_estimators=10, n_jobs = 2, random_state=42)
+    >>> clf.fit(X_train, y_train)
+    >>> clf.score(X_test, y_test)
 
     Notes
     -----
