577a5364009bc86fca19d2293a2169876220b2f2
Abhishek Tiwari <55629809+tabhishek432@users.noreply.github.com>
2021-06-12 20:42:11 +0530

    Add eg to docstring for STSF classifier
    Add eg to docstring for STSF(interval_based) classifier

--- sktime/classification/interval_based/_stsf.py
+++ sktime/classification/interval_based/_stsf.py
@@ -62,7 +62,7 @@ class SupervisedTimeSeriesForest(ForestClassifier, BaseClassifier):
     >>> from sktime.datasets import load_arrow_head
     >>> X, y = load_arrow_head(return_X_y=True)
     >>> X_train, X_test, y_train, y_test = train_test_split(X, y)
-    >>> clf = SupervisedTimeSeriesForest(n_estimators=10, n_jobs = 2, random_state=42)
+    >>> clf = SupervisedTimeSeriesForest(n_estimators=10, n_jobs = 1, random_state=42)
     >>> clf.fit(X_train, y_train)
     >>> clf.score(X_test, y_test)
 
