0ac507802a33ddf8ee9bb7526a8c3ea31709eb2a
Viktor Kazakov <viktorkazakov@outlook.com>
2021-06-15 15:48:18 +0100

    minor change to config

--- sktime/tests/_config.py
+++ sktime/tests/_config.py
@@ -287,9 +287,9 @@ ESTIMATOR_TEST_PARAMS = {
                 "forecaster",
                 NaiveForecaster(strategy="mean"),
                 {
-                    "fit": {"y": "imputer", "X": "original_X", "fh": "original_fh"},
+                    "fit": {"y": "imputer", "fh": "original_fh"},
                     "predict": {"fh": "original_fh"},
-                    "update": {"y": "imputer", "X": "original_X"},
+                    "update": {"y": "imputer"},
                 },
             ),
         ]
