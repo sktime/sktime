9468c0dfecdcef652848518eaf196a9af33d8bc7
Guzal Bulatova <guzalbulatova@gmail.com>
2021-06-18 15:08:46 +0200

    added simple tests

---
+++ sktime/transformations/series/tests/test_theta.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3 -u
+# -*- coding: utf-8 -*-
+"""copyright: sktime developers, BSD-3-Clause License (see LICENSE file)."""
+
+__author__ = ["Guzal Bulatova"]
+__all__ = []
+
+import numpy as np
+from scipy.stats import linregress
+
+from sktime.datasets import load_airline
+from sktime.transformations.series.theta import ThetaLinesTransformer
+
+
+def test_theta_standard():
+    y = load_airline()
+
+    t = ThetaLinesTransformer(0)
+    actual = t.transform(y)
+    x = np.arange(y.size) + 1
+    lin_regress = linregress(x, y)
+    expected_0 = lin_regress.intercept + lin_regress.slope * x
+
+    np.testing.assert_array_equal(actual, expected_0)
