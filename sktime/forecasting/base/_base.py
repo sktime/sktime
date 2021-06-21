bf16b0b4930b7e256a4db38039d798c07a621bbc
Franz Király <f.kiraly@ucl.ac.uk>
2021-06-21 22:04:11 +0100

    added tags in BaseForecaster; some new tags

--- sktime/forecasting/base/_base.py
+++ sktime/forecasting/base/_base.py
@@ -58,6 +58,14 @@ class BaseForecaster(BaseEstimator):
     forecasters.
     """
 
+    _tags = {
+        "requires-fh-in-fit": True,  # is forecasting horizon already required in fit?
+        "handles_missing_data": False,  # can estimator handle missing data?
+        "univariate_only": True,  # can estimator deal with multivariate series y?
+        "X-y_must_have_same_index": True,  # can estimator handle different X/y index?
+        "enforce_index_type": None  # index type that needs to be enforced in X/y
+    }
+
     def __init__(self):
         self._is_fitted = False
 
