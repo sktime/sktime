7a525ff17a2594aefa28b0dec60afcf25115e49e
Franz Király <f.kiraly@ucl.ac.uk>
2021-06-22 21:08:09 +0100

    split annotator in panel and stream

--- sktime/annotation/base/_base.py
+++ sktime/annotation/base/_base.py
@@ -1,17 +1,40 @@
 # -*- coding: utf-8 -*-
 """
-Base class template for forecaster scitype.
+Base class template for annotator base types:
+ - BaseStreamAnnotator - annotating a data stream, with temporal update
+ - BasePanelAnnotator - annotating a panel of sequences or time series
 
-    class name: BaseAnnotator
+ Class summaries below:
+
+---
+
+    class name: BasePanelAnnotator
 
 Scitype defining methods:
     fitting              - fit(self, X, Y=None, Z=None)
-    annotating           - predict(self, X, Y=None, Z=None)
+    annotating           - predict(self, X, Z=None)
 
     NOT YET IMPLEMENTED
-    updating (temporal)  - update(self, X, Y=None, Z=None)
-    update&predict       - update_predict(self, X, X_new, Y=None, Z=None, Z_new=None)
-    updating (minibatch) - update_batch(self, X, Y=None, Z=None)
+    updating (minibatch) - update(self, X, Y=None, Z=None)
+    update&annotate      - update_predict(self, X, X_new, Y=None, Z=None, Z_new=None)
+
+Inspection methods:
+    hyper-parameter inspection  - get_params()
+    fitted parameter inspection - get_fitted_params()
+
+State:
+    fitted model/strategy   - by convention, any attributes ending in "_"
+    fitted state flag       - check_is_fitted()
+
+---
+
+    class name: BaseStreamAnnotator
+
+Scitype defining methods:
+    fitting              - fit(self, X, Y=None)
+    annotating           - predict(self, X)
+    updating (temporal)  - update(self, X, Y=None)
+    update&annotate      - update_predict(self, X)
 
 Inspection methods:
     hyper-parameter inspection  - get_params()
@@ -31,7 +54,7 @@ from sktime.base import BaseEstimator
 import pandas as pd
 
 
-class BaseAnnotator(BaseEstimator):
+class BasePanelAnnotator(BaseEstimator):
     """Base annotator
 
     The base annotator specifies the methods and method
@@ -43,7 +66,7 @@ class BaseAnnotator(BaseEstimator):
 
     # default tags
     _tags = {
-        "handles-panel": False,  # can handle panel annotations, i.e., list X/y?
+        "handles-panel": True,  # can handle panel annotations, i.e., list X/y?
         "handles-missing-data": False,  # can handle missing data in X, Y, Z
         "annotation-type": "point",  # can be point, segment or both
         "annotation-labels": "none",  # can be one of, or list-subset of
@@ -54,7 +77,7 @@ class BaseAnnotator(BaseEstimator):
 
         self._is_fitted = False
 
-        super(BaseAnnotator, self).__init__()
+        super(BasePanelAnnotator, self).__init__()
 
     def fit(self, X, Y=None, Z=None):
         """Fit to training data.
@@ -199,7 +222,198 @@ class BaseAnnotator(BaseEstimator):
         raise NotImplementedError("abstract method")
 
 
-# maybe for later:
-# class BaseSegmenter(BaseAnnotator)
-#
-# here you override fit/predict
+class BaseStreamAnnotator(BaseEstimator):
+    """Base stream annotator
+    assumes "predict" data is temporal future of "fit"
+        single time series in both, no meta-data
+
+    The base stream annotator specifies the methods and method
+    signatures that all annotators have to implement.
+
+    Specific implementations of these methods is deferred to concrete
+    annotators.
+    """
+
+    # default tags
+    _tags = {
+        "handles-panel": False,  # can handle panel annotations, i.e., list X/Y?
+        "handles-missing-data": False,  # can handle missing data in X, Y
+        "annotation-type": "point",  # can be point, segment or both
+        "annotation-labels": "none",  # can be one of, or list-subset of
+        #   "label", "outlier", "change"
+    }
+
+    def __init__(self):
+
+        self._is_fitted = False
+
+        self._X = None
+        self._Y = None
+
+        super(BaseStreamAnnotator, self).__init__()
+
+    def fit(self, X, Y=None):
+        """Fit to training data.
+
+        Parameters
+        ----------
+        X : pd.DataFrame
+            training data to fit model to, time series
+        Y : pd.Series, optional
+            ground truth annotations for training if annotator is supervised
+        Returns
+        -------
+        self : returns a reference to self
+
+        State change
+        ------------
+        creates fitted model (attributes ending in "_")
+        sets _is_fitted flag to true
+        """
+
+        self._X = X
+        self._Y = Y
+
+        # fkiraly: insert checks/conversions here, after PR #1012 I suggest
+
+        self._fit(self, X=X, Y=Y)
+
+        # this should happen last
+        self._is_fitted = True
+
+        return self
+
+    def predict(self, X):
+        """Create annotations on test/deployment data.
+
+        Parameters
+        ----------
+        X : pd.DataFrame - data to annotate, time series
+
+        Returns
+        -------
+        Y : pd.Series - annotations for sequence X
+            exact format depends on annotation type
+        """
+
+        self.check_is_fitted()
+
+        # fkiraly: insert checks/conversions here, after PR #1012 I suggest
+
+        Y = self._predict(self, X=X)
+
+        return Y
+
+    def update(self, X, Y=None):
+        """update model with new data and optional ground truth annotations
+
+        Parameters
+        ----------
+        X : pd.DataFrame
+            training data to update model with, time series
+        Y : pd.Series, optional
+            ground truth annotations for training if annotator is supervised
+        Returns
+        -------
+        self : returns a reference to self
+
+        State change
+        ------------
+        updates fitted model (attributes ending in "_")
+        """
+
+        self.check_is_fitted()
+
+        self._X = self._X.append(X)
+
+        if Y is not None:
+            self._Y.append(Y)
+
+        self._update(X=X, Y=Y)
+
+        return self
+
+    def update_predict(self, X):
+        """update model with new data and create annotations for it
+
+        Parameters
+        ----------
+        X : pd.DataFrame
+            training data to update model with, time series
+
+        Returns
+        -------
+        Y : pd.Series - annotations for sequence X
+            exact format depends on annotation type
+
+        State change
+        ------------
+        updates fitted model (attributes ending in "_")
+        """
+
+        self.update(X=X)
+        Y = self.predict(X)
+
+        return Y
+
+    def _fit(self, X, Y=None):
+        """Fit to training data.
+
+        core logic
+
+        Parameters
+        ----------
+        X : pd.DataFrame
+            training data to fit model to, time series
+        Y : pd.Series, optional
+            ground truth annotations for training if annotator is supervised
+        Returns
+        -------
+        self : returns a reference to self
+
+        State change
+        ------------
+        creates fitted model (attributes ending in "_")
+        """
+        raise NotImplementedError("abstract method")
+
+    def _predict(self, X):
+        """Create annotations on test/deployment data.
+
+        core logic
+
+        Parameters
+        ----------
+        X : pd.DataFrame - data to annotate, time series
+
+        Returns
+        -------
+        Y : pd.Series - annotations for sequence X
+            exact format depends on annotation type
+        """
+        raise NotImplementedError("abstract method")
+
+    def _update(self, X, Y=None):
+        """update model with new data and optional ground truth annotations
+
+        core logic
+
+        Parameters
+        ----------
+        X : pd.DataFrame
+            training data to update model with, time series
+        Y : pd.Series, optional
+            ground truth annotations for training if annotator is supervised
+        Returns
+        -------
+        self : returns a reference to self
+
+        State change
+        ------------
+        updates fitted model (attributes ending in "_")
+        """
+
+        # default/fallback: re-fit to all data
+        self._fit(self._X, self._Y)
+
+        return self
