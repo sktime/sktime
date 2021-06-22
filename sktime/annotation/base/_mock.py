227ab0a0b0c7fc1238d87ad2ff35cabfaa4f6b29
Franz Király <f.kiraly@ucl.ac.uk>
2021-06-22 21:08:17 +0100

    two versions of mock, panel and stream

--- sktime/annotation/base/_mock.py
+++ sktime/annotation/base/_mock.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-from sktime.annotation.base._base import BaseAnnotator
+from sktime.annotation.base._base import BasePanelAnnotator, BaseStreamAnnotator
 
 
-class MockAnnotator(BaseAnnotator):
-    """Mock annotator
+class MockPanelAnnotator(BasePanelAnnotator):
+    """Mock stream annotator
 
     for testing purposes. This "silly" annotator annotates
         the n-th time point as positive, everything else negative
@@ -33,6 +33,41 @@ class MockAnnotator(BaseAnnotator):
         for Xi in Xt:
             Xi.iloc[:] = False
             if len(Xi) > n:
-                Xt.iloc[n] = True
+                Xi.iloc[n] = True
+
+        return Xt
+
+
+class MockStreamAnnotator(BaseStreamAnnotator):
+    """Mock stream annotator
+
+    for testing purposes. This "silly" annotator annotates
+        the n-th time point as positive, everything else negative
+
+    Parameters
+    ----------
+    n : int, optional (default=1)
+        the iloc index of the sequence(s) to annotate as positive
+    """
+
+    def __init__(self, n=1):
+
+        self.n = n
+
+        super().__init__()
+
+    def _fit(self, X, Y=None, Z=None):
+
+        return self
+
+    def _predict(self, X, Y=None):
+
+        n = self.n
+
+        Xt = X.copy()
+
+        Xt.iloc[:] = False
+        if len(Xt) > n:
+            Xt.iloc[n] = True
 
         return Xt
