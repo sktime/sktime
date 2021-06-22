df32de9743b1660ebe743efdbac9085bb89d9504
thayeylolu <hamdallahhassanah@gmail.com>
2021-06-22 12:57:57 +0100

    changes to the requires_fit in mutliplexer

--- sktime/forecasting/compose/_multiplexer.py
+++ sktime/forecasting/compose/_multiplexer.py
@@ -80,7 +80,7 @@ class MultiplexForecaster(_HeterogenousEnsembleForecaster):
 
     _tags = {
         "univariate-only": True,
-        "requires-fh-in-fit": True,
+        "requires-fh-in-fit": False,
         "handles-missing-data": False,
     }
 
