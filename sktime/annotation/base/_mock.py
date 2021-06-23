e54f8f0b2e27347e5b0c5e3d9ba4d29fe534bddc
Franz Király <f.kiraly@ucl.ac.uk>
2021-06-23 20:58:30 +0100

    added back mock panel annotator

---
+++ sktime/annotation/base/_mock.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from sktime.annotation.base._base_panel import BasePanelAnnotator
+
+
+class MockPanelAnnotator(BasePanelAnnotator):
+    """Mock stream annotator
+
+    for testing purposes. This "silly" annotator annotates
+        the n-th time point as positive, everything else negative
+
+    Parameters
+    ----------
+    n : int, optional (default=1)
+        the iloc index of the sequence(s) to annotate as positive
+    """
+
+    def __init__(self, n=1):
+
+        self.n = n
+
+        super().__init__()
+
+    def _fit(self, X, Y=None, Z=None):
+
+        return self
+
+    def _predict(self, X, Y=None, Z=None):
+
+        n = self.n
+
+        Xt = X.copy()
+
+        for Xi in Xt:
+            Xi.iloc[:] = False
+            if len(Xi) > n:
+                Xi.iloc[n] = True
+
+        return Xt
