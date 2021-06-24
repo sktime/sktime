df97e7a796262045c0e2e504e73d7fe32b7d6cf6
Tony Bagnall <ajb@uea.ac.uk>
2021-06-24 14:19:41 +0100

    remove unused import

--- sktime/utils/data_io.py
+++ sktime/utils/data_io.py
@@ -5,7 +5,6 @@ import textwrap
 
 import numpy as np
 import pandas as pd
-from sklearn.metrics import accuracy_score as acc
 from sktime.utils.data_processing import _make_column_names, from_long_to_nested
 
 
