9831df43413383120989162b8375f156c815bd39
Franz Király <f.kiraly@ucl.ac.uk>
2021-06-27 21:26:03 +0100

    I guess this was breaking the setup.py?

--- sktime/utils/__init__.py
+++ sktime/utils/__init__.py
@@ -1,13 +1,1 @@
 # -*- coding: utf-8 -*-
-
-from sktime.registry import (
-    _get_name,
-    _has_tag,
-    all_estimators,
-)
-
-__all__ = [
-    "_get_name",
-    "_has_tag",
-    "all_estimators",
-]
