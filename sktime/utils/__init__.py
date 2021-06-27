b8882e6fff54e6e8f7c5c1c7a1da587773fd7eef
Franz Király <f.kiraly@ucl.ac.uk>
2021-06-27 21:01:16 +0100

    added all_estimators to utils registry to make it downwards compatible

--- sktime/utils/__init__.py
+++ sktime/utils/__init__.py
@@ -1,1 +1,13 @@
 # -*- coding: utf-8 -*-
+
+from sktime.registry import (
+    _get_name,
+    _has_tag,
+    all_estimators,
+)
+
+__all__ = [
+    "_get_name",
+    "_has_tag",
+    "all_estimators",
+]
