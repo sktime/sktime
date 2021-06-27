3bdbd1ecee900e5b84ebb254852840c5954a06c0
Tony Bagnall <ajb@uea.ac.uk>
2021-06-27 19:17:47 +0100

    Clustering (#1049)
    * basic functionality
    
    * basic kmeans implementation
    
    * Improved the base code for k means
    
    * Improved the base code for k means
    
    * Renamed misslabeled mixins
    
    * Removed accidental edit
    
    * Delete pyvenv.cfg
    
    * Delete python3.9
    
    * Delete easy_install
    
    * Delete easy_install-3.9
    
    * Delete python3
    
    * Delete python
    
    * Delete pip
    
    * Delete pip3
    
    * Delete pip3.9
    
    * Fixed linting
    
    * Barycenter averaging implemented
    
    * Update .all-contributorsrc
    
    * Update .all-contributorsrc
    
    * Fixed tests
    
    * Fixed warning from sklearn
    
    * Added k medoids and reformatted structure
    
    * basic functionality
    
    * basic kmeans implementation
    
    * Improved the base code for k means
    
    * Improved the base code for k means
    
    * Renamed misslabeled mixins
    
    * Removed accidental edit
    
    * Delete pyvenv.cfg
    
    * Delete python3.9
    
    * Delete easy_install
    
    * Delete easy_install-3.9
    
    * Delete python3
    
    * Delete python
    
    * Delete pip
    
    * Delete pip3
    
    * Delete pip3.9
    
    * Fixed linting
    
    * Barycenter averaging implemented
    
    * Fixed tests
    
    * Update .all-contributorsrc
    
    * Update .all-contributorsrc
    
    * Fixed warning from sklearn
    
    * Added k medoids and reformatted structure
    
    * rebased from main
    
    * first refactor of experiments
    
    * some doc strings added
    
    * removed dependency and reverted accidental delete
    
    * fixed base estimator
    
    * adding all to clustering directory
    
    * clustering examples
    
    * Update clustering_examples.py
    
    * updated defaults
    
    * Updated docstrings
    
    * clustering experiments changes
    
    * add check_X to partition  clusterer
    
    * formatting
    
    * formatting 2
    
    * remove mystery whitespace comment
    
    * Final cluster update before Main (#979)
    
    * Followed feedback given on pr. Additionally added the unit test for BaseCluster as a new estimator
    
    * fixed error in merge
    
    * Suggested changes and improved unit tests (#1000)
    
    * Followed feedback given on pr. Additionally added the unit test for BaseCluster as a new estimator
    
    * fixed error in merge
    
    * Made Markus changes and improved unit tests
    
    * fixed softdepend error
    
    * removed extra
    
    * reverted accidental change
    
    * fixed bug
    
    * Removed softdependency
    
    * Clustering (#1045)
    
    * Reverted everything and added only the files I've changed back in to hopefully fix the pr
    
    * Reverted everything and added only the files I've changed back in to hopefully fix the pr
    
    * Changed name back
    
    * Update .all-contributorsrc
    
    * Clustering small changes (#1054)
    
    * Reverted everything and added only the files I've changed back in to hopefully fix the pr
    
    * Reverted everything and added only the files I've changed back in to hopefully fix the pr
    
    * Changed name back
    
    * Final Markus changes and updated some docs
    
    * fixed linting
    
    * removed upsupport annotations
    
    * softdependencies updated
    
    * softdependencies updated
    
    Co-authored-by: Chris Holder <chrisholder987@hotmail.com>

---
+++ sktime/clustering/base/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+__all__ = [
+    "BaseCluster",
+    "ClusterMixin",
+    "BaseClusterCenterInitializer",
+    "BaseClusterAverage",
+    "BaseApproximate",
+]
+
+from sktime.clustering.base.base import (
+    BaseCluster,
+    ClusterMixin,
+    BaseClusterCenterInitializer,
+    BaseClusterAverage,
+    BaseApproximate,
+)
