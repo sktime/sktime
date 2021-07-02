9f49ea61ca78c7ce67ba5bfad2a63117fae37c9d
chrisholder <chrisholder987@hotmail.com>
2021-07-02 20:35:36 +0100

    initial tests implemented

--- sktime/dists_kernels/tests/test_all_dist_kernels.py
+++ sktime/dists_kernels/tests/test_all_dist_kernels.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import numpy as np
 import pytest
 
 from sktime.dists_kernels._base import BasePairwiseTransformer
@@ -6,62 +7,59 @@ from sktime.utils import all_estimators
 from sktime.utils._testing.panel import make_transformer_problem
 
 PAIRWISE_TRANSFORMERS_TAB = all_estimators(
-    estimator_types="transformer-pairwise-tab", return_names=False
+    estimator_types="transformer-pairwise-tabular", return_names=False
 )
 PAIRWISE_TRANSFORMERS_PAN = all_estimators(
-    estimator_types="transformer-pairwise-ts", return_names=False
+    estimator_types="transformer-pairwise-panel", return_names=False
 )
 
-# testing data
 X1 = make_transformer_problem(
-    n_instances=1, n_columns=2, random_state=1, return_numpy=True
+    n_instances=4, n_columns=4, n_timepoints=5, random_state=1, return_numpy=True
 )[0]
 X2 = make_transformer_problem(
-    n_instances=1, n_columns=2, random_state=2, return_numpy=True
+    n_instances=5, n_columns=5, n_timepoints=5, random_state=2, return_numpy=True
 )[0]
+
 X1_df = make_transformer_problem(
-    n_instances=1, n_columns=1, random_state=1, return_numpy=False
-)
+    n_instances=4, n_columns=4, n_timepoints=5, random_state=1, return_numpy=True
+)[0]
 X2_df = make_transformer_problem(
-    n_instances=1, n_columns=1, random_state=2, return_numpy=False
-)
+    n_instances=5, n_columns=5, n_timepoints=5, random_state=2, return_numpy=True
+)[0]
 
 
 @pytest.mark.parametrize("pairwise_transformers_tab", PAIRWISE_TRANSFORMERS_TAB)
 def test_pairwise_transformers_tab(pairwise_transformers_tab):
+    _general_pairwise_transformer_tests(X1, X2, pairwise_transformers_tab)
+    _general_pairwise_transformer_tests(X1_df, X2_df, pairwise_transformers_tab)
+
+
+def _general_pairwise_transformer_tests(x, y, pairwise_transformers_tab):
     transformer: BasePairwiseTransformer = pairwise_transformers_tab()
-    tranformation = transformer.transform(X1, X2)
-    return tranformation
 
+    # test return matrix
+    transformation = transformer.transform(X1, X2)
 
-# @pytest.mark.parametrize("pairwise_transformers_pan", PAIRWISE_TRANSFORMERS_PAN)
-# def test_pairwise_transformers_tab(pairwise_transformers_pan):
-#     transformer = pairwise_transformers_pan(X1, X2)
-#     test = transformer
-#     print(test)
-#     print(type(test))
+    assert transformer.symmetric is False, (
+        f"Symmetric is set to wrong value for {pairwise_transformers_tab} "
+        f"when both X1 and X2 passed"
+    )
+    assert isinstance(
+        transformation, np.ndarray
+    ), f"Shape of matrix returned is wrong for {pairwise_transformers_tab}"
+    assert transformation.shape == (
+        4,
+        5,
+    ), f"Shape of matrix returned is wrong for {pairwise_transformers_tab}"
 
-
-"""
-Behaviour: returns pairwise distance/kernel matrix
-    between samples in X and X2
-        if X2 is not passed, is equal to X
-
-alias for transform
-
-Parameters
-----------
-X: pd.DataFrame of length n, or 2D np.array with n rows
-X2: pd.DataFrame of length m, or 2D np.array with m rows, optional
-    default X2 = X
-
-Returns
--------
-distmat: np.array of shape [n, m]
-    (i,j)-th entry contains distance/kernel between X.iloc[i] and X2.iloc[j]
-
-Writes to self
---------------
-symmetric: bool = True if X2 was not passed, False if X2 was passed
-    for use to make internal calculations efficient, e.g., in _transform
-"""
+    # test symmetric
+    transformer: BasePairwiseTransformer = pairwise_transformers_tab()
+    transformation = transformer.transform(X1)
+    for i in range(len(X1)):
+        row = (transformation[i, :]).T
+        column = transformation[:, i]
+        assert np.array_equal(row, column)
+    assert transformer.symmetric, (
+        f"Symmetric is set to wrong value for {pairwise_transformers_tab} "
+        f"when only X1"
+    )
