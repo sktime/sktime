a99d5bec5989142686c549bf4a369881436bd250
Ryan Kuhns <rnkuhns@gmail.com>
2021-07-04 14:51:09 -0400

    Updateed Differencer unit tests

--- sktime/transformations/series/tests/test_differencer.py
+++ sktime/transformations/series/tests/test_differencer.py
@@ -6,40 +6,57 @@ __author__ = ["Ryan Kuhns"]
 __all__ = []
 
 import numpy as np
+import pandas as pd
 import pytest
 
 from sktime.datasets import load_airline
 from sktime.transformations.series.difference import Differencer
 
 y_airline = load_airline()
+y_airline_df = pd.concat([y_airline, y_airline], axis=1)
+y_airline_df.columns = ["Passengers 1", "Passengers 2"]
+
+test_cases = [y_airline, y_airline_df]
 lags_to_test = [1, 12, (3), [5], np.array([7]), (8, 3), [1, 12], np.array([5, 7, 1])]
 
 
-@pytest.mark.parametrize("y", [y_airline])
+@pytest.mark.parametrize("y", test_cases)
 @pytest.mark.parametrize("lags", lags_to_test)
 def test_differencer_same_series(y, lags):
     transformer = Differencer(lags=lags)
     y_transform = transformer.fit_transform(y)
     y_reconstructed = transformer.inverse_transform(y_transform)
 
+    # Reconstruction should return the reconstructed series for same indices
+    # that are in the `Z` timeseries passed to inverse_transform
+    np.testing.assert_array_almost_equal(y.loc[y_reconstructed.index], y_reconstructed)
+
+
+@pytest.mark.parametrize("y", test_cases)
+@pytest.mark.parametrize("lags", lags_to_test)
+def test_differencer_remove_missing_false(y, lags):
+    transformer = Differencer(lags=lags, remove_missing=False)
+    y_transform = transformer.fit_transform(y)
+    y_reconstructed = transformer.inverse_transform(y_transform)
+
     np.testing.assert_array_almost_equal(y, y_reconstructed)
 
 
-@pytest.mark.parametrize("y", [y_airline])
+@pytest.mark.parametrize("y", test_cases)
 @pytest.mark.parametrize("lags", lags_to_test)
 def test_differencer_prediction(y, lags):
     y_train = y.iloc[:-12].copy()
     y_true = y.iloc[-12:].copy()
 
-    transformer = Differencer(lags=[1, 12], use_with_predict=True)
+    transformer = Differencer(lags=[1, 12])
     y_transform = transformer.fit_transform(y)
 
     # Use the actual transformed values as predictions since we know we should
     # be able to convert them to the units of the original series and exactly
-    # match the y_true values for this period (they should have zero forecast error)
+    # match the y_true values for this period
     y_pred = y_transform.iloc[-12:].copy()
 
-    # Redo the transformer's fit (its in transform so we use fit_transform)
+    # Redo the transformer's fit and transformation
     # Now the transformer doesn't know anything about the values in y_true
     # This simulates use-case with a forecasting pipeline
     y_transform = transformer.fit_transform(y_train)
