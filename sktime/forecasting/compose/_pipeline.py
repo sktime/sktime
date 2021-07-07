97f817e4e0373547197ac87f475853596d60ba0a
Franz Király <f.kiraly@ucl.ac.uk>
2021-07-07 13:50:21 +0100

    and removed _has_tag again

--- sktime/forecasting/compose/_pipeline.py
+++ sktime/forecasting/compose/_pipeline.py
@@ -11,7 +11,6 @@ from sktime.base import _HeterogenousMetaEstimator
 from sktime.forecasting.base._base import BaseForecaster
 from sktime.forecasting.base._base import DEFAULT_ALPHA
 from sktime.transformations.base import _SeriesToSeriesTransformer
-from sktime.utils import _has_tag
 from sktime.utils.validation.series import check_series
 
 
