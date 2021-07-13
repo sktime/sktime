952a9d3f3477f3e61fc6f2be2ea74a24d0a4deab
Franz Király <f.kiraly@ucl.ac.uk>
2021-07-13 21:34:19 +0100

    BaseMetric inherits from BaseObject now

--- sktime/performance_metrics/base/_base.py
+++ sktime/performance_metrics/base/_base.py
@@ -5,11 +5,10 @@
 __author__ = ["Ryan Kuhns"]
 __all__ = ["BaseMetric"]
 
-import inspect
-from sklearn.base import BaseEstimator
+from sklearn.base import BaseObject
 
 
-class BaseMetric(BaseEstimator):
+class BaseMetric(BaseObject):
     """Base class for defining metrics in sktime.
 
     Extends scikit-learn's BaseEstimator.
@@ -22,27 +21,3 @@ class BaseMetric(BaseEstimator):
     def __call__(self, y_true, y_pred, **kwargs):
         """Calculate metric value using underlying metric function."""
         NotImplementedError("abstract method")
-
-    # This is copied from sktime.base.BaseEstimator. Choice to copy was made to
-    # Avoid the not applicable functionality from BaseEstimator that tripped
-    # up unit tests (e.g. is_fitted, check_is_fitted).
-    @classmethod
-    def _all_tags(cls):
-        """Get tags from estimator class and all its parent classes."""
-        # We here create a separate estimator tag interface in addition to the one in
-        # scikit-learn to make sure we do not interfere with scikit-learn's one
-        # when we inherit from scikit-learn classes. We also make estimator tags a
-        # class rather than object attribute.
-        collected_tags = dict()
-
-        # We exclude the last two parent classes; sklearn.base.BaseEstimator and
-        # the basic Python object.
-        for parent_class in reversed(inspect.getmro(cls)[:-2]):
-            if hasattr(parent_class, "_tags"):
-                # Need the if here because mixins might not have _more_tags
-                # but might do redundant work in estimators
-                # (i.e. calling more tags on BaseEstimator multiple times)
-                more_tags = parent_class._tags
-                collected_tags.update(more_tags)
-
-        return collected_tags
