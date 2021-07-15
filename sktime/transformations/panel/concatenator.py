c64eae751426610c9fb050a30609b0d3c96acc9c
Viktor Kazakov <viktorkazakov@outlook.com>
2021-07-15 17:18:42 +0100

    bug fix

---
+++ sktime/transformations/panel/concatenator.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from sktime.transformations.base import _PanelToTabularTransformer
+import pandas as pd
+
+
+__author__ = ["Viktor Kazakov"]
+__all__ = ["Concatenator"]
+
+
+class Concatenator(_PanelToTabularTransformer):
+    """
+    Concatenates
+    """
+
+    def __init__(self):
+        super(Concatenator, self).__init__()
+
+    def fit_transform(self, X, y=None):
+        """
+        Parameters
+        ----------
+        X : list of pandas dataframes
+        """
+        if type(X) != list:
+            # Only for passing the sktime checks. `X` must be a list.
+            X = [X]
+        return pd.concat(X, axis=1)
+
+    def transform(self, X, y=None):
+        """
+        Parameters
+        ----------
+        X : list of pandas dataframes
+        """
+        return self.fit_transform(X=X, y=y)
