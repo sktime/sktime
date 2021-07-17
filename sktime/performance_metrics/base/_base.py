c99da7a3adcd43908f6a412583eef483ad06521b
Ryan Kuhns <rnkuhns@gmail.com>
2021-07-17 17:02:54 -0400

    Fix bug in BaseMetric __call__

--- sktime/performance_metrics/base/_base.py
+++ sktime/performance_metrics/base/_base.py
@@ -11,13 +11,19 @@ from sktime.base import BaseObject
 class BaseMetric(BaseObject):
     """Base class for defining metrics in sktime.
 
-    Extends sktime BaseObject.
+    Parameters
+    ----------
+    func : function
+        Function that implements the aggregate metric.
+    name : str, default=None
+        Optional name to pass to metric's name attribute. If None, then the
+        function's name is used.
     """
 
-    def __init__(self, func, name=None):
+    def __init__(self, func, sample_func=None, name=None):
         self._func = func
         self.name = name if name is not None else func.__name__
 
     def __call__(self, y_true, y_pred, **kwargs):
         """Calculate metric value using underlying metric function."""
-        NotImplementedError("abstract method")
+        raise NotImplementedError("abstract method")
