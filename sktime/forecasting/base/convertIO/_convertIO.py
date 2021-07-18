1b187c25248b6772e61034802afe4632895a47d4
Franz Király <f.kiraly@ucl.ac.uk>
2021-07-18 21:17:44 +0100

    removed unnecessary line

--- sktime/forecasting/base/convertIO/_convertIO.py
+++ sktime/forecasting/base/convertIO/_convertIO.py
@@ -274,9 +274,7 @@ def convert(what, from_type: str, to_type: str, as_scitype: str, store=None):
 
     key = (from_type, to_type, as_scitype)
 
-    ckys = list(convert_dict.keys())
-
-    if key not in ckys:
+    if key not in convert_dict.keys():
         raise TypeError(
             "no conversion defined from type " + str(from_type) + " to " + str(to_type)
         )
