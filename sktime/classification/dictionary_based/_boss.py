af4e6b0910add16e322dc2383e474f8349249063
ltoniazzi <61414566+ltoniazzi@users.noreply.github.com>
2021-07-19 08:24:00 +1200

    add examples in docstrings in classification module (#1164)

--- sktime/classification/dictionary_based/_boss.py
+++ sktime/classification/dictionary_based/_boss.py
@@ -78,6 +78,17 @@ class BOSSEnsemble(BaseClassifier):
     For the Java version, see
     https://github.com/uea-machine-learning/tsml/blob/master/src/main/java/tsml/
     classifiers/dictionary_based/BOSS.java
+
+    Example
+    -------
+    >>> from sktime.classification.dictionary_based import BOSSEnsemble
+    >>> from sktime.datasets import load_italy_power_demand
+    >>> X_train, y_train = load_italy_power_demand(split="train", return_X_y=True)
+    >>> X_test, y_test = load_italy_power_demand(split="test", return_X_y=True)
+    >>> clf = BOSSEnsemble()
+    >>> clf.fit(X_train, y_train)
+    BOSSEnsemble(...)
+    >>> y_pred = clf.predict(X_test)
     """
 
     # Capability tags
