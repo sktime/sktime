5bab347c1ed57dbb8f847743ca51ab6442176ab0
Franz Király <f.kiraly@ucl.ac.uk>
2021-07-22 08:32:31 +0100

    Forecasting support for multivariate y and multiple input/output types - working prototype (#980)
    This PR introduces multiple input/output type support for `X` and `y` in forecasters, including generic support for multivariate `y`, i.e., `pd.DataFrame`, and the possibility to pass `np.ndarray` and `pd.Series` to either argument. This is loosely based on the design in [STEP no.5](https://github.com/sktime/enhancement-proposals/tree/main/steps/05_scitype_based_IO_checks) and subsequent discussions with @mloning around #1074.
    
    The key ingredients are:
    
    * converters parameterized by from, to, and as - in the `convertIO`module. Besides the obvious conversion functionality, the converters can be given access to a dictionary via reference in the `store` argument, where information for inverting lossy conversions (like from `pd.DataFrame` to `np.array`) can be stored
    * a new tay `y:scitype` which can be `"univariate"`, `"multivariate"`, or `"both"`, indicating what type of `y` are supported (multivariate here means 2 dims or more)
    * new tags which encode the type of `y` and `X` that the private `_fit`, `_predict`, and `_update` assume internally - for now, it's just one type and not a list of compatible types
    * some logic in the public "plumbing" area of `fit`, `predict`, `update`, which converts inputs to the public layer to the desired input of the logic layer and back
    * expanding tests and checks that ensure that errors are raised when the wrong types are passed, and changes that ensure the new allowe inputs such as `pd.DataFrame` are allowed rather than blocked by the checks

---
+++ sktime/forecasting/base/convertIO/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+"""Module exports: series type converters."""
+__all__ = ["convert", "convert_to", "mtype"]
+
+from sktime.forecasting.base.convertIO._convertIO import convert, convert_to, mtype
