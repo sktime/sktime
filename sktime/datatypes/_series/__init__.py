2ec97f1e9a0e5a4502868433e64118d8302a0376
Franz Király <f.kiraly@ucl.ac.uk>
2021-07-24 18:38:19 +0100

    example/fixture retrieval by mtype/scitype

--- sktime/datatypes/_series/__init__.py
+++ sktime/datatypes/_series/__init__.py
@@ -4,6 +4,9 @@
 from sktime.datatypes._series._convert import convert_dict as convert_dict_Series
 from sktime.datatypes._series._mtypes import infer_mtype_dict as infer_mtype_dict_Series
 
+from sktime.datatypes._series._examples import example_dict as example_dict_Series
+from sktime.datatypes._series._examples import example_dict_lossy as example_dict_lossy_Series
+
 from sktime.datatypes._series._registry import MTYPE_LIST_SERIES, MTYPE_REGISTER_SERIES
 
 __all__ = [
@@ -11,4 +14,6 @@ __all__ = [
     "infer_mtype_dict_Series",
     "MTYPE_LIST_SERIES",
     "MTYPE_REGISTER_SERIES",
+    "example_dict_Series",
+    "example_dict_lossy_Series",
 ]
