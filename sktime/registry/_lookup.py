97ef5b6f960e7b018fc6218df5ef4d2bc0bfd249
Franz Király <f.kiraly@ucl.ac.uk>
2021-07-24 14:06:02 +0100

    fix indentation bug

--- sktime/registry/_lookup.py
+++ sktime/registry/_lookup.py
@@ -232,7 +232,7 @@ def _check_tag_cond(estimator, filter_tags=None, as_dataframe=True):
     for (key, value) in filter_tags.items():
         if not isinstance(value, list):
             value = [value]
-    cond_sat = cond_sat and estimator.get_class_tag(key) in set(value)
+        cond_sat = cond_sat and estimator.get_class_tag(key) in set(value)
 
     return cond_sat
 
