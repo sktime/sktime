885c760f68216db9c86ee930985431cc9ee48872
Franz Király <f.kiraly@ucl.ac.uk>
2021-07-24 13:26:20 +0100

    export tag validity checker

--- sktime/registry/__init__.py
+++ sktime/registry/__init__.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 """Sktime registry module exports."""
 
-from sktime.registry._tags import ESTIMATOR_TAG_REGISTER, ESTIMATOR_TAG_LIST
+from sktime.registry._tags import (
+    ESTIMATOR_TAG_REGISTER,
+    ESTIMATOR_TAG_LIST,
+    check_tag_is_valid,
+)
+
 from sktime.registry._base_classes import (
     BASE_CLASS_REGISTER,
     BASE_CLASS_LIST,
@@ -19,6 +24,7 @@ from sktime.registry._lookup import all_estimators, all_tags
 __all__ = [
     "all_estimators",
     "all_tags",
+    "check_tag_is_valid",
     "ESTIMATOR_TAG_LIST",
     "ESTIMATOR_TAG_REGISTER",
     "BASE_CLASS_REGISTER",
