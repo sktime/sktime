76952ca1fe3d64dedbd0e084a41f1f20601aa7b5
tensorflow-as-tf <ajancso1@gmail.com>
2021-07-28 22:06:17 +0200

    draft for KalmanFilter

---
+++ sktime/transformations/series/kalman.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3 -u
+# -*- coding: utf-8 -*-
+# copyright: sktime developers, BSD-3-Clause License (see LICENSE file)
+
+__author__ = ["Anthony Jancso"]
+__all__ = ["KalmanFilter"]
+
+# import numpy as np
+
+
+class KalmanFilter:
+    def __init__(self) -> None:
+        pass
+
+    def predict(self) -> None:
+        pass
+
+    def update(self) -> None:
+        pass
