843cc2c8da36f4f686ba4d631645ccaa805264f1
Tony Bagnall <ajb@uea.ac.uk>
2021-08-01 10:56:11 +0100

    Update test_experiments.py

--- sktime/benchmarking/tests/test_experiments.py
+++ sktime/benchmarking/tests/test_experiments.py
@@ -18,8 +18,8 @@ def test_run_clustering_experiment():
     Currently it just checks the files have been created, then deletes them.
     """
     dataset = "UnitTest"
-    train_X, train_Y = load_UCR_UEA_dataset(dataset, "TRAIN",return_X_y=True)
-    test_X, test_Y = load_UCR_UEA_dataset(dataset, "TEST",return_X_y=True)
+    train_X, train_Y = load_UCR_UEA_dataset(dataset, "TRAIN", return_X_y=True)
+    test_X, test_Y = load_UCR_UEA_dataset(dataset, "TEST", return_X_y=True)
     run_clustering_experiment(
         train_X,
         TimeSeriesKMeans(n_clusters=2),
@@ -36,7 +36,7 @@ def test_run_clustering_experiment():
     os.remove("../Temp/kmeans2/Predictions/UnitTest/testResample0.csv")
     os.remove("../Temp/kmeans2/Predictions/UnitTest/trainResample0.csv")
 
-    
+
 # def test_load_and_run_clustering_experiment():
 #     """Test loading, running and saving.
 #
