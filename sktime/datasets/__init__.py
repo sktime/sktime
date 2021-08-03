5291773feb7f3fb35ca687294eb7c89a26438f97
Tony Bagnall <ajb@uea.ac.uk>
2021-08-03 14:22:52 +0100

    correct notebook, make data_io private

--- sktime/datasets/__init__.py
+++ sktime/datasets/__init__.py
@@ -18,17 +18,17 @@ __all__ = [
     "load_PBS_dataset",
 ]
 
-from sktime.datasets.data_io import load_airline
-from sktime.datasets.data_io import load_gunpoint
-from sktime.datasets.data_io import load_arrow_head
-from sktime.datasets.data_io import load_basic_motions
-from sktime.datasets.data_io import load_osuleaf
-from sktime.datasets.data_io import load_italy_power_demand
-from sktime.datasets.data_io import load_japanese_vowels
-from sktime.datasets.data_io import load_longley
-from sktime.datasets.data_io import load_lynx
-from sktime.datasets.data_io import load_shampoo_sales
-from sktime.datasets.data_io import load_UCR_UEA_dataset
-from sktime.datasets.data_io import load_unit_test
-from sktime.datasets.data_io import load_uschange
-from sktime.datasets.data_io import load_PBS_dataset
+from sktime.datasets._data_io import load_airline
+from sktime.datasets._data_io import load_gunpoint
+from sktime.datasets._data_io import load_arrow_head
+from sktime.datasets._data_io import load_basic_motions
+from sktime.datasets._data_io import load_osuleaf
+from sktime.datasets._data_io import load_italy_power_demand
+from sktime.datasets._data_io import load_japanese_vowels
+from sktime.datasets._data_io import load_longley
+from sktime.datasets._data_io import load_lynx
+from sktime.datasets._data_io import load_shampoo_sales
+from sktime.datasets._data_io import load_UCR_UEA_dataset
+from sktime.datasets._data_io import load_unit_test
+from sktime.datasets._data_io import load_uschange
+from sktime.datasets._data_io import load_PBS_dataset
