29dbf6652e9b6eb3aece9efd91443ef20fd03f36
Ryan Kuhns <rnkuhns@gmail.com>
2021-08-06 10:10:37 -0400

    Add module docstring

--- sktime/transformations/series/tests/test_differencer.py
+++ sktime/transformations/series/tests/test_differencer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3 -u
 # -*- coding: utf-8 -*-
 # copyright: sktime developers, BSD-3-Clause License (see LICENSE file)
+"""Unit tests of Differencer functionality."""
 
 __author__ = ["Ryan Kuhns"]
 __all__ = []
