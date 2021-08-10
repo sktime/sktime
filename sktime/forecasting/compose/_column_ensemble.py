083c7dca47b8edff622cb1c15151b3cd4a094e9e
GuzalBulatova <guzalbulatova@gmail.com>
2021-08-10 15:58:27 +0200

    Updated tag and author strings

--- sktime/forecasting/compose/_column_ensemble.py
+++ sktime/forecasting/compose/_column_ensemble.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """copyright: sktime developers, BSD-3-Clause License (see LICENSE file)."""
 
-__author__ = ["Guzal Bulatova", "Markus Löning"]
+__author__ = ["GuzalBulatova", "mloning"]
 __all__ = ["ColumnEnsembleForecaster"]
 
 import numpy as np
@@ -45,7 +45,7 @@ class ColumnEnsembleForecaster(_HeterogenousEnsembleForecaster):
         "scitype:y": "both",
         "univariate-only": False,
         "y_inner_mtype": "pd.DataFrame",
-        "requires-fh-in-fit": False,
+        "requires-fh-in-fit": True,
         "handles-missing-data": False,
     }
 
--- sktime/forecasting/compose/tests/test_column_ensemble.py
+++ sktime/forecasting/compose/tests/test_column_ensemble.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """copyright: sktime developers, BSD-3-Clause License (see LICENSE file)."""
 
-__author__ = ["Guzal Bulatova"]
+__author__ = ["GuzalBulatova"]
 
 import numpy as np
 import pandas as pd
