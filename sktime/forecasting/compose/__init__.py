c53ca44b23d3bca3440331b68135838e606f691d
Viktor Kazakov <viktorkazakov@outlook.com>
2021-08-12 23:30:46 +0100

    docstring

--- sktime/forecasting/compose/__init__.py
+++ sktime/forecasting/compose/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3 -u
 # -*- coding: utf-8 -*-
 # copyright: sktime developers, BSD-3-Clause License (see LICENSE file)
-
+"""Import of modules."""
 __author__ = ["Markus Löning"]
 
 __all__ = [
