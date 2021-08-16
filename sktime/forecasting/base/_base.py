413f4210121b2616ec24c7df822cedc775be89e5
Franz Király <f.kiraly@ucl.ac.uk>
2021-08-16 18:13:10 +0100

    linting

--- sktime/forecasting/base/_base.py
+++ sktime/forecasting/base/_base.py
@@ -43,7 +43,6 @@ from sktime.utils.validation.forecasting import check_X
 from sktime.utils.validation.forecasting import check_alpha
 from sktime.utils.validation.forecasting import check_cv
 from sktime.utils.validation.forecasting import check_fh
-from sktime.utils.validation.forecasting import check_y
 from sktime.utils.validation.forecasting import check_y_X
 from sktime.utils.validation.series import check_series, check_equal_time_index
 
