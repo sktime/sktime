3000e996afba131cb1fd9cc9048cf93987a9555d
Victor Dremov (Виктор Дремов) <37803199+dremovv@users.noreply.github.com>
2021-08-18 18:18:42 +0300

    remove unused imports

--- sktime/classification/kernel_based/_arsenal.py
+++ sktime/classification/kernel_based/_arsenal.py
@@ -16,8 +16,6 @@ from sklearn.utils.multiclass import class_distribution
 from sktime.classification.base import BaseClassifier
 from sktime.transformations.panel.rocket import Rocket
 from sktime.utils.validation import check_n_jobs
-from sktime.utils.validation.panel import check_X
-from sktime.utils.validation.panel import check_X_y
 
 
 class Arsenal(BaseClassifier):
