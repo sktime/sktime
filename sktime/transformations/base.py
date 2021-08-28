a09335285ee36915acf9ac6d34cf5573e36d9f8d
Franz Király <f.kiraly@ucl.ac.uk>
2021-08-28 23:01:24 +0100

    started refactoring base class

--- sktime/transformations/base.py
+++ sktime/transformations/base.py
@@ -1,5 +1,39 @@
-#!/usr/bin/env python3 -u
 # -*- coding: utf-8 -*-
+"""
+Base class template for transformers.
+
+    class name: BaseTransformer
+
+Covers all types of transformers.
+Type and behaviour of transformer is determined by the following tags:
+    "scitype:transform-output" tag with values "Primivites", or "Series"
+        this determines type of output of transform
+        if "Primitives", output is pd.DataFrame with as many rows as X has instances
+            i-th instance of X is transformed into i-th row of output
+        if "Series", output is a Series or Panel, with as many instances as X
+            i-th instance of X is transformed into i-th instance of output
+        Series are treated as one-instance-Panels
+            if Series is input, output is a 1-row pd.DataFrame or a Series
+    "scitype:instancewise" tag which is boolean
+        if True, fit/transform is statistically independent by instance
+
+Scitype defining methods:
+    fitting         - fit(self, X, y=None)
+    transform       - transform(self, X, y=None)
+    fit&transform   - fit_transform(self, X, y=None)
+    updating        - update(self, X, y=None)
+
+Inspection methods:
+    hyper-parameter inspection  - get_params()
+    fitted parameter inspection - get_fitted_params()
+
+State:
+    fitted model/strategy   - by convention, any attributes ending in "_"
+    fitted state flag       - is_fitted (property)
+    fitted state inspection - check_is_fitted()
+
+copyright: sktime developers, BSD-3-Clause License (see LICENSE file)
+"""
 
 __author__ = ["Markus Löning"]
 __all__ = [
@@ -34,12 +68,12 @@ Panel = Union[pd.DataFrame, np.ndarray]  # 3d or nested array
 
 
 class BaseTransformer(BaseEstimator):
-    """Transformer base class"""
+    """Transformer base class."""
 
     def __init__(self):
         super(BaseTransformer, self).__init__()
 
-    def fit(self, Z, X=None):
+    def fit(self, X, y=None, Z=None):
         """
         Fit transformer to X and y.
 
@@ -47,23 +81,30 @@ class BaseTransformer(BaseEstimator):
 
         Parameters
         ----------
-        Z : TimeSeries
+        X : TimeSeries
             Data to be transformed
-        X : TimeSeries, optional (default=None)
+        y : TimeSeries, optional (default=None)
             Extra data
 
         Returns
         -------
         self : a fitted instance of the estimator
         """
+        X = _handle_alias(X, Z)
+
         self._is_fitted = True
+
+        self.fit(X=X, y=y)
+
         return self
 
-    def transform(self, Z, X=None):
+    def transform(self, X, y=None, Z=None):
         """Transform data. Returns a transformed version of X."""
-        raise NotImplementedError("abstract method")
+        X = _handle_alias(X, Z)
 
-    def fit_transform(self, Z, X=None):
+        return self._transform(X=X, y=y)
+
+    def fit_transform(self, X, y=None, Z=None):
         """Fit to data, then transform it.
 
         Fits transformer to X and y with optional parameters fit_params
@@ -81,20 +122,64 @@ class BaseTransformer(BaseEstimator):
         Zt : pd.DataFrame, pd.Series or np.ndarray
             Transformed data.
         """
+        X = _handle_alias(X, Z)
         # Non-optimized default implementation; override when a better
         # method is possible for a given algorithm.
-        if X is None:
-            # Fit method of arity 1 (unsupervised transformation)
-            return self.fit(Z).transform(Z)
-        else:
-            # Fit method of arity 2 (supervised transformation)
-            return self.fit(Z, X).transform(Z)
-
+        return self.fit(X, y, Z).transform(X, y, Z)
+#
     # def inverse_transform(self, Z, X=None):
     #     raise NotImplementedError("abstract method")
     #
     # def update(self, Z, X=None, update_params=False):
     #     raise NotImplementedError("abstract method")
+
+    def _fit(self, X, y=None):
+        """
+        Fit transformer to X and y.
+
+        core logic
+
+        Parameters
+        ----------
+        X : TimeSeries
+            Data to be transformed
+        y : TimeSeries, optional (default=None)
+            Extra data
+
+        Returns
+        -------
+        self : a fitted instance of the estimator
+        """
+        # default fit is "no fitting happens"
+        return self
+
+    def _transform(self, X, y=None):
+        """Transform data. Returns a transformed version of X."""
+        raise NotImplementedError("abstract method")
+
+
+def _handle_alias(X, Z):
+    """Handle Z as an alias for X, return X/Z.
+
+    Parameters
+    ----------
+    X: any object
+    Z: any object
+
+    Returns
+    -------
+    X if Z is None, Z if X is None
+
+    Raises
+    ------
+    ValueError both X and Z are not None
+    """
+    if Z is None:
+        return X
+    elif X is None:
+        return Z
+    else:
+        raise ValueError("X and Z are aliases, at most one of them should be passed")
 
 
 class _SeriesToPrimitivesTransformer(BaseTransformer):
