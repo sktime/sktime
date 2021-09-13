dfccfd234a0356be863bf223e41936e751d666a2
chrisholder <chrisholder987@hotmail.com>
2021-09-13 05:32:23 +0100

    Added enum type for PanelMtype

--- sktime/datatypes/_panel/_registry.py
+++ sktime/datatypes/_panel/_registry.py
@@ -1,37 +1,46 @@
 # -*- coding: utf-8 -*-
 
+__all__ = ["MTYPE_REGISTER_PANEL", "MTYPE_LIST_PANEL", "PanelMtype"]
+
 import pandas as pd
 
-__all__ = [
-    "MTYPE_REGISTER_PANEL",
-    "MTYPE_LIST_PANEL",
-]
+from sktime.base._registry_enum import BaseRegistryEnum
 
 
-MTYPE_REGISTER_PANEL = [
-    (
+class PanelMtype(BaseRegistryEnum):
+    NESTED_UNIV = (
         "nested_univ",
-        "Panel",
         "pd.DataFrame with one column per variable, pd.Series in cells",
-    ),
-    (
+        "Panel",
+    )
+    NUMPY3D = (
         "numpy3D",
-        "Panel",
         "3D np.array of format (n_instances, n_columns, n_timepoints)",
-    ),
-    (
+        "Panel",
+    )
+    NUMPYFLAT = (
         "numpyflat",
-        "Panel",
         "2D np.array of format (n_instances, n_columns*n_timepoints)",
-    ),
-    ("pd-multiindex", "Panel", "pd.DataFrame with multi-index (instances, timepoints)"),
-    ("pd-wide", "Panel", "pd.DataFrame in wide format, cols = (instance*timepoints)"),
-    (
-        "pd-long",
         "Panel",
+    )
+    PD_MULTIINDEX = (
+        "pd-multiindex",
+        "pd.DataFrame with multi-index (instances, timepoints)",
+        "Panel",
+    )
+    PD_WIDE = (
+        "pd-wide",
+        "pd.DataFrame in wide format, cols = (instance*timepoints)",
+        "Panel",
+    )
+    PD_LONG = (
+        "pd-long",
         "pd.DataFrame in long format, cols = (index, time_index, column)",
-    ),
-    ("df-list", "Panel", "list of pd.DataFrame"),
-]
+        "Panel",
+    )
+    DF_LIST = ("df-list", "list of pd.DataFrame", "Panel")
+
+
+MTYPE_REGISTER_PANEL = [tuple(mtype) for mtype in PanelMtype]
 
 MTYPE_LIST_PANEL = pd.DataFrame(MTYPE_REGISTER_PANEL)[0].values
