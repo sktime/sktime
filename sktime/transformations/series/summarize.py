6dc18d67d2fae6e83dffe42e3c85736418db95e5
Ryan Kuhns <rnkuhns@gmail.com>
2021-09-18 10:50:02 -0400

    Change quantile default and add unit tests

--- sktime/transformations/series/summarize.py
+++ sktime/transformations/series/summarize.py
@@ -1,127 +1,117 @@
 #!/usr/bin/env python3 -u
 # -*- coding: utf-8 -*-
 # copyright: sktime developers, BSD-3-Clause License (see LICENSE file)
-"""Implements transformers for summarizing a time series."""
+"""Implement transformers for summarizing a time series."""
 
 __author__ = ["mloning", "RNKuhns"]
 __all__ = ["SummaryTransformer", "MeanTransformer"]
 
+import numpy as np
 import pandas as pd
 
 from sktime.transformations.base import _SeriesToPrimitivesTransformer
 from sktime.utils._maint import deprecated
 from sktime.utils.validation.series import check_series
 
-
-ALLOWED_SUM_FUNCS = (
+ALLOWED_SUM_FUNCS = [
     "mean",
     "min",
     "max",
     "median",
     "sum",
     "skew",
-    "kurtosis",
+    "kurt",
     "var",
     "std",
+    "mad",
     "sem",
     "nunique",
     "count",
-)
+]
 
 
-def _series_summary(series_or_df, summary_function="mean", axis=0):
-    """Calculate summary value of a time series.
-
-    For multivariate data the summary is applied to each column.
+def _check_summary_function(summary_function):
+    """Validate summary_function.
 
     Parameters
     ----------
-    series_or_df : pd.Series or pd.DataFrame
-        The series to summarize.
-    summary_function : str
-        One of pandas summary functions ("mean", "min", "max", "sum",
-        "skew", "kurtosis", "var", "std", "sem", "nunique", "count") that is used
-        to summarize each column of the dataset.
-    axis : int, default=0
-        Axis to apply the summary function along. Only used for multivariate data.
-
-    Raises
-    ------
-    ValueError :
-        `series_or_df` must be pd.Series or pd>DataFrame.
-    ValueError :
-        `summary_function must be one of ("mean", "min", "max", "median", "sum",
-        "skew", "kurtosis", "var", "std", "sem", "nunique", "count").
+    summary_function : str, list or tuple
+        Either a string or list/tuple of strings indicating the pandas summary
+        functions ("mean", "min", "max", "median", "sum", "skew", "kurtosis",
+        "var", "std", "mad", "sem", "nunique", "count") that is used to summarize
+        each column of the dataset.
 
     Returns
     -------
-    summary_value : scalar or pd.Series
-        If `series_or_df` is univariate then a scalar is returned. Otherwise,
-        a pd.Series is returned.
+    summary_function : list or tuple
+        The summary functions that will be used to summarize the dataset.
     """
-    if isinstance(series_or_df, pd.Series):
-        is_series = True
-    elif isinstance(series_or_df, pd.DataFrame):
-        is_series = False
+    msg = f"""`summary_function` must be str or a list or tuple made up of
+          {ALLOWED_SUM_FUNCS}.
+          """
+    if isinstance(summary_function, str):
+        if summary_function not in ALLOWED_SUM_FUNCS:
+            raise ValueError(msg)
+        summary_function = [summary_function]
+    elif isinstance(summary_function, (list, tuple)):
+        if not all([func in ALLOWED_SUM_FUNCS for func in summary_function]):
+            raise ValueError(msg)
     else:
-        raise ValueError("`series_or_df` must be pd.Series or pd>DataFrame.")
+        raise ValueError(msg)
+    return summary_function
 
-    if summary_function == "mean":
-        summary_value = series_or_df.mean(axis=axis)
-    elif summary_function == "min":
-        summary_value = series_or_df.min(axis=axis)
-    elif summary_function == "max":
-        summary_value = series_or_df.max(axis=axis)
-    elif summary_function == "median":
-        summary_value = series_or_df.median(axis=axis)
-    elif summary_function == "sum":
-        summary_value = series_or_df.sum(axis=axis)
-    elif summary_function == "skew":
-        summary_value = series_or_df.skew(axis=axis)
-    elif summary_function == "kurtosis":
-        summary_value = series_or_df.kurtosis(axis=axis)
-    elif summary_function == "std":
-        summary_value = series_or_df.std(axis=axis)
-    elif summary_function == "var":
-        summary_value = series_or_df.var(axis=axis)
-    elif summary_function == "mad":
-        summary_value = series_or_df.mad(axis=axis)
-    elif summary_function == "sem":
-        summary_value = series_or_df.sem(axis=axis)
-    elif summary_function == "nunique":
-        if is_series:
-            summary_value = series_or_df.nunique()
-        else:
-            summary_value = series_or_df.nunique(axis=axis)
-    elif summary_function == "count":
-        if is_series:
-            summary_value = series_or_df.count()
-        else:
-            summary_value = series_or_df.count(axis=axis)
-    else:
-        raise ValueError(f"`summary_function must be one of {ALLOWED_SUM_FUNCS}.")
 
-    # For univariate data in DataFrame return scalar like when input is pd.Series
-    if is_series is False and series_or_df.shape[1] == 1:
-        summary_value = summary_value.iloc[0]
+def _check_quantiles(quantiles):
+    """Validate quantiles.
 
-    return summary_value
+    Parameters
+    ----------
+    quantiles : str, list, tuple or None
+        Either a string or list/tuple of strings indicating the pandas summary
+        functions ("mean", "min", "max", "median", "sum", "skew", "kurtosis",
+        "var", "std", "mad", "sem", "nunique", "count") that is used to summarize
+        each column of the dataset.
+
+    Returns
+    -------
+    quantiles : list or tuple
+        The validated quantiles that will be used to summarize the dataset.
+    """
+    msg = """`quantiles` must be int, float or a list or tuple made up of
+          int and float values that are between 0 and 1.
+          """
+    if isinstance(quantiles, (int, float)):
+        if not 0.0 <= quantiles <= 1.0:
+            raise ValueError(msg)
+        quantiles = [quantiles]
+    elif isinstance(quantiles, (list, tuple)):
+        if len(quantiles) == 0 or not all(
+            [isinstance(q, (int, float)) and 0.0 <= q <= 1.0 for q in quantiles]
+        ):
+            raise ValueError(msg)
+    elif quantiles is not None:
+        raise ValueError(msg)
+    return quantiles
 
 
 class SummaryTransformer(_SeriesToPrimitivesTransformer):
     """Calculate summary value of a time series.
 
-    Calculates scalar value when applied to a :term:`univariate time series`.
-    If input is :term:`multivariate time series` then Scalar values for each
-    column and returned as elements of a pd.Series.
+    For :term:`univariate time series` a combination of summary functions and
+    quantiles of the input series are calculated. If the input is a
+    :term:`multivariate time series` then the summary functions and quantiles
+    are calculated separately for each column.
 
     Parameters
     ----------
-    summary_function : str, default="mean"
-        One of pandas Series and DataFrame summary functions
-        ("mean", "min", "max", "median", "sum", "skew", "kurtosis", "var",
-        "std", "sem", "nunique", "count") that is used to summarize each column
-        of the dataset.
+    summary_function : str, list, tuple, default=("mean", "std", "min", "max")
+        Either a string, or list or tuple of strings indicating the pandas
+        summary functions that are used to summarize each column of the dataset.
+        Must be one of ("mean", "min", "max", "median", "sum", "skew", "kurt",
+        "var", "std", "mad", "sem", "nunique", "count").
+    quantiles : str, list, tuple or None, default=(0.1, 0.25, 0.5, 0.75, 0.9)
+        Optional list of series quantiles to calculate. If None, no quantiles
+        are calculated.
 
     See Also
     --------
@@ -130,14 +120,15 @@ class SummaryTransformer(_SeriesToPrimitivesTransformer):
 
     Notes
     -----
-    This provides a wrapper around pandas DataFrame and Series summary methods.
+    This provides a wrapper around pandas DataFrame and Series agg and
+    quantile methods.
 
     Examples
     --------
     >>> from sktime.transformations.series.summarize import SummaryTransformer
     >>> from sktime.datasets import load_airline
     >>> y = load_airline()
-    >>> transformer = SummaryTransformer(summary_function="median")
+    >>> transformer = SummaryTransformer()
     >>> y_mean = transformer.fit_transform(y)
     """
 
@@ -145,8 +136,13 @@ class SummaryTransformer(_SeriesToPrimitivesTransformer):
         "fit-in-transform": True,
     }
 
-    def __init__(self, summary_function="mean"):
+    def __init__(
+        self,
+        summary_function=("mean", "std", "min", "max"),
+        quantiles=(0.1, 0.25, 0.5, 0.75, 0.9),
+    ):
         self.summary_function = summary_function
+        self.quantiles = quantiles
         super(SummaryTransformer, self).__init__()
 
     def _transform(self, Z, X=None):
@@ -163,8 +159,23 @@ class SummaryTransformer(_SeriesToPrimitivesTransformer):
             If `series_or_df` is univariate then a scalar is returned. Otherwise,
             a pd.Series is returned.
         """
-        summary_value = _series_summary(Z, self.summary_function)
-        return summary_value
+        if self.summary_function is None and self.quantiles is None:
+            raise ValueError(
+                "One of `summary_function` and `quantiles` must not be None."
+            )
+        summary_function = _check_summary_function(self.summary_function)
+        quantiles = _check_quantiles(self.quantiles)
+
+        summary_value = Z.agg(summary_function)
+        if quantiles is not None:
+            quantile_value = Z.quantile(quantiles)
+            summary_value = pd.concat([summary_value, quantile_value])
+
+        if isinstance(Z, pd.Series):
+            summary_value.name = Z.name
+            summary_value = pd.DataFrame(summary_value)
+
+        return summary_value.T
 
     def transform(self, Z, X=None):
         """Transform series.
@@ -176,9 +187,9 @@ class SummaryTransformer(_SeriesToPrimitivesTransformer):
 
         Returns
         -------
-        summary_value : scalar or pd.Series
-            If `series_or_df` is univariate then a scalar is returned. Otherwise,
-            a pd.Series is returned.
+        summary_value : pd.DataFrame
+            DataFrame where series are instances (rows) and calculated summary
+            values are treated as features (columns).
         """
         self.check_is_fitted()
         Z = check_series(Z)
@@ -186,21 +197,13 @@ class SummaryTransformer(_SeriesToPrimitivesTransformer):
         return summary_value
 
 
-class MeanTransformer(SummaryTransformer):
+class MeanTransformer(_SeriesToPrimitivesTransformer):
     """Calculate mean value of a time series.
-
-    Calculates scalar value when applied to a :term:`univariate time series`.
-    If input is :term:`multivariate time series` then Scalar values for each
-    column and returned as elements of a pd.Series.
 
     See Also
     --------
     SummaryTransformer :
-        Calculate summary value of a time series.
-
-    Notes
-    -----
-    This provides a wrapper around pandas DataFrame and Series summary methods.
+        Calculate summary values of a time series.
 
     Examples
     --------
@@ -217,5 +220,15 @@ class MeanTransformer(SummaryTransformer):
         Will be removed in release 0.9.
         """
     )
-    def __init__(self):
-        super().__init__(summary_function="mean")
+    def transform(self, Z, X=None):
+        """Transform series.
+        Parameters
+        ----------
+        Z : pd.Series
+        Returns
+        -------
+        float/int
+        """
+        self.check_is_fitted()
+        Z = check_series(Z)
+        return np.mean(Z, axis=0)
