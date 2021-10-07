885a76a861f69b0be29863f3fec4bf4b1266c9ca
Ryan Kuhns <rnkuhns@gmail.com>
2021-10-06 20:35:48 -0400

    Reorder more imports

--- sktime/performance_metrics/forecasting/_functions.py
+++ sktime/performance_metrics/forecasting/_functions.py
@@ -10,12 +10,13 @@ the lower the better.
 
 import numpy as np
 from scipy.stats import gmean
-from sklearn.utils.stats import _weighted_percentile
-from sklearn.utils.validation import check_consistent_length
-from sklearn.metrics._regression import _check_reg_targets
 from sklearn.metrics import mean_absolute_error as _mean_absolute_error
 from sklearn.metrics import mean_squared_error as _mean_squared_error
 from sklearn.metrics import median_absolute_error as _median_absolute_error
+from sklearn.utils.stats import _weighted_percentile
+from sklearn.utils.validation import check_consistent_length
+from sklearn.metrics._regression import _check_reg_targets
+
 from sktime.utils.stats import _weighted_geometric_mean
 from sktime.utils.validation.series import check_series
 
