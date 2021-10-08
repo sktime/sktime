f816f1f8a1332563e240661f30bef59aabb388a0
Franz Király <f.kiraly@ucl.ac.uk>
2021-10-09 00:09:29 +0100

    removed duplicate utility functions from test_all_estimators

--- sktime/tests/test_all_estimators.py
+++ sktime/tests/test_all_estimators.py
@@ -16,27 +16,20 @@ from inspect import signature
 
 import joblib
 import numpy as np
-import pandas as pd
 import pytest
-from pandas.testing import assert_frame_equal
 from sklearn import clone
 from sklearn.utils._testing import set_random_state
 from sklearn.utils.estimator_checks import (
+    _assert_array_almost_equal,
+    _assert_array_equal,
+    _get_args,
+    _make_args,
     check_get_params_invariance as _check_get_params_invariance,
 )
 from sklearn.utils.estimator_checks import check_set_params as _check_set_params
-from sklearn.utils.validation import check_random_state
-
-from sktime.annotation.base import BaseSeriesAnnotator
 from sktime.base import BaseEstimator
-from sktime.classification.base import BaseClassifier
-from sktime.clustering.base.base import BaseClusterer
-from sktime.datatypes._panel._check import is_nested_dataframe
-from sktime.dists_kernels import BasePairwiseTransformer, BasePairwiseTransformerPanel
 from sktime.exceptions import NotFittedError
-from sktime.forecasting.base import BaseForecaster
 from sktime.registry import all_estimators
-from sktime.regression.base import BaseRegressor
 from sktime.tests._config import (
     EXCLUDE_ESTIMATORS,
     EXCLUDED_TESTS,
@@ -46,21 +39,7 @@ from sktime.tests._config import (
     VALID_ESTIMATOR_TYPES,
     VALID_TRANSFORMER_TYPES,
 )
-from sktime.transformations.base import (
-    _PanelToPanelTransformer,
-    _PanelToTabularTransformer,
-    _SeriesToPrimitivesTransformer,
-    _SeriesToSeriesTransformer,
-)
-from sktime.utils._testing.annotation import make_annotation_problem
 from sktime.utils._testing.deep_equals import deep_equals
-from sktime.utils._testing.forecasting import _make_series, make_forecasting_problem
-from sktime.utils._testing.panel import (
-    _make_panel_X,
-    make_classification_problem,
-    make_clustering_problem,
-    make_regression_problem,
-)
 
 ALL_ESTIMATORS = all_estimators(
     return_names=False, exclude_estimators=EXCLUDE_ESTIMATORS
@@ -591,222 +570,3 @@ def _get_err_msg(estimator):
         f"Invalid estimator type: {type(estimator)}. Valid estimator types are: "
         f"{VALID_ESTIMATOR_TYPES}"
     )
-
-
-def _construct_instance(Estimator):
-    """Construct Estimator instance if possible."""
-    # return the instance of the class with default parameters
-    return Estimator.create_test_instance()
-
-
-def _make_args(estimator, method, **kwargs):
-    """Generate testing arguments for estimator methods."""
-    if method == "fit":
-        return _make_fit_args(estimator, **kwargs)
-    if method == "update":
-        raise NotImplementedError()
-    elif method in ("predict", "predict_proba", "decision_function"):
-        return _make_predict_args(estimator, **kwargs)
-    elif method == "transform":
-        return _make_transform_args(estimator, **kwargs)
-    elif method == "inverse_transform":
-        return _make_inverse_transform_args(estimator, **kwargs)
-    else:
-        raise ValueError(f"Method: {method} not supported")
-
-
-def _make_fit_args(estimator, **kwargs):
-    if isinstance(estimator, BaseForecaster):
-        # we need to handle the TransformedTargetForecaster separately
-        if isinstance(estimator, _SeriesToSeriesTransformer):
-            y = _make_series(**kwargs)
-        else:
-            y = make_forecasting_problem(**kwargs)
-        fh = 1
-        X = None
-        return y, X, fh
-    elif isinstance(estimator, BaseSeriesAnnotator):
-        X = make_annotation_problem(**kwargs)
-        return (X,)
-    elif isinstance(estimator, BaseClassifier):
-        return make_classification_problem(**kwargs)
-    elif isinstance(estimator, BaseRegressor):
-        return make_regression_problem(**kwargs)
-    elif isinstance(
-        estimator, (_SeriesToPrimitivesTransformer, _SeriesToSeriesTransformer)
-    ):
-        X = _make_series(**kwargs)
-        return (X,)
-    elif isinstance(estimator, (_PanelToTabularTransformer, _PanelToPanelTransformer)):
-        return make_classification_problem(**kwargs)
-    elif isinstance(estimator, BaseClusterer):
-        return (make_clustering_problem(**kwargs),)
-    elif isinstance(estimator, BasePairwiseTransformer):
-        return None, None
-    elif isinstance(estimator, BasePairwiseTransformerPanel):
-        return None, None
-    else:
-        raise ValueError(_get_err_msg(estimator))
-
-
-def _make_predict_args(estimator, **kwargs):
-    if isinstance(estimator, BaseForecaster):
-        fh = 1
-        return (fh,)
-    elif isinstance(estimator, (BaseClassifier, BaseRegressor)):
-        X = _make_panel_X(**kwargs)
-        return (X,)
-    elif isinstance(estimator, BaseSeriesAnnotator):
-        X = make_annotation_problem(n_timepoints=10, **kwargs)
-        return (X,)
-    elif isinstance(estimator, BaseClusterer):
-        X = _make_panel_X(**kwargs)
-        return (X,)
-    else:
-        raise ValueError(_get_err_msg(estimator))
-
-
-def _make_transform_args(estimator, **kwargs):
-    if isinstance(
-        estimator, (_SeriesToPrimitivesTransformer, _SeriesToSeriesTransformer)
-    ):
-        X = _make_series(**kwargs)
-        return (X,)
-    elif isinstance(
-        estimator,
-        (
-            _PanelToTabularTransformer,
-            _PanelToPanelTransformer,
-        ),
-    ):
-        X = _make_panel_X(**kwargs)
-        return (X,)
-    elif isinstance(estimator, BasePairwiseTransformer):
-        d = {"col1": [1, 2], "col2": [3, 4]}
-        return pd.DataFrame(d), pd.DataFrame(d)
-    elif isinstance(estimator, BasePairwiseTransformerPanel):
-        d = pd.DataFrame({"col1": [1, 2], "col2": [3, 4]})
-        X = [d, d]
-        return X, X
-    else:
-        raise ValueError(_get_err_msg(estimator))
-
-
-def _make_inverse_transform_args(estimator, **kwargs):
-    if isinstance(estimator, _SeriesToPrimitivesTransformer):
-        X = _make_primitives(**kwargs)
-        return (X,)
-    elif isinstance(estimator, _SeriesToSeriesTransformer):
-        X = _make_series(**kwargs)
-        return (X,)
-    elif isinstance(estimator, _PanelToTabularTransformer):
-        X = _make_tabular_X(**kwargs)
-        return (X,)
-    elif isinstance(estimator, _PanelToPanelTransformer):
-        X = _make_panel_X(**kwargs)
-        return (X,)
-    else:
-        raise ValueError(_get_err_msg(estimator))
-
-
-def _make_primitives(n_columns=1, random_state=None):
-    """Generate one or more primitives, for checking inverse-transform."""
-    rng = check_random_state(random_state)
-    if n_columns == 1:
-        return rng.rand()
-    return rng.rand(size=(n_columns,))
-
-
-def _make_tabular_X(n_instances=20, n_columns=1, return_numpy=True, random_state=None):
-    """Generate tabular X, for checking inverse-transform."""
-    rng = check_random_state(random_state)
-    X = rng.rand(n_instances, n_columns)
-    if return_numpy:
-        return X
-    else:
-        return pd.DataFrame(X)
-
-
-def _compare_nested_frame(func, x, y, **kwargs):
-    """Comper two nested pd.DataFrames.
-
-    Parameters
-    ----------
-    func : function
-        Function from np.testing for comparing arrays.
-    x : pd.DataFrame
-    y : pd.DataFrame
-    kwargs : dict
-        Keyword argument for function
-
-    Raises
-    ------
-    AssertionError
-        If x and y are not equal
-    """
-    # We iterate over columns and rows to make cell-wise comparisons.
-    # Tabularizing the data first would simplify this, but does not
-    # work for unequal length data.
-
-    # In rare cases, x and y may be empty (e.g. TSFreshRelevantFeatureExtractor) and
-    # we cannot compare individual cells, so we simply check if everything else is
-    # equal here.
-    assert isinstance(x, pd.DataFrame)
-    if x.empty:
-        assert_frame_equal(x, y)
-
-    elif is_nested_dataframe(x):
-        # Check if both inputs have the same shape
-        if not x.shape == y.shape:
-            raise ValueError("Found inputs with different shapes")
-
-        # Iterate over columns
-        n_columns = x.shape[1]
-        for i in range(n_columns):
-            xc = x.iloc[:, i].tolist()
-            yc = y.iloc[:, i].tolist()
-
-            # Iterate over rows, checking if individual cells are equal
-            for xci, yci in zip(xc, yc):
-                func(xci, yci, **kwargs)
-
-
-def _assert_array_almost_equal(x, y, decimal=6, err_msg=""):
-    func = np.testing.assert_array_almost_equal
-    if isinstance(x, pd.DataFrame):
-        _compare_nested_frame(func, x, y, decimal=decimal, err_msg=err_msg)
-    else:
-        func(x, y, decimal=decimal, err_msg=err_msg)
-
-
-def _assert_array_equal(x, y, err_msg=""):
-    func = np.testing.assert_array_equal
-    if isinstance(x, pd.DataFrame):
-        _compare_nested_frame(func, x, y, err_msg=err_msg)
-    else:
-        func(x, y, err_msg=err_msg)
-
-
-def _get_args(function, varargs=False):
-    """Get function arguments."""
-    try:
-        params = signature(function).parameters
-    except ValueError:
-        # Error on builtin C function
-        return []
-    args = [
-        key
-        for key, param in params.items()
-        if param.kind not in (param.VAR_POSITIONAL, param.VAR_KEYWORD)
-    ]
-    if varargs:
-        varargs = [
-            param.name
-            for param in params.values()
-            if param.kind == param.VAR_POSITIONAL
-        ]
-        if len(varargs) == 0:
-            varargs = None
-        return args, varargs
-    else:
-        return args
