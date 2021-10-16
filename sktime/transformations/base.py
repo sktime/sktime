ef56dceeba28aa2b75e6b2fbfb4fda7557efceb4
Franz Király <f.kiraly@ucl.ac.uk>
2021-10-16 21:13:12 +0100

    added error message to defensive assertion

--- sktime/transformations/base.py
+++ sktime/transformations/base.py
@@ -166,7 +166,7 @@ class BaseTransformer(BaseEstimator):
             X_mtype = mtype(X)
             X_scitype = mtype_to_scitype(X_mtype)
             # this should have converted X to panel, ensure that's the case
-            assert X_scitype == "Panel"
+            assert X_scitype == "Panel", "attempted conversion to Panel unsuccessful"
 
         # 3. internal only has Series but X is Panel: loop over instances
         elif X_scitype == "Panel" and "Panel" not in X_inner_scitypes:
