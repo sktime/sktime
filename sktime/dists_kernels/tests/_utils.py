f356bbfca7f2ab9824369c2a2d351ab3863e970c
chrisholder <chrisholder987@hotmail.com>
2021-10-26 00:24:55 +0100

    fixed bug with creation of test distance

--- sktime/dists_kernels/tests/_utils.py
+++ sktime/dists_kernels/tests/_utils.py
@@ -6,8 +6,8 @@ from sktime.utils._testing.series import _make_series
 
 def create_test_distance_numpy(
     n_instance: int,
-    n_columns: int,
-    n_timepoints: int,
+    n_columns: int = None,
+    n_timepoints: int = None,
     random_state: int = 1,
 ):
     """Create a test numpy distance.
@@ -18,8 +18,8 @@ def create_test_distance_numpy(
         Number of instances to create.
     n_columns: int
         Number of columns to create.
-    n_timepoints: int
-        Number of timepoints to create.
+    n_timepoints: int, defaults = None
+        Number of timepoints to create in each column.
     random_state: int, defaults = 1
         Random state to initialise with.
 
@@ -29,16 +29,26 @@ def create_test_distance_numpy(
         Numpy array of shape specific. If 1 instance then 2D array returned,
         if > 1 instance then 3D array returned.
     """
+    num_dims = 3
+    if n_timepoints is None:
+        n_timepoints = 1
+        num_dims -= 1
+    if n_columns is None:
+        n_columns = 1
+        num_dims -= 1
+
     df = _create_test_distances(
         n_instance=n_instance,
         n_columns=n_columns,
         n_timepoints=n_timepoints,
         random_state=random_state,
     )
-    if n_instance > 1:
+    if num_dims == 3:
         return convert_to(df, to_type="numpy3D")
+    elif num_dims == 2:
+        return convert_to(df, to_type="numpy3D")[:, :, 0]
     else:
-        return convert_to(df, to_type="np.ndarray")
+        return convert_to(df, to_type="numpy3D")[:, 0, 0]
 
 
 def _create_test_distances(n_instance, n_columns, n_timepoints, random_state=1):
