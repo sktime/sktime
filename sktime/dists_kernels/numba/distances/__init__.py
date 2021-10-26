ba92c22542ad92a3df1c53b2afbd3b7a67389ba8
chrisholder <chrisholder987@hotmail.com>
2021-10-26 15:18:20 +0100

    rename refactor

--- sktime/dists_kernels/numba/distances/__init__.py
+++ sktime/dists_kernels/numba/distances/__init__.py
@@ -15,7 +15,7 @@ from sktime.dists_kernels.numba.distances.euclidean_distance import (
     euclidean_distance,
     pairwise_euclidean_distance,
 )
-from sktime.dists_kernels.numba.distances.pairwise_distances import pairwise_distance
+from sktime.dists_kernels.numba.distances.pairwise_distance import pairwise_distance
 from sktime.dists_kernels.numba.distances.squared_distance import (
     pairwise_squared_distance,
     squared_distance,
