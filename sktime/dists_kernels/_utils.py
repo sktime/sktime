64edddf5cb18450d36302455eecf03e9b5b8c383
chrisholder <chrisholder987@hotmail.com>
2021-10-28 02:52:43 +0100

    utils

--- sktime/dists_kernels/_utils.py
+++ sktime/dists_kernels/_utils.py
@@ -1,93 +1,44 @@
 # -*- coding: utf-8 -*-
-from typing import Callable, Tuple
-
 import numpy as np
 
 
-def _check_pairwise_timeseries(x: np.ndarray) -> np.ndarray:
-    """Check and format a timeseries for pairwise calculation.
-
-    Parameters
-    ----------
-    x: np.ndarray (1D, 2D or 3D array)
-        Timeseries for pairwise.
-
-    Returns
-    -------
-    validated_x: np.ndarray (3D array)
-        Validated timeseries in 3D numpy array format.
-    """
-    if x.ndim <= 1:
-        _x = to_numba_timeseries(x)
-    else:
-        _x = x
-
-    if _x.ndim <= 2:
-        validated_x = np.reshape(_x, _x.shape + (1,))
-    else:
-        validated_x = _x
-
-    return validated_x
-
-
-def validate_pairwise_params(
-    x: np.ndarray,
-    y: np.ndarray = None,
-    factory: Callable = None,
-) -> Tuple[np.ndarray, np.ndarray, bool]:
-    """Validate pairwise parameters.
-
-    Parameters
-    ----------
-    x: np.ndarray (1D, 2D or 3D array)
-        First timeseries.
-    y: np.ndarray (1D, 2D or 3D array)
-        Second timeseries.
-    factory: Callable[
-        [np.ndarray, np.ndarray, bool, dict],
-        Callable[[np.ndarray, np.ndarray], float]
-    ]
-        Numba factory used to generate numba compiled functions to be used.
-
-    Returns
-    -------
-    np.ndarray (3D array)
-        First validated timeseries. 3D numpy array.
-    np.ndarray (3D array)
-        Second validated timeseries. 3D numpy array.
-    symmetric: bool
-        Boolean that is true when x == y and false when x != y. Used in some instances
-        to speed up pairwise computation.
-    """
-    if factory is None:
-        raise ValueError("You must specify a numba_distance_factory")
-
-    if y is None:
-        y = np.copy(x)
-        symmetric = True
-    else:
-        symmetric = np.array_equal(x, y)
-
-    validated_x = _check_pairwise_timeseries(x)
-    validated_y = _check_pairwise_timeseries(y)
-
-    return validated_x, validated_y, symmetric
-
-
-def to_numba_timeseries(x):
+def to_numba_timeseries(x: np.ndarray) -> np.ndarray:
     """Convert a timeseries to a valid timeseries for numba use.
 
     Parameters
     ----------
-    x: np.ndarray (1d or 2d array)
+    x: np.ndarray (1d, 2d or 3d array)
         A timeseries.
 
     Returns
     -------
-    np.ndarray (2d array)
-        2d array that is a validated and formatted timeseries for numba use.
+    np.ndarray (3d array)
+        3d array that is the formatted timeseries.
+
+    Raises
+    ------
+    ValueError
+        If the value provided is not a numpy array
+        If the matrix provided is greater than 3 dimensions
     """
+    if not isinstance(x, np.ndarray):
+        raise ValueError(
+            f"The value {x} is an invalid timeseries. To perform a "
+            f"distance computation a numpy arrays must be provided."
+        )
+
     _x = np.array(x, copy=True, dtype=np.float)
-    if _x.ndim < 2:
-        _x = np.reshape(x, (-1, 1))
+    num_dims = _x.ndim
+    if num_dims == 1:
+        shape = _x.shape
+        _x = np.reshape(_x, (1, 1, shape[0]))
+    elif num_dims == 2:
+        shape = _x.shape
+        _x = np.reshape(_x, (1, shape[0], shape[1]))
+    elif num_dims > 3:
+        raise ValueError(
+            "The matrix provided has more than 3 dimensions. This is not"
+            "supported. Please provide a matrix with less than "
+            "3 dimensions"
+        )
     return _x
