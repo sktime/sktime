66b557b6c3ca27b9029e26dd0a2430b546c5e64b
florian.stinner <fstinner@eonerc.rwth-aachen.de>
2021-10-31 15:01:09 +0100

    delete MultiRocket classifier from classification/kernel_based/__init__.py

--- sktime/classification/kernel_based/__init__.py
+++ sktime/classification/kernel_based/__init__.py
@@ -1,17 +1,6 @@
 # -*- coding: utf-8 -*-
 """Kernel based time series classifiers."""
-__all__ = [
-    "ROCKETClassifier",
-    "Arsenal",
-    "MultiRocketClassifier",
-    "MultiRocketMultivariateClassifier",
-]
+__all__ = ["ROCKETClassifier", "Arsenal"]
 
 from sktime.classification.kernel_based._arsenal import Arsenal
 from sktime.classification.kernel_based._rocket_classifier import ROCKETClassifier
-from sktime.classification.kernel_based._multirocket_classifier import (
-    MultiRocketClassifier,
-)
-from sktime.classification.kernel_based._multirocket_multivariate_classifier import (
-    MultiRocketMultivariateClassifier,
-)
