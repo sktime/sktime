0fcace2440f0a768bfe6be295163bff78e06606e
Tony Bagnall <ajb@uea.ac.uk>
2021-11-04 11:52:19 +0000

    bug recreation in classification to force tests

---
+++ sktime/classification/import_bug_test/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""Test."""
+
+__author__ = ["TonyBagnall"]
+# -*- coding: utf-8 -*-
+__all__ = [
+    "public_function1",
+    "public_function2",
+]
+from classification.import_bug_test._import_from_here import public_function1
+from classification.import_bug_test._import_from_here import public_function2
--- sktime/contrib/import_test/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Test."""
-
-__author__ = ["TonyBagnall"]
