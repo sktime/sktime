560030daab185a6fdcd816a207bbf39da71ac3ee
Tony Bagnall <ajb@uea.ac.uk>
2021-11-04 11:53:59 +0000

    Update _import_to_here.py

--- sktime/classification/import_bug_test2/_import_to_here.py
+++ sktime/classification/import_bug_test2/_import_to_here.py
@@ -4,8 +4,12 @@
 
 __author__ = ["TonyBagnall"]
 
-from classification.import_bug_test import public_function1
+from classification.import_bug_test import public_function1, public_function2
 
 
 def import_CI_bug():
     return public_function1()+public_function1()
+
+
+def import_CI_bug2():
+    return public_function2()+public_function2()
