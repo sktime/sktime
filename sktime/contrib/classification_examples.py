a531b0bb897967230a15f98e409d98200d13bccc
Tony Bagnall <ajb@uea.ac.uk>
2021-11-11 09:00:03 +0000

    Update classification_examples.py

--- sktime/contrib/classification_examples.py
+++ sktime/contrib/classification_examples.py
@@ -10,6 +10,7 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 
 from sktime.classification.hybrid import HIVECOTEV2
+from sktime.classification.kernel_based import Arsenal
 from sktime.datasets import load_unit_test
 
 
@@ -25,16 +26,23 @@ def build_classifiers():
     # Create an array
     # Random forest, rocket and HC2.
     randf = RandomForestClassifier()
-    trainX, train_y, testX, test_y = make_toy_problem()
+    trainX, train_y, testX, test_y = make_toy_2d_problem()
     randf.fit(trainX, train_y)
     print("Fit complete")
     print(" rand f acc = ", randf.score(testX, test_y))
     hc2 = HIVECOTEV2(time_limit_in_minutes=1)
-    hc2.fit(trainX, train_y)
-    print(" HC2 acc = ", hc2.score(testX, test_y))
+#    hc2.fit(trainX, train_y)
+#   print(" HC2 acc = ", hc2.score(testX, test_y))
+    afc = Arsenal()
+    afc.fit(trainX, train_y)
+    print(" Arsenal acc = ", afc.score(testX, test_y))
+    trainX, train_y, testX, test_y = make_toy_3d_problem()
+    afc.fit(trainX, train_y)
+    print(" Arsenal acc = ", afc.score(testX, test_y))
 
 
-def make_toy_problem():
+
+def make_toy_2d_problem():
     """Make a toy classification problem out of numpy arrays."""
     X_train_class1 = np.random.uniform(-1, 1, size=(20, 100))
     y_train_class1 = np.zeros(
@@ -60,6 +68,31 @@ def make_toy_problem():
     return X_train, y_train, X_test, y_test
 
 
+def make_toy_3d_problem():
+    """Make a toy 3D classification problem out of numpy arrays."""
+    X_train_class1 = np.random.uniform(-1, 1, size=(20, 3, 100))
+    y_train_class1 = np.zeros(
+        20,
+    )
+    X_train_class2 = np.random.uniform(-0.9, 1.1, size=(20, 3, 100))
+    y_train_class2 = np.ones(
+        20,
+    )
+    X_train = np.concatenate((X_train_class1, X_train_class2), axis=0)
+    y_train = np.concatenate((y_train_class1, y_train_class2))
+
+    X_test_class1 = np.random.uniform(-1, 1, size=(20, 3, 100))
+    y_test_class1 = np.zeros(
+        20,
+    )
+    X_test_class2 = np.random.uniform(-0.9, 1.1, size=(20, 3, 100))
+    y_test_class2 = np.ones(
+        20,
+    )
+    X_test = np.concatenate((X_test_class1, X_test_class2), axis=0)
+    y_test = np.concatenate((y_test_class1, y_test_class2))
+    return X_train, y_train, X_test, y_test
+
 def compare_classifiers():
     """Build pipeline classifiers and compare to published results."""
     # Data set list
@@ -75,3 +108,5 @@ def compare_classifiers():
     # Pull down accuracies
 
     # Draw CD diagram
+
+build_classifiers()
