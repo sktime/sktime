7540f08da21a6d9ceecb87416bfe62a72798827f
Lukasz Mentel <lmmentel@gmail.com>
2021-11-30 17:07:20 +0100

    simplify the azure devops build

--- build_tools/azure/build_wheels.sh
+++ build_tools/azure/build_wheels.sh
@@ -30,21 +30,22 @@ cd /io/ # Change directory
 
 for PYTHON in "${PYTHON_VERSIONS[@]}"; do
   # Install requirements
-  "${PYTHON}/pip" install -r "$REQUIREMENTS"
+  # "${PYTHON}/pip" install -r "$REQUIREMENTS"
 
   # Build wheel
-  "${PYTHON}/python" setup.py bdist_wheel
+  #"${PYTHON}/python" setup.py bdist_wheel
+  "${PYTHON}/python" -m pip install .[all_extras,dev]
 done
 
 # Bundle external shared libraries into the wheels using the auditwheel library
-for wheel in dist/sktime-*.whl; do
-  auditwheel repair --plat "$PLATFORM" --wheel-dir dist/ "$wheel"
-done
+# for wheel in dist/sktime-*.whl; do
+#   auditwheel repair --plat "$PLATFORM" --wheel-dir dist/ "$wheel"
+# done
 
 # Install built wheel and test
 for PYTHON in "${PYTHON_VERSIONS[@]}"; do
   # Install from wheel
-  "${PYTHON}/pip" install --pre --no-index --no-deps --find-links dist/ sktime
+  # "${PYTHON}/pip" install --pre --no-index --no-deps --find-links dist/ sktime
 
   # Run tests
   "${PYTHON}/pytest" --showlocals --durations=20 --junitxml=junit/test-results.xml --cov=sktime --cov-report=xml --cov-report=html --pyargs sktime
