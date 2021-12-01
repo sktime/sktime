dae402c71ec6b0db07b06cffbd38382da4832b04
Lukasz Mentel <lmmentel@gmail.com>
2021-12-01 13:55:24 +0100

    remove mentions of requirements

--- build_tools/azure/build_wheels.sh
+++ build_tools/azure/build_wheels.sh
@@ -5,7 +5,6 @@
 # - https://github.com/pypa/python-manylinux-demo/blob/master/travis/build-wheels.sh
 
 set -e -x
-echo "$REQUIREMENTS"
 echo "$INCLUDED_VERSIONS"
 
 # Split input string of included Python version by comma into array
@@ -29,24 +28,11 @@ echo "Included Python versions: " "${PYTHON_VERSIONS[@]}"
 cd /io/ # Change directory
 
 for PYTHON in "${PYTHON_VERSIONS[@]}"; do
-  # Install requirements
-  # "${PYTHON}/pip" install -r "$REQUIREMENTS"
-
-  # Build wheel
-  #"${PYTHON}/python" setup.py bdist_wheel
   "${PYTHON}/python" -m pip install .[all_extras,dev]
 done
 
-# Bundle external shared libraries into the wheels using the auditwheel library
-# for wheel in dist/sktime-*.whl; do
-#   auditwheel repair --plat "$PLATFORM" --wheel-dir dist/ "$wheel"
-# done
-
 # Install built wheel and test
 for PYTHON in "${PYTHON_VERSIONS[@]}"; do
-  # Install from wheel
-  # "${PYTHON}/pip" install --pre --no-index --no-deps --find-links dist/ sktime
-
   # Run tests
   "${PYTHON}/pytest" --showlocals --durations=20 --junitxml=junit/test-results.xml --cov=sktime --cov-report=xml --cov-report=html --pyargs sktime
 done
