36c014c3f9de2c7acb3c9b8c053294cd50cfb95f
mloning <markus.loning.17@ucl.ac.uk>
2021-12-04 19:02:11 +0000

    fail on missing init

--- build_tools/fail_on_missing_init_files.sh
+++ build_tools/fail_on_missing_init_files.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Script to search for missing init files.
+# Script to search for missing init FILES.
 set -euxo pipefail
 
-if [[ -n `find ./sktime/ -type d '!' -exec test -e "{}/__init__.py" ";" -not -path "**/__pycache__" -not -path "**/datasets/data*" -not -path "**/contrib/*" -print` ]]
+FILES=$( find ./sktime -type d '!' -exec test -e "{}/__init__.py" ";" -not -path "**/__pycache__" -not -path "**/datasets/data*" -not -path "**/contrib/*" -print )
+
+if [[ -n "$FILES" ]]
 then
-    echo "Missing init files detected."
+    echo "Missing __init__.py files detected in the following modules:"
+    echo "$FILES"
     exit 1
 fi
