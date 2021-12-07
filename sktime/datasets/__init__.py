c5f77e6a3ba641d534bbc0f6aad50a92ff1197de
Tony Bagnall <ajb@uea.ac.uk>
2021-12-07 13:59:50 +0000

    remove unnecessary import from init in datasets

--- sktime/datasets/__init__.py
+++ sktime/datasets/__init__.py
@@ -21,25 +21,5 @@ __all__ = [
     "load_electric_devices_segmentation",
     "load_acsf1",
     "load_macroeconomic",
+    "load_from_tsfile_to_dataframe",
 ]
-
-from sktime.datasets._data_io import (
-    load_acsf1,
-    load_airline,
-    load_arrow_head,
-    load_basic_motions,
-    load_electric_devices_segmentation,
-    load_gun_point_segmentation,
-    load_gunpoint,
-    load_italy_power_demand,
-    load_japanese_vowels,
-    load_longley,
-    load_lynx,
-    load_macroeconomic,
-    load_osuleaf,
-    load_PBS_dataset,
-    load_shampoo_sales,
-    load_UCR_UEA_dataset,
-    load_unit_test,
-    load_uschange,
-)
