574b3f443b2642501cb334981730ebf957aead6a
Franz Király <f.kiraly@ucl.ac.uk>
2021-12-12 22:49:48 +0000

    fix in mtype check, and ambiguous mtype call in BaseForecaster

--- sktime/datatypes/_check.py
+++ sktime/datatypes/_check.py
@@ -47,6 +47,9 @@ def _check_scitype_valid(scitype: str = None):
     """Check validity of scitype."""
     valid_scitypes = list(set([x[1] for x in check_dict.keys()]))
 
+    if not isinstance(scitype, str):
+        raise TypeError(f"scitype should be a str but found {type(scitype)}")
+
     if scitype is not None and scitype not in valid_scitypes:
         raise TypeError(scitype + " is not a supported scitype")
 
@@ -174,8 +177,6 @@ def check_is_mtype(
     TypeError if no checks defined for mtype/scitype combination
     TypeError if mtype input argument is not of expected type
     """
-    _check_scitype_valid(scitype)
-
     def ret(valid, msg, metadata, return_metadata):
         if return_metadata:
             return valid, msg, metadata
@@ -195,6 +196,7 @@ def check_is_mtype(
         if scitype is None:
             scitype_of_m = mtype_to_scitype(m)
         else:
+            _check_scitype_valid(scitype)
             scitype_of_m = scitype
         key = (m, scitype_of_m)
         if (m, scitype_of_m) not in valid_keys:
@@ -274,15 +276,15 @@ def check_raise(obj, mtype: str, scitype: str = None, var_name: str = "input"):
         raise TypeError(msg)
 
 
-def mtype(obj, as_scitype: str = None):
+def mtype(obj, as_scitype: Union[str, List[str]] = None):
     """Infer the mtype of an object considered as a specific scitype.
 
     Parameters
     ----------
     obj : object to infer type of - any type, should comply with and mtype spec
         if as_scitype is provided, this needs to be mtype belonging to scitype
-    as_scitype : str, optional, default=None
-        name of scitype the object "obj" is considered as, finds mtype for that
+    as_scitype : str, or list of str, optional, default=None
+        name of scitype(s) the object "obj" is considered as, finds mtype for that
         if None (default), does not assume a specific as_scitype and tests all mtypes
             generally, as_scitype should be provided for maximum efficiency
 
@@ -298,17 +300,26 @@ def mtype(obj, as_scitype: str = None):
     if obj is None:
         return None
 
-    _check_scitype_valid(as_scitype)
+    if isinstance(as_scitype, str):
+        as_scitype = [as_scitype]
+
+    if isinstance(as_scitype, list):
+        for scitype in as_scitype:
+            _check_scitype_valid(scitype)
+    else:
+        raise TypeError("as_scitype must be str or list of str")
 
     if as_scitype is None:
-        mtypes = np.array([x[0] for x in check_dict.keys()])
+        m_plus_scitypes = [(x[0], x[1]) for x in check_dict.keys()]
     else:
-        mtypes = np.array([x[0] for x in check_dict.keys() if x[1] == as_scitype])
+        m_plus_scitypes = [
+            (x[0], x[1]) for x in check_dict.keys() if x[1] in as_scitype
+        ]
 
     res = [
-        mtype
-        for mtype in mtypes
-        if check_is_mtype(obj, mtype=mtype, scitype=as_scitype)
+        m_plus_scitype[0]
+        for m_plus_scitype in m_plus_scitypes
+        if check_is_mtype(obj, mtype=m_plus_scitype[0], scitype=m_plus_scitype[1])
     ]
 
     if len(res) > 1:
