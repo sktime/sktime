c4c9d8ee74a033849ae54b631246fbe7107f83cb
Aparna-Sakshi <aparnasakshi912@gmail.com>
2021-12-18 00:15:25 +0530

    updated docstring, deleted estimator from _config

--- sktime/forecasting/compose/_column_ensemble.py
+++ sktime/forecasting/compose/_column_ensemble.py
@@ -228,11 +228,11 @@ class ColumnEnsembleForecaster(_HeterogenousEnsembleForecaster):
 
         Returns
         -------
-        params : dict or list of dict, default = {}
-            Parameters to create testing instances of the class
+        params : dict or list of dict, default={}
+            Parameters to create testing instances of the class.
             Each dict are parameters to construct an "interesting" test instance, i.e.,
             `MyClass(**params)` or `MyClass(**params[i])` creates a valid test instance.
-            `create_test_instance` uses the first (or only) dictionary in `params`
+            `create_test_instance` uses the first (or only) dictionary in `params`.
         """
         # imports
         from sktime.forecasting.naive import NaiveForecaster
