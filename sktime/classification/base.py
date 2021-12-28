15dfc983a15afb2047c49a8c313a5ba91691a40a
Franz Király <f.kiraly@ucl.ac.uk>
2021-12-28 15:35:53 +0000

    docstring linting

--- sktime/classification/base.py
+++ sktime/classification/base.py
@@ -251,7 +251,7 @@ class BaseClassifier(BaseEstimator):
         ----------
         X : guaranteed to be of a type in self.get_tag("X_inner_mtype")
             if self.get_tag("X_inner_mtype") = "numpy3D":
-                3D np.ndarray of shape = [n_instances,n_dimensions,series_length]
+                3D np.ndarray of shape = [n_instances, n_dimensions, series_length]
             if self.get_tag("X_inner_mtype") = "nested_univ":
                 pd.DataFrame with each column a dimension, each cell a pd.Series
             for list of other mtypes, see datatypes.SCITYPE_REGISTER
@@ -281,7 +281,7 @@ class BaseClassifier(BaseEstimator):
         ----------
         X : guaranteed to be of a type in self.get_tag("X_inner_mtype")
             if self.get_tag("X_inner_mtype") = "numpy3D":
-                3D np.ndarray of shape = [n_instances,n_dimensions,series_length]
+                3D np.ndarray of shape = [n_instances, n_dimensions, series_length]
             if self.get_tag("X_inner_mtype") = "nested_univ":
                 pd.DataFrame with each column a dimension, each cell a pd.Series
             for list of other mtypes, see datatypes.SCITYPE_REGISTER
@@ -307,7 +307,7 @@ class BaseClassifier(BaseEstimator):
         ----------
         X : guaranteed to be of a type in self.get_tag("X_inner_mtype")
             if self.get_tag("X_inner_mtype") = "numpy3D":
-                3D np.ndarray of shape = [n_instances,n_dimensions,series_length]
+                3D np.ndarray of shape = [n_instances, n_dimensions, series_length]
             if self.get_tag("X_inner_mtype") = "nested_univ":
                 pd.DataFrame with each column a dimension, each cell a pd.Series
             for list of other mtypes, see datatypes.SCITYPE_REGISTER
