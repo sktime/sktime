a24034584f9cb66affcd8ed5758e2ef729cd5b5b
Franz Király <f.kiraly@ucl.ac.uk>
2022-01-05 21:47:17 +0000

    work

--- sktime/utils/_testing/scenarios_getter.py
+++ sktime/utils/_testing/scenarios_getter.py
@@ -8,6 +8,7 @@ __all__ = ["retrieve_scenarios"]
 
 from inspect import isclass
 
+from sktime.base import BaseObject
 from sktime.registry import BASE_CLASS_LIST, BASE_CLASS_SCITYPE_LIST
 from sktime.utils._testing.scenarios_forecasting import scenarios_forecasting
 
@@ -15,43 +16,37 @@ scenarios = dict()
 scenarios["forecaster"] = scenarios_forecasting
 
 
-def retrieve_scenarios(obj=None, estimator_type=None):
+def retrieve_scenarios(obj):
     """Retrieve test scenarios for obj, or by estimator scitype string.
 
     Exactly one of the arguments obj, estimator_type must be provided.
 
     Parameters
     ----------
-    obj : class or object, inheriting from BaseObject.
-    estimator_types: string, list of string, optional (default=None)
-        Which kind of estimator/object to retrieve estimators for.
-        if str or list of str, strings define scitypes specified in search
-            possible str values are entries of registry.BASE_CLASS_REGISTER (first col)
-                for instance 'classifier', 'regressor', 'transformer', 'forecaster'
+    obj : class or object, or string.
+        Which kind of estimator/object to retrieve scenarios for.
+        If object, must be a class or object inheriting from BaseObject.
+        If string, must be in registry.BASE_CLASS_REGISTER (first col)
+            for instance 'classifier', 'regressor', 'transformer', 'forecaster'
 
     Returns
     -------
     scenarios : list of objects, instances of BaseScenario
     """
-    if obj is not None and estimator_type is not None:
-        raise ValueError("exactly one of obj, estimator_type should be passed")
-    if obj is None and estimator_type is None:
-        raise ValueError("exactly one of obj, estimator_type should be passed")
-
-    if not isinstance(estimator_type, str):
-        raise TypeError("estimator_type must be a str")
-    if estimator_type not in BASE_CLASS_SCITYPE_LIST:
+    if not isinstance(obj, (str, BaseObject)):
+        raise TypeError("obj must be a str or inherit from BaseObject")
+    if isinstance(obj, str) and obj not in BASE_CLASS_SCITYPE_LIST:
         raise ValueError(
-            "estimator_type must be a valid scitype string, "
+            "if obj is a str, then obj must be a valid scitype string, "
             "see registry.BASE_CLASS_SCITYPE_LIST for valid scitype strings"
         )
 
-    if obj is not None:
+    if not isinstance(obj, str):
         estimator_type = _scitype_from_class(obj)
 
     scenarios_for_type = scenarios.get(estimator_type)
 
-    scenarios_for_type = [x for x in scenarios_for_type if x.isapplicable(obj)]
+    scenarios_for_type = [x() for x in scenarios_for_type if x.isapplicable(obj)]
 
     if scenarios_for_type is None:
         return []
