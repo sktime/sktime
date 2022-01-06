cd8ecdef16859bb5bbdf0d6a06bd269164bbce35
Stanislav Khrapov <stanislav.khrapov@dbschenker.com>
2022-01-06 13:09:22 +0100

    roll back to main

--- sktime/forecasting/base/__init__.py
+++ sktime/forecasting/base/__init__.py
@@ -3,7 +3,10 @@
 # copyright: sktime developers, BSD-3-Clause License (see LICENSE file)
 """Implements base classes for forecasting in sktime."""
 
-__all__ = ["ForecastingHorizon", "BaseForecaster"]
+__all__ = [
+    "ForecastingHorizon",
+    "BaseForecaster",
+]
 
 from sktime.forecasting.base._base import BaseForecaster
 from sktime.forecasting.base._fh import ForecastingHorizon
