95e46619afea96241bda888ec4a1a839e589645e
Franz Király <f.kiraly@ucl.ac.uk>
2022-01-06 17:24:32 +0000

    deep_equals return_msg

--- sktime/utils/_testing/deep_equals.py
+++ sktime/utils/_testing/deep_equals.py
@@ -16,7 +16,7 @@ import numpy as np
 import pandas as pd
 
 
-def deep_equals(x, y):
+def deep_equals(x, y, return_msg=False):
     """Test two objects for equality in value.
 
     Correct if x/y are one of the following valid types:
@@ -30,52 +30,76 @@ def deep_equals(x, y):
 
     Parameters
     ----------
-    x: object
-    y: object
+    x : object
+    y : object
+    return_msg : bool, optional, default=False
+        whether to return informative message about what is not equal
 
     Returns
     -------
-    bool - True if x and y are equal in value
+    is_equal: bool - True if x and y are equal in value
         x and y do not need to be equal in reference
+    msg : str, only returned if return_msg = True
+        indication of what is the reason for not being equal
     """
+
+    def ret(is_equal, msg):
+        if return_msg:
+            if is_equal:
+                msg = ""
+            return is_equal, msg
+        else:
+            return is_equal
+
     if type(x) != type(y):
-        return False
+        return ret(False, "type")
 
     # we now know all types are the same
     # so now we compare values
     if isinstance(x, pd.Series):
         if x.dtype != y.dtype:
-            return False
+            return ret(False, "dtype")
         # if columns are object, recurse over entries and index
         if x.dtype == "object":
             index_equal = x.index.equals(y.index)
-            return index_equal and deep_equals(list(x.values), list(y.values))
+            values_equal, values_msg = deep_equals(
+                list(x.values), list(y.values), return_msg=True
+            )
+            if not index_equal:
+                msg = ".index"
+            elif not values_equal:
+                msg = ".values" + values_msg
+            return ret(index_equal and values_equal, msg)
         else:
-            return x.equals(y)
+            return ret(x.equals(y), ".series_equals")
     elif isinstance(x, pd.DataFrame):
         if not x.columns.equals(y.columns):
-            return False
+            return ret(False, ".columns")
         # if columns are equal and at least one is object, recurse over Series
         if sum(x.dtypes == "object") > 0:
-            return np.all([deep_equals(x[c], y[c]) for c in x.columns])
+            for c in x.columns:
+                is_equal, msg = deep_equals(x[c], y[c])
+                if not is_equal:
+                    return ret(False, f'["{c}"]' + msg)
+            return ret(True, "")
         else:
-            return x.equals(y)
+            return ret(x.equals(y), ".df_equals")
     elif isinstance(x, np.ndarray):
         if x.dtype != y.dtype:
-            return False
-        return np.array_equal(x, y, equal_nan=True)
+            return ret(False, ".dtype")
+        return ret(np.array_equal(x, y, equal_nan=True), ".values")
     # recursion through lists, tuples and dicts
     elif isinstance(x, (list, tuple)):
-        return _tuple_equals(x, y)
+        return ret(*_tuple_equals(x, y, return_msg=True))
     elif isinstance(x, dict):
-        return _dict_equals(x, y)
+        return ret(*_dict_equals(x, y, return_msg=True))
     elif x != y:
-        return False
+        return ret(False, " !=")
 
-    return True
+    return ret(True, "")
 
 
-def _tuple_equals(x, y):
+def _tuple_equals(x, y, return_msg=False):
     """Test two tuples or lists for equality.
 
     Correct if tuples/lists contain the following valid types:
@@ -93,10 +117,19 @@ def _tuple_equals(x, y):
     bool - True if x and y are equal in value
         x and y do not need to be equal in reference
     """
+
+    def ret(is_equal, msg):
+        if return_msg:
+            if is_equal:
+                msg = ""
+            return is_equal, msg
+        else:
+            return is_equal
+
     n = len(x)
 
     if n != len(y):
-        return False
+        return ret(False, ".len")
 
     # we now know dicts are same length
     for i in range(n):
@@ -104,13 +137,14 @@ def _tuple_equals(x, y):
         yi = y[i]
 
         # recurse through xi/yi
-        if not deep_equals(xi, yi):
-            return False
+        is_equal, msg = deep_equals(xi, yi, return_msg=True)
+        if not is_equal:
+            return ret(False, f"[{i}]" + msg)
 
-    return True
+    return ret(True, "")
 
 
-def _dict_equals(x, y):
+def _dict_equals(x, y, return_msg=False):
     """Test two dicts for equality.
 
     Correct if dicts contain the following valid types:
@@ -127,11 +161,20 @@ def _dict_equals(x, y):
     -------
     bool - True if x and y have equal keys and values
     """
+
+    def ret(is_equal, msg):
+        if return_msg:
+            if is_equal:
+                msg = ""
+            return is_equal, msg
+        else:
+            return is_equal
+
     xkeys = set(x.keys())
     ykeys = set(y.keys())
 
     if xkeys != ykeys:
-        return False
+        return ret(False, ".keys")
 
     # we now know all keys are the same
     for key in xkeys:
@@ -139,7 +182,8 @@ def _dict_equals(x, y):
         yi = y[key]
 
         # recurse through xi/yi
-        if not deep_equals(xi, yi):
-            return False
+        is_equal, msg = deep_equals(xi, yi, return_msg=True)
+        if not is_equal:
+            return ret(False, f"[{key}]" + msg)
 
-    return True
+    return ret(True, "")
