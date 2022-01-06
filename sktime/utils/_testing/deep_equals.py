70c6cac7e1d0ce3eb71a34345cab55d4f1ee8beb
Franz Király <f.kiraly@ucl.ac.uk>
2022-01-06 17:36:44 +0000

    adds x/y to error message

--- sktime/utils/_testing/deep_equals.py
+++ sktime/utils/_testing/deep_equals.py
@@ -64,13 +64,13 @@ def deep_equals(x, y, return_msg=False):
             return is_equal
 
     if type(x) != type(y):
-        return ret(False, ".type")
+        return ret(False, f".type, x.type = {type(x)} != y.type = {type(y)}")
 
     # we now know all types are the same
     # so now we compare values
     if isinstance(x, pd.Series):
         if x.dtype != y.dtype:
-            return ret(False, ".dtype")
+            return ret(False, f".dtype, x.dtype= {x.dtype} != y.dtype = {y.dtype}")
         # if columns are object, recurse over entries and index
         if x.dtype == "object":
             index_equal = x.index.equals(y.index)
@@ -78,15 +78,17 @@ def deep_equals(x, y, return_msg=False):
                 list(x.values), list(y.values), return_msg=True
             )
             if not index_equal:
-                msg = ".index"
+                msg = f".index, x.index: {x.index}, y.index: {y.index}"
             elif not values_equal:
                 msg = ".values" + values_msg
             return ret(index_equal and values_equal, msg)
         else:
-            return ret(x.equals(y), ".series_equals")
+            return ret(x.equals(y), f".series_equals, x = {x} != y = {y}")
     elif isinstance(x, pd.DataFrame):
         if not x.columns.equals(y.columns):
-            return ret(False, ".columns")
+            return ret(
+                False, f".columns, x.columns = {x.columns} != y.columns = {y.columns}"
+            )
         # if columns are equal and at least one is object, recurse over Series
         if sum(x.dtypes == "object") > 0:
             for c in x.columns:
@@ -95,10 +97,10 @@ def deep_equals(x, y, return_msg=False):
                     return ret(False, f'["{c}"]' + msg)
             return ret(True, "")
         else:
-            return ret(x.equals(y), ".df_equals")
+            return ret(x.equals(y), f".df_equals, x = {x} != y = {y}")
     elif isinstance(x, np.ndarray):
         if x.dtype != y.dtype:
-            return ret(False, ".dtype")
+            return ret(False, f".dtype, x.dtype = {x.dtype} != y.dtype = {y.dtype}")
         return ret(np.array_equal(x, y, equal_nan=True), ".values")
     # recursion through lists, tuples and dicts
     elif isinstance(x, (list, tuple)):
@@ -148,7 +150,7 @@ def _tuple_equals(x, y, return_msg=False):
     n = len(x)
 
     if n != len(y):
-        return ret(False, ".len")
+        return ret(False, f".len, x.len = {n} != y.len = {len(y)}")
 
     # we now know dicts are same length
     for i in range(n):
@@ -201,7 +203,7 @@ def _dict_equals(x, y, return_msg=False):
     ykeys = set(y.keys())
 
     if xkeys != ykeys:
-        return ret(False, ".keys")
+        return ret(False, f".keys, x.keys = {xkeys} != y.keys = {ykeys}")
 
     # we now know all keys are the same
     for key in xkeys:
