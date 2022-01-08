d88a94328a77a9256bb80ae81b1c708f4e7de387
Franz Király <f.kiraly@ucl.ac.uk>
2022-01-08 23:03:58 +0000

    output_mtype bug cont

--- sktime/transformations/base.py
+++ sktime/transformations/base.py
@@ -787,7 +787,10 @@ class BaseTransformer(BaseEstimator):
             Xt = convert_Panel_to_Series(Xt)
 
         if output_scitype == "Series":
-            # if the transformer outputs multivariate series,
+            # output mtype is input mtype
+            X_output_mtype = X_input_mtype
+
+            # exception to this: if the transformer outputs multivariate series,
             #   we cannot convert back to pd.Series, do pd.DataFrame instead then
             #   this happens only for Series, not Panel
             if X_input_scitype == "Series":
@@ -798,8 +801,7 @@ class BaseTransformer(BaseEstimator):
                 )
                 if not metadata["is_univariate"] and X_input_mtype == "pd.Series":
                     X_output_mtype = "pd.DataFrame"
-            else:
-                X_output_mtype = X_input_mtype
+
             Xt = convert_to(
                 Xt,
                 to_type=X_output_mtype,
