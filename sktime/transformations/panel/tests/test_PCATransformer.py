61c79bb1af9cc356bac21709ff954f6c98f5c31d
Franz Király <f.kiraly@ucl.ac.uk>
2022-01-10 01:07:56 +0000

    more tests for PCA removed (duplicate)

--- sktime/transformations/panel/tests/test_PCATransformer.py
+++ sktime/transformations/panel/tests/test_PCATransformer.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
+"""Tests for PCATransformer."""
 import numpy as np
 import pytest
 
-from sktime.exceptions import NotFittedError
 from sktime.transformations.panel.pca import PCATransformer
 from sktime.utils._testing.panel import _make_nested_from_array
 
 
-# Check that exception is raised for bad input args.
 @pytest.mark.parametrize("bad_components", ["str", 1.2, -1.2, -1, 11])
 def test_bad_input_args(bad_components):
+    """Check that exception is raised for bad input args."""
     X = _make_nested_from_array(np.ones(10), n_instances=10, n_columns=1)
 
     if isinstance(bad_components, str):
@@ -18,24 +18,3 @@ def test_bad_input_args(bad_components):
     else:
         with pytest.raises(ValueError):
             PCATransformer(n_components=bad_components).fit(X)
-
-
-# Test that PCATransformer fails if attempt to transform before fit
-def test_early_trans_fail():
-    X = _make_nested_from_array(np.ones(10), n_instances=1, n_columns=1)
-    pca = PCATransformer(n_components=1)
-
-    with pytest.raises(NotFittedError):
-        pca.transform(X)
-
-
-# Test output format and dimensions.
-@pytest.mark.parametrize(
-    "n_instances,len_series,n_components",
-    [
-        (5, 2, 1),
-        (5, 10, 1),
-        (5, 10, 3),
-        (5, 10, 5),
-    ],
-)
