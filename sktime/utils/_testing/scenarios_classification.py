1f7ecd99e0548cc5d3c31789fedabc94a849fb3c
Franz Király <f.kiraly@ucl.ac.uk>
2022-01-11 20:41:53 +0000

    classification scenario: X, y to have same number of instances, and 20 time points

--- sktime/utils/_testing/scenarios_classification.py
+++ sktime/utils/_testing/scenarios_classification.py
@@ -51,9 +51,9 @@ class ClassifierTestScenario(TestScenario, BaseObject):
         return args
 
 
-y = _make_classification_y(random_state=RAND_SEED)
-X = _make_panel_X(n_instances=10, n_timepoints=10, random_state=RAND_SEED, y=y)
-X_test = _make_panel_X(n_instances=5, n_timepoints=10, random_state=RAND_SEED)
+y = _make_classification_y(n_instances=10, random_state=RAND_SEED)
+X = _make_panel_X(n_instances=10, n_timepoints=20, random_state=RAND_SEED, y=y)
+X_test = _make_panel_X(n_instances=5, n_timepoints=20, random_state=RAND_SEED)
 
 
 class ClassifierFitPredict(ClassifierTestScenario):
