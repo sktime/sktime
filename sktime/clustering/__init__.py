893467cb1a9515f6a29f605342f826bff837ed4c
chrisholder <chrisholder987@hotmail.com>
2022-01-13 15:35:35 +0000

    move base into init

--- sktime/clustering/__init__.py
+++ sktime/clustering/__init__.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 """Time series clustering module."""
-__all__ = ["TimeSeriesKMeans", "TimeSeriesKMedoids"]
+__all__ = ["BaseClusterer", "TimeSeriesKMeans", "TimeSeriesKMedoids"]
 __author__ = ["chrisholder", "TonyBagnall"]
 
+from sktime.clustering._base import BaseClusterer
 from sktime.clustering._k_means import TimeSeriesKMeans
 from sktime.clustering._k_medoids import TimeSeriesKMedoids
