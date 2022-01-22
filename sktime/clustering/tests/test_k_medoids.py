16f956607e7ddaa382ebc4ac902cd245456871ae
chrisholder <chrisholder987@hotmail.com>
2022-01-22 16:55:06 +0000

    perf(kmedoids): reduced the test time for kmedoids

--- sktime/clustering/tests/test_k_medoids.py
+++ sktime/clustering/tests/test_k_medoids.py
@@ -53,11 +53,11 @@ expected_results = {
 
 expected_score = {"medoids": 0.3153846153846154}
 
-train_expected_score = {"medoids": 0.48717948717948717}
+train_expected_score = {"medoids": 0.4858974358974359}
 
-expected_inertia = {"medoids": 290241.1549727707}
+expected_inertia = {"medoids": 291267.56256896566}
 
-expected_iters = {"medoids": 300}
+expected_iters = {"medoids": 5}
 
 expected_labels = {
     "medoids": [
@@ -79,7 +79,7 @@ expected_labels = {
         4,
         1,
         0,
-        4,
+        1,
         4,
         0,
         0,
@@ -110,7 +110,7 @@ def test_kmedoids():
     X_train, y_train = load_basic_motions(split="train")
     X_test, y_test = load_basic_motions(split="test")
 
-    kmedoids = TimeSeriesKMedoids(random_state=1, n_init=2)
+    kmedoids = TimeSeriesKMedoids(random_state=1, n_init=2, max_iter=5)
     train_predict = kmedoids.fit_predict(X_train)
     train_score = metrics.rand_score(y_train, train_predict)
     test_medoids_result = kmedoids.predict(X_test)
