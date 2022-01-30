62c89e6b6b3f2ee7cf1f3d785b179ce12e9a10ce
Tony Bagnall <ajb@uea.ac.uk>
2022-01-30 20:49:13 +0000

    Merge branch 'main' into testing_by_module
    # Conflicts:
    #	sktime/clustering/tests/test_k_medoids.py

---
+++ sktime/series_as_features/tests/_config.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3 -u
+# -*- coding: utf-8 -*-
+
+__author__ = ["Markus Löning"]
+__all__ = []
+
+import numpy as np
+import pandas as pd
+
+N_INSTANCES = [10, 15]
+N_COLUMNS = [3, 5]
+N_TIMEPOINTS = [3, 5]
+N_CLASSES = [2, 5]
+ACCEPTED_OUTPUT_TYPES = (np.ndarray, pd.Series)
