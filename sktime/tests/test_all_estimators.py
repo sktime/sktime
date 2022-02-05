f71493b1a11885840bb1a3343afb1c194fe3b738
Franz Király <f.kiraly@ucl.ac.uk>
2022-02-05 16:47:36 +0000

    added E501 skip for long doctest

--- sktime/tests/test_all_estimators.py
+++ sktime/tests/test_all_estimators.py
@@ -265,7 +265,7 @@ class QuickTester:
 
     def run_tests(
         self, est, return_exceptions=True, tests_to_run=None, fixtures_to_run=None
-    ):
+    ):  # noqa: E501
         """Run all tests on one single estimator.
 
         All tests in self are run on the following estimator type fixtures:
