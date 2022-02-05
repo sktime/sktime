9879a11eae0c171384d1045f27122c1764c62fd8
Franz Király <f.kiraly@ucl.ac.uk>
2022-02-05 21:51:15 +0000

    linting

--- sktime/utils/estimator_checks.py
+++ sktime/utils/estimator_checks.py
@@ -4,12 +4,9 @@
 __author__ = ["fkiraly"]
 __all__ = ["check_estimator"]
 
-
-from sktime.registry import scitype
-
 from sktime.forecasting.tests.test_all_forecasters import TestAllForecasters
+from sktime.registry import scitype
 from sktime.tests.test_all_estimators import TestAllEstimators
-
 
 testclass_dict = dict()
 testclass_dict["forecaster"] = TestAllForecasters
