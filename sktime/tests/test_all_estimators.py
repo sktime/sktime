dee6b14817fd01f9ea0b67b74e1a44855292d5c7
Franz Király <f.kiraly@ucl.ac.uk>
2022-02-09 07:40:56 +0000

    removing multiprocessing_idempotent

--- sktime/tests/test_all_estimators.py
+++ sktime/tests/test_all_estimators.py
@@ -663,7 +663,8 @@ def test_persistence_via_pickle(estimator_instance):
             err_msg="Results are not the same after pickling",
         )
 
-
+# todo: this needs to be diagnosed and fixed - temporary skip
+@pytest.mark.skip(reason="hangs on mac and unix remote tests")
 def test_multiprocessing_idempotent(estimator_class, scenario):
     """Test that single and multi-process run results are identical.
 
