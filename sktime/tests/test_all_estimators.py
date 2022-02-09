1c0a7d969ac00fa426c915d96546638f87cd627a
Franz Király <f.kiraly@ucl.ac.uk>
2022-02-09 07:41:13 +0000

    linting

--- sktime/tests/test_all_estimators.py
+++ sktime/tests/test_all_estimators.py
@@ -663,6 +663,7 @@ def test_persistence_via_pickle(estimator_instance):
             err_msg="Results are not the same after pickling",
         )
 
+
 # todo: this needs to be diagnosed and fixed - temporary skip
 @pytest.mark.skip(reason="hangs on mac and unix remote tests")
 def test_multiprocessing_idempotent(estimator_class, scenario):
