f7f39632ae8e0514b23c72fa5f926acdd2083f5d
Franz Király <f.kiraly@ucl.ac.uk>
2022-02-12 13:34:07 +0000

    made 3D distance downwards compatible

--- sktime/distances/_distance_pairwise.py
+++ sktime/distances/_distance_pairwise.py
@@ -5,6 +5,8 @@ Applies pairwise_distance to all distanes exported by module, so
     exported distances function with 1D, 2D and 3D input.
 """
 
+import numpy as np
+
 from sktime.distances._distance import ALL_DISTANCES, pairwise_distance
 
 assign = [(x.__name__, i) for i, x in enumerate(ALL_DISTANCES)]
@@ -14,7 +16,15 @@ def _vectorize_distance(distance):
     """Turn 2D distance in one that can handle 1D, 2D and 3D input."""
 
     def _3D_distance(X, X2, **params):
-        return pairwise_distance(X, X2, metric=distance, **params)
+        msg = "X and X2 must be np.ndarray, of dim 1, 2, or 3"
+        if not isinstance(X, np.ndarray) or not isinstance(X2, np.ndarray):
+            raise TypeError(msg)
+        if X.ndim > 3 or X2.ndim > 3 or X.ndim < 1 or X2.ndim < 1:
+            raise TypeError(msg)
+        if X.ndim < 3 and X2.ndim < 3:
+            return distance(X, X2, **params)
+        else:
+            return pairwise_distance(X, X2, metric=distance, **params)
 
     return _3D_distance
 
