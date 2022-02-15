6c0422834e1e5ad2798490fb7579f0a81eccac2d
Franz Király <f.kiraly@ucl.ac.uk>
2022-02-15 15:07:44 +0000

    linting

--- sktime/tests/test_all_estimators.py
+++ sktime/tests/test_all_estimators.py
@@ -49,7 +49,6 @@ from sktime.utils._testing.estimator_checks import (
     _get_args,
     _has_capability,
     _list_required_methods,
-    _make_args,
 )
 from sktime.utils._testing.scenarios_getter import retrieve_scenarios
 
