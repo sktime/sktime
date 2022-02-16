94f318dcd7a379fcdd0cdd98b5f64793a5ce84c0
Franz Király <f.kiraly@ucl.ac.uk>
2022-02-16 09:37:00 +0000

    forgot to add pd_Series_Table to the registry

--- sktime/datatypes/_table/_registry.py
+++ sktime/datatypes/_table/_registry.py
@@ -12,6 +12,7 @@ MTYPE_REGISTER_TABLE = [
     ("pd_DataFrame_Table", "Table", "pd.DataFrame representation of a data table"),
     ("numpy1D", "Table", "1D np.narray representation of a univariate table"),
     ("numpy2D", "Table", "2D np.narray representation of a univariate table"),
+    ("pd_Series_Table", "Table", "pd.Series representation of a data table"),
 ]
 
 MTYPE_LIST_TABLE = pd.DataFrame(MTYPE_REGISTER_TABLE)[0].values
