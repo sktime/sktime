c8200f6366f4addf7890966afec0eebb1176b363
TonyBagnall <ajb@uea.ac.uk>
2022-02-19 17:24:51 +0000

    Update clustering_experiments.py

--- sktime/contrib/clustering_experiments.py
+++ sktime/contrib/clustering_experiments.py
@@ -104,7 +104,7 @@ if __name__ == "__main__":
     if sys.argv.__len__() > 1:  # cluster run, this is fragile
         print(sys.argv)
         data_dir = "/home/ajb/data/Univariate_ts/"
-        results_dir = "/home/ajb/results/kmedoids/normed/"
+        results_dir = "/home/ajb/results/kmeans/non-normed/"
         dataset = sys.argv[1]
         resample = int(sys.argv[2]) - 1
         tf = True
