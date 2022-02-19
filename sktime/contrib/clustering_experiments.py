8569d9eeff76ccbd57d6ec8c7d6d9801a12eb5e8
TonyBagnall <ajb@uea.ac.uk>
2022-02-19 17:23:13 +0000

    Update clustering_experiments.py

--- sktime/contrib/clustering_experiments.py
+++ sktime/contrib/clustering_experiments.py
@@ -124,13 +124,13 @@ if __name__ == "__main__":
         dataset = "Chinatown"
         resample = 27
         tf = True
-        distance = "dtw"
+        distance = "ddtw"
     train_X, train_Y = load_ts(f"{data_dir}/{dataset}/{dataset}_TRAIN.ts",
                                return_data_type="numpy2d")
     test_X, test_Y = load_ts(f"{data_dir}/{dataset}/{dataset}_TEST.ts",
                              return_data_type="numpy2d")
-    normalize(train_X, norm="l1", copy=False)
-    normalize(test_X, norm="l1", copy=False)
+#    normalize(train_X, norm="l1", copy=False)
+#    normalize(test_X, norm="l1", copy=False)
     if tune:
         window = tune_window(distance, train_X)
         name = clusterer + "-" + distance+"-tuned"
