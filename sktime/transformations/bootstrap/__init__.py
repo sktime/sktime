e7d905ee91d8f4a3de25b056845bdee9d4642810
ltsaprounis <leonidas.tsap@gmail.com>
2022-02-23 20:32:43 +0000

    added mbb in __init__

--- sktime/transformations/bootstrap/__init__.py
+++ sktime/transformations/bootstrap/__init__.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 """Bootstrapping methods for time series augmentation."""
 
-__all__ = ["BootsrappingTransformer"]
+__all__ = ["BootsrappingTransformer", "MovingBlockBootsrapTransformer"]
 
-from sktime.transformations.bootstrap._mbb import BootsrappingTransformer
+from sktime.transformations.bootstrap._mbb import (
+    BootsrappingTransformer,
+    MovingBlockBootsrapTransformer,
+)
