a2021d81e2d30c0137abf14381b928c81c06fe7c
chrisholder <chrisholder987@hotmail.com>
2022-02-25 13:58:57 +0000

    bug(clustering): fixed init

--- sktime/clustering/__init__.py
+++ sktime/clustering/__init__.py
@@ -2,11 +2,7 @@
 """Time series clustering module."""
 __all__ = [
     "BaseClusterer",
-    "TimeSeriesKMeans",
-    "TimeSeriesKMedoids",
 ]
 __author__ = ["chrisholder", "TonyBagnall"]
 
 from sktime.clustering._base import BaseClusterer
-from sktime.clustering._k_means import TimeSeriesKMeans
-from sktime.clustering._k_medoids import TimeSeriesKMedoids
