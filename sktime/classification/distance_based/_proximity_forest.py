17f1bcc3a3c2cdb69e70f45b821e6595831d02fa
Tony Bagnall <ajb@uea.ac.uk>
2022-02-28 16:11:02 +0000

    speed up PF example

--- sktime/classification/distance_based/_proximity_forest.py
+++ sktime/classification/distance_based/_proximity_forest.py
@@ -1291,7 +1291,7 @@ class ProximityForest(BaseClassifier):
     >>> from sktime.datasets import load_unit_test
     >>> X_train, y_train = load_unit_test(split="train", return_X_y=True)
     >>> X_test, y_test = load_unit_test(split="test", return_X_y=True)
-    >>> clf = ProximityForest(n_estimators=5)
+    >>> clf = ProximityForest(n_estimators=2,n_stump_evaluations=1)
     >>> clf.fit(X_train, y_train)
     ProximityForest(...)
     >>> y_pred = clf.predict(X_test)
