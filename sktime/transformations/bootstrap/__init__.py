e0d609000d4ed9df9b39122d5cac9afc5922a317
ltsaprounis <leonidas.tsap@gmail.com>
2022-02-28 14:24:07 +0000

    __init__ docs

--- sktime/transformations/bootstrap/__init__.py
+++ sktime/transformations/bootstrap/__init__.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
-"""Bootstrapping methods for time series."""
+"""Bootstrapping methods for time series.
+
+Transforms take a series as input and return a panel of synthetic time series generated
+by a resampling algorithm on the observed time series.
+"""
 
 __all__ = ["STLBootsrapTransformer", "MovingBlockBootsrapTransformer"]
 
