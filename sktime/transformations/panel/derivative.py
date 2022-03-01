8a324199b7a4a0cd744b78df2cea507e2fa2996b
chrisholder <chrisholder987@hotmail.com>
2022-03-01 17:59:56 +0000

    bug(transformers): fix flag

--- sktime/transformations/panel/derivative.py
+++ sktime/transformations/panel/derivative.py
@@ -10,7 +10,7 @@ class DerivativeTransformer(BaseTransformer):
     """Derivative transformer of a time series."""
 
     _tags = {
-        "scitype:instancewise": False,  # is this an instance-wise transform?
+        "scitype:instancewise": True,  # is this an instance-wise transform?
         "X_inner_mtype": "numpy3D",  # which mtypes do _fit/_predict support for X?
         "y_inner_mtype": "None",  # which mtypes do _fit/_predict support for X?
         "fit-in-transform": True,
