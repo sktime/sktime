3b0816948d61063f79ba79d11096c7ca67ad02e4
Danbartl <danielbartling@gmail.com>
2022-03-07 09:54:13 +0100

    Deprecation

---
+++ sktime/transformations/series/window_summarizer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3 -u
+# -*- coding: utf-8 -*-
+# copyright: sktime developers, BSD-3-Clause License (see LICENSE file)
+"""Implement transformers for summarizing a time series."""
+
+import warnings
+
+warnings.warn(
+    "WindowSummarizer will be moved to summarize. In future, use"
+    + "sktime.transformations.series.summarize import WindowSummarizer"
+)
