f9e62dc5144589b4cef85ec4d442fab4924a2d08
Franz Király <f.kiraly@ucl.ac.uk>
2022-03-13 19:10:34 +0000

    test module docstring

--- sktime/transformations/tests/test_base.py
+++ sktime/transformations/tests/test_base.py
@@ -4,6 +4,10 @@
 
 Each test covers a "decision path" in the base class boilerplate,
     with a focus on frequently breaking paths in base class refactor and bugfixing.
+The path taken depends on tags of a given transformer, and input data type.
+Concrete transformer classes from sktime are imported to cover
+    different combinations of transformer tags.
+Transformer scenarios cover different combinations of input data types.
 """
 
 __author__ = ["fkiraly"]
