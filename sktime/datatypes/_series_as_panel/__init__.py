98caa990de3a807c90d23c9b898d757fbbe03585
Franz Király <f.kiraly@ucl.ac.uk>
2022-03-14 01:41:09 +0000

    convert_to_scitype export

--- sktime/datatypes/_series_as_panel/__init__.py
+++ sktime/datatypes/_series_as_panel/__init__.py
@@ -4,9 +4,11 @@
 from sktime.datatypes._series_as_panel._convert import (
     convert_Panel_to_Series,
     convert_Series_to_Panel,
+    convert_to_scitype,
 )
 
 __all__ = [
     "convert_Panel_to_Series",
     "convert_Series_to_Panel",
+    "convert_to_scitype",
 ]
