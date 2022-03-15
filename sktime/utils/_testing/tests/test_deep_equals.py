b5e26deeeee9c93738d53d656df60f2fbdcd2fdc
Franz Király <f.kiraly@ucl.ac.uk>
2022-03-15 00:50:10 +0000

    tests

---
+++ sktime/utils/_testing/tests/test_deep_equals.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""Tests for deep_equals utility."""
+import pytest
+
+import pandas as pd
+import numpy as np
+
+from copy import deepcopy
+
+from sktime.forecasting.base import ForecastingHorizon
+from sktime.utils._testing.deep_equals import deep_equals
+
+
+# examples used for comparison below
+EXAMPLES = [
+    42,
+    [],
+    ((((())))),
+    [([([([()])])])],
+    np.array([2, 3, 4]),
+    np.array([2, 4, 5]),
+    pd.DataFrame({"a": [4, 2]}),
+    pd.DataFrame({"a": [4, 3]}),
+    (np.array([1, 2, 4]), [pd.DataFrame({"a": [4, 2]})]),
+    ForecastingHorizon([1, 2, 3], is_relative=True),
+    ForecastingHorizon([1, 2, 3], is_relative=False),
+    {"foo": [42], "bar": pd.Series([1, 2])},
+    {"bar": [42], "foo": pd.Series([1, 2])},
+]
+
+
+@pytest.mark.parametrize("fixture", EXAMPLES)
+def test_deep_equals_positive(fixture):
+    """Tests that deep_equals correctly identifies equal objects as equal."""
+    x = deepcopy(fixture)
+    y = deepcopy(fixture)
+
+    msg = (
+        f"deep_copy incorrectly returned False for two copies of "
+        f"the following object: {x}"
+    )
+    assert deep_equals(x, y), msg
+
+
+@pytest.mark.parametrize("fixture1", EXAMPLES)
+@pytest.mark.parametrize("fixture2", EXAMPLES)
+def test_deep_equals_negative(fixture1, fixture2):
+    """Tests that deep_equals correctly identifies unequal objects as unequal."""
+    x = deepcopy(fixture1)
+    y = deepcopy(fixture2)
+
+    msg = (
+        f"deep_copy incorrectly returned True when comparing "
+        f"the following different objects: x={x}, y={y}"
+    )
+    assert not deep_equals(x, y), msg
