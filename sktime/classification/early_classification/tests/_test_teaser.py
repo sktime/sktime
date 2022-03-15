e6dfba22b91c1acae938f2c44c6ab8e13d8fa600
Matthew Middlehurst <pfm15hbu@uea.ac.uk>
2022-03-15 10:26:25 +0000

    teaser near test

--- sktime/classification/early_classification/tests/_test_teaser.py
+++ sktime/classification/early_classification/tests/_test_teaser.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """TEASER test code."""
 import numpy as np
+import pytest
 from numpy import testing
 from sklearn.ensemble import IsolationForest
 
@@ -85,30 +86,23 @@ def test_teaser_near_classification_points():
     # train probability threshold
     teaser = TEASER(
         random_state=0,
-        classification_points=[14, 16, 18, 20, 24],
+        classification_points=[6, 10, 16, 24],
         estimator=TimeSeriesForestClassifier(n_estimators=10, random_state=0),
-        one_class_classifier=IsolationForest(n_estimators=5),
-        one_class_param_grid={"bootstrap": [True, False]},
     )
     teaser.fit(X_train, y_train)
 
-    final_probas = np.zeros((10, 2))
-    final_decisions = np.zeros(10)
-
     # use test_points that are not within list above
-    test_points = [15, 17, 19, 21, 24]
+    test_points = [7, 10, 17, 18]
+
     X_test = from_nested_to_3d_numpy(X_test)
     states = None
     for i in test_points:
         X = X_test[indices, :, :i]
-        probas, decisions, states = teaser.predict_proba(X, state_info=states)
-
-        for n in range(10):
-            if decisions[n] and final_decisions[n] == 0:
-                final_probas[n] = probas[n]
-                final_decisions[n] = i
-
-    testing.assert_array_equal(final_probas, test_near_classification_points)
+        if i == 18:
+            with pytest.raises(ValueError):
+                probas, decisions, states = teaser.predict_proba(X, state_info=states)
+        else:
+            probas, decisions, states = teaser.predict_proba(X, state_info=states)
 
 
 teaser_unit_test_probas = np.array(
@@ -137,21 +131,6 @@ teaser_if_unit_test_probas = np.array(
         [1.0, 0.0],
         [0.2, 0.8],
         [0.9, 0.1],
-        [1.0, 0.0],
-    ]
-)
-
-test_near_classification_points = np.array(
-    [
-        [0.1, 0.9],
-        [0.7, 0.3],
-        [0.0, 1.0],
-        [1.0, 0.0],
-        [0.9, 0.1],
-        [1.0, 0.0],
-        [0.9, 0.1],
-        [0.2, 0.8],
-        [0.6, 0.4],
         [1.0, 0.0],
     ]
 )
