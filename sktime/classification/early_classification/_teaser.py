update jpuyter notebook, correct earliness computation

sktime/classification/early_classification/_teaser.py
@@ -592,13 +592,17 @@ class TEASER(BaseClassifier):
                 for i in range(len(state_info))
             ]
         )
-        earliness = 1 - np.average(
+        earliness = np.average(
             [
                 self._classification_points[state_info[i, 0]] / series_length
                 for i in range(len(state_info))
             ]
         )
-        return (2 * accuracy * earliness) / (accuracy + earliness), accuracy, earliness
+        return (
+            (2 * accuracy * (1 - earliness)) / (accuracy + (1 - earliness)),
+            accuracy,
+            earliness,
+        )
 
     @staticmethod
     def _update_state_info(acccept_decision, preds, state_info, idx, time_stamp):

sktime/classification/early_classification/tests/_test_teaser.py
@@ -118,9 +118,10 @@ def test_teaser_full_length():
 
     hm, acc, earl = teaser.score(X_test, y_test)
     testing.assert_allclose(acc, 0.818182, rtol=0.01)
-    testing.assert_allclose(earl, 0.2121212, rtol=0.01)
+    testing.assert_allclose(earl, 0.787878, rtol=0.01)
+
     testing.assert_allclose(teaser._train_accuracy, 0.9, rtol=0.01)
-    testing.assert_allclose(teaser._train_earliness, 0.266, rtol=0.01)
+    testing.assert_allclose(teaser._train_earliness, 0.7333, rtol=0.01)
 
 
 teaser_unit_test_probas = np.array(