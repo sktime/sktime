7d9caf8177bf91267973c7478f39d7d7aa4eb25b
Matthew Middlehurst <pfm15hbu@uea.ac.uk>
2022-03-18 14:42:25 +0000

    SCITYPES_WITHOUT_TAGS edit. I thought this was a data thing not a base class one

--- sktime/registry/tests/test_lookup.py
+++ sktime/registry/tests/test_lookup.py
@@ -20,7 +20,7 @@ VALID_SCITYPES_SET = set(
 )
 
 # some scitypes have no associated tags yet
-SCITYPES_WITHOUT_TAGS = ["series-annotator", "clusterer"]
+SCITYPES_WITHOUT_TAGS = ["series-annotator", "clusterer", "early_classifier"]
 
 # shorthands for easy reading
 b = BASE_CLASS_SCITYPE_LIST
