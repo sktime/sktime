1a7752587a7a304d0fcfe27cfe5bc7aab9f0f2a4
ubuntu <fede.garza.ramirez@gmail.com>
2022-03-19 03:51:22 +0000

    fix: add correct module for statsforecast (check deps)

--- build_tools/azure/check_soft_dependencies.py
+++ build_tools/azure/check_soft_dependencies.py
@@ -20,7 +20,7 @@ SOFT_DEPENDENCIES = {
     "sktime.benchmarking.experiments": ["tsfresh", "esig"],
     "sktime.forecasting.all": ["pmdarima", "prophet", "tbats"],
     "sktime.forecasting.arima": ["pmdarima"],
-    "sktime.forecasting.arima_statsforecast": ["statsforecast"],
+    "sktime.forecasting.statsforecast": ["statsforecast"],
     "sktime.forecasting.hcrystalball": ["hcrystalball"],
     "sktime.forecasting.tbats": ["tbats"],
     "sktime.forecasting.bats": ["tbats"],
