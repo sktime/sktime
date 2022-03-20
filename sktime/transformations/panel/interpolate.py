7d1642deb9dc13b27c706eac4df721493bb07551
Franz Király <f.kiraly@ucl.ac.uk>
2022-03-20 13:52:55 +0000

    refactored TSinterpolator

--- sktime/transformations/panel/interpolate.py
+++ sktime/transformations/panel/interpolate.py
@@ -1,23 +1,41 @@
 # -*- coding: utf-8 -*-
+"""Time series interpolator/re-sampler."""
 import numpy as np
 from scipy import interpolate
 
-from sktime.transformations.base import _PanelToPanelTransformer
+from sktime.transformations.base import BaseTransformer
 from sktime.utils.validation.panel import check_X
 
 
-class TSInterpolator(_PanelToPanelTransformer):
-    """Transformer that rescales series for another number of points.
+__author__ = ["mloning"]
+
+
+class TSInterpolator(BaseTransformer):
+    """Time series interpolator/re-sampler.
+
+    Transformer that rescales series for another number of points.
     For each cell in dataframe transformer fits scipy linear interp1d
     and samples user defined number of points. Points are generated
     by numpy.linspace. After transformation each cell will be a numpy.array
     of defined size.
     """
 
+    _tags = {
+        "scitype:transform-input": "Series",
+        # what is the scitype of X: Series, or Panel
+        "scitype:transform-output": "Series",
+        # what scitype is returned: Primitives, Series, Panel
+        "scitype:instancewise": False,  # is this an instance-wise transform?
+        "X_inner_mtype": "nested_univ",  # which mtypes do _fit/_predict support for X?
+        "y_inner_mtype": "None",  # which mtypes do _fit/_predict support for X?
+        "fit_is_empty": True,
+    }
+
     _required_parameter = ["length"]
 
     def __init__(self, length):
-        """
+        """Initialize estimator.
+
         Parameters
         ----------
         length : integer, the length of time series to resize to.
@@ -29,7 +47,9 @@ class TSInterpolator(_PanelToPanelTransformer):
         super(TSInterpolator, self).__init__()
 
     def _resize_cell(self, cell):
-        """Resizes the array. Firstly 1d linear interpolation is fitted on
+        """Resize a single array.
+
+        Resizes the array. Firstly 1d linear interpolation is fitted on
            original array as y and numpy.linspace(0, 1, len(cell)) as x.
            Then user defined number of points is sampled in
            numpy.linspace(0, 1, length) and returned into cell as numpy array.
@@ -59,8 +79,8 @@ class TSInterpolator(_PanelToPanelTransformer):
         """
         return coll.apply(self._resize_cell)
 
-    def transform(self, X, y=None):
-        """Takes series in each cell, train linear interpolation and samples n.
+    def _transform(self, X, y=None):
+        """Take series in each cell, train linear interpolation and samples n.
 
         Parameters
         ----------
@@ -72,6 +92,4 @@ class TSInterpolator(_PanelToPanelTransformer):
         pandas DataFrame : Transformed pandas DataFrame with same number
                             of rows and columns
         """
-        self.check_is_fitted()
-        X = check_X(X, coerce_to_pandas=True)
         return X.apply(self._resize_col)
