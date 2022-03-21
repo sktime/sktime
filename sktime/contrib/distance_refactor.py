d3e2e34221384cef90008a53587f7da9dff797c6
Tony Bagnall <ajb@uea.ac.uk>
2022-03-21 13:11:03 +0000

    distance correctness

--- sktime/contrib/distance_refactor.py
+++ sktime/contrib/distance_refactor.py
@@ -3,7 +3,14 @@
 import numpy as np
 
 from sktime.datasets import load_basic_motions, load_unit_test
-from sktime.distances import dtw_distance
+from sktime.distances import (
+    ddtw_distance,
+    dtw_distance,
+    erp_distance,
+    lcss_distance,
+    msm_distance,
+    wdtw_distance,
+)
 
 
 def difference_test():
@@ -19,34 +26,28 @@ def difference_test():
     d2 = X_train[2]
     d3 = np.array([1, 2, 3, 4, 5, 6])
     d4 = np.array([3, 4, 5, 6, 7, 8])
-
-    dist1 = dtw_distance(d1, d2)
-    dist2 = dtw_distance(d3, d4)
+    #    d1 = d1.transpose()
+    #    d1 = d2.transpose()
+    print("Shape  = ", d1.shape)
+    name = "LCSS"
+    dist1 = lcss_distance(d1, d2, epsilon=0.0)
+    print(name, " g = 0 dist = ", dist1)
+    dist1 = lcss_distance(d1, d2, epsilon=50.0)
+    print(name, " g = 0.1. dist 1 = ", dist1)
+    dist1 = lcss_distance(d1, d2, epsilon=200.0)
+    print(name, " g window = 1 dist 1 = ", dist1)
     print(" SHAPE  = ", d1.shape)
-    print(
-        " 100% window distance 1D numpy input dist 1 = ", dist1, " distance 2 = ", dist2
-    )
-    dist1 = dtw_distance(d1, d2, window=0.1)
-    dist2 = dtw_distance(d3, d4, window=0.1)
-    print(" SHAPE  = ", d1.shape)
-    print(
-        " 10% window distance 1D numpy input dist 1 = ", dist1, " distance 2 = ", dist2
-    )
-    dist1 = dtw_distance(d1, d2, window=0.0)
-    dist2 = dtw_distance(d3, d4, window=0.0)
-    print(" SHAPE  = ", d1.shape)
-    print(
-        " 0% window distance 1D numpy input dist 1 = ", dist1, " distance 2 = ", dist2
-    )
     X_train, y_train = load_unit_test(split="train", return_type="numpy3d")
+    print(" SHAPE  = ", X_train[0].shape)
+
     c1 = X_train[0]
     c2 = X_train[1]
     c3 = np.array([[1, 2, 3, 4, 5, 6]])
     c4 = np.array([[3, 4, 5, 6, 7, 8]])
     dist1 = dtw_distance(c1, c2)
     dist2 = dtw_distance(c3, c4)
-    print(" SHAPE  = ", c1.shape)
-    print(" distance (1,m) input d1 = ", dist1, " distance 2 = ", dist2)
+    #    print(" SHAPE  = ", c1.shape)
+    #    print(" distance (1,m) input d1 = ", dist1, " distance 2 = ", dist2)
     X_train, y_train = load_basic_motions(split="train", return_type="numpy3d")
     b1 = X_train[0]
     b2 = X_train[1]
@@ -54,25 +55,27 @@ def difference_test():
     b4 = np.array([[3, 4, 5, 6, 7, 8], [1, 2, 3, 4, 5, 6]])
     dist1 = dtw_distance(b1, b2)
     dist2 = dtw_distance(b3, b4)
-    print(" SHAPE  = ", b1.shape)
-    print(" distance (d,m) input d1 = ", dist1, " distance 2 = ", dist2)
+    #   print(" 3D load SHAPE  = ", b1.shape)
+    #    print(" distance (d,m) input d1 = ", dist1, " distance 2 = ", dist2)
     a1 = c1.transpose()
     a2 = c2.transpose(1, 0)
     a3 = b1.transpose()
     a4 = b1.transpose()
-    print(" Wrong shape univariate = ", a1.shape)
-    print(" Wrong shape multi = ", a3.shape)
+    #    print(" Wrong shape univariate = ", a1.shape)
+    #    print(" Wrong shape multi = ", a3.shape)
     dist1 = dtw_distance(a1, a2)
-    print(" distance 1 = ", dist1)
+    #    print(" distance 1 = ", dist1)
     dist2 = dtw_distance(a3, a4)
-    print(" distance 2 = ", dist2)
+    #   print(" distance 2 = ", dist2)
     X_train, y_train = load_basic_motions(split="train", return_type="numpy3d")
     dist2 = dtw_distance(X_train[0], X_train[1], window=0.0)
-    print(" Window = 0.0, BASIC MOTIONS DIST = ", dist2)
+    #   print(" Window = 0.0, BASIC MOTIONS DIST = ", dist2)
     dist2 = dtw_distance(X_train[0], X_train[1], window=0.1)
-    print(" Window = 0.1, BASIC MOTIONS DIST = ", dist2)
+    #   print(" Window = 0.1, BASIC MOTIONS DIST = ", dist2)
     dist2 = dtw_distance(X_train[0], X_train[1], window=1.0)
-    print(" Window = 1, BASIC MOTIONS DIST = ", dist2)
+
+
+#  print(" Window = 1, BASIC MOTIONS DIST = ", dist2)
 
 
 if __name__ == "__main__":
--- sktime/distances/tests/test_distance_correctness.py
+++ sktime/distances/tests/test_distance_correctness.py
@@ -4,27 +4,51 @@
 Compare the distance calculations on the 1D and 2D (d,m) format input against the
 results generated with tsml, LINK TO CODE.
 """
-from sktime.datasets import load_basic_motions, load_unit_test
-from sktime.distances import dtw_distance
+from numpy.testing import assert_almost_equal
 
-distance_functions = [
-    "euclidean",
+from sktime.datasets import load_unit_test
+from sktime.distances import (  # edr_distance,
+    ddtw_distance,
+    dtw_distance,
+    erp_distance,
+    lcss_distance,
+    msm_distance,
+    wdtw_distance,
+)
+
+distances = [
     "dtw",
     "wdtw",
     "msm",
-    "erp",
-    "lcss",
     "ddtw",
-    "wddtw",
 ]
+distance_functions = {
+    "dtw": dtw_distance,
+    "wdtw": wdtw_distance,
+    "msm": msm_distance,
+    "ddtw": ddtw_distance
+    #    "euclidean",
+    #    "erp",
+    #    "lcss",
+    #    "ddtw",
+    #    "wddtw",
+}
 
+distance_parameters = {
+    "dtw": [0.0, 0.1, 1.0],  # window
+    "wdtw": [0.0, 0.1, 1.0],  # parameter g
+    "msm": [0.0, 0.1, 1.0],  # parameter c
+    "erp": [0.0, 0.1, 1.0],  # window
+    "lcss": [0.0, 50.0, 200.0],  # window
+    "ddtw": [0.0, 0.1, 1.0],
+}
 unit_test_distances = {
     "euclidean": 0.0,
-    "dtw": [275854.0, 315012.0, 384147.0],
-    "wdtw": 0.0,
-    "msm": 0.0,
-    "erp": 0.0,
-    "lcss": 0.0,
+    "dtw": [384147.0, 315012.0, 275854.0],
+    "wdtw": [137927.0, 68406.15849, 2.2296],
+    "msm": [1515.0, 1516.4, 1529.0],
+    "erp": [168.0, 1172.0, 2275.0],
+    "lcss": [1.0, 0.45833, 0.08333],
     "ddtw": 0.0,
     "wddtw": 0.0,
 }
@@ -40,25 +64,26 @@ basic_motions_distances = {
 }
 
 
-def test_dtw():
+def test_correctness():
     """Test dtw correctness."""
     trainX, trainy = load_unit_test(return_type="numpy3D")
-    d = dtw_distance(trainX[0], trainX[2], window=1.0)
-    assert d == unit_test_distances["dtw"][0]
-    d = dtw_distance(trainX[0], trainX[2], window=0.1)
-    assert d == unit_test_distances["dtw"][1]
-    d = dtw_distance(trainX[0], trainX[2], window=0.0)
-    assert d == unit_test_distances["dtw"][2]
-    trainX, trainy = load_unit_test(return_type="numpy2D")
-    d = dtw_distance(trainX[0], trainX[2], window=1.0)
-    assert d == unit_test_distances["dtw"][0]
-    d = dtw_distance(trainX[0], trainX[2], window=0.1)
-    assert d == unit_test_distances["dtw"][1]
-    d = dtw_distance(trainX[0], trainX[2], window=0.0)
-    assert d == unit_test_distances["dtw"][2]
-    trainX, trainy = load_basic_motions(return_type="numpy3D")
-    d = dtw_distance(trainX[0], trainX[2], window=1.0)
-    assert d == unit_test_distances["dtw"][0]
-    d = dtw_distance(trainX[0], trainX[2], window=0.1)
-    assert d == unit_test_distances["dtw"][1]
-    d = dtw_distance(trainX[0], trainX[2], window=0.0)
+    trainX2, trainy2 = load_unit_test(return_type="numpy2D")
+    #    trainX3, trainy3 = load_basic_motions(return_type="numpy3D")
+    # Test instances from UnitTest
+    cases1 = [trainX[0], trainX2[0]]
+    cases2 = [trainX[2], trainX2[2]]
+    # Add test cases1 and 2 are the same
+    for i in range(0, len(cases1)):
+        for j in range(0, 3):
+            d = dtw_distance(cases1[i], cases2[i], window=distance_parameters["dtw"][j])
+            assert_almost_equal(d, unit_test_distances["dtw"][j], 4)
+            d = wdtw_distance(cases1[i], cases2[i], g=distance_parameters["wdtw"][j])
+            assert_almost_equal(d, unit_test_distances["wdtw"][j], 4)
+            d = msm_distance(cases1[i], cases2[i], c=distance_parameters["msm"][j])
+            assert_almost_equal(d, unit_test_distances["msm"][j], 4)
+            d = erp_distance(cases1[i], cases2[i], window=distance_parameters["erp"][j])
+            assert_almost_equal(d, unit_test_distances["erp"][j], 4)
+            d = lcss_distance(
+                cases1[i], cases2[i], epsilon=distance_parameters["lcss"][j]
+            )
+            assert_almost_equal(d, unit_test_distances["lcss"][j], 4)
