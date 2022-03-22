603c7eed6defb60c827d38c7d9db3048529d833a
ubuntu <fede.garza.ramirez@gmail.com>
2022-03-22 23:52:44 +0000

    fix: match StatsForecastAutoARIMA params names with AutoARIMA

--- sktime/forecasting/statsforecast.py
+++ sktime/forecasting/statsforecast.py
@@ -23,49 +23,80 @@ class StatsForecastAutoARIMA(_StatsForecastAdapter):
 
     Parameters
     ----------
+    start_p: int (default 2)
+        Starting value of p in stepwise procedure.
     d: int optional (default None)
         Order of first-differencing.
         If missing, will choose a value based on `test`.
+    start_q: int (default 2)
+        Starting value of q in stepwise procedure.
+    max_p: int (default 5)
+        Maximum value of p.
+    max_d: int (default 2)
+        Maximum number of non-seasonal differences
+    max_q: int (default 5)
+        Maximum value of q.
+    start_P: int (default 1)
+        Starting value of P in stepwise procedure.
     D: int optional (default None)
         Order of seasonal-differencing.
         If missing, will choose a value based on `season_test`.
-    max_p: int (default 5)
-        Maximum value of p.
-    max_q: int (default 5)
-        Maximum value of q.
+    start_Q: int (default 1)
+        Starting value of Q in stepwise procedure.
     max_P: int (default 2)
         Maximum value of P.
+    max_D: int (default 1)
+        Maximum number of seasonal differences
     max_Q: int (default 2)
         Maximum value of Q.
     max_order: int (default 5)
         Maximum value of p+q+P+Q if model selection is not stepwise.
-    max_d: int (default 2)
-        Maximum number of non-seasonal differences
-    max_D: int (default 1)
-        Maximum number of seasonal differences
-    start_p: int (default 2)
-        Starting value of p in stepwise procedure.
-    start_q: int (default 2)
-        Starting value of q in stepwise procedure.
-    start_P: int (default 1)
-        Starting value of P in stepwise procedure.
-    start_Q: int (default 1)
-        Starting value of Q in stepwise procedure.
-    stationary: bool (default False)
-        If True, restricts search to stationary models.
+    sp: int (default 1)
+        Number of observations per unit of time.
+        For example 24 for Hourly data.
     seasonal: bool (default True)
         If False, restricts search to non-seasonal models.
-    ic: str (default 'aicc')
+    stationary: bool (default False)
+        If True, restricts search to stationary models.
+    information_criterion: str (default 'aicc')
         Information criterion to be used in model selection.
+    test: str (default 'kpss')
+        Type of unit root test to use. See ndiffs for details.
+    seasonal_test: str (default 'seas')
+        This determines which method is used to select the number
+        of seasonal differences.
+        The default method is to use a measure of seasonal
+        strength computed from an STL decomposition.
+        Other possibilities involve seasonal unit root tests.
     stepwise: bool (default True)
         If True, will do stepwise selection (faster).
         Otherwise, it searches over all models.
         Non-stepwise selection can be very slow,
         especially for seasonal models.
-    nmodels: int (default 94)
-        Maximum number of models considered in the stepwise search.
+    n_jobs: int (default 2)
+        Allows the user to specify the amount of parallel processes to be used
+        if parallel = True and stepwise = False.
+        If None, then the number of logical cores is
+        automatically detected and all available cores are used.
+    trend: bool (default True)
+        If True, models with drift terms are considered.
+    method: str optional (default None)
+        fitting method: maximum likelihood or minimize conditional
+        sum-of-squares.
+        The default (unless there are missing values)
+        is to use conditional-sum-of-squares to find starting values,
+        then maximum likelihood. Can be abbreviated.
+    offset_test_args: dict optional (default None)
+        Additional arguments to be passed to the unit root test.
+    seasonal_test_args: dict optional (default None)
+        Additional arguments to be passed to the seasonal
+        unit root test. See nsdiffs for details.
     trace: bool (default False)
         If True, the list of ARIMA models considered will be reported.
+    n_fits: int (default 94)
+        Maximum number of models considered in the stepwise search.
+    with_intercept: bool (default True)
+        If True, models with a non-zero mean are considered.
     approximation: bool optional (default None)
         If True, estimation is via conditional sums of squares
         and the information criteria used for model
@@ -74,12 +105,6 @@ class StatsForecastAutoARIMA(_StatsForecastAdapter):
         maximum likelihood estimation.
         Approximation should be used for long time series
         or a high seasonal period to avoid excessive computation times.
-    method: str optional (default None)
-        fitting method: maximum likelihood or minimize conditional
-        sum-of-squares.
-        The default (unless there are missing values)
-        is to use conditional-sum-of-squares to find starting values,
-        then maximum likelihood. Can be abbreviated.
     truncate: bool optional (default None)
         An integer value indicating how many observations
         to use in model selection.
@@ -88,23 +113,6 @@ class StatsForecastAutoARIMA(_StatsForecastAdapter):
         and approximation=True.
         All observations are used if either truncate=None
         or approximation=False.
-    test: str (default 'kpss')
-        Type of unit root test to use. See ndiffs for details.
-    test_kwargs: str optional (default None)
-        Additional arguments to be passed to the unit root test.
-    seasonal_test: str (default 'seas')
-        This determines which method is used to select the number
-        of seasonal differences.
-        The default method is to use a measure of seasonal
-        strength computed from an STL decomposition.
-        Other possibilities involve seasonal unit root tests.
-    seasonal_test_kwargs: dict optional (default None)
-        Additional arguments to be passed to the seasonal
-        unit root test. See nsdiffs for details.
-    allowdrift: bool (default True)
-        If True, models with drift terms are considered.
-    allowmean: bool (default True)
-        If True, models with a non-zero mean are considered.
     blambda: float optional (default None)
         Box-Cox transformation parameter.
         If lambda="auto", then a transformation is automatically
@@ -121,14 +129,6 @@ class StatsForecastAutoARIMA(_StatsForecastAdapter):
         If True and stepwise = False, then the specification search
         is done in parallel.
         This can give a significant speedup on multicore machines.
-    num_cores: int (default 2)
-        Allows the user to specify the amount of parallel processes to be used
-        if parallel = True and stepwise = False.
-        If None, then the number of logical cores is
-        automatically detected and all available cores are used.
-    period: int (default 1)
-        Number of observations per unit of time.
-        For example 24 for Hourly data.
 
     Notes
     -----
@@ -142,75 +142,75 @@ class StatsForecastAutoARIMA(_StatsForecastAdapter):
 
     def __init__(
         self,
+        start_p: int = 2,
         d: Optional[int] = None,
-        D: Optional[int] = None,
+        start_q: int = 2,
         max_p: int = 5,
+        max_d: int = 2,
         max_q: int = 5,
+        start_P: int = 1,
+        D: Optional[int] = None,
+        start_Q: int = 1,
         max_P: int = 2,
+        max_D: int = 1,
         max_Q: int = 2,
         max_order: int = 5,
-        max_d: int = 2,
-        max_D: int = 1,
-        start_p: int = 2,
-        start_q: int = 2,
-        start_P: int = 1,
-        start_Q: int = 1,
-        stationary: bool = False,
+        sp: int = 1,
         seasonal: bool = True,
-        ic: str = "aicc",
-        stepwise: bool = True,
-        nmodels: int = 94,
-        trace: bool = False,
-        approximation: Optional[bool] = None,
-        method: Optional[str] = None,
-        truncate: Optional[bool] = None,
+        stationary: bool = False,
+        information_criterion: str = "aicc",
         test: str = "kpss",
-        test_kwargs: Optional[str] = None,
         seasonal_test: str = "seas",
-        seasonal_test_kwargs: Optional[Dict] = None,
-        allowdrift: bool = True,
-        allowmean: bool = True,
+        stepwise: bool = True,
+        n_jobs: int = 2,
+        trend: bool = True,
+        method: Optional[str] = None,
+        offset_test_args: Optional[str] = None,
+        seasonal_test_args: Optional[Dict] = None,
+        trace: bool = False,
+        n_fits: int = 94,
+        with_intercept: bool = True,
+        approximation: Optional[bool] = None,
+        truncate: Optional[bool] = None,
         blambda: Optional[float] = None,
         biasadj: bool = False,
         parallel: bool = False,
-        num_cores: int = 2,
-        period: int = 1,
     ):
         _check_soft_dependencies("statsforecast", severity="error", object=self)
 
+        self.start_p = start_p
         self.d = d
-        self.D = D
+        self.start_q = start_q
         self.max_p = max_p
+        self.max_d = max_d
         self.max_q = max_q
+        self.start_P = start_P
+        self.D = D
+        self.start_Q = start_Q
         self.max_P = max_P
+        self.max_D = max_D
         self.max_Q = max_Q
         self.max_order = max_order
-        self.max_d = max_d
-        self.max_D = max_D
-        self.start_p = start_p
-        self.start_q = start_q
-        self.start_P = start_P
-        self.start_Q = start_Q
-        self.stationary = stationary
+        self.sp = sp
         self.seasonal = seasonal
-        self.ic = ic
-        self.stepwise = stepwise
-        self.nmodels = nmodels
-        self.trace = trace
-        self.approximation = approximation
-        self.method = method
-        self.truncate = truncate
+        self.stationary = stationary
+        self.information_criterion = information_criterion
         self.test = test
-        self.test_kwargs = test_kwargs
         self.seasonal_test = seasonal_test
-        self.seasonal_test_kwargs = seasonal_test_kwargs
-        self.allowdrift = allowdrift
-        self.allowmean = allowmean
+        self.stepwise = stepwise
+        self.n_jobs = n_jobs
+        self.trend = trend
+        self.method = method
+        self.offset_test_args = offset_test_args
+        self.seasonal_test_args = seasonal_test_args
+        self.trace = trace
+        self.n_fits = n_fits
+        self.with_intercept = with_intercept
+        self.approximation = approximation
+        self.truncate = truncate
         self.blambda = blambda
         self.biasadj = biasadj
         self.parallel = parallel
-        self.num_cores = num_cores
-        self.period = period
 
         super(StatsForecastAutoARIMA, self).__init__()
 
@@ -234,24 +234,24 @@ class StatsForecastAutoARIMA(_StatsForecastAdapter):
             start_Q=self.start_Q,
             stationary=self.stationary,
             seasonal=self.seasonal,
-            ic=self.ic,
+            ic=self.information_criterion,
             stepwise=self.stepwise,
-            nmodels=self.nmodels,
+            nmodels=self.n_fits,
             trace=self.trace,
             approximation=self.approximation,
             method=self.method,
             truncate=self.truncate,
             test=self.test,
-            test_kwargs=self.test_kwargs,
+            test_kwargs=self.offset_test_args,
             seasonal_test=self.seasonal_test,
-            seasonal_test_kwargs=self.seasonal_test_kwargs,
-            allowdrift=self.allowdrift,
-            allowmean=self.allowmean,
+            seasonal_test_kwargs=self.seasonal_test_args,
+            allowdrift=self.trend,
+            allowmean=self.with_intercept,
             blambda=self.blambda,
             biasadj=self.biasadj,
             parallel=self.parallel,
-            num_cores=self.num_cores,
-            period=self.period,
+            num_cores=self.n_jobs,
+            period=self.sp,
         )
 
     @classmethod
