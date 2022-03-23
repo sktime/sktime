d397d5731a884856e23be01e9245a32e2153b99b
dionysisbacchus <100272830+dionysisbacchus@users.noreply.github.com>
2022-03-23 14:47:51 +0200

    test_param_refactor (#2273)

--- sktime/classification/kernel_based/_arsenal.py
+++ sktime/classification/kernel_based/_arsenal.py
@@ -409,3 +409,20 @@ class Arsenal(BaseClassifier):
             results[oob[n]][self._class_dictionary[pred]] += weight
 
         return results, weight, oob
+
+    @classmethod
+    def get_test_params(cls):
+        """Return testing parameter settings for the estimator.
+
+        Returns
+        -------
+        params : dict or list of dict, default={}
+            Parameters to create testing instances of the class.
+            Each dict are parameters to construct an "interesting" test instance, i.e.,
+            `MyClass(**params)` or `MyClass(**params[i])` creates a valid test instance.
+            `create_test_instance` uses the first (or only) dictionary in `params`.
+
+        """
+        params = {"num_kernels": 50, "n_estimators": 3}
+
+        return params
