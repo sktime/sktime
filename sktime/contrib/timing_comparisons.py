344f9156c42e0b4bc3e88edf762ade7dfe2ff91c
Tony Bagnall <ajb@uea.ac.uk>
2022-03-24 12:16:41 +0000

    downsize example in notebook

--- sktime/contrib/timing_comparisons.py
+++ sktime/contrib/timing_comparisons.py
@@ -9,8 +9,27 @@ __author__ = ["TonyBagnall"]
 
 import numpy as np
 
+from sktime.clustering.k_means import TimeSeriesKMeans
+from sktime.clustering.k_medoids import TimeSeriesKMedoids
+
 
 def distance_timing():
     """Time distance functions for increased series length."""
+    k_medoids = TimeSeriesKMedoids(
+        n_clusters=5,  # Number of desired centers
+        init_algorithm="forgy",  # Center initialisation technique
+        max_iter=10,  # Maximum number of iterations for refinement on training set
+        metric="dtw",  # Distance metric to use
+        random_state=1,
+    )
+    k_means = TimeSeriesKMeans(
+        n_clusters=5,  # Number of desired centers
+        init_algorithm="forgy",  # Center initialisation technique
+        max_iter=10,  # Maximum number of iterations for refinement on training set
+        metric="dtw",  # Distance metric to use
+        random_state=1,
+    )
     for i in range(100):
-        first = np.ndarray()
+        first = np.ndarray((100, 100 * (i + 1)))
+        k_means.fit()
+        k_medoids.fit()
