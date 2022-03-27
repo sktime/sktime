f767c61d51042ebd13da3524e9abacbaeb842806
Franz Király <f.kiraly@ucl.ac.uk>
2022-03-27 20:19:39 +0100

    fixed _has_capability

--- sktime/utils/_testing/estimator_checks.py
+++ sktime/utils/_testing/estimator_checks.py
@@ -6,7 +6,7 @@ copyright: sktime developers, BSD-3-Clause License (see LICENSE file)
 
 __author__ = ["mloning", "fkiraly"]
 
-from inspect import signature
+from inspect import isclass, signature
 
 import numpy as np
 import pandas as pd
@@ -329,18 +329,28 @@ def _get_args(function, varargs=False):
 
 def _has_capability(est, method: str) -> bool:
     """Check whether estimator has capability of method."""
+
+    def get_tag(est, tag_name, tag_value_default=None):
+        if isclass(est):
+            return est.get_class_tag(
+                tag_name=tag_name, tag_value_default=tag_value_default
+            )
+        else:
+            return est.get_tag(tag_name=tag_name, tag_value_default=tag_value_default)
+
     if not hasattr(est, method):
         return False
     if method == "inverse_transform":
-        return est.get_class_tag("capability:inverse_transform", False)
+        return get_tag(est, "capability:inverse_transform", False)
     if method in [
         "predict_proba",
         "predict_interval",
         "predict_quantiles",
         "predict_var",
     ]:
-        # all classifiers implement predict_proba
-        if method == "predict_proba" and isinstance(est, BaseClassifier):
+        ALWAYS_HAVE_PREDICT_PROBA = (BaseClassifier, BaseClusterer)
+        # all classifiers and clusterers implement predict_proba
+        if method == "predict_proba" and isinstance(est, ALWAYS_HAVE_PREDICT_PROBA):
             return True
-        return est.get_class_tag("capability:pred_int", False)
+        return get_tag(est, "capability:pred_int", False)
     return True
