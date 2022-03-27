82490b715e62fd75fbb8d8bc240e08e3f31c1258
Franz Király <f.kiraly@ucl.ac.uk>
2022-03-27 21:38:56 +0100

    remove inverse_transform tag

--- sktime/transformations/compose.py
+++ sktime/transformations/compose.py
@@ -476,7 +476,7 @@ class FeatureUnion(BaseTransformer, _HeterogenousMetaEstimator):
         self._anytagis_then_set("fit_is_empty", False, True, ests)
         self._anytagis_then_set("transform-returns-same-time-index", False, True, ests)
         self._anytagis_then_set("skip-inverse-transform", True, False, ests)
-        self._anytagis_then_set("capability:inverse_transform", False, True, ests)
+        # self._anytagis_then_set("capability:inverse_transform", False, True, ests)
         self._anytagis_then_set("handles-missing-data", False, True, ests)
         self._anytagis_then_set("univariate-only", True, False, ests)
 
