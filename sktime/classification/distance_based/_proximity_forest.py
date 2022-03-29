8d6c9a1d2ad748dba81b420b095f94bd180795d6
Matthew <m.middlehurst@uea.ac.uk>
2022-03-29 14:37:21 +0100

    pf max depth

--- sktime/classification/distance_based/_proximity_forest.py
+++ sktime/classification/distance_based/_proximity_forest.py
@@ -1038,7 +1038,7 @@ class ProximityTree(BaseClassifier):
     >>> from sktime.datasets import load_unit_test
     >>> X_train, y_train = load_unit_test(split="train", return_X_y=True)
     >>> X_test, y_test = load_unit_test(split="test", return_X_y=True)
-    >>> clf = ProximityTree()
+    >>> clf = ProximityTree(max_depth=4)
     >>> clf.fit(X_train, y_train)
     ProximityTree(...)
     >>> y_pred = clf.predict(X_test)
@@ -1291,7 +1291,7 @@ class ProximityForest(BaseClassifier):
     >>> from sktime.datasets import load_unit_test
     >>> X_train, y_train = load_unit_test(split="train", return_X_y=True)
     >>> X_test, y_test = load_unit_test(split="test", return_X_y=True)
-    >>> clf = ProximityForest(n_estimators=2)
+    >>> clf = ProximityForest(n_estimators=2, max_depth=4)
     >>> clf.fit(X_train, y_train)
     ProximityForest(...)
     >>> y_pred = clf.predict(X_test)
