291f3ea0eb0bc5b9c2e14035b4a0d541b082304d
Franz Király <f.kiraly@ucl.ac.uk>
2022-04-02 02:08:27 +0100

    test for error changed to test for warning

--- sktime/classification/tests/test_base.py
+++ sktime/classification/tests/test_base.py
@@ -73,8 +73,8 @@ class _DummyConvertPandas(BaseClassifier):
 
 
 multivariate_message = r"X must be univariate, this classifier cannot deal with"
-missing_message = r"The data has missing values"
-unequal_message = r"The data has unequal length series"
+missing_message = r"has missing values"
+unequal_message = r"has unequal length series"
 incorrect_X_data_structure = r"must be a np.array or a pd.Series"
 incorrect_y_data_structure = r"must be 1-dimensional"
 
@@ -130,7 +130,7 @@ def test_check_capabilities():
     handles_none = _DummyClassifier()
 
     handles_none._check_capabilities(False, False, False)
-    with pytest.raises(ValueError, match=missing_message):
+    with pytest.warns(UserWarning, match=missing_message):
         handles_none._check_capabilities(True, True, True)
         handles_none._check_capabilities(True, True, False)
         handles_none._check_capabilities(True, False, False)
@@ -139,7 +139,7 @@ def test_check_capabilities():
         handles_none._check_capabilities(False, True, True)
         handles_none._check_capabilities(False, True, False)
         handles_none._check_capabilities(False, False, True)
-    with pytest.raises(ValueError, match=unequal_message):
+    with pytest.warns(UserWarning, match=unequal_message):
         handles_none._check_capabilities(False, False, True)
 
     handles_all = _DummyHandlesAllInput()
