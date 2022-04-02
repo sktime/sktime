449df7a715395ecea6544c7fa5a2278244c0a446
Franz Király <f.kiraly@ucl.ac.uk>
2022-04-02 17:06:22 +0100

    HOG1D tag

--- sktime/transformations/panel/hog1d.py
+++ sktime/transformations/panel/hog1d.py
@@ -46,7 +46,7 @@ class HOG1DTransformer(BaseTransformer):
         # what is the scitype of X: Series, or Panel
         "scitype:transform-output": "Series",
         # what scitype is returned: Primitives, Series, Panel
-        "scitype:instancewise": False,  # is this an instance-wise transform?
+        "scitype:instancewise": True,  # is this an instance-wise transform?
         "X_inner_mtype": "nested_univ",  # which mtypes do _fit/_predict support for X?
         "y_inner_mtype": "None",  # which mtypes do _fit/_predict support for X?
         "fit_is_empty": True,
