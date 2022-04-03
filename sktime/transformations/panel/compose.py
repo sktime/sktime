8c0537d2eb28a3414085d88f0704b1c8f8f98340
Franz Király <f.kiraly@ucl.ac.uk>
2022-04-03 13:20:17 +0100

    don't skip fit

--- sktime/transformations/panel/compose.py
+++ sktime/transformations/panel/compose.py
@@ -254,6 +254,8 @@ class _RowTransformer(BaseTransformer):
         self.check_transformer = check_transformer
         super(_RowTransformer, self).__init__()
         self.clone_tags(transformer)
+        # fit needs to be run, or the internal fit may not be updated
+        self.set_tags("fit_is_empty", False)
 
     def _fit(self, *args, **kwargs):
         """Fit to the data."""
