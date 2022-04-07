c0fe0d75e1641e73d0e6002ffb5c04d9185944a9
Franz Király <f.kiraly@ucl.ac.uk>
2022-04-08 00:03:46 +0100

    corrected "not" in docstring

--- sktime/utils/tests/test_check_estimator.py
+++ sktime/utils/tests/test_check_estimator.py
@@ -27,7 +27,7 @@ def test_check_estimator_passed(estimator_class):
 
 @pytest.mark.parametrize("estimator_class", EXAMPLE_CLASSES)
 def test_check_estimator_does_not_raise(estimator_class):
-    """Test that check_estimator does raise when asked to raise exceptions."""
+    """Test that check_estimator does not raise when asked to raise exceptions."""
     estimator_instance = estimator_class.create_test_instance()
 
     check_estimator(estimator_class, return_exceptions=False, verbose=False)
