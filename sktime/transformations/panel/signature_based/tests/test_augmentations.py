93f4d1c6cc60886db62322fd506671daf0529131
Franz Király <f.kiraly@ucl.ac.uk>
2022-04-08 14:53:00 +0100

    [ENH] Transformers module full refactor - part IV, `panel` module (3rd batch) (#2369)
    This PR refactors more transformers in the `transformations` module to the new interface, and continues #2253.
    This is part IV - working on the `transformations.panel` module.
    
    For all transformers in this PR, the following was carried out:
    * inheritance parent is now `BaseTransformer`
    * adding tags as appropriate
    * moving logic to underscore methods `_fit`, `_transform`, and `_inverse_transform` (where applicable)
    * removal of check/conversion logic which is already covered by `BaseTransformer`
    * linting and cleaning up docstrings

--- sktime/transformations/panel/signature_based/tests/test_augmentations.py
+++ sktime/transformations/panel/signature_based/tests/test_augmentations.py
@@ -1,32 +1,39 @@
 # -*- coding: utf-8 -*-
+"""Test the signature module augmentations."""
 import numpy as np
+
 from sktime.transformations.panel.signature_based._augmentations import (
     _AddTime,
     _BasePoint,
-    _LeadLag,
     _CumulativeSum,
     _InvisibilityReset,
+    _LeadLag,
     _make_augmentation_pipeline,
 )
 
 
 def test_augmentations():
+    """Test the signature module augmentations."""
     # Build an array X, note that this is [n_sample, n_channels, length] shape.
     X = np.array([[1, 2], [1, 1], [1, 4]]).reshape(-1, 3, 2)
+    X = np.swapaxes(X, 1, 2)
 
     # Leadlag
     leadlag = np.array(
         [[[1, 2, 1, 2], [1, 1, 1, 2], [1, 1, 1, 1], [1, 4, 1, 1], [1, 4, 1, 4]]]
     )
-    assert np.allclose(_LeadLag().transform(X), leadlag)
+    leadlag = np.swapaxes(leadlag, 1, 2)
+    assert np.allclose(_LeadLag().fit_transform(X), leadlag)
 
     # Add time
     addtime = np.array([[[0.0, 1.0, 2.0], [0.5, 1.0, 1.0], [1.0, 1.0, 4.0]]])
-    assert np.allclose(_AddTime().transform(X), addtime)
+    addtime = np.swapaxes(addtime, 1, 2)
+    assert np.allclose(_AddTime().fit_transform(X), addtime)
 
     # Basepoint
     basepoint = np.array([[[0.0, 0.0], [1.0, 2.0], [1.0, 1.0], [1.0, 4.0]]])
-    assert np.allclose(_BasePoint().transform(X), basepoint)
+    basepoint = np.swapaxes(basepoint, 1, 2)
+    assert np.allclose(_BasePoint().fit_transform(X), basepoint)
 
     # Invisibility reset
     ir = np.array(
@@ -40,11 +47,13 @@ def test_augmentations():
             ]
         ]
     )
-    assert np.allclose(_InvisibilityReset().transform(X), ir)
+    ir = np.swapaxes(ir, 1, 2)
+    assert np.allclose(_InvisibilityReset().fit_transform(X), ir)
 
     # Cumulative sum
     cumsum = np.array([[[1, 2], [2, 3], [3, 7]]])
-    assert np.allclose(_CumulativeSum().transform(X), cumsum)
+    cumsum = np.swapaxes(cumsum, 1, 2)
+    assert np.allclose(_CumulativeSum().fit_transform(X), cumsum)
 
     # Test multiple augmentations
     pipeline = _make_augmentation_pipeline(["leadlag", "addtime"])
@@ -59,4 +68,5 @@ def test_augmentations():
             ]
         ]
     )
-    assert np.allclose(pipeline.transform(X), lladdtime)
+    lladdtime = np.swapaxes(lladdtime, 1, 2)
+    assert np.allclose(pipeline.fit_transform(X), lladdtime)
