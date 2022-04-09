0f5f19e4b8e857bf25c3bf8301a650f0fe7fddd0
Chris Holder <chrisholder987@hotmail.com>
2022-04-09 22:59:05 +0200

    enh(distances): dtw path

--- sktime/distances/base/_types.py
+++ sktime/distances/base/_types.py
@@ -8,13 +8,13 @@ __all__ = [
     "ValidCallableTypes",
 ]
 
-from typing import Callable, Union
+from typing import Callable, Union, List
 
 import numpy as np
 
 # Callable types
 DistanceCallable = (Callable[[np.ndarray, np.ndarray], float],)
-DistancePathCallable = (Callable[[np.ndarray, np.ndarray], Union[np.ndarray, float]])
+DistancePathCallable = (Callable[[np.ndarray, np.ndarray], Union[List, float]])
 DistanceFactoryCallable = Callable[
     [np.ndarray, np.ndarray, dict], Callable[[np.ndarray, np.ndarray], float]
 ]
