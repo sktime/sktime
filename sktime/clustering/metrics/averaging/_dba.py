ce945246c61a8133e8c20d95040b23d7bf1d134c
Chris Holder <chrisholder987@hotmail.com>
2022-04-09 23:14:21 +0200

    feat(clustering) basic dba implementation

--- sktime/clustering/metrics/averaging/_dba.py
+++ sktime/clustering/metrics/averaging/_dba.py
@@ -23,6 +23,7 @@ def dba(X: np.ndarray, iterations = 30):
     path_callable = distance_path_factory(X[0], X[1], metric='dtw')
     for i in range(iterations):
         center = _dba_update(center, X, path_callable)
+    return center
 
 
 
@@ -41,17 +42,17 @@ def _dba_update(center: np.ndarray, X: np.ndarray, path_callable):
     np.ndarray (2d array of shape (n_dimensions, series_length)
         The time series that is the computed average series.
     """
-    X_size = len(X)
-    alignment = np.zeros(X_size)
+    X_size, X_timepoints, X_dims = X.shape
+    alignment = []
     for i in range(X_size):
         curr_ts = X[i]
         curr_alignment, _ = path_callable(center, curr_ts)
         for j in range(len(curr_alignment)):
-            joe = curr_alignment[j]
-            test = X[curr_alignment[j]]
+            if len(alignment) <= j:
+                alignment.append(np.zeros(X_dims))
             alignment[j] += X[curr_alignment[j]]
 
-    alignment = alignment/X_size
+    alignment = np.array(alignment)/X_size
     return alignment
 
 
--- sktime/clustering/tests/metrics/test_dba.py
+++ sktime/clustering/tests/metrics/test_dba.py
@@ -4,10 +4,12 @@ import numpy as np
 from sktime.clustering.metrics.medoids import medoids
 from sktime.clustering.metrics.averaging._dba import dba
 from sktime.distances.tests._utils import create_test_distance_numpy
+import matplotlib.pyplot as plt
 
 
 def test_dba():
     """Test medoids."""
     X = create_test_distance_numpy(10, 3, 3, random_state=2)
     test_dba = dba(X)
+    test_medoids = medoids(X)
     joe = ''
