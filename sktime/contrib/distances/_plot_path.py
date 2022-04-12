36f58cd930260f4992e05a51616ae937701cf64e
chris holder <chrisholder987@hotmail.com>
2022-04-12 11:02:31 +0100

    enh(contrib): basic plotting stuff

--- sktime/contrib/distances/_plot_path.py
+++ sktime/contrib/distances/_plot_path.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from sktime.distances import distance_path
 
 x = np.array([
@@ -29,8 +30,51 @@ y = np.array([
 
 METRIC = 'dtw'
 
+def plot_cost_matrix_color_map(cost_matrix):
+    plt.rcParams["figure.figsize"] = [7.50, 3.50]
+    plt.rcParams["figure.autolayout"] = True
+
+    fig, ax = plt.subplots()
+    min_val, max_val = 0, cost_matrix.shape[0]
+    ax.matshow(cost_matrix, cmap='ocean')
+
+    for i in range(max_val):
+        for j in range(max_val):
+            c = cost_matrix[j, i]
+            ax.text(i, j, str(round(c, 2)), va='center', ha='center', size=6)
+    plt.show()
+
+def plot_cost_matrix_color_map_path(cost_matrix, path):
+    plt.rcParams["figure.figsize"] = [7.50, 3.50]
+    plt.rcParams["figure.autolayout"] = True
+
+    fig, ax = plt.subplots()
+    min_val, max_val = 0, cost_matrix.shape[0]
+
+    plot_matrix = np.zeros_like(cost_matrix)
+
+    for i in range(max_val):
+        for j in range(max_val):
+            if (i, j) in path:
+                plot_matrix[i, j] = 1.0
+            else:
+                plot_matrix[i, j] = 0.0
+
+    ax.matshow(plot_matrix, cmap='ocean')
+
+    for i in range(max_val):
+        for j in range(max_val):
+            c = cost_matrix[i, j]
+            ax.text(i, j, str(round(c, 2)), va='center', ha='center', size=6)
+    plt.show()
+
+
 def plot_paths():
-    path, distance = distance_path(x, y, metric=METRIC)
+    path, distance, cost_matrix = \
+        distance_path(x, y, metric=METRIC, return_cost_matrix=True)
+    # plot_cost_matrix_color_map(cost_matrix)
+    plot_cost_matrix_color_map_path(cost_matrix, path)
+
     joe = ''
     pass
 
