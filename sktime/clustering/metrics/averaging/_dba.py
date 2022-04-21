d977e5d54ab1c0c24fc41cccde9acdfa8e12fb15
chris holder <chrisholder987@hotmail.com>
2022-04-21 11:09:12 +0100

    bug(clustering): merged file properly

--- sktime/clustering/metrics/averaging/_dba.py
+++ sktime/clustering/metrics/averaging/_dba.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
+__author__ = ['chrisholder']
+
 import numpy as np
 from numba import njit
 
 from sktime.clustering.metrics.medoids import medoids
-from sktime.distances._distance import distance_path_factory
+from sktime.distances._distance import distance_alignment_path_factory
 from tslearn.metrics import dtw_path
 from scipy.interpolate import interp1d
+from sktime.distances.base import DistanceAlignmentPathCallable
+
 
 def _init_avg(_X, barycenter_size):
     X = _X.copy()
@@ -24,50 +28,71 @@ def _init_avg(_X, barycenter_size):
     return result
 
 
-
-def _dba(X: np.ndarray, iterations=50, distance_metric='dtw'):
+def dba(
+        X: np.ndarray,
+        iterations=30,
+        averaging_distance_metric='dtw',
+        **kwargs
+) -> np.ndarray:
     """Compute the dtw barycenter average of time series.
 
     Parameters
     ----------
-    X : np.ndarray (3d array of shape (n_instances, n_dimensions, series_length))
+    X : np.ndarray (3d array of shape (n, m, p) where n is number of instances, m
+                    is the dimensions and p is the timepoints))
         Time series instances compute average from.
-    iterations: int
+    iterations: int, defaults = 30
         Number iterations for dba to update over.
+    distance_metric: str, defaults = 'dtw'
+        String that is the distance metric to derive the distance alignment path.
+    distance_params: dict, defaults = {}
+        Distance parameters to use with distance alignment path.
 
     Returns
     -------
-    np.ndarray (2d array of shape (n_dimensions, series_length)
+    np.ndarray (2d array of shape (m, p) where m is the number of dimensions and p is
+                the number of time points.)
         The time series that is the computed average series.
     """
     if len(X) <= 1:
         return X
 
-    # test = X.copy()
-    # test = test.reshape(test.shape[0], test.shape[2], test.shape[1])
-    # center = _init_avg(test, X.shape[2])
-    # center = center.reshape(center.shape[1], center.shape[0])
     # center = medoids(X)
     center = _init_avg(X, X.shape[2])
-    path_callable = distance_path_factory(X[0], X[1], metric=distance_metric)
+    path_callable = distance_alignment_path_factory(
+        X[0],
+        X[1],
+        metric=averaging_distance_metric,
+        **kwargs
+    )
     for i in range(iterations):
         center = _dba_update(center, X, path_callable)
     return center
 
+
 @njit(cache=True)
-def _dba_update(center: np.ndarray, X: np.ndarray, path_callable):
-    """Perform a update iteration for dba.
+def _dba_update(
+        center: np.ndarray,
+        X: np.ndarray,
+        path_callable: DistanceAlignmentPathCallable
+):
+    """Perform an update iteration for dba.
 
     Parameters
     ----------
-    center: np.ndarray (2d array of shape (series_length, n_dimensions))
+    center: np.ndarray (2d array of shape (m, p) where m is the number of dimensions
+                        and p is the number of time point)
         Time series that is the current center (or average).
-    X : np.ndarray (3d array of shape (n_instances, series_length, n_dimensions)))
+    X : np.ndarray (3d array of shape (n, m, p) where n is number of instances, m
+                    is the dimensions and p is the timepoints))
         Time series instances compute average from.
+    path_callable: Callable[Union[np.ndarray, np.ndarray], tuple[list[tuple], float]]
+        Callable that returns the distance path.
 
     Returns
     -------
-    np.ndarray (2d array of shape (n_dimensions, series_length)
+    np.ndarray (2d array of shape (m, p) where m is the number of dimensions and p is
+                the number of time points.)
         The time series that is the computed average series.
     """
     X_size, X_dims, X_timepoints = X.shape
