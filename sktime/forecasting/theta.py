312bf1feec67e512c1204074e405e763f608c25f
Franz Király <f.kiraly@ucl.ac.uk>
2022-04-22 00:34:16 +0100

    fix ThetaForecaster with data frame

--- sktime/forecasting/theta.py
+++ sktime/forecasting/theta.py
@@ -149,6 +149,8 @@ class ThetaForecaster(ExponentialSmoothing):
             The forecasters horizon with the steps ahead to to predict.
             Default is
             one-step ahead forecast, i.e. np.array([1]).
+        X : pd.DataFrame, optional (default=None)
+            Exogenous time series
 
         Returns
         -------
@@ -218,7 +220,7 @@ class ThetaForecaster(ExponentialSmoothing):
             self.fh.to_relative(self.cutoff) * self.initial_level_ ** 2 + 1
         )
 
-        y_pred = super(ThetaForecaster, self).predict(fh, X)
+        y_pred = super(ThetaForecaster, self)._predict(fh, X)
 
         # we assume normal additive noise with sem variance
         for a in alpha:
