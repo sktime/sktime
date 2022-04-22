ce795bdce784194ec3a3fd3304e8c2a9436a66b0
ltsaprounis <leonidas.tsap@gmail.com>
2022-04-22 23:14:11 +0100

    added tests for quantiles function

--- sktime/forecasting/compose/tests/test_bagging.py
+++ sktime/forecasting/compose/tests/test_bagging.py
@@ -3,12 +3,12 @@
 # copyright: sktime developers, BSD-3-Clause License (see LICENSE file)
 """Tests for Bagging Forecasters."""
 
+import pandas as pd
 import pytest
 
 from sktime.datasets import load_airline
 from sktime.forecasting.compose import BaggingForecaster
-
-# from sktime.forecasting.compose._bagging import _calculate_data_quantiles
+from sktime.forecasting.compose._bagging import _calculate_data_quantiles
 from sktime.forecasting.naive import NaiveForecaster
 from sktime.transformations.bootstrap import STLBootstrapTransformer
 from sktime.transformations.series.boxcox import LogTransformer
@@ -42,3 +42,24 @@ def test_bagging_forecaster_forecaster_type_error(forecaster):
         f.fit(y)
         msg = "forecaster in BaggingForecaster should be an sktime Forecaster"
         assert msg == ex.value
+
+
+def test_calculate_data_quantiles():
+    """Test that we calculate quantiles correctly."""
+    series_names = ["s1", "s2", "s3"]
+    fh = [1, 2]
+    alpha = [0, 0.5, 1]
+    quantiles_column_index = pd.MultiIndex.from_product([["Quantiles"], alpha])
+
+    index = pd.MultiIndex.from_product(
+        [series_names, fh], names=["time_series", "time"]
+    )
+    df = pd.DataFrame(data=[1, 10, 2, 11, 3, 12], index=index)
+
+    output_df = pd.DataFrame(
+        data=[[1.0, 2.0, 3.0], [10.0, 11.0, 12.0]],
+        columns=quantiles_column_index,
+        index=pd.Index(data=fh, name="time"),
+    )
+
+    pd.testing.assert_frame_equal(_calculate_data_quantiles(df, alpha), output_df)
