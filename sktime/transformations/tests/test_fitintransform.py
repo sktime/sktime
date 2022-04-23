b471980de58cfa90052605cdc0663c4f8e57f082
Martin Walter <mf-walter@web.de>
2022-04-23 18:08:56 +0200

    Fix tests

--- sktime/transformations/tests/test_fitintransform.py
+++ sktime/transformations/tests/test_fitintransform.py
@@ -6,35 +6,35 @@
 __author__ = ["aiwalter"]
 __all__ = []
 
-from pandas.testing import assert_frame_equal
+from pandas.testing import assert_series_equal
 
 from sktime.forecasting.model_selection import temporal_train_test_split
 from sktime.transformations.compose import FitInTransform
 from sktime.transformations.series.boxcox import BoxCoxTransformer
 from sktime.utils._testing.forecasting import make_forecasting_problem
 
-y, X = make_forecasting_problem(make_X=True)
-y_train, y_test, X_train, X_test = temporal_train_test_split(y, X)
+X = make_forecasting_problem()
+X_train, X_test = temporal_train_test_split(X)
 
 
 def test_transform_fitintransform():
     """Test fit/transform against BoxCoxTransformer."""
     fitintransform = FitInTransform(BoxCoxTransformer())
-    fitintransform.fit(X=X_train, y=y_train)
-    y_hat = fitintransform.transform(X=X_test, y=y_test)
+    fitintransform.fit(X=X_train)
+    y_hat = fitintransform.transform(X=X_test)
 
-    y_hat_expected = BoxCoxTransformer().fit_transform(X_test, y_test)
-    assert_frame_equal(y_hat, y_hat_expected)
+    y_hat_expected = BoxCoxTransformer().fit_transform(X_test)
+    assert_series_equal(y_hat, y_hat_expected)
 
 
 def test_inverse_transform_fitintransform():
     """Test inverse_transform against BoxCoxTransformer."""
     fitintransform = FitInTransform(BoxCoxTransformer())
-    fitintransform.fit(X=X_train, y=y_train)
-    _ = fitintransform.transform(X=X_test, y=y_test)
+    fitintransform.fit(X=X_train)
+    _ = fitintransform.transform(X=X_test)
 
-    exponent = BoxCoxTransformer().fit(X_test, y_test)
+    exponent = BoxCoxTransformer().fit(X_test)
 
-    y_inv = fitintransform.inverse_transform(X_train, y_train)
-    y_inv_expected = exponent.inverse_transform(X_train, y_train)
-    assert_frame_equal(y_inv, y_inv_expected)
+    y_inv = fitintransform.inverse_transform(X_train)
+    y_inv_expected = exponent.inverse_transform(X_train)
+    assert_series_equal(y_inv, y_inv_expected)
