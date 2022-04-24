a4e2bb8436fbde44be5b6a906957bb47a711b059
Franz Király <f.kiraly@ucl.ac.uk>
2022-04-24 15:41:01 +0100

    dtw soft dependency checks

--- sktime/alignment/dtw_python.py
+++ sktime/alignment/dtw_python.py
@@ -13,7 +13,13 @@ from sklearn import clone
 from sktime.alignment.base import BaseAligner
 from sktime.utils.validation._dependencies import _check_soft_dependencies
 
-_check_soft_dependencies("dtw", severity="warning", suppress_import_stdout=True)
+_check_soft_dependencies(
+    "dtw-python",
+    package_import_alias={"dtw-python": "dtw"},
+    severity="warning",
+    object="AlignerDTW or AlignerDTWfromDist",
+    suppress_import_stdout=True,
+)
 
 
 class AlignerDTW(BaseAligner):
@@ -66,8 +72,13 @@ class AlignerDTW(BaseAligner):
         variable_to_align=None,
     ):
         """Construct instance."""
-        _check_soft_dependencies("dtw", severity="error", object=self)
-
+        _check_soft_dependencies(
+            "dtw-python",
+            package_import_alias={"dtw-python": "dtw"},
+            severity="error",
+            object=self,
+            suppress_import_stdout=True,
+        )
         super(AlignerDTW, self).__init__()
 
         self.dist_method = dist_method
@@ -234,6 +245,13 @@ class AlignerDTWfromDist(BaseAligner):
         open_end=False,
     ):
         """Construct instance."""
+        _check_soft_dependencies(
+            "dtw-python",
+            package_import_alias={"dtw-python": "dtw"},
+            severity="error",
+            object=self,
+            suppress_import_stdout=True,
+        )
         super(AlignerDTWfromDist, self).__init__()
 
         self.dist_trafo = dist_trafo
