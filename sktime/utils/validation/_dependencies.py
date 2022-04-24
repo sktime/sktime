6f4f23d105cbeb5154a0fbe871ad5011a4803634
Franz Király <f.kiraly@ucl.ac.uk>
2022-04-24 15:33:43 +0100

    add import alias to _check_soft_dependencies

--- sktime/utils/validation/_dependencies.py
+++ sktime/utils/validation/_dependencies.py
@@ -8,14 +8,23 @@ from importlib import import_module
 
 
 def _check_soft_dependencies(
-    *packages, severity="error", object=None, suppress_import_stdout=False
+    *packages,
+    package_import_alias=None,
+    severity="error",
+    object=None,
+    suppress_import_stdout=False,
 ):
     """Check if required soft dependencies are installed and raise error or warning.
 
     Parameters
     ----------
     packages : str or tuple of str
-        One or more package names to check
+        One or more package names to check. This needs to be the *package* name,
+        i.e., the name of the package on pypi, installed by pip install package
+    package_import_alias : dict with str keys and values, optional, default=empty
+        key-value pairs are package name, import name
+        import name is str used in python import, i.e., from import_name import ...
+        should be provided if import name differs from package name
     severity : str, "error" (default) or "warning"
         whether the check should raise an error, or only a warning
     object : python object or None, default=None
@@ -29,15 +38,35 @@ def _check_soft_dependencies(
     ModuleNotFoundError
         error with informative message, asking to install required soft dependencies
     """
+    if not all(isinstance(x, str) for x in packages):
+        raise TypeError("packages must be str or tuple of str")
+
+    if package_import_alias is None:
+        package_import_alias = dict()
+    msg = "package_import_alias must be a dict with str keys and values"
+    if not isinstance(package_import_alias, dict):
+        raise TypeError(msg)
+    if not all(isinstance(x, str) for x in package_import_alias.keys()):
+        raise TypeError(msg)
+    if not all(isinstance(x, str) for x in package_import_alias.values()):
+        raise TypeError(msg)
+
     for package in packages:
+        # determine the package import
+        if package in package_import_alias.keys():
+            package_import_name = package_import_alias[package]
+        else:
+            package_import_name = package
+        # attempt import - if not possible, we know we need to raise warning/exception
         try:
             if suppress_import_stdout:
                 # setup text trap, import, then restore
                 sys.stdout = io.StringIO()
-                import_module(package)
+                import_module(package_import_name)
                 sys.stdout = sys.__stdout__
             else:
-                import_module(package)
+                import_module(package_import_name)
+        # if package cannot be imported, make the user aware of installation requirement
         except ModuleNotFoundError as e:
             if object is None:
                 msg = (
@@ -50,8 +79,8 @@ def _check_soft_dependencies(
             else:
                 class_name = type(object).__name__
                 msg = (
-                    f"{class_name} requires package '{package}' in python "
-                    f"environment to be instantiated, but '{package}' was not found. "
+                    f"{class_name} requires package '{package}' to be present "
+                    f"in the python environment, but '{package}' was not found. "
                     f"'{package}' is a soft dependency and not included in the base "
                     f"sktime installation. Please run: `pip install {package}` to "
                     f"install the {package} package. "
