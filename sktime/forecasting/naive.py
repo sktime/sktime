74b17259c9648548670017283225944e46dc90f8
Franz Király <f.kiraly@ucl.ac.uk>
2022-04-24 19:39:24 +0100

    forgot passing fh

--- sktime/forecasting/naive.py
+++ sktime/forecasting/naive.py
@@ -485,7 +485,7 @@ class NaiveVariance(BaseForecaster):
 
         if self.fh_early_:
             self.residuals_matrix_ = self._compute_sliding_residuals(
-                y=y, X=X, forecaster=self.forecaster
+                y=y, fh=fh, X=X, forecaster=self.forecaster
             )
 
         return self
@@ -497,7 +497,7 @@ class NaiveVariance(BaseForecaster):
         self.forecaster_.update(y, X, update_params=update_params)
         if update_params and self._fh is not None:
             self.residuals_matrix_ = self._compute_sliding_residuals(
-                y=self._y, X=self._X, forecaster=self.forecaster
+                y=self._y, fh=self.fh, X=self._X, forecaster=self.forecaster
             )
         return self
 
@@ -565,7 +565,7 @@ class NaiveVariance(BaseForecaster):
             residuals_matrix = self.residuals_matrix_
         else:
             residuals_matrix = self._compute_sliding_residuals(
-                y=self._y, X=self._X, forecaster=self.forecaster
+                y=self._y, fh=fh, X=self._X, forecaster=self.forecaster
             )
 
         fh_relative = fh.to_relative(self.cutoff)
@@ -599,7 +599,7 @@ class NaiveVariance(BaseForecaster):
 
         return pred_var
 
-    def _compute_sliding_residuals(self, y, X, forecaster):
+    def _compute_sliding_residuals(self, y, fh, X, forecaster):
         """Compute sliding residuals used in uncertainty estimates."""
         y_index = y.index
         residuals_matrix = pd.DataFrame(columns=y_index, index=y_index, dtype="float")
@@ -608,7 +608,7 @@ class NaiveVariance(BaseForecaster):
             forecaster = clone(forecaster)
             subset = y[:id]  # subset on which we fit
             try:
-                forecaster.fit(subset)
+                forecaster.fit(subset, fh=fh)
             except ValueError:
                 if self.verbose:
                     warn(
