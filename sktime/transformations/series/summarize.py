3e06b359206a42ce4498d2eb4fe755c3c6316eec
ltsaprounis <leonidas.tsap@gmail.com>
2022-04-27 23:50:54 +0100

    use levels order instead of names in group_by

--- sktime/transformations/series/summarize.py
+++ sktime/transformations/series/summarize.py
@@ -343,7 +343,8 @@ class WindowSummarizer(BaseTransformer):
             bfill = False
         for cols in target_cols:
             if isinstance(X.index, pd.MultiIndex):
-                X_grouped = X.groupby("instances")[cols]
+                hier_levels = list(range(X.index.nlevels - 1))
+                X_grouped = X.groupby(level=hier_levels)[cols]
                 df = Parallel(n_jobs=self.n_jobs)(
                     delayed(_window_feature)(X_grouped, **kwargs, bfill=bfill)
                     for index, kwargs in func_dict.iterrows()
