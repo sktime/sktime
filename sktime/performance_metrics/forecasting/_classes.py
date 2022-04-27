1c330f5e82cb1cbb132f08fccdb57e80c26e0140
Franz Király <f.kiraly@ucl.ac.uk>
2022-04-27 18:02:46 +0100

    linting

--- sktime/performance_metrics/forecasting/_classes.py
+++ sktime/performance_metrics/forecasting/_classes.py
@@ -359,7 +359,7 @@ class BaseForecastingErrorMetric(BaseMetric):
             y_pred.columns = y_true.columns
 
         # check multioutput arg
-        # allowed_multioutput_str = 
+        # allowed_multioutput_str =
         # ("raw_values", "uniform_average", "variance_weighted")
         allowed_multioutput_str = ("raw_values", "uniform_average")
 
