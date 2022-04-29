6851ba22c2da9f176a0d9d9951e245f31ce1e849
Franz Király <f.kiraly@ucl.ac.uk>
2022-04-29 14:59:48 +0100

    linting

--- sktime/forecasting/compose/tests/test_multiplex.py
+++ sktime/forecasting/compose/tests/test_multiplex.py
@@ -127,8 +127,8 @@ def test_multiplex_or_dunder():
     assert isinstance(multiplex_same_name_three_test, MultiplexForecaster)
     assert len(multiplex_same_name_three_test.forecasters) == 3
     forecaster_param_names = multiplex_same_name_three_test._get_estimator_names(
-                    multiplex_same_name_three_test.forecasters_
-        )
+         multiplex_same_name_three_test.forecasters_
+    )
     assert len(set(forecaster_param_names)) == 3
 
     # test we get a ValueError if we try to | with anything else:
