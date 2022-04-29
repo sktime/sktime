ad2e87dbdb304d0602a79fda5f497b40901d33af
Andrew Jarman <26150780+AJarman@users.noreply.github.com>
2022-04-29 11:28:23 +0000

    added some test parameters

--- sktime/transformations/series/rounding.py
+++ sktime/transformations/series/rounding.py
@@ -2,18 +2,19 @@
 # -*- coding: utf-8 -*-
 # copyright: sktime developers, BSD-3-Clause License (see LICENSE file)
 
-"""Implements a transformer which will round Series to the nearest integer,
- multiple of a number; or nearest value in a set of specified values."""
+"""Implements a transformer which will round objects to specific values."""
 
 __author__ = ["AJarman"]
 __all__ = ["Discretizer"]
 
 # stdlib
-from typing import Optional, Union, List
+from typing import List, Optional, Union
+from warnings import warn
+
+import numpy as np
 
 # data
 import pandas as pd
-import numpy as np
 
 # internal
 from sktime.transformations.base import BaseTransformer
@@ -22,14 +23,17 @@ from sktime.transformations.base import BaseTransformer
 class Discretizer(BaseTransformer):
     """
     A transformer which can be used to round values by methods useful for forecasting.
-    This is particularly useful as the last step of a `TransformedTargetForecaster` to convert
+
+    This is Useful as the last step of a `TransformedTargetForecaster` to convert
      floating point forecasts to discrete values.
     The default method is to round to the nearest integer.
-    - By using `round_to_dp`, the values will be rounded to the specified number of decimal places.
+    The following utilties are available:
+    - By using `round_to_dp`, the values will be rounded to the specified number
+     of decimal places.
     - By using `round_to_multiple`, the values will be rounded to the nearest multiple
      of the specified value.
-    - By using `round_to_list`, the values will be rounded to the nearest value in the specified
-     list.
+    - By using `round_to_list`, the values will be rounded to the nearest value in the
+     specified list.
 
     Parameters
     ----------
@@ -48,7 +52,8 @@ class Discretizer(BaseTransformer):
     >>> transformer = Discretizer() # default, round to nearest integer.
     >>> transformer1 = Discretizer(round_to_dp=2) # round to 2 decimal places.
     >>> transformer2 = Discretizer(round_to_multiple=2) # round to multiples of 2.
-    >>> transformer2 = Discretizer(round_to_list=[1,10,100,1000,10000]) # round to specific values.
+    # round to specific values.
+    >>> transformer2 = Discretizer(round_to_list=[1,10,100,1000,10000])
     >>> y_hat = transformer.fit_transform(y)
     """
 
@@ -83,9 +88,12 @@ class Discretizer(BaseTransformer):
         # is transform result always guaranteed to contain no missing values?
     }
 
-    def __init__(self, round_to_dp: Optional[int] = None,
-                 round_to_multiple: Optional[Union[int, float]] = None,
-                 round_to_list: Optional[List[Union[float, int]]] = None):
+    def __init__(
+        self,
+        round_to_dp: Optional[int] = None,
+        round_to_multiple: Optional[Union[int, float]] = None,
+        round_to_list: Optional[List[Union[float, int]]] = None,
+    ):
 
         # Write any hyper-parameters and components to self
         self.round_to_dp = round_to_dp
@@ -107,7 +115,8 @@ class Discretizer(BaseTransformer):
         Warns
         -----
         UserWarning
-            (1) If more than one of round_to_dp, round_to_multiple, or round_to_list are not None.
+            (1) If more than one of round_to_dp, round_to_multiple, or round_to_list
+             are not None.
 
         Raises
         ------
@@ -117,56 +126,74 @@ class Discretizer(BaseTransformer):
             (3)  `round_to_list` is not None and is not an int or float.
             (4)  `round_to_list` is not None and any element is not an int or float.
         """
-        if sum((bool(i) for i in (
-                self.round_to_dp,
-                self.round_to_list,
-                self.round_to_multiple
-                ))) > 1:
+        if (
+            sum(
+                (
+                    bool(i)
+                    for i in (
+                        self.round_to_dp,
+                        self.round_to_list,
+                        self.round_to_multiple,
+                    )
+                )
+            )
+            > 1
+        ):
             # Raise warning if more than one argument passed
-            raise UserWarning(
+            warn(
                 """
                     More than one argument has been passed,
                     only one argument can be applied.
                     These are evaluated/applied in the order:
                     `round_to_dp` , `round_to_list`, `round_to_multiple`.
-                    """)
+                    If more than one behaviour is required, use multiple
+                     instances of Discretizer.
+                    """,
+                category=UserWarning,
+            )
 
         if self.round_to_dp is not None and not isinstance(
-                self.round_to_dp, (int, float)):
+            self.round_to_dp, (int, float)
+        ):
             raise ValueError(
                 f"""
                 round_to_dp: {self.round_to_dp} is not an integer or float.
-                """)
+                """
+            )
 
         if self.round_to_multiple is not None and not isinstance(
-                self.round_to_multiple, (int, float)):
+            self.round_to_multiple, (int, float)
+        ):
             raise ValueError(
                 f"""
                 round_to_multiple: {self.round_to_multiple} is not an integer or float.
                 """
             )
-        if self.round_to_list is not None and not isinstance(
-                self.round_to_list, list):
+        if self.round_to_list is not None and not isinstance(self.round_to_list, list):
             raise ValueError(
                 f"""
-                round_to_list: {self.round_to_list}, {type(self.round_to_list)} is not a list.
-                """)
+                round_to_list: {self.round_to_list},
+                 {type(self.round_to_list)} is not a list.
+                """
+            )
         if self.round_to_list is not None and not all(
-            (
-                isinstance(i, (int, float)) for i in self.round_to_list)):
+            (isinstance(i, (int, float)) for i in self.round_to_list)
+        ):
             wrong_types: list = [
-                type(i) for i in self.round_to_list if not isinstance(
-                    i, (int, float))]
+                type(i) for i in self.round_to_list if not isinstance(i, (int, float))
+            ]
             raise ValueError(
                 f"""
-                round_to_list: {self.round_to_list}, not all elements are integer or float.
+                round_to_list: {self.round_to_list}, not all elements are
+                 integer or float.
                 {wrong_types} were found in the list.
-                """)
+                """
+            )
 
     def _transform(self, X, y=None):
         """Transform X and return a transformed version.
 
-        private _transform containing core logic, called from transform
+        Private _transform containing core logic, called from transform.
 
         Parameters
         ----------
@@ -180,7 +207,6 @@ class Discretizer(BaseTransformer):
         -------
         transformed version of X
         """
-
         if self._default:
             # no args passed, use default
             # traditional rounding and convert to int
@@ -198,14 +224,6 @@ class Discretizer(BaseTransformer):
             # First convert list to ndarray
             # Return result from method call
             return round_to_nearest_value(X, np.array(self.round_to_list))
-
-    def _inverse_transform(self, X, y=None):
-        """Not possible to back-calculate transformation without storing all values
-        , therefore just return X"""
-        return X
-
-    # todo: return default parameters, so that a test instance can be created
-    #   required for automated unit and integration testing of estimator
 
     @classmethod
     def get_test_params(cls, parameter_set="default"):
@@ -226,8 +244,6 @@ class Discretizer(BaseTransformer):
             `MyClass(**params)` or `MyClass(**params[i])` creates a valid test instance.
             `create_test_instance` uses the first (or only) dictionary in `params`
         """
-
-        # todo: set the testing parameters for the estimators
         # Testing parameters can be dictionary or list of dictionaries
         # Testing parameter choice should cover internal cases well.
         #
@@ -259,48 +275,66 @@ class Discretizer(BaseTransformer):
         #     return params
         #
         # # "default" params - always returned except for "special_param_set" value
-        # params = {"est": value3, "parama": value4}
-        # return params
+        if parameter_set == "airline_default":
+            # will round to integer
+            return {}
+        elif parameter_set == "airline_round_to_dp":
+            return {"round_to_dp": 2}
+        elif parameter_set == "airline_round_to_multiple":
+            return {"round_to_multiple": 10}
+        elif parameter_set == "airline_round_to_list":
+            return {"round_to_list": [450, 500, 550]}
+        elif parameter_set == "airline_round_to_multiple_and_dp":
+            # will only round to DP
+            return {"round_to_multiple": 10, "round_to_dp": 2}
+        elif parameter_set == "airline_round_to_list_and_multiple":
+            # will only round to multiple
+            return {"round_to_list": [450, 500, 550], "round_to_multiple": 10}
+        elif parameter_set == "raise_ValueError_on_invalid_parameter_round_to_dp":
+            return {"round_to_dp": ["dfndjn", 1000]}
+        elif parameter_set == "raise_ValueError_on_invalid_parameter_round_to_multiple":
+            return {"round_to_multiple": "tree"}
+        elif parameter_set == "raise_ValueError_on_invalid_parameter_round_to_list":
+            return {"round_to_list": [1, 2, 3, "tree"]}
 
 
-def round_to_nearest_value(values: Union[np.ndarray, pd.Series, pd.DataFrame()],
-                           allowed_values: Union[np.ndarray]) -> Union[np.ndarray, pd.Series, pd.DataFrame]:
+def round_to_nearest_value(
+    values: Union[np.ndarray, pd.Series, pd.DataFrame], allowed_values: np.ndarray
+) -> Union[np.ndarray, pd.Series, pd.DataFrame]:
     """
-    Finds the closest values in allowed_values from values.
-    Returns the original index if values is a Series.
+    Find the closest values in allowed_values from values.
+
+    Will returns the original index if values is a Series.
 
     Parameters
     ----------
     values : Union[np.ndarray,pd.Series, pd.DataFrame]
         Values to be rounded to values of second array.
     allowed_values : Union[np.ndarray]
-        Values that are allowed, (desired discrete outputs).
+        Values that are allowed, 1-d array (desired discrete outputs).
 
     Returns
     -------
     Union[np.ndarray,pd.Series, pd.DataFrame]
         Object transformed to closest values in allowed_values from values.
     """
-    print(type(values))
-    is_pandas:bool = False
-    if isinstance(values, (pd.Series,pd.DataFrame)):
-        original_index = values.index
-        is_pandas:bool = True
+    is_pandas: bool = False
+    if isinstance(values, (pd.Series, pd.DataFrame)):
+        is_pandas: bool = True
 
-    # Sort outputs array if isn't sorted.
+    # Sort outputs array if isn't sorted already.
     if not all(allowed_values[:-1] <= allowed_values[1:]):
         allowed_values = np.sort(allowed_values)
 
-    # find index to the left of the allowed_values
+    # Create an array same size as values with the closest index
+    # to the values in allowed_values.
     idxs = np.searchsorted(allowed_values, values, side="left")
-    
+
     # find indexes where previous (left) index is closer
-    prev_idx_is_less = (
-        (idxs == len(allowed_values)) | (
-            np.fabs(values - allowed_values[np.maximum(idxs-1, 0)]) <
-            np.fabs(values - allowed_values[np.minimum(idxs, len(allowed_values)-1)])
-            )
-        )
+    prev_idx_is_less = (idxs == len(allowed_values)) | (
+        np.fabs(values - allowed_values[np.maximum(idxs - 1, 0)])
+        < np.fabs(values - allowed_values[np.minimum(idxs, len(allowed_values) - 1)])
+    )
     # apply change to indexes
     idxs[prev_idx_is_less] -= 1
 
