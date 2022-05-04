bf6a053e2772092df39665f68436c3fd1e6bdd36
Tony Bagnall <ajb@uea.ac.uk>
2022-05-04 12:33:18 +0100

    no overwrite

--- sktime/_contrib/clustering_experiments.py
+++ sktime/_contrib/clustering_experiments.py
@@ -189,6 +189,6 @@ if __name__ == "__main__":
         cls_name=name,
         dataset_name=dataset,
         resample_id=resample,
-        overwrite=True,
+        overwrite=False,
     )
     print("done")
