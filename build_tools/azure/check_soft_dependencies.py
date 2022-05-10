f956a35d23cb7385706918a73656731f8addc3eb
Tony Bagnall <ajb@uea.ac.uk>
2022-05-10 19:35:28 +0100

    add modules to SOFT_DEPENDENCIES

--- build_tools/azure/check_soft_dependencies.py
+++ build_tools/azure/check_soft_dependencies.py
@@ -26,6 +26,7 @@ SOFT_DEPENDENCIES = {
     "sktime.forecasting.bats": ["tbats"],
     "sktime.forecasting.fbprophet": ["prophet"],
     "sktime.classification.feature_based": ["tsfresh", "esig"],
+    "sktime.classification.deep_learning": ["tensorflow"],
     "sktime.networks": ["tensorflow"],
     "sktime.transformations.panel.tsfresh": ["tsfresh"],
     "sktime.transformations.series.matrix_profile": ["stumpy"],
