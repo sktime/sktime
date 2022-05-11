9ffa1f2174bea80a9080fd72c578737939a3d7a6
k1m190r <igor.kim@shell.com>
2022-05-11 22:55:27 +0200

    corrected docstrings for growth_floor and _cap set interval_width back to 0

--- sktime/forecasting/fbprophet.py
+++ sktime/forecasting/fbprophet.py
@@ -38,13 +38,11 @@ class Prophet(_ProphetAdapter):
     growth: str, default="linear"
         String 'linear' or 'logistic' to specify a linear or logistic
         trend.
-    growth_floor: float, array like, default=0
-        Growth saturation minimum value. If float floor is constant.
-        If array like - Variable floor (must be same length as y).
-    growth_cap: float, array like, default=None
-        Growth saturation maximum aka carrying capacity. If float capacity is constant.
-        If array like - Variable capacity (must be same length as y).
-        Must be specified if `growth="logistic"`.
+    growth_floor: float, default=0
+        Growth saturation minimum value.
+    growth_cap: float, default=None
+        Growth saturation maximum aka carrying capacity.
+        Conditionally optional: must be specified if `growth="logistic"`.
     changepoints: list or None, default=None
         List of dates at which to include potential changepoints. If
         not specified, potential changepoints are selected automatically.
@@ -151,7 +149,7 @@ class Prophet(_ProphetAdapter):
         uncertainty_samples=1000,
         stan_backend=None,
         verbose=0,
-        interval_width=0.80,
+        interval_width=0,
     ):
         _check_soft_dependencies("prophet", severity="error", object=self)
 
