af197f1781b0d94636b5027650b220a44e57d5f5
k1m190r <igor.kim@shell.com>
2022-05-24 00:16:47 +0200

    [BUG] Fix Prophet with logistic growth #1079 (#2609)
    Adds the possibility to define the required "cap" and "floor" parameters for Prophet when its growth is set to logistic.

--- sktime/forecasting/fbprophet.py
+++ sktime/forecasting/fbprophet.py
@@ -37,7 +37,15 @@ class Prophet(_ProphetAdapter):
             country_name: Name of the country, like 'UnitedStates' or 'US'
     growth: str, default="linear"
         String 'linear' or 'logistic' to specify a linear or logistic
-        trend.
+        trend. If 'logistic' specified float for 'growth_cap' must be provided.
+    growth_floor: float, default=0
+        Growth saturation minimum value.
+        Used only if  `growth="logistic"`, has no effect otherwise
+        (if `growth` is not `"logistic"`).
+    growth_cap: float, default=None
+        Growth saturation maximum aka carrying capacity.
+        Mandatory (float) iff `growth="logistic"`, has no effect and is optional,
+        otherwise (if `growth` is not `"logistic"`).
     changepoints: list or None, default=None
         List of dates at which to include potential changepoints. If
         not specified, potential changepoints are selected automatically.
@@ -126,6 +134,8 @@ class Prophet(_ProphetAdapter):
         add_country_holidays=None,
         # Args of fbprophet
         growth="linear",
+        growth_floor=0.0,
+        growth_cap=None,
         changepoints=None,
         n_changepoints=25,
         changepoint_range=0.8,
@@ -150,6 +160,8 @@ class Prophet(_ProphetAdapter):
         self.add_country_holidays = add_country_holidays
 
         self.growth = growth
+        self.growth_floor = growth_floor
+        self.growth_cap = growth_cap
         self.changepoints = changepoints
         self.n_changepoints = n_changepoints
         self.changepoint_range = changepoint_range
