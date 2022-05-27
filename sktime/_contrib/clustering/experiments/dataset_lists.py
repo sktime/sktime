8e22f6881dbc86038e48b2dc0b220261c1f290c7
chris holder <chrisholder987@hotmail.com>
2022-05-27 04:15:08 +0100

    added basic test for server

--- sktime/_contrib/clustering/experiments/dataset_lists.py
+++ sktime/_contrib/clustering/experiments/dataset_lists.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 VARIABLE_LENGTH = [
     "AsphaltObstacles",
     "AsphaltPavementType",
@@ -12,122 +13,122 @@ VARIABLE_LENGTH = [
     "GesturePebbleZ2",
     "PickupGestureWiimoteZ",
     "PLAID",
-    "ShakeGestureWiimoteZ"
+    "ShakeGestureWiimoteZ",
 ]
 
 EQUAL_LENGTH = [
-"ACSF1",
-"Adiac",        # 390,391,176,37
-"ArrowHead",    # 36,175,251,3
-"Beef",         # 30,30,470,5
-"BeetleFly",    # 20,20,512,2
-"BirdChicken",  # 20,20,512,2
-"BME",
-"Car",          # 60,60,577,4
-"CBF",                      # 30,900,128,3
-"Chinatown",
-"ChlorineConcentration",    # 467,3840,166,3
-"CinCECGTorso", # 40,1380,1639,4
-"Coffee", # 28,28,286,2
-"Computers", # 250,250,720,2
-"CricketX", # 390,390,300,12
-"CricketY", # 390,390,300,12
-"CricketZ", # 390,390,300,12
-"Crop",
-"DiatomSizeReduction", # 16,306,345,4
-"DistalPhalanxOutlineAgeGroup", # 400,139,80,3
-"DistalPhalanxOutlineCorrect", # 600,276,80,2
-"DistalPhalanxTW", # 400,139,80,6
-"Earthquakes", # 322,139,512,2
-"ECG200",   #100, 100, 96
-"ECG5000",  #4500, 500,140
-"ECGFiveDays", # 23,861,136,2
-"ElectricDevices", # 8926,7711,96,7
-"EOGHorizontalSignal",
-"EOGVerticalSignal",
-"EthanolLevel",
-"FaceAll", # 560,1690,131,14
-"FaceFour", # 24,88,350,4
-"FacesUCR", # 200,2050,131,14
-"FiftyWords", # 450,455,270,50
-"Fish", # 175,175,463,7
-"FordA", # 3601,1320,500,2
-"FordB", # 3636,810,500,2
-"FreezerRegularTrain",
-"FreezerSmallTrain",
-"GunPoint", # 50,150,150,2
-"GunPointAgeSpan",
-"GunPointMaleVersusFemale",
-"GunPointOldVersusYoung",
-"Ham",      #105,109,431
-"HandOutlines", # 1000,370,2709,2
-"Haptics", # 155,308,1092,5
-"Herring", # 64,64,512,2
-"HouseTwenty",
-"InlineSkate", # 100,550,1882,7
-"InsectEPGRegularTrain",
-"InsectEPGSmallTrain",
-"InsectWingbeatSound",#1980,220,256
-"ItalyPowerDemand", # 67,1029,24,2
-"LargeKitchenAppliances", # 375,375,720,3
-"Lightning2", # 60,61,637,2
-"Lightning7", # 70,73,319,7
-"Mallat", # 55,2345,1024,8
-"Meat",#60,60,448
-"MedicalImages", # 381,760,99,10
-"MiddlePhalanxOutlineAgeGroup", # 400,154,80,3
-"MiddlePhalanxOutlineCorrect", # 600,291,80,2
-"MiddlePhalanxTW", # 399,154,80,6
-"MixedShapesRegularTrain",
-"MixedShapesSmallTrain",
-"MoteStrain", # 20,1252,84,2
-"NonInvasiveFetalECGThorax1", # 1800,1965,750,42
-"NonInvasiveFetalECGThorax2", # 1800,1965,750,42
-"OliveOil", # 30,30,570,4
-"OSULeaf", # 200,242,427,6
-"PhalangesOutlinesCorrect", # 1800,858,80,2
-"Phoneme",#1896,214, 1024
-"PigAirwayPressure",
-"PigArtPressure",
-"PigCVP",
-"Plane", # 105,105,144,7
-"PowerCons",
-"ProximalPhalanxOutlineAgeGroup", # 400,205,80,3
-"ProximalPhalanxOutlineCorrect", # 600,291,80,2
-"ProximalPhalanxTW", # 400,205,80,6
-"RefrigerationDevices", # 375,375,720,3
-"Rock",
-"ScreenType", # 375,375,720,3
-"SemgHandGenderCh2",
-"SemgHandMovementCh2",
-"SemgHandSubjectCh2",
-"ShapeletSim", # 20,180,500,2
-"ShapesAll", # 600,600,512,60
-"SmallKitchenAppliances", # 375,375,720,3
-"SmoothSubspace",
-"SonyAIBORobotSurface1", # 20,601,70,2
-"SonyAIBORobotSurface2", # 27,953,65,2
-"StarLightCurves", # 1000,8236,1024,3
-"Strawberry",#370,613,235
-"SwedishLeaf", # 500,625,128,15
-"Symbols", # 25,995,398,6
-"SyntheticControl", # 300,300,60,6
-"ToeSegmentation1", # 40,228,277,2
-"ToeSegmentation2", # 36,130,343,2
-"Trace", # 100,100,275,4
-"TwoLeadECG", # 23,1139,82,2
-"TwoPatterns", # 1000,4000,128,4
-"UMD",
-"UWaveGestureLibraryAll", # 896,3582,945,8
-"UWaveGestureLibraryX", # 896,3582,315,8
-"UWaveGestureLibraryY", # 896,3582,315,8
-"UWaveGestureLibraryZ", # 896,3582,315,8
-"Wafer", # 1000,6164,152,2
-"Wine",#54	57	234
-"WordSynonyms", # 267,638,270,25
-"Worms", #77, 181,900,5
-"WormsTwoClass",#77, 181,900,5
-"Yoga" # 300,3000,426,2
+    "ACSF1",
+    "Adiac",  # 390,391,176,37
+    "ArrowHead",  # 36,175,251,3
+    "Beef",  # 30,30,470,5
+    "BeetleFly",  # 20,20,512,2
+    "BirdChicken",  # 20,20,512,2
+    "BME",
+    "Car",  # 60,60,577,4
+    "CBF",  # 30,900,128,3
+    "Chinatown",
+    "ChlorineConcentration",  # 467,3840,166,3
+    "CinCECGTorso",  # 40,1380,1639,4
+    "Coffee",  # 28,28,286,2
+    "Computers",  # 250,250,720,2
+    "CricketX",  # 390,390,300,12
+    "CricketY",  # 390,390,300,12
+    "CricketZ",  # 390,390,300,12
+    "Crop",
+    "DiatomSizeReduction",  # 16,306,345,4
+    "DistalPhalanxOutlineAgeGroup",  # 400,139,80,3
+    "DistalPhalanxOutlineCorrect",  # 600,276,80,2
+    "DistalPhalanxTW",  # 400,139,80,6
+    "Earthquakes",  # 322,139,512,2
+    "ECG200",  # 100, 100, 96
+    "ECG5000",  # 4500, 500,140
+    "ECGFiveDays",  # 23,861,136,2
+    "ElectricDevices",  # 8926,7711,96,7
+    "EOGHorizontalSignal",
+    "EOGVerticalSignal",
+    "EthanolLevel",
+    "FaceAll",  # 560,1690,131,14
+    "FaceFour",  # 24,88,350,4
+    "FacesUCR",  # 200,2050,131,14
+    "FiftyWords",  # 450,455,270,50
+    "Fish",  # 175,175,463,7
+    "FordA",  # 3601,1320,500,2
+    "FordB",  # 3636,810,500,2
+    "FreezerRegularTrain",
+    "FreezerSmallTrain",
+    "GunPoint",  # 50,150,150,2
+    "GunPointAgeSpan",
+    "GunPointMaleVersusFemale",
+    "GunPointOldVersusYoung",
+    "Ham",  # 105,109,431
+    "HandOutlines",  # 1000,370,2709,2
+    "Haptics",  # 155,308,1092,5
+    "Herring",  # 64,64,512,2
+    "HouseTwenty",
+    "InlineSkate",  # 100,550,1882,7
+    "InsectEPGRegularTrain",
+    "InsectEPGSmallTrain",
+    "InsectWingbeatSound",  # 1980,220,256
+    "ItalyPowerDemand",  # 67,1029,24,2
+    "LargeKitchenAppliances",  # 375,375,720,3
+    "Lightning2",  # 60,61,637,2
+    "Lightning7",  # 70,73,319,7
+    "Mallat",  # 55,2345,1024,8
+    "Meat",  # 60,60,448
+    "MedicalImages",  # 381,760,99,10
+    "MiddlePhalanxOutlineAgeGroup",  # 400,154,80,3
+    "MiddlePhalanxOutlineCorrect",  # 600,291,80,2
+    "MiddlePhalanxTW",  # 399,154,80,6
+    "MixedShapesRegularTrain",
+    "MixedShapesSmallTrain",
+    "MoteStrain",  # 20,1252,84,2
+    "NonInvasiveFetalECGThorax1",  # 1800,1965,750,42
+    "NonInvasiveFetalECGThorax2",  # 1800,1965,750,42
+    "OliveOil",  # 30,30,570,4
+    "OSULeaf",  # 200,242,427,6
+    "PhalangesOutlinesCorrect",  # 1800,858,80,2
+    "Phoneme",  # 1896,214, 1024
+    "PigAirwayPressure",
+    "PigArtPressure",
+    "PigCVP",
+    "Plane",  # 105,105,144,7
+    "PowerCons",
+    "ProximalPhalanxOutlineAgeGroup",  # 400,205,80,3
+    "ProximalPhalanxOutlineCorrect",  # 600,291,80,2
+    "ProximalPhalanxTW",  # 400,205,80,6
+    "RefrigerationDevices",  # 375,375,720,3
+    "Rock",
+    "ScreenType",  # 375,375,720,3
+    "SemgHandGenderCh2",
+    "SemgHandMovementCh2",
+    "SemgHandSubjectCh2",
+    "ShapeletSim",  # 20,180,500,2
+    "ShapesAll",  # 600,600,512,60
+    "SmallKitchenAppliances",  # 375,375,720,3
+    "SmoothSubspace",
+    "SonyAIBORobotSurface1",  # 20,601,70,2
+    "SonyAIBORobotSurface2",  # 27,953,65,2
+    "StarLightCurves",  # 1000,8236,1024,3
+    "Strawberry",  # 370,613,235
+    "SwedishLeaf",  # 500,625,128,15
+    "Symbols",  # 25,995,398,6
+    "SyntheticControl",  # 300,300,60,6
+    "ToeSegmentation1",  # 40,228,277,2
+    "ToeSegmentation2",  # 36,130,343,2
+    "Trace",  # 100,100,275,4
+    "TwoLeadECG",  # 23,1139,82,2
+    "TwoPatterns",  # 1000,4000,128,4
+    "UMD",
+    "UWaveGestureLibraryAll",  # 896,3582,945,8
+    "UWaveGestureLibraryX",  # 896,3582,315,8
+    "UWaveGestureLibraryY",  # 896,3582,315,8
+    "UWaveGestureLibraryZ",  # 896,3582,315,8
+    "Wafer",  # 1000,6164,152,2
+    "Wine",  # 54	57	234
+    "WordSynonyms",  # 267,638,270,25
+    "Worms",  # 77, 181,900,5
+    "WormsTwoClass",  # 77, 181,900,5
+    "Yoga",  # 300,3000,426,2
 ]
 
 EQUAL_LENGTH_LOWER = list((map(lambda x: x.lower(), EQUAL_LENGTH)))
--- sktime/_contrib/clustering/experiments/elastic-dist-comparison/kmeans.py
+++ sktime/_contrib/clustering/experiments/elastic-dist-comparison/kmeans.py
@@ -1,34 +1,48 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+
 import numpy as np
 import pandas as pd
-import os
 
-from sktime.benchmarking.experiments import run_clustering_experiment
-from sktime.clustering.k_means import TimeSeriesKMeans
 from sktime._contrib.clustering.experiments.base import BaseExperiment
 from sktime._contrib.clustering.experiments.dataset_lists import EQUAL_LENGTH_LOWER
+from sktime.benchmarking.experiments import run_clustering_experiment
+from sktime.clustering.k_means import TimeSeriesKMeans
 
 ignore_dataset = []
 
+
 class KmeansExperiment(BaseExperiment):
-
-
-    def _run_experiment_for_dataset(self, X_train: pd.DataFrame, y_train: np.ndarray,
-                                    X_test: pd.DataFrame, y_test: np.ndarray, dataset_name: str):
+    def _run_experiment_for_dataset(
+        self,
+        X_train: pd.DataFrame,
+        y_train: np.ndarray,
+        X_test: pd.DataFrame,
+        y_test: np.ndarray,
+        dataset_name: str,
+    ):
         if dataset_name.lower() not in EQUAL_LENGTH_LOWER:
             return
 
         n_classes = len(set(y_train))
 
+        # k_means_clusterer = TimeSeriesKMeans(
+        #     n_clusters=n_classes,
+        #     metric="msm",
+        #     averaging_method='dba',
+        #     average_params={'averaging_distance_metric': 'msm', 'medoids_distance_metric': 'msm'}
+        # )
+
         k_means_clusterer = TimeSeriesKMeans(
-            n_init=1,  # Set to 10 as num classes is 10
             n_clusters=n_classes,
-            metric='euclidean',
+            metric="euclidean",
         )
 
         run_clustering_experiment(
             X_train,
             k_means_clusterer,
-            results_path=f'{self.result_path}/{self.experiment_name}',
+            results_path=f"{self.result_path}/{self.experiment_name}",
             trainY=y_train,
             testX=X_test,
             testY=y_test,
@@ -38,11 +52,32 @@ class KmeansExperiment(BaseExperiment):
             overwrite=False,
         )
 
-if __name__ == '__main__':
-    kmeans_experiment = KmeansExperiment(
-        experiment_name='test_experiment',
-        dataset_path=os.path.abspath('C:/Users/chris/Documents/Masters/datasets/Univariate_ts/'),
-        result_path=os.path.abspath('C:/Users/chris/Documents/Masters/results/'),
-    )
+
+if __name__ == "__main__":
+
+    server = False
+    log_file = None
+    if server is False:
+        kmeans_experiment = KmeansExperiment(
+            experiment_name="msm_dba",
+            dataset_path=os.path.abspath(
+                "C:/Users/chris/Documents/Masters/datasets/Univariate_ts/"
+            ),
+            result_path=os.path.abspath("C:/Users/chris/Documents/Masters/results/"),
+        )
+    else:
+        old_stdout = sys.stdout
+        log_file = open("./message.log", "w")
+        sys.stdout = log_file
+
+        kmeans_experiment = KmeansExperiment(
+            experiment_name="msm_dba",
+            dataset_path=os.path.abspath(
+                "/root/datasets/Univariate_ts/"
+            ),
+            result_path=os.path.abspath("/root/results/"),
+        )
     kmeans_experiment.run_experiment()
 
+    if log_file is not None:
+        log_file.close()
