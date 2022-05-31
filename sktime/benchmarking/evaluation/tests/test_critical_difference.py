b29d4e919275f1ebdeda5ce58faae67438693eaf
chris holder <chrisholder987@hotmail.com>
2022-05-31 21:09:48 +0100

    enh(benchmarking) finalised critical difference

--- sktime/benchmarking/evaluation/tests/test_critical_difference.py
+++ sktime/benchmarking/evaluation/tests/test_critical_difference.py
@@ -1,23 +1,73 @@
-import numpy as np
+# -*- coding: utf-8 -*-
+"""Test for critical difference diagram."""
 import random
-from sktime.benchmarking.evaluation.utils._critical_difference_diagram import _plot_critical_difference_diagram
+
+import numpy as np
+import pandas as pd
+
+from sktime.benchmarking.evaluation.utils._critical_difference_diagram import (
+    _plot_critical_difference_diagram,
+    create_critical_difference_diagram,
+)
+
 
 def test_critical_difference():
+    """Test different size critical difference diagrams."""
+    random.seed(20)
 
+    min_estimators = 2
     max_estimators = 20
 
     figures = []
 
-    for i in range(2, 20):
+    for i in range(min_estimators, max_estimators):
         estimators = []
         ranks = []
         for j in range(1, i + 1):
-            estimators.append(f'cls{j}')
+            estimators.append(f"cls{j}")
             ranks.append(
-                [random.uniform(1.1,len(estimators)) for _ in range(len(estimators))][-1]
+                [random.uniform(1.1, len(estimators)) for _ in range(len(estimators))][
+                    -1
+                ]
             )
         sorted_rank = np.flip(np.sort(ranks))
         figures.append(_plot_critical_difference_diagram(estimators, list(sorted_rank)))
 
     for fig in figures:
         fig.show()
+
+
+def test_crit():
+    """Test complete critical difference."""
+    num_estimators = 10
+    datasets = 20
+    np.random.seed(20)
+    metric1 = np.random.rand(num_estimators * datasets)
+    metric2 = np.random.rand(num_estimators * datasets)
+    metric3 = np.random.rand(num_estimators * datasets)
+
+    data = []
+    for i in range(1, num_estimators + 1):
+        curr_estimators = f"cls_{i}"
+        for j in range(datasets):
+            curr_dataset = f"dataset_{j}"
+            metric_pos = i * j
+            data.append(
+                [
+                    curr_estimators,
+                    curr_dataset,
+                    metric1[metric_pos],
+                    metric2[metric_pos],
+                    metric3[metric_pos],
+                ]
+            )
+
+    df = pd.DataFrame(data)
+    create_critical_difference_diagram(
+        df,
+        title="test",
+        alpha=100.0,
+    )
+
+    df = pd.read_csv("./test.csv", index_col=False)
+    create_critical_difference_diagram(df, output_path="./", title="Accuracy")
--- sktime/benchmarking/evaluation/utils/_critical_difference_diagram.py
+++ sktime/benchmarking/evaluation/utils/_critical_difference_diagram.py
@@ -1,55 +1,109 @@
-from typing import List, Tuple
+# -*- coding: utf-8 -*-
 import math
-import Orange
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 import operator
-import networkx
 from operator import itemgetter
+from typing import List, Tuple
 
-
+import matplotlib.pyplot as plt
+import networkx
+import numpy as np
+import pandas as pd
 from scipy.stats import friedmanchisquare, wilcoxon
 
 
-def create_critical_difference_diagram(df, output_path, title):
-    """Creates a critical difference diagram.
+def create_critical_difference_diagram(
+    df,
+    output_path=None,
+    title=None,
+    alpha: float = 0.05,
+    color: str = "0.0",
+    space_between_labels: float = 0.15,
+    fontsize: float = 15,
+    title_fontsize: float = 20,
+    figure_width: float = None,
+    figure_height: float = None,
+) -> List[plt.Figure]:
+    """Create a critical difference diagram.
 
     Parameters
     ----------
     df: pd.DataFrame
         The data frame should have three columns index 0 should be the estimators
-        names, index 1 should be the dataset and index 3 should be the accuracy the
-        estimator scored for the datasets. For examples:
+        names, index 1 should be the dataset and index 3 and onwards should be the
+         estimators metric scored for the datasets. For examples:
         ----------------------------------
-        | estimator | dataset | accuracy |
-        | cls1      | data1   | 1.2      |
-        | cls2      | data2   | 3.4      |
-        | cls1      | data2   | 1.4      |
-        | cls2      | data1   | 1.3      |
+        | estimator | dataset | metric1  | metric2 |
+        | cls1      | data1   | 1.2      | 1.2     |
+        | cls2      | data2   | 3.4      | 1.4     |
+        | cls1      | data2   | 1.4      | 1.3     |
+        | cls2      | data1   | 1.3      | 1.2     |
         ----------------------------------
+        Each metric will have its own critical difference diagram produced for it.
+    output_path: str, defaults = None
+        String that is the path to output the figure. If not specified then figure
+        isn't written
+    title: str, defaults = None
+        Title for figures. If not specified, no title is given then the figure
+        is named the same as the column provided. If you want no title simply
+        specify title = ''.
+    alpha: float, defaults = 0.05
+        Alpha value to use to reject Holm hypothesis.
+    color: str, defaults = '0.0'
+        The matplotlib color of the lines.
+    space_between_labels: float, defaults = 0.25
+        The space that is between each estimator's line.
+    fontsize: float, defaults = 15
+        Fontsize for text in graphic
+    title_fontsize: float, defaults = 20
+        Fontsize for title of graphic
+    figure_width: float, defaults = None
+        Width of the figure. If not set then will be automatically defined.
+    figure_height: float, defaults = None
+        Height of figure. If not set then will be automatically defined.
+
+    Returns
+    -------
+    List[plt.Figures]
+        List of critical difference diagram figures.
     """
-    avg_ranks = compute_average_rank(df)
-    p_values = compute_wilcoxon_signed_rank(df)
+    num_metrics = len(df.columns) - 2
+    figures = []
+    for i in range(2, num_metrics + 2):
+        graph_title = title
+        if title is None:
+            graph_title = df.columns[i]
+        curr_df = df.iloc[:, 0:2]
+        curr_df["metric"] = df.iloc[:, i]
+        avg_ranks = _compute_average_rank(curr_df)
+        p_values = _compute_wilcoxon_signed_rank(curr_df, alpha)
+        cliques = form_cliques(p_values, avg_ranks.keys())
 
-    cliques = form_cliques(p_values, avg_ranks.keys())
+        figure = _plot_critical_difference_diagram(
+            estimators=list(avg_ranks.keys()),
+            ranks=list(avg_ranks.values),
+            cliques=cliques,
+            color=color,
+            space_between_labels=space_between_labels,
+            title=graph_title,
+            fontsize=fontsize,
+            title_fontsize=title_fontsize,
+            figure_width=figure_width,
+            figure_height=figure_height,
+        )
 
-    estimators = list(avg_ranks.keys())
-    ranks = list(avg_ranks.values)
-    cliques = cliques
-    joe = ''
+        if output_path is not None:
+            curr_path = output_path
+            if (
+                ".jpg" not in output_path
+                and ".png" not in output_path
+                and ".jpeg" not in output_path
+            ):
+                # Means they've not given an actual file name
+                curr_path = f"{output_path}/{graph_title}.png"
+            figure.savefig(curr_path)
+        figures.append(figure)
 
-    figure_1 = _plot_critical_difference_diagram(
-        estimators=list(avg_ranks.keys()),
-        ranks=list(avg_ranks.values),
-        cliques=cliques
-    )
-
-    # figure_2 = _plot_critical_difference_diagram(estimators=)
-
-
-
-    joe = ''
+    return figures
 
 
 def form_cliques(p_values, estimators) -> List[List[int]]:
@@ -84,27 +138,32 @@ def form_cliques(p_values, estimators) -> List[List[int]]:
             max_j = max(i, j)
             graph[min_i, max_j] = 1
 
-    return list(networkx.find_cliques(networkx.Graph(graph))) # TODO: rewrite this function nativly in sktime.
+    return list(
+        networkx.find_cliques(networkx.Graph(graph))
+    )  # TODO: rewrite this function nativly in sktime.
 
 
-def compute_wilcoxon_signed_rank(df: pd.DataFrame, alpha=0.05) -> List[Tuple]:
+def _compute_wilcoxon_signed_rank(
+    df: pd.DataFrame,
+    alpha=0.05,
+) -> List[Tuple]:
     """Compute the wilcoxon signed rank for a dataframe of result.
 
     Parameters
     ----------
     df: pd.DataFrame
         The data frame should have three columns index 0 should be the estimators
-        names, index 1 should be the dataset and index 3 should be the accuracy the
-        estimator scored for the datasets. For examples:
+        names, index 1 should be the dataset and index 3 and onwards should be the
+         estimators metric scored for the datasets. For examples:
         ----------------------------------
-        | estimator | dataset | accuracy |
+        | estimator | dataset | metric1  |
         | cls1      | data1   | 1.2      |
         | cls2      | data2   | 3.4      |
         | cls1      | data2   | 1.4      |
         | cls2      | data1   | 1.3      |
         ----------------------------------
-    alpha: float
-        Alpha value to use to reject Holm hypotheiss.
+    alpha: float, defaults = 0.05
+        Alpha values to use to reject Holm hypothesis.
 
     Returns
     -------
@@ -114,36 +173,37 @@ def compute_wilcoxon_signed_rank(df: pd.DataFrame, alpha=0.05) -> List[Tuple]:
         and index 3 is a boolean that when true means two classifiers are not critically
         different and false means they are critically different.
     """
-    datasets = (df.iloc[:, 1]).unique()
-    num_datasets = len(datasets)
     estimators = (df.iloc[:, 0]).unique()
 
     acc_arr = []
     for estimator in estimators:
-        acc_arr.append((df.loc[df[df.columns[0]] == estimator][df.columns[-1]])
-                       .to_numpy())
+        acc_arr.append(
+            (df.loc[df[df.columns[0]] == estimator][df.columns[-1]]).to_numpy()
+        )
 
     friedman_p_value = friedmanchisquare(*acc_arr)[1]
 
     if friedman_p_value >= alpha:
-        raise ValueError("The estimators results provided cannot reject the null"
-                         "hypothesis.")
+        raise ValueError(
+            "The estimators results provided cannot reject the null" "hypothesis."
+        )
     p_values = []
 
     for i in range(len(estimators)):
         curr_estimator = estimators[i]
-        curr_estimator_accuracy = \
-            (df.loc[df[df.columns[0]] == curr_estimator][df.columns[-1]]).to_numpy()
+        curr_estimator_accuracy = (
+            df.loc[df[df.columns[0]] == curr_estimator][df.columns[-1]]
+        ).to_numpy()
         for j in range(i + 1, len(estimators)):
             curr_compare_estimator = estimators[j]
-            curr_compare_estimator_accuracy = \
-                (df.loc[df[df.columns[0]] == curr_compare_estimator][df.columns[-1]]) \
-                    .to_numpy()
+            curr_compare_estimator_accuracy = (
+                df.loc[df[df.columns[0]] == curr_compare_estimator][df.columns[-1]]
+            ).to_numpy()
 
             p_value = wilcoxon(
                 curr_estimator_accuracy,
                 curr_compare_estimator_accuracy,
-                zero_method='pratt'
+                zero_method="pratt",
             )[1]
 
             p_values.append((curr_estimator, curr_compare_estimator, p_value, False))
@@ -159,22 +219,23 @@ def compute_wilcoxon_signed_rank(df: pd.DataFrame, alpha=0.05) -> List[Tuple]:
     return p_values
 
 
-def compute_average_rank(df: pd.DataFrame) -> pd.Series:
+def _compute_average_rank(df: pd.DataFrame) -> pd.Series:
     """Compute the average ranking for each estimator.
 
     Parameters
     ----------
     df: pd.DataFrame
         The data frame should have three columns index 0 should be the estimators
-        names, index 1 should be the dataset and index 3 should be the accuracy the
-        estimator scored for the datasets. For examples:
+        names, index 1 should be the dataset and index 3 and onwards should be the
+         estimators metric scored for the datasets. For examples:
         ----------------------------------
-        | estimator | dataset | accuracy |
-        | cls1      | data1   | 1.2      |
-        | cls2      | data2   | 3.4      |
-        | cls1      | data2   | 1.4      |
-        | cls2      | data1   | 1.3      |
+        | estimator | dataset | metric1  | metric2 |
+        | cls1      | data1   | 1.2      | 1.2     |
+        | cls2      | data2   | 3.4      | 1.4     |
+        | cls1      | data2   | 1.4      | 1.3     |
+        | cls2      | data1   | 1.3      | 1.2     |
         ----------------------------------
+
     Returns
     -------
     Pd.Series
@@ -185,8 +246,9 @@ def compute_average_rank(df: pd.DataFrame) -> pd.Series:
     num_datasets = len(datasets)
     estimators = (df.iloc[:, 0]).unique()
 
-    sorted_df = df.loc[df[df.columns[0]].isin(estimators)]. \
-        sort_values([df.columns[0], df.columns[1]])
+    sorted_df = df.loc[df[df.columns[0]].isin(estimators)].sort_values(
+        [df.columns[0], df.columns[1]]
+    )
     rank_data = np.array(sorted_df[df.columns[-1]]).reshape(
         len(estimators), num_datasets
     )
@@ -194,24 +256,27 @@ def compute_average_rank(df: pd.DataFrame) -> pd.Series:
     df_ranks = pd.DataFrame(
         data=rank_data,
         index=np.sort(estimators),
-        columns=np.unique(sorted_df[df.columns[1]])
+        columns=np.unique(sorted_df[df.columns[1]]),
     )
 
-    ranking_values = \
+    ranking_values = (
         df_ranks.rank(ascending=False).mean(axis=1).sort_values(ascending=False)
+    )
 
     return ranking_values
 
 
 def _plot_critical_difference_diagram(
-        estimators: List[str],
-        ranks: List[float],
-        cliques: List[List[int]] = [],
-        color: str = '0.0',
-        space_between_labels: float = 0.15,
-        title: str = '',
-        fontsize: float = 15,
-        title_fontsize: float = 20
+    estimators: List[str],
+    ranks: List[float],
+    cliques: List[List[int]] = None,
+    color: str = "0.0",
+    space_between_labels: float = 0.15,
+    title: str = "",
+    fontsize: float = 15,
+    title_fontsize: float = 20,
+    figure_width: float = None,
+    figure_height: float = None,
 ):
     """Plot the critical difference diagram.
 
@@ -221,24 +286,32 @@ def _plot_critical_difference_diagram(
         List of string that is the estimators to plot.
     ranks: List[str]
         List of floats that is the ranking for each estimator. The indexes should map
-        to the indexes of the estimators parameter.
+        to the indexes of the estimator's parameter.
     cliques: List[List[int]], defaults = []
         List where each inner List is a list of ints that represents the indexes of the
         estimators that are not critically different from one another.
     color: str, defaults = '0.0'
         The matplotlib color of the lines.
+    space_between_labels: float, defaults = 0.25
+        Space between each estimator's line.
     title: str, defaults = '
         Title for the critical difference diagram
     fontsize: float, defaults = 15
         Fontsize for text in graphic
     title_fontsize: float, defaults = 20
         Fontsize for title of graphic
+    figure_width: float, defaults = None
+        Width of the figure. If not set then will be automatically defined.
+    figure_height: float, defaults = None
+        Height of figure. If not set then will be automatically defined.
 
     Returns
     -------
     plt.Figure
         Figure containing the critical difference diagram.
     """
+    if cliques is None:
+        cliques = []
     # Get the scale i.e 1 - x
     min_rank = 1
     max_rank = math.ceil(max(ranks) + (min(ranks) - 1))
@@ -247,8 +320,9 @@ def _plot_critical_difference_diagram(
     num_labels_left = math.floor(len(estimators) / 2)
     num_labels_right = math.ceil(len(estimators) / 2)
 
-    labels_x_right = [min_rank - 2] * num_labels_left
-    labels_x_left = [max_rank + 2] * num_labels_right
+    label_distance = max_rank / 2
+    labels_x_right = [min_rank - label_distance] * num_labels_left
+    labels_x_left = [max_rank + label_distance] * num_labels_right
     labels_x_positions = labels_x_left + labels_x_right
 
     number_line_y = num_labels_right
@@ -256,26 +330,32 @@ def _plot_critical_difference_diagram(
     labels_y_positions = []
 
     # This is also dependent on the number of cliques
-    cliques_modifier = len(cliques) * 0.1
+    num_cliques = 0
+    for clique in cliques:
+        if len(clique) > 1:
+            num_cliques += 1
+
+    cliques_modifier = num_cliques * 0.1
     start = number_line_y - space_between_labels - cliques_modifier
     for _ in range(len(labels_x_left)):
         labels_y_positions.append(start)
         start -= space_between_labels
 
-    start = number_line_y - (space_between_labels * len(labels_x_right)) - cliques_modifier
+    start = (
+        number_line_y - (space_between_labels * len(labels_x_right)) - cliques_modifier
+    )
     for _ in range(len(labels_x_right)):
         labels_y_positions.append(start)
         start += space_between_labels
 
     x_point_on_line = np.array(ranks)
     y_point_on_line = np.array([number_line_y] * len(ranks))
-    fig,ax = plt.subplots()
+    fig, ax = plt.subplots()
 
     min_y = min(labels_y_positions)
     max_y = number_line_y + space_between_labels
 
     ax.set_ylim([min_y, max_y])
-
 
     ax.scatter(labels_x_positions, labels_y_positions, s=0)
     ax.scatter(x_point_on_line, y_point_on_line, c="g", alpha=0.5, s=0)
@@ -284,10 +364,8 @@ def _plot_critical_difference_diagram(
     start = 1
     end = math.ceil(max(ranks) + (min(ranks) - 1))
     ax.plot(
-        np.array([start, end]), np.array([number_line_y, number_line_y]),
-        color=color
+        np.array([start, end]), np.array([number_line_y, number_line_y]), color=color
     )
-
 
     # This will draw the right angle lines to connect labels
     CONNECTION_LINE = "angle,angleA=360,angleB=90,rad=0"
@@ -298,29 +376,37 @@ def _plot_critical_difference_diagram(
             xy=(x_point_on_line[i], y_point_on_line[i]),
             xytext=(labels_x_positions[i], labels_y_positions[i]),
             fontsize=fontsize,
-            arrowprops=dict(arrowstyle="-", color=color,
-                            shrinkA=13, shrinkB=1,
-                            patchA=None, patchB=None,
-                            connectionstyle=CONNECTION_LINE,
-                            ),
+            arrowprops=dict(
+                arrowstyle="-",
+                color=color,
+                shrinkA=25,
+                shrinkB=0,
+                patchA=None,
+                patchB=None,
+                connectionstyle=CONNECTION_LINE,
+            ),
         )
 
-        if i > num_labels_left:
-            ha = 'right'
-            x_pos_modifier = 0.06
+        if i > num_labels_left or (
+            num_labels_left == num_labels_right and i >= num_labels_left
+        ):
+            ha = "right"
+            x_pos_modifier = -20
         else:
-            ha = 'left'
-            x_pos_modifier = -0.4
+            ha = "left"
+            x_pos_modifier = 50
 
-        x_pos = labels_x_positions[i] + x_pos_modifier
-        y_pos = labels_y_positions[i] + 0.02
+        x_pos = x_pos_modifier
+        y_pos = 5
 
         ax.annotate(
             str(round(ranks[i], 3)),
-            xy=(x_pos, y_pos),
+            xy=(labels_x_positions[i], labels_y_positions[i]),
             xytext=(x_pos, y_pos),
+            textcoords="offset points",
+            # horizontalalignment='left', verticalalignment='bottom',
             fontsize=fontsize,
-            ha=ha
+            ha=ha,
         )
 
     # Clique positions
@@ -331,12 +417,11 @@ def _plot_critical_difference_diagram(
         ax.plot(x, y, color=color, linewidth=5.0)
         cliques_y -= 0.1
 
-
     for i in range(1, end * 2):
         curr = (i * 0.5) + 0.5
         if curr % 1 == 0:
             ax.annotate(
-                int(curr), (curr, number_line_y + 0.06), ha='center', fontsize=fontsize
+                int(curr), (curr, number_line_y + 0.06), ha="center", fontsize=fontsize
             )
             ax.plot([curr, curr], [number_line_y, number_line_y + 0.05], color=color)
         else:
@@ -345,12 +430,15 @@ def _plot_critical_difference_diagram(
     ax.set_title(title, fontsize=title_fontsize)
     plt.gca().invert_xaxis()
 
-    plt.axis('off')
+    plt.axis("off")
     fig.tight_layout()
 
+    if figure_width is None:
+        fig.set_figwidth(number_line_y + 5)
+    else:
+        fig.set_figwidth(figure_width)
+
+    if figure_height is not None:
+        fig.set_figheight(figure_height)
+
     return fig
-
-
-if __name__ == '__main__':
-    df = pd.read_csv('./test.csv', index_col=False)
-    create_critical_difference_diagram(df, output_path='./', title='Accuracy')
