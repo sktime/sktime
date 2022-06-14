a2b45559e2097eeb9c3d26b2c507dd9acae5fc52
Franz Király <f.kiraly@ucl.ac.uk>
2022-06-14 18:08:35 +0100

    conversion ConformalIntervals

--- sktime/forecasting/conformal.py
+++ sktime/forecasting/conformal.py
@@ -14,6 +14,7 @@ import numpy as np
 import pandas as pd
 from sklearn.base import clone
 
+from sktime.datatypes import convert
 from sktime.datatypes._utilities import get_slice
 from sktime.forecasting.base import BaseForecaster
 
@@ -231,6 +232,9 @@ class ConformalIntervals(BaseForecaster):
             pred_int.loc[fh_ind] = pred_int_row
 
         y_pred = self.predict(fh=fh, X=X)
+        y_pred = convert(
+            y_pred, from_type=self._y_mtype_last_seen, to_type="pd.Series"
+        )
 
         for col in cols:
             if self.method in ABS_RESIDUAL_BASED:
