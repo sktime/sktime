0a8e2bf5424ae2b1980bf5b1cc9739e61f4c92c6
dbcerigo <dbcerigo@gmail.com>
2022-06-22 17:12:40 +0100

    [ENH] Init validation and estimator registries

--- sktime/benchmarking/__init__.py
+++ sktime/benchmarking/__init__.py
@@ -1,2 +1,6 @@
 # -*- coding: utf-8 -*-
 """Implements running validations on estimators storing results for benchmarking."""
+from kotsu import registration
+
+estimator_registry = registration.ModelRegistry()
+validation_registry = registration.ValidationRegistry()
