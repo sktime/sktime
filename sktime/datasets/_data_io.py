b835ceb0736845227d97ff90ed3f63562bfdbf4f
TTran92 <tran.aggies@gmail.com>
2022-06-27 20:59:56 -0500

    Fix EOF

--- sktime/datasets/_data_io.py
+++ sktime/datasets/_data_io.py
@@ -1937,7 +1937,7 @@ def load_from_multiindex_to_listdataset(
 
     >>> DATA_PATH = os.path.join(os.path.dirname(sktime.__file__), "datasets/data")
     >>> train_x, train_y = load_from_tsfile_to_dataframe(
-    >>> os.path.join(DATA_PATH, "ArrowHead/ArrowHead_TRAIN.ts"))
+        os.path.join(DATA_PATH, "ArrowHead/ArrowHead_TRAIN.ts"))
 
     >>> train_x_multi = convert_to(train_x, to_type="pd-multiindex")
     >>> listdataset = load_from_multiindex_to_listdataset(train_x_multi, train_y)
