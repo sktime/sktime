cfffd48de14bd994f64e67dcb309ee64cc354f13
Tony Bagnall <ajb@uea.ac.uk>
2022-06-29 16:09:48 +0100

    download all multivariate problems

--- sktime/_contrib/evaluation/classifier_evaluation.py
+++ sktime/_contrib/evaluation/classifier_evaluation.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Working code that will be moved into its own package."""
-
+from sktime.datasets import load_UCR_UEA_dataset
 
 """ 39 UEA multivariate time series classification problems, 2022 version"""
 multivariate = [
@@ -49,3 +49,13 @@ multivariate = [
 if __name__ == "__main__":
     """Main test"""
     print("Length of multivariate  = ", len(multivariate))
+    for problem in multivariate:
+        X, y = load_UCR_UEA_dataset(problem)
+        print(
+            " Shape of combined train test for ",
+            problem,
+            " is X = ",
+            X.shape,
+            " Y = ",
+            y.shape,
+        )
