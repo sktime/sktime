3c4e88bab2a87751ffffd61b72c9020d3af4ed99
Tony Bagnall <ajb@uea.ac.uk>
2022-06-29 09:30:52 +0100

    drafting classifier experiments reboot

---
+++ sktime/_contrib/evaluation/classifier_evaluation.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""Working code that will be moved into its own package."""
+
+
+""" 39 UEA multivariate time series classification problems, 2022 version"""
+multivariate = [
+    "ArticularyWordRecognition",
+    "AsphaltObstaclesCoordinates",
+    "AsphaltPavementTypeCoordinates",
+    "AsphaltRegularityCoordinates",
+    "AtrialFibrillation",
+    "BasicMotions",
+    "CharacterTrajectories",
+    "Cricket",
+    "DuckDuckGeese",
+    "EigenWorms",
+    "EMOPain",
+    "Epilepsy",
+    "ERing",
+    "EthanolConcentration",
+    "EyesOpenShut",
+    "FaceDetection",
+    "FingerMovements",
+    "HandMovementDirection",
+    "Handwriting",
+    "Heartbeat",
+    "InsectWingbeat",
+    "JapaneseVowels",
+    "Libras",
+    "LSST",
+    "MindReading",
+    "MotorImagery",
+    "MotionSenseHAR",
+    "NATOPS",
+    "PenDigits",
+    "PEMS-SF",
+    "PhonemeSpectra",
+    "RacketSports",
+    "SelfRegulationSCP1",
+    "SelfRegulationSCP2",
+    "Siemens",
+    "SpokenArabicDigits",
+    "StandWalkJump",
+    "Tiselac",
+    "UWaveGestureLibrary",
+]
+
+
+if __name__ == "__main__":
+    """Main test"""
+    print("Length of multivariate  = ", len(multivariate))
