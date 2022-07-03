440ae2db0763ef8c72d0d42752a46da7be90d9c2
Franz Király <f.kiraly@ucl.ac.uk>
2022-07-03 02:06:40 +0100

    fixed return of fixture

--- sktime/tests/test_all_estimators.py
+++ sktime/tests/test_all_estimators.py
@@ -340,9 +340,9 @@ class BaseFixtureGenerator:
             estimator_instance_params = estimator_instance_params[test_param_id]
         estimator_instance = estimator_class(**estimator_instance_params)
         set_random_state(estimator_instance, random_state=random_state)
-        estimator_fitted = scenario.run(estimator_instance, method_sequence=["fit"])
+        scenario.run(estimator_instance, method_sequence=["fit"])
 
-        return estimator_fitted
+        return estimator_instance
 
     def _generate_method_nsc(self, test_name, **kwargs):
         """Return estimator test scenario.
