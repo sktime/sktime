76754d1bf9a716613cef110ec1d3d6505b40717f
TTran92 <tran.aggies@gmail.com>
2022-07-03 16:23:35 -0500

    Final commit

--- sktime/forecasting/model_evaluation/_functions.py
+++ sktime/forecasting/model_evaluation/_functions.py
@@ -82,22 +82,23 @@ def evaluate(
         interval metrics, quantile foreast metrics.
         https://www.sktime.org/en/stable/api_reference/performance_metrics.html?highlight=metrics
 
-        To evaluate models/estimators using a specific metric, provide them to the scoring arg
-        simply add that metrics as follows
+        To evaluate models/estimators using a specific metric, provide them to the
+        scoring arg simply add that metrics as follows
     >>> from sktime.performance_metrics.forecasting import MeanAbsoluteError
     >>> loss = MeanAbsoluteError()
     >>> results = evaluate(forecaster=forecaster, y=y, cv=cv, scoring=loss)
 
         An example of an interval metric is the PinballLoss.
-        It can be used with all probabilistic forecasters, which can be listed as follows:
-        predictions or quantile predictions, the PinballLoss
+        It can be used with all probabilistic forecasters, which can be listed
+        as follows: predictions or quantile predictions, the PinballLoss
         metrics can be used. The list of estimators that are compatible with
         this metrics are
     >>> from sktime.registry import all_estimators
     >>> all_estimators("forecaster", filter_tags={"capability:pred_int": True},
     ... as_dataframe=True)
 
-        To evaluate probabilistic forecasts, simply pass a probabilistic metric like PinballLoss:
+        To evaluate probabilistic forecasts, simply pass a probabilistic metric
+        like PinballLoss:
     >>> from sktime.forecasting.naive import NaiveVariance
     >>> from sktime.performance_metrics.forecasting.probabilistic import PinballLoss
     >>> loss = PinballLoss()
