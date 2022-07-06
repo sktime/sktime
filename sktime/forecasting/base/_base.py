f542b5504c7832ccb23970c692e923635bd42873
Franz Király <f.kiraly@ucl.ac.uk>
2022-07-06 01:17:18 +0100

    check_fh arg

--- sktime/forecasting/base/_base.py
+++ sktime/forecasting/base/_base.py
@@ -1496,7 +1496,7 @@ class BaseForecaster(BaseEstimator):
         # B. fh is passed
         else:
             # If fh is passed, coerce to ForecastingHorizon and validate (all cases)
-            fh = check_fh(fh=fh, y=self._cutoff)
+            fh = check_fh(fh=fh, freq=self._cutoff)
 
             # fh is written to self if one of the following is true
             # - estimator has not been fitted yet (for safety from side effects)
