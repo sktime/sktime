37cf03bf8fdc6ef9b59ebc3aabf8ec31a70b90d0
Katie Buchhorn <34525024+KatieBuc@users.noreply.github.com>
2022-07-11 21:02:42 +1000

    Update sktime/forecasting/varmax.py
    Co-authored-by: Martin Walter <mf-walter@web.de>

--- sktime/forecasting/varmax.py
+++ sktime/forecasting/varmax.py
@@ -191,7 +191,6 @@ class VARMAX(_StatsModelsAdapter):
 
     Examples
     --------
-    >>> import numpy as np
     >>> import warnings
     >>> from sktime.forecasting.varmax import VARMAX
     >>> from sktime.datasets import load_longley
