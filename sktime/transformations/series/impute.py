52fd3a0e470e4502617be4965285b9ee3b6f0392
Franz Király <f.kiraly@ucl.ac.uk>
2022-07-12 12:22:33 +0100

    [MNT] 0.13.0 deprecation actions (#2895)
    Deprecation actions for 0.13.0
    
    * remove `hcrystalball` dependency and forecaster (separate PR: https://github.com/alan-turing-institute/sktime/pull/2858)
    * remove `capability:early_prediction` tag
    * remove `drop_na` arg in `Differencer`, default of `na_handling` changed to `fill_zero`
    * remove `lag_config` argument of `WindowSummarizer`
    * remove `greater_is_better` property from metrics, now a tag
    * remove `func` and `name` args from all metrics

--- sktime/transformations/series/impute.py
+++ sktime/transformations/series/impute.py
@@ -7,8 +7,6 @@ __author__ = ["aiwalter"]
 __all__ = ["Imputer"]
 
 
-from warnings import warn
-
 import numpy as np
 from sklearn.utils import check_random_state
 
@@ -170,16 +168,6 @@ class Imputer(BaseTransformer):
         """
         X = X.copy()
 
-        # TODO v0.13.0: Remove this if statement and warning
-        if self.method in ["drift", "mean", "median", "random"]:
-            warn(
-                """Imputer methods "drift", "mean", "median", "random" have been
-                moved to the fit() method, so usage via transform() is
-                deprecated. To still fit on the transform data only, please use the
-                new FitInTransform transformer.
-                """,
-                DeprecationWarning,
-            )
         # replace missing_values with np.nan
         if self.missing_values:
             X = X.replace(to_replace=self.missing_values, value=np.nan)
@@ -190,7 +178,7 @@ class Imputer(BaseTransformer):
         if self.method == "random":
             for col in X.columns:
                 X[col] = X[col].apply(
-                    lambda i: self._get_random(col) if np.isnan(i) else i
+                    lambda i: self._get_random(col) if np.isnan(i) else i  # noqa: B023
                 )
         elif self.method == "constant":
             X = X.fillna(value=self.value)
