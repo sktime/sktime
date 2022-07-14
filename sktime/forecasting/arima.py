4872075015ffe9aac1c35a878c1adf8003adba47
Franz Király <f.kiraly@ucl.ac.uk>
2022-07-15 00:52:48 +0100

    clustering

--- sktime/forecasting/arima.py
+++ sktime/forecasting/arima.py
@@ -614,16 +614,16 @@ class ARIMA(_PmdArimaAdapter):
 
     Examples
     --------
-    >>> from sktime.datasets import load_airline  # doctest: +SKIP
+    >>> from sktime.datasets import load_airline
     >>> from sktime.forecasting.arima import ARIMA
     >>> y = load_airline()
-    >>> forecaster = ARIMA(
+    >>> forecaster = ARIMA(  # doctest: +SKIP
     ...     order=(1, 1, 0),
     ...     seasonal_order=(0, 1, 0, 12),
     ...     suppress_warnings=True)
-    >>> forecaster.fit(y)
+    >>> forecaster.fit(y)  # doctest: +SKIP
     ARIMA(...)
-    >>> y_pred = forecaster.predict(fh=[1,2,3])
+    >>> y_pred = forecaster.predict(fh=[1,2,3])  # doctest: +SKIP
     """  # noqa: E501
 
     _tags = {"handles-missing-data": True}
