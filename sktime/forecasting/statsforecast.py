69c46111e7e0874af8ed2edfa844fda00c1200f3
Franz Király <f.kiraly@ucl.ac.uk>
2022-07-15 08:49:20 +0100

    linting

--- sktime/forecasting/statsforecast.py
+++ sktime/forecasting/statsforecast.py
@@ -155,7 +155,9 @@ class StatsForecastAutoARIMA(_StatsForecastAdapter):
     >>> from sktime.datasets import load_airline
     >>> from sktime.forecasting.statsforecast import StatsForecastAutoARIMA
     >>> y = load_airline()
-    >>> forecaster = StatsForecastAutoARIMA(sp=12, d=0, max_p=2, max_q=2)  # doctest: +SKIP
+    >>> forecaster = StatsForecastAutoARIMA(  # doctest: +SKIP
+    ...     sp=12, d=0, max_p=2, max_q=2
+    ... )
     >>> forecaster.fit(y)  # doctest: +SKIP
     StatsForecastAutoARIMA(...)
     >>> y_pred = forecaster.predict(fh=[1,2,3])  # doctest: +SKIP
