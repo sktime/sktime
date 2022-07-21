5767dbaac759ed6ac43a8977f4c46beb93d0f18c
Franz Király <f.kiraly@ucl.ac.uk>
2022-07-21 20:25:53 +0100

    [ENH] parameter checking and move of `super.__init__` in extension templates (#3010)
    This PR updates extension templates to the decision in https://github.com/alan-turing-institute/sktime/issues/2573.
    
    That is, `__init__` can now contain additional logic of parameter checking kind, or tag setting, but it must be after the call to `super.__init__`.

--- extension_templates/classification.py
+++ extension_templates/classification.py
@@ -90,16 +90,19 @@ class MyTimeSeriesClassifier(BaseClassifier):
         self.parama = parama
         self.paramb = paramb
         self.paramc = paramc
-        # important: no checking or other logic should happen here
+
+        # todo: change "MyTimeSeriesClassifier" to the name of the class
+        super(MyTimeSeriesClassifier, self).__init__()
+
+        # todo: optional, parameter checking logic (if applicable) should happen here
+        # if writes derived values to self, should *not* overwrite self.parama etc
+        # instead, write to self._parama, self._newparam (starting with _)
 
         # todo: default estimators should have None arg defaults
         #  and be initialized here
         #  do this only with default estimators, not with parameters
         # if est2 is None:
         #     self.estimator = MyDefaultEstimator()
-
-        # todo: change "MyTimeSeriesClassifier" to the name of the class
-        super(MyTimeSeriesClassifier, self).__init__()
 
         # todo: if tags of estimator depend on component tags, set these here
         #  only needed if estimator is a composite
