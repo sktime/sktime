672e79cbd549c6de15418daa866d016a59afb1e5
Mirae Parker <miraep8@gmail.com>
2022-07-20 20:07:42 -0400

    change BaseClass

--- sktime/annotation/base/_base.py
+++ sktime/annotation/base/_base.py
@@ -22,66 +22,11 @@ State:
 """
 
 __author__ = ["satya-pattnaik ", "fkiraly"]
-__all__ = ["SimpleBaseEstimator", "BaseSeriesAnnotator"]
-
-import abc
+__all__ = ["BaseSeriesAnnotator"]
 
 from sktime.base import BaseEstimator
 from sktime.utils.validation.annotation import check_fmt, check_labels
 from sktime.utils.validation.series import check_series
-
-
-class SimpleBaseEstimator(metaclass=abc.ABCMeta):
-    """An experiment in a more simple Base Object."""
-
-    @abc.abstractmethod
-    def fit(self):
-        """Fit."""
-        pass
-
-    @abc.abstractmethod
-    def predict(self):
-        """Predict."""
-        pass
-
-    def get_params(self, name_str="", deep=True):
-        """Get all parameters for this Estimator."""
-        param_return = {}
-        for parameter in self.parameters.keys():
-            param_key = "".join([name_str, parameter])
-            param_return[param_key] = getattr(self, parameter)
-            if deep and isinstance(param_return[param_key], SimpleBaseEstimator):
-                param_return.update(
-                    param_return[param_key].get_params(
-                        name_str="".join([parameter, "__"])
-                    )
-                )
-        return param_return
-
-    def make_list(self, obj):
-        """Make obj a list if it isn't already."""
-        if isinstance(obj, list):
-            return obj
-        return [obj]
-
-    def set_params(self, **kwargs):
-        """Update all parameters for valid parameters and update types."""
-        for parameter, value in kwargs.items():
-            if parameter in self.parameters.keys() and type(value) in self.make_list(
-                self.parameters[parameter]
-            ):
-                setattr(self, parameter, value)
-            else:
-                raise ValueError(
-                    "".join(
-                        [
-                            f"{type(self).__name__} doesn't have a parameter ",
-                            f"{parameter} which accepts input of type",
-                            f"{type(value).__name__}. ",
-                            f"Valid options and types are {self.parameters}",
-                        ]
-                    )
-                )
 
 
 class BaseSeriesAnnotator(BaseEstimator):
