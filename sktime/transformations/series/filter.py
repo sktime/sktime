60bd0fb0a7fe10f6ab1e24c80603d8466533d6b3
Franz Király <f.kiraly@ucl.ac.uk>
2022-07-22 10:28:02 +0100

    import warning

--- sktime/transformations/series/filter.py
+++ sktime/transformations/series/filter.py
@@ -7,6 +7,10 @@ __all__ = ["Filter"]
 import numpy as np
 
 from sktime.transformations.base import BaseTransformer
+from sktime.utils.validation._dependencies import _check_soft_dependencies
+
+
+_check_soft_dependencies("mne", severity="warning")
 
 
 class Filter(BaseTransformer):
