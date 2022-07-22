4f330c9ffdda2be21b48eec6327ffc65791c3ed0
Franz Király <f.kiraly@ucl.ac.uk>
2022-07-22 10:28:50 +0100

    linting

--- sktime/transformations/series/filter.py
+++ sktime/transformations/series/filter.py
@@ -9,7 +9,6 @@ import numpy as np
 from sktime.transformations.base import BaseTransformer
 from sktime.utils.validation._dependencies import _check_soft_dependencies
 
-
 _check_soft_dependencies("mne", severity="warning")
 
 
