84885d7bfa4ce087145fbe9bffca7b29a115050b
Mirae Parker <miraep8@gmail.com>
2022-07-22 05:49:41 -0400

    Update sktime/annotation/hmm.py
    Co-authored-by: Lukasz Mentel <lmmentel@users.noreply.github.com>

--- sktime/annotation/hmm.py
+++ sktime/annotation/hmm.py
@@ -3,8 +3,7 @@
 HMM Annotation Estimator.
 
 Implements a basic Hidden Markov Model (HMM) as an annotation estimator.
-To read more about the algorithm, check out the wikipedia page:
-(https://en.wikipedia.org/wiki/Hidden_Markov_model
+To read more about the algorithm, check out the `HMM wikipedia page <https://en.wikipedia.org/wiki/Hidden_Markov_model>`_.
 """
 from typing import Tuple
 
