606945d47c5c026ff5d07c601f58e577f5065861
Franz Király <f.kiraly@ucl.ac.uk>
2022-07-23 20:54:53 +0100

    do not pass train distances

--- sktime/classification/distance_based/_time_series_neighbors.py
+++ sktime/classification/distance_based/_time_series_neighbors.py
@@ -20,6 +20,10 @@ param_names=None)
 __author__ = ["jasonlines", "TonyBagnall", "chrisholder", "fkiraly"]
 __all__ = ["KNeighborsTimeSeriesClassifier"]
 
+from inspect import signature
+
+import numpy as np
+
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neighbors._base import _check_weights
 
@@ -81,10 +85,12 @@ class KNeighborsTimeSeriesClassifier(BaseClassifier):
                 X, X2 which are pd_multiindex and numpy3D mtype
         can be pairwise panel transformer inheriting from BasePairwiseTransformerPanel
     distance_params : dict, optional. default = None.
-        dictionary for metric parameters , in case that distance is a str
+        dictionary for distance parameters, in case that distance is a str or callable
     distance_mtype : str, or list of str optional. default = None.
         mtype that distance expects for X and X2, if a callable
             only set this if distance is not BasePairwiseTransformerPanel descendant
+    pass_train_distances : bool, optional, default = False.
+        Whether distances between training points are computed and passed to sklearn
     leaf_size : int, default=30
         Leaf size passed to BallTree or KDTree.  This can affect the
         speed of the construction and query, as well as the memory
@@ -123,6 +129,7 @@ class KNeighborsTimeSeriesClassifier(BaseClassifier):
         distance="dtw",
         distance_params=None,
         distance_mtype=None,
+        pass_train_distances=False,
         leaf_size=30,
         n_jobs=None,
     ):
@@ -132,6 +139,7 @@ class KNeighborsTimeSeriesClassifier(BaseClassifier):
         self.distance = distance
         self.distance_params = distance_params
         self.distance_mtype = distance_mtype
+        self.pass_train_distances = pass_train_distances
         self.leaf_size = leaf_size
         self.n_jobs = n_jobs
 
@@ -162,7 +170,7 @@ class KNeighborsTimeSeriesClassifier(BaseClassifier):
         elif distance_mtype is not None:
             self.set_tags(X_inner_mtype=distance_mtype)
 
-    def _distance(self, X, X2):
+    def _distance(self, X, X2=None):
         """Compute distance - unified interface to str code and callable."""
         distance = self.distance
         distance_params = self.distance_params
@@ -172,7 +180,16 @@ class KNeighborsTimeSeriesClassifier(BaseClassifier):
         if isinstance(distance, str):
             return pairwise_distance(X, X2, distance, **distance_params)
         else:
-            return distance(X, X2)
+            if X2 is not None:
+                return distance(X, X2, **distance_params)
+            # if X2 is None, check if distance allows None X2 to mean "X2=X"
+            else:
+                sig = signature(distance).parameters
+                X2_sig = sig[list(sig.keys())[1]]
+                if X2_sig.default is not None:
+                    return distance(X, X2, **distance_params)
+                else:
+                    return distance(X, **distance_params)
 
     def _fit(self, X, y):
         """Fit the model using X as training data and y as target values.
@@ -190,7 +207,10 @@ class KNeighborsTimeSeriesClassifier(BaseClassifier):
         # store full data as indexed X
         self._X = X
 
-        dist_mat = self._distance(X, X)
+        if self.pass_train_distances:
+            dist_mat = self._distance(X)
+        else:
+            dist_mat = np.zeros([len(X), len(X)], dtype="float")
 
         self.knn_estimator_.fit(dist_mat, y)
 
