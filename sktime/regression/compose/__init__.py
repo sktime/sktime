ea4e72b19ff9981d76264f884ee7259af6829602
Franz Király <f.kiraly@ucl.ac.uk>
2022-07-28 22:54:13 +0100

    export SklearnRegressorPipeline

--- sktime/regression/compose/__init__.py
+++ sktime/regression/compose/__init__.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 """Implement composite time series regression estimators."""
 
-__all__ = ["ComposableTimeSeriesForestRegressor", "RegressorPipeline"]
+__all__ = [
+    "ComposableTimeSeriesForestRegressor",
+    "RegressorPipeline",
+    "SklearnRegressorPipeline",
+]
 
 from sktime.regression.compose._ensemble import ComposableTimeSeriesForestRegressor
-from sktime.regression.compose._pipeline import RegressorPipeline
+from sktime.regression.compose._pipeline import (
+    RegressorPipeline,
+    SklearnRegressorPipeline,
+)
