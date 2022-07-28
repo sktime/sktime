8413cad4680a1dabc3a779b7bfbf75284513ccf2
Franz Király <f.kiraly@ucl.ac.uk>
2022-07-29 00:27:03 +0100

    add tags and test params

--- sktime/forecasting/model_selection/_split.py
+++ sktime/forecasting/model_selection/_split.py
@@ -1028,6 +1028,27 @@ class BaseWindowSplitter(BaseSplitter):
                 cutoffs = np.hstack((-1, cutoffs))
             return cutoffs
 
+    @classmethod
+    def get_test_params(cls, parameter_set="default"):
+        """Return testing parameter settings for the splitter.
+
+        Parameters
+        ----------
+        parameter_set : str, default="default"
+            Name of the set of test parameters to return, for use in tests. If no
+            special parameters are defined for a value, will return `"default"` set.
+
+        Returns
+        -------
+        params : dict or list of dict, default = {}
+            Parameters to create testing instances of the class
+            Each dict are parameters to construct an "interesting" test instance, i.e.,
+            `MyClass(**params)` or `MyClass(**params[i])` creates a valid test instance.
+            `create_test_instance` uses the first (or only) dictionary in `params`
+        """
+        params = {"cutoffs": np.array([3, 7, 10]), "fh": 2, "window_length": 3}
+        return params
+
 
 class SlidingWindowSplitter(BaseWindowSplitter):
     """Sliding window splitter.
@@ -1224,6 +1245,27 @@ class SingleWindowSplitter(BaseSplitter):
         y = get_index_for_series(y)
         end = _get_end(y_index=y, fh=fh)
         return np.array([end])
+
+    @classmethod
+    def get_test_params(cls, parameter_set="default"):
+        """Return testing parameter settings for the splitter.
+
+        Parameters
+        ----------
+        parameter_set : str, default="default"
+            Name of the set of test parameters to return, for use in tests. If no
+            special parameters are defined for a value, will return `"default"` set.
+
+        Returns
+        -------
+        params : dict or list of dict, default = {}
+            Parameters to create testing instances of the class
+            Each dict are parameters to construct an "interesting" test instance, i.e.,
+            `MyClass(**params)` or `MyClass(**params[i])` creates a valid test instance.
+            `create_test_instance` uses the first (or only) dictionary in `params`
+        """
+        params = {"fh": 3)
+        return params
 
 
 def temporal_train_test_split(
