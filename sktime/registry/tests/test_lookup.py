3b36494856eba4b5a7d2ad118d057d14526a9747
Franz Király <f.kiraly@ucl.ac.uk>
2022-07-29 10:16:29 +0100

    add more base classes without tags

--- sktime/registry/tests/test_lookup.py
+++ sktime/registry/tests/test_lookup.py
@@ -20,7 +20,7 @@ VALID_SCITYPES_SET = set(
 )
 
 # some scitypes have no associated tags yet
-SCITYPES_WITHOUT_TAGS = ["series-annotator", "clusterer"]
+SCITYPES_WITHOUT_TAGS = ["series-annotator", "clusterer", "object", "splitter"]
 
 # shorthands for easy reading
 b = BASE_CLASS_SCITYPE_LIST
