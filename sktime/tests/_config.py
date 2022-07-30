2bc752b5138a155214d1ed81c176f90fe71d98b4
Franz Király <f.kiraly@ucl.ac.uk>
2022-07-30 17:34:42 +0100

    [ENH] Featurizer simplified (#2922)
    This simplifies @aiwalter's `Featurizer` from #1727 to a transformer `YtoX` that can be used in composition.
    
    Essentially, @aiwalter's `Featurizer` was a pipeline composite of three things: (a) switching `y` to `X`, (b) applying the `transformer`, (c) applying a lag.
    
    This seems unnecessarily complicated, since the same effect can be achieved by (a), and a transformer and finally `Lag`, chained in a `TransformerPipeline`.
    
    In the example, `YtoX() * ExponentTransformer() * Lag(lags)` instead of `Featurizer(lags=lags, transformer=ExponentTransformer())`, which is more design parsimonous and python zen.
    
    So, we can simply create a transformer for (a) and achieve the rest by pipeline composition.
    
    Added benefit is having everything for hierarchical data, and the ability to specify `lags` in temporal units, not just ints (as in the `Lag` transformer).
    
    Further additions/changes in this PR:
    * name changed to `YtoX`, upon suggestion of @aiwalter
    * transformer scenarios with `y` were sharpened: scenario with `Series` `y`, as needed for `Featurizer`, was added. `is_applicable` cleaned up to match scenarios with `y` only with estimators suitable for that specific scitype of `y`.

--- sktime/tests/_config.py
+++ sktime/tests/_config.py
@@ -35,6 +35,9 @@ EXCLUDE_ESTIMATORS = [
     "TSFreshRelevantFeatureExtractor",
     # PlateauFinder seems to be broken, see #2259
     "PlateauFinder",
+    "RandomIntervalSegmenter",
+    "RandomIntervalFeatureExtractor",
+    "RandomShapeletTransform",
 ]
 
 
