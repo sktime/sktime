e786bc4a1b5f0d445d79b072147652f70ec7af3c
Matthew Middlehurst <pfm15hbu@uea.ac.uk>
2022-08-04 16:55:05 +0100

    todo

--- sktime/classification/early_classification/_probability_threshold.py
+++ sktime/classification/early_classification/_probability_threshold.py
@@ -22,7 +22,7 @@ from sktime.classification.interval_based import CanonicalIntervalForest
 from sktime.utils.validation.panel import check_X
 
 
-# TODO: remove in v0.15.0
+# TODO: remove message in v0.15.0 and change base class
 @deprecated(
     version="0.13.0",
     reason="The base class of ProbabilityThresholdEarlyClassifier will be changed to BaseEarlyClassifier in v0.15.0. This will change how classification safety decisions are made and returned, see BaseEarlyClassifier or TEASER for the new interface.",  # noqa: E501
