e90ee897bdacdb3df0328fd294a83de4191ee34c
Franz Király <f.kiraly@ucl.ac.uk>
2022-08-11 19:23:07 +0100

    exog forecasting trafo dunder

--- sktime/forecasting/compose/_pipeline.py
+++ sktime/forecasting/compose/_pipeline.py
@@ -262,20 +262,36 @@ class ForecastingPipeline(_Pipeline):
     >>> from sktime.forecasting.naive import NaiveForecaster
     >>> from sktime.forecasting.compose import ForecastingPipeline
     >>> from sktime.transformations.series.impute import Imputer
-    >>> from sktime.transformations.series.adapt import TabularToSeriesAdaptor
     >>> from sktime.forecasting.base import ForecastingHorizon
     >>> from sktime.forecasting.model_selection import temporal_train_test_split
     >>> from sklearn.preprocessing import MinMaxScaler
     >>> y, X = load_longley()
     >>> y_train, _, X_train, X_test = temporal_train_test_split(y, X)
     >>> fh = ForecastingHorizon(X_test.index, is_relative=False)
+
+        Example 1: string/estimator pairs
     >>> pipe = ForecastingPipeline(steps=[
     ...     ("imputer", Imputer(method="mean")),
-    ...     ("minmaxscaler", TabularToSeriesAdaptor(MinMaxScaler())),
-    ...     ("forecaster", NaiveForecaster(strategy="drift"))])
+    ...     ("minmaxscaler", MinMaxScaler()),
+    ...     ("forecaster", NaiveForecaster(strategy="drift"))]),
     >>> pipe.fit(y_train, X_train)
     ForecastingPipeline(...)
     >>> y_pred = pipe.predict(fh=fh, X=X_test)
+
+        Example 2: without strings
+    >>> pipe = ForecastingPipeline([
+    ...     Imputer(method="mean"),
+    ...     MinMaxScaler(),
+    ...     NaiveForecaster(strategy="drift"),
+    ... ])
+
+        Example 3: using the dunder method
+    >>> forecaster = NaiveForecaster(strategy="drift")
+    >>> imputer = Imputer(method="mean")
+    >>> pipe = (imputer * MinMaxScaler()) ** forecaster
+
+        Example 3b: using the dunder method, alternative
+    >>> pipe = imputer ** MinMaxScaler() ** forecaster
     """
 
     _tags = {
@@ -309,6 +325,49 @@ class ForecastingPipeline(_Pipeline):
     def forecaster_(self):
         """Return reference to the forecaster in the pipeline. Valid after _fit."""
         return self.steps_[-1][1]
+
+    def __rmul__(self, other):
+        """Magic ** method, return (left) concatenated ForecastingPipeline.
+
+        Implemented for `other` being a transformer, otherwise returns `NotImplemented`.
+
+        Parameters
+        ----------
+        other: `sktime` transformer, must inherit from BaseTransformer
+            otherwise, `NotImplemented` is returned
+
+        Returns
+        -------
+        ForecastingPipeline object,
+            concatenation of `other` (first) with `self` (last).
+            not nested, contains only non-TransformerPipeline `sktime` steps
+        """
+        from sktime.transformations.base import BaseTransformer
+        from sktime.transformations.compose import TransformerPipeline
+
+        _, ests = zip(*self.steps_)
+        names = tuple(self._get_estimator_names(self.steps))
+        if isinstance(other, TransformerPipeline):
+            _, trafos_o = zip(*other.steps_)
+            names_o = tuple(other._get_estimator_names(other.steps))
+            new_names = names_o + names
+            new_ests = trafos_o + ests
+        elif isinstance(other, BaseTransformer):
+            new_names = (type(other).__name__,) + names
+            new_ests = (other,) + ests
+        elif self._is_name_and_est(other, BaseTransformer):
+            other_name = other[0]
+            other_trafo = other[1]
+            new_names = (other_name,) + names
+            new_ests = (other_trafo,) + ests
+        else:
+            return NotImplemented
+
+        # if all the names are equal to class names, we eat them away
+        if all(type(x[1]).__name__ == x[0] for x in zip(new_names, new_ests)):
+            return ForecastingPipeline(steps=list(new_ests))
+        else:
+            return ForecastingPipeline(steps=list(zip(new_names, new_ests)))
 
     def _fit(self, y, X=None, fh=None):
         """Fit to training data.
