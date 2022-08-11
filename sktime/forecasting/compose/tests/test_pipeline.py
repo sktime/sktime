556bf28da04fcd62f01bc81b8b1801602a22cf67
Franz Király <f.kiraly@ucl.ac.uk>
2022-08-11 19:28:23 +0100

    tests for dunders

--- sktime/forecasting/compose/tests/test_pipeline.py
+++ sktime/forecasting/compose/tests/test_pipeline.py
@@ -8,16 +8,15 @@ __all__ = []
 
 import numpy as np
 import pandas as pd
-import pytest
 from sklearn.preprocessing import MinMaxScaler
 
 from sktime.datasets import load_airline
 from sktime.datatypes import get_examples
 from sktime.datatypes._utilities import get_window
-from sktime.forecasting.arima import ARIMA
 from sktime.forecasting.compose import ForecastingPipeline, TransformedTargetForecaster
 from sktime.forecasting.model_selection import temporal_train_test_split
 from sktime.forecasting.naive import NaiveForecaster
+from sktime.forecasting.sarimax import SARIMAX
 from sktime.forecasting.trend import PolynomialTrendForecaster
 from sktime.transformations.hierarchical.aggregate import Aggregator
 from sktime.transformations.series.adapt import TabularToSeriesAdaptor
@@ -25,7 +24,7 @@ from sktime.transformations.series.detrend import Detrender
 from sktime.transformations.series.exponent import ExponentTransformer
 from sktime.transformations.series.impute import Imputer
 from sktime.transformations.series.outlier_detection import HampelFilter
-from sktime.utils.validation._dependencies import _check_estimator_deps
+from sktime.utils._testing.series import _make_series
 
 
 def test_pipeline():
@@ -130,10 +129,6 @@ def test_pipeline_with_detrender():
     trans_fc.predict(1)
 
 
-@pytest.mark.skipif(
-    not _check_estimator_deps(ARIMA, severity="none"),
-    reason="skip test if required soft dependencies not available",
-)
 def test_nested_pipeline_with_index_creation_y_before_X():
     """Tests a nested pipeline where y indices are created before X indices.
 
@@ -148,7 +143,7 @@ def test_nested_pipeline_with_index_creation_y_before_X():
     X_test = get_window(X, window_length=1)
 
     # Aggregator creates indices for y (via *), then for X (via ForecastingPipeline)
-    f = Aggregator() * ForecastingPipeline([Aggregator(), ARIMA()])
+    f = Aggregator() * ForecastingPipeline([Aggregator(), SARIMAX()])
 
     f.fit(y=y_train, X=X_train, fh=1)
     y_pred = f.predict(X=X_test)
@@ -159,10 +154,6 @@ def test_nested_pipeline_with_index_creation_y_before_X():
     assert len(y_pred) == 9
 
 
-@pytest.mark.skipif(
-    not _check_estimator_deps(ARIMA, severity="none"),
-    reason="skip test if required soft dependencies not available",
-)
 def test_nested_pipeline_with_index_creation_X_before_y():
     """Tests a nested pipeline where X indices are created before y indices.
 
@@ -177,7 +168,7 @@ def test_nested_pipeline_with_index_creation_X_before_y():
     X_test = get_window(X, window_length=1)
 
     # Aggregator creates indices for X (via ForecastingPipeline), then for y (via *)
-    f = ForecastingPipeline([Aggregator(), Aggregator() * ARIMA()])
+    f = ForecastingPipeline([Aggregator(), Aggregator() * SARIMAX()])
 
     f.fit(y=y_train, X=X_train, fh=1)
     y_pred = f.predict(X=X_test)
@@ -186,3 +177,71 @@ def test_nested_pipeline_with_index_creation_X_before_y():
     assert isinstance(y_pred, pd.DataFrame)
     assert isinstance(y_pred.index, pd.MultiIndex)
     assert len(y_pred) == 9
+
+
+def test_forecasting_pipeline_dunder_endog():
+    """Test forecasting pipeline dunder for endogeneous transformation."""
+    y = load_airline()
+    y_train, y_test = temporal_train_test_split(y)
+
+    forecaster = ExponentTransformer() * MinMaxScaler() * NaiveForecaster()
+
+    fh = np.arange(len(y_test)) + 1
+    forecaster.fit(y_train, fh=fh)
+    actual = forecaster.predict()
+
+    def compute_expected_y_pred(y_train, fh):
+        # fitting
+        yt = y_train.copy()
+        t1 = ExponentTransformer()
+        yt = t1.fit_transform(yt)
+        t2 = TabularToSeriesAdaptor(MinMaxScaler())
+        yt = t2.fit_transform(yt)
+        forecaster = NaiveForecaster()
+        forecaster.fit(yt, fh=fh)
+
+        # predicting
+        y_pred = forecaster.predict()
+        y_pred = t2.inverse_transform(y_pred)
+        y_pred = t1.inverse_transform(y_pred)
+        return y_pred
+
+    expected = compute_expected_y_pred(y_train, fh)
+    np.testing.assert_array_equal(actual, expected)
+
+
+def test_forecasting_pipeline_dunder_exSog():
+    """Test forecasting pipeline dunder for exogeneous transformation."""
+    y = _make_series()
+    y_train, y_test = temporal_train_test_split(y)
+    X = _make_series(n_columns=2)
+    X_train, X_test = temporal_train_test_split(X)
+
+    forecaster = ExponentTransformer() ** MinMaxScaler() ** SARIMAX()
+    forecaster_alt = (ExponentTransformer() * MinMaxScaler()) ** SARIMAX()
+
+    fh = np.arange(len(y_test)) + 1
+    forecaster.fit(y_train, fh=fh, X=X_train)
+    actual = forecaster.predict(X=X_test)
+
+    forecaster_alt.fit(y_train, fh=fh, X=X_train)
+    actual_alt = forecaster_alt.predict(X=X_test)
+
+    def compute_expected_y_pred(y_train, X_train, X_test, fh):
+        # fitting
+        yt = y_train.copy()
+        Xt = X_train.copy()
+        t1 = ExponentTransformer()
+        Xt = t1.fit_transform(Xt)
+        t2 = TabularToSeriesAdaptor(MinMaxScaler())
+        Xt = t2.fit_transform(Xt)
+        forecaster = SARIMAX()
+        forecaster.fit(yt, fh=fh, X=Xt)
+
+        # predicting
+        y_pred = forecaster.predict(X=X_test)
+        return y_pred
+
+    expected = compute_expected_y_pred(y_train, X_train, X_test, fh)
+    np.testing.assert_array_equal(actual, expected)
+    np.testing.assert_array_equal(actual_alt, expected)
