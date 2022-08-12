169238321f76567b7147e00bb9be579be231f5f2
Franz Király <f.kiraly@ucl.ac.uk>
2022-08-12 18:24:22 +0100

    export

--- sktime/dists_kernels/__init__.py
+++ sktime/dists_kernels/__init__.py
@@ -7,12 +7,13 @@ from sktime.dists_kernels._base import (
     BasePairwiseTransformer,
     BasePairwiseTransformerPanel,
 )
-from sktime.dists_kernels.compose_tab_to_panel import AggrDist
+from sktime.dists_kernels.compose_tab_to_panel import AggrDist, FlatDist
 from sktime.dists_kernels.scipy_dist import ScipyDist
 
 __all__ = [
     "BasePairwiseTransformer",
     "BasePairwiseTransformerPanel",
     "AggrDist",
+    "FlatDist",
     "ScipyDist",
 ]
