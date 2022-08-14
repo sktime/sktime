7b13c753da81a5810377f1b86e5f2d04386f6d08
Franz Király <f.kiraly@ucl.ac.uk>
2022-08-14 18:33:09 +0100

    fix ref

--- sktime/transformations/series/compose.py
+++ sktime/transformations/series/compose.py
@@ -96,7 +96,7 @@ class OptionalPassthrough(BaseTransformer):
         "scitype:transform-output": "Series",
         # what scitype is returned: Primitives, Series, Panel
         "scitype:instancewise": True,  # is this an instance-wise transform?
-        "X_inner_mtype": ALL_MTYPES,
+        "X_inner_mtype": CORE_MTYPES,
         # which mtypes do _fit/_predict support for X?
         "y_inner_mtype": "None",  # which mtypes do _fit/_predict support for y?
         "univariate-only": False,
