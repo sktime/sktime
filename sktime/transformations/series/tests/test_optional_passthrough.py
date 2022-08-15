c77df65dae8353ea025c5a47a3f1955b7ee92f47
Martin Walter <mf-walter@web.de>
2022-08-15 17:27:05 +0200

    Added test for comparison with BoxCox

--- sktime/transformations/series/tests/test_optional_passthrough.py
+++ sktime/transformations/series/tests/test_optional_passthrough.py
@@ -5,8 +5,8 @@
 import pytest
 from pandas.testing import assert_series_equal
 
+from sktime.transformations.series.boxcox import BoxCoxTransformer
 from sktime.transformations.series.compose import OptionalPassthrough
-from sktime.transformations.series.exponent import ExponentTransformer
 from sktime.utils._testing.series import _make_series
 
 
@@ -15,14 +15,26 @@ def test_passthrough(passthrough):
     """Test that passthrough works as expected."""
     y = _make_series(n_columns=1)
 
-    transformer = OptionalPassthrough(ExponentTransformer(), passthrough=passthrough)
-    y_hat = transformer.fit_transform(y)
-    y_inv = transformer.inverse_transform(y_hat)
+    optional_passthourgh = OptionalPassthrough(
+        BoxCoxTransformer(), passthrough=passthrough
+    )
+    box_cox = BoxCoxTransformer()
 
-    assert_series_equal(y, y_inv)
+    y_hat_passthrough = optional_passthourgh.fit_transform(y)
+    y_inv_passthrough = optional_passthourgh.inverse_transform(y_hat_passthrough)
+
+    y_hat_boxcox = box_cox.fit_transform(y)
+    y_inv_boxcox = box_cox.inverse_transform(y_hat_boxcox)
+
+    assert_series_equal(y, y_inv_passthrough)
 
     if passthrough:
-        assert_series_equal(y, y_hat)
-    else:
+        assert_series_equal(y, y_hat_passthrough)
         with pytest.raises(AssertionError):
-            assert_series_equal(y, y_hat)
+            assert_series_equal(y_hat_boxcox, y_hat_passthrough)
+    else:
+        assert_series_equal(y_hat_passthrough, y_hat_boxcox)
+        assert_series_equal(y_inv_passthrough, y_inv_boxcox)
+
+        with pytest.raises(AssertionError):
+            assert_series_equal(y, y_hat_passthrough)
