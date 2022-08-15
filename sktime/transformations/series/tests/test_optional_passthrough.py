68e7db8c97aad43ff0eb6689e3f5308e4f87c84a
Martin Walter <mf-walter@web.de>
2022-08-15 15:42:27 +0200

    Test inverse_transform

--- sktime/transformations/series/tests/test_optional_passthrough.py
+++ sktime/transformations/series/tests/test_optional_passthrough.py
@@ -12,7 +12,7 @@ from sktime.utils._testing.series import _make_series
 
 @pytest.mark.parametrize("passthrough", [True, False])
 def test_passthrough(passthrough):
-    """Test that pandas.method and PandasTransformAdaptor.fit.transform is the same."""
+    """Test that passthrough works as expected."""
     y = _make_series(n_columns=1)
 
     transformer = OptionalPassthrough(ExponentTransformer(), passthrough=passthrough)
