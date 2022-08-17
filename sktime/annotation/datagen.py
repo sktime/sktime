980706763ccb2318494f45bc9c45fab4aadfa04f
Katie Buchhorn <katiebuchhorn@gmail.com>
2022-08-17 10:08:55 +1000

    feedback from code review

--- sktime/annotation/datagen.py
+++ sktime/annotation/datagen.py
@@ -8,11 +8,11 @@ import numpy.typing as npt
 from sklearn.utils.validation import check_random_state
 
 
-def multivariate_mean_shift(
+def piecewise_normal_multivariate(
     means: npt.ArrayLike,
     lengths: npt.ArrayLike,
-    vars: Union[npt.ArrayLike, float] = 1.0,
-    covs: npt.ArrayLike = None,
+    variances: Union[npt.ArrayLike, float] = 1.0,
+    covariances: npt.ArrayLike = None,
     random_state: Union[int, np.random.RandomState] = None,
 ) -> npt.ArrayLike:
     """
@@ -20,7 +20,7 @@ def multivariate_mean_shift(
 
     Each segment has length specified in ``lengths`` and data sampled from a
     multivariate normal distribution with a mean from ``means`` and covariance
-    from ``covs`` (either specified or built from ``vars`` when unspecified)
+    from ``covariances`` (either specified or built from ``variances`` when unspecified)
 
     Parameters
     ----------
@@ -28,11 +28,11 @@ def multivariate_mean_shift(
         Means of the segments to be generated of shape (L, N)
     lengths : array_like
         Lengths of the segments to be generated of shape (L,)
-    vars : float or array_like (default=1.0)
+    variances : float or array_like (default=1.0)
         Variance of the segments to be generated
-    covs : array_like (default=None)
+    covariances : array_like (default=None)
         Covariances of segments to be generated of shape (L, N, N)
-        If None, this will be constructed from vars
+        If None, this will be constructed from variances
     random_state : int or np.random.RandomState
         Either a random seed or RandomState instance
 
@@ -44,8 +44,9 @@ def multivariate_mean_shift(
 
     Examples
     --------
-    >>> from sktime.annotation.datagen import multivariate_shift
-    >>> multivariate_shift([[1, 1], [2, 2], [3, 3]], lengths=[2, 3, 1], random_state=2)
+    >>> from sktime.annotation.datagen import piecewise_normal_multivariate
+    >>> piecewise_normal_multivariate([[1, 1], [2, 2], [3, 3]],
+    lengths=[2, 3, 1], random_state=2)
     array([[ 0.58324215,  0.94373317],
         [-1.1361961 ,  2.64027081],
         [ 0.20656441,  1.15825263],
@@ -53,9 +54,9 @@ def multivariate_mean_shift(
         [ 0.94204778,  1.09099239],
         [ 3.55145404,  5.29220801]])
 
-    >>> from sktime.annotation.datagen import multivariate_shift
-    >>> multivariate_shift([[1, 1], [2, 2], [3, 3]], lengths=[2, 3, 1],
-    vars = [[1.0, 1.0], [1.0, 1.0], [1.0, 1.0]], random_state=2)
+    >>> from sktime.annotation.datagen import piecewise_normal_multivariate
+    >>> piecewise_normal_multivariate([[1, 1], [2, 2], [3, 3]], lengths=[2, 3, 1],
+    variances = [[1.0, 1.0], [1.0, 1.0], [1.0, 1.0]], random_state=2)
     array([[ 0.58324215,  0.94373317],
         [-1.1361961 ,  2.64027081],
         [ 0.20656441,  1.15825263],
@@ -63,9 +64,10 @@ def multivariate_mean_shift(
         [ 0.94204778,  1.09099239],
         [ 3.55145404,  5.29220801]])
 
-    >>> from sktime.annotation.datagen import multivariate_shift
-    >>> multivariate_shift([[1, 1], [2, 2], [3, 3]], lengths=[2, 3, 1], covs =
-    [[[1.0, 0], [0, 1.0]], [[1.0, 0], [0, 1.0]], [[1.0, 0], [0, 1.0]]], random_state=2)
+    >>> from sktime.annotation.datagen import piecewise_normal_multivariate
+    >>> piecewise_normal_multivariate([[1, 1], [2, 2], [3, 3]], lengths=[2, 3, 1],
+    covariances = [[[1.0, 0], [0, 1.0]], [[1.0, 0], [0, 1.0]], [[1.0, 0], [0, 1.0]]],
+    random_state=2)
     array([[ 0.58324215,  0.94373317],
         [-1.1361961 ,  2.64027081],
         [ 0.20656441,  1.15825263],
@@ -73,9 +75,9 @@ def multivariate_mean_shift(
         [ 0.94204778,  1.09099239],
         [ 3.55145404,  5.29220801]])
 
-    >>> from sktime.annotation.datagen import multivariate_shift
-    >>> multivariate_shift([[1, 3], [4, 5]], lengths=[3, 3], covs = [[[0.5, 0.3],
-    [0.3, 1.0]], [[1.0, 0.3], [0.3, 0.7]]], random_state=2)
+    >>> from sktime.annotation.datagen import piecewise_normal_multivariate
+    >>> piecewise_normal_multivariate([[1, 3], [4, 5]], lengths=[3, 3],
+    covariances = [[[0.5, 0.3], [0.3, 1.0]], [[1.0, 0.3], [0.3, 0.7]]], random_state=2)
     array([[ 0.78066776,  2.61125356],
         [ 0.92296736,  0.51689669],
         [-0.2694238 ,  1.47959507],
@@ -85,7 +87,7 @@ def multivariate_mean_shift(
 
     """
 
-    def get_covs(var):
+    def get_covariances(var):
         """Fill 1D variance array of length N to 2D covariance array of size (N,N)."""
         cov = np.zeros((N, N), float)
         np.fill_diagonal(cov, var)
@@ -98,31 +100,37 @@ def multivariate_mean_shift(
 
     # if no covariance is specified, build it from variance
     # assuming independent random variables
-    if covs is None:
-        assert vars is not None
+    if covariances is None:
+        assert variances is not None
 
-        # vars van be specified as a float, make 1D array, repeat L times
-        if isinstance(vars, (float, int)):
-            vars = np.repeat(vars, N)
-            vars = np.tile(vars, (L, 1))
+        # variances van be specified as a float, make 1D array, repeat L times
+        if isinstance(variances, (float, int)):
+            variances = np.repeat(variances, N)
+            variances = np.tile(variances, (L, 1))
 
-        assert np.array(vars).shape == (L, N)
+        assert np.array(variances).shape == (L, N)
 
         # get covariance matrices from variance arrays
-        covs = [get_covs(var) for var in vars]
+        covariances = [get_covariances(var) for var in variances]
 
-    assert np.array(covs).shape[0] == L
-    assert np.array(covs).shape[1] == N
+    # if covariance is specified, then check if symmetric (eigenvalues are real)
+    # and check if matrix is positive semidefinite (eigenvalues are nonnegative)
+    else:
+        assert all([np.allclose(cov, cov.T) for cov in covariances])
+        assert all([np.all(np.linalg.eigvals(cov) >= 0) for cov in covariances])
+
+    assert np.array(covariances).shape[0] == L
+    assert np.array(covariances).shape[1] == N
 
     segments_data = [
         rng.multivariate_normal(mean=mean, cov=cov, size=length)
-        for mean, cov, length in zip(means, covs, lengths)
+        for mean, cov, length in zip(means, covariances, lengths)
     ]
 
     return np.array(np.concatenate(tuple(segments_data)))
 
 
-def mean_shift(
+def piecewise_normal(
     means: npt.ArrayLike,
     lengths: npt.ArrayLike,
     std_dev: Union[npt.ArrayLike, float] = 1.0,
@@ -152,18 +160,19 @@ def mean_shift(
 
     Examples
     --------
-    >>> from sktime.annotation.datagen import mean_shift
-    >>> mean_shift([1, 2, 3], lengths=[2, 4, 8], random_state=42) # doctest: +SKIP
+    >>> from sktime.annotation.datagen import piecewise_normal
+    >>> piecewise_normal([1, 2, 3], lengths=[2, 4, 8], random_state=42) # doctest: +SKIP
     array([1.49671415, 0.8617357 , 2.64768854, 3.52302986, 1.76584663,
         1.76586304, 4.57921282, 3.76743473, 2.53052561, 3.54256004,
         2.53658231, 2.53427025, 3.24196227, 1.08671976])
 
-    >>> from sktime.annotation.datagen import mean_shift
-    >>> mean_shift([1, 2, 3], lengths=[2, 4, 8], std_dev=0) # doctest: +SKIP
+    >>> from sktime.annotation.datagen import piecewise_normal
+    >>> piecewise_normal([1, 2, 3], lengths=[2, 4, 8], std_dev=0) # doctest: +SKIP
     array([1., 1., 2., 2., 2., 2., 3., 3., 3., 3., 3., 3., 3., 3.])
 
-    >>> from sktime.annotation.datagen import mean_shift
-    >>> mean_shift([1, 2, 3], lengths=[2, 4, 8], std_dev=[0, 0.5, 1.0]) # doctest: +SKIP
+    >>> from sktime.annotation.datagen import piecewise_normal
+    >>> piecewise_normal([1, 2, 3], lengths=[2, 4, 8], std_dev=[0, 0.5, 1.0])
+    # doctest: +SKIP
     array([1.        , 1.        , 2.32384427, 2.76151493, 1.88292331,
         1.88293152, 4.57921282, 3.76743473, 2.53052561, 3.54256004,
         2.53658231, 2.53427025, 3.24196227, 1.08671976])
@@ -192,7 +201,7 @@ def labels_with_repeats(means: npt.ArrayLike, std_dev: npt.ArrayLike) -> npt.Arr
     return labels[indices]
 
 
-def label_mean_shift(
+def label_piecewise_normal(
     means: npt.ArrayLike,
     lengths: npt.ArrayLike,
     std_dev: Union[npt.ArrayLike, float] = 1.0,
@@ -240,7 +249,7 @@ class GenBasicGauss:
 
     def sample(self):
         """Generate univariate mean shift random data sample."""
-        return mean_shift(
+        return piecewise_normal(
             means=self.means,
             lengths=self.lengths,
             std_dev=self.std_dev,
