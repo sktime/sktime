6fd345fbf7abf4faba793eb192db3272a3acac5f
Katie Buchhorn <katiebuchhorn@gmail.com>
2022-08-17 16:04:27 +1000

    missed an EOL backslash

--- sktime/annotation/datagen.py
+++ sktime/annotation/datagen.py
@@ -67,7 +67,7 @@ def piecewise_normal_multivariate(
 
     >>> from sktime.annotation.datagen import piecewise_normal_multivariate
     >>> piecewise_normal_multivariate([[1, 1], [2, 2], [3, 3]], lengths=[2, 3, 1],\
-        covariances = [[[1.0, 0], [0, 1.0]], [[1.0, 0], [0, 1.0]], [[1.0, 0],
+        covariances = [[[1.0, 0], [0, 1.0]], [[1.0, 0], [0, 1.0]], [[1.0, 0],\
         [0, 1.0]]], random_state=2)
     array([[ 0.58324215,  0.94373317],
            [-1.1361961 ,  2.64027081],
