26b52270c6636f4697d0b5ee8142044607069146
benHeid <benHeid@users.noreply.github.com>
2022-08-19 15:25:00 +0200

    * Fix linting errors

--- sktime/utils/_testing/deep_equals.py
+++ sktime/utils/_testing/deep_equals.py
@@ -15,10 +15,6 @@ __all__ = ["deep_equals"]
 import numpy as np
 import pandas as pd
 
-from sktime.utils.validation._dependencies import _check_soft_dependencies
-
-
-
 
 def deep_equals(x, y, return_msg=False):
     """Test two objects for equality in value.
