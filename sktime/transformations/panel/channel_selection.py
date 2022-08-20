0a6afa2cc6dd5ee7a0188d216a08bac9c565fb78
Franz Király <f.kiraly@ucl.ac.uk>
2022-08-20 16:56:44 +0100

    test cases

--- sktime/transformations/panel/channel_selection.py
+++ sktime/transformations/panel/channel_selection.py
@@ -260,6 +260,34 @@ class ElbowClassSum(BaseTransformer):
         return X[self.channels_selected]
 
 
+    @classmethod
+    def get_test_params(cls, parameter_set="default"):
+        """Return testing parameter settings for the estimator.
+
+        Parameters
+        ----------
+        parameter_set : str, default="default"
+            Name of the set of test parameters to return, for use in tests. If no
+            special parameters are defined for a value, will return `"default"` set.
+
+        Returns
+        -------
+        params : dict or list of dict, default = {}
+            Parameters to create testing instances of the class
+            Each dict are parameters to construct an "interesting" test instance, i.e.,
+            `MyClass(**params)` or `MyClass(**params[i])` creates a valid test instance.
+            `create_test_instance` uses the first (or only) dictionary in `params`
+        """
+        from sktime.dists_kernels import DtwDist
+
+        # default params
+        params1 = {}
+
+        # with custom distance
+        params2 = {"distance": DtwDist()}
+
+        return [params1, params2]
+
 class ElbowClassPairwise(BaseTransformer):
     """Elbow Class Pairwise (ECP) transformer to select a subset of channels.
 
