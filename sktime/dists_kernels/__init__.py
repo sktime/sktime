0ced8e775d06921ebdb61504a23d6dc27cd51af9
Franz Király <f.kiraly@ucl.ac.uk>
2022-08-20 16:57:25 +0100

    export dtwdist fix merge acc

--- sktime/dists_kernels/__init__.py
+++ sktime/dists_kernels/__init__.py
@@ -6,6 +6,7 @@ from sktime.dists_kernels._base import (
     BasePairwiseTransformerPanel,
 )
 from sktime.dists_kernels.compose_tab_to_panel import AggrDist, FlatDist
+from sktime.dists_kernels.dtw import DtwDist
 from sktime.dists_kernels.edit_dist import EditDist
 from sktime.dists_kernels.scipy_dist import ScipyDist
 
@@ -13,6 +14,7 @@ __all__ = [
     "BasePairwiseTransformer",
     "BasePairwiseTransformerPanel",
     "AggrDist",
+    "DtwDist",
     "EditDist",
     "FlatDist",
     "ScipyDist",
