49b243af52b0099c4daf6e11c03e33af88eb99d2
ltsaprounis <leonidas.tsap@gmail.com>
2022-08-26 00:03:27 +0100

    enforce PeriodIndex for meaningful fit

--- sktime/transformations/series/fourier.py
+++ sktime/transformations/series/fourier.py
@@ -9,6 +9,7 @@ from copy import deepcopy
 from typing import List, Union
 
 import numpy as np
+import pandas as pd
 
 from sktime.transformations.base import BaseTransformer
 
@@ -44,26 +45,6 @@ class FourierFeatures(BaseTransformer):
     >>> y_hat = transformer.fit_transform(y)
     """
 
-    # todo: fill out estimator tags here
-    #  tags are inherited from parent class if they are not set
-    #
-    # todo: define the transformer scitype by setting the tags
-    #   scitype:transform-input - the expected input scitype of X
-    #   scitype:transform-output - the output scitype that transform produces
-    #   scitype:transform-labels - whether y is used and if yes which scitype
-    #   scitype:instancewise - whether transform uses all samples or acts by instance
-    #
-    # todo: define internal types for X, y in _fit/_transform by setting the tags
-    #   X_inner_mtype - the internal mtype used for X in _fit and _transform
-    #   y_inner_mtype - if y is used, the internal mtype used for y; usually "None"
-    #   setting this guarantees that X, y passed to _fit, _transform are of above types
-    #   for possible mtypes see datatypes.MTYPE_REGISTER, or the datatypes tutorial
-    #
-    #  when scitype:transform-input is set to Panel:
-    #   X_inner_mtype must be changed to one or a list of sktime Panel mtypes
-    #  when scitype:transform-labels is set to Series or Panel:
-    #   y_inner_mtype must be changed to one or a list of compatible sktime mtypes
-    #  the other tags are "safe defaults" which can usually be left as-is
     _tags = {
         "scitype:transform-input": "Series",
         # what is the scitype of X: Series, or Panel
@@ -78,12 +59,13 @@ class FourierFeatures(BaseTransformer):
         # this can be a Panel mtype even if transform-input is Series, vectorized
         "y_inner_mtype": "None",  # which mtypes do _fit/_predict support for y?
         "requires_y": False,  # does y need to be passed in fit?
-        "enforce_index_type": None,  # index type that needs to be enforced in X/y
+        "enforce_index_type": pd.PeriodIndex,  # index type that needs to be enforced
+        # in X/y
         "fit_is_empty": False,  # is fit empty and can be skipped? Yes = True
         "X-y-must-have-same-index": False,  # can estimator handle different X/y index?
         "transform-returns-same-time-index": True,
         # does transform return have the same time index as input X
-        "skip-inverse-transform": False,  # is inverse-transform skipped when called?
+        "skip-inverse-transform": True,  # is inverse-transform skipped when called?
         "capability:unequal_length": False,
         # can the transformer handle unequal length time series (if passed Panel)?
         "capability:unequal_length:removes": False,
@@ -95,43 +77,14 @@ class FourierFeatures(BaseTransformer):
         # is transform result always guaranteed to contain no missing values?
         "python_version": None,  # PEP 440 python version specifier to limit versions
     }
-    # in case of inheritance, concrete class should typically set tags
-    #  alternatively, descendants can set tags in __init__
-    #  avoid if possible, but see __init__ for instructions when needed
 
     # todo: add any hyper-parameters and components to constructor
     def __init__(self, sp_list: List[Union[int, float]], fourier_terms_list: List[int]):
-        # estimators should precede parameters
-        #  if estimators have default values, set None and initalize below
-
-        # todo: write any hyper-parameters and components to self
         self.sp_list = sp_list
         self.fourier_terms_list = fourier_terms_list
 
-        # todo: change "MyTransformer" to the name of the class
         super(FourierFeatures, self).__init__()
 
-        # todo: optional, parameter checking logic (if applicable) should happen here
-        # if writes derived values to self, should *not* overwrite self.parama etc
-        # instead, write to self._parama, self._newparam (starting with _)
-
-        # todo: default estimators should have None arg defaults
-        #  and be initialized here
-        #  do this only with default estimators, not with parameters
-        # if est2 is None:
-        #     self.est2 = MyDefaultEstimator()
-
-        # todo: if tags of estimator depend on component tags, set these here
-        #  only needed if estimator is a composite
-        #  tags set in the constructor apply to the object and override the class
-        #
-        # example 1: conditional setting of a tag
-        # if est.foo == 42:
-        #   self.set_tags(handles-missing-data=True)
-        # example 2: cloning tags from component
-        #   self.clone_tags(est2, ["enforce_index_type", "handles-missing-data"])
-
-    # todo: implement this, mandatory (except in special case below)
     def _fit(self, X, y=None):
         """Fit transformer to X and y.
 
@@ -178,28 +131,13 @@ class FourierFeatures(BaseTransformer):
                         "exists from other seasonal period, fourier term pairs."
                     )
 
+        # store the integer form of the minimum date in the prediod index
+        # this is used to make sure that time t is calculated with reference to
+        # the data passed on fit
+        self.min_t_ = np.min(X.index.astype(int))
+
         return self
 
-        # implement here
-        # X, y passed to this function are always of X_inner_mtype, y_inner_mtype
-        # IMPORTANT: avoid side effects to X, y
-        #
-        # any model parameters should be written to attributes ending in "_"
-        #  attributes set by the constructor must not be overwritten
-        #  if used, estimators should be cloned to attributes ending in "_"
-        #  the clones, not the originals, should be used or fitted if needed
-        #
-        # special case: if no fitting happens before transformation
-        #  then: delete _fit (don't implement)
-        #   set "fit_is_empty" tag to True
-        #
-        # Note: when interfacing a model that has fit, with parameters
-        #   that are not data (X, y) or data-like,
-        #   but model parameters, *don't* add as arguments to fit, but treat as follows:
-        #   1. pass to constructor,  2. write to self in constructor,
-        #   3. read from self in _fit,  4. pass to interfaced_model.fit in _fit
-
-    # todo: implement this, mandatory
     def _transform(self, X, y=None):
         """Transform X and return a transformed version.
 
@@ -218,9 +156,8 @@ class FourierFeatures(BaseTransformer):
         transformed version of X
         """
         X_transformed = deepcopy(X)
-        int_index = X_transformed.reset_index().index
+        int_index = X_transformed.index.astype(int) - self.min_t_
 
-        X_transformed["t"] = int_index
         for sp_k in self.sp_k_pairs_list_:
             sp = sp_k[0]
             k = sp_k[1]
@@ -230,85 +167,6 @@ class FourierFeatures(BaseTransformer):
 
         return X_transformed
 
-    # todo: consider implementing this, optional
-    # if not implementing, delete the _inverse_transform method
-    # inverse transform exists only if transform does not change scitype
-    #  i.e., Series transformed to Series
-    def _inverse_transform(self, X, y=None):
-        """Inverse transform, inverse operation to transform.
-
-        private _inverse_transform containing core logic, called from inverse_transform
-
-        Parameters
-        ----------
-        X : Series or Panel of mtype X_inner_mtype
-            if X_inner_mtype is list, _inverse_transform must support all types in it
-            Data to be inverse transformed
-        y : Series or Panel of mtype y_inner_mtype, optional (default=None)
-            Additional data, e.g., labels for transformation
-
-        Returns
-        -------
-        inverse transformed version of X
-        """
-        # implement here
-        # IMPORTANT: avoid side effects to X, y
-        #
-        # type conventions are exactly those in _transform, reversed
-        #
-        # for example: if transform-output is "Series":
-        #  return should be of same mtype as input, X_inner_mtype
-        #  if multiple X_inner_mtype are supported, ensure same input/output
-        #
-        # todo: add the return mtype/scitype to the docstring, e.g.,
-        #  Returns
-        #  -------
-        #  X_inv_transformed : Series of mtype pd.DataFrame
-        #       inverse transformed version of X
-
-    # todo: consider implementing this, optional
-    # if not implementing, delete the _update method
-    # standard behaviour is "no update"
-    # also delete in the case where there is no fitting
-    def _update(self, X, y=None):
-        """Update transformer with X and y.
-
-        private _update containing the core logic, called from update
-
-        Parameters
-        ----------
-        X : Series or Panel of mtype X_inner_mtype
-            if X_inner_mtype is list, _update must support all types in it
-            Data to update transformer with
-        y : Series or Panel of mtype y_inner_mtype, default=None
-            Additional data, e.g., labels for tarnsformation
-
-        Returns
-        -------
-        self: reference to self
-        """
-        # implement here
-        # X, y passed to this function are always of X_inner_mtype, y_inner_mtype
-        # IMPORTANT: avoid side effects to X, y
-        #
-        # any model parameters should be written to attributes ending in "_"
-        #  attributes set by the constructor must not be overwritten
-        #  if used, estimators should be cloned to attributes ending in "_"
-        #  the clones, not the originals, should be used or fitted if needed
-
-    # todo: consider implementing this, optional
-    # if not implementing, delete the method
-    def get_fitted_params(self):
-        """Get fitted parameters.
-
-        Returns
-        -------
-        fitted_params : dict
-        """
-        # implement here
-
-    # todo: return default parameters, so that a test instance can be created
-    #   required for automated unit and integration testing of estimator
     @classmethod
     def get_test_params(cls, parameter_set="default"):
         """Return testing parameter settings for the estimator.
