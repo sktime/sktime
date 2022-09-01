37c21bf94d1b7418ddbc26d46db8b5080d6ec9d6
Katie Buchhorn <katiebuchhorn@gmail.com>
2022-09-01 13:19:08 +1000

    wip

--- sktime/transformations/series/dobin.py
+++ sktime/transformations/series/dobin.py
@@ -2,3 +2,47 @@
 """Preprocessing algorithm DOBIN (Distance based Outlier BasIs using Neighbors)."""
 
 import numpy as np
+
+__author__ = ["KatieBuc"]
+__all__ = ["DOBIN"]
+
+class DOBIN(BaseTransformer):
+    """
+
+    Parameters
+    ----------
+
+
+    Attributes
+    ----------
+
+
+    References
+    ----------
+
+
+    Examples
+    --------
+    """
+
+    def __init__(
+        self,
+        alpha=0.01,
+        k=10,
+        knn_algorithm="brute",
+        normalize=unitize,
+        p=0.5,
+        size_threshold=50,
+        outlier_tail="max",
+    ):
+        self.alpha = alpha
+        self.k = k
+        self.knn_algorithm = knn_algorithm
+        self.normalize = normalize
+        self.p = p
+        self.size_threshold = size_threshold
+        self.outlier_tail = outlier_tail
+        super(STRAY, self).__init__()
+
+    def():
+        
