8b7d5683fc3c3c091589b47297315b8303a2dcfc
Franz Király <f.kiraly@ucl.ac.uk>
2022-09-11 20:44:01 +0100

    linting

--- sktime/classification/deep_learning/mlp.py
+++ sktime/classification/deep_learning/mlp.py
@@ -57,9 +57,8 @@ class MLPClassifier(BaseDeepClassifier):
     >>> from sktime.classification.deep_learning.mlp import MLPClassifier
     >>> from sktime.datasets import load_unit_test
     >>> X_train, y_train = load_unit_test(split="train", return_X_y=True)
-    >>> X_test, y_test = load_unit_test(split="test", return_X_y=True)
-    >>> mlp = MLPClassifier() # doctest: +SKIP
-    >>> mlp.fit(X_train, y_train) # doctest: +SKIP
+    >>> mlp = MLPClassifier()  # doctest: +SKIP
+    >>> mlp.fit(X_train, y_train)  # doctest: +SKIP
     MLPClassifier(...)
     """
 
