3fad66ac1d4c7e19386e39b0d3733fd136cec30d
Mirae Parker <miraep8@gmail.com>
2022-09-12 11:35:48 -0400

    fixing bugs

--- sktime/annotation/hmm_learn/base.py
+++ sktime/annotation/hmm_learn/base.py
@@ -10,6 +10,7 @@ Please see the original library
 (https://github.com/hmmlearn/hmmlearn/blob/main/lib/hmmlearn/hmm.py)
 """
 
+import numpy as np
 import pandas as pd
 
 from sktime.annotation.base import BaseSeriesAnnotator
@@ -27,18 +28,62 @@ class BaseHMMLearn(BaseSeriesAnnotator):
     def __init__(self):
         super(BaseHMMLearn, self).__init__()
 
-    def _fit(self, X, Y=None):
+    @staticmethod
+    def _fix_input(X):
+        """Convert input X into the format needed.
+
+        Parameters
+        ----------
+        X : arraylike (1D np.ndarray or pd.series), shape = [num_observations]
+            Observations to apply labels to.
+
+        Returns
+        -------
+        X : arraylike (2D np.ndarray), shape = [1, num_observations]
+            Observations to apply labels to.
+        series: bool - whether or not X was originally a pd.Series
+        index: pd.index, the index if X was originally a series object.
+        """
         series = isinstance(X, pd.Series)
+        index = None
         if series:
+            index = X.index
             X = (X.to_numpy()).reshape((-1, 1))
+        if isinstance(X, np.ndarray) and X.ndim == 1:
+            X = X.reshape((-1, 1))
+        return X, series, index
+
+    def _fit(self, X, Y=None):
+        """Ensure X is correct type, then fit wrapped estimator.
+
+        Parameters
+        ----------
+        X : arraylike (1D np.ndarray or pd.series), shape = [num_observations]
+            Observations to apply labels to.
+
+        Returns
+        -------
+        self :
+            Reference to self.
+        """
+        X, _, _ = self._fix_input(X)
         self._hmm_estimator = self._hmm_estimator.fit(X)
         return self
 
     def _predict(self, X):
-        series = isinstance(X, pd.Series)
-        if series:
-            index = X.index
-            X = (X.to_numpy()).reshape((-1, 1))
+        """Ensure the input type is correct, then predict using wrapped estimator.
+
+        Parameters
+        ----------
+        X : 1D np.array, shape = [num_observations]
+            Observations to apply labels to.
+
+        Returns
+        -------
+        annotated_x : array-like, shape = [num_observations]
+            Array of predicted class labels, same size as input.
+        """
+        X, series, index = self._fix_input(X)
         X_prime = self._hmm_estimator.predict(X)
         if series:
             X_prime = pd.Series(X_prime, index=index)
