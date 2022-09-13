77fc971a9c37cdf30d787295c7b167e38e17f1ad
Lukasz Mentel <lmmentel@gmail.com>
2022-09-13 13:03:59 +0200

    :poop:

--- sktime/annotation/igts.py
+++ sktime/annotation/igts.py
@@ -21,7 +21,7 @@ import pandas as pd
 from attrs import asdict, define
 
 from sktime.base import BaseEstimator
-from sktime.utils.validation._dependencies import _check_soft_dependencies
+# from sktime.utils.validation._dependencies import _check_soft_dependencies
 
 __all__ = ["InformationGainSegmentation"]
 __author__ = ["lmmentel"]
