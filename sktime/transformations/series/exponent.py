c4af87e7e88693009a12512219ef88a93a727cbb
Franz Király <f.kiraly@ucl.ac.uk>
2022-09-15 00:27:57 +0100

    SqrtTransformer

--- sktime/transformations/series/exponent.py
+++ sktime/transformations/series/exponent.py
@@ -241,3 +241,24 @@ class SqrtTransformer(ExponentTransformer):
 
     def __init__(self, offset="auto"):
         super().__init__(power=0.5, offset=offset)
+
+    @classmethod
+    def get_test_params(cls, parameter_set="default"):
+        """Return testing parameter settings for the estimator.
+
+        Parameters
+        ----------
+        parameter_set : str, default="default"
+            Name of the set of test parameters to return, for use in tests. If no
+            special parameters are defined for a value, will return `"default"` set.
+            There are currently no reserved values for transformers.
+
+        Returns
+        -------
+        params : dict or list of dict, default = {}
+            Parameters to create testing instances of the class
+            Each dict are parameters to construct an "interesting" test instance, i.e.,
+            `MyClass(**params)` or `MyClass(**params[i])` creates a valid test instance.
+            `create_test_instance` uses the first (or only) dictionary in `params`
+        """
+        return [{}, {"offset": 4.2}]
