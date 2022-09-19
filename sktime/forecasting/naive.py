fix: Formula for sigma_res

sktime/forecasting/naive.py
@@ -436,7 +436,7 @@ class NaiveForecaster(_BaseWindowForecaster):
             if sp == 1
             else np.sqrt(np.floor((h - 1) / sp) + 1),
             "mean": lambda h: np.repeat(np.sqrt(1 + (1 / window_length)), len(h)),
-            "drift": lambda h: np.sqrt(h * (1 + h / (T - 1))),
+            "drift": lambda h: np.sqrt(h * (1 + (h / (T - 1)))),
         }
 
         fh_periods = np.array(fh.to_relative(self.cutoff))

sktime/forecasting/tests/test_naive.py
@@ -305,9 +305,9 @@ def test_naive_predict_var_backwards(strategy, sp, window_length):
         # This is trival because square root of (h) when h=1 is just 1
         sigma_res = sigma / np.sqrt(h)
     elif strategy == "mean":
-        sigma_res = sigma / np.sqrt(2 / T)
+        sigma_res = sigma / np.sqrt(1 + (1 / T))
     else:
-        sigma_res = sigma / np.sqrt(2 / (T - 1))
+        sigma_res = sigma / np.sqrt(1 + (1 / (T - 1)))
 
     upper_bound, lower_bound = (sigma + 0.001), (sigma - 0.001)
     assert lower_bound < sigma_res < upper_bound