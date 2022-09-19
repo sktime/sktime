e7dbf33ff8a707a91bc655708b6a1d556f824aa5
Katie Buchhorn <katiebuchhorn@gmail.com>
2022-09-19 17:06:46 +1000

    unit tests

--- sktime/annotation/tests/test_e_agglo.py
+++ sktime/annotation/tests/test_e_agglo.py
@@ -9,21 +9,55 @@ import pandas as pd
 from sktime.annotation.e_agglo import EAGGLO
 
 
-def test_process_data():
+def test_default_params_univariate():
     """Test data."""
     X = pd.DataFrame([-7.207066, -5.722571, 5.889715, 5.488990])
-    member = np.array([0, 0, 1, 2])
 
-    model = EAGGLO(member=member)
-    model._process_data(X)
-    model.N_
-    model.sizes_
-    model.D_
-    model.left_
-    model.right_
-    model.open_
-    model.merged_
-    model.D_
-    model.fit_
-    model.progression_
-    model.lm_
+    cluster_expected = [0, 0, 1, 1]
+    fit_expected = [104.77424, 134.51387, 186.92586, -31.15431]
+
+    model = EAGGLO()
+    fitted_model = model._fit(X)
+
+    cluster_actual = fitted_model.cluster_
+    fit_actual = fitted_model.fit_
+
+    assert np.allclose(cluster_actual, cluster_expected)
+    assert np.allclose(fit_actual, fit_expected)
+
+
+def test_other_params_univariate():
+    """Test data."""
+    X = pd.DataFrame([-7.207066, -5.722571, 5.889715, 5.488990])
+
+    cluster_expected = [0, 0, 1, 1]
+    fit_expected = [1182.754, 1772.526, -295.421]
+
+    model = EAGGLO(member=np.array([0, 0, 1, 2]), alpha=2)
+    fitted_model = model._fit(X)
+
+    cluster_actual = fitted_model.cluster_
+    fit_actual = fitted_model.fit_
+
+    assert np.allclose(cluster_actual, cluster_expected)
+    assert np.allclose(fit_actual, fit_expected)
+
+
+def test_other_params_multivariate():
+    """Test data."""
+    X = pd.DataFrame(
+        [-6.475593, -6.709440, 4.892682, 4.748014, 5.476172],
+        [-6.501258, -7.629093, -7.167619, -8.180040, -7.340993],
+    )
+
+    cluster_expected = [0, 0, 1, 1, 1]
+    fit_expected = [118.57864, 132.67679, 156.54321, 208.60813, -33.52631]
+
+    model = EAGGLO()
+    fitted_model = model._fit(X)
+
+    cluster_actual = fitted_model.cluster_
+    fit_actual = fitted_model.fit_
+
+    assert np.allclose(cluster_actual, cluster_expected)
+    assert np.allclose(fit_actual, fit_expected)
