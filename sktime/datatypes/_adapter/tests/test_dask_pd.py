dbe246195096def51311c2b0141eb98fc951aede
Franz Király <f.kiraly@ucl.ac.uk>
2022-10-01 16:15:25 +0100

    linting

--- sktime/datatypes/_adapter/tests/test_dask_pd.py
+++ sktime/datatypes/_adapter/tests/test_dask_pd.py
@@ -8,10 +8,10 @@ import pandas as pd
 import pytest
 
 from sktime.datatypes._adapter.dask import (
-    convert_dask_to_pandas, convert_pandas_to_dask
+    convert_dask_to_pandas,
+    convert_pandas_to_dask,
 )
 from sktime.utils.validation._dependencies import _check_soft_dependencies
-
 
 # simple pd.DataFrame fixture
 pd_fixture_simple = pd.DataFrame({"foo": [2, 3, 4], "bar": [3, 4, 5]})
