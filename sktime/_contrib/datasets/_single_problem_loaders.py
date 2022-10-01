1b645d7afab233d400488fd9451615363db85900
Franz Király <f.kiraly@ucl.ac.uk>
2022-10-01 20:46:00 +0100

    _single_problem_loaders

--- sktime/_contrib/datasets/_single_problem_loaders.py
+++ sktime/_contrib/datasets/_single_problem_loaders.py
@@ -41,13 +41,13 @@ import os
 
 import numpy as np
 import pandas as pd
-import statsmodels.api as sm
 
 from sktime._contrib.datasets._data_io import (
     _load_dataset,
     _load_provided_dataset,
     load_tsf_to_dataframe,
 )
+from sktime.utils.validation._dependencies import _check_soft_dependencies
 
 DIRNAME = "data"
 MODULE = os.path.dirname(__file__)
@@ -936,6 +936,9 @@ def load_macroeconomic():
     .. [3] Data Source: Bureau of Labor Statistics, U.S. Department of Labor;
           http://www.bls.gov/data/; accessed December 15, 2009.
     """
+    _check_soft_dependencies("statsmodels")
+    import statsmodels.api as sm
+
     y = sm.datasets.macrodata.load_pandas().data
     y["year"] = y["year"].astype(int).astype(str)
     y["quarter"] = y["quarter"].astype(int).astype(str).apply(lambda x: "Q" + x)
