6fd2766a961205790e845761257ca33bade73c48
Mirae Parker <miraep8@gmail.com>
2022-10-10 08:34:57 -0400

    adding back space

--- sktime/annotation/tests/test_all_annotators.py
+++ sktime/annotation/tests/test_all_annotators.py
@@ -11,6 +11,7 @@ from sktime.utils.validation._dependencies import _check_estimator_deps
 
 ALL_ANNOTATORS = all_estimators(estimator_types="series-annotator", return_names=False)
 
+
 @pytest.mark.parametrize("Estimator", ALL_ANNOTATORS)
 def test_output_type(Estimator):
     """Test annotator output type."""
