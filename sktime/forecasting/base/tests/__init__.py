05f46a61915b8799598eaa55de56d43e2bc02d67
Franz Király <f.kiraly@ucl.ac.uk>
2022-10-22 16:45:24 +0100

    forgotten export

--- sktime/forecasting/base/tests/__init__.py
+++ sktime/forecasting/base/tests/__init__.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python3 -u
-# coding: utf-8
+# -*- coding: utf-8 -*-
+"""Tests for the forecaster base module."""
 # copyright: sktime developers, BSD-3-Clause License (see LICENSE file)
 
 __author__ = ["mloning"]
