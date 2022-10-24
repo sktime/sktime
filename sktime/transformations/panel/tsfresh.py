b1976026ef03eb0956aeac9f5d16ebfef03c59dd
Franz Király <f.kiraly@ucl.ac.uk>
2022-10-24 16:35:54 +0100

    [MNT] replace author names by GitHub ID in author fields, linting (#3628)
    This PR replaces some author names by GitHub IDs in author fields.
    
    This is the convention, and full names are still linked via the contributor register.
    
    Also lints all the affected files - no content is changed.

--- sktime/transformations/panel/tsfresh.py
+++ sktime/transformations/panel/tsfresh.py
@@ -2,7 +2,7 @@
 """tsfresh interface class."""
 # copyright: sktime developers, BSD-3-Clause License (see LICENSE file)
 
-__author__ = ["AyushmaanSeth", "Markus Löning", "Alwin Wang"]
+__author__ = ["AyushmaanSeth", "mloning", "Alwin Wang"]
 __all__ = ["TSFreshFeatureExtractor", "TSFreshRelevantFeatureExtractor"]
 
 from warnings import warn
