bf619347d2059fd16c048fe935aa04d40d884eb2
Matthew Middlehurst <pfm15hbu@uea.ac.uk>
2022-10-26 13:59:50 +0100

    init imports

--- sktime/dists_kernels/distances/__init__.py
+++ sktime/dists_kernels/distances/__init__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-"""Module exports for dist_kernels module."""
+"""Module exports for dist_kernels distances subpackage."""
 
-from sktime.dists_kernels.compose_tab_to_panel import AggrDist, FlatDist
-from sktime.dists_kernels.dtw import DtwDist
-from sktime.dists_kernels.edit_dist import EditDist
-from sktime.dists_kernels.scipy_dist import ScipyDist
+from sktime.dists_kernels.distances.compose_tab_to_panel import AggrDist, FlatDist
+from sktime.dists_kernels.distances.dtw import DtwDist
+from sktime.dists_kernels.distances.edit_dist import EditDist
+from sktime.dists_kernels.distances.scipy_dist import ScipyDist
 
 __all__ = [
     "AggrDist",
