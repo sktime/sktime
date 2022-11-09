b385bba5ebba5487b09c9b66b0c293174c9d1c2d
Kai Lion <kalion@ethz.ch>
2022-11-09 17:37:44 +0100

    started working on TimeBinner

--- sktime/transformations/panel/reduce.py
+++ sktime/transformations/panel/reduce.py
@@ -5,6 +5,8 @@
 __author__ = ["mloning", "fkiraly"]
 __all__ = ["Tabularizer"]
 
+import pandas as pd
+
 from sktime.datatypes import convert, convert_to
 from sktime.transformations.base import BaseTransformer
 
@@ -67,3 +69,184 @@ class Tabularizer(BaseTransformer):
         """
         Xt = convert(X, from_type="numpyflat", to_type="numpy3D", as_scitype="Panel")
         return Xt
+
+
+class TimeBinner(BaseTransformer):
+    """A transformer that turns time series/panel data into tabular data.
+
+    This estimator converts nested pandas dataframe containing
+    time-series/panel data with numpy arrays or pandas Series in
+    dataframe cells into a tabular pandas dataframe with only primitives in
+    cells. The primitives are calculated based on Intervals defined
+    by the IntervalIndex and aggregated by aggfunc.
+
+    This is useful for transforming time-series/panel data
+    into a format that is accepted by standard validation learning
+    algorithms (as in sklearn).
+
+    Parameters
+    ----------
+    idx : pd.IntervalIndex
+        IntervalIndex defining intervals considered by aggfunc
+    aggfunc : callable
+        Function used to aggregate the values in intervals
+    """
+
+    # todo: fill out estimator tags here
+    #  tags are inherited from parent class if they are not set
+    #
+    # todo: define the transformer scitype by setting the tags
+    #   scitype:transform-input - the expected input scitype of X
+    #   scitype:transform-output - the output scitype that transform produces
+    #   scitype:transform-labels - whether y is used and if yes which scitype
+    #   scitype:instancewise - whether transform uses all samples or acts by instance
+    #
+    # todo: define internal types for X, y in _fit/_transform by setting the tags
+    #   X_inner_mtype - the internal mtype used for X in _fit and _transform
+    #   y_inner_mtype - if y is used, the internal mtype used for y; usually "None"
+    #   setting this guarantees that X, y passed to _fit, _transform are of above types
+    #   for possible mtypes see datatypes.MTYPE_REGISTER, or the datatypes tutorial
+    #
+    #  when scitype:transform-input is set to Panel:
+    #   X_inner_mtype must be changed to one or a list of sktime Panel mtypes
+    #  when scitype:transform-labels is set to Series or Panel:
+    #   y_inner_mtype must be changed to one or a list of compatible sktime mtypes
+    #  the other tags are "safe defaults" which can usually be left as-is
+    _tags = {
+        "fit_is_empty": True,
+        "univariate-only": False,
+        "scitype:transform-input": "Series",
+        # what is the scitype of X: Series, or Panel
+        "scitype:transform-output": "Primitives",
+        # what is the scitype of y: None (not needed), Primitives, Series, Panel
+        "scitype:instancewise": True,  # is this an instance-wise transform?
+        "X_inner_mtype": ["nested_univ", "numpy3D"],
+        # which mtypes do _fit/_predict support for X?
+        "y_inner_mtype": "None",  # and for y?
+    }
+
+    # todo: add any hyper-parameters and components to constructor
+    def __init__(self, idx, aggfunc):
+        # estimators should precede parameters
+        #  if estimators have default values, set None and initalize below
+
+        # todo: write any hyper-parameters and components to self
+        self.idx = idx
+        self.aggfunc = aggfunc
+
+        super(TimeBinner, self).__init__()
+
+        # todo: optional, parameter checking logic (if applicable) should happen here
+        assert isinstance(
+            self.idx, pd.IntervalIndex
+        ), "idx should be of type pd.IntervalIndex"
+        assert callable(self.aggfunc), "aggfunc should be a function"
+        # if writes derived values to self, should *not* overwrite self.parama etc
+        # instead, write to self._parama, self._newparam (starting with _)
+
+    # todo: implement this, mandatory
+    def _transform(self, X, y=None):
+        """Transform X and return a transformed version.
+
+        private _transform containing core logic, called from transform
+
+        Parameters
+        ----------
+        X : Series or Panel of mtype X_inner_mtype
+            if X_inner_mtype is list, _transform must support all types in it
+            Data to be transformed
+        y : Series or Panel of mtype y_inner_mtype, default=None
+            Additional data, e.g., labels for transformation
+
+        Returns
+        -------
+        transformed version of X
+        """
+        # implement here
+        # X, y passed to this function are always of X_inner_mtype, y_inner_mtype
+        # IMPORTANT: avoid side effects to X, y
+        #
+        # if transform-output is "Primitives":
+        #  return should be pd.DataFrame, with as many rows as instances in input
+        #  if input is a single series, return should be single-row pd.DataFrame
+        # if transform-output is "Series":
+        #  return should be of same mtype as input, X_inner_mtype
+        #  if multiple X_inner_mtype are supported, ensure same input/output
+        # if transform-output is "Panel":
+        #  return a multi-indexed pd.DataFrame of Panel mtype pd_multiindex
+        #
+        # todo: add the return mtype/scitype to the docstring, e.g.,
+        #  Returns
+        #  -------
+        #  X_transformed : Series of mtype pd.DataFrame
+        #       transformed version of X
+        idx = pd.cut(X.iloc[0, 0].index, bins=self.idx, include_lowest=True)
+        Xt = X.applymap(lambda x: x.groupby(idx).aggregate(self.aggfunc))
+        Xt = convert_to(Xt, to_type="numpyflat", as_scitype="Panel")
+        return Xt
+
+    # todo: return default parameters, so that a test instance can be created
+    #   required for automated unit and integration testing of estimator
+    @classmethod
+    def get_test_params(cls, parameter_set="default"):
+        """Return testing parameter settings for the estimator.
+
+        Parameters
+        ----------
+        parameter_set : str, default="default"
+            Name of the set of test parameters to return, for use in tests. If no
+            special parameters are defined for a value, will return `"default"` set.
+            There are currently no reserved values for transformers.
+
+        Returns
+        -------
+        params : dict or list of dict, default = {}
+            Parameters to create testing instances of the class
+            Each dict are parameters to construct an "interesting" test instance, i.e.,
+            `MyClass(**params)` or `MyClass(**params[i])` creates a valid test instance.
+            `create_test_instance` uses the first (or only) dictionary in `params`
+        """
+
+        # todo: set the testing parameters for the estimators
+        # Testing parameters can be dictionary or list of dictionaries
+        # Testing parameter choice should cover internal cases well.
+        #
+        # this method can, if required, use:
+        #   class properties (e.g., inherited); parent class test case
+        #   imported objects such as estimators from sktime or sklearn
+        # important: all such imports should be *inside get_test_params*, not at the top
+        #            since imports are used only at testing time
+        #
+        # The parameter_set argument is not used for automated, module level tests.
+        #   It can be used in custom, estimator specific tests, for "special" settings.
+        # A parameter dictionary must be returned *for all values* of parameter_set,
+        #   i.e., "parameter_set not available" errors should never be raised.
+        #
+        # example 1: specify params as dictionary
+        # any number of params can be specified
+        # params = {"est": value0, "parama": value1, "paramb": value2}
+        #
+        # example 2: specify params as list of dictionary
+        # note: Only first dictionary will be used by create_test_instance
+        # params = [{"est": value1, "parama": value2},
+        #           {"est": value3, "parama": value4}]
+        # return params
+        #
+        # example 3: parameter set depending on param_set value
+        #   note: only needed if a separate parameter set is needed in tests
+        # if parameter_set == "special_param_set":
+        #     params = {"est": value1, "parama": value2}
+        #     return params
+        #
+        # # "default" params - always returned except for "special_param_set" value
+        # params = {"est": value3, "parama": value4}
+        # return params
+        import pandas as pd
+
+        idx = pd.interval_range(start=0, end=100, freq=10, closed="left")
+
+        def aggfunc(x):
+            return sum(x)
+
+        params = {"idx": idx, "aggfunc": aggfunc}
+        return params
---
+++ sktime/transformations/panel/tests/test_timebinner.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""Tests for TimeBinner."""
+
+import numpy as np
+import pandas as pd
+
+from sktime.datasets import load_basic_motions
+from sktime.transformations.panel.reduce import TimeBinner
+
+
+def test_timebinner():
+    """ "Test TimeBinner."""
+    X, y = load_basic_motions(return_X_y=True)
+
+    def aggfunc(x):
+        return sum(x)
+
+    freq = 8
+    idx = pd.interval_range(start=0, end=100, freq=freq, closed="left")
+    tb = TimeBinner(idx=idx, aggfunc=aggfunc)
+    tb.fit(X)
+    Xtb = tb.transform(X)
+    assert np.isclose(np.sum(X.iloc[0, 0][freq : (2 * freq)]), Xtb.iloc[1, 1])
+
+
+def test_timebinner2():
+    """ "Test TimeBinner."""
+    X, y = load_basic_motions(return_X_y=True)
+
+    def aggfunc(x):
+        return min(x)
+
+    freq = 10
+    idx = pd.interval_range(start=0, end=100, freq=freq, closed="left")
+    tb = TimeBinner(idx=idx, aggfunc=aggfunc)
+    tb.fit(X)
+    Xtb = tb.transform(X)
+    assert np.isclose(np.sum(X.iloc[3, 5][8 * 10 : 9 * 10]), Xtb.iloc[3, 58])
