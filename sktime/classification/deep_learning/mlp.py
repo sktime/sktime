63e4965920debf4059d36e7cefa7c8e586119aac
Tony Bagnall <ajb@uea.ac.uk>
2022-11-11 09:50:59 +0000

    mlp classification test parameters

--- sktime/classification/deep_learning/mlp.py
+++ sktime/classification/deep_learning/mlp.py
@@ -172,3 +172,39 @@ class MLPClassifier(BaseDeepClassifier):
             callbacks=self._callbacks,
         )
         return self
+
+    @classmethod
+    def get_test_params(cls, parameter_set="default"):
+        """Return testing parameter settings for the estimator.
+
+        Parameters
+        ----------
+        parameter_set : str, default="default"
+            Name of the set of test parameters to return, for use in tests. If no
+            special parameters are defined for a value, will return `"default"` set.
+            For classifiers, a "default" set of parameters should be provided for
+            general testing, and a "results_comparison" set for comparing against
+            previously recorded results if the general set does not produce suitable
+            probabilities to compare against.
+
+        Returns
+        -------
+        params : dict or list of dict, default={}
+            Parameters to create testing instances of the class.
+            Each dict are parameters to construct an "interesting" test instance, i.e.,
+            `MyClass(**params)` or `MyClass(**params[i])` creates a valid test instance.
+            `create_test_instance` uses the first (or only) dictionary in `params`.
+        """
+        param1 = {
+            "n_epochs": 10,
+            "batch_size": 4,
+            "use_bias": False,
+        }
+
+        param2 = {
+            "n_epochs": 12,
+            "batch_size": 6,
+            "use_bias": True,
+        }
+
+        return [param1, param2]
