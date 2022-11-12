f3eff8a4412f5b8856f104834af7f1651c6c1ed9
AurumnPegasus <subramanian.shivansh@gmail.com>
2022-11-13 01:06:47 +0530

    mvp of forecaster completed

--- sktime/forecasting/deep_learning/base.py
+++ sktime/forecasting/deep_learning/base.py
@@ -11,9 +11,30 @@ from sktime.forecasting.base import BaseForecaster
 
 
 class BaseDeepForecastor(BaseForecaster, ABC):
-    """Temp."""
+    """Abstract base class for deep learning time series forecasters.
 
-    def __init__(self, batch_size=40):
+    The base classifier provides a deep learning default method for
+    _predict, and provides a new abstract method for building a
+    model.
+
+    Parameters
+    ----------
+    batch_size : int, default = 4
+        training batch size for the model
+
+    Arguments
+    ---------
+    self.model = None
+
+    """
+
+    _tags = {
+        "X_inner_mtype": "numpy3D",
+        "capability:multivariate": True,
+        "python_dependencies": "tensorflow",
+    }
+
+    def __init__(self, batch_size=4, random_state=None):
         super(BaseDeepForecastor).__init__()
 
         self.batch_size = batch_size
@@ -21,12 +42,73 @@ class BaseDeepForecastor(BaseForecaster, ABC):
 
     @abstractmethod
     def build_model(self, input_shape, **kwargs):
-        """Temp."""
+        """Construct a compiled, un-trained, keras model that is ready for training.
+
+        Parameters
+        ----------
+        input_shape : tuple
+            The shape of the data fed into the input layer
+        n_classes: int
+            The number of classes, which shall become the size of the output
+            layer
+
+        Returns
+        -------
+        A compiled Keras Model
+        """
         ...
 
     def _predict(self, fh, X=None):
-        """Temp."""
-        X = X.transpose((0, 2, 1))
-        y_pred = self.model_.predict(X, self.batch_size)
-        y_pred = np.squeeze(y_pred, axis=-1)
-        return y_pred
+        """Get predictions for steps mentioned in fh based on given y and X.
+
+        Parameters
+        ----------
+        fh: list of int
+            Forecasting Horizon for the forecaster.
+        X: np.ndarray of shape = (n_instances (n), exog_dimensions (d))
+            Exogeneous data for data prediction.
+
+        Returns
+        -------
+        fvalues: list with predictions of relevant fh.
+        """
+        currentPred = 1
+        lastPred = max(fh)
+        fvalues = []
+        fh = set(fh)
+        source = self.source[-1]
+        source = source[np.newaxis, :, :]
+        while currentPred <= lastPred:
+            yhat = self.model_.predict(source)
+            source = np.delete(source, axis=2, obj=0)
+            source = np.insert(source, obj=source.shape[-1], values=yhat, axis=-1)
+            if currentPred in fh:
+                fvalues.append(yhat)
+
+            currentPred += 1
+        return fvalues
+
+    def splitSeq(self, steps, seq):
+        """Get window sized instances of sequence.
+
+        Parameters
+        ----------
+        steps: int
+            Window Size of the forecaster.
+        seq: np.ndarray of shape = (n_instances (n), n_dimensions (d))
+            Data to split in window-sized instances.
+
+        Returns
+        -------
+        source: list containing the data on which model is trained.
+        target: list of future predictions of data.
+        """
+        source, target = [], []
+        for i in range(len(seq)):
+            end_idx = i + steps
+            if end_idx > len(seq) - 1:
+                break
+            seq_src, seq_tgt = seq[i:end_idx], seq[end_idx]
+            source.append(seq_src)
+            target.append(seq_tgt)
+        return source, target
