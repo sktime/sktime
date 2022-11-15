sktime/sktime

commit 0ce1b838bd9837b7f80f8e6ddce2fa912059b881

updated emails in pyproject.toml

pyproject.toml
@@ -10,8 +10,8 @@ authors = [
 ]
 maintainers = [
     {name = "sktime developers", email = "info@sktime.org"},
-    {name = "Franz Király", email = "f.kiraly@ucl.ac.uk"},
-    {name = "Guzal Bulatova"},
+    {name = "Franz Király", email = "fkiraly@sktime.org"},
+    {name = "Guzal Bulatova", email = "gbulatova@sktime.org},
     {name = "Martin Walter", email = "mwalter@sktime.org"},
 ]
 readme = "README.md"

commit 2655914ea419c3aabb9dec856b43fe84c4035adb

[MNT] fix `pyproject.toml` broken string (#3797)
fix typo in string introduced in https://github.com/sktime/sktime/commit/0ce1b838bd9837b7f80f8e6ddce2fa912059b881

pyproject.toml
@@ -11,7 +11,7 @@ authors = [
 maintainers = [
     {name = "sktime developers", email = "info@sktime.org"},
     {name = "Franz Király", email = "fkiraly@sktime.org"},
-    {name = "Guzal Bulatova", email = "gbulatova@sktime.org},
+    {name = "Guzal Bulatova", email = "gbulatova@sktime.org"},
     {name = "Martin Walter", email = "mwalter@sktime.org"},
 ]
 readme = "README.md"

sktime/_contrib/data_io.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
-"""Utilities for loading datasets that are unapproved of in the main repo."""
+"""Utilities for loading datasets that are unapproved of in the main repo.
+
+Old code, delete.
+"""
 
 __author__ = [
     "Emiliathewolf",