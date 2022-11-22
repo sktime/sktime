93a7981b1c7e20f615164e486af84d725bfc0834
Franz Király <f.kiraly@ucl.ac.uk>
2022-11-22 20:21:47 +0000

    linting

--- sktime/transformations/tests/test_optionalpassthrough.py
+++ sktime/transformations/tests/test_optionalpassthrough.py
@@ -18,31 +18,32 @@ def test_optionalpassthrough():
     from sktime.forecasting.compose import TransformedTargetForecaster
     from sktime.forecasting.model_selection import (
         ForecastingGridSearchCV,
-        SlidingWindowSplitter)
+        SlidingWindowSplitter,
+    )
     from sktime.forecasting.naive import NaiveForecaster
     from sktime.transformations.series.adapt import TabularToSeriesAdaptor
     from sktime.transformations.series.compose import OptionalPassthrough
     from sktime.transformations.series.detrend import Deseasonalizer
 
     # create pipeline
-    pipe = TransformedTargetForecaster(steps=[
-        ("deseasonalizer", OptionalPassthrough(Deseasonalizer())),
-        ("scaler", OptionalPassthrough(TabularToSeriesAdaptor(StandardScaler()))),
-        ("forecaster", NaiveForecaster())])
+    pipe = TransformedTargetForecaster(
+        steps=[
+            ("deseasonalizer", OptionalPassthrough(Deseasonalizer())),
+            ("scaler", OptionalPassthrough(TabularToSeriesAdaptor(StandardScaler()))),
+            ("forecaster", NaiveForecaster()),
+        ]
+    )
     # putting it all together in a grid search
     cv = SlidingWindowSplitter(
-        initial_window=60,
-        window_length=24,
-        start_with_window=True,
-        step_length=48)
+        initial_window=60, window_length=24, start_with_window=True, step_length=48
+    )
     param_grid = {
-        "deseasonalizer__passthrough" : [True, False],
+        "deseasonalizer__passthrough": [True, False],
         "scaler__transformer__transformer__with_mean": [True, False],
-        "scaler__passthrough" : [True, False],
-        "forecaster__strategy": ["drift", "mean", "last"]}
+        "scaler__passthrough": [True, False],
+        "forecaster__strategy": ["drift", "mean", "last"],
+    }
     gscv = ForecastingGridSearchCV(
-        forecaster=pipe,
-        param_grid=param_grid,
-        cv=cv,
-        n_jobs=-1)
+        forecaster=pipe, param_grid=param_grid, cv=cv, n_jobs=-1
+    )
     gscv.fit(load_airline())
