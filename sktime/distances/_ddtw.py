d316f7d4feb8b98ed7e0287c35eadf63c89e7d61
Franz Király <f.kiraly@ucl.ac.uk>
2022-11-27 21:30:19 +0000

    ddtw

--- sktime/distances/_ddtw.py
+++ sktime/distances/_ddtw.py
@@ -39,45 +39,12 @@ def average_of_slope_transform(X: np.ndarray) -> np.ndarray:
                 len(q.shape[1]))
         The derviative of the time series X.
     """
+    from sktime.distances._ddtw_numba import average_of_slope
+
     derivative_X = []
     for val in X:
         derivative_X.append(average_of_slope(val))
     return np.array(derivative_X)
-
-
-@njit(cache=True, fastmath=True)
-def average_of_slope(q: np.ndarray) -> np.ndarray:
-    r"""Compute the average of a slope between points.
-
-    Computes the average of the slope of the line through the point in question and
-    its left neighbour, and the slope of the line through the left neighbour and the
-    right neighbour. proposed in [1] for use in this context.
-
-    .. math::
-    q'_(i) = \frac{{}(q_{i} - q_{i-1} + ((q_{i+1} - q_{i-1}/2)}{2}
-
-    Where q is the original time series and q' is the derived time series.
-
-    Parameters
-    ----------
-    q: np.ndarray (of shape (d, m) where d is the dimensions and m is the timepoints.
-        A time series.
-
-    Returns
-    -------
-    np.ndarray (2d array of shape nxm where n is len(q.shape[0]-2) and m is
-                len(q.shape[1]))
-        Array containing the derivative of q.
-
-    References
-    ----------
-    .. [1] Keogh E, Pazzani M Derivative dynamic time warping. In: proceedings of 1st
-    SIAM International Conference on Data Mining, 2001
-    """
-    q = q.transpose()
-    q2 = 0.25 * q[2:] + 0.5 * q[1:-1] - 0.75 * q[:-2]
-    q2 = q2.transpose()
-    return q2
 
 
 class _DdtwDistance(NumbaDistance):
@@ -95,7 +62,7 @@ class _DdtwDistance(NumbaDistance):
         window: float = None,
         itakura_max_slope: float = None,
         bounding_matrix: np.ndarray = None,
-        compute_derivative: DerivativeCallable = average_of_slope,
+        compute_derivative=None,
         **kwargs: Any,
     ) -> DistanceAlignmentPathCallable:
         """Create a no_python compiled ddtw distance alignment path callable.
@@ -143,6 +110,11 @@ class _DdtwDistance(NumbaDistance):
             If the itakura_max_slope is not a float or int.
             If the compute derivative callable is not no_python compiled.
         """
+        if compute_derivative is None:
+            from sktime.distances._ddtw_numba import average_of_slope
+
+            compute_derivative = average_of_slope
+
         _bounding_matrix = resolve_bounding_matrix(
             x, y, window, itakura_max_slope, bounding_matrix
         )
@@ -189,7 +161,7 @@ class _DdtwDistance(NumbaDistance):
         window: float = None,
         itakura_max_slope: float = None,
         bounding_matrix: np.ndarray = None,
-        compute_derivative: DerivativeCallable = average_of_slope,
+        compute_derivative=None,
         **kwargs: Any,
     ) -> DistanceCallable:
         """Create a no_python compiled ddtw distance callable.
@@ -235,6 +207,11 @@ class _DdtwDistance(NumbaDistance):
             If the itakura_max_slope is not a float or int.
             If the compute derivative callable is not no_python compiled.
         """
+        if compute_derivative is None:
+            from sktime.distances._ddtw_numba import average_of_slope
+
+            compute_derivative = average_of_slope
+
         _bounding_matrix = resolve_bounding_matrix(
             x, y, window, itakura_max_slope, bounding_matrix
         )
