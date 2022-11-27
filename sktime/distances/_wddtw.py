cbb67463c06fcf523f4e264d7fe4c073c04c4f58
Franz Király <f.kiraly@ucl.ac.uk>
2022-11-27 23:09:01 +0000

    wdtw

--- sktime/distances/_wddtw.py
+++ sktime/distances/_wddtw.py
@@ -5,7 +5,6 @@ from typing import Any, List, Tuple
 
 import numpy as np
 
-from sktime.distances._wdtw import _weighted_cost_matrix
 from sktime.distances.base import (
     DistanceAlignmentPathCallable,
     DistanceCallable,
@@ -87,6 +86,7 @@ class _WddtwDistance(NumbaDistance):
 
         from sktime.distances._distance_alignment_paths import compute_min_return_path
         from sktime.distances._numba_utils import is_no_python_compiled_callable
+        from sktime.distances._wdtw import _weighted_cost_matrix
 
         if compute_derivative is None:
             from sktime.distances._ddtw_numba import average_of_slope
@@ -199,6 +199,7 @@ class _WddtwDistance(NumbaDistance):
         from numba import njit
 
         from sktime.distances._numba_utils import is_no_python_compiled_callable
+        from sktime.distances._wdtw import _weighted_cost_matrix
 
         if compute_derivative is None:
             from sktime.distances._ddtw_numba import average_of_slope
--- sktime/distances/_wdtw.py
+++ sktime/distances/_wdtw.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
 __author__ = ["chrisholder", "TonyBagnall"]
 
-import warnings
 from typing import Any, List, Tuple
 
 import numpy as np
-from numba import njit
-from numba.core.errors import NumbaWarning
 
 from sktime.distances.base import (
     DistanceAlignmentPathCallable,
@@ -14,9 +11,6 @@ from sktime.distances.base import (
     NumbaDistance,
 )
 from sktime.distances.lower_bounding import resolve_bounding_matrix
-
-# Warning occurs when using large time series (i.e. 1000x1000)
-warnings.simplefilter("ignore", category=NumbaWarning)
 
 
 class _WdtwDistance(NumbaDistance):
@@ -100,7 +94,10 @@ class _WdtwDistance(NumbaDistance):
             If the itakura_max_slope is not a float or int.
             If the value of g is not a float
         """
+        from numba import njit
+
         from sktime.distances._distance_alignment_paths import compute_min_return_path
+        from sktime.distances._wdtw_numba import _weighted_cost_matrix
 
         _bounding_matrix = resolve_bounding_matrix(
             x, y, window, itakura_max_slope, bounding_matrix
@@ -190,6 +187,10 @@ class _WdtwDistance(NumbaDistance):
             If the itakura_max_slope is not a float or int.
             If the value of g is not a float
         """
+        from numba import njit
+
+        from sktime.distances._wdtw_numba import _weighted_cost_matrix
+
         _bounding_matrix = resolve_bounding_matrix(
             x, y, window, itakura_max_slope, bounding_matrix
         )
@@ -208,51 +209,3 @@ class _WdtwDistance(NumbaDistance):
             return cost_matrix[-1, -1]
 
         return numba_wdtw_distance
-
-
-@njit(cache=True)
-def _weighted_cost_matrix(
-    x: np.ndarray, y: np.ndarray, bounding_matrix: np.ndarray, g: float
-):
-    """Compute the wdtw cost matrix between two time series.
-
-    Parameters
-    ----------
-    x: np.ndarray (2d array)
-        First time series.
-    y: np.ndarray (2d array)
-        Second time series.
-    bounding_matrix: np.ndarray (2d of size mxn where m is len(x) and n is len(y))
-        Bounding matrix where the values in bound are marked by finite values and
-        outside bound points are infinite values.
-    g: float
-        Constant that controls the curvature (slope) of the function; that is, g
-        controls the level of penalisation for the points with larger phase difference.
-
-    Returns
-    -------
-    np.ndarray
-        Weighted cost matrix between x and y time series.
-    """
-    dimensions = x.shape[0]
-    x_size = x.shape[1]
-    y_size = y.shape[1]
-    cost_matrix = np.full((x_size + 1, y_size + 1), np.inf)
-    cost_matrix[0, 0] = 0.0
-
-    weight_vector = np.array(
-        [1 / (1 + np.exp(-g * (i - x_size / 2))) for i in range(0, x_size)]
-    )
-
-    for i in range(x_size):
-        for j in range(y_size):
-            if np.isfinite(bounding_matrix[i, j]):
-                sum = 0
-                for k in range(dimensions):
-                    sum += (x[k][i] - y[k][j]) * (x[k][i] - y[k][j])
-                cost_matrix[i + 1, j + 1] = (
-                    min(cost_matrix[i, j + 1], cost_matrix[i + 1, j], cost_matrix[i, j])
-                    + weight_vector[np.abs(i - j)] * sum
-                )
-
-    return cost_matrix[1:, 1:]
