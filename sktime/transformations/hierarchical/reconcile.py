95bbc50fd0467ee8b18745f90b9c4617451ae381
Ciaran Gilbert <ciaran.gilbert@shell.com>
2022-11-30 13:14:36 +0000

    remove commented out code

--- sktime/transformations/hierarchical/reconcile.py
+++ sktime/transformations/hierarchical/reconcile.py
@@ -291,7 +291,6 @@ def _get_s_matrix(X):
             agg_ind = list(i)[::-1]
             for j in range(len(agg_ind)):
                 agg_ind[j] = "__total"
-                # ptint()
                 # insert indicator
                 s_matrix.loc[tuple(agg_ind[::-1]), i] = 1.0
         else:
