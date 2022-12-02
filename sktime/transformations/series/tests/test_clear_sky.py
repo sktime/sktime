a4560cc3c18b86679b3cb9720b51ccf938e48543
Ciaran Gilbert <ciaran.gilbert@shell.com>
2022-12-02 16:54:46 +0000

    add clearsky tests and index exceptions

--- sktime/transformations/series/tests/test_clear_sky.py
+++ sktime/transformations/series/tests/test_clear_sky.py
@@ -6,6 +6,7 @@
 __author__ = ["ciaran-g"]
 
 import numpy as np
+import pandas as pd
 import pytest
 
 from sktime.datasets import load_solar
@@ -69,7 +70,7 @@ output_chk = [
     reason="skip test if required soft dependency for hmmlearn not available",
 )
 def test_clearsky_trafo_vals():
-    """Tests clear sky trafo with and without missing values."""
+    """Tests clear sky trafo with and without missing values and period intex."""
     y = load_solar(api_version=None)
     cs_model = ClearSky()
     y_trafo = cs_model.fit_transform(y)
@@ -85,3 +86,56 @@ def test_clearsky_trafo_vals():
     msg = "ClearSky transformer not returning correct number of values."
     assert y.count() == y_trafo.count(), msg
     assert y_missing.count() == y_trafo_missing.count(), msg
+
+    y_period = y.copy()
+    y_period.index = y_period.index.to_period()
+    cs_model_period = ClearSky()
+    y_trafo_period = cs_model_period.fit_transform(y_period)
+
+    msg = "PeriodIndex and DatetimeIndex returning different values"
+    assert np.all(y_trafo_period.values == y_trafo.values), msg
+
+
+@pytest.mark.skipif(
+    not _check_soft_dependencies("statsmodels", severity="none"),
+    reason="skip test if required soft dependency for hmmlearn not available",
+)
+def test_clearsky_trafo_range_exception():
+    """Tests clear sky trafo exception with range index."""
+    y = load_solar(api_version=None)
+
+    # range index should not work
+    y = y.reset_index(drop=True)
+    cs_model = ClearSky()
+    with pytest.raises(ValueError):
+        cs_model.fit_transform(y)
+
+
+@pytest.mark.skipif(
+    not _check_soft_dependencies("statsmodels", severity="none"),
+    reason="skip test if required soft dependency for hmmlearn not available",
+)
+def test_clearsky_trafo_nofreq_exception():
+    """Tests clear sky trafo exception with no set/inferrable freq in index."""
+    y = load_solar(api_version=None)
+
+    # no set or inferrable frequency should not work
+    y = y.drop(pd.to_datetime("2021-05-01 00:30:00", utc=True))
+    cs_model = ClearSky()
+    with pytest.raises(ValueError):
+        cs_model.fit_transform(y)
+
+
+@pytest.mark.skipif(
+    not _check_soft_dependencies("statsmodels", severity="none"),
+    reason="skip test if required soft dependency for hmmlearn not available",
+)
+def test_clearsky_trafo_daily_exception():
+    """Tests clear sky trafo exception with geq daily freq in index."""
+    y = load_solar(api_version=None)
+
+    # geq daily frequency should not work
+    y = y.asfreq("1D")
+    cs_model = ClearSky()
+    with pytest.raises(ValueError):
+        cs_model.fit_transform(y)
