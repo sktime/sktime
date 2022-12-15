7455997df5d22fb1d503e60c239856b18bd95d50
Beth Rice <beth@field.energy>
2022-12-15 08:56:19 +0000

    Add author name

--- sktime/forecasting/tests/test_interval_wrappers.py
+++ sktime/forecasting/tests/test_interval_wrappers.py
@@ -2,7 +2,7 @@
 # copyright: sktime developers, BSD-3-Clause License (see LICENSE file)
 """Tests the conformal interval wrapper."""
 
-__author__ = ["fkiraly"]
+__author__ = ["fkiraly", "bethrice44"]
 
 import numpy as np
 import pandas as pd
