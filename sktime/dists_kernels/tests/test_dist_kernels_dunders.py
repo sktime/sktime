37a16c9469c7bdd10af1ccbb7a4ed4a6c151fc81
Franz Király <f.kiraly@ucl.ac.uk>
2022-12-18 18:17:06 +0000

    tests complete

--- sktime/dists_kernels/tests/test_dist_kernels_dunders.py
+++ sktime/dists_kernels/tests/test_dist_kernels_dunders.py
@@ -4,14 +4,19 @@
 __author__ = ["fkiraly"]
 
 import numpy as np
+import pytest
 
 from sktime.dists_kernels.algebra import CombinedDistance
 from sktime.dists_kernels.compose import PwTrafoPanelPipeline
 from sktime.dists_kernels.edit_dist import EditDist
 from sktime.utils._testing.panel import _make_panel_X
 
-X1 = _make_panel_X(n_instances=5, n_columns=5, n_timepoints=5, random_state=1)
-X2 = _make_panel_X(n_instances=6, n_columns=5, n_timepoints=5, random_state=2)
+X1 = _make_panel_X(
+    n_instances=5, n_columns=5, n_timepoints=5, random_state=1, all_positive=True
+)
+X2 = _make_panel_X(
+    n_instances=6, n_columns=5, n_timepoints=5, random_state=2, all_positive=True
+)
 
 
 def test_mul_algebra_dunder():
@@ -112,8 +117,8 @@ def test_pw_trafo_pipeline_mul_dunder():
     from sktime.transformations.series.exponent import ExponentTransformer
 
     t3 = EditDist()
-    t1 = ExponentTransformer()
-    t2 = ExponentTransformer(2)
+    t1 = ExponentTransformer(2)
+    t2 = ExponentTransformer(0.5)
 
     m3 = t3.transform(X1, X2)
 
@@ -121,10 +126,42 @@ def test_pw_trafo_pipeline_mul_dunder():
     assert isinstance(t23, PwTrafoPanelPipeline)
     assert len(t23.transformers) == 1
 
+    m23 = t23.transform(X1, X2)
+    X1t = t2.clone().fit_transform(X1)
+    X2t = t2.clone().fit_transform(X2)
+    m23_manual = t3.transform(X1t, X2t)
+    assert np.allclose(m23, m23_manual)
+
     t123 = t1 * t2 * t3
     assert isinstance(t123, PwTrafoPanelPipeline)
-    assert len(t23.transformers) == 2
+    assert len(t123.transformers) == 2
 
     m123 = t123.transform(X1, X2)
 
     assert np.allclose(m123, m3)
+
+
+@pytest.mark.parametrize("constant", [0, 1, -0.25])
+def test_dunders_with_constants(constant):
+    """Tests creation of pairwise panel trafo pipeliens using mul dunder."""
+    t = EditDist()
+
+    m = t.transform(X1, X2)
+
+    tplusc = t + constant
+
+    assert isinstance(tplusc, CombinedDistance)
+    assert len(tplusc.pw_trafos) == 2
+    assert tplusc.get_params()["operation"] == "+"
+
+    mplusc = tplusc.transform(X1, X2)
+    assert np.allclose(m + constant, mplusc)
+
+    ttimesc = t * constant
+
+    assert isinstance(ttimesc, CombinedDistance)
+    assert len(ttimesc.pw_trafos) == 2
+    assert ttimesc.get_params()["operation"] == "*"
+
+    mtimesc = ttimesc.transform(X1, X2)
+    assert np.allclose(m * constant, mtimesc)
