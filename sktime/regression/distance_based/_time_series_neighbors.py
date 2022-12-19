5d17b0b87e5ba01874206f9d82ac84de0495ff91
Franz Király <f.kiraly@ucl.ac.uk>
2022-12-19 00:44:56 +0000

    [ENH] `sklearn 1.2.0` compatibility - remove private `_check_weights` import in `KNeighborsTimeSeriesClassifier` and -`Regressor` (#3918)
    This PR fixes a compatibility issue with `sklearn 1.2` and removes the private import `_check_weights` from `KNeighborsTimeSeriesClassifier` and `KNeighborsTimeSeriesRegressor`.
    
    This can be done without deprecation or change in functionality, because `_check_weights` was just an erroneous leftover from an earlier version that used inheritance and not composition.
    
    Right now, the `sklearn` classifier is wrapped as a component, so `_check_weights` (or its 1.2 equivalent) is called again inside the component - therefore, naive removal simply removes an unnecessary duplication.

--- sktime/regression/distance_based/_time_series_neighbors.py
+++ sktime/regression/distance_based/_time_series_neighbors.py
@@ -13,7 +13,6 @@ __author__ = ["fkiraly"]
 __all__ = ["KNeighborsTimeSeriesRegressor"]
 
 from sklearn.neighbors import KNeighborsRegressor
-from sklearn.neighbors._base import _check_weights
 
 from sktime.distances import pairwise_distance
 from sktime.regression.base import BaseRegressor
@@ -132,7 +131,7 @@ class KNeighborsTimeSeriesRegressor(BaseRegressor):
             leaf_size=leaf_size,
             n_jobs=n_jobs,
         )
-        self.weights = _check_weights(weights)
+        self.weights = weights
 
         super(KNeighborsTimeSeriesRegressor, self).__init__()
 
