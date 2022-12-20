bae1ef4d3203b61116e60aa10f11574938181f0f
Franz Király <f.kiraly@ucl.ac.uk>
2022-12-20 17:37:32 +0000

    clustering pipeline

---
+++ sktime/clustering/compose/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""Compositions for clusterers."""
+# copyright: sktime developers, BSD-3-Clause License (see LICENSE file)
+
+__author__ = ["fkiraly"]
+__all__ = ["ClustererPipeline"]
+
+from sktime.clustering.compose._pipeline import ClustererPipeline
