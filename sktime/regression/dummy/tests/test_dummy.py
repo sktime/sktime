d789be8df81a0605c55b422d0b92c40a72aa3292
badrmarani <badr-eddine.marani@centrale.centralelille.fr>
2022-12-20 19:56:19 +0100

    add unit tests

---
+++ sktime/regression/dummy/tests/test_dummy.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""Test function of DummyRegressor."""
+import numpy as np
+
+from sktime.regression.dummy import DummyRegressor
+from sktime.datasets import load_unit_test
+
+
+def test_dummy_regressor():
+    """Test function for DummyClassifier."""
+    X_train, y_train = load_unit_test(split="train", return_type="numpy3D")
+    X_test, _ = load_unit_test(split="test", return_type="numpy3D")
+    dummy = DummyRegressor()
+    dummy.fit(X_train, y_train)
+    pred = dummy.predict(X_test)
+    assert all(i == "1.5" for i in pred)
