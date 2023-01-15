69bc0802d35ac944ddfeb86590680d7b25bf150c
Franz Király <f.kiraly@ucl.ac.uk>
2023-01-15 08:07:12 +0000

    ColumnEnsembleForecaster

--- sktime/forecasting/compose/_column_ensemble.py
+++ sktime/forecasting/compose/_column_ensemble.py
@@ -97,7 +97,11 @@ class ColumnEnsembleForecaster(_HeterogenousEnsembleForecaster):
     # if the estimator is fittable, _HeterogenousMetaEstimator also
     # provides an override for get_fitted_params for params from the fitted estimators
     # the fitted estimators should be in a different attribute, _steps_fitted_attr
-    _steps_fitted_attr = "forecasters_"
+    _steps_fitted_attr = "forecasters_name_est_"
+
+    @property
+    def forecasters_name_est_(self):
+        return [(name, forecaster) for (name, forecaster, _)in self.forecasters_)
 
     def __init__(self, forecasters):
         self.forecasters = forecasters
