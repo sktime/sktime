1fa139c5957a00d039318e87909664ed52f646e6
Kevin <kevinlamdata@gmail.com>
2023-01-18 20:03:00 -0500

    Remove Multinomial from init

--- sktime/annotation/hmm_learn/__init__.py
+++ sktime/annotation/hmm_learn/__init__.py
@@ -3,9 +3,8 @@
 # copyright: sktime developers, BSD-3-Clause License (see LICENSE file)
 """Implements wrappers for estimators from hmmlearn."""
 
-__all__ = ["BaseHMMLearn", "GaussianHMM", "GMMHMM", "MultinomialHMM"]
+__all__ = ["BaseHMMLearn", "GaussianHMM", "GMMHMM"]
 
 from sktime.annotation.hmm_learn.base import BaseHMMLearn
 from sktime.annotation.hmm_learn.gaussian import GaussianHMM
 from sktime.annotation.hmm_learn.gmm import GMMHMM
-from sktime.annotation.hmm_learn.multinomial import MultinomialHMM
