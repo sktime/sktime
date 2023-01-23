49dba1bb27ae1dc21b7096ed8abaa1f98d0a79bf
Franz Király <f.kiraly@ucl.ac.uk>
2023-01-23 23:06:57 +0000

    linting

--- sktime/forecasting/base/_base.py
+++ sktime/forecasting/base/_base.py
@@ -39,7 +39,6 @@ __author__ = ["mloning", "big-o", "fkiraly", "sveameyer13", "miraep8"]
 __all__ = ["BaseForecaster"]
 
 from copy import deepcopy
-from itertools import product
 from warnings import warn
 
 import numpy as np
