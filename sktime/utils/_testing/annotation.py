9414c242c7191bb97c810fdce404af94d76fcd0c
Kevin <kevinlamdata@gmail.com>
2023-01-23 15:46:23 -0500

    Make requested change for make_annotation_problem
    Co-authored-by: Paul <pauljr97@gmail.com>
    Co-authored-by: Margaret <margaret.gorlin@gmail.com>

--- sktime/utils/_testing/annotation.py
+++ sktime/utils/_testing/annotation.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+__author__ = ["mloning", "fkiraly", "klam-data", "pyyim", "mgorlin"]
+__all__ = []
+
 from sktime.utils._testing.series import _make_series
 
 
@@ -10,13 +13,19 @@ def make_annotation_problem(
     make_X=False,
     n_columns=2,
     random_state=None,
+    estimator_type=None,
 ):
+    integer_only = False
+    if estimator_type == "Poisson":
+        integer_only = True
+
     y = _make_series(
         n_timepoints=n_timepoints,
         n_columns=1,
         all_positive=all_positive,
         index_type=index_type,
         random_state=random_state,
+        integer_only=integer_only,
     )
 
     if not make_X:
@@ -28,6 +37,7 @@ def make_annotation_problem(
         all_positive=all_positive,
         index_type=index_type,
         random_state=random_state,
+        integer_only=integer_only,
     )
     X.index = y.index
     return y, X
