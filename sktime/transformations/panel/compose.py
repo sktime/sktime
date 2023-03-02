0dca12bbc7bfdea99e5a317982b3f641162c1838
Franz Király <f.kiraly@ucl.ac.uk>
2023-03-02 23:15:49 +0000

    [ENH] `sktime` native column ensemble transformer (#4232)
    This adds a `ColumnEnsembleTransformer` native to `sktime` which follows syntax and logic of the `ColumnEnsembleForecaster`.
    
    It relies on the `_ColumnEnsemble` mixin created in https://github.com/sktime/sktime/pull/4231
    
    This currently does not replace the `ColumnTransformer` as the latter has additional parameters, so would require deprecation at this point in time.
    
    Instead, I think we want to add the parameters that `ColumnTransformer` has, to `ColumnEnsembleForecaster` and `ColumnEnsembleTransformer` where applicable and later refactor-deprecate using the `sktime` native transformer with full parameter set.

--- sktime/transformations/panel/compose.py
+++ sktime/transformations/panel/compose.py
@@ -4,6 +4,12 @@
 This module has meta-transformations that is build using the pre-existing
 transformations as building blocks.
 """
+
+__author__ = ["mloning", "sajaysurya", "fkiraly"]
+__all__ = ["ColumnTransformer", "ColumnConcatenator"]
+
+from warnings import warn
+
 import numpy as np
 import pandas as pd
 from scipy import sparse
@@ -12,9 +18,6 @@ from sklearn.compose import ColumnTransformer as _ColumnTransformer
 from sktime.transformations.base import BaseTransformer, _PanelToPanelTransformer
 from sktime.utils.multiindex import flatten_multiindex
 from sktime.utils.validation.panel import check_X
-
-__author__ = ["mloning", "sajaysurya", "fkiraly"]
-__all__ = ["ColumnTransformer", "ColumnConcatenator"]
 
 
 class ColumnTransformer(_ColumnTransformer, _PanelToPanelTransformer):
@@ -115,6 +118,17 @@ class ColumnTransformer(_ColumnTransformer, _PanelToPanelTransformer):
         transformer_weights=None,
         preserve_dataframe=True,
     ):
+
+        warn(
+            "ColumnTransformer is not fully compliant with the sktime interface "
+            "and will be replaced by sktime.transformations.ColumnEnsembleTransformer "
+            "in a future version. Deprecation horizon and instructions will "
+            "be added to this message, once ColumnEnsembleTransformer can replace "
+            "key parameters of ColumnTransformer. If not using parameters remainder, "
+            "sparse_threshold, n_jobs, transformer_weights, or preserve_dataframe, "
+            "ColumnTransformer can simply be replaced by ColumnEnsembleTransformer."
+        )
+
         super(ColumnTransformer, self).__init__(
             transformers=transformers,
             remainder=remainder,
