fix broadcast of index in Normal

sktime/proba/normal.py
@@ -39,7 +39,13 @@ class Normal(BaseDistribution):
 
         self.mu = mu
         self.sigma = sigma
+        self.index = index
+        self.columns = columns
 
+        # todo: untangle index handling
+        # and broadcast of parameters.
+        # move this functionality to the base class
+        # 0.18.0?
         self._mu, self._sigma = self._get_bc_params()
         shape = self._mu.shape
 
@@ -54,10 +60,10 @@ class Normal(BaseDistribution):
     def _get_bc_params(self):
         """Fully broadcast parameters of self, given param shapes and index, columns."""
         to_broadcast = [self.mu, self.sigma]
-        if hasattr(self, "index"):
+        if hasattr(self, "index") and self.index is not None:
             to_broadcast += [self.index.to_numpy().reshape(-1, 1)]
-        if hasattr(self, "columns"):
-            to_broadcast += [self.index.to_numpy()]
+        if hasattr(self, "columns") and self.columns is not None:
+            to_broadcast += [self.columns.to_numpy()]
         bc = np.broadcast_arrays(*to_broadcast)
         return bc[0], bc[1]
 