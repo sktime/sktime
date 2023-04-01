7ef9d08a88853ce4f726b7df6f5ab55eca193c6c
Franz Király <f.kiraly@ucl.ac.uk>
2023-04-01 13:06:51 +0100

    draft complete

--- sktime/transformations/compose/_ixtox.py
+++ sktime/transformations/compose/_ixtox.py
@@ -11,22 +11,34 @@ from sktime.transformations.base import BaseTransformer
 class IxToX(BaseTransformer):
     """Create exogeneous features based on time index or hierarchy values.
 
-    Replaces exogeneous features (`X`) by endogeneous data (`y`).
+    Returns index of `X` in `transform` as transformed features.
+    By default, time features only.
+    Can also be used to select hierarchy levels in case of hierarchical input,
+    via the `levels` argument.
+
+    Return columns of `transform` applied to `pandas` based containers
+    have same name as level if levels have name in `transform` input,
+    otherwise `index` (time) and `level_X.{N}` where N is the level index integer.
 
     To *add* instead of *replace*, use `FeatureUnion` and/or the `+` dunder.
+
+    For more custom options or a direct `pandas` interface,
+    an alternative is `PandasTransformAdaptor` with `method="reset_index"`.
 
     Parameters
     ----------
     coerce_to_type : str, optional, default="float"
         type to coerce the index columns to when passed to `X`
-    levels : None (default) or iterable of pandas index level name elements
+    level : None (default), int, str, or iterable of pandas index level name elements
         if passed, selects the hierarchy levels that will be turned into columns in `X`
+        if passed, passed on as `level` to `reset_index` internally
         if None, will convert only the time index (last level) into features
-    source : str, optional, default="auto"
+        Note that this is different from the default of `reset_index`
+    ix_source : str, optional, default="X"
         which object to take the index from
-        default = "auto" = `X` if passed to `fit`/`predict`, otherwise `y` or `fh`
-        "y" = `y` as passed to `fit`, from `fh` in `predict`-like methods
-        "X" = `X` as passed to `fit`, `predict`, etc
+        default = "X" = `X` as passed to `transform`
+            if used within `ForecastingPipeline`, this means `X` by default
+        "y" = `y` as passed to `transform`, if passed (not `None`), otherwise `X`
     """
 
     _tags = {
@@ -37,12 +49,14 @@ class IxToX(BaseTransformer):
         "y_inner_mtype": ["pd.DataFrame", "pd-multiindex", "pd_multiindex_hier"],
         "scitype:y": "both",
         "fit_is_empty": True,
-        "requires_y": True,
+        "requires_y": False,
     }
 
-    def __init__(self, subset_index=False):
+    def __init__(self, coerce_to_type="float", level=None, ix_source="X"):
 
-        self.subset_index = subset_index
+        self.coerce_to_type = coerce_to_type
+        self.level = level
+        self.ix_source = ix_source
 
         super(IxToX, self).__init__()
 
@@ -60,12 +74,27 @@ class IxToX(BaseTransformer):
 
         Returns
         -------
-        y, as a transformed version of X
+        transformed version of X
         """
-        if self.subset_index:
-            return y.loc[X.index.intersection(y.index)]
-        else:
-            return y
+        if self.ix_source == "y" and y is not None:
+            X = y
+
+        level = self.level
+        if level is None:
+            level = -1
+
+        X_only_ix = X.drop(columns=X.columns)
+        X_ix_in_df = X_only_ix.reset_index(level=level)
+
+        if X.index.names[-1] is None:
+            newcols = X_ix_in_df.columns
+            newcols[-1] = "index"
+            X_ix_in_df.columns = newcols
+
+        X_ix_in_df.index = X.index
+        X_ix_in_df = X_ix_in_df.astype(self.coerce_to_type)
+
+        return X_ix_in_df
 
     def _inverse_transform(self, X, y=None):
         """Inverse transform, inverse operation to transform.
@@ -84,7 +113,4 @@ class IxToX(BaseTransformer):
         -------
         inverse transformed version of X
         """
-        if self.subset_index:
-            return y.loc[X.index.intersection(y.index)]
-        else:
-            return y
+        return X
