8edb410e06d762a1437e8312b18f68c31c9c2ee6
Franz Király <f.kiraly@ucl.ac.uk>
2023-04-02 16:04:45 +0100

    fix test

--- sktime/utils/_maint/tests/test_show_versions.py
+++ sktime/utils/_maint/tests/test_show_versions.py
@@ -19,15 +19,14 @@ def test_deps_info():
     """Test that _get_deps_info returns package/version dict as per contract."""
     deps_info = _get_deps_info()
     assert isinstance(deps_info, dict)
-    assert set(deps_info.keys()) == set(DEFAULT_DEPS_TO_SHOW)
+    assert set(deps_info.keys()) == set(["sktime"])
 
-    deps_info_with_set = _get_deps_info(DEFAULT_DEPS_TO_SHOW)
-    assert isinstance(deps_info_with_set, dict)
-    assert set(deps_info.keys()) == set(deps_info_with_set.keys())
+    deps_info_default = _get_deps_info(DEFAULT_DEPS_TO_SHOW)
+    assert isinstance(deps_info_default, dict)
+    assert set(deps_info_default.keys()) == set(DEFAULT_DEPS_TO_SHOW)
 
     for key in DEFAULT_DEPS_TO_SHOW:
-        assert deps_info[key] == _check_soft_dependencies(key, severity="none")
-        assert deps_info[key] == deps_info_with_set[key]
+        key_is_available = _check_soft_dependencies(key, severity="none")
+        assert (deps_info_default[key] is None) != key_is_available
         deps_single_key = _get_deps_info([key])
         assert set(deps_single_key.keys()) == {key}
-        assert deps_info[key] == deps_single_key[key]
