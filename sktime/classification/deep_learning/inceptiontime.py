c527561c090e9a406c51735a20846f9fdc9f87c6
Franz Király <f.kiraly@ucl.ac.uk>
2023-04-15 20:07:25 +0100

    complete migration

--- sktime/classification/deep_learning/inceptiontime.py
+++ sktime/classification/deep_learning/inceptiontime.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 """InceptionTime for classification."""
-__author__ = "James Large"
+__author__ = "james-large"
 __all__ = ["InceptionTimeClassifier"]
+
+from copy import deepcopy
 
 from sklearn.utils import check_random_state
 
 from sktime.classification.deep_learning.base import BaseDeepClassifier
 from sktime.networks.inceptiontime import InceptionTimeNetwork
-from sktime.utils.data import check_and_clean_data, check_and_clean_validation_data
 from sktime.utils.validation._dependencies import _check_dl_dependencies
-
-_check_dl_dependencies("tensorflow", severity="warning")
 
 
 class InceptionTimeClassifier(BaseDeepClassifier):
@@ -18,7 +17,7 @@ class InceptionTimeClassifier(BaseDeepClassifier):
 
     Parameters
     ----------
-    nb_filters: int,
+    n_filters: int,
     use_residual: boolean,
     use_bottleneck: boolean,
     depth: int
@@ -49,7 +48,7 @@ class InceptionTimeClassifier(BaseDeepClassifier):
         n_epochs=1500,
         batch_size=64,
         kernel_size=41 - 1,
-        nb_filters=32,
+        n_filters=32,
         use_residual=True,
         use_bottleneck=True,
         bottleneck_size=32,
@@ -74,13 +73,24 @@ class InceptionTimeClassifier(BaseDeepClassifier):
         self.loss = loss
         self.metrics = metrics
         self.n_epochs = n_epochs
-        self.nb_filters = nb_filters
+        self.n_filters = n_filters
         self.random_state = random_state
         self.use_bottleneck = use_bottleneck
         self.use_residual = use_residual
         self.verbose = verbose
         self._is_fitted = False
-        self._network = InceptionTimeNetwork()
+
+        network_params = {
+            "n_filters": n_filters,
+            "use_residual": use_residual,
+            "use_bottleneck": use_bottleneck,
+            "bottleneck_size": bottleneck_size,
+            "depth": depth,
+            "kernel_size": kernel_size,
+            "random_state": random_state,
+        }
+
+        self._network = InceptionTimeNetwork(**network_params)
 
     def build_model(self, input_shape, n_classes, **kwargs):
         """Construct a compiled, un-trained, keras model that is ready for training.
@@ -131,70 +141,82 @@ class InceptionTimeClassifier(BaseDeepClassifier):
 
         return model
 
-    def _fit(
-        self, X, y, input_checks=True, validation_X=None, validation_y=None, **kwargs
-    ):
+    def _fit(self, X, y):
         """Fit the classifier on the training set (X, y).
 
         Parameters
         ----------
-        X : a nested pd.Dataframe, or (if input_checks=False) array-like of
-        shape = (n_instances, series_length, n_dimensions)
-            The training input samples. If a 2D array-like is passed,
-            n_dimensions is assumed to be 1.
-        y : array-like, shape = [n_instances]
+        X : np.ndarray of shape = (n_instances (n), n_dimensions (d), series_length (m))
+            The training input samples.
+        y : np.ndarray of shape n
             The training data class labels.
-        input_checks : boolean
-            whether to check the X and y parameters
-        validation_X : a nested pd.Dataframe, or array-like of shape =
-        (n_instances, series_length, n_dimensions)
-            The validation samples. If a 2D array-like is passed,
-            n_dimensions is assumed to be 1.
-            Unless strictly defined by the user via callbacks (such as
-            EarlyStopping), the presence or state of the validation
-            data does not alter training in any way. Predictions at each epoch
-            are stored in the model's fit history.
-        validation_y : array-like, shape = [n_instances]
-            The validation class labels.
 
         Returns
         -------
         self : object
         """
-        self._callbacks = self.callbacks
-        self.random_state = check_random_state(self.random_state)
-
-        X = check_and_clean_data(X, y, input_checks=input_checks)
         y_onehot = self.convert_y_to_keras(y)
+        # Transpose to conform to Keras input style.
+        X = X.transpose(0, 2, 1)
 
-        validation_data = check_and_clean_validation_data(
-            validation_X, validation_y, self.label_encoder, self.onehot_encoder
-        )
-
-        # ignore the number of instances, X.shape[0],
-        # just want the shape of each instance
+        check_random_state(self.random_state)
         self.input_shape = X.shape[1:]
-
-        if self.batch_size is None:
-            self.batch_size = int(min(X.shape[0] / 10, 16))
-        else:
-            self.batch_size = self.batch_size
-
         self.model_ = self.build_model(self.input_shape, self.n_classes_)
-
         if self.verbose:
             self.model_.summary()
-
         self.history = self.model_.fit(
             X,
             y_onehot,
             batch_size=self.batch_size,
             epochs=self.n_epochs,
             verbose=self.verbose,
-            callbacks=self._callbacks,
-            validation_data=validation_data,
+            callbacks=deepcopy(self.callbacks) if self.callbacks else [],
         )
-
-        self._is_fitted = True
-
         return self
+
+    @classmethod
+    def get_test_params(cls, parameter_set="default"):
+        """Return testing parameter settings for the estimator.
+
+        Parameters
+        ----------
+        parameter_set : str, default="default"
+            Name of the set of test parameters to return, for use in tests. If no
+            special parameters are defined for a value, will return `"default"` set.
+            For classifiers, a "default" set of parameters should be provided for
+            general testing, and a "results_comparison" set for comparing against
+            previously recorded results if the general set does not produce suitable
+            probabilities to compare against.
+
+        Returns
+        -------
+        params : dict or list of dict, default={}
+            Parameters to create testing instances of the class.
+            Each dict are parameters to construct an "interesting" test instance, i.e.,
+            `MyClass(**params)` or `MyClass(**params[i])` creates a valid test instance.
+            `create_test_instance` uses the first (or only) dictionary in `params`.
+        """
+        from sktime.utils.validation._dependencies import _check_soft_dependencies
+
+        param1 = {
+            "n_epochs": 10,
+            "batch_size": 4,
+        }
+
+        param2 = {
+            "n_epochs": 12,
+            "batch_size": 6,
+        }
+        test_params = [param1, param2]
+
+        if _check_soft_dependencies("keras", severity="none"):
+            from keras.callbacks import LambdaCallback
+
+            test_params.append(
+                {
+                    "n_epochs": 2,
+                    "callbacks": [LambdaCallback()],
+                }
+            )
+
+        return test_params
