e5603886abcf0d7c53dace04323eabcc425a2614
Franz Király <f.kiraly@ucl.ac.uk>
2023-04-17 15:34:15 +0100

    test consistency

--- sktime/utils/tests/test_seasonal.py
+++ sktime/utils/tests/test_seasonal.py
@@ -15,7 +15,7 @@ from sktime.utils.seasonality import _pivot_sp, _unpivot_sp
 @pytest.mark.parametrize("index_type", ["period", "datetime", "range", "int"])
 @pytest.mark.parametrize("sp", [2, 10])
 def test_pivot_sp(sp, index_type, n_timepoints):
-    """Test that random_partition returns a disjoint partition."""
+    """Test _pivot_sp contract."""
     df = _make_series(
         n_timepoints=n_timepoints,
         n_columns=1,
@@ -49,7 +49,7 @@ def test_pivot_sp(sp, index_type, n_timepoints):
 @pytest.mark.parametrize("index_type", ["period", "datetime", "range", "int"])
 @pytest.mark.parametrize("sp", [2, 10])
 def test_unpivot_sp(sp, index_type, n_timepoints):
-    """Test that random_partition returns a disjoint partition."""
+    """Test _unpivot_sp contract."""
     df = _make_series(
         n_timepoints=n_timepoints,
         n_columns=1,
@@ -70,3 +70,26 @@ def test_unpivot_sp(sp, index_type, n_timepoints):
     assert len(df_unpivot) == len(df)
     assert np.all(df_unpivot.index == df.index)
     assert np.all(df_unpivot == df)
+
+
+@pytest.mark.parametrize("n_timepoints", [50, 2])
+@pytest.mark.parametrize("index_type", ["period", "datetime", "range", "int"])
+@pytest.mark.parametrize("sp", [2, 10])
+def test_pivot_sp_consistent(sp, index_type, n_timepoints):
+    """Test _pivot_sp consistency between offsets."""
+    df = _make_series(
+        n_timepoints=n_timepoints,
+        n_columns=1,
+        index_type=index_type,
+        return_mtype="pd.DataFrame",
+    )
+    df2 = df.iloc[1:]
+
+    df_pivot = _pivot_sp(df, sp)
+    df2_pivot = _pivot_sp(df2, sp, anchor=df)
+
+    df_pivot_values = df_pivot.values.flatten()
+    df_pivot_values = df_pivot_values[~np.isnan(df_pivot_values)]
+    df2_pivot_values = df2_pivot.values.flatten()
+    df2_pivot_values = df2_pivot_values[~np.isnan(df2_pivot_values)]
+    assert np.all(df_pivot_values[1:] == df2_pivot_values)
