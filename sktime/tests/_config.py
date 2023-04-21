750f02bb44ab7f0fe6767b45c6bb706653f49008
Franz Király <f.kiraly@ucl.ac.uk>
2023-04-21 20:49:41 +0100

    [BUG] fix broken `DynamicFactor._predict_interval` (#4479)
    This fixes `DynamicFactor._predict_interval` which seems not to have
    been correctly implemented, but masked by failing coverage.
    
    * fixes https://github.com/sktime/sktime/issues/4362, the wrong
    parameters were passed to the inner `statsmodels` forecaster
    * fixes an unreported bug where `alpha` should have been `1 - coverage`
    but was `0.5 - 0.5 * coverage` (wrong factor 2)

--- sktime/tests/_config.py
+++ sktime/tests/_config.py
@@ -130,12 +130,6 @@ EXCLUDED_TESTS = {
     # SAX returns strange output format
     # this needs to be fixed, was not tested previously due to legacy exception
     "SAX": "test_fit_transform_output",
-    # known bug in DynamicFactor, returns wrong index, #4362
-    "DynamicFactor": [
-        "test_predict_interval",
-        "test_predict_quantiles",
-        "test_predict_proba",
-    ],
 }
 
 # We use estimator tags in addition to class hierarchies to further distinguish
