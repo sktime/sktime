c4469538bb99f8bc6db2e29641a5b952f01b74df
Franz Király <f.kiraly@ucl.ac.uk>
2023-05-17 14:25:13 +0100

    fix import

--- sktime/classification/model_selection/__init__.py
+++ sktime/classification/model_selection/__init__.py
@@ -2,6 +2,6 @@
 """Tuning of time series classifiers."""
 # copyright: sktime developers, BSD-3-Clause License (see LICENSE file)
 
-from sktime.classification.model_selection import TSCGridSearchCV
+from sktime.classification.model_selection._tune import TSCGridSearchCV
 
 __all__ = ["TSCGridSearchCV"]
