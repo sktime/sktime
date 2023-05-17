7c8685ce30f2e8dc441a11f9672dc7b84ea48e16
Franz Király <f.kiraly@ucl.ac.uk>
2023-05-17 19:39:18 +0100

    [MNT] remove `tsfresh` python version bounds from estimators (#4573)
    The `tsfresh` facing estimators had a python version bound of `<3.10` so
    far.
    
    It seems that `tsfresh` is now compatible with more recent python
    versions, so this bound is removed.

--- sktime/transformations/panel/tsfresh.py
+++ sktime/transformations/panel/tsfresh.py
@@ -205,9 +205,7 @@ class TSFreshFeatureExtractor(_TSFreshFeatureExtractor):
     --------
     >>> from sklearn.model_selection import train_test_split
     >>> from sktime.datasets import load_arrow_head
-    >>> from sktime.transformations.panel.tsfresh import (
-    ...     TSFreshFeatureExtractor
-    ... )
+    >>> from sktime.transformations.panel.tsfresh import TSFreshFeatureExtractor
     >>> X, y = load_arrow_head(return_X_y=True)
     >>> X_train, X_test, y_train, y_test = train_test_split(X, y)
     >>> ts_eff = TSFreshFeatureExtractor(
