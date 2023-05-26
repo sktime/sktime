a75288b59f59d5be6b1e49e3b693ba48313f7af3
Franz Király <f.kiraly@ucl.ac.uk>
2023-05-26 23:14:00 +0100

    docstrings

--- sktime/forecasting/compose/_pipeline.py
+++ sktime/forecasting/compose/_pipeline.py
@@ -633,17 +633,10 @@ class ForecastingPipeline(_Pipeline):
 
         Returns
         -------
-        pred_dist : tfp Distribution object
-            if marginal=True:
-                batch shape is 1D and same length as fh
-                event shape is 1D, with length equal number of variables being forecast
-                i-th (batch) distribution is forecast for i-th entry of fh
-                j-th (event) index is j-th variable, order as y in `fit`/`update`
-            if marginal=False:
-                there is a single batch
-                event shape is 2D, of shape (len(fh), no. variables)
-                i-th (event dim 1) distribution is forecast for i-th entry of fh
-                j-th (event dim 1) index is j-th variable, order as y in `fit`/`update`
+        pred_dist : sktime BaseDistribution
+            predictive distribution
+            if marginal=True, will be marginal distribution by time point
+            if marginal=False and implemented by method, will be joint
         """
         X = self._transform(X=X)
         return self.forecaster_.predict_proba(fh=fh, X=X, marginal=marginal)
@@ -1520,17 +1513,10 @@ class ForecastX(BaseForecaster):
 
         Returns
         -------
-        pred_dist : tfp Distribution object
-            if marginal=True:
-                batch shape is 1D and same length as fh
-                event shape is 1D, with length equal number of variables being forecast
-                i-th (batch) distribution is forecast for i-th entry of fh
-                j-th (event) index is j-th variable, order as y in `fit`/`update`
-            if marginal=False:
-                there is a single batch
-                event shape is 2D, of shape (len(fh), no. variables)
-                i-th (event dim 1) distribution is forecast for i-th entry of fh
-                j-th (event dim 1) index is j-th variable, order as y in `fit`/`update`
+        pred_dist : sktime BaseDistribution
+            predictive distribution
+            if marginal=True, will be marginal distribution by time point
+            if marginal=False and implemented by method, will be joint
         """
         X = self._get_forecaster_X_prediction(fh=fh, X=X)
         y_pred = self.forecaster_y_.predict_proba(fh=fh, X=X, marginal=marginal)
