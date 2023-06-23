53036ab9b5c76617d952ae7ad822a9b3b3c2893b
Colin Fallon <CTFallon@users.noreply.github.com>
2023-06-23 17:49:31 -0400

    autoreg initial commit

---
+++ sktime/forecasting/auto_reg.py
@@ -0,0 +1,282 @@
+# -*- coding: utf-8 -*-
+# copyright: sktime developers, BSD-3-Clause License (see LICENSE file)
+
+"""Interfaces AutoReg Forecaster from statsmodels.
+
+Available from statsmodels.tsa.ar_model.
+"""
+
+__author__ = ["jonathanbechtel", "mgazian000", "CTFallon"]
+__all__ = ["AutoREG"]
+
+from sktime.forecasting.base.adapters import _StatsModelsAdapter
+
+
+class AutoREG(_StatsModelsAdapter):
+    """Autoregressive AR-X(p) model.
+
+    Estimate an AR-X model using Conditional Maximum Likelihood (OLS).
+
+    Parameters
+    ----------
+    endog : array_like
+        A 1-d endogenous response variable. The dependent variable.
+    lags : {None, int, list[int]}
+        The number of lags to include in the model if an integer or the
+        list of lag indices to include.  For example, [1, 4] will only
+        include lags 1 and 4 while lags=4 will include lags 1, 2, 3, and 4.
+        None excludes all AR lags, and behave identically to 0.
+    trend : {'n', 'c', 't', 'ct'}
+        The trend to include in the model:
+
+        * 'n' - No trend.
+        * 'c' - Constant only.
+        * 't' - Time trend only.
+        * 'ct' - Constant and time trend.
+
+    seasonal : bool
+        Flag indicating whether to include seasonal dummies in the model. If
+        seasonal is True and trend includes 'c', then the first period
+        is excluded from the seasonal terms.
+    exog : array_like, optional
+        Exogenous variables to include in the model. Must have the same number
+        of observations as endog and should be aligned so that endog[i] is
+        regressed on exog[i].
+    hold_back : {None, int}
+        Initial observations to exclude from the estimation sample.  If None,
+        then hold_back is equal to the maximum lag in the model.  Set to a
+        non-zero value to produce comparable models with different lag
+        length.  For example, to compare the fit of a model with lags=3 and
+        lags=1, set hold_back=3 which ensures that both models are estimated
+        using observations 3,...,nobs. hold_back must be >= the maximum lag in
+        the model.
+    period : {None, int}
+        The period of the data. Only used if seasonal is True. This parameter
+        can be omitted if using a pandas object for endog that contains a
+        recognized frequency.
+    missing : str
+        Available options are 'none', 'drop', and 'raise'. If 'none', no nan
+        checking is done. If 'drop', any observations with nans are dropped.
+        If 'raise', an error is raised. Default is 'none'.
+    deterministic : DeterministicProcess
+        A deterministic process.  If provided, trend and seasonal are ignored.
+        A warning is raised if trend is not "n" and seasonal is not False.
+    old_names : bool
+        Flag indicating whether to use the v0.11 names or the v0.12+ names.
+
+        .. deprecated:: 0.13.0
+
+           old_names is deprecated and will be removed after 0.14 is
+           released. You must update any code reliant on the old variable
+           names to use the new names.
+
+    # to do:  update Example with an SKTime specific example
+    Examples
+    --------
+    >>> import statsmodels.api as sm
+    >>> from statsmodels.tsa.ar_model import AutoReg
+    >>> data = sm.datasets.sunspots.load_pandas().data['SUNACTIVITY']
+    >>> out = 'AIC: {0:0.3f}, HQIC: {1:0.3f}, BIC: {2:0.3f}'
+    """
+
+    _tags = {
+        "y_inner_mtype": "pd.Series",
+        "X_inner_mtype": "pd.DataFrame",
+        "scitype:y": "univariate",
+        "ignores-exogeneous-X": True,
+        "requires-fh-in-fit": False,
+    }
+
+    # todo: add any hyper-parameters and components to constructor
+    def __init__(
+        self,
+        lags=None,
+        trend="c",
+        seasonal=False,
+        hold_back=None,
+        period=None,
+        missing="none",
+        deterministic=None,
+        old_names=False,
+        cov_type="nonrobust",
+        cov_kwds=None,
+        use_t=True,
+        dynamic=False,
+        exog_oos=None,
+    ):
+        # Model Params
+        self.lags = lags
+        self.trend = trend
+        self.seasonal = seasonal
+        self.hold_back = hold_back
+        self.period = period
+        self.missing = missing
+        self.deterministic = deterministic
+        self.old_names = old_names
+
+        # Fit Params
+        self.cov_type = cov_type
+        self.cov_kwds = cov_kwds
+        self.use_t = use_t
+
+        # Predcit Params
+        self.dynamic = dynamic
+        self.exog_oos = exog_oos
+
+        super(AutoREG, self).__init__()
+
+        # todo: optional, parameter checking logic (if applicable) should happen here
+        # if writes derived values to self, should *not* overwrite self.parama etc
+        # instead, write to self._parama, self._newparam (starting with _)
+
+    # todo: implement this, mandatory
+    def _fit(self, y, X=None, fh=None):
+        """Fit forecaster to training data.
+
+        private _fit containing the core logic, called from fit
+
+        Writes to self:
+            Sets fitted model attributes ending in "_".
+
+        Parameters
+        ----------
+        y : guaranteed to be of a type in self.get_tag("y_inner_mtype")
+            Time series to which to fit the forecaster.
+            if self.get_tag("scitype:y")=="univariate":
+                guaranteed to have a single column/variable
+            if self.get_tag("scitype:y")=="multivariate":
+                guaranteed to have 2 or more columns
+            if self.get_tag("scitype:y")=="both": no restrictions apply
+        fh : guaranteed to be ForecastingHorizon or None, optional (default=None)
+            The forecasting horizon with the steps ahead to to predict.
+            Required (non-optional) here if self.get_tag("requires-fh-in-fit")==True
+            Otherwise, if not passed in _fit, guaranteed to be passed in _predict
+        X : optional (default=None)
+            guaranteed to be of a type in self.get_tag("X_inner_mtype")
+            Exogeneous time series to fit to.
+
+        Returns
+        -------
+        self : reference to self
+        """
+        from statsmodels.tsa.ar_model import AutoReg as _AutoReg
+
+        self._forecaster = _AutoReg(
+            endog=y,
+            lags=self.lags,
+            trend=self.trend,
+            seasonal=self.seasonal,
+            exog=X,
+            hold_back=self.hold_back,
+            peroid=self.period,
+            missing=self.missing,
+            deterministic=self.deterministic,
+            old_names=self.old_names,
+        )
+
+        self._fitted_forecaster = self._forecaster.fit(
+            cov_type=self.cov_type, cov_kwds=self.cov_kwds, use_t=self.use_t
+        )
+
+        # Post-fit params
+        self.sigma2_ = self._fitted_forecaster.sigma2()
+        self.fpe_ = self._fitted_forecaster.fpe()
+        self.resid_ = self._fitted_forecaster.resid()
+
+        return self
+
+    # todo: implement this, mandatory
+    def _predict(self, fh, X=None):
+        """Forecast time series at future horizon.
+
+        private _predict containing the core logic, called from predict
+
+        State required:
+            Requires state to be "fitted".
+
+        Accesses in self:
+            Fitted model attributes ending in "_"
+            self.cutoff
+
+        Parameters
+        ----------
+        fh : guaranteed to be ForecastingHorizon or None, optional (default=None)
+            The forecasting horizon with the steps ahead to to predict.
+            If not passed in _fit, guaranteed to be passed here
+        X : pd.DataFrame, optional (default=None)
+            Exogenous time series
+
+        Returns
+        -------
+        y_pred : pd.Series
+            Point predictions
+        """
+        # statsmodels requires zero-based indexing starting at the
+        # beginning of the training series when passing integers
+
+        start, end = fh.to_absolute_int(self._y.index[0], self.cutoff)[[0, -1]]
+        # statsmodels forecasts all periods from start to end of forecasting
+        # horizon, but only return given time points in forecasting horizon
+        valid_indices = fh.to_absolute_index(self.cutoff)
+
+        y_pred = self._fitted_forecaster.predict(
+            start=start, end=end, exog=self._X, exog_oos=X, fixed_oos=self.fixed_oos
+        )
+        y_pred.name = self._y.name
+        return y_pred.loc[valid_indices]
+        # implement here
+        # IMPORTANT: avoid side effects to X, fh
+
+    # todo: implement this if this is an estimator contributed to sktime
+    #   or to run local automated unit and integration testing of estimator
+    #   method should return default parameters, so that a test instance can be created
+    @classmethod
+    def get_test_params(cls, parameter_set="default"):
+        """Return testing parameter settings for the estimator.
+
+        Parameters
+        ----------
+        parameter_set : str, default="default"
+            Name of the set of test parameters to return, for use in tests. If no
+            special parameters are defined for a value, will return `"default"` set.
+            There are currently no reserved values for forecasters.
+
+        Returns
+        -------
+        params : dict or list of dict, default = {}
+            Parameters to create testing instances of the class
+            Each dict are parameters to construct an "interesting" test instance, i.e.,
+            `MyClass(**params)` or `MyClass(**params[i])` creates a valid test instance.
+            `create_test_instance` uses the first (or only) dictionary in `params`
+        """
+
+        # todo: set the testing parameters for the estimators
+        # Testing parameters can be dictionary or list of dictionaries.
+        # Testing parameter choice should cover internal cases well.
+        #   for "simple" extension, ignore the parameter_set argument.
+        #
+        # this method can, if required, use:
+        #   class properties (e.g., inherited); parent class test case
+        #   imported objects such as estimators from sktime or sklearn
+        # important: all such imports should be *inside get_test_params*, not at the top
+        #            since imports are used only at testing time
+        #
+        # A good parameter set should primarily satisfy two criteria,
+        #   1. Chosen set of parameters should have a low testing time,
+        #      ideally in the magnitude of few seconds for the entire test suite.
+        #       This is vital for the cases where default values result in
+        #       "big" models which not only increases test time but also
+        #       run into the risk of test workers crashing.
+        #   2. There should be a minimum two such parameter sets with different
+        #      sets of values to ensure a wide range of code coverage is provided.
+        #
+        # example 1: specify params as dictionary
+        # any number of params can be specified
+        # params = {"est": value0, "parama": value1, "paramb": value2}
+        #
+        # example 2: specify params as list of dictionary
+        # note: Only first dictionary will be used by create_test_instance
+        # params = [{"est": value1, "parama": value2},
+        #           {"est": value3, "parama": value4}]
+        #
+        # return params
