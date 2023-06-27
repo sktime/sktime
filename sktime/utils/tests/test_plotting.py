1c62ab98457c627d4ea5edc17a2815dda48e38ce
benHeid <benHeid@users.noreply.github.com>
2023-06-27 22:17:40 +0200

    Set the matplotlib backend to agg to avoid that a GUI is triggered.

--- sktime/utils/tests/test_plotting.py
+++ sktime/utils/tests/test_plotting.py
@@ -48,14 +48,14 @@ def valid_data_types():
     return valid_data_types
 
 
-# todo: remove skip when issue #2066 has been fixed
-@pytest.mark.skip(reason="sporadic failure on win CI/CD, see issue 2066")
 @pytest.mark.parametrize("series_to_plot", series_to_test)
 def test_plot_series_runs_without_error(series_to_plot):
     """Test whether plot_series runs without error."""
     _check_soft_dependencies("matplotlib")
+    import matplotlib
     import matplotlib.pyplot as plt
 
+    matplotlib.use("agg")
     _plot_series(series_to_plot)
     plt.gcf().canvas.draw_idle()
 
@@ -69,8 +69,6 @@ def test_plot_series_runs_without_error(series_to_plot):
     plt.close()
 
 
-# todo: remove skip when issue #2066 has been fixed
-@pytest.mark.skip(reason="sporadic failure on win CI/CD, see issue 2066")
 @pytest.mark.parametrize("series_to_plot", invalid_input_types)
 def test_plot_series_invalid_input_type_raises_error(series_to_plot, valid_data_types):
     """Tests whether plot_series raises error for invalid input types."""
@@ -88,8 +86,6 @@ def test_plot_series_invalid_input_type_raises_error(series_to_plot, valid_data_
             _plot_series(series_to_plot)
 
 
-# todo: remove skip when issue #2066 has been fixed
-@pytest.mark.skip(reason="sporadic failure on win CI/CD, see issue 2066")
 @pytest.mark.parametrize(
     "series_to_plot", [(y_airline_true, y_airline_test.reset_index(drop=True))]
 )
@@ -102,8 +98,6 @@ def test_plot_series_with_unequal_index_type_raises_error(
         _plot_series(series_to_plot)
 
 
-# todo: remove skip when issue #2066 has been fixed
-@pytest.mark.skip(reason="sporadic failure on win CI/CD, see issue 2066")
 @pytest.mark.parametrize("series_to_plot", series_to_test)
 def test_plot_series_invalid_marker_kwarg_len_raises_error(series_to_plot):
     """Tests whether plot_series raises error for inconsistent series/markers."""
@@ -121,8 +115,6 @@ def test_plot_series_invalid_marker_kwarg_len_raises_error(series_to_plot):
         _plot_series(series_to_plot, markers=markers)
 
 
-# todo: remove skip when issue #2066 has been fixed
-@pytest.mark.skip(reason="sporadic failure on win CI/CD, see issue 2066")
 @pytest.mark.parametrize("series_to_plot", series_to_test)
 def test_plot_series_invalid_label_kwarg_len_raises_error(series_to_plot):
     """Tests whether plot_series raises error for inconsistent series/labels."""
@@ -140,14 +132,14 @@ def test_plot_series_invalid_label_kwarg_len_raises_error(series_to_plot):
         _plot_series(series_to_plot, labels=labels)
 
 
-# todo: remove skip when issue #2066 has been fixed
-@pytest.mark.skip(reason="sporadic failure on win CI/CD, see issue 2066")
 @pytest.mark.parametrize("series_to_plot", series_to_test)
 def test_plot_series_output_type(series_to_plot):
     """Tests whether plot_series returns plt.fig and plt.ax."""
     _check_soft_dependencies("matplotlib")
+    import matplotlib
     import matplotlib.pyplot as plt
 
+    matplotlib.use("agg")
     # Test output case where kwarg ax=None
     fig, ax = _plot_series(series_to_plot)
 
@@ -175,14 +167,15 @@ def test_plot_series_output_type(series_to_plot):
     )
 
 
-# todo: remove skip when issue #2066 has been fixed
-@pytest.mark.skip(reason="sporadic failure on win CI/CD, see issue 2066")
 def test_plot_series_uniform_treatment_of_int64_range_index_types():
     """Verify that plot_series treats Int64 and Range indices equally."""
     # We test that int64 and range indices are treated uniformly and do not raise an
     # error of inconsistent index types
     _check_soft_dependencies("matplotlib")
+    import matplotlib
     import matplotlib.pyplot as plt
+
+    matplotlib.use("agg")
 
     y1 = pd.Series(np.arange(10))
     y2 = pd.Series(np.random.normal(size=10))
@@ -193,23 +186,21 @@ def test_plot_series_uniform_treatment_of_int64_range_index_types():
     plt.close()
 
 
-# todo: remove skip when issue #2066 has been fixed
-@pytest.mark.skip(reason="sporadic failure on win CI/CD, see issue 2066")
 # Generically test whether plots only accepting univariate input run
 @pytest.mark.parametrize("series_to_plot", [y_airline])
 @pytest.mark.parametrize("plot_func", univariate_plots)
 def test_univariate_plots_run_without_error(series_to_plot, plot_func):
     """Tests whether plots that accept univariate series run without error."""
     _check_soft_dependencies("matplotlib")
+    import matplotlib
     import matplotlib.pyplot as plt
 
+    matplotlib.use("agg")
     plot_func(series_to_plot)
     plt.gcf().canvas.draw_idle()
     plt.close()
 
 
-# todo: remove skip when issue #2066 has been fixed
-@pytest.mark.skip(reason="sporadic failure on win CI/CD, see issue 2066")
 # Generically test whether plots only accepting univariate input
 # raise an error when invalid input type is found
 @pytest.mark.parametrize("series_to_plot", invalid_input_types)
@@ -231,16 +222,16 @@ def test_univariate_plots_invalid_input_type_raises_error(
             plot_func(series_to_plot)
 
 
-# todo: remove skip when issue #2066 has been fixed
-@pytest.mark.skip(reason="sporadic failure on win CI/CD, see issue 2066")
 # Generically test output of plots only accepting univariate input
 @pytest.mark.parametrize("series_to_plot", [y_airline])
 @pytest.mark.parametrize("plot_func", univariate_plots)
 def test_univariate_plots_output_type(series_to_plot, plot_func):
     """Tests whether plots accepting univariate series have correct output types."""
     _check_soft_dependencies("matplotlib")
+    import matplotlib
     import matplotlib.pyplot as plt
 
+    matplotlib.use("agg")
     fig, ax = plot_func(series_to_plot)
 
     is_fig_figure = isinstance(fig, plt.Figure)
@@ -255,8 +246,6 @@ def test_univariate_plots_output_type(series_to_plot, plot_func):
     )
 
 
-# todo: remove skip when issue #2066 has been fixed
-@pytest.mark.skip(reason="sporadic failure on win CI/CD, see issue 2066")
 # For plots that only accept univariate input, from here onwards are
 # tests specific to a given plot. E.g. to test specific arguments or functionality.
 @pytest.mark.parametrize("series_to_plot", [y_airline])
@@ -265,15 +254,17 @@ def test_univariate_plots_output_type(series_to_plot, plot_func):
 def test_plot_lags_arguments(series_to_plot, lags, suptitle):
     """Tests whether plot_lags run with different input arguments."""
     _check_soft_dependencies("matplotlib")
+
+    import matplotlib
     import matplotlib.pyplot as plt
+
+    matplotlib.use("agg")
 
     plot_lags(series_to_plot, lags=lags, suptitle=suptitle)
     plt.gcf().canvas.draw_idle()
     plt.close()
 
 
-# todo: remove skip when issue #2066 has been fixed
-@pytest.mark.skip(reason="sporadic failure on win CI/CD, see issue 2066")
 @pytest.mark.parametrize("series_to_plot", [y_airline])
 @pytest.mark.parametrize("lags", [6, 12, 24, 36])
 @pytest.mark.parametrize("suptitle", ["Correlation Plot", None])
@@ -281,8 +272,11 @@ def test_plot_lags_arguments(series_to_plot, lags, suptitle):
 def test_plot_correlations_arguments(series_to_plot, lags, suptitle, series_title):
     """Tests whether plot_lags run with different input arguments."""
     _check_soft_dependencies("matplotlib")
+
+    import matplotlib
     import matplotlib.pyplot as plt
 
+    matplotlib.use("agg")
     plot_correlations(
         series_to_plot, lags=lags, suptitle=suptitle, series_title=series_title
     )
