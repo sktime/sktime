1ce55622d5530a38bff55013134026a41c81b648
Franz Király <f.kiraly@ucl.ac.uk>
2023-07-08 16:25:42 +0100

    linting

--- sktime/forecasting/compose/_seasonal.py
+++ sktime/forecasting/compose/_seasonal.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3 -u
-# -*- coding: utf-8 -*-
-# copyright: sktime developers, BSD-3-Clause License (see LICENSE file)
-
 """Seasonal reduction to multivariate."""
+# copyright: sktime developers, BSD-3-Clause License (see LICENSE file)
 
 __author__ = ["fkiraly"]
 
