ead08564394ecd8d3717fc96ff9b835ee2a38e0e
Franz Király <f.kiraly@ucl.ac.uk>
2023-07-12 01:20:05 +0100

    [DOC] minor improvements to forecasting and transformer extension templates (#4828)
    Makes minor improvements to the forecasting and transformer extension
    templates:
    
    * adds reference to the "simple" extension template in the preamble
    * updates the files to comply with the new (post-3.7) linting
    conventions

--- extension_templates/transformer_simple.py
+++ extension_templates/transformer_simple.py
@@ -4,7 +4,7 @@
 Contains only bare minimum of implementation requirements for a functional transformer.
 Also assumes *no composition*, i.e., no transformer or other estimator components.
 For advanced cases (inverse transform, composition, etc),
-    see full extension template in forecasting.py
+    see full extension template in transformer.py
 
 Purpose of this implementation template:
     quick implementation of new estimators following the template
