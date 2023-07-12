af4f262b7379b9c3d6809395630eba26910726dd
Luca Miniati <luca.miniati@gmail.com>
2023-07-11 19:45:30 -0700

    add python_dependencies to StatsForecastMSTL

--- sktime/forecasting/statsforecast.py
+++ sktime/forecasting/statsforecast.py
@@ -591,6 +591,7 @@ class StatsForecastMSTL(_GeneralisedStatsForecastAdapter):
         "ignores-exogeneous-X": True,
         "capability:pred_int": True,
         "capability:pred_int:insample": True,
+        "python_dependencies": ["statsmodels", "statsforecast"]
     }
 
     def __init__(
