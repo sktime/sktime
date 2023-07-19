a5e9379e328d6981cf5068d953421f5f94626a9a
loo <luca.miniati@gmail.com>
2023-07-18 18:33:36 -0700

    fix fh

--- sktime/forecasting/base/adapters/_generalised_statsforecast.py
+++ sktime/forecasting/base/adapters/_generalised_statsforecast.py
@@ -354,14 +354,16 @@ class StatsForecastBackAdapter:
                 "supports it."
             )
 
-        mean = self.estimator.predict(fh=h, X=X)[:, 0]
+        mean = self.estimator.predict(fh=range(1, h + 1), X=X)[:, 0]
         if level is None:
             return {"mean": mean}
 
         level = sorted(level)
         coverage = [round(1 - (_l / 100), 2) for _l in level]
 
-        pred_int = self.estimator.predict_interval(fh=h, X=X, coverage=coverage)
+        pred_int = self.estimator.predict_interval(
+            fh=range(1, h + 1), X=X, coverage=coverage
+        )
 
         return self.format_pred_int("mean", mean, pred_int, coverage, level)
 
