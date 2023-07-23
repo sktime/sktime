589256d2d5a4d5674602fc9bbe8d82b817555e3e
Julia Kraus <j.kraus@appliedai.de>
2023-07-23 22:01:36 +0200

    refactor: split `test_output` into two functions, one for quantile predictions, and one for interval predictions
    Reason: The original functions had 96 parameter combinations and hence 96 tests. Also, quantiles and intervals have their dedicated metrics, so cross-combinations are unnecessary. The separation speeds up the test suite considerably.

--- sktime/performance_metrics/forecasting/probabilistic/tests/test_probabilistic_metrics.py
+++ sktime/performance_metrics/forecasting/probabilistic/tests/test_probabilistic_metrics.py
@@ -32,45 +32,6 @@ alpha_m = [0.05, 0.5, 0.95]
 coverage_s = 0.9
 coverage_m = [0.7, 0.8, 0.9, 0.99]
 
-# alpha will be either s or m
-# n_column will be either 1 (uni) or 3 (multi)
-@pytest.fixture
-def get_quantile_data(request):
-
-    # get params for fixture
-    n_columns, alpha = request.param
-
-    # create data
-    y = _make_series(n_columns=n_columns)
-    y_train, y_test = temporal_train_test_split(y)
-    fh = np.arange(len(y_test)) + 1
-
-    # fit model
-    f = NaiveVariance(NaiveForecaster())
-    f.fit(y_train)
-
-    # predict model
-    quantile_pred = f.predict_quantiles(fh=fh, alpha=alpha)
-
-    return y_test, quantile_pred
-
-@pytest.fixture
-def get_interval_data(request):
-
-    n_columns, coverage = request.param
-
-    y = _make_series(n_columns=n_columns)
-    y_train, y_test = temporal_train_test_split(y)
-    fh = np.arange(len(y_test)) + 1
-
-    # fit model
-    f = NaiveVariance(NaiveForecaster())
-    f.fit(y_train)
-
-    # predict model
-    interval_pred = f.predict_interval(fh=fh, coverage=coverage)
-
-    return y_test, interval_pred
 
 @pytest.fixture
 def sample_data(request):
@@ -96,52 +57,33 @@ def sample_data(request):
 
     return
 
-######## Test the parametrized fixtures ##############
-# Use the parametric fixture with multiple parameters in a test
-@pytest.mark.parametrize("get_quantile_data",
-                         [(1, alpha_s), (3, alpha_s), (1, alpha_s), (3, alpha_s)], indirect=True)
-def test_get_data_quantile(get_quantile_data):
-
-    y_true, y_pred = get_quantile_data
-    assert isinstance(y_true, (pd.Series, pd.DataFrame))
-    assert isinstance(y_pred, pd.DataFrame)
-
-
-# Use the parametric fixture with multiple parameters in a test
-@pytest.mark.parametrize("get_interval_data",
-                         [(1, coverage_s), (3, coverage_s), (1, coverage_m), (3, coverage_m)], indirect=True)
-def test_get_data_interval(get_interval_data):
-    y_true, y_pred = get_interval_data
-    assert isinstance(y_true, (pd.Series, pd.DataFrame))
-    assert isinstance(y_pred, pd.DataFrame)
-
-# Use the parametric fixture with multiple parameters in a test
-@pytest.mark.parametrize("get_data",
+# Test the parametrized fixture
+@pytest.mark.parametrize("sample_data",
                          [(1, alpha_s, 'quantile'), (3, alpha_s, 'quantile'), (1, alpha_m, 'quantile'),
                           (3, alpha_m, 'quantile'),
                           (1, coverage_s, 'interval'), (3, coverage_s, 'interval'), (1, coverage_m, 'interval'),
                           (3, coverage_m, 'interval')], indirect=True)
-def test_get_data(sample_data):
+def test_sample_data(sample_data):
     y_true, y_pred = sample_data
     assert isinstance(y_true, (pd.Series, pd.DataFrame))
     assert isinstance(y_pred, pd.DataFrame)
 
 
-# # @pytest.mark.parametrize("get_data",
+# # @pytest.mark.parametrize("sample_data",
 # #                          [(1, alpha_s, 'interval'), (3, alpha_s, 'interval'),
 # #                           (1, alpha_m, 'interval'), (3, alpha_m, 'interval'),
 # #                           (1, coverage_s, 'quantile'), (3, coverage_s, 'quantile'),
 # #                           (1, coverage_m, 'quantile'), (3, coverage_m, 'quantile')], indirect=True)
-# @pytest.mark.parametrize("get_data",
+# @pytest.mark.parametrize("sample_data",
 #                          [(1, coverage_s, 'interval'), (3, coverage_s, 'interval'),
 #                           (1, alpha_s, 'quantile'), (3, alpha_s, 'quantile')
 #                           ], indirect=True)
 # @pytest.mark.parametrize("metric", all_metrics)
 # @pytest.mark.parametrize("multioutput", ["uniform_average", "raw_values"])
 # @pytest.mark.parametrize("score_average", [True, False])
-# def test_output(metric, score_average, multioutput, get_data):
+# def test_output(metric, score_average, multioutput, sample_data):
 
-#     y_true, y_pred = get_data
+#     y_true, y_pred = sample_data
 #     """Test output is correct class and shape for given data."""
 #     loss = metric.create_test_instance()
 #     loss.set_params(score_average=score_average, multioutput=multioutput)
@@ -163,7 +105,7 @@ def test_get_data(sample_data):
 #             no_vars = 0
 
 #     if score_average and multioutput == "uniform_average":
-#         assert isinstance(eval_loss, float)
+#         assert isinstance(eval_loss, float)I
 #         assert isinstance(index_loss, pd.Series)
 
 #         assert len(index_loss) == y_pred.shape[0]
@@ -266,7 +208,7 @@ def helper_check_output(metric, score_average, multioutput, sample_data):
         assert len(eval_loss) == no_vars
 
 
-@pytest.mark.parametrize("get_data",
+@pytest.mark.parametrize("sample_data",
                          [(1, alpha_s, 'quantile'), (3, alpha_s, 'quantile'),
                           (1, alpha_m, 'quantile'), (3, alpha_m, 'quantile')
                           ], indirect=True)
@@ -275,68 +217,11 @@ def helper_check_output(metric, score_average, multioutput, sample_data):
 @pytest.mark.parametrize("score_average", [True, False])
 def test_output_quantiles(metric, score_average, multioutput, sample_data):
 
-    y_true, y_pred = sample_data
-    """Test output is correct class and shape for given data."""
-    loss = metric.create_test_instance()
-    loss.set_params(score_average=score_average, multioutput=multioutput)
+    helper_check_output(metric, score_average, multioutput, sample_data)
 
-    eval_loss = loss(y_true, y_pred)
-    index_loss = loss.evaluate_by_index(y_true, y_pred)
 
-    no_vars = len(y_pred.columns.get_level_values(0).unique())
-    no_scores = len(y_pred.columns.get_level_values(1).unique())
 
-    if (
-        0.5 in y_pred.columns.get_level_values(1)
-        and loss.get_tag("scitype:y_pred") == "pred_interval"
-        and y_pred.columns.nlevels == 2
-    ):
-        no_scores = no_scores - 1
-        no_scores = no_scores / 2  # one interval loss per two quantiles given
-        if no_scores == 0:  # if only 0.5 quant, no output to interval loss
-            no_vars = 0
-
-    if score_average and multioutput == "uniform_average":
-        assert isinstance(eval_loss, float)
-        assert isinstance(index_loss, pd.Series)
-
-        assert len(index_loss) == y_pred.shape[0]
-
-    if not score_average and multioutput == "uniform_average":
-        assert isinstance(eval_loss, pd.Series)
-        assert isinstance(index_loss, pd.DataFrame)
-
-        # get two quantiles from each interval so if not score averaging
-        # get twice number of unique coverages
-        if (
-            loss.get_tag("scitype:y_pred") == "pred_quantiles"
-            and y_pred.columns.nlevels == 3
-        ):
-            assert len(eval_loss) == 2 * no_scores
-        else:
-            assert len(eval_loss) == no_scores
-
-    if not score_average and multioutput == "raw_values":
-        assert isinstance(eval_loss, pd.Series)
-        assert isinstance(index_loss, pd.DataFrame)
-
-        true_len = no_vars * no_scores
-
-        if (
-            loss.get_tag("scitype:y_pred") == "pred_quantiles"
-            and y_pred.columns.nlevels == 3
-        ):
-            assert len(eval_loss) == 2 * true_len
-        else:
-            assert len(eval_loss) == true_len
-
-    if score_average and multioutput == "raw_values":
-        assert isinstance(eval_loss, pd.Series)
-        assert isinstance(index_loss, pd.DataFrame)
-
-        assert len(eval_loss) == no_vars
-
-@pytest.mark.parametrize("get_data",
+@pytest.mark.parametrize("sample_data",
                          [(1, coverage_s, 'interval'), (3, coverage_s, 'interval'),
                           (1, coverage_m, 'interval'), (3, coverage_m, 'interval')], indirect=True)
 @pytest.mark.parametrize("metric", interval_metrics)
@@ -344,87 +229,27 @@ def test_output_quantiles(metric, score_average, multioutput, sample_data):
 @pytest.mark.parametrize("score_average", [True, False])
 def test_output_intervals(metric, score_average, multioutput, sample_data):
 
-    y_true, y_pred = sample_data
-    """Test output is correct class and shape for given data."""
-    loss = metric.create_test_instance()
-    loss.set_params(score_average=score_average, multioutput=multioutput)
-
-    eval_loss = loss(y_true, y_pred)
-    index_loss = loss.evaluate_by_index(y_true, y_pred)
-
-    no_vars = len(y_pred.columns.get_level_values(0).unique())
-    no_scores = len(y_pred.columns.get_level_values(1).unique())
-
-    if (
-        0.5 in y_pred.columns.get_level_values(1)
-        and loss.get_tag("scitype:y_pred") == "pred_interval"
-        and y_pred.columns.nlevels == 2
-    ):
-        no_scores = no_scores - 1
-        no_scores = no_scores / 2  # one interval loss per two quantiles given
-        if no_scores == 0:  # if only 0.5 quant, no output to interval loss
-            no_vars = 0
-
-    if score_average and multioutput == "uniform_average":
-        assert isinstance(eval_loss, float)
-        assert isinstance(index_loss, pd.Series)
-
-        assert len(index_loss) == y_pred.shape[0]
-
-    if not score_average and multioutput == "uniform_average":
-        assert isinstance(eval_loss, pd.Series)
-        assert isinstance(index_loss, pd.DataFrame)
-
-        # get two quantiles from each interval so if not score averaging
-        # get twice number of unique coverages
-        if (
-            loss.get_tag("scitype:y_pred") == "pred_quantiles"
-            and y_pred.columns.nlevels == 3
-        ):
-            assert len(eval_loss) == 2 * no_scores
-        else:
-            assert len(eval_loss) == no_scores
-
-    if not score_average and multioutput == "raw_values":
-        assert isinstance(eval_loss, pd.Series)
-        assert isinstance(index_loss, pd.DataFrame)
-
-        true_len = no_vars * no_scores
-
-        if (
-            loss.get_tag("scitype:y_pred") == "pred_quantiles"
-            and y_pred.columns.nlevels == 3
-        ):
-            assert len(eval_loss) == 2 * true_len
-        else:
-            assert len(eval_loss) == true_len
-
-    if score_average and multioutput == "raw_values":
-        assert isinstance(eval_loss, pd.Series)
-        assert isinstance(index_loss, pd.DataFrame)
-
-        assert len(eval_loss) == no_vars
+    helper_check_output(metric, score_average, multioutput, sample_data)
 
 
 
-# This test tests the quantile data
-@pytest.mark.parametrize("Metric", quantile_metrics)
-@pytest.mark.parametrize("get_data",
+@pytest.mark.parametrize("metric", quantile_metrics)
+@pytest.mark.parametrize("sample_data",
                          [(1, alpha_s, 'quantile'), (3, alpha_s, 'quantile'),
                           (1, alpha_m, 'quantile'), (3, alpha_m, 'quantile')],
                           indirect=True)
-def test_evaluate_alpha_positive(Metric, sample_data):
+def test_evaluate_alpha_positive(metric, sample_data):
     """Tests output when required quantile is present."""
     # 0.5 in test quantile data don't raise error.
 
     y_true, y_pred = sample_data
 
-    Loss = Metric.create_test_instance().set_params(alpha=0.5, score_average=False)
+    Loss = metric.create_test_instance().set_params(alpha=0.5, score_average=False)
     res = Loss(y_true=y_true, y_pred=y_pred)
     assert len(res) == 1
 
     if all(x in y_pred.columns.get_level_values(1) for x in [0.5, 0.95]):
-        Loss = Metric.create_test_instance().set_params(
+        Loss = metric.create_test_instance().set_params(
             alpha=[0.5, 0.95], score_average=False
         )
         res = Loss(y_true=y_true, y_pred=y_pred)
@@ -432,16 +257,16 @@ def test_evaluate_alpha_positive(Metric, sample_data):
 
 
 # This test tests quantile data
-@pytest.mark.parametrize("get_data",
+@pytest.mark.parametrize("sample_data",
                          [(1, alpha_s, 'quantile'), (3, alpha_s, 'quantile'),
                           (1, alpha_m, 'quantile'), (3, alpha_m, 'quantile')],
                           indirect=True)
-@pytest.mark.parametrize("Metric", quantile_metrics)
+@pytest.mark.parametrize("metric", quantile_metrics)
 
-def test_evaluate_alpha_negative(Metric, sample_data):
+def test_evaluate_alpha_negative(metric, sample_data):
     """Tests whether correct error raised when required quantile not present."""
     y_true, y_pred = sample_data
     with pytest.raises(ValueError):
         # 0.3 not in test quantile data so raise error.
-        Loss = Metric.create_test_instance().set_params(alpha=0.3)
+        Loss = metric.create_test_instance().set_params(alpha=0.3)
         res = Loss(y_true=y_true, y_pred=y_pred)  # noqa
