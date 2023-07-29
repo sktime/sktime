0268141aa177a013051b281b361e8062e2fa0034
Franz Király <f.kiraly@ucl.ac.uk>
2023-07-29 10:26:51 +0100

    [BUG] fix `PluginParamsForecaster` in `params: dict` case (#4922)
    Fixes https://github.com/sktime/sktime/issues/4921
    
    The internal logic for parsing the `dict` for plugin specification was
    jumbled - the subsetting would subset the opposite way round, compared
    to the plugin direction.
    
    This has now been fixed by changing the plugin direction, making it also
    consistent with user expectation in the `dict` case.
    
    The failure case from
    https://github.com/sktime/sktime/discussions/4884#discussioncomment-6506787
    has been added as a test.
    
    Deprecation is not needed imo, as this replaces a bug failure with
    actual behaviour, even if it changes the specification claimed by the
    docstring.

--- sktime/param_est/tests/test_plugin.py
+++ sktime/param_est/tests/test_plugin.py
@@ -3,9 +3,10 @@
 
 __author__ = ["fkiraly"]
 
+import numpy as np
 import pytest
 
-from sktime.datasets import load_airline
+from sktime.datasets import load_airline, load_longley
 from sktime.forecasting.naive import NaiveForecaster
 from sktime.param_est.fixed import FixedParams
 from sktime.param_est.plugin import PluginParamsForecaster
@@ -34,3 +35,46 @@ def test_seasonality_acf():
     PluginParamsForecaster(
         FixedParams({"foo": 12}), NaiveForecaster(), params={"foo": "sp"}
     )
+
+
+def test_paramplugin_dict():
+    """Test PluginParamsForecaster with param: dict.
+
+    Failure case from bug report #4921.
+    """
+    from sklearn.ensemble import RandomForestRegressor
+
+    from sktime.forecasting.base import ForecastingHorizon
+    from sktime.forecasting.compose import EnsembleForecaster, make_reduction
+    from sktime.forecasting.model_selection import (
+        ExpandingWindowSplitter,
+        ForecastingGridSearchCV,
+    )
+    from sktime.param_est.plugin import PluginParamsForecaster
+
+    y, X = load_longley()
+    horizon = ForecastingHorizon(np.arange(1, 4), is_relative=True)
+
+    random_forest = make_reduction(
+        RandomForestRegressor(),
+        window_length=2,
+        strategy="direct",
+        windows_identical=False,
+    )
+    expanding_window_cv = ExpandingWindowSplitter(fh=horizon, step_length=1)
+    cv_random_forest = ForecastingGridSearchCV(
+        forecaster=random_forest,
+        cv=expanding_window_cv,
+        param_grid={"estimator__max_features": np.linspace(0.1, 0.9, num=5)},
+        return_n_best_forecasters=3,
+    )
+
+    ensembler = EnsembleForecaster(forecasters=[], aggfunc="median")
+    plugin_fcst = PluginParamsForecaster(
+        param_est=cv_random_forest,
+        forecaster=ensembler,
+        params={"forecasters": "n_best_forecasters"},
+    )
+
+    plugin_fcst.fit(y, X, fh=horizon)
+    plugin_fcst.predict(fh=horizon, X=X)
