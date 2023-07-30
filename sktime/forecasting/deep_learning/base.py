679926fb29c8f7ffece146ae047b95ace25b8f1e
loo <luca.miniati@gmail.com>
2023-07-30 11:46:41 -0700

    changes

--- sktime/forecasting/deep_learning/base.py
+++ sktime/forecasting/deep_learning/base.py
@@ -1,6 +1,8 @@
 """Abstract base class for PyTorch network forecasters."""
 from abc import ABC
 
+import numpy as np
+import pandas as pd
 import torch
 
 from sktime.forecasting.base import BaseForecaster
@@ -17,6 +19,7 @@ class BaseDeepNetworkPyTorch(BaseForecaster, ABC):
         lr=0.003,
         num_epochs=50,
         batch_size=8,
+        shuffle=True,
     ):
         super().__init__()
 
@@ -26,6 +29,7 @@ class BaseDeepNetworkPyTorch(BaseForecaster, ABC):
         self.optimizer = optimizer
         self.num_epochs = num_epochs
         self.batch_size = batch_size
+        self.shuffle = shuffle
 
     def _fit(self, y, X, fh):
         """Fit the network.
@@ -38,11 +42,7 @@ class BaseDeepNetworkPyTorch(BaseForecaster, ABC):
         X : iterable-style or map-style dataset
             see (https://pytorch.org/docs/stable/data.html) for more information
         """
-        from torch.utils.data import DataLoader
-
-        from sktime.networks.data import ETTDataset
-
-        dataloader = DataLoader(ETTDataset(y), batch_size=self.batch_size, shuffle=True)
+        dataloader = self.build_pytorch_dataloader(y)
         self.network.train()
 
         criterion = self.criterion()
@@ -58,5 +58,81 @@ class BaseDeepNetworkPyTorch(BaseForecaster, ABC):
 
     def _predict(self, X, **kwargs):
         """Predict with fitted model."""
-        self.network.eval()
-        return self.network(X)
+        dataloader = self.build_pytorch_dataloader(X)
+        y_pred = []
+        for x, _ in dataloader:
+            y_pred.append(self.network(x).detach().numpy())
+        return np.concatenate(y_pred)
+
+    def build_pytorch_dataloader(self, y):
+        """Build PyTorch DataLoader for training."""
+        from torch.utils.data import DataLoader
+
+        return DataLoader(
+            PyTorchDataset(
+                y, self.network.seq_len, self.network.pred_len, shuffle=self.shuffle
+            ),
+            self.batch_size,
+        )
+
+    def get_y_true(self, y):
+        """Get y_true values for validation."""
+        y_true = []
+        for i in range(len(y) - self.network.seq_len - self.network.pred_len + 1):
+            y_true_values = y.iloc[
+                i
+                + self.network.seq_len : i
+                + self.network.seq_len
+                + self.network.pred_len
+            ]
+            y_true.append(y_true_values)
+        return y_true
+
+
+class PyTorchDataset:
+    """Dataset for use in sktime deep learning forecasters."""
+
+    def __init__(self, y, seq_len, pred_len, shuffle):
+        self.seq_len = seq_len
+        self.pred_len = pred_len
+
+        if shuffle:
+            self.y = [
+                y.iloc[i : i + seq_len + pred_len]
+                for i in range(0, len(y) - seq_len - pred_len + 1)
+            ]
+            np.random.shuffle(self.y)
+            self.y = pd.concat(self.y, ignore_index=True)
+        else:
+            self.y = y
+
+    def __len__(self):
+        """Return length of dataset."""
+        return len(self.y) // (self.seq_len + self.pred_len)
+
+    def __getitem__(self, i):
+        """Return data point."""
+        return (
+            torch.tensor(
+                self.y.iloc[
+                    i
+                    + self.seq_len
+                    + self.pred_len : i
+                    + self.seq_len
+                    + self.pred_len
+                    + self.seq_len
+                ].values
+            ).float(),
+            torch.from_numpy(
+                self.y.iloc[
+                    i
+                    + self.seq_len
+                    + self.pred_len
+                    + self.seq_len : i
+                    + self.seq_len
+                    + self.pred_len
+                    + self.seq_len
+                    + self.pred_len
+                ].values
+            ).float(),
+        )
--- sktime/forecasting/ltsf.py
+++ sktime/forecasting/ltsf.py
@@ -1,8 +1,84 @@
 """Deep Learning Forecasters using LTSF-Linear Models."""
 
 import torch
+import torch.nn as nn
 
 from sktime.forecasting.deep_learning.base import BaseDeepNetworkPyTorch
+
+
+class LTSFLinearNetwork(nn.Module):
+    """LSTF-Linear Network.
+
+    Just one Linear layer. Taken from _[1].
+
+    Parameters
+    ----------
+    seq_len : int
+        length of input sequence
+    pred_len : int
+        length of prediction (forecast horizon)
+    in_channels : int, default=None
+        number of input channels passed to network
+    individual : bool, default=False
+        boolean flag that controls whether the network treats each channel individually"
+        "or applies a single linear layer across all channels. If individual=True, the"
+        "a separate linear layer is created for each input channel. If"
+        "individual=False, a single shared linear layer is used for all channels."
+
+    References
+    ----------
+    [1] @inproceedings{Zeng2022AreTE,
+        title={Are Transformers Effective for Time Series Forecasting?},
+        author={Ailing Zeng and Muxi Chen and Lei Zhang and Qiang Xu},
+        journal={Proceedings of the AAAI Conference on Artificial Intelligence},
+        year={2023}
+    }
+        [Source]: https://github.com/cure-lab/LTSF-Linear/blob/main/models/Linear.py
+    """
+
+    def __init__(
+        self,
+        seq_len,
+        pred_len,
+        in_channels=1,
+        individual=False,
+    ):
+        super().__init__()
+        self.seq_len = seq_len
+        self.pred_len = pred_len
+        self.in_channels = in_channels
+        self.individual = individual
+
+        if self.individual:
+            self.Linear = nn.ModuleList()
+            for _ in range(self.in_channels):
+                self.Linear.append(nn.Linear(self.seq_len, self.pred_len))
+        else:
+            self.Linear = nn.Linear(self.seq_len, self.pred_len)
+
+    def forward(self, x):
+        """Forward pass for LSTF-Linear Network.
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            torch.Tensor of shape [Batch, Input Sequence Length, Channel]
+
+        Returns
+        -------
+        x : torch.Tensor
+            output of Linear Model. x.shape = [Batch, Output Length, Channel]
+        """
+        if self.individual:
+            output = torch.zeros(
+                [x.size(0), self.pred_len, x.size(2)], dtype=x.dtype
+            ).to(x.device)
+            for i in range(self.in_channels):
+                output[:, :, i] = self.Linear[i](x[:, :, i])
+            x = output
+        else:
+            x = self.Linear(x.permute(0, 2, 1)).permute(0, 2, 1)
+        return x  # [Batch, Output Length, Channel]
 
 
 class LTSFLinearForecaster(BaseDeepNetworkPyTorch):
@@ -35,20 +111,24 @@ class LTSFLinearForecaster(BaseDeepNetworkPyTorch):
 
     # TODO: fix docstring
 
+    _tags = {
+        "scitype:y": "both",
+        "y_inner_mtype": "pd.DataFrame",
+        "requires-fh-in-fit": False,
+    }
+
     def __init__(
         self,
-        seq_len=10,  # L : Historical data
-        pred_len=1,  # T : Future predictions
+        seq_len,  # L : Historical data
+        pred_len,  # T : Future predictions
+        in_channels=1,
         criterion=torch.nn.MSELoss,
         optimizer=torch.optim.Adam,
-        lr=0.003,
+        lr=0.001,
         num_epochs=16,
         batch_size=8,
-        in_channels=1,
         individual=False,
     ):
-        from sktime.networks.ltsf import LTSFLinearNetwork
-
         self.seq_len = seq_len
         self.pred_len = pred_len
         self.criterion = criterion
@@ -72,3 +152,5 @@ class LTSFLinearForecaster(BaseDeepNetworkPyTorch):
             num_epochs=num_epochs,
             batch_size=batch_size,
         )
+
+        self._fh = pred_len
