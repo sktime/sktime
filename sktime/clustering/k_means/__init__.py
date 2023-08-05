ddc2ed90ce063d181325fbde9fc98e84ffa4f780
Franz Király <f.kiraly@ucl.ac.uk>
2023-08-05 23:24:17 +0100

    kmeans tslearn

---
+++ sktime/clustering/k_means/__init__.py
@@ -0,0 +1,8 @@
+"""Time series clustering module."""
+__all__ = [
+    "TimeSeriesKMeans",
+    "TimeSeriesKMeansTslearn"
+]
+
+from sktime.clustering.k_means._k_means import TimeSeriesKMeans
+from sktime.clustering.k_means._k_means_tslearn import TimeSeriesKMeansTslearn
