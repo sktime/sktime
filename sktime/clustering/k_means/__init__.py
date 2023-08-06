8c46d0ac2726aad59e36b9b7198174c9410282c3
Franz Király <f.kiraly@ucl.ac.uk>
2023-08-06 11:24:55 +0100

    Update __init__.py

--- sktime/clustering/k_means/__init__.py
+++ sktime/clustering/k_means/__init__.py
@@ -1,8 +1,5 @@
-"""Time series clustering module."""
-__all__ = [
-    "TimeSeriesKMeans",
-    "TimeSeriesKMeansTslearn"
-]
+"""Time series kmeans clustering module."""
+__all__ = ["TimeSeriesKMeans", "TimeSeriesKMeansTslearn"]
 
 from sktime.clustering.k_means._k_means import TimeSeriesKMeans
 from sktime.clustering.k_means._k_means_tslearn import TimeSeriesKMeansTslearn
