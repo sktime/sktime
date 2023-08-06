3279c1f0cba661fa65c8c671e0bb1088f3f136e6
Anirban Ray <39331844+yarnabrina@users.noreply.github.com>
2023-08-06 19:51:47 +0530

    initial failing commit for darts support

---
+++ sktime/forecasting/base/adapters/_darts.py
@@ -0,0 +1,241 @@
+# copyright: sktime developers, BSD-3-Clause License (see LICENSE file)
+"""Implements adapter for Darts models."""
+import abc
+from typing import List, Optional
+
+import pandas
+
+from sktime.forecasting.base import BaseForecaster, ForecastingHorizon
+
+__author__ = ["yarnabrina"]
+
+
+class _DartsAdapter(BaseForecaster):
+    """Base adapter class for Darts models.
+
+    Parameters
+    ----------
+    past_covariates : Optional[List[str]], optional
+        column names in ``X`` which are known only for historical data, by default None
+    num_samples : Optional[int], optional
+        Number of times a prediction is sampled from a probabilistic model, by default
+        1000
+
+    Notes
+    -----
+    If unspecified, all columns will be assumed to be known during prediction duration.
+    """
+
+    _tags = {
+        "y_inner_mtype": "pd.DataFrame",
+        "X_inner_mtype": "pd.DataFrame",
+        "requires-fh-in-fit": False,
+        "enforce_index_type": pandas.DatetimeIndex,
+        "handles-missing-data": False,
+        "capability:insample": False,
+        "python_version": ">=3.8",
+        "python_dependencies": ["u8darts"],
+        "python_dependecies_alias": {"u8darts": "darts"},
+    }
+
+    def __init__(
+        self: "_DartsAdapter",
+        past_covariates: Optional[List[str]] = None,
+        num_samples: Optional[int] = 1000,
+    ) -> None:
+        super().__init__()
+
+        self.past_covariates = [] if past_covariates is None else past_covariates
+        self.num_samples = num_samples
+
+    @staticmethod
+    def convert_dataframe_to_timeseries(dataset: pandas.DataFrame):
+        """Convert dataset for compatibility with ``darts``.
+
+        Parameters
+        ----------
+        dataset : pandas.DataFrame
+            source dataset to convert from
+
+        Returns
+        -------
+        darts.TimeSeries
+            converted target dataset
+        """
+        import darts
+
+        return darts.TimeSeries.from_dataframe(dataset)
+
+    def convert_exogenous_dataset(
+        self: "_DartsAdapter", dataset: Optional[pandas.DataFrame]
+    ):
+        """Make exogenous features to ``darts`` compatible, if available.
+
+        Parameters
+        ----------
+        dataset : Optional[pandas.DataFrame]
+            available data on exogenous features
+
+        Returns
+        -------
+        Tuple[darts.TimeSeries, darts.TimeSeries]
+            converted data on future known and future unknown exogenous features
+        """
+        if dataset is None and self.past_covariates:
+            raise ValueError(
+                f"Expected following exogenous features: {self.past_covariates}."
+            )
+
+        if dataset is None:
+            future_known_dataset = None
+            future_unknown_dataset = None
+        elif self.past_covariates:
+            future_unknown_dataset = self.convert_dataframe_to_timeseries(
+                dataset[self.past_covariates]
+            )
+            future_known_dataset = self.convert_dataframe_to_timeseries(
+                dataset.drop(columns=self.past_covariates)
+            )
+        else:
+            future_unknown_dataset = None
+            future_known_dataset = self.convert_dataframe_to_timeseries(dataset)
+
+        return future_known_dataset, future_unknown_dataset
+
+    @property
+    @abc.abstractmethod
+    def _forecaster(self: "_DartsAdapter"):
+        """Create Darts model."""
+
+    def _fit(
+        self: "_DartsAdapter",
+        y: pandas.DataFrame,
+        X: Optional[pandas.DataFrame],
+        fh: Optional[ForecastingHorizon],
+    ):
+        """Fit forecaster to training data.
+
+        private _fit containing the core logic, called from fit
+
+        Writes to self:
+            Sets fitted model attributes ending in "_".
+
+        Parameters
+        ----------
+        y : pd.DataFrame
+            if self.get_tag("scitype:y")=="univariate":
+                guaranteed to have a single column
+            if self.get_tag("scitype:y")=="both": no restrictions apply
+        fh : guaranteed to be ForecastingHorizon or None, optional (default=None)
+            The forecasting horizon with the steps ahead to to predict.
+            Required (non-optional) here.
+        X : pd.DataFrame, optional (default=None)
+            Exogeneous time series to fit to.
+
+        Returns
+        -------
+        self : reference to self
+        """
+        del fh  # avoid being detected as unused by ``vulture`` like tools
+
+        endogenous_actuals = self.convert_dataframe_to_timeseries(y)
+        unknown_exogenous, known_exogenous = self.convert_exogenous_dataset(X)
+
+        self._forecaster.fit(
+            endogenous_actuals,
+            past_covariates=unknown_exogenous,
+            future_covariates=known_exogenous,
+        )
+
+        return self
+
+    def _predict(
+        self: "_DartsAdapter",
+        fh: Optional[ForecastingHorizon],
+        X: Optional[pandas.DataFrame],
+    ):
+        """Forecast time series at future horizon.
+
+        private _predict containing the core logic, called from predict
+
+        State required:
+            Requires state to be "fitted".
+
+        Accesses in self:
+            Fitted model attributes ending in "_"
+            self.cutoff
+
+        Parameters
+        ----------
+        fh : guaranteed to be ForecastingHorizon or None, optional (default=None)
+            The forecasting horizon with the steps ahead to to predict.
+        X : pd.DataFrame, optional (default=None)
+            Exogenous time series
+
+        Returns
+        -------
+        y_pred : pd.DataFrame
+            Point predictions
+        """
+        unknown_exogenous, known_exogenous = self.convert_exogenous_dataset(X)
+
+        maximum_forecast_horizon = fh.to_relative(self.cutoff)[-1]
+
+        endogenous_point_predictions = self._forecaster.predict(
+            maximum_forecast_horizon,
+            past_covariates=unknown_exogenous,
+            future_covariates=known_exogenous,
+            num_samples=1,
+        ).pd_dataframe()
+
+        return endogenous_point_predictions
+
+    # todo 0.22.0 - switch legacy_interface default to False
+    # todo 0.23.0 - remove legacy_interface arg
+    def _predict_quantiles(
+        self,
+        fh: Optional[ForecastingHorizon],
+        X: Optional[pandas.DataFrame],
+        alpha: List[float],
+        legacy_interface: Optional[bool] = True,
+    ):
+        """Compute/return prediction quantiles for a forecast.
+
+        private _predict_quantiles containing the core logic,
+            called from predict_quantiles and default _predict_interval
+
+        Parameters
+        ----------
+        fh : guaranteed to be ForecastingHorizon
+            The forecasting horizon with the steps ahead to to predict.
+        X : optional (default=None)
+            guaranteed to be of a type in self.get_tag("X_inner_mtype")
+            Exogeneous time series to predict from.
+        alpha : list of float, optional (default=[0.5])
+            A list of probabilities at which quantile forecasts are computed.
+
+        Returns
+        -------
+        quantiles : pd.DataFrame
+            Column has multi-index: first level is variable name from y in fit,
+                second level being the values of alpha passed to the function.
+            Row index is fh, with additional (upper) levels equal to instance levels,
+                    from y seen in fit, if y_inner_mtype is Panel or Hierarchical.
+            Entries are quantile forecasts, for var in col index,
+                at quantile probability in second col index, for the row index.
+        """
+        unknown_exogenous, known_exogenous = self.convert_exogenous_dataset(X)
+
+        maximum_forecast_horizon = fh.to_relative(self.cutoff)[-1]
+
+        endogenous_quantile_predictions = self._forecaster.predict(
+            maximum_forecast_horizon,
+            past_covariates=unknown_exogenous,
+            future_covariates=known_exogenous,
+            num_samples=self.num_samples,
+        ).quantiles_df(quantiles=alpha)
+
+        return endogenous_quantile_predictions
+
+
+__all__ = ["_DartsAdapter"]
---
+++ sktime/forecasting/darts.py
@@ -0,0 +1,172 @@
+# copyright: sktime developers, BSD-3-Clause License (see LICENSE file)
+"""Interfaces to estimators from darts by Unit8."""
+from typing import Any, List, Optional, Tuple, Union
+
+from sktime.forecasting.base.adapters._darts import _DartsAdapter
+
+__author__ = ["yarnabrina"]
+
+
+class DartsXGBModel(_DartsAdapter):
+    """Darts XGBModel Estimator.
+
+    This is based on implementation of XGBoost Model in darts [1]_ by Unit8.
+
+    Parameters
+    ----------
+    past_covariates : Optional[List[str]], optional
+        column names in ``X`` which are known only for historical data, by default None
+    num_samples : Optional[int], optional
+        Number of times a prediction is sampled from a probabilistic model, by default
+        1000
+    lags
+        Lagged target values used to predict the next time step. If an integer is given
+        the last `lags` past lags are used (from -1 backward). Otherwise a list of
+        integers with lags is required (each lag must be < 0).
+    lags_past_covariates
+        Number of lagged past_covariates values used to predict the next time step. If
+        an integer is given the last `lags_past_covariates` past lags are used
+        (inclusive, starting from lag -1). Otherwise a list of integers
+        with lags < 0 is required.
+    lags_future_covariates
+        Number of lagged future_covariates values used to predict the next time step. If
+        a tuple (past, future) is given the last `past` lags in the past are used
+        (inclusive, starting from lag -1) along with the first `future` future lags
+        (starting from 0 - the prediction time - up to `future - 1` included). Otherwise
+        a list of integers with lags is required.
+    output_chunk_length
+        Number of time steps predicted at once by the internal regression model. Does
+        not have to equal the forecast horizon `n` used in `predict()`. However, setting
+        `output_chunk_length` equal to the forecast horizon may be useful if the
+        covariates don't extend far enough into the future.
+    add_encoders
+        A large number of past and future covariates can be automatically generated with
+        `add_encoders`. This can be done by adding multiple pre-defined index encoders
+        and/or custom user-made functions that will be used as index encoders.
+        Additionally, a transformer such as Darts' :class:`Scaler` can be added to
+        transform the generated covariates. This happens all under one hood and only
+        needs to be specified at model creation. Read
+        :meth:`SequentialEncoder <darts.dataprocessing.encoders.SequentialEncoder>`
+        to find out more about ``add_encoders``. Default: ``None``. An example showing
+        some of ``add_encoders`` features:
+
+        .. highlight:: python
+        .. code-block:: python
+
+            add_encoders={
+                'cyclic': {'future': ['month']},
+                'datetime_attribute': {'future': ['hour', 'dayofweek']},
+                'position': {'past': ['relative'], 'future': ['relative']},
+                'custom': {'past': [lambda idx: (idx.year - 1950) / 50]},
+                'transformer': Scaler()
+            }
+        ..
+    likelihood
+        Can be set to `poisson` or `quantile`. If set, the model will be probabilistic,
+        allowing sampling at prediction time. This will overwrite any `objective`
+        parameter.
+    quantiles
+        Fit the model to these quantiles if the `likelihood` is set to `quantile`.
+    random_state
+        Control the randomness in the fitting procedure and for sampling. Default:
+        ``None``.
+    multi_models
+        If True, a separate model will be trained for each future lag to predict. If
+        False, a single model is trained to predict at step 'output_chunk_length' in the
+        future. Default: True.
+    use_static_covariates
+        Whether the model should use static covariate information in case the input
+        `series` passed to ``fit()`` contain static covariates. If ``True``, and static
+        covariates are available at fitting time, will enforce that all target `series`
+        have the same static covariate dimensionality in ``fit()`` and ``predict()``.
+    **kwargs
+        Additional keyword arguments passed to `xgb.XGBRegressor`.
+
+    References
+    ----------
+    .. [1] https://github.com/unit8co/darts
+
+    Notes
+    -----
+    If unspecified, all columns will be assumed to be known during prediction duration.
+    """
+
+    _tags = {
+        "scitype:y": "both",
+        "ignores-exogeneous-X": False,
+        "capability:pred_int": True,
+    }
+
+    def __init__(
+        self: "DartsXGBModel",
+        past_covariates: Optional[List[str]] = None,
+        num_samples: Optional[int] = 1000,
+        lags: Optional[Union[int, list]] = None,
+        lags_past_covariates: Optional[Union[int, List[int]]] = None,
+        lags_future_covariates: Optional[Union[Tuple[int, int], List[int]]] = None,
+        output_chunk_length: Optional[int] = 1,
+        add_encoders: Optional[dict] = None,
+        likelihood: Optional[str] = None,
+        quantiles: Optional[List[float]] = None,
+        random_state: Optional[int] = None,
+        multi_models: Optional[bool] = True,
+        use_static_covariates: Optional[bool] = True,
+        **kwargs: Any,
+    ) -> None:
+        self.lags = lags
+        self.lags_past_covariates = lags_past_covariates
+        self.lags_future_covariates = lags_future_covariates
+        self.output_chunk_length = output_chunk_length
+        self.add_encoders = add_encoders
+        self.likelihood = likelihood
+        self.quantiles = quantiles
+        self.random_state = random_state
+        self.multi_models = multi_models
+        self.use_static_covariates = use_static_covariates
+        self.kwargs = kwargs
+
+        super().__init__(past_covariates=past_covariates, num_samples=num_samples)
+
+    @property
+    def _forecaster(self: "DartsXGBModel"):
+        """Create Darts model."""
+        from darts.models import XGBModel
+
+        return XGBModel(
+            lags=self.lags,
+            lags_past_covariates=self.lags_past_covariates,
+            lags_future_covariates=self.lags_future_covariates,
+            output_chunk_length=self.output_chunk_length,
+            add_encoders=self.add_encoders,
+            likelihood=self.likelihood,
+            quantiles=self.quantiles,
+            random_state=self.random_state,
+            multi_models=self.multi_models,
+            use_static_covariates=self.use_static_covariates,
+            kwargs=self.kwargs,
+        )
+
+    @classmethod
+    def get_test_params(cls, parameter_set="default"):
+        """Return testing parameter settings for the estimator.
+
+        Parameters
+        ----------
+        parameter_set : str, default="default"
+            Name of the set of test parameters to return, for use in tests. If no
+            special parameters are defined for a value, will return `"default"` set.
+            There are currently no reserved values for forecasters.
+
+        Returns
+        -------
+        params : dict or list of dict, default = {}
+            Parameters to create testing instances of the class
+        """
+        del parameter_set  # to avoid being detected as unused by ``vulture`` etc.
+
+        params = [{"lags": 1}]
+
+        return params
+
+
+__all__ = ["DartsXGBModel"]
