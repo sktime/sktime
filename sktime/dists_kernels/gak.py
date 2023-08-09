5fbe7d2bf59a0bb7d8bfc564cb338272af09a2ae
Franz Király <f.kiraly@ucl.ac.uk>
2023-08-09 10:27:50 +0100

    Update gak.py

--- sktime/dists_kernels/gak.py
+++ sktime/dists_kernels/gak.py
@@ -20,17 +20,6 @@ class GAKernel(_TslearnPwTrafoAdapter, BasePairwiseTransformerPanel):
     normalized : bool, optional, default=True
         if True, computes the normalized GAK (``cdist_gak``)
         if False, computes unnormalized GAK (``gak``)
-    n_jobs : int or None, optional, default=None
-        The number of jobs to run in parallel.
-        ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
-        ``-1`` means using all processors. See scikit-learns'
-        `Glossary <https://scikit-learn.org/stable/glossary.html#term-n-jobs>`__
-        for more details.
-    verbose : int, optional, default=0
-        The verbosity level: if non zero, progress messages are printed.
-        Above 50, the output is sent to stdout.
-        The frequency of the messages increases with the verbosity level.
-        If it more than 10, all iterations are reported.
 
     References
     ----------
@@ -39,21 +28,13 @@ class GAKernel(_TslearnPwTrafoAdapter, BasePairwiseTransformerPanel):
 
     _tags = {"symmetric": True, "pwtrafo_type": "kernel"}
 
-    def __init__(
-        self,
-        sigma=1.0,
-        normalized=True,
-        n_jobs=None,
-        verbose=0,
-    ):
+    def __init__(self, sigma=1.0, normalized=True):
         self.sigma = sigma
         self.normalized = normalized
-        self.n_jobs = n_jobs
-        self.verbose = verbose
 
         super().__init__()
 
-    _inner_params = ["sigma", "n_jobs", "verbose"]
+    _inner_params = ["sigma"]
 
     def _get_tslearn_pwtrafo(self):
         """Adapter method to get tslearn pwtrafo."""
