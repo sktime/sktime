2a24cb9af29b80b0238908dee8124df11a0489ed
Franz Király <f.kiraly@ucl.ac.uk>
2023-08-10 13:47:24 +0100

    add back n_jobs and verbose param

--- sktime/dists_kernels/gak.py
+++ sktime/dists_kernels/gak.py
@@ -17,6 +17,17 @@ class GAKernel(_TslearnPwTrafoAdapter, BasePairwiseTransformerPanel):
     ----------
     sigma : float, default 1.
         Bandwidth of the internal gaussian kernel used for GAK
+    n_jobs : int or None, optional, default=None
+        The number of jobs to run in parallel.
+        ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
+        ``-1`` means using all processors. See scikit-learns'
+        `Glossary <https://scikit-learn.org/stable/glossary.html#term-n-jobs>`__
+        for more details.
+    verbose : int, optional, default=0
+        The verbosity level: if non zero, progress messages are printed.
+        Above 50, the output is sent to stdout.
+        The frequency of the messages increases with the verbosity level.
+        If it more than 10, all iterations are reported.
 
     References
     ----------
@@ -25,12 +36,17 @@ class GAKernel(_TslearnPwTrafoAdapter, BasePairwiseTransformerPanel):
 
     _tags = {"symmetric": True, "pwtrafo_type": "kernel"}
 
-    def __init__(self, sigma=1.0):
+    def __init__(
+        self,
+        sigma=1.0,
+        n_jobs=None,
+        verbose=0,
+    ):
         self.sigma = sigma
+        self.n_jobs = n_jobs
+        self.verbose = verbose
 
         super().__init__()
-
-    _inner_params = ["sigma"]
 
     def _get_tslearn_pwtrafo(self):
         """Adapter method to get tslearn pwtrafo."""
