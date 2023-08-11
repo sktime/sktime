f6979a2f1b695b75c388e300e75449d0b960f5bf
loo <luca.miniati@gmail.com>
2023-08-10 18:42:50 -0700

    update LTSF

--- examples/ltsf/ltsf_example.py
+++ examples/ltsf/ltsf_example.py
@@ -8,25 +8,32 @@ from sktime.forecasting.ltsf import LTSFLinearForecaster
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-data = pd.read_csv("ETT/ETTh1.csv").drop("date", axis=1)
-test_size = 0.3
-train = data.iloc[int(len(data) * test_size) :]
-test = data.iloc[: int(len(data) * test_size)]
+train = pd.read_csv("ETT/ETTh1.csv").drop("date", axis=1)
+# test_size = 0.3
+# train = data.iloc[int(len(data) * test_size) :]
+# test = data.iloc[: int(len(data) * test_size)]
 
-seq_len = 96
+seq_len = 336
 pred_len = 96
 
 network = LTSFLinearForecaster(
-    seq_len=96,
-    pred_len=1,
-    lr=0.001,
+    seq_len=seq_len,
+    pred_len=pred_len,
+    lr=0.005,
     batch_size=32,
     in_channels=7,
-    num_epochs=200,
+    num_epochs=600,
+    individual=False,
 )
-network.fit(train)
-y_pred = network.predict(X=test)
-y_true = network.get_y_true(test)
 
-mse = mean_squared_error(y_true, y_pred)
-mae = mean_absolute_error(y_true, y_pred)
+try:
+    network.fit(train)
+    y_pred = network.predict(X=train)
+    y_true = network.get_y_true(train)
+
+    mse = mean_squared_error(y_true, y_pred)
+    mae = mean_absolute_error(y_true, y_pred)
+except Exception:
+    network.save("model.pth")
+
+network.save("model.pth")
