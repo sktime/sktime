1ace8f07bf164349f0f8f9cb40135de9e37c2646
Alex Gregory <alex.gregory1@outlook.com>
2023-08-12 11:06:30 +0100

    Change author

--- sktime/proba/t.py
+++ sktime/proba/t.py
@@ -1,7 +1,7 @@
 # copyright: sktime developers, BSD-3-Clause License (see LICENSE file)
 """Student's t-distribution."""
 
-__author__ = ["fkiraly"]
+__author__ = ["Alex-JG3"]
 
 import numpy as np
 import pandas as pd
