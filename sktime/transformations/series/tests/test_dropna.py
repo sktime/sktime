dfd397596ba734b20c7a32453c8322b94a996414
Franz Király <f.kiraly@ucl.ac.uk>
2023-08-24 15:21:29 +0100

    remove superfluous line

--- sktime/transformations/series/tests/test_dropna.py
+++ sktime/transformations/series/tests/test_dropna.py
@@ -61,7 +61,6 @@ def X_many_na():
     X_many_na.loc[:, "POP"] = np.nan
     X_many_na.loc["1960", :] = np.nan
     X_many_na.loc["1962", :] = np.nan
-    X_many_na.dropna(axis=1)
     return X_many_na
 
 
