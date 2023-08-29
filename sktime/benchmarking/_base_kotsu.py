60e72b6e9551052b9732af6d51b4d558e5f7e724
hazrulakmal <hazrulakmal121@gmail.com>
2023-08-29 09:07:27 +0100

    typo in TypeError id format

--- sktime/benchmarking/_base_kotsu.py
+++ sktime/benchmarking/_base_kotsu.py
@@ -11,7 +11,7 @@ def _check_id_format(id_format: str, id: str) -> None:
     """Check if given input ID follows regex specified in id_format."""
     if id_format is not None:
         if not isinstance(id_format, str):
-            raise TypeError(f"entity_id must be str but receive {type(id_format)}")
+            raise TypeError(f"id_format must be str but receive {type(id_format)}")
         entity_id_re = re.compile(id_format)
         match = entity_id_re.search(id)
         if not match:
