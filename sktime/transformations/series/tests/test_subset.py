54e1adea14ce53a263b0c18907efbecfec46dc83
benHeid <benHeid@users.noreply.github.com>
2023-09-10 11:20:55 +0200

    Add missing tag in ColumnSelect.

--- sktime/transformations/series/tests/test_subset.py
+++ sktime/transformations/series/tests/test_subset.py
@@ -5,6 +5,7 @@ __author__ = ["fkiraly"]
 
 import pandas as pd
 import pytest
+from forecasting.naive import NaiveForecaster
 
 from sktime.datasets import load_airline, load_longley
 from sktime.forecasting.naive import NaiveForecaster
