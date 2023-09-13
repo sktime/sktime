0d82ed33389a28f4d3164f4e2b39f4cb0557b8cb
hazrulakmal <hazrulakmal121@gmail.com>
2023-09-13 09:24:15 +0000

    mlflow

--- sktime/benchmarking/experimental/_base.py
+++ sktime/benchmarking/experimental/_base.py
@@ -3,7 +3,40 @@
 from abc import ABC, abstractmethod
 from typing import Any
 
-from sktime.benchmarking.experimental.logger import BaseLogger
+
+class BaseLogger(ABC):
+    """Abstact class that provides blueprint for logger classes."""
+
+    @abstractmethod
+    def log_metrics(self, *args: Any, **kwargs: Any) -> None:
+        """Log scores."""
+        pass
+
+    @abstractmethod
+    def log_hyperparams(self, *args: Any, **kwargs: Any) -> None:
+        """Log estimator parameters."""
+        pass
+
+    @abstractmethod
+    def log_estimator(self, *args: Any, **kwargs: Any) -> None:
+        """Save estimator state."""
+        pass
+
+    @abstractmethod
+    def log_graph(self, *args: Any, **kwargs: Any) -> None:
+        """For logging visualisation."""
+        pass
+
+    @abstractmethod
+    def log_cvsplit(self, *args: Any, **kwargs: Any) -> None:
+        """For logging cvsplit metadata.
+
+        Log cv split metadata such as cuttoff time, len, number of splits
+        I think this is a better aproach than saving them as multiple small
+        dataset chuncks. Managing datasets is delegated to dataset class so that
+        we have more control?
+        """
+        pass
 
 
 class BaseEvaluator(ABC):
---
+++ sktime/benchmarking/experimental/loggers/mlflow.py
@@ -0,0 +1,83 @@
+"""Uniform interface of different experiment tracking packages."""
+
+import os
+from typing import Any, Dict, Optional
+
+from sktime.base._base import BaseEstimator
+from sktime.benchmarking.experimental._base import BaseLogger
+from sktime.utils.validation._dependencies import _check_soft_dependencies
+
+# mlflavour no seperation of dependencies.
+if _check_soft_dependencies("mlflow", severity="warning"):
+    import mlflow
+    from mlflow.tracking import MlflowCient
+
+    from sktime.utils import mlflow_sktime
+
+LOCAL_FILE_URI_PREFIX = "file:"
+DEFAULT_ENV = os.getenv("MLFLOW_TRACKING_URI")
+
+
+class MLFlowLogger(BaseLogger):
+    """Manage the interface for tracking using Mlflow."""
+
+    def __init__(
+        self,
+        experiment_name: str = "benchmark_logs",
+        run_name: Optional[str] = None,
+        tags: Optional[Dict[str, Any]] = None,
+        save_dir: Optional[str] = "./benchmarkruns",
+        tracking_uri: Optional[str] = DEFAULT_ENV,
+        artifact_location: Optional[str] = None,
+        run_id: Optional[str] = None,
+    ):
+        _check_soft_dependencies("mlflow", severity="error")
+        if tracking_uri is None:
+            os.getenv("MLFLOW_TRACKING_URI")
+            tracking_uri = f"{LOCAL_FILE_URI_PREFIX}{save_dir}"
+
+        self._experiment_name = experiment_name
+        self._run_name = run_name
+        self.tags = tags
+        self._save_dir = save_dir
+        self._artifact_location = artifact_location
+        self._run_id = run_id
+        self._client = MlflowCient(tracking_uri)
+
+    def log_metric(self, metric_name: str, metric_values: float) -> None:
+        """Log a metric under the current run."""
+        mlflow.log_metric(metric_name, metric_values)
+
+    def log_metrics(self, metrics: Dict[str, float]) -> None:
+        """Log multiple metrics for the current run."""
+        mlflow.log_metrics(metrics)
+
+    def log_hyperparams(self, params: Dict[str, Any]) -> None:
+        """Log a batch of estimator params for the current run."""
+        mlflow.log_params(params)
+
+    def log_estimator(self, estimator: BaseEstimator) -> None:
+        """Save estimator state as an artifact."""
+        mlflow_sktime.log_model(
+            sktime_model=estimator,
+            artifact_path=self._artifact_location,
+            registered_model_name=estimator.__class__.__name__,
+        )
+
+    def log_graph(self, graph, save_kwargs: Dict["str", Any]) -> None:
+        """Log a figure as an artifact.
+
+        The following figure objects are supported:
+        - matplotlib.figure.Figure_
+        - plotly.graph_objects.Figure_
+        """
+        mlflow.log_figure(graph, self._artifact_location, save_kwargs=save_kwargs)
+
+    def get_estimator(self, estimator_id) -> None:
+        """Fetch estimator from artifact."""
+        path = f"{self._artifact_location}/{estimator_id}"
+        mlflow_sktime.load_model(path)
+
+    def log_cvsplit(self, cvsplit_info: Dict[str, Any]) -> None:
+        """Log cvsplit metadata as an artifact."""
+        mlflow.log_dict(cvsplit_info, self._artifact_location)
