390ab9297bda520acba11aebaa90798989d2ba17
BensHamza <hamza-benslimane@outlook.com>
2023-09-22 22:08:28 +0200

    Added `todo` for the 0.25.0 release

--- sktime/forecasting/model_selection/_split.py
+++ sktime/forecasting/model_selection/_split.py
@@ -2,6 +2,13 @@
 # copyright: sktime developers, BSD-3-Clause License (see LICENSE file)
 """Implement dataset splitting for model evaluation and selection."""
 
+# This module is deprecated. Below the deprecation cycle :
+# - Until the 0.25.0 release, imports are allowed from this location but raise a
+# DeprecationWarning.
+# - In the 0.25.0 release, this file (sktime/forecasting/model_selection/_split.py)
+# will be removed.
+# todo 0.25.0 : Please ensure this file is deleted.
+
 __all__ = [
     "ExpandingGreedySplitter",
     "ExpandingWindowSplitter",
