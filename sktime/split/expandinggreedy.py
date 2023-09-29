80034f6bc87de9ed47230c79a8fcef3796f472fd
Franz Király <f.kiraly@ucl.ac.uk>
2023-09-29 18:45:47 +0200

    Update expandinggreedy.py

--- sktime/split/expandinggreedy.py
+++ sktime/split/expandinggreedy.py
@@ -7,6 +7,7 @@ __author__ = ["davidgilbertson"]
 __all__ = [
     "ExpandingGreedySplitter",
 ]
+__author__ = ["davidgilbertson"]
 
 import numpy as np
 import pandas as pd
@@ -18,21 +19,25 @@ from sktime.split.base._common import SPLIT_GENERATOR_TYPE
 class ExpandingGreedySplitter(BaseSplitter):
     """Splitter that successively cuts test folds off the end of the series.
 
-    Takes an integer `test_size` that defines the number of steps included in the
+    Takes an integer ``test_size`` that defines the number of steps included in the
     test set of each fold. The train set of each fold will contain all data before
-    the test set. If the data contains multiple instances, `test_size` is
+    the test set. If the data contains multiple instances, ``test_size`` is
     _per instance_.
 
-    If no `step_length` is defined, the test sets (one for each fold) will be
-    adjacent, taken from the end of the dataset.
+    If no ``step_length`` is defined, the test sets (one for each fold) will be
+    adjacent and disjoint, taken from the end of the dataset.
 
-    For example, with `test_size=7` and `folds=5`, the test sets in total will cover
+    For example, with ``test_size=7`` and ``folds=5``, the test sets in total will cover
     the last 35 steps of the data with no overlap.
 
     Parameters
     ----------
-    test_size : int
-        The number of steps included in the test set of each fold.
+    test_size : int or float
+        If int: the number of steps included in the test set of each fold.
+            Formally, steps are consecutive ``iloc`` indices.
+        If float: the proportion of steps included in the test set of each fold,
+            as a proportion of the total number of index values.
+            Cave: not the ``loc`` proportion between start and end.
     folds : int, default = 5
         The number of folds.
     step_length : int, optional
@@ -41,7 +46,7 @@ class ExpandingGreedySplitter(BaseSplitter):
     Examples
     --------
     >>> import numpy as np
-    >>> from sktime.split import ExpandingGreedySplitter
+    >>> from sktime.forecasting.model_selection import ExpandingGreedySplitter
 
     >>> ts = np.arange(10)
     >>> splitter = ExpandingGreedySplitter(test_size=3, folds=2)
@@ -61,18 +66,28 @@ class ExpandingGreedySplitter(BaseSplitter):
         self.step_length = step_length
         self.fh = np.arange(test_size) + 1
 
+        # no algorithm implemented that is faster for float than naive iteration
+        if isinstance(test_size, float):
+            self.set_tags(**{"split_hierarchical", False})
+
     def _split(self, y: pd.Index) -> SPLIT_GENERATOR_TYPE:
+
+        test_size = self.test_size
+
+        if isinstance(test_size, float):
+            _test_size = round(len(y) * test_size)
+
         if isinstance(y, pd.MultiIndex):
             groups = pd.Series(index=y).groupby(y.names[:-1])
             reverse_idx = groups.transform("size") - groups.cumcount() - 1
         else:
             reverse_idx = np.arange(len(y))[::-1]
 
-        step_length = self.step_length or self.test_size
+        step_length = self.step_length or _test_size
 
         for i in reversed(range(self.folds)):
             tst_end = i * step_length
-            trn_end = tst_end + self.test_size
+            trn_end = tst_end + _test_size
             trn_indices = np.flatnonzero(reverse_idx >= trn_end)
             tst_indices = np.flatnonzero(
                 (reverse_idx < trn_end) & (reverse_idx >= tst_end)
@@ -99,4 +114,5 @@ class ExpandingGreedySplitter(BaseSplitter):
         """
         params1 = {"test_size": 1}
         params2 = {"test_size": 3, "folds": 2, "step_length": 2}
-        return [params1, params2]
+        params3 = {"test_size": 0.2, "folds": 2}
+        return [params1, params2, params3]
