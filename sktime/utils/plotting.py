99386c07c934380ebff7d2e29d3b155a9290d495
Franz Király <f.kiraly@ucl.ac.uk>
2023-10-27 10:09:01 +0200

    Update plotting.py

--- sktime/utils/plotting.py
+++ sktime/utils/plotting.py
@@ -176,20 +176,27 @@ def plot_series(
         ax.legend()
     if pred_interval is not None:
         check_interval_df(pred_interval, series[-1].index)
-        ax = plot_interval(ax, pred_interval)
+        ax = plot_interval(ax, pred_interval, index)
     if _ax_kwarg_is_none:
         return fig, ax
     else:
         return ax
 
 
-def plot_interval(ax, interval_df):
+def plot_interval(ax, interval_df, ix=None):
     cov = interval_df.columns.levels[1][0]
     var_name = interval_df.columns.levels[0][0]
+    print(ix)
+    x_ix = np.argwhere(ix.isin(interval_df.index)).ravel()
+    x_ix = np.array(x_ix)
+
+    print(x_ix)
+
     ax.fill_between(
-        ax.get_lines()[-1].get_xdata(),
-        interval_df[var_name][cov]["lower"].astype("float64"),
-        interval_df[var_name][cov]["upper"].astype("float64"),
+        # ax.get_lines()[-1].get_xdata()[-3:],
+        x_ix,
+        interval_df[var_name][cov]["lower"].astype("float64").to_numpy(),
+        interval_df[var_name][cov]["upper"].astype("float64").to_numpy(),
         alpha=0.2,
         color=ax.get_lines()[-1].get_c(),
         label=f"{int(cov * 100)}% prediction interval",
