28eb5699429fd3710ca1152549bf259ace996ea8
Franz Király <f.kiraly@ucl.ac.uk>
2023-11-12 09:56:44 +0100

    error msg

--- sktime/utils/parallel.py
+++ sktime/utils/parallel.py
@@ -106,8 +106,17 @@ def _parallelize_joblib(fun, iter, meta, backend, backend_params):
         # raise a ValueError
         if backend == "joblib_custom":
             raise ValueError(
-                '"joblib_custom" was selected as backend, but no backend string was '
-                "passed in the backend parameters dict. Please specify a backend."
+                '"joblib_custom" was selected as first layer parallelization backend, '
+                "but no backend string was "
+                '"passed in the backend parameters dict, e.g., "spark". '
+                "Please specify a backend to joblib as a key-value pair "
+                "in the backend_params arg or the backend:parallel:params config "
+                'when using "joblib_custom". '
+                'For clarity, "joblib_custom" should only be used for two-layer '
+                "backend dispatch, where the first layer is joblib, "
+                "and the second layer is a custom backend of joblib, e.g., spark."
+                "For first-party joblib backends, please use the backend string "
+                'of sktime directly, e.g., by specifying "multiprocessing".'
             )
         # in all other cases, we ensure the backend parameter is one of
         # "loky", "multiprocessing" or "threading", as passed via backend
