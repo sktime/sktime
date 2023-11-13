ff5b5ef6fd95854dda29a09e850bd6c9e22e5ee8
Franz Király <f.kiraly@ucl.ac.uk>
2023-11-13 21:44:40 +0100

    Update test_sklearn_df_adapt.py

--- sktime/utils/sklearn/tests/test_sklearn_df_adapt.py
+++ sktime/utils/sklearn/tests/test_sklearn_df_adapt.py
@@ -10,7 +10,7 @@ from sktime.utils.sklearn._adapt_df import prep_skl_df
 
 
 @pytest.mark.parametrize("copy_df", [True, False])
-def test_prep_skl_df_positive(copy_df):
+def test_prep_skl_df_coercion(copy_df):
     """Test that prep_skl_df behaves correctly on the coercion case."""
     mixed_example = pd.DataFrame({0: [1, 2, 3], "b": [1, 2, 3]})
 
@@ -23,7 +23,7 @@ def test_prep_skl_df_positive(copy_df):
 
 
 @pytest.mark.parametrize("copy_df", [True, False])
-def test_prep_skl_df_positive(copy_df):
+def test_prep_skl_df_non_coercion(copy_df):
     """Test that prep_skl_df behaves correctly on the non-coercion case."""
     mixed_example = pd.DataFrame({"a": [1, 2, 3], "b": [1, 2, 3]})
 
