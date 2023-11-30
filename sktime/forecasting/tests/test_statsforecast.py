12b1d352346fc9aa3445f2096c7109414f942916
Ben Sully <ben.sully@grafana.com>
2023-11-30 18:49:14 +0000

    Fix statsforecast assertion; skip if statsforecast not installed

--- sktime/forecasting/tests/test_statsforecast.py
+++ sktime/forecasting/tests/test_statsforecast.py
@@ -1,12 +1,23 @@
+"""Tests for StatsForecast.
+
+# copyright: sktime developers, BSD-3-Clause License (see LICENSE file)
+"""
+
 from unittest.mock import patch
 
 import numpy as np
 import pandas as pd
+import pytest
 
 from sktime.forecasting.base import ForecastingHorizon
 from sktime.forecasting.statsforecast import StatsForecastMSTL
+from sktime.tests.test_switch import run_test_for_class
 
 
+@pytest.mark.skipif(
+    not run_test_for_class(StatsForecastMSTL),
+    reason="run test only if softdeps are present and incrementally (if requested)",
+)
 @patch("statsforecast.models.AutoETS", autospec=True)
 def test_statsforecast_mstl(mock_autoets):
     """
@@ -29,4 +40,4 @@ def test_statsforecast_mstl(mock_autoets):
     fh_index = pd.PeriodIndex(pd.date_range("1961-01", periods=36, freq="M"))
     fh = ForecastingHorizon(fh_index, is_relative=False)
     model.predict_interval(fh, coverage=0.95)
-    predict.assert_called_with(36, X=None, level=[0.95])
+    predict.assert_called_with(36, X=None, level=[95.0])
