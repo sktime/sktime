3805ade26aa96413b65a58094de11d793c355a61
Franz Király <f.kiraly@ucl.ac.uk>
2023-12-09 19:12:12 +0100

    split exports

--- sktime/forecasting/model_selection/__init__.py
+++ sktime/forecasting/model_selection/__init__.py
@@ -4,29 +4,11 @@
 
 __author__ = ["mloning", "kkoralturk"]
 __all__ = [
-    "CutoffSplitter",
-    "SameLocSplitter",
-    "SingleWindowSplitter",
-    "SlidingWindowSplitter",
-    "temporal_train_test_split",
-    "ExpandingGreedySplitter",
-    "ExpandingWindowSplitter",
-    "TestPlusTrainSplitter",
     "ForecastingGridSearchCV",
     "ForecastingRandomizedSearchCV",
     "ForecastingSkoptSearchCV",
 ]
 
-from sktime.forecasting.model_selection._split import (
-    CutoffSplitter,
-    ExpandingGreedySplitter,
-    ExpandingWindowSplitter,
-    SameLocSplitter,
-    SingleWindowSplitter,
-    SlidingWindowSplitter,
-    TestPlusTrainSplitter,
-    temporal_train_test_split,
-)
 from sktime.forecasting.model_selection._tune import (
     ForecastingGridSearchCV,
     ForecastingRandomizedSearchCV,
