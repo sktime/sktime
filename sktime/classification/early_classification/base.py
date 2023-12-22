4cc2b5208702a55b97bd12e503b0a9823e1aaaf6
Franz Király <f.kiraly@ucl.ac.uk>
2023-12-22 19:35:00 +0100

    add missing multioutput tag in early classif

--- sktime/classification/early_classification/base.py
+++ sktime/classification/early_classification/base.py
@@ -56,6 +56,7 @@ class BaseEarlyClassifier(BaseEstimator, ABC):
         "object_type": "early_classifier",  # type of object
         "X_inner_mtype": "numpy3D",  # which type do _fit/_predict, support for X?
         #    it should be either "numpy3D" or "nested_univ" (nested pd.DataFrame)
+        "capability:multioutput": False,  # whether classifier supports multioutput
         "capability:multivariate": False,
         "capability:unequal_length": False,
         "capability:missing_values": False,
