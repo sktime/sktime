c479d779d9970656fa1723c0cede509606ac4cd8
Franz Király <f.kiraly@ucl.ac.uk>
2023-12-26 15:02:35 +0100

    add warning

--- sktime/forecasting/model_selection/__init__.py
+++ sktime/forecasting/model_selection/__init__.py
@@ -14,4 +14,25 @@ from sktime.forecasting.model_selection._tune import (
     ForecastingRandomizedSearchCV,
     ForecastingSkoptSearchCV,
 )
-from sktime.split import temporal_train_test_split
+
+# todo 0.26.0 - check whether we should remove, otherwise bump
+def temporal_train_test_split(
+        y, X=None, test_size=None, train_size=None, fh=None, anchor="start"
+):
+    """Split time series data into temporal train and test sets."""
+    from warnings import warn
+
+    from sktime.split import temporal_train_test_split as _tts
+
+    warn(
+        "WARNING - the old location of temporal_train_test_split in "
+        "sktime.forecasting.model_selection is deprecated and is scheduled "
+        "imminent removal in a MINOR version. "
+        "Please update any import statements to "
+        "from sktime.split import temporal_train_test_split.",
+        DeprecationWarning,
+    )
+
+    return _tts(
+        y=y, X=X, test_size=test_size, train_size=train_size, fh=fh, anchor=anchor
+    )
