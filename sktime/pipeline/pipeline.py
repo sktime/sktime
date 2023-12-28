3a92b30fd49c7d69f9713a1e33cd053b19dfa465
benheid <benheid@users.noreply.github.com>
2023-12-28 14:43:58 +0100

    Fix tests

--- sktime/pipeline/pipeline.py
+++ sktime/pipeline/pipeline.py
@@ -204,9 +204,7 @@ class Pipeline(BaseEstimator):
         self.counter += 1
         # Check if not already an skobject cloned from the provided
         # skobject is part of the pipeline
-        if (id(skobject) not in self.id_to_obj) or self.id_to_obj[
-            id(skobject)
-        ]() is None:
+        if (id(skobject) not in self.id_to_obj):
             # In this case store that skobject to id_to_obj to prevent that
             # the garbage collector reassigns the id.
             self.id_to_obj[id(skobject)] = skobject
