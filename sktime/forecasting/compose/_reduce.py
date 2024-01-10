1a45b79ae74e48514a976eeb2d21f90c41d71560
Franz Király <f.kiraly@ucl.ac.uk>
2024-01-11 00:54:19 +0100

    Update _reduce.py

--- sktime/forecasting/compose/_reduce.py
+++ sktime/forecasting/compose/_reduce.py
@@ -581,12 +581,11 @@ class _DirectReducer(_Reducer):
 
             if self.transformers_ is not None:
                 fh_rel = fh.to_relative(self.cutoff)
-                yt_cut = _cut_df(yt, n_timepoints - fh_rel[i] + 1)
                 Xt_cut = _cut_df(Xt, n_timepoints - fh_rel[i] + 1, type="head")
+                yt_cut = _cut_df(yt, n_timepoints - fh_rel[i] + 1)
             elif self.windows_identical is True or (fh_rel[i] - 1) == 0:
-                print(yt.shape)
-                yt_cut = yt[:, i]
                 Xt_cut = Xt
+                yt_cut = yt[:, i]
             else:
                 Xt_cut = Xt[: -(fh_rel[i] - 1)]
                 yt_cut = yt[: -(fh_rel[i] - 1), i]
