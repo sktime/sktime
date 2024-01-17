272c81a7a3e778935906f4a8dfce721e61240481
Franz Király <f.kiraly@ucl.ac.uk>
2024-01-17 19:51:25 +0100

    test

---
+++ sktime/forecasting/compose/tests/test_ignorex.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3 -u
+# copyright: sktime developers, BSD-3-Clause License (see LICENSE file)
+"""Tests for Bagging Forecasters."""
+
+from unittest.mock import MagicMock
+
+import pandas as pd
+import pytest
+
+from sktime.datasets import load_airline
+from sktime.forecasting.compose import IgnoreX
+
+
+@pytest.mark.skipif(
+    not run_test_for_class(IgnoreX),
+    reason="run test only if softdeps are present and incrementally (if requested)",
+)
+@pytest.mark.parametrize("ignore_x", [True, False, None])
+def test_ignoreX(ignore_x):
+    """Test that indeed X=None is passed iff the args of IgnoreX claim to do so."""
+    y = load_airline()
+
+    fcst = MagicMock()
+
+    if ignore_x is None:
+        igx = IgnoreX(forecaster=fcst)
+    else:
+        igx = IgnoreX(forecaster=fcst, ignore_x=ignore_x)
+
+    igx.fit(y, fh=[1, 2, 3])
+
+    mock_fitted = igx.forecaster_.fit
+    call_args_list = mock_fitted.call_args_list
+    all_calls_X_none = all(call_args[1]['X'] is None for call_args in call_args_list)
+    any_calls_X_none = any(call_args[1]['X'] is None for call_args in call_args_list)
+
+    if ignore_x in [True, None]:
+        assert all_calls_X_none
+    else:
+        assert not any_calls_X_none
