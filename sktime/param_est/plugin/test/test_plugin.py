12e67b2c467f9941b32988d57267175d51dbf26d
Franz Király <f.kiraly@ucl.ac.uk>
2024-01-17 12:05:59 +0100

    Update test_plugin.py

--- sktime/param_est/plugin/test/test_plugin.py
+++ sktime/param_est/plugin/test/test_plugin.py
@@ -1,11 +1,14 @@
 # copyright: sktime developers, BSD-3-Clause License (see LICENSE file)
 """Tests for plugin composites for parameter estimators."""
 
+import pandas as pd
+import pytest
+
 from sktime.param_est.seasonality import SeasonalityACF
 
 
 @pytest.mark.skipif(
-    not _check_estimator_deps(ARIMA, severity="none"),
+    not _check_estimator_deps(SeasonalityACF, severity="none"),
     reason="skip test if required soft dependencies not available",
 )
 def test_plugin_fcst():
@@ -31,6 +34,7 @@ def test_plugin_fcst():
     # fit sp_auto to data, predict, and inspect the tuned sp parameter
     sp_auto.fit(y, fh=[1, 2, 3])
     y_pred = sp_auto.predict()
+    assert isinstance(y_pred, pd.Series)
 
     assert sp_auto.forecaster_.get_params()["sp"] == 12
 
@@ -54,7 +58,7 @@ def test_plugin_fcst():
 
 
 @pytest.mark.skipif(
-    not _check_estimator_deps(ARIMA, severity="none"),
+    not _check_estimator_deps(SeasonalityACF, severity="none"),
     reason="skip test if required soft dependencies not available",
 )
 def test_plugin_trafo():
@@ -79,6 +83,7 @@ def test_plugin_trafo():
     sp_auto.fit(X)
 
     Xt = sp_auto.transform(X)
+    assert isinstance(Xt, pd.Series)
     assert sp_auto.transfomer_.get_params()["sp"] == 12
 
     # shorthand ways to specify sp_auto, via dunder, does the same
@@ -92,7 +97,7 @@ def test_plugin_trafo():
     assert sp_auto3 == sp_auto
 
     # plugin with dict
-    sp_plugin = PluginParamsForecaster(
+    sp_plugin = PluginParamsTransformer(
         FixedParams({"foo": 42}), Deseasonalizer(), params={"foo": "sp"}
     )
 
