40c1d74056b2c4eccd887171077fac9c215ee234
Franz Király <f.kiraly@ucl.ac.uk>
2024-01-20 12:04:50 +0100

    Update test_fh.py

--- sktime/forecasting/base/tests/test_fh.py
+++ sktime/forecasting/base/tests/test_fh.py
@@ -324,6 +324,15 @@ NON_FIXED_FREQUENCY_STRINGS = ["W-WED", "W-SUN", "W-SAT", "M"]
 FREQUENCY_STRINGS = [*FIXED_FREQUENCY_STRINGS, *NON_FIXED_FREQUENCY_STRINGS]
 
 
+def _get_expected_freqstr(freqstr):
+    # special case for 10min, T is being deprecated and replaced by min
+    if freqstr == "10min":
+        fh_freqstr_expected = "10T"
+    else:
+        fh_freqstr_expected = freqstr
+    return fh_freqstr_expected
+
+
 @pytest.mark.parametrize("freqstr", FREQUENCY_STRINGS)
 def test_to_absolute_freq(freqstr):
     """Test conversion when anchorings included in frequency."""
@@ -332,7 +341,7 @@ def test_to_absolute_freq(freqstr):
     fh = ForecastingHorizon([1, 2, 3])
 
     abs_fh = fh.to_absolute(cutoff)
-    assert abs_fh._values.freqstr == freqstr
+    assert abs_fh._values.freqstr == _get_expected_freqstr(freqstr)
 
 
 @pytest.mark.parametrize("freqstr", FREQUENCY_STRINGS)
@@ -347,10 +356,7 @@ def test_absolute_to_absolute_with_integer_horizon(freqstr):
     converted_abs_fh = abs_fh.to_relative(cutoff).to_absolute(cutoff)
     assert_array_equal(abs_fh, converted_abs_fh)
     fh_freqstr = converted_abs_fh._values.freqstr
-    # special case for 10min, T is being deprecated and replaced by min
-    if "T" in freqstr:
-        fh_freqstr = fh_freqstr.replace("T", "min")
-    assert fh_freqstr == freqstr
+    assert fh_freqstr == _get_expected_freqstr(freqstr)
 
 
 @pytest.mark.parametrize("freqstr", FIXED_FREQUENCY_STRINGS)
@@ -367,7 +373,8 @@ def test_absolute_to_absolute_with_timedelta_horizon(freqstr):
 
     converted_abs_fh = abs_fh.to_relative(cutoff).to_absolute(cutoff)
     assert_array_equal(abs_fh, converted_abs_fh)
-    assert converted_abs_fh._values.freqstr == freqstr
+
+    assert converted_abs_fh._values.freqstr == _get_expected_freqstr(freqstr)
 
 
 @pytest.mark.parametrize("freqstr", FREQUENCY_STRINGS)
