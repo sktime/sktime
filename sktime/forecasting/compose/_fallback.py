8cd3bfe6a5b22acc1faaca131d30338680b4cde6
Max Frohlich <max.frohlich@gmail.com>
2024-01-21 08:48:10 -0800

    [ENH] Introduction of FallbackForecaster #5779

--- sktime/forecasting/compose/_fallback.py
+++ sktime/forecasting/compose/_fallback.py
@@ -3,14 +3,16 @@
 """
 Implements the FallbackForecaster.
 
-The FallbackForecaster works with a list of forecasters and tries to fit them in order. If the active forecaster fails during prediction,
-it proceeds to the next. This ensures a robust forecasting mechanism by providing fallback options.
+The FallbackForecaster works with a list of forecasters and tries to fit them in order.
+If the active forecaster fails during prediction, it proceeds to the next. This ensures
+a robust forecasting mechanism by providing fallback options.
 """
 
 __author__ = ["ninedigits"]
 __all__ = ["FallbackForecaster"]
 
 import warnings
+
 from sktime.base import _HeterogenousMetaEstimator
 from sktime.forecasting.base import BaseForecaster
 
@@ -18,9 +20,10 @@ from sktime.forecasting.base import BaseForecaster
 class FallbackForecaster(_HeterogenousMetaEstimator, BaseForecaster):
     """Forecaster that sequentially tries a list of forecasting models.
 
-    Attempts to fit the provided forecasters in the order they are given. If a forecaster fails during fitting or
-    prediction, it proceeds to the next one. This class is useful in scenarios where the reliability of individual
-    forecasting models may be in question, and a fallback mechanism is desired.
+    Attempts to fit the provided forecasters in the order they are given. If a
+    forecaster fails during fitting or prediction, it proceeds to the next one. This
+    class is useful in scenarios where the reliability of individual forecasting models
+    may be in question, and a fallback mechanism is desired.
 
     Parameters
     ----------
@@ -28,6 +31,7 @@ class FallbackForecaster(_HeterogenousMetaEstimator, BaseForecaster):
         Forecasters to be tried sequentially.
     warn : bool, default=False
         If True, raises warnings when a forecaster fails to fit or predict.
+
     Examples
     --------
     >>> from sktime.forecasting.naive import NaiveForecaster
@@ -52,17 +56,40 @@ class FallbackForecaster(_HeterogenousMetaEstimator, BaseForecaster):
     >>> forecaster.fit(y=y, fh=[1, 2, 3])
     FallbackForecaster(...)
     >>> y_pred = forecaster.predict()
-
     """
 
     def __init__(self, forecasters, warn=False):
-        super(FallbackForecaster, self).__init__()
-        self.forecasters = forecasters
+        super().__init__()
+        self.forecasters = tuple(forecasters)
         self.current_forecaster = None
         self.current_name = None
-        self.current_forecaster_index = 0
         self.warn = warn
+        self._set_forecast_pointer(0)
+
         self._anytagis_then_set("requires-fh-in-fit", True, False, self.forecasters)
+
+    def _set_forecast_pointer(self, val):
+        """Set the current forecaster index to a specified value.
+
+        This method ensures the persistence of `__current_forecaster_index` across the
+        invocations of `.fit()`. Typically, calling `.fit()` invokes `.reset()`, which,
+        by default, would erase certain instance variables, including
+        `__current_forecaster_index`. By using this method, `__current_forecaster_index`
+        is preserved or initialized when necessary.
+
+        Parameters
+        ----------
+        val : int
+            The new value to which `__current_forecaster_index` should be set.
+
+        Returns
+        -------
+        None
+        """
+        try:
+            self.__current_forecaster_index
+        except AttributeError:
+            self.__current_forecaster_index = val
 
     def _fit(self, y, X=None, fh=None):
         """Fit the forecasters in the given order until one succeeds.
@@ -83,23 +110,27 @@ class FallbackForecaster(_HeterogenousMetaEstimator, BaseForecaster):
         Raises
         ------
         RuntimeError
-            If all forecasters fail to fit."""
-        self.y = y
-        for name, forecaster in self.forecasters[self.current_forecaster_index :]:
+            If all forecasters fail to fit.
+        """
+        for name, forecaster in self.forecasters[self.__current_forecaster_index :]:
             try:
                 forecaster.fit(y, X=X, fh=fh)
                 self.current_forecaster = forecaster
                 self.current_name = name
                 return self
             except Exception as e:
-                self.current_forecaster_index += 1
+                self.__current_forecaster_index += 1
                 if self.warn:
-                    warnings.warn(f"Forecaster {name} failed to fit with error: {e}")
+                    warnings.warn(
+                        f"Forecaster {name} failed to fit with error: {e}", stacklevel=2
+                    )
 
         raise RuntimeError("All forecasters failed to fit.")
 
     def _predict(self, fh, X=None):
-        """Predict using the current forecaster. If it fails, fit and predict with the next forecaster.
+        """Predict using the current forecaster.
+
+        If predict fails, fit and predict with the next forecaster.
 
         Parameters
         ----------
@@ -124,16 +155,17 @@ class FallbackForecaster(_HeterogenousMetaEstimator, BaseForecaster):
         try:
             return self.current_forecaster.predict(fh, X)
         except Exception as e:
-            warnings.warn(f"Current forecaster failed at prediction with error: {e}")
-
-            # Remove the failed forecaster
-            self.forecasters.pop(0)
+            if self.warn:
+                warnings.warn(
+                    f"Current forecaster failed at prediction with error: {e}",
+                    stacklevel=2,
+                )
+            self.__current_forecaster_index += 1
 
             if not self.forecasters:
                 raise RuntimeError("No remaining forecasters to attempt prediction.")
 
             # Fit the next forecaster and retry prediction
             self.current_forecaster = None
-            self._fit(self.y, X, fh)
-            return self._predict(fh, X)
-
+            self.fit(self._y, X, fh)
+            return self.predict(fh, X)
--- sktime/forecasting/compose/tests/test_fallback.py
+++ sktime/forecasting/compose/tests/test_fallback.py
@@ -2,22 +2,22 @@
 # copyright: sktime developers, BSD-3-Clause License (see LICENSE file).
 """Unit tests for the FallbackForecaster functionality.
 
-Tests cover the basic operations of the FallbackForecaster, ensuring proper functionality
-of fitting, predicting, updating, and handling of errors in the forecasting process."""
+Tests cover the basic operations of the FallbackForecaster, ensuring proper
+functionality of fitting, predicting, updating, and handling of errors in the
+forecasting process.
+"""
 
 __author__ = ["ninedigits"]
 
-import pytest
 import pandas as pd
+import pytest
 
-from sktime.forecasting.compose import FallbackForecaster
-from sktime.forecasting.compose import EnsembleForecaster
+from sktime.base import _HeterogenousMetaEstimator
+from sktime.forecasting.base._base import BaseForecaster
+from sktime.forecasting.compose import EnsembleForecaster, FallbackForecaster
 from sktime.forecasting.naive import NaiveForecaster
 from sktime.forecasting.trend import PolynomialTrendForecaster
 from sktime.utils._testing.forecasting import make_forecasting_problem
-
-from sktime.forecasting.base._base import BaseForecaster
-from sktime.base import _HeterogenousMetaEstimator
 
 
 class ForecastingError(Exception):
@@ -41,14 +41,15 @@ class ForecastingError(Exception):
 class DummyForecaster(_HeterogenousMetaEstimator, BaseForecaster):
     """Dummy forecaster used for testing the FallbackForecaster.
 
-    This forecaster is intentionally designed to fail at specified stages of the forecasting
-    process (fit, predict, or update) to test the robustness and fallback mechanisms of the
-    FallbackForecaster.
+    This forecaster is intentionally designed to fail at specified stages of the
+    forecasting process (fit, predict, or update) to test the robustness and fallback
+    mechanisms of the FallbackForecaster.
 
     Parameters
     ----------
     raise_at : str
-        Stage at which the forecaster should fail. Options are "fit", "predict", "update".
+        Stage at which the forecaster should fail. Options are "fit", "predict",
+        "update".
 
     Raises
     ------
@@ -57,7 +58,7 @@ class DummyForecaster(_HeterogenousMetaEstimator, BaseForecaster):
     """
 
     def __init__(self, raise_at="fit"):
-        super(DummyForecaster, self).__init__()
+        super().__init__()
         __valid__ = ["fit", "predict", "update"]
         if raise_at not in __valid__:
             raise AttributeError(f"`raise_at` must choose from {__valid__}")
@@ -90,7 +91,7 @@ class DummyForecaster(_HeterogenousMetaEstimator, BaseForecaster):
 
 
 def test_raises_at_fit():
-    """Test dummy forecaster raises at predict"""
+    """Test dummy forecaster raises at fit"""
     # Start with negative time series, Theta model will fail here
     y = make_forecasting_problem(random_state=42)
     forecaster = DummyForecaster(raise_at="fit")
@@ -109,7 +110,7 @@ def test_raises_at_predict():
 
 
 def test_raises_at_update():
-    """Test dummy forecaster raises at predict"""
+    """Test dummy forecaster raises at update"""
     # Start with negative time series, Theta model will fail here
     y = make_forecasting_problem(random_state=42)
     forecaster = DummyForecaster(raise_at="update")
@@ -137,7 +138,7 @@ def test_fallbackforecaster_fails_at_fit():
     forecaster = FallbackForecaster(
         [
             (
-                "ensemble1",
+                "ensemble1_fails_at_fit",
                 ensemble_forecaster_1,
             ),
             (
@@ -172,7 +173,7 @@ def test_fallbackforecaster_fails_at_predict():
     forecaster = FallbackForecaster(
         [
             (
-                "ensemble1",
+                "ensemble1_fails_at_predict",
                 ensemble_forecaster_1,
             ),
             (
@@ -192,46 +193,49 @@ def test_fallbackforecaster_fails_at_predict():
 def test_fallbackforecaster_fails_twice():
     """First two FallbackForecasters fail, third succeeds"""
     y = make_forecasting_problem(random_state=42)
-    ensemble_forecaster_1 = EnsembleForecaster(
+    forecaster_1 = EnsembleForecaster(
         [
-            ("trend", PolynomialTrendForecaster()),
-            ("fails", DummyForecaster(raise_at="fit")),
+            ("trend_0", PolynomialTrendForecaster()),
+            ("fails_at_fit_0", DummyForecaster(raise_at="fit")),
         ]
     )
-    ensemble_forecaster_2 = EnsembleForecaster(
+    forecaster_2 = EnsembleForecaster(
         [
-            ("trend", PolynomialTrendForecaster()),
-            ("fails", DummyForecaster(raise_at="predict")),
+            ("trend_1", PolynomialTrendForecaster()),
+            ("fails_at_predict_1", DummyForecaster(raise_at="predict")),
         ]
     )
-    ensemble_forecaster_3 = EnsembleForecaster(
+    forecaster_3 = EnsembleForecaster(
         [
-            ("trend", PolynomialTrendForecaster()),
-            ("naive", NaiveForecaster()),
+            ("trend_2", PolynomialTrendForecaster()),
+            ("naive_2", NaiveForecaster()),
         ]
     )
+    forecaster_4 = PolynomialTrendForecaster()
     forecaster = FallbackForecaster(
         [
             (
-                "ensemble1",
-                ensemble_forecaster_1,
+                "ensemble1_fails_at_fit",
+                forecaster_1,
             ),
             (
-                "ensemble2",
-                ensemble_forecaster_2,
+                "ensemble2_fails_at_predict",
+                forecaster_2,
             ),
             (
-                "ensemble3",
-                ensemble_forecaster_3,
+                "ensemble3_succeeds",
+                forecaster_3,
             ),
-            ("forecaster4", PolynomialTrendForecaster()),
+            ("forecaster4_isnt_called", forecaster_4),
         ]
     )
     forecaster.fit(y=y, fh=[1, 2, 3])
     y_pred_actual = forecaster.predict()
-    y_pred_expected = ensemble_forecaster_3.predict()
+
+    forecaster_3.fit(y=y, fh=[1, 2, 3])
+    y_pred_expected = forecaster_3.predict()
     name = forecaster.current_name
-    assert name == "ensemble3"
+    assert name == "ensemble3_succeeds"
     pd.testing.assert_series_equal(y_pred_expected, y_pred_actual)
 
 
@@ -304,7 +308,6 @@ def test_all_forecasters_fail2():
     forecaster = FallbackForecaster([forecaster1, forecaster2, forecaster3])
     with pytest.raises(RuntimeError):
         forecaster.fit(y=y, fh=[1, 2, 3])
-        forecaster.predict()
 
 
 def test_all_forecasters_fail3():
@@ -318,3 +321,24 @@ def test_all_forecasters_fail3():
     with pytest.raises(RuntimeError):
         forecaster.fit(y=y, fh=[1, 2, 3])
         forecaster.predict()
+
+
+def test_many_forecasters_fail1():
+    """All forecasters fail at predict"""
+    # Start with negative time series, Theta model will fail here
+    y = make_forecasting_problem(random_state=42)
+    forecaster1 = ("raise_predict1", DummyForecaster(raise_at="predict"))
+    forecaster2 = ("raise_fit2", DummyForecaster(raise_at="fit"))
+    forecaster3 = ("raise_fit3", DummyForecaster(raise_at="fit"))
+    forecaster4 = ("raise_predict4", DummyForecaster(raise_at="predict"))
+    forecaster5 = ("forecaster5", PolynomialTrendForecaster())
+    forecaster = FallbackForecaster(
+        [forecaster1, forecaster2, forecaster3, forecaster4, forecaster5]
+    )
+    forecaster.fit(y, fh=[1, 2, 3])
+    y_pred_actual = forecaster.predict()
+    y_name_actual = forecaster.current_name
+    y_pred_expected = forecaster5[1].predict()
+    y_name_expected = forecaster5[0]
+    pd.testing.assert_series_equal(y_pred_actual, y_pred_expected)
+    assert y_name_actual == y_name_expected
