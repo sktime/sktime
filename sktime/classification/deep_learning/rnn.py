efd5361a47ae2c7f1ae4ed12ed013e02b7270df2
Franz Király <f.kiraly@ucl.ac.uk>
2024-01-24 13:12:07 +0100

    finish deprecation of num_epochs inn RNN classifier and regressor

--- sktime/classification/deep_learning/rnn.py
+++ sktime/classification/deep_learning/rnn.py
@@ -14,10 +14,6 @@ from sktime.utils.validation._dependencies import _check_dl_dependencies
 from sktime.utils.warnings import warn
 
 
-# todo: 0.26.0 - please remove num_epochs parameter and related logic,
-# because parameter num_epochs usage is deprecated and will be renamed
-# to n_epochs.  Also n_epochs should be moved to become the first
-# argument in the __init__ and super().__init__.
 class SimpleRNNClassifier(BaseDeepClassifier):
     """Simple recurrent neural network.
 
@@ -63,7 +59,7 @@ class SimpleRNNClassifier(BaseDeepClassifier):
 
     def __init__(
         self,
-        num_epochs=None,
+        n_epochs=100,
         batch_size=1,
         units=6,
         callbacks=None,
@@ -75,26 +71,11 @@ class SimpleRNNClassifier(BaseDeepClassifier):
         activation="sigmoid",
         use_bias=True,
         optimizer=None,
-        n_epochs=100,
     ):
         _check_dl_dependencies(severity="error")
+
         super().__init__()
-        # todo: 0.26.0 - remove this, replace by
-        # self._n_epochs = n_epochs
-        # Deprecated Parameter Handling
-        if num_epochs is not None:
-            warn(
-                "In SimpleRNNClassifier, the parameter 'num_epochs' is deprecated "
-                "and will be removed in v0.26.0. It will be renamed to n_epochs. "
-                "To avoid this warning, update your code to use 'n_epochs'. ",
-                DeprecationWarning,
-                stacklevel=2,
-            )
-            self._n_epochs = num_epochs
-        else:
-            self._n_epochs = n_epochs
-        self.num_epochs = num_epochs
-        # end remove
+
         self.batch_size = batch_size
         self.verbose = verbose
         self.units = units
@@ -218,7 +199,7 @@ class SimpleRNNClassifier(BaseDeepClassifier):
             X,
             y_onehot,
             batch_size=self.batch_size,
-            epochs=self._n_epochs,
+            epochs=self.n_epochs,
             verbose=self.verbose,
             callbacks=self.callbacks_,
         )
