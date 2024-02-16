2d95aea8ce56c8a374ce9d8538dc8154cea55c6a
Franz Király <f.kiraly@ucl.ac.uk>
2024-02-16 12:59:41 +0100

    unequal length

--- sktime/classification/distance_based/_time_series_neighbors.py
+++ sktime/classification/distance_based/_time_series_neighbors.py
@@ -240,22 +240,52 @@ class KNeighborsTimeSeriesClassifier(BaseClassifier):
         if n_vars == 1:
             x = np.reshape(x, (1, n_vars, -1))
             y = np.reshape(y, (1, n_vars, -1))
-        else:
+        elif self._X_metadata["is_equal_length"]:
             x = np.reshape(x, (-1, n_vars))
             y = np.reshape(y, (-1, n_vars))
             x_ix = pd.MultiIndex.from_product([[0], range(len(x))])
             y_ix = pd.MultiIndex.from_product([[0], range(len(y))])
             x = pd.DataFrame(x, index=x_ix)
             y = pd.DataFrame(y, index=y_ix)
+        else:  # multivariate, unequal length
+            # in _convert_X_to_sklearn, we have encoded the length as the first column
+            # this was coerced to float, so we round to avoid rounding errors
+            x_len = round(x[0])
+            y_len = round(y[0])
+            # pd.pivot switches the axes, compared to numpy
+            x = np.reshape(x[1:], (n_vars, -1)).T
+            y = np.reshape(y[1:], (n_vars, -1)).T
+            # cut to length
+            x = x[:x_len]
+            y = y[:y_len]
+            x_ix = pd.MultiIndex.from_product([[0], range(x_len)])
+            y_ix = pd.MultiIndex.from_product([[0], range(y_len)])
+            x = pd.DataFrame(x, index=x_ix)
+            y = pd.DataFrame(y, index=y_ix)
         return self._distance(x, y)[0, 0]
 
     def _convert_X_to_sklearn(self, X):
         """Convert X to 2D numpy for sklearn."""
+        # special treatment for unequal length series
+        if not self._X_metadata["is_equal_length"]:
+            # then we know we are dealing with pd-multiindex
+            # as a trick to deal with unequal length data,
+            # we flatten encode the length as the first column
+            X_w_ix = X.reset_index(-1)
+            X_pivot = X_w_ix.pivot(columns=[X_w_ix.columns[0]])
+            # fillna since this creates nan but sklearn does not accept these
+            # the fill value does not matter as the distance ignores it
+            X_pivot = X_pivot.fillna(0).to_numpy()
+            X_lens = X.groupby(X_w_ix.index).size().to_numpy()
+            # add the first column, encoding length of individual series
+            X_w_lens = np.concatenate([X_lens[:, None], X_pivot], axis=1)
+            return X_w_lens
+
+        # equal length series case
         if isinstance(X, np.ndarray):
             X_mtype = "numpy3D"
         else:
             X_mtype = "pd-multiindex"
-            print(X)
         return convert(X, from_type=X_mtype, to_type="numpyflat")
 
     def _fit(self, X, y):
