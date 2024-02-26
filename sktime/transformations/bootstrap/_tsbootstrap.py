6576d5e0d6e7f8fbb620b779c409237d748e5bec
benheid <benheid@users.noreply.github.com>
2024-02-26 18:24:09 +0100

    fix linting

--- sktime/transformations/bootstrap/_tsbootstrap.py
+++ sktime/transformations/bootstrap/_tsbootstrap.py
@@ -115,7 +115,7 @@ class TSBootstrapAdapter(BaseTransformer):
         deps = cls.get_class_tag("python_dependencies")
         _check_soft_dependencies(deps, severity="error")
 
-        from tsbootstrap import MovingBlockBootstrap, BlockBootstrap
+        from tsbootstrap import BlockBootstrap, MovingBlockBootstrap
 
         params = [
             {"bootstrap": BlockBootstrap(n_bootstraps=10)},
