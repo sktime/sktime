1613ff92e3dee2e78485dc6adbe6bb8dbebbd19d
Xinyu-Wu-0000 <xinyu.wuxinyu@outlook.com>
2024-03-09 14:52:51 +0800

    Second test parameter set for shapeDTW

--- sktime/classification/distance_based/_shape_dtw.py
+++ sktime/classification/distance_based/_shape_dtw.py
@@ -502,3 +502,31 @@ class ShapeDTW(BaseClassifier):
                 colToAdd.append(pd.Series(inst))
             df[col] = colToAdd
         return df
+
+    @classmethod
+    def get_test_params(cls, parameter_set="default"):
+        """Return testing parameter settings for the estimator.
+
+        Parameters
+        ----------
+        parameter_set : str, default="default"
+            Name of the set of test parameters to return, for use in tests. If no
+            special parameters are defined for a value, will return ``"default"`` set.
+
+
+        Returns
+        -------
+        params : dict or list of dict, default = {}
+            Parameters to create testing instances of the class
+            Each dict are parameters to construct an "interesting" test instance, i.e.,
+            ``MyClass(**params)`` or ``MyClass(**params[i])`` creates a valid test
+            instance.
+            ``create_test_instance`` uses the first (or only) dictionary in ``params``
+        """
+        params1 = {}
+        params2 = {
+            "n_neighbors": 3,
+            "shape_descriptor_function": "compound",
+            "shape_descriptor_functions": ['paa', 'dwt']
+        }
+        return [params1, params2]
