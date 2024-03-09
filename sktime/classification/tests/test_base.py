62be204fd4fc9b28cae1434eae388673150e6632
benheid <benheid@users.noreply.github.com>
2024-03-09 20:41:38 +0100

    fix linting

--- sktime/classification/tests/test_base.py
+++ sktime/classification/tests/test_base.py
@@ -500,9 +500,7 @@ def test_deep_estimator_full(optimizer):
         assert serialize(full_dummy.optimizer) == serialize(deserialized_full.optimizer)
 
         # assert weights of optimizers are same
-        assert (
-            full_dummy.optimizer.variables == deserialized_full.optimizer.variables
-        )
+        assert full_dummy.optimizer.variables == deserialized_full.optimizer.variables
 
         # remove optimizers from both to do full dict check,
         # since two different objects
