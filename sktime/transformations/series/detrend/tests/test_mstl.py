7f9af044d88395cb240bbf32e011eaf8ba15d63c
Krishna Tripathi <121068842+kcentric@users.noreply.github.com>
2024-03-08 22:00:54 -0600

    Fix typo in test_mstl.py
    Fixed typos, added copyright comment and additional heading. File should be good to go.

--- sktime/transformations/series/detrend/tests/test_mstl.py
+++ sktime/transformations/series/detrend/tests/test_mstl.py
@@ -1,18 +1,20 @@
+# copyright: sktime developers, BSD-3-Clause License (see LICENSE file)
+"""Tests MSTL functionality."""
+
+__all__ = ["test-MSTL"]
+__authors__ = ["krishna-t"]
+
 import numpy as np
 import pandas as pd
 from sktime.datasets import load_airline
 from sktime.transformations.series.detrend.mstl import MSTL
 
 def test_transform_returns_correct_components():
-    # TODO: See if we need to create a fresh time series dataset?
-    # np.random.seed(42)
-    # data = np.random.randn(100)
-    # index = pd.date_range(start="2020-01-01", periods=100, freq="D")
-    # series = pd.Series(data, index=index)
-
+    """Tests whether expected components are returned when *return_components* 
+    parameter is switched on."""
     # Load our default test dataset
     series = load_airline()
-    series.index = y.index.to_timestamp()
+    series.index = series.index.to_timestamp()
     
     # Initialize the MSTL transformer with specific parameters
     transformer = MSTL(periods=[3,12], return_components=True)
@@ -24,9 +26,8 @@ def test_transform_returns_correct_components():
     transformed = transformer.transform(series)
 
     # Check if the transformed data has the expected components
-    assert 'transformed' in transformed.columns, "Transformed component missing"
-    assert 'trend' in transformed.columns, "Trend component missing"
-    assert 'resid' in transformed.columns, "Residual component missing"
-    # TODO: Specify "Seasonal_3" and "Seasonal_12" missing for our specific data or not?
-    assert 'seasonal_3' in transformed.columns, "Seasonal component missing" 
-    assert 'seasonal_12' in transformed.columns, "Seasonal component missing"
+    assert 'transformed' in transformed.columns, "Transformed component missing!"
+    assert 'trend' in transformed.columns, "Trend component missing!"
+    assert 'resid' in transformed.columns, "Residual component missing!"
+    assert 'seasonal_3' in transformed.columns, "Seasonal component missing!" 
+    assert 'seasonal_12' in transformed.columns, "Seasonal component missing!"
