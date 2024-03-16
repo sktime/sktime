4d03297aed2d22ed96d04205b2f0ef826dc0994e
iaryangoyal <aryangoyal2011@gmail.com>
2024-03-16 10:13:16 +0530

    Changes in Rocket regression

--- sktime/regression/kernel_based/_rocket_regressor.py
+++ sktime/regression/kernel_based/_rocket_regressor.py
@@ -204,18 +204,23 @@ class RocketRegressor(_DelegatedRegressor, BaseRegressor):
     def get_test_params(cls, parameter_set="default"):
         """Return testing parameter settings for the estimator.
 
-        Parameters
-        ----------
-        parameter_set : str, default="default"
-            Name of the set of test parameters to return, for use in tests. If no
-            special parameters are defined for a value, will return `"default"` set.
+    Parameters
+    ----------
+    parameter_set : str, default="default"
+        Name of the set of test parameters to return, for use in tests. If no
+        special parameters are defined for a value, will return ``"default"`` set.
 
-        Returns
-        -------
-        params : dict or list of dict, default={}
-            Parameters to create testing instances of the class.
-            Each dict are parameters to construct an "interesting" test instance, i.e.,
-            `MyClass(**params)` or `MyClass(**params[i])` creates a valid test instance.
-            `create_test_instance` uses the first (or only) dictionary in `params`.
-        """
-        return {"num_kernels": 20}
+    Returns
+    -------
+    params : dict or list of dict, default={}
+        Parameters to create testing instances of the class.
+        Each dict are parameters to construct an "interesting" test instance, i.e.,
+        ``MyClass(**params)`` or ``MyClass(**params[i])`` creates a valid test
+        instance.
+        ``create_test_instance`` uses the first (or only) dictionary in ``params``.
+    """
+        if parameter_set == "default":
+            return [{"num_kernels": 20}, {"num_kernels": 100}]
+
+        return {}
+
