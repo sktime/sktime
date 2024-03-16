9a697b833a2b7dc6980aa0f2b7f7cc6bc944f6f7
Franz Király <f.kiraly@ucl.ac.uk>
2024-03-16 22:55:57 +0100

    fix inceptiontime

--- sktime/regression/deep_learning/inceptiontime.py
+++ sktime/regression/deep_learning/inceptiontime.py
@@ -153,7 +153,7 @@ class InceptionTimeRegressor(BaseDeepRegressor):
 
         check_random_state(self.random_state)
         self.input_shape = X.shape[1:]
-        self.model_ = self.build_model(self.input_shape, self.n_classes_)
+        self.model_ = self.build_model(self.input_shape)
         if self.verbose:
             self.model_.summary()
 
