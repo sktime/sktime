c2cd970d90a47e69d6a3636451ad7cd2c905f408
Franz Király <f.kiraly@ucl.ac.uk>
2024-03-17 19:46:35 +0100

    linting

--- sktime/utils/validation/_dependencies.py
+++ sktime/utils/validation/_dependencies.py
@@ -413,9 +413,8 @@ def _check_env_marker(obj, package=None, msg=None, severity="error"):
     except InvalidMarker:
         msg_version = (
             f"wrong format for env_marker tag, "
-            f'must be PEP 508 compatible specifier string, e.g., '
-            'platform_system!="windows",'
-            f' but found "{est_marker_tag}"'
+            f"must be PEP 508 compatible specifier string, e.g., "
+            f'platform_system!="windows", but found "{est_marker_tag}"'
         )
         raise InvalidMarker(msg_version)
 
