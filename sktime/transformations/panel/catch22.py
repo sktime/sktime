ca472a1f1f909d1c802185556847b27e52c83f68
Franz Király <f.kiraly@ucl.ac.uk>
2024-03-19 22:45:09 +0100

    also fix in short feat name dict

--- sktime/transformations/panel/catch22.py
+++ sktime/transformations/panel/catch22.py
@@ -101,7 +101,7 @@ SHORT_FEATURE_NAMES_DICT = {
     "SB_MotifThree_quantile_hh": "entropy_pairs",
     "FC_LocalSimple_mean1_tauresrat": "whiten_timescale",
     "CO_Embed2_Dist_tau_d_expfit_meandiff": "embedding_dist",
-    "SC_FluctAnal_2_dfa_50_1_2_logi_prop_r1": "dfa",
+    "SC_FluctAnal_2_dfa_50_1_2_logi_prop_r1_prop_r1": "dfa",
     "SC_FluctAnal_2_rsrangefit_50_1_logi_prop_r1": "rs_range",
     "SB_TransitionMatrix_3ac_sumdiagcov": "transition_matrix",
     "PD_PeriodicityWang_th0_01": "periodicity",
