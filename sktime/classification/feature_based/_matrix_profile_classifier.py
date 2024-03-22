9209ca4b253a9565074d754be7f4395feee0680b
MMTrooper <89777534+MMTrooper@users.noreply.github.com>
2024-03-23 00:15:38 +0300

    Added Parameters

--- sktime/classification/feature_based/_matrix_profile_classifier.py
+++ sktime/classification/feature_based/_matrix_profile_classifier.py
@@ -194,4 +194,12 @@ class MatrixProfileClassifier(BaseClassifier):
             instance.
             ``create_test_instance`` uses the first (or only) dictionary in ``params``.
         """
-        return {"subsequence_length": 4}
+        if parameter_set == "results_comparison":
+            return {
+                "subsequence_length": 8,
+            }
+        else:
+            param1 = {"subsequence_length": 4}
+            param2 = {"subsequence_length": 10}
+
+            return [param1, param2]
