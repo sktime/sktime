83305f4bdd326ea2c71fcebc250437938d41df1a
shankariraja <125146652+shankariraja@users.noreply.github.com>
2024-03-23 12:46:37 +0530

    Add new test parameter sets for TimeSeriesKMeansTslearn

--- sktime/clustering/k_means/_k_means_tslearn.py
+++ sktime/clustering/k_means/_k_means_tslearn.py
@@ -204,8 +204,20 @@ class TimeSeriesKMeansTslearn(_TslearnAdapter, BaseClusterer):
             "random_state": None,
             "init": "random",
         }
+        params3 = {
+        "n_clusters": 5,
+        "max_iter": 100,
+        "tol": 1e-5,
+        "n_init": 5,
+        "metric": "euclidean",
+        "max_iter_barycenter": 20,
+        "verbose": 1,
+        "random_state": 42,
+        "init": "random",
+        }
+        return [params2, params3]
         # return [params1, params2]
-        return [params2]
+        # return [params2]
 
     def _score(self, X, y=None) -> float:
         return np.abs(self.inertia_)
