6e5e7199d0ec016b5ec592da2a19ae31d33a8aa4
ksharma6 <ksharma6@tutanota.com>
2024-03-25 10:39:13 -0700

    minor refactoring

--- sktime/regression/model_selection/__init__.py
+++ sktime/regression/model_selection/__init__.py
@@ -1,9 +1,6 @@
-#!/usr/bin/env python3 -u
+"""Tuning of time series regressors."""
 # copyright: sktime developers, BSD-3-Clause License (see LICENSE file)
-"""Implements functionality for selecting regression models."""
-
-__all__ = [
-    "TSRGridSearchCV",
-]
 
 from sktime.regression.model_selection._tune import TSRGridSearchCV
+
+__all__ = ["TSRGridSearchCV"]
