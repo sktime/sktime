167691017635af98c247450138823f04bc812fc9
Franz Király <f.kiraly@ucl.ac.uk>
2024-03-30 14:27:04 +0100

    Update random_state.py

--- sktime/utils/random_state.py
+++ sktime/utils/random_state.py
@@ -2,9 +2,13 @@
 # copied from scikit-learn to avoid dependency on sklearn private methods
 
 import numpy as np
-from sklearn.utils import check_random_state
+
+from sktime.utils.validation._dependencies import _check_soft_dependencies
 
 
+# todo 0.29.0 - check if this can be completely replaced by skbase set_random_state
+# this is feasible if skbase lower bound becomes 0.7.2 or larger
+# CAUTION: default random_state current is 0, in skbase this is random_state=None
 def set_random_state(estimator, random_state=0):
     """Set fixed random_state parameters for an estimator.
 
@@ -20,12 +24,20 @@ def set_random_state(estimator, random_state=0):
         Pseudo-random number generator to control the generation of the random
         integers. Pass an int for reproducible output across multiple function calls.
 
-    Notes
-    -----
-    This does not necessarily set *all* ``random_state`` attributes that
-    control an estimator's randomness, only those accessible through
-    ``estimator.get_params()``.
+    Returns
+    -------
+    estimator : estimator
+        reference to ``estimator`` with state changed, random seed set
     """
+    if _check_soft_dependencies(
+        "scikit-base>=0.7.2",
+        package_import_alias={"scikit-base": "skbase"},
+        severity="none",
+    ):
+        from skbase.utils.random_state import set_random_state as _set_random_state
+
+        return set_random_state(estimator, random_state)
+
     random_state = check_random_state(random_state)
     to_set = {}
     for key in sorted(estimator.get_params(deep=True)):
@@ -34,3 +46,5 @@ def set_random_state(estimator, random_state=0):
 
     if to_set:
         estimator.set_params(**to_set)
+
+    return estimator
