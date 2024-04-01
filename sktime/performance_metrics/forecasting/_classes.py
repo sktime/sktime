c516ec77174512df443607ce8e3f7c439fffa842
Kaustubh Upadhyay <97105592+KaustubhUp025@users.noreply.github.com>
2024-04-01 11:02:11 +0530

    Update _classes.py Implementing efficient _evaluate_by_index for GeometricMeanAbsoluteError

--- sktime/performance_metrics/forecasting/_classes.py
+++ sktime/performance_metrics/forecasting/_classes.py
@@ -1642,7 +1642,47 @@ class GeometricMeanAbsoluteError(BaseForecastingErrorMetricFunc):
     0.7000014418652152
     """
 
-    func = geometric_mean_absolute_error
+    def _evaluate_by_index(self, y_true, y_pred, **kwargs):
+        """Return the metric evaluated at each time point.
+
+        private _evaluate_by_index containing core logic, called from evaluate_by_index
+
+        Parameters
+        ----------
+        y_true : time series in sktime compatible pandas based data container format
+            Ground truth (correct) target values
+            y can be in one of the following formats:
+            Series scitype: pd.DataFrame
+            Panel scitype: pd.DataFrame with 2-level row MultiIndex
+            Hierarchical scitype: pd.DataFrame with 3 or more level row MultiIndex
+        y_pred :time series in sktime compatible data container format
+            Forecasted values to evaluate
+            must be of same format as y_true, same indices and columns if indexed
+
+        Returns
+        -------
+        loss : pd.Series or pd.DataFrame
+            Calculated metric, by time point (default=jackknife pseudo-values).
+            pd.Series if self.multioutput="uniform_average" or array-like
+                index is equal to index of y_true
+                entry at index i is metric at time i, averaged over variables
+            pd.DataFrame if self.multioutput="raw_values"
+                index and columns equal to those of y_true
+                i,j-th entry is metric at time i, at variable j
+        """
+        multioutput = self.multioutput
+
+        raw_values = (y_true - y_pred).abs()
+
+        if isinstance(multioutput, str):
+            if multioutput == "raw_values":
+                return raw_values
+
+            if multioutput == "uniform_average":
+                return raw_values.mean(axis=1)
+
+        # else, we expect multioutput to be array-like
+        return raw_values.dot(multioutput)
 
 
 class GeometricMeanSquaredError(BaseForecastingErrorMetricFunc):
