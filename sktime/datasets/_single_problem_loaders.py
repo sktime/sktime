97fd4da84d2b3e4643840a8d36a6d42616bc4e2a
Pranav <pranavprajapati586@gmail.com>
2024-04-05 17:53:44 +0530

    Fix tsf data error log and make it more precise

--- sktime/datasets/_single_problem_loaders.py
+++ sktime/datasets/_single_problem_loaders.py
@@ -1413,10 +1413,8 @@ def load_forecastingdata(
         # valid dataset names for classification, regression, forecasting datasets repo
         if name not in list(tsf_all_datasets):
             raise ValueError(
-                {name}
-                + " is not a valid dataset name. \
-                    List of valid dataset names can be found at \
-                    sktime.datasets.tsf_dataset_names.tsf_all_datasets"
+                f"{name} is not a valid dataset name. "
+                f"List of valid datasets is {tsf_all_datasets}"
             )
 
         url = f"https://zenodo.org/record/{tsf_all[name]}/files/{name}.zip"
