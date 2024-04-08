c7a31bdfd1f1ed6672331e833b189d9262368422
ArthrowAbstract <arthrowabstract@gmail.com>
2024-04-08 05:51:05 +0530

    update tests._config

--- sktime/tests/_config.py
+++ sktime/tests/_config.py
@@ -33,7 +33,7 @@ EXCLUDE_ESTIMATORS = [
     "TimeSeriesLloyds",  # an abstract class, but does not follow naming convention
     # DL classifier suspected to cause hangs and memouts, see #4610
     "FCNClassifier",
-    "MACNNClassifier",
+    #"MACNNClassifier",
     "EditDist",
     "CNNClassifier",
     "FCNClassifier",
@@ -45,7 +45,7 @@ EXCLUDE_ESTIMATORS = [
     "ResNetRegressor",
     "FCNRegressor",
     "LSTMFCNRegressor",
-    "MACNNRegressor",
+    #"MACNNRegressor",
     "CNTCClassifier",
     "CNTCRegressor",
     # splitters excluded with undiagnosed failures, see #6194
@@ -161,6 +161,7 @@ EXCLUDED_TESTS = {
     ],
     "MACNNClassifier": [
         "test_fit_idempotent",
+        "test_persistence_via_pickle",
     ],
     "FCNRegressor": [
         "test_fit_idempotent",
