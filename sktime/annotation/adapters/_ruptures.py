e40c498619fb848bb768561fb3e05c9bc54b13d1
duydl <duy.dole.00ece@gmail.com>
2024-04-13 22:19:52 +0900

    Add other search methods

--- sktime/annotation/adapters/_ruptures.py
+++ sktime/annotation/adapters/_ruptures.py
@@ -14,23 +14,45 @@ class RupturesKernelCPD(BaseSeriesAnnotator):
     Available kernels:
 
     - `linear`: $k(x,y) = x^T y$.
-    - `rbf`: $k(x, y) = exp(\gamma \|x-y\|^2)$ where $\gamma>0$
-    (`gamma`) is a user-defined parameter.
+    - `rbf`: $k(x, y) = \exp(\gamma \|x-y\|^2)$ where $\gamma>0$
+      (`gamma`) is a user-defined parameter.
     - `cosine`: $k(x,y)= (x^T y)/(\|x\|\|y\|)$.
 
     Parameters
     ----------
-        kernel (str, optional): name of the kernel, ["linear", "rbf", "cosine"]
-        min_size (int, optional): minimum segment length.
-        jump (int, optional): not considered, set to 1.
-        params (dict, optional): a dictionary of parameters for the kernel instance
-        n_bkps (int, optional): Number of change points. Defaults to None.
-        pen (float, optional): penalty value (>0). Defaults to None. Not considered
-            if n_bkps is not None.
+    kernel : str, optional
+        Name of the kernel, ["linear", "rbf", "cosine"].
+    min_size : int, optional
+        Minimum segment length.
+    jump : int, optional
+        Not considered, set to 1.
+    params : dict, optional
+        A dictionary of parameters for the kernel instance.
+    n_cps : int, optional
+        Number of change points. Defaults to None.
+    pen : float, optional
+        Penalty value (>0). Defaults to None. Not considered
+        if n_cps is not None.
+
+    Attributes
+    ----------
+    _tags : dict
+        Tags for the estimator indicating its properties.
+    _estimator : ruptures.KernelCPD
+        Instance of the Ruptures KernelCPD estimator.
+    _n_cps : int, optional
+        Number of change points.
+    _pen : float, optional
+        Penalty value.
+
+    See Also
+    --------
+    ruptures.KernelCPD : Ruptures library's KernelCPD estimator.
+
     """
 
     _tags = {
-        "univariate-only": True,
+        "univariate-only": False,
         "fit_is_empty": True,
         "python_dependencies": "ruptures",
     }
@@ -60,8 +82,10 @@ class RupturesKernelCPD(BaseSeriesAnnotator):
 
         Parameters
         ----------
-        X : arraylike (1D np.ndarray or pd.series), shape = [num_observations]
-            Observations to apply labels.
+        X : array-like, shape = (n_samples, n_features) or (n_samples,)
+            Input data. Multivariate time series data where each row represents
+            a sample, and each column represents a feature. If 1D, it represents
+            univariate time series.
 
         Returns
         -------
@@ -75,12 +99,14 @@ class RupturesKernelCPD(BaseSeriesAnnotator):
 
         Parameters
         ----------
-        X : 1D np.array, shape = [num_observations]
-            Observations to apply labels to.
+        X : array-like, shape = (n_samples, n_features) or (n_samples,)
+            Input data. Multivariate time series data where each row represents
+            a sample, and each column represents a feature. If 1D, it represents
+            univariate time series.
 
         Returns
         -------
-        cps :
+        cps : array-like
             A sorted list of change points.
         """
         cps = self._estimator.fit_predict(X, n_bkps=self._n_cps, pen=self._pen)
@@ -108,13 +134,189 @@ class RupturesKernelCPD(BaseSeriesAnnotator):
         return [{"pen": None, "n_cps": 1}, {"pen": 5, "n_cps": None}]
 
 
-# class RupturesBinsegCPD(BaseSeriesAnnotator):
-#     pass
+class RupturesBinsegCPD(BaseSeriesAnnotator):
+    """Adapter for Ruptures Binseg.
+
+    Parameters
+    ----------
+    model (str, optional): segment model, ["l1", "l2", "rbf",...].
+        Not used if ``'custom_cost'`` is not None.
+    custom_cost (BaseCost, optional): custom cost function. Defaults to None.
+    min_size (int, optional): minimum segment length. Defaults to 2 samples.
+    jump (int, optional): subsample (one every *jump* points).
+    n_cps (int): Number of breakpoints to find before stopping.
+    pen (float): Penalty value (>0).
+    epsilon (float): Reconstruction budget (>0).
+    params (dict, optional): a dictionary of parameters for the cost instance.
+    """
+
+    _tags = {
+        "univariate-only": False,
+        "fit_is_empty": True,
+        "python_dependencies": "ruptures",
+    }
+
+    def __init__(
+        self,
+        model="l2",
+        custom_cost=None,
+        min_size=2,
+        jump=1,
+        n_cps=None,
+        pen=None,
+        epsilon=None,
+        params=None,
+    ):
+        import ruptures
+
+        self._estimator = ruptures.Binseg(
+            model=model,
+            custom_cost=custom_cost,
+            min_size=min_size,
+            jump=jump,
+            params=params,
+        )
+        self._n_cps = n_cps
+        self._pen = pen
+        self._epsilon = epsilon
+        super().__init__()
+
+    def _fit(self, X, Y=None):
+        """Fit the Binseg instance to the input data.
+
+        Parameters
+        ----------
+            X (array-like): Input data. Shape (n_samples, n_features) or (n_samples,).
+
+        Returns
+        -------
+            self
+        """
+        return self
+
+    def _predict(self, X):
+        """Predict the optimal breakpoints using the fitted Binseg instance.
+
+        Parameters
+        ----------
+            X (array-like): Input data. Shape (n_samples, n_features) or (n_samples,).
+
+        Returns
+        -------
+            list: Sorted list of breakpoints.
+        """
+        return self._estimator.fit_predict(
+            X, n_bkps=self._n_cps, pen=self._pen, epsilon=self._epsilon
+        )
 
 
-# class RupturesWindowCPD(BaseSeriesAnnotator):
-#     pass
+class RupturesWindowCPD(BaseSeriesAnnotator):
+    """Adapter for Ruptures Window."""
+
+    def __init__(
+        self,
+        width=100,
+        model="l2",
+        custom_cost=None,
+        min_size=2,
+        jump=1,
+        n_cps=None,
+        pen=None,
+        params=None,
+    ):
+        import ruptures
+
+        super().__init__()
+
+        self._estimator = ruptures.Window(
+            width=width,
+            model=model,
+            custom_cost=custom_cost,
+            min_size=min_size,
+            jump=jump,
+            params=params,
+        )
+        self._width = width
+        self._n_cps = n_cps
+        self._pen = pen
+
+    def _fit(self, X, Y=None):
+        """Fit the Window instance to the input data.
+
+        Parameters
+        ----------
+            X (array-like): Input data. Shape (n_samples, n_features) or (n_samples,).
+
+        Returns
+        -------
+            self
+        """
+        return self
+
+    def _predict(self, X):
+        """Predict the optimal breakpoints using the fitted Window instance.
+
+        Parameters
+        ----------
+            X (array-like): Input data. Shape (n_samples, n_features) or (n_samples,).
+
+        Returns
+        -------
+            list: Sorted list of breakpoints.
+        """
+        return self._estimator.fit_predict(X, n_bkps=self._n_cps, pen=self._pen)
 
 
-# class RupturesBottomUpCPD(BaseSeriesAnnotator):
-#     pass
+class RupturesBottomUpCPD(BaseSeriesAnnotator):
+    """Adapter for Ruptures BottomUp."""
+
+    def __init__(
+        self,
+        model="l2",
+        custom_cost=None,
+        min_size=2,
+        jump=1,
+        n_cps=None,
+        pen=None,
+        params=None,
+    ):
+        import ruptures
+
+        super().__init__()
+
+        self._estimator = ruptures.BottomUp(
+            model=model,
+            custom_cost=custom_cost,
+            min_size=min_size,
+            jump=jump,
+            params=params,
+        )
+
+        self._n_cps = n_cps
+        self._pen = pen
+
+    def _fit(self, X, Y=None):
+        """Fit the Window instance to the input data.
+
+        Parameters
+        ----------
+            X (array-like): Input data. Shape (n_samples, n_features) or (n_samples,).
+
+        Returns
+        -------
+            self
+        """
+        return self
+
+    def _predict(self, X):
+        """Predict the optimal breakpoints using the fitted Window instance.
+
+        Parameters
+        ----------
+            X (array-like): Input data. Shape (n_samples, n_features) or (n_samples,).
+
+        Returns
+        -------
+            list: Sorted list of breakpoints.
+        """
+        return self._estimator.fit_predict(X, n_bkps=self._n_cps, pen=self._pen)
