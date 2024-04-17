4c79dc28420cb97d6aef16401dbded281ed3a83d
Franz Király <f.kiraly@ucl.ac.uk>
2024-04-17 13:41:16 +0100

    Update _config.py

--- sktime/tests/_config.py
+++ sktime/tests/_config.py
@@ -229,7 +229,7 @@ EXCLUDED_TESTS = {
         "test_multiprocessing_idempotent",
     ],
     # ShapeletLearningClassifier is non-pickleable due to DL dependencies
-    "ShapeletTransform": [
+    "ShapeletLearningClassifierTslearn": [
         "test_persistence_via_pickle",
         "test_save_estimators_to_file",
         "test_fit_idempotent",
