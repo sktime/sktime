6f24cf01a4537d7ac91227dd461b04f368a56122
Franz Király <f.kiraly@ucl.ac.uk>
2024-04-19 14:47:54 +0100

    move in

---
+++ sktime/forecasting/reconcile/__init__.py
@@ -0,0 +1,6 @@
+# copyright: sktime developers, BSD-3-Clause License (see LICENSE file)
+"""Forecast reconciliation forecasters."""
+
+__all__ = ["ReconcilerForecaster"]
+
+from sktime.forecasting.reconcile._reconcile import ReconcilerForecaster
