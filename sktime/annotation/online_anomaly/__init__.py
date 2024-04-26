b9c8c689142c9aea7ac8717148ebd0e0c9e05612
duydl <duy.dole.00ece@gmail.com>
2024-04-26 11:32:46 +0700

    Add __init__ file in online anomaly

---
+++ sktime/annotation/online_anomaly/__init__.py
@@ -0,0 +1,8 @@
+"""Implements online anomaly detection."""
+
+__all__ = ["OnlineBayesCPD", "StudentTDistribution"]
+
+from sktime.annotation.online_anomaly.online_bayes_cpd import (
+    OnlineBayesCPD,
+    StudentTDistribution,
+)
