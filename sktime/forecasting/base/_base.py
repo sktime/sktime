0e7e22aaeb22b230458657eaccdf98f15cb39fb8
Xinyu-Wu-0000 <xinyu.wuxinyu@outlook.com>
2024-05-27 09:08:59 +0800

    self._y in predict if y=None

--- sktime/forecasting/base/_base.py
+++ sktime/forecasting/base/_base.py
@@ -2596,7 +2596,8 @@ class BaseGlobalForecaster(BaseForecaster):
         """
         # handle inputs
         self.check_is_fitted()
-
+        if y is None:
+            y = deepcopy(self._y)
         # check and convert X/y
         X_inner, y_inner = self._check_X_y(X=X, y=y)
 
