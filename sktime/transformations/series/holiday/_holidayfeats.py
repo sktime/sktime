e1415c0f377d8e617c769efe4d33c25fcb76da48
hirwa <hirwanshutiflx@gmail.com>
2024-06-05 23:55:13 +0530

    loops pruning and proper warning

--- sktime/transformations/series/holiday/_holidayfeats.py
+++ sktime/transformations/series/holiday/_holidayfeats.py
@@ -6,6 +6,7 @@ __author__ = ["mloning", "VyomkeshVyas"]
 __all__ = ["HolidayFeatures"]
 
 import datetime
+import warnings
 from collections import defaultdict
 from datetime import date
 from typing import Dict
@@ -306,14 +307,17 @@ def _generate_holidays(
             if name in holidays_by_name:
                 dates = holidays_by_name[name]
             else:
-                raise ValueError(f"holiday: {name} not found in calendar.")
+                warnings.warn(
+                    message=f"Holiday '{name}' not found in calendar. Skipping.",
+                    stacklevel=2,
+                )
 
+            # We then get the number of days before and after
+            # the holiday.
+            before, after = window
+            neg_before = -before
             # For each holiday, we iterate over dates.
             for dte in dates:
-                # We then get the number of days before and after
-                # the holiday.
-                before, after = window
-
                 # Finally, we add all days within the window to the
                 # holiday, making sure that we do not overwrite
                 # already existing holidays.
@@ -322,19 +326,14 @@ def _generate_holidays(
                     "are not overlapping with other holidays."
                 )
 
-                for days in range(1, before + 1):
-                    date_before = dte - datetime.timedelta(days=days)
-                    if date_before not in holidays_by_date:
-                        holidays_by_date[date_before] = name
+                for days in range(neg_before, after + 1):
+                    date_window = dte + datetime.timedelta(days=days)
+                    if date_window not in holidays_by_date:
+                        holidays_by_date[date_window] = name
+                    elif holidays_by_date[date_window] != name:
+                        holidays_by_date[date_window] = f", {name}"
                     else:
-                        raise ValueError(msg)
-
-                for days in range(1, after + 1):
-                    date_after = dte + datetime.timedelta(days=days)
-                    if date_after not in holidays_by_date:
-                        holidays_by_date[date_after] = name
-                    else:
-                        raise ValueError(msg)
+                        warnings.warn(message=f"{msg}", stacklevel=2)
 
     if include_bridge_days:
         # Iterate over holidays.
