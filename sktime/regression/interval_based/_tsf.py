d31c0868ec245845b657a90983ba04e4d4f3c36d
ksharma6 <ksharma6@tutanota.com>
2024-06-10 10:13:29 -0700

    Added self.criterion to __init__ to satisfy sklearn BaseForest requirement

--- sktime/regression/interval_based/_tsf.py
+++ sktime/regression/interval_based/_tsf.py
@@ -76,7 +76,14 @@ class TimeSeriesForestRegressor(BaseTimeSeriesForest, ForestRegressor, BaseRegre
     _tags = {
         # packaging info
         # --------------
-        "authors": ["TonyBagnall", "kkoziara", "luiszugasti", "kanand77", "mloning"],
+        "authors": [
+            "TonyBagnall",
+            "kkoziara",
+            "luiszugasti",
+            "kanand77",
+            "mloning",
+            "ksharma6",
+        ],
         "maintainers": ["kkoziara", "luiszugasti", "kanand77"],
         # estimator type
         # --------------
@@ -93,6 +100,9 @@ class TimeSeriesForestRegressor(BaseTimeSeriesForest, ForestRegressor, BaseRegre
         n_jobs=1,
         random_state=None,
     ):
+        self.criterion = "gini"  # needed for BaseForest in sklearn > 1.4.0,
+        # because sklearn tag logic looks at this attribute
+
         super().__init__(
             min_interval=min_interval,
             n_estimators=n_estimators,
