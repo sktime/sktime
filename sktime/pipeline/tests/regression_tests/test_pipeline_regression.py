552a79e0330212a2ead531baa14387c99c36fde6
Franz Király <f.kiraly@ucl.ac.uk>
2024-06-13 10:35:46 +0100

    [BUG] fix test failures introduced by diff test refactor (#6585)
    This fixes accidental failures introduced by #6563
    
    Causes:
    
    * the tests are not triggered to run if the tests themselve change (this
    PR triggers them by a change in a comment)
    * inocuous change of forecaster seems to have added coverage for an
    independent bug, see https://github.com/sktime/sktime/issues/6584

--- sktime/pipeline/tests/regression_tests/test_pipeline_regression.py
+++ sktime/pipeline/tests/regression_tests/test_pipeline_regression.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import pytest
-from skbase.utils.dependencies import _check_soft_dependencies
 from sklearn.linear_model import Ridge
 
 from sktime.classification.dummy import DummyClassifier
@@ -18,6 +17,7 @@ from sktime.transformations.series.difference import Differencer
 from sktime.transformations.series.exponent import ExponentTransformer
 from sktime.transformations.series.lag import Lag
 from sktime.utils._testing.hierarchical import _bottom_hier_datagen, _make_hierarchical
+from sktime.utils.dependencies import _check_soft_dependencies
 
 
 def test_transformer_regression():
