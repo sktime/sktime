9f30bf4e0a9b5ed7369cb9415818717b87c226eb
Franz Király <f.kiraly@ucl.ac.uk>
2024-06-15 13:32:35 +0100

    Update test_check_estimator.py

--- sktime/utils/tests/test_check_estimator.py
+++ sktime/utils/tests/test_check_estimator.py
@@ -6,12 +6,18 @@ import pytest
 
 from sktime.classification.dummy import DummyClassifier
 from sktime.forecasting.dummy import ForecastKnownValues
+from sktime.tests.test_switch import run_test_for_class, run_test_module_changed
 from sktime.transformations.series.exponent import ExponentTransformer
 from sktime.utils.estimator_checks import check_estimator
 
 EXAMPLE_CLASSES = [DummyClassifier, ForecastKnownValues, ExponentTransformer]
 
 
+@pytest.mark.skipif(
+    not run_test_module_changed(["sktime.utils", "sktime.tests"])
+    and not run_test_for_class(EXAMPLE_CLASSES),
+    reason="Run if check_estimator or TestAll classes have changed.",
+)
 @pytest.mark.parametrize("estimator_class", EXAMPLE_CLASSES)
 def test_check_estimator_passed(estimator_class):
     """Test that check_estimator returns only passed tests for examples we know pass.
@@ -54,6 +60,13 @@ def _check_none_failed_and_only_few_skipped(result):
     assert skip_ratio < 0.1
 
 
+@pytest.mark.skipif(
+    not run_test_module_changed(["sktime.utils", "sktime.tests"])
+    and not run_test_for_class(EXAMPLE_CLASSES),
+    reason="Run if check_estimator or TestAll classes have changed.",
+    # run_test_for_class will return True if TestAll has changed,
+    # because test classes impacting EXAMPLE_CLASSES have changed
+)
 @pytest.mark.parametrize("estimator_class", EXAMPLE_CLASSES)
 def test_check_estimator_does_not_raise(estimator_class):
     """Test that check_estimator does not raise exceptions on examples we know pass."""
@@ -64,6 +77,13 @@ def test_check_estimator_does_not_raise(estimator_class):
     check_estimator(estimator_instance, raise_exceptions=True, verbose=False)
 
 
+@pytest.mark.skipif(
+    not run_test_module_changed(["sktime.utils", "sktime.tests"])
+    and not run_test_for_class(ExponentTransformer),
+    reason="Run if check_estimator or TestAll classes have changed.",
+    # run_test_for_class will return True if TestAll has changed,
+    # because it is a test class impacting ExponentTransformer
+)
 def test_check_estimator_subset_tests():
     """Test that subsetting by tests_to_run and tests_to_exclude works as intended."""
     tests_to_run = [
