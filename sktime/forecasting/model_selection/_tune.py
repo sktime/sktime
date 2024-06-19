6043073fb2c41b644febbc58a17fb04d90565146
Miguel Krause <miguelkrause@LaptopvonMiguel.fritz.box>
2024-06-19 16:36:22 +0200

    feat: add doc strings with example and tags

--- sktime/forecasting/model_selection/_tune.py
+++ sktime/forecasting/model_selection/_tune.py
@@ -1601,6 +1601,158 @@ def _fit_and_score_skopt(params, meta):
 
 
 class ForecastingOptunaSearchCV(BaseGridSearch):
+    """Search over hyperparameters over specified parameter distributions for a forecaster using Optuna.
+
+    Experimental: This feature is under development and interface may likely to change.
+
+    Parameters
+    ----------
+        scoring=None,
+        strategy="refit",
+        refit=True,
+        verbose=0,
+        return_n_best_forecasters=1,
+        backend="loky",
+        update_behaviour="full_refit",
+        error_score=np.nan,
+        n_evals=100,
+    
+    forecaster : sktime forecaster, BaseForecaster instance or interface compatible
+        The forecaster to tune, must implement the sktime forecaster interface.
+        sklearn regressors can be used, but must first be converted to forecasters
+        via one of the reduction compositors, e.g., via ``make_reduction``
+    cv : cross-validation generator or an iterable
+        Splitter used for generating validation folds.
+        e.g. ExpandingWindowSplitter()
+    param_grid : dict
+        Dictionary with parameters names as keys and lists of parameter distributions from which to sample parameter values.
+        e.g. {"forecaster": optuna.distributions.CategoricalDistribution(
+        (STLForecaster(), ThetaForecaster())}
+   scoring : sktime metric (BaseMetric), str, or callable, optional (default=None)
+        scoring metric to use in tuning the forecaster
+
+        * sktime metric objects (BaseMetric) descendants can be searched
+        with the ``registry.all_estimators`` search utility,
+        for instance via ``all_estimators("metric", as_dataframe=True)``
+
+        * If callable, must have signature
+        ``(y_true: 1D np.ndarray, y_pred: 1D np.ndarray) -> float``,
+        assuming np.ndarrays being of the same length, and lower being better.
+        Metrics in sktime.performance_metrics.forecasting are all of this form.
+
+        * If str, uses registry.resolve_alias to resolve to one of the above.
+          Valid strings are valid registry.craft specs, which include
+          string repr-s of any BaseMetric object, e.g., "MeanSquaredError()";
+          and keys of registry.ALIAS_DICT referring to metrics.
+
+        * If None, defaults to MeanAbsolutePercentageError()
+    strategy : {"refit", "update", "no-update_params"}, optional, default="refit"
+        data ingestion strategy in fitting cv, passed to ``evaluate`` internally
+        defines the ingestion mode when the forecaster sees new data when window expands
+        "refit" = a new copy of the forecaster is fitted to each training window
+        "update" = forecaster is updated with training window data, in sequence provided
+        "no-update_params" = fit to first training window, re-used without fit or update.
+    refit : bool, default=True
+        Refit an estimator using the best found parameters on the whole dataset.
+    verbose : int, default=0
+        Controls the verbosity: the higher, the more messages.
+    return_n_best_forecasters : int, default=1
+        Number of best forecasters to return.
+    backend : str, default="loky"
+        Backend to use when running the fit.
+    update_behaviour : str, default="full_refit"
+        Determines how to update the forecaster during fitting.
+    error_score : 'raise' or numeric, default=np.nan
+        Value to assign to the score if an error occurs in estimator fitting.
+    n_evals : int, default=100
+        Number of parameter settings that are sampled. n_iter trades
+        off runtime vs quality of the solution. 
+    
+    Attributes
+    ----------
+    best_index_ : int
+    best_score_: float
+        Score of the best model
+    best_params_ : dict
+        Best parameter values across the parameter grid
+    best_forecaster_ : estimator
+        Fitted estimator with the best parameters
+    cv_results_ : dict
+        Results from grid search cross validation
+    n_best_forecasters_: list of tuples ("rank", <forecaster>)
+        The "rank" is in relation to best_forecaster_
+    n_best_scores_: list of float
+        The scores of n_best_forecasters_ sorted from best to worst
+        score of forecasters
+    
+    Examples
+    --------
+
+    >>> from sktime.forecasting.model_selection import (
+    ...     ForecastingOptunaSearchCV,
+    ...     )
+    >>> from sktime.datasets import load_shampoo_sales
+    >>> import warnings
+    >>> warnings.simplefilter(action="ignore", category=FutureWarning)
+    >>> from sktime.forecasting.base import ForecastingHorizon
+    >>>from sktime.split import (
+    ...     ExpandingWindowSplitter,)
+    >>> from sktime.split import temporal_train_test_split
+    >>> from sklearn.preprocessing import MinMaxScaler, RobustScaler
+    >>> from sktime.forecasting.compose import TransformedTargetForecaster
+    >>> from sktime.transformations.series.adapt import TabularToSeriesAdaptor
+    >>> from sktime.transformations.series.detrend import Deseasonalizer, Detrender
+    >>> from sktime.forecasting.naive import NaiveForecaster
+    >>> from sktime.forecasting.theta import ThetaForecaster
+    >>> from sktime.forecasting.trend import STLForecaster
+    >>> import optuna
+
+    >>> y = load_shampoo_sales()
+    >>> y_train, y_test = temporal_train_test_split(y=y, test_size=6)
+    >>> fh = ForecastingHorizon(y_test.index, is_relative=False).to_relative(
+    >>> cutoff=y_train.index[-1]
+    ...     )
+    >>> cv = ExpandingWindowSplitter(fh=fh, initial_window=24, step_length=1)
+    >>> forecaster = TransformedTargetForecaster(
+    >>> steps=[
+    ...     ("detrender", Detrender()),
+    ...     ("deseasonalizer", Deseasonalizer()),
+    ...     ("scaler", TabularToSeriesAdaptor(RobustScaler())),
+    ...     ("minmax2", TabularToSeriesAdaptor(MinMaxScaler((1, 10)))),
+    ...     ("forecaster", NaiveForecaster()),
+    ...     ]
+    ...     )
+    >>> param_grid = {
+    ...     "scaler__transformer__with_scaling": optuna.distributions.CategoricalDistribution(
+    ...          (True, False)
+    ...     ),
+    "forecaster": optuna.distributions.CategoricalDistribution(
+    ...     (STLForecaster(), ThetaForecaster())
+    ...     ),
+    ...     }
+    >>> gscv = ForecastingOptunaSearchCV(
+    ...     forecaster=forecaster,
+    ...     param_grid=param_grid,
+    ...     cv=cv,
+    ...     n_evals=10,
+    ...     )
+    >>> gscv.fit(y)
+    >>> print(f"{gscv.best_params_=}")
+    """
+
+    _tags = {
+        "authors": ["gareth-brown-86","mk406"],
+        "maintainers": ["gareth-brown-86","mk406"],
+        "scitype:y": "both",
+        "requires-fh-in-fit": False,
+        "handles-missing-data": False,
+        "ignores-exogeneous-X": True,
+        "capability:pred_int": True,
+        "capability:pred_int:insample": True,
+        "python_dependencies": ["optuna"],
+        "python_version": ">= 3.6",
+    }
+    
     def __init__(
         self,
         forecaster,
