8d9f77c0469f4fe2f6a808f3acf034cfa8857d06
julian fong <cyfongj@gmail.com>
2024-06-19 17:27:10 -0400

    updates

--- sktime/forecasting/hf_momentfm_forecaster.py
+++ sktime/forecasting/hf_momentfm_forecaster.py
@@ -1,1 +1,19 @@
 """Interface for the momentfm deep learning time series forecaster."""
+
+# from skbase.utils.dependencies import _check_soft_dependencies
+
+from sktime.forecasting.base import BaseForecaster  # , ForecastingHorizon
+
+# if _check_soft_dependencies("momentfm", severity="none"):
+#     import momemtfm
+
+
+__author__ = ["julian-fong"]
+
+
+class MomentFMForecaster(BaseForecaster):
+    """
+    Interface for forecasting with the deep learning time series model momentfm.
+
+    MomentFM is a collection of
+    """
