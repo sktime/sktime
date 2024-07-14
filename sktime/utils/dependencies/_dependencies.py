0f35d47871dd6bb233f0d6ca38182e5903cc0efb
Franz Király <f.kiraly@ucl.ac.uk>
2024-07-14 22:06:10 +0100

    Reapply "Update _dependencies.py"
    This reverts commit 6da976663052188eb360f0d3a72fcc0e81bada4b.

--- sktime/utils/dependencies/_dependencies.py
+++ sktime/utils/dependencies/_dependencies.py
@@ -285,7 +285,9 @@ def _get_installed_packages_private():
     by accident.
     """
     result = subprocess.run(  # noqa: S603
-        ["pip", "list", "--format=json"], capture_output=True, text=True  # noqa: S607
+        ["pip", "list", "--format=json"],  # noqa: S607
+        capture_output=True,
+        text=True,
     )
     packages = {}
     if result.returncode == 0:
