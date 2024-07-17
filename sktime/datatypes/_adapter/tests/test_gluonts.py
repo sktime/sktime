d3e568e854c0af0d8fc76f78ea068d2ee34b3732
Shlok Sabarwal <34071276+shlok191@users.noreply.github.com>
2024-07-17 14:54:22 -0700

    [ENH] Adding support for GluonTS' PandasDataset object (#6668)
    #### Reference Issues/PRs
    Fixes #6658 
    
    #### What does this implement/fix? Explain your changes.
    This implementation adds conversion functions to easily deal with
    `pd.DataFrame` of the `long` and `long` varieties in addition to lists
    and dictionaries of the same to `gluonTS PandasDataset` objects. This
    also includes the conversion of `pd.Series` to `gluonTS PandasDataset`
    objects.

--- sktime/datatypes/_adapter/tests/test_gluonts.py
+++ sktime/datatypes/_adapter/tests/test_gluonts.py
@@ -2,7 +2,12 @@ import numpy as np
 import pandas as pd
 import pytest
 
-from sktime.datatypes._adapter.gluonts import convert_pandas_to_listDataset
+from sktime.datatypes._adapter.gluonts import (
+    convert_pandas_collection_to_pandasDataset,
+    convert_pandas_multiindex_to_pandasDataset,
+    convert_pandas_series_to_pandasDataset,
+    convert_pandas_to_listDataset,
+)
 from sktime.utils.dependencies import _check_soft_dependencies
 
 
@@ -49,3 +54,59 @@ def test_pandas_to_ListDataset(pandas_df):
     for _, group_data in pandas_df.groupby(level=0):
         np.testing.assert_allclose(group_data.values, generated_list[idx]["target"])
         idx += 1
+
+
+@pytest.mark.skipif(
+    not _check_soft_dependencies("gluonts", severity="none"),
+    reason="skip test if required soft dependency for GluonTS not available",
+)
+@pytest.mark.parametrize(
+    "pandas_obj, conversion_function",
+    [
+        (
+            pd.DataFrame(
+                {
+                    "series_id": ["A", "A", "A", "B", "B", "B", "C", "C", "C"],
+                    "timepoints": pd.date_range("2022-01-01", periods=9, freq="D"),
+                    "target": np.random.randn(9),
+                },
+            )
+            .set_index(["series_id", "timepoints"])
+            .sort_index(),
+            convert_pandas_multiindex_to_pandasDataset,
+        ),
+        (
+            {
+                "A": pd.DataFrame(
+                    {
+                        "timestamp": pd.date_range("2022-01-01", periods=50, freq="D"),
+                        "target": np.random.randn(50),
+                    }
+                ),
+                "B": pd.DataFrame(
+                    {
+                        "timestamp": pd.date_range("2022-01-01", periods=50, freq="D"),
+                        "target": np.random.randn(50),
+                    }
+                ),
+                "C": pd.DataFrame(
+                    {
+                        "timestamp": pd.date_range("2022-01-01", periods=50, freq="D"),
+                        "target": np.random.randn(50),
+                    }
+                ),
+            },
+            convert_pandas_collection_to_pandasDataset,
+        ),
+        (
+            pd.Series(
+                np.random.randn(50),
+                index=pd.date_range("2022-01-01", periods=50, freq="D"),
+            ),
+            convert_pandas_series_to_pandasDataset,
+        ),
+    ],
+)
+def test_pandas_df_to_PandasDataset(pandas_obj, conversion_function):
+    # Attempting to convert the pandas object to a gluonTS PandasDataset
+    conversion_function(pandas_obj)
