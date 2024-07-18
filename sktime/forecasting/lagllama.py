f27b535c6fad2900da76f25bd3a21350aac08991
Shlok Sabarwal <shlok190303@gmail.com>
2024-07-17 23:20:39 -0700

    added test params for LagLlama

--- sktime/forecasting/lagllama.py
+++ sktime/forecasting/lagllama.py
@@ -8,7 +8,7 @@ import subprocess
 from sktime.forecasting.base import _BaseGlobalForecaster
 
 
-class _LagLlamaAdapter(_BaseGlobalForecaster):
+class LagLlamaForecaster(_BaseGlobalForecaster):
     """Base class that interfaces the LagLlama forecaster.
 
     Parameters
@@ -336,47 +336,17 @@ class _LagLlamaAdapter(_BaseGlobalForecaster):
             `MyClass(**params)` or `MyClass(**params[i])` creates a valid test instance.
             `create_test_instance` uses the first (or only) dictionary in `params`
         """
+        params = [
+            {
+                "device": "cpu",
+                "context_length": 32,
+                "prediction_length": 100,
+                "num_samples": 10,
+                "batch_size": 32,
+                "nonnegative_pred_samples": False,
+                "lr": 5e-5,
+                "trainer_kwargs": {"num_epochs": 10},
+            }
+        ]
 
-        # todo: set the testing parameters for the estimators
-        # Testing parameters can be dictionary or list of dictionaries
-        # Testing parameter choice should cover internal cases well.
-        #
-        # this method can, if required, use:
-        #   class properties (e.g., inherited); parent class test case
-        #   imported objects such as estimators from sktime or sklearn
-        # important: all such imports should be *inside get_test_params*, not at the top
-        #            since imports are used only at testing time
-        #
-        # The parameter_set argument is not used for automated, module level tests.
-        #   It can be used in custom, estimator specific tests, for "special" settings.
-        # A parameter dictionary must be returned *for all values* of parameter_set,
-        #   i.e., "parameter_set not available" errors should never be raised.
-        #
-        # A good parameter set should primarily satisfy two criteria,
-        #   1. Chosen set of parameters should have a low testing time,
-        #      ideally in the magnitude of few seconds for the entire test suite.
-        #       This is vital for the cases where default values result in
-        #       "big" models which not only increases test time but also
-        #       run into the risk of test workers crashing.
-        #   2. There should be a minimum two such parameter sets with different
-        #      sets of values to ensure a wide range of code coverage is provided.
-        #
-        # example 1: specify params as dictionary
-        # any number of params can be specified
-        # params = {"est": value0, "parama": value1, "paramb": value2}
-        #
-        # example 2: specify params as list of dictionary
-        # note: Only first dictionary will be used by create_test_instance
-        # params = [{"est": value1, "parama": value2},
-        #           {"est": value3, "parama": value4}]
-        # return params
-        #
-        # example 3: parameter set depending on param_set value
-        #   note: only needed if a separate parameter set is needed in tests
-        # if parameter_set == "special_param_set":
-        #     params = {"est": value1, "parama": value2}
-        #     return params
-        #
-        # # "default" params - always returned except for "special_param_set" value
-        # params = {"est": value3, "parama": value4}
-        # return params
+        return params
