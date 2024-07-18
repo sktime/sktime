09471bd207a4a331cb174e1a0db3356ae81f718c
Sultan Orazbayev <contact@econpoint.com>
2024-07-18 11:26:08 -0400

    Add tests and fix the reference to .levels.

--- sktime/utils/validation/tests/test_forecasting.py
+++ sktime/utils/validation/tests/test_forecasting.py
@@ -9,7 +9,10 @@ import pandas as pd
 import pytest
 from pytest import raises
 
+from sktime.forecasting.var import VAR
 from sktime.tests.test_switch import run_test_for_class
+from sktime.utils.dependencies import _check_soft_dependencies
+from sktime.utils.plotting import plot_series
 from sktime.utils.validation.forecasting import check_fh
 
 empty_input = (np.array([], dtype=int), [], pd.Index([], dtype=int))
@@ -24,3 +27,30 @@ def test_check_fh_empty_input(arg):
     """Test that fh validation throws an error with empty container."""
     with raises(ValueError, match="`fh` must not be empty"):
         check_fh(arg)
+
+
+def test_plotting_dataframe_with_unused_levels():
+    """After subsetting, pd.DataFrame keeps information about unused levels."""
+
+    _check_soft_dependencies("matplotlib")
+
+    import matplotlib
+    import matplotlib.pyplot as plt
+
+    y_train = pd.DataFrame(np.random.rand(100, 3), columns=["x1", "x2", "x3"])
+    forecaster = VAR(maxlags=1)
+    forecaster.fit(y_train)
+    y_pred = forecaster.predict(fh=range(1, 10))
+    coverage = 0.9
+    y_pred_ints1 = forecaster.predict_interval(coverage=coverage)
+    variable_to_plot = "x1"
+    pred_interval = y_pred_ints1[[variable_to_plot]].copy()
+
+    matplotlib.use("agg")
+    plot_series(
+        y_train[[variable_to_plot]],
+        y_pred[[variable_to_plot]],
+        pred_interval=pred_interval,
+    )
+    plt.gcf().canvas.draw_idle()
+    plt.close()
