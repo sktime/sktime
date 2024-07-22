ff5bc679c24d7374a558f4114314a683feaefd0c
Pranav <pranavprajapati586@gmail.com>
2024-07-22 20:30:10 +0530

    Support loading of newest weights

--- sktime/forecasting/moirai_forecaster.py
+++ sktime/forecasting/moirai_forecaster.py
@@ -4,17 +4,8 @@ import pandas as pd
 
 from sktime.utils.dependencies import _check_soft_dependencies
 
-if _check_soft_dependencies("torch", severity="none"):
-    import torch
-
 if _check_soft_dependencies("gluonts", severity="none"):
     from gluonts.dataset.pandas import PandasDataset
-
-if _check_soft_dependencies("uni2ts", severity="none"):
-    from uni2ts.model.moirai import MoiraiForecast
-
-if _check_soft_dependencies("huggingface_hub", severity="none"):
-    from huggingface_hub import hf_hub_download
 
 from sktime.forecasting.base import _BaseGlobalForecaster
 
@@ -74,11 +65,12 @@ class MOIRAIForecaster(_BaseGlobalForecaster):
         "enforce_index_type": None,
         "handles-missing-data": False,
         "capability:pred_int": False,
-        "python_dependencies": ["uni2ts", "gluonts", "torch"],
+        "python_dependencies": ["salesforce-uni2ts", "gluonts", "torch"],
         "X_inner_mtype": "pd.DataFrame",
         "y_inner_mtype": "pd.DataFrame",
         "capability:insample": False,
         "capability:pred_int:insample": False,
+        "python_dependencies_alias": {"salesforce-uni2ts": "uni2ts"},
     }
 
     def __init__(
@@ -102,42 +94,53 @@ class MOIRAIForecaster(_BaseGlobalForecaster):
         self.deterministic = deterministic
         self.batch_size = batch_size
 
-    def _fit(self, y, X, fh):
-        # Load model and extract config
+    def _instantiate_model(self, ds, fh):
+
+        from uni2ts.model.moirai import MoiraiForecast
 
         if fh is not None:
             prediction_length = max(fh.to_relative(self.cutoff))
         else:
             prediction_length = 1
 
-        if X is not None:
-            df = pd.concat([y, X], axis=1)
-            target = y.columns
-            feat_dynamic_real = X.columns
+        # load the sktime moirai weights by default
+        model_kwargs = {
+            "prediction_length": prediction_length,
+            "context_length": self.context_length,
+            "patch_size": self.patch_size,
+            "num_samples": self.num_samples,
+            "target_dim": 2,
+            "feat_dynamic_real_dim": ds.num_feat_dynamic_real,
+            "past_feat_dynamic_real_dim": ds.num_past_feat_dynamic_real,
+        }
+
+        # Instantiate model with latest salesforce weights
+        if self.checkpoint_path.startswith("Salesforce"):
+            from uni2ts.model.moirai import MoiraiModule
+
+            model_kwargs["module"] = MoiraiModule.from_pretrained(self.checkpoint_path)
+
+            return MoiraiForecast(**model_kwargs)
+
         else:
-            df = y
-            target = y.columns
-            feat_dynamic_real = None
-        ds = PandasDataset(df, target=target, feat_dynamic_real=feat_dynamic_real)
+            from huggingface_hub import hf_hub_download
 
-        self.model = MoiraiForecast.load_from_checkpoint(
-            checkpoint_path=hf_hub_download(
+            model_kwargs["checkpoint_path"] = hf_hub_download(
                 repo_id=self.checkpoint_path, filename="model.ckpt", revision="temp"
-            ),
-            prediction_length=prediction_length,
-            context_length=self.context_length,
-            patch_size=self.patch_size,
-            num_samples=self.num_samples,
-            target_dim=2,
-            feat_dynamic_real_dim=ds.num_feat_dynamic_real,
-            past_feat_dynamic_real_dim=ds.num_past_feat_dynamic_real,
-            map_location="cuda:0" if torch.cuda.is_available() else "cpu",
-        )
+            )
 
+            return MoiraiForecast.load_from_checkpoint(**model_kwargs)
+
+    def _fit(self, y, X, fh):
+        # Load model and extract config
+        ds = self._get_pandas_dataset(y, X)
+        self.model = self._instantiate_model(ds, fh)
         self.model.to(self.map_location)
 
     def _predict(self, fh, y=None, X=None):
         if self.deterministic:
+            import torch
+
             torch.manual_seed(42)
 
         if fh is None:
@@ -157,9 +160,9 @@ class MOIRAIForecaster(_BaseGlobalForecaster):
             self.cutoff[0], periods=max(fh._values) + 1, freq=self._y.index.freq
         )[1:]
 
-        df_y = pd.concat(
-            [self._y, pd.DataFrame(columns=self._y.columns, index=pred_index)]
-        )
+        y = pd.DataFrame(columns=self._y.columns, index=pred_index)
+        y.fillna(0, inplace=True)
+        df_y = pd.concat([self._y, y], axis=0)
         if X is not None:
             df_x = pd.concat([self._X, X], axis=0)
             df_test = pd.concat([df_y, df_x], axis=1)
@@ -179,7 +182,27 @@ class MOIRAIForecaster(_BaseGlobalForecaster):
         forecast_it = iter(forecasts)
         forecast = next(forecast_it)
 
-        return forecast.mean_ts.to_timestamp().loc[fh.to_absolute(self.cutoff)._values]
+        # return forecast.mean_ts.to_timestamp().
+        # loc[fh.to_absolute(self.cutoff)._values]
+        return forecast.mean_ts
+
+    # def get_prediction_df(self):
+
+    def _get_pandas_dataset(self, y, X):
+        if X is not None:
+            df = pd.concat([y, X], axis=1)
+            target = y.columns
+            feat_dynamic_real = X.columns
+        else:
+            df = y
+            target = y.columns
+            feat_dynamic_real = None
+        return PandasDataset(
+            df,
+            target=target,
+            feat_dynamic_real=feat_dynamic_real,
+            freq=self._y.index.freq,
+        )
 
     @classmethod
     def get_test_params(cls, parameter_set="default"):
