1f29baa3277f11a52b1787b90e9218892a7660ef
geetu040 <raoarmaghanshakir040@gmail.com>
2024-07-26 18:49:25 +0200

    implement basic predict. [no ci]

--- sktime/forecasting/compose/_autoregressive.py
+++ sktime/forecasting/compose/_autoregressive.py
@@ -1,7 +1,12 @@
 # copyright: sktime developers, BSD-3-Clause License (see LICENSE file)
 """AutoRegressiveWrapper for Forecasters with fixed prediction length."""
 
-from sktime.forecasting.base import _BaseGlobalForecaster
+__author__ = ["geetu040"]
+
+import pandas as pd
+
+from sktime.forecasting.base import ForecastingHorizon, _BaseGlobalForecaster
+from sktime.utils.dependencies import _check_soft_dependencies
 
 
 class AutoRegressiveWrapper(_BaseGlobalForecaster):
@@ -21,13 +26,16 @@ class AutoRegressiveWrapper(_BaseGlobalForecaster):
             "pd-multiindex",
             "pd_multiindex_hier",
         ],
-        "ignores-exogeneous-X": False,
-        "requires-fh-in-fit": True,  # TODO: look at this
+        "ignores-exogeneous-X": True,  # TODO: look at this
+        "requires-fh-in-fit": False,
+        "capability:insample": False,
         "capability:global_forecasting": True,
     }
 
-    def __init__(self, forecaster, aggregate_method=None):
+    def __init__(self, forecaster, horizon_length, aggregate_method=None):
         self.forecaster = forecaster
+        self.horizon_length = horizon_length
+        # TODO: check the need for this
         self.aggregate_method = (
             aggregate_method
             if aggregate_method is not None
@@ -39,10 +47,34 @@ class AutoRegressiveWrapper(_BaseGlobalForecaster):
         super().__init__()
 
     def _fit(self, y, X=None, fh=None):
-        self.forecaster.fit(y=y, X=X, fh=fh)
+        # ignore the fh provided fit
+        _fh = ForecastingHorizon(range(1, self.horizon_length))
+
+        self.forecaster.fit(y=y, X=X, fh=_fh)
 
     def _predict(self, fh, X=None, y=None):
-        pass
+        # use fh to find the maximum length to forecast regressively
+        max_fh = max(
+            *(fh.to_relative(self._cutoff)._values + 1),
+            self.horizon_length,
+        )
+
+        # minimum number of iterations needed to exceed forecast till max_fh
+        n_iterations = (max_fh + self.horizon_length - 1) // self.horizon_length
+
+        # regressive forecasting loop
+        y_ = self._y if y is None else y
+        for _ in range(n_iterations):
+            preds = self.forecaster.predict(y=y_)
+            y_ = pd.concat([y_, preds])
+            # TODO: update this concatenation for multi-index
+
+        # collect forecasting points from y_
+        absolute_horizons = fh.to_absolute_index(self.cutoff)
+        dateindex = y_.index.get_level_values(-1).map(lambda x: x in absolute_horizons)
+        preds = y_.loc[dateindex]
+
+        return preds
 
     @classmethod
     def get_test_params(cls, parameter_set="default"):
@@ -63,10 +95,23 @@ class AutoRegressiveWrapper(_BaseGlobalForecaster):
             `MyClass(**params)` or `MyClass(**params[i])` creates a valid test instance.
             `create_test_instance` uses the first (or only) dictionary in `params`
         """
-        # TODO: Fill this
         test_params = []
-        params_no_broadcasting = [
-            dict(p, **{"broadcasting": False}) for p in test_params
-        ]
-        test_params.extend(params_no_broadcasting)
+        if _check_soft_dependencies("pytorch-forecasting", severity="none"):
+            from sktime.forecasting.pytorchforecasting import PytorchForecastingTFT
+
+            test_params = [
+                {
+                    "forecaster": PytorchForecastingTFT(
+                        trainer_params={
+                            "max_epochs": 1,
+                            "limit_train_batches": 10,
+                        },
+                        dataset_params={
+                            "max_encoder_length": 3,
+                        },
+                        random_log_path=True,
+                    ),
+                    "horizon_length": 3,
+                }
+            ]
         return test_params
