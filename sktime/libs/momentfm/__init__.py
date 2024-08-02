9a3359b9c24870a09330323775d436319fca4288
julian fong <cyfongj@gmail.com>
2024-08-02 14:44:22 -0400

    added private lib for momentfm

---
+++ sktime/libs/momentfm/__init__.py
@@ -0,0 +1,44 @@
+"""Python implementations of momentfm forecaster.
+
+Unofficial fork of the ``momentfm`` package, maintained in ``sktime``.
+
+sktime migration: 2024, August
+
+Original authors: Mononito Goswami and Konrad Szafer and Arjun Choudhry and
+Yifu Cai and Shuo Li and Artur Dubrawski
+
+2013 and 2024 releases subject to following license:
+
+All code contained except that in pykalman/utils.py is released under the
+license below. All code in pykalman/utils.py is released under the license
+contained therein.
+
+New BSD License
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+  a. Redistributions of source code must retain the above copyright notice,
+     this list of conditions and the following disclaimer.
+  b. Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in the
+     documentation and/or other materials provided with the distribution.
+  c. Neither the name of Daniel Duckworth nor the names of
+     its contributors may be used to endorse or promote products
+     derived from this software without specific prior written
+     permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+DAMAGE.
+"""
