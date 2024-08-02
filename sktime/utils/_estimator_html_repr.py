6c5dcba622bc629dabdd899839adf738e8f36ba4
Mateusz <kasprowicz.mat@gmail.com>
2024-08-02 18:27:39 +0200

    Add _HTMLDocumentationLinkMixin to _estimator_html_repr.py

--- sktime/utils/_estimator_html_repr.py
+++ sktime/utils/_estimator_html_repr.py
@@ -6,6 +6,10 @@ from contextlib import closing
 from io import StringIO
 from string import Template
 
+from packaging.version import parse as parse_version
+
+from .. import __version__
+
 __author__ = ["RNKuhns"]
 
 
@@ -401,3 +405,83 @@ def _object_html_repr(base_object):
 
         html_output = out.getvalue()
         return html_output
+
+
+class _HTMLDocumentationLinkMixin:
+    """Mixin class allowing to generate a link to the API documentation.
+
+    This mixin relies on three attributes:
+    - `_doc_link_module`: it corresponds to the root module (e.g. `sklearn`). Using this
+      mixin, the default value is `sklearn`.
+    - `_doc_link_template`: it corresponds to the template used to generate the
+      link to the API documentation. Using this mixin, the default value is
+      `"https://sktime.net/en/{version_url}/api_reference/auto_generated/ # TODO: update
+      {modpath}.html"`.
+    - `_doc_link_url_param_generator`: it corresponds to a function that generates the
+      parameters to be used in the template when the estimator module and name are not
+      sufficient.
+
+    The method :meth:`_get_doc_link` generates the link to the API documentation for a
+    given estimator.
+
+    This useful provides all the necessary states for
+    :func:`sklearn.utils.estimator_html_repr` to generate a link to the API
+    documentation for the estimator HTML diagram.
+
+    Examples # TODO: update
+    --------
+    If the default values for `_doc_link_module`, `_doc_link_template` are not suitable,
+    then you can override them:
+    >>> from sklearn.base import BaseEstimator
+    >>> estimator = BaseEstimator()
+    >>> estimator._doc_link_template = "https://website.com/{single_param}.html"
+    >>> def url_param_generator(estimator):
+    ...     return {"single_param": estimator.__class__.__name__}
+    >>> estimator._doc_link_url_param_generator = url_param_generator
+    >>> estimator._get_doc_link()
+    'https://website.com/BaseEstimator.html'
+    """
+
+    _doc_link_module = "sktime"
+    _doc_link_url_param_generator = None
+
+    @property
+    def _doc_link_template(self):
+        sktime_version = parse_version(__version__).base_version
+        return getattr(
+            self,
+            "__doc_link_template",
+            (
+                f"https://www.sktime.net/en/v{sktime_version}"
+                "/api_reference/auto_generated/{modpath}.html"
+            ),
+        )
+
+    @_doc_link_template.setter
+    def _doc_link_template(self, value):
+        setattr(self, "__doc_link_template", value)
+
+    def _get_doc_link(self):
+        """Generate a link to the API documentation for a given base object.
+
+        This method generates the link to the estimator's documentation page
+        by using the template defined by the attribute `_doc_link_template`.
+
+        Returns
+        -------
+        url : str
+            The URL to the API documentation for this estimator. If the estimator does
+            not belong to module `_doc_link_module`, the empty string (i.e. `""`) is
+            returned.
+        """
+        if self.__class__.__module__.split(".")[0] != self._doc_link_module:
+            return ""
+
+        if self._doc_link_url_param_generator is None:
+            modclass = self.__class__
+            modpath = str(modclass)[8:-2]
+
+            return self._doc_link_template.format(modpath=modpath)
+        return self._doc_link_template.format(
+            **self._doc_link_url_param_generator(self)
+        )
