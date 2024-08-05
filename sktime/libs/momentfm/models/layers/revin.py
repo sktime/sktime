3f7ea905087789bc6f83a9942c90c05f2d1f0f47
julian fong <cyfongj@gmail.com>
2024-08-05 15:52:00 -0400

    added revin file

--- sktime/libs/momentfm/models/layers/revin.py
+++ sktime/libs/momentfm/models/layers/revin.py
@@ -1,27 +1,34 @@
+"""Layer revin file."""
+
 import torch
 from torch import nn
 
 
 def nanvar(tensor, dim=None, keepdim=False):
+    """Calculate the variance of all non-NaN elements."""
     tensor_mean = tensor.nanmean(dim=dim, keepdim=True)
     output = (tensor - tensor_mean).square().nanmean(dim=dim, keepdim=keepdim)
     return output
 
 
 def nanstd(tensor, dim=None, keepdim=False):
+    """Calculate the std of all non-NaN elements."""
     output = nanvar(tensor, dim=dim, keepdim=keepdim)
     output = output.sqrt()
     return output
 
 
 class RevIN(nn.Module):
+    """RevIN class."""
+
     def __init__(self, num_features: int, eps: float = 1e-5, affine: bool = False):
-        """
+        """Initialize RevIN layer.
+
         :param num_features: the number of features or channels
         :param eps: a value added for numerical stability
         :param affine: if True, RevIN has learnable affine parameters
         """
-        super(RevIN, self).__init__()
+        super().__init__()
         self.num_features = num_features
         self.eps = eps
         self.affine = affine
@@ -30,7 +37,8 @@ class RevIN(nn.Module):
             self._init_params()
 
     def forward(self, x: torch.Tensor, mode: str = "norm", mask: torch.Tensor = None):
-        """
+        """Forward Function.
+
         :param x: input tensor of shape (batch_size, n_channels, seq_len)
         :param mode: 'norm' or 'denorm'
         :param mask: input mask of shape (batch_size, seq_len)
@@ -51,7 +59,8 @@ class RevIN(nn.Module):
         self.affine_bias = nn.Parameter(torch.zeros(1, self.num_features, 1))
 
     def _get_statistics(self, x, mask=None):
-        """
+        """Get statistics.
+
         x    : batch_size x n_channels x seq_len
         mask : batch_size x seq_len
         """
