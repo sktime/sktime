8c228a8c52d3afe62ba1be21479e13510dc5c0b6
julian fong <cyfongj@gmail.com>
2024-08-05 11:01:38 -0400

    updated licensing and renamed file

--- sktime/libs/momentfm/__init__.py
+++ sktime/libs/momentfm/__init__.py
@@ -1,44 +1,35 @@
-"""Python implementations of momentfm forecaster.
+"""Temporary python implementation of momentfm forecaster.
 
 Unofficial fork of the ``momentfm`` package, maintained in ``sktime``.
+For more details, please visit https://github.com/moment-timeseries-foundation-model/moment/tree/main
 
 sktime migration: 2024, August
 
 Original authors: Mononito Goswami and Konrad Szafer and Arjun Choudhry and
 Yifu Cai and Shuo Li and Artur Dubrawski
 
-2013 and 2024 releases subject to following license:
+2024 releases subject to following license:
 
-All code contained except that in pykalman/utils.py is released under the
-license below. All code in pykalman/utils.py is released under the license
-contained therein.
+All momentfm code contained is released under the license below.
 
-New BSD License
+MIT License
 
-All rights reserved.
+Copyright (c) 2024 Auton Lab, Carnegie Mellon University
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
 
-  a. Redistributions of source code must retain the above copyright notice,
-     this list of conditions and the following disclaimer.
-  b. Redistributions in binary form must reproduce the above copyright
-     notice, this list of conditions and the following disclaimer in the
-     documentation and/or other materials provided with the distribution.
-  c. Neither the name of Daniel Duckworth nor the names of
-     its contributors may be used to endorse or promote products
-     derived from this software without specific prior written
-     permission.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
-ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
-OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
-DAMAGE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
