efc591925b5f5c6dfa9f6234be4b70fc3ca16d52
Ankit-1204 <ankit2003upadhyay@gmail.com>
2024-08-09 18:22:43 +0530

    Fixed mishandling of pd.Series

--- sktime/annotation/clust.py
+++ sktime/annotation/clust.py
@@ -72,9 +72,9 @@ class ClusterSegmenter(BaseSeriesAnnotator):
         ------------
         creates fitted model (attributes ending in "_")
         """
-        cloned_clusterer = clone(self._clusterer)
         if isinstance(X, pd.Series):
             X = X.to_frame()
+        cloned_clusterer = clone(self._clusterer)
         self.n_instances, self.n_timepoints = X.shape
         X_flat = X.values.reshape(-1, 1)
         cloned_clusterer.fit(X_flat)
