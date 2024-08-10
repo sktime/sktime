44e0c6990a7628795bec6bfbfd96a79358ac5fa1
Franz Király <f.kiraly@ucl.ac.uk>
2024-08-10 15:25:16 +0100

    Update t.py

--- sktime/proba/t.py
+++ sktime/proba/t.py
@@ -5,4 +5,9 @@ __author__ = ["Alex-JG3", "ivarzap"]
 
 __all__ = ["TDistribution"]
 
-from skpro.distributions.t import TDistribution
+from sktime.utils.dependencies import _check_soft_dependencies
+
+if _check_soft_dependencies("skpro", severity="none"):
+    from skpro.distributions.t import TDistribution
+else:
+    from sktime.proba._t import TDistribution
