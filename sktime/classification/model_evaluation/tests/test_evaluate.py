d49ce8543b172df183d0b04cc35bf4d8dcd17e06
ksharma6 <ksharma6@tutanota.com>
2024-08-13 15:31:44 -0700

    First commit of tests for `evaluate`

---
+++ sktime/classification/model_evaluation/tests/test_evaluate.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3 -u
+# copyright: sktime developers, BSD-3-Clause License (see LICENSE file)
+"""Tests for model classification module.
+
+In particular, function `evaluate`, that performs time series cross-validation, is
+tested with various configurations for correct output.
+"""
+
+__author__ = ["ksharma6"]
