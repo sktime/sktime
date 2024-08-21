92cbb9ec4aeea8e18543651ecb33501793f5684e
geetu040 <raoarmaghanshakir040@gmail.com>
2024-08-21 20:53:39 +0200

    update examples, docstring and informative messages

--- sktime/forecasting/compose/_autoregressive.py
+++ sktime/forecasting/compose/_autoregressive.py
@@ -40,14 +40,27 @@ class AutoRegressiveWrapper(_BaseGlobalForecaster):
     Attributes
     ----------
     forecaster : _BaseGlobalForecaster
-        An instance of a forecasting model that is compatible with
-        the `AutoRegressiveWrapper`. This forecaster must support
-        the methods required for fitting and predicting.
-    # TODO: None
-    horizon_length : int
-        The length of the forecasting horizon, defining how many time
-        steps into the future the forecaster should predict with each
-        call to the `predict` method.
+        An instance of a global forecasting model.
+        Wrapping a forecaster with this class is beneficial when:
+        - Variable Forecasting Horizons: The forecasting task requires
+          predictions over a varying number of future time steps, which
+          the underlying forecaster cannot handle natively.
+        - Training Efficiency: The forecaster is computationally expensive
+          to train, especially when the output size increases. By using
+          the autoregressive approach, the model can be trained more efficiently
+          on smaller output sizes while still producing long-horizon forecasts.
+    horizon_length : int, default=None
+        Specifies the number of time steps into the future that the forecaster
+        should predict in each call to the `predict` method.
+        - If set to an integer value, this determines the fixed length of the
+          forecasting horizon.
+        - If set to None (the default), the `horizon_length` is automatically
+          determined based on the provided `fh` (forecasting horizon) during the
+          fitting process. The maximum value in `fh` will be used as the
+          `horizon_length`.
+        Note: If `horizon_length` is None and `fh` is not provided during the
+        fitting stage, a `ValueError` will be raised. In such cases, you must
+        either specify `horizon_length` or provide a valid `fh` when calling `fit`.
     aggregate_method : callable, default=numpy.mean
         A method for aggregating predictions over the autoregressive
         iterations. If not specified, defaults to `numpy.mean`.
@@ -56,92 +69,84 @@ class AutoRegressiveWrapper(_BaseGlobalForecaster):
     -------
     >>> import numpy as np
     >>> from sktime.datasets import load_airline
-    >>> from sktime.forecasting.pytorchforecasting import PytorchForecastingTFT
+    >>> from sktime.forecasting.pytorchforecasting import PytorchForecastingNBeats
     >>> from sktime.forecasting.compose import AutoRegressiveWrapper
+    >>> from sktime.utils.plotting import plot_series
     >>>
+    >>> # prepare data
     >>> y = load_airline()
-    >>> forecaster = PytorchForecastingTFT(trainer_params={
-    ...     "max_epochs": 5,
+    >>>
+    >>> # create forecaster and wrapper
+    >>> forecaster = PytorchForecastingNBeats(trainer_params={
+    ...     "max_epochs": 20,
     ... }) # doctest: +SKIP
     >>> wrapper = AutoRegressiveWrapper(
     ...     forecaster=forecaster,
-    ...     horizon_length=3, # prediction_length for forecaster
+    ...     horizon_length=5, # prediction_length for forecaster
     ...     aggregate_method=np.mean,
     ... ) # doctest: +SKIP
-    >>>
     >>> wrapper.fit(y) # doctest: +SKIP
     >>>
     >>> # forecast multiple forecasting horizons with same trained model
-    >>> preds = wrapper.predict(fh=[1, 5]) # doctest: +SKIP
-    >>> preds = wrapper.predict(fh=[8, 9, 10, 11, 12]) # doctest: +SKIP
+    >>> y_pred = wrapper.predict(fh=[1, 5]) # doctest: +SKIP
+    >>> y_pred = wrapper.predict(fh=[8, 9, 10, 11, 12]) # doctest: +SKIP
+    >>> y_pred = wrapper.predict(fh=list(range(1, 30))) # doctest: +SKIP
+    >>>
+    >>> # plot the forecasts
+    >>> plot_series(y, y_pred) # doctest: +SKIP
 
     >>> import numpy as np
-    >>> from sktime.forecasting.pytorchforecasting import PytorchForecastingTFT
+    >>> from sktime.forecasting.pytorchforecasting import PytorchForecastingDeepAR
     >>> from sktime.forecasting.compose import AutoRegressiveWrapper
     >>> from sktime.utils._testing.hierarchical import _make_hierarchical
     >>> from sklearn.model_selection import train_test_split
+    >>> from sktime.utils.plotting import plot_series
     >>>
     >>> # generate and prepare random data
+    >>> max_pred_len = 20
     >>> data = _make_hierarchical(
-    ...     hierarchy_levels=(5, 200), max_timepoints=50, min_timepoints=50, n_columns=3
+    ...     hierarchy_levels=(5, 100),
+    ...     max_timepoints=100,
+    ...     min_timepoints=100,
+    ...     n_columns=3
     ... )
-    >>> x = data[["c0", "c1"]]
+    >>> X = data[["c0", "c1"]]
     >>> y = data["c2"].to_frame()
     >>> X_train, X_test, y_train, y_test = train_test_split(
-    ...     x, y, test_size=0.2, train_size=0.8, shuffle=False
+    ...     X, y, test_size=0.4, shuffle=False
     ... )
-    >>> len_levels = len(y_test.index.names)
-    >>> y_test = y_test.groupby(level=list(range(len_levels - 1))).apply(
-    ...     lambda x: x.droplevel(list(range(len_levels - 1))).iloc[:-20]
+    >>> y_true = y_test.groupby(level=[0,1]).apply(
+    ...     lambda x: x.droplevel([0,1]).iloc[-max_pred_len:]
+    ... )
+    >>> y_test = y_test.groupby(level=[0,1]).apply(
+    ...     lambda x: x.droplevel([0,1]).iloc[:-max_pred_len]
     ... )
     >>>
     >>> # create forecaster and wrapper
-    >>> forecaster = PytorchForecastingTFT(trainer_params={
-    ...     "max_epochs": 1,
-    ... }) # doctest: +SKIP
+    >>> forecaster = PytorchForecastingDeepAR(
+    ...     trainer_params={"max_epochs": 1},
+    ...     broadcasting=False,
+    ... ) # doctest: +SKIP
     >>> wrapper = AutoRegressiveWrapper(
     ...     forecaster=forecaster,
     ...     horizon_length=3, # prediction_length for forecaster
     ...     aggregate_method=np.mean,
     ... ) # doctest: +SKIP
-    >>> wrapper.fit(y) # doctest: +SKIP
+    >>> wrapper.fit(y=y_train, X=X_train) # doctest: +SKIP
     >>>
     >>> # forecast multiple forecasting horizons with same trained model
-    >>>
-    >>> wrapper.predict(fh=[1, 5]) # doctest: +SKIP
-                                c2
-    h0   h1    time
-    h0_0 h1_0  2000-02-20  5.278717
-            2000-02-24  5.303032
-        h1_1  2000-02-20  5.106282
-            2000-02-24  5.119611
-        h1_10 2000-02-20  4.924220
-    ...                         ...
-    h0_4 h1_97 2000-02-24  4.815347
-        h1_98 2000-02-20  5.022863
-            2000-02-24  5.044283
-        h1_99 2000-02-20  4.714049
-            2000-02-24  4.768126
-
-    [2000 rows x 1 columns]
-    >>>
-    >>> wrapper.predict(fh=[8, 9, 10, 11, 12]) # doctest: +SKIP
-                                c2
-    h0   h1    time
-    h0_0 h1_0  2000-02-27  5.312222
-            2000-02-28  5.323535
-            2000-02-29  5.351599
-            2000-03-01  5.367970
-            2000-03-02  5.379033
-    ...                         ...
-    h0_4 h1_99 2000-02-27  4.754828
-            2000-02-28  4.766777
-            2000-02-29  4.872816
-            2000-03-01  4.886779
-            2000-03-02  4.896248
-
-    [5000 rows x 1 columns]
-    >>>
+    >>> y_pred = wrapper.predict(fh=[1, 5]) # doctest: +SKIP
+    >>> y_pred = wrapper.predict(y=y_test, X=X_test, fh=[1, 2, 4, 6]) # doctest: +SKIP
+    >>> y_pred = wrapper.predict(
+    ...     y=y_test,
+    ...     X=X_test,
+    ...     fh=[8, 9, 10, 11, 12]
+    ... ) # doctest: +SKIP
+    >>> y_pred = wrapper.predict(
+    ...     y=y_test,
+    ...     X=X_test,
+    ...     fh=list(range(1, max_pred_len))
+    ... ) # doctest: +SKIP
     """
 
     _tags = {
@@ -178,14 +183,25 @@ class AutoRegressiveWrapper(_BaseGlobalForecaster):
             "capability:global_forecasting", tag_value_default=False, raise_error=False
         )
         if not gt:
-            # TODO: improve the error message
-            raise ValueError("forecaster should be global")
+            raise ValueError(
+                f"Error in {self.__class__.__name__}: "
+                f"The provided forecaster, {self.forecaster.__class__.__name__}, "
+                "does not have global forecasting capabilities. "
+                "To use the AutoRegressiveWrapper, "
+                "the forecaster must be global in nature. "
+                "Ensure that the forecaster has the "
+                "'capability:global_forecasting' tag set to True. "
+                "You can verify this by calling "
+                "forecaster.get_tag('capability:global_forecasting')."
+            )
 
     def _fit(self, y, X=None, fh=None):
         if self.horizon_length is None and fh is None:
-            # TODO: improve message here
-            raise ValueError("Both horizon_length and fh cannot be None")
-
+            raise ValueError(
+                f"Error in {self.__class__.__name__}, "
+                "Either 'horizon_length' must be specified during initialization or "
+                "'fh' must be provided during fit. Both cannot be None."
+            )
         if fh is not None:
             max_fh = max(fh.to_relative(self._cutoff)._values)
             if self.horizon_length is None:
