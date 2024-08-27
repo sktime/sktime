sktime/sktime

commit 04edc4b306ba2cbd8339f4cad7fdf50d2f2d647c

[ENH] Fix html representations of pipelines and nested estimators (#6930)
#### Reference Issues/PRs
Fixes #6903 

#### What does this implement/fix? Explain your changes.
- Fixes pipeline rendering direction from left-to-right to top-to-bottom
- Fixes HTML display for nested estimators

sktime/forecasting/compose/_pipeline.py
@@ -13,6 +13,7 @@ from sktime.forecasting.base._base import BaseForecaster
 from sktime.forecasting.base._delegate import _DelegatedForecaster
 from sktime.forecasting.base._fh import ForecastingHorizon
 from sktime.registry import scitype
+from sktime.utils._estimator_html_repr import _VisualBlock
 from sktime.utils.validation.series import check_series
 from sktime.utils.warnings import warn
 
@@ -277,6 +278,25 @@ class _Pipeline(_HeterogenousMetaEstimator, BaseForecaster):
         params3 = {"steps": [Detrender(), YfromX.create_test_instance()]}
 
         return [params1, params2, params3]
+
+    def _sk_visual_block_(self):
+        _, estimators = zip(*self.steps)
+
+        def _get_name(name, est):
+            if est is None or est == "passthrough":
+                return f"{name}: passthrough"
+            # Is an estimator
+            return f"{name}: {est.__class__.__name__}"
+
+        names = [_get_name(name, est) for name, est in self.steps]
+        name_details = [str(est) for est in estimators]
+        return _VisualBlock(
+            "serial",
+            estimators,
+            names=names,
+            name_details=name_details,
+            dash_wrapped=False,
+        )
 
 
 # we ensure that internally we convert to pd.DataFrame for now

sktime/utils/_estimator_html_repr.py
@@ -1,10 +1,11 @@
 """Functionality to represent instance of BaseObject as html."""
-# based on the slearn module of the same name
+# based on the sklearn module of the same name
 
 import html
 import importlib
 import uuid
 from contextlib import closing
+from inspect import isclass
 from io import StringIO
 from pathlib import Path
 from string import Template
@@ -113,15 +114,20 @@ def _get_visual_block(base_object):
     elif base_object is None:
         return _VisualBlock("single", base_object, names="None", name_details="None")
 
-    # check if base_object looks like a meta base_object wraps base_object
-    if hasattr(base_object, "get_params"):
-        base_objects = []
-        for key, value in base_object.get_params().items():
-            # Only look at the BaseObjects in the first layer
-            if "__" not in key and hasattr(value, "get_params"):
-                base_objects.append(value)
-        if len(base_objects):
-            return _VisualBlock("parallel", base_objects, names=None)
+    # check if estimator looks like a meta estimator (wraps estimators)
+    if hasattr(base_object, "get_params") and not isclass(base_object):
+        base_objects = [
+            (key, est)
+            for key, est in base_object.get_params(deep=False).items()
+            if hasattr(est, "get_params") and hasattr(est, "fit") and not isclass(est)
+        ]
+        if base_objects:
+            return _VisualBlock(
+                "parallel",
+                [est for _, est in base_objects],
+                names=[f"{key}: {est.__class__.__name__}" for key, est in base_objects],
+                name_details=[str(est) for _, est in base_objects],
+            )
 
     return _VisualBlock(
         "single",
@@ -236,16 +242,34 @@ def _object_html_repr(base_object):
         return html_output
 
 
+def _get_reduced_path(input_path_string):
+    """Remove submodules starting with an underscore to get a reduced path string."""
+    substrings = input_path_string.split(".")
+
+    index_to_remove = None
+    for i, substring in enumerate(substrings):
+        if substring.startswith("_"):
+            index_to_remove = i
+            break
+
+    if index_to_remove is not None:
+        substrings = substrings[:index_to_remove] + substrings[-1:]
+
+    result_string = ".".join(substrings)
+
+    return result_string
+
+
 class _HTMLDocumentationLinkMixin:
     """Mixin class allowing to generate a link to the API documentation.
 
     This mixin relies on three attributes:
-    - `_doc_link_module`: it corresponds to the root module (e.g. `sklearn`). Using this
-      mixin, the default value is `sklearn`.
+    - `_doc_link_module`: it corresponds to the root module (e.g. `sktime`). Using this
+      mixin, the default value is `sktime`.
     - `_doc_link_template`: it corresponds to the template used to generate the
       link to the API documentation. Using this mixin, the default value is
-      `"https://www.sktime.net/en/v{version_url}/api_reference/auto_generated/
-      {modpath}.html"`.
+      `"https://www.sktime.net/en/v{sktime_version}/api_reference/auto_generated/
+      {path_reduced}.html"`.
     - `_doc_link_url_param_generator`: it corresponds to a function that generates the
       parameters to be used in the template when the estimator module and name are not
       sufficient.
@@ -269,7 +293,7 @@ class _HTMLDocumentationLinkMixin:
             "__doc_link_template",
             (
                 f"https://www.sktime.net/en/v{sktime_version}"
-                "/api_reference/auto_generated/{modpath}.html"
+                "/api_reference/auto_generated/{reduced_path}.html"
             ),
         )
 
@@ -294,10 +318,10 @@ class _HTMLDocumentationLinkMixin:
             return ""
 
         if self._doc_link_url_param_generator is None:
-            modclass = self.__class__
-            modpath = str(modclass)[8:-2]
+            modpath = str(self.__class__)[8:-2]
+            reduced_path = _get_reduced_path(modpath)
 
-            return self._doc_link_template.format(modpath=modpath)
+            return self._doc_link_template.format(reduced_path=reduced_path)
         return self._doc_link_template.format(
             **self._doc_link_url_param_generator(self)
         )

sktime/utils/tests/test_estimator_html_repr.py
@@ -22,12 +22,12 @@ def test_html_documentation_link_mixin_sktime(mock_version):
         # other branches than `main` (that is "dev").
         assert (
             mixin._doc_link_template == f"https://www.sktime.net/en/v{sktime_version}"
-            "/api_reference/auto_generated/{modpath}.html"
+            "/api_reference/auto_generated/{reduced_path}.html"
         )
         assert (
             mixin._get_doc_link() == f"https://www.sktime.net/en/v{sktime_version}"
             "/api_reference/auto_generated/"
-            "sktime.utils._estimator_html_repr._HTMLDocumentationLinkMixin.html"
+            "sktime.utils._HTMLDocumentationLinkMixin.html"
         )
 
 
@@ -37,10 +37,10 @@ def test_html_documentation_link_mixin_get_doc_link():
 
     # if we set `_doc_link`, then we expect to infer a module and name for the estimator
     mixin._doc_link_module = "sktime"
-    mixin._doc_link_template = "https://website.com/{modpath}.html"
+    mixin._doc_link_template = "https://website.com/{reduced_path}.html"
     assert (
         mixin._get_doc_link() == "https://website.com/"
-        "sktime.utils._estimator_html_repr._HTMLDocumentationLinkMixin.html"
+        "sktime.utils._HTMLDocumentationLinkMixin.html"
     )
 
 

commit 8ae6f85887c019bad43429389493adceb71180ce

[BUG] fix html display for `TransformedTargetForecaster` and `ForecastingPipeline` (#7043)
The html display for `TransformedTargetForecaster` is failing since
#6930, if estimators are passed as a list instead of list of tuples.

This is fixed by using the attribute that always returns a list of
tuples. A test is also added.

sktime/forecasting/compose/_pipeline.py
@@ -280,15 +280,8 @@ class _Pipeline(_HeterogenousMetaEstimator, BaseForecaster):
         return [params1, params2, params3]
 
     def _sk_visual_block_(self):
-        _, estimators = zip(*self.steps)
+        names, estimators = zip(*self._steps)
 
-        def _get_name(name, est):
-            if est is None or est == "passthrough":
-                return f"{name}: passthrough"
-            # Is an estimator
-            return f"{name}: {est.__class__.__name__}"
-
-        names = [_get_name(name, est) for name, est in self.steps]
         name_details = [str(est) for est in estimators]
         return _VisualBlock(
             "serial",

sktime/forecasting/compose/tests/test_pipeline.py
@@ -604,3 +604,21 @@ def test_pipeline_featurizer_noexog():
     # if the pipeline skips the FourierFeatures step,
     # then the predictions would be all constant, we test that this is not the case
     assert not np.allclose(y_pred.diff()[1:], np.zeros_like(y_pred[1:]))
+
+
+@pytest.mark.skipif(
+    not run_test_for_class(
+        [ForecastingPipeline, TransformedTargetForecaster, YfromX, Detrender],
+    ),
+    reason="run test only if softdeps are present and incrementally (if requested)",
+)
+def test_pipeline_display():
+    """Test that pipeline displays correctly."""
+    from sktime.forecasting.compose import TransformedTargetForecaster, YfromX
+    from sktime.transformations.series.detrend import Detrender
+
+    f = TransformedTargetForecaster([Detrender(), YfromX.create_test_instance()])
+    f._sk_visual_block_()
+
+    f = ForecastingPipeline([Detrender(), YfromX.create_test_instance()])
+    f._sk_visual_block_()