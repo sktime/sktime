f6cd93c493910d088dbff0e3ed0bde6053eef432
Franz Király <f.kiraly@ucl.ac.uk>
2024-09-04 16:25:10 +0100

    Update expandingcutoff.py

--- sktime/split/expandingcutoff.py
+++ sktime/split/expandingcutoff.py
@@ -56,14 +56,17 @@ class ExpandingCutoffSplitter(BaseSplitter):
     functions as an iloc indexer.
 
     For example for ``cutoff = 10``, ``step_length = 1`` and ``fh = [1, 2, 3, 4, 5, 6]``
-    here is a representation of the folds::
+    here is a representation of the folds:
 
+    ```
                           c
     |---------------------|----fh----|------|
     | * * * * * * * * * * x x x x x x - - - |
     | * * * * * * * * * * * x x x x x x - - |
     | * * * * * * * * * * * * x x x x x x - |
     | * * * * * * * * * * * * * x x x x x x |
+
+    ```
 
     ``c`` = cutoff date or index.
 
