13f1abdb792783c94c446d087a0e343fcafb4e2b
julian fong <cyfongj@gmail.com>
2024-09-07 15:31:25 -0400

    edited error in forecasting metric

--- sktime/libs/momentfm/utils/forecasting_metrics.py
+++ sktime/libs/momentfm/utils/forecasting_metrics.py
@@ -305,12 +305,12 @@ if _check_soft_dependencies(["torch"], severity="none"):
                     f"SMAPE should be lower than 200 but was found to be {error}"
                 )
         else:
+            print(type(error))
             # assert all(error <= 200), "SMAPE should be lower than 200"
             # if all(error > 200):
             #     warnings.warn(
             #         f"SMAPE should be lower than 200 but was found to be {error}"
             #     )
-            pass
 
         return error
 
