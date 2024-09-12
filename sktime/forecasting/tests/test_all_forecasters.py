5efd2ed56dd4cfb9a4712e6a794dfffd7d9d9316
benheid <benheid@users.noreply.github.com>
2024-09-12 07:31:07 +0200

    apply gc

--- sktime/forecasting/tests/test_all_forecasters.py
+++ sktime/forecasting/tests/test_all_forecasters.py
@@ -148,6 +148,14 @@ class ForecasterFixtureGenerator(BaseFixtureGenerator):
 class TestAllForecasters(ForecasterFixtureGenerator, QuickTester):
     """Module level tests for all sktime forecasters."""
 
+    @pytest.fixture(autouse=True)
+    def apply_collection(self):
+        print("Collecting garbage")
+        yield
+        import gc
+
+        gc.collect()
+
     def test_get_fitted_params(self, estimator_instance, scenario):
         """Test get_fitted_params."""
         scenario.run(estimator_instance, method_sequence=["fit"])
@@ -945,6 +953,14 @@ class TestAllGlobalForecasters(TestAllObjects):
     """Module level tests for all global forecasters."""
 
     estimator_type_filter = "global_forecaster"
+
+    @pytest.fixture(autouse=True)
+    def apply_collection(self):
+        print("Collecting garbage")
+        yield
+        import gc
+
+        gc.collect()
 
     def test_global_forecasting_tag(self, estimator_class):
         global_forecasting_tag = estimator_class.get_class_tag(
