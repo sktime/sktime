d7b570ee2fd437913b4d63482a34c19a74e4fffe
Benedikt Heidrich <benHeid@users.noreply.github.com>
2024-09-14 14:26:33 +0200

    Update test_all_forecasters.py

--- sktime/forecasting/tests/test_all_forecasters.py
+++ sktime/forecasting/tests/test_all_forecasters.py
@@ -11,6 +11,7 @@ from inspect import signature
 import numpy as np
 import pandas as pd
 import pytest
+import torch
 
 from sktime.datatypes import check_is_mtype
 from sktime.datatypes._utilities import get_cutoff
@@ -46,6 +47,8 @@ from sktime.utils._testing.forecasting import (
 )
 from sktime.utils._testing.series import _make_series
 from sktime.utils.validation.forecasting import check_fh
+
+torch.set_num_threads(1)
 
 # get all forecasters
 FH0 = 1
