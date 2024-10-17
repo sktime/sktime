6606dfbe5786c1acef7f1233784c31e7b431f640
Franz Király <f.kiraly@ucl.ac.uk>
2024-10-17 22:59:32 +0200

    Create __init__.py

---
+++ sktime/detection/__init__.py
@@ -0,0 +1,39 @@
+"""Time series anomaly, changepoint detection, segmentation."""
+
+from sktime.annotation import (
+    adapters,
+    base,
+    bs,
+    clasp,
+    clust,
+    compose,
+    datagen,
+    eagglo,
+    ggs,
+    hmm,
+    hmm_learn,
+    igts,
+    lof,
+    outlier_detection,
+    plotting,
+    stray,
+)
+
+__all__ = [
+    "adapters",
+    "base",
+    "bs",
+    "clasp",
+    "clust",
+    "compose",
+    "datagen",
+    "eagglo",
+    "ggs",
+    "hmm",
+    "hmm_learn",
+    "igts",
+    "lof",
+    "outlier_detection",
+    "plotting",
+    "stray",
+]
