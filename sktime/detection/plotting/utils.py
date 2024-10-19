415b546f7029f42215b06e0c2fabcc7aeb9b0507
Franz Király <f.kiraly@ucl.ac.uk>
2024-10-19 13:33:44 +0200

    Update utils.py

--- sktime/detection/plotting/utils.py
+++ sktime/detection/plotting/utils.py
@@ -2,8 +2,6 @@
 # copyright: sktime developers, BSD-3-Clause License (see LICENSE file)
 """Utility class for plotting functionality."""
 
-import numpy as np
-
 from sktime.annotation.plotting.utils import (
     plot_time_series_with_change_points,
     plot_time_series_with_profiles,
