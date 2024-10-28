2baf01a97bb5387de5ac8e398f49509bb5c4d73e
Sanskar Modi <sansyprog8@gmail.com>
2024-10-28 23:35:30 +0530

    updated implementation to solve some issues

--- sktime/forecasting/boxcox_bias_adjusted_forecaster.py
+++ sktime/forecasting/boxcox_bias_adjusted_forecaster.py
@@ -4,11 +4,11 @@
 
 __author__ = ["sanskarmodi8"]
 
+import numpy as np
 import pandas as pd
 from scipy.special import inv_boxcox
 
 from sktime.forecasting.base import BaseForecaster
-from sktime.forecasting.base._delegate import _DelegatedForecaster
 from sktime.transformations.series.boxcox import BoxCoxTransformer
 
 
@@ -47,12 +47,17 @@ class BoxCoxBiasAdjustedForecaster(BaseForecaster):
            OTexts: Melbourne, Australia. OTexts.com/fpp2
     """
 
-    def __init__(self, forecaster, lmbda=None):
+    _tags = {
+        "capability:pred_int": True,
+        "capability:pred_var": True,
+    }
+
+    def __init__(self, forecaster, lambda_fixed=None):
         self.forecaster = forecaster
-        self.lmbda = lmbda
+        self.lambda_fixed = lambda_fixed
         self.boxcox_transformer_ = None
-        # Clone tags from the wrapped forecaster to this forecaster
-        _DelegatedForecaster._set_delegated_tags(self, forecaster)
+        self._y_name = None
+        self.clone_tags(forecaster)
         super().__init__()
 
     def _fit(self, y, X=None, fh=None):
@@ -71,18 +76,13 @@ class BoxCoxBiasAdjustedForecaster(BaseForecaster):
         -------
         self : returns an instance of self.
         """
-        self.boxcox_transformer_ = BoxCoxTransformer(lmbda=self.lmbda)
-        self.y_transformed = self.boxcox_transformer_.fit_transform(y)
+        self._y_name = y.name if isinstance(y, pd.Series) else None
+
+        self.boxcox_transformer_ = BoxCoxTransformer(lambda_fixed=self.lambda_fixed)
+        y_transformed = self.boxcox_transformer_.fit_transform(y)
 
         self.forecaster_ = self.forecaster.clone()
-        self.forecaster_.fit(y=self.y_transformed, X=X, fh=fh)
-
-        # Check if the wrapped forecaster supports prediction intervals
-        if not self.forecaster_.get_tag("capability:pred_int"):
-            raise ValueError(
-                "The wrapped forecaster must support prediction intervals "
-                "(capability:pred_int) to enable bias adjustment."
-            )
+        self.forecaster_.fit(y=y_transformed, X=X, fh=fh)
 
         return self
 
@@ -105,37 +105,53 @@ class BoxCoxBiasAdjustedForecaster(BaseForecaster):
         variance = self.forecaster_.predict_var(fh, X)
 
         y_pred = self._apply_bias_adjustment(y_pred_transformed, variance)
+        if isinstance(y_pred, pd.DataFrame) and y_pred.shape[1] > 1:
+            y_pred = y_pred.iloc[:, 0]
+
+        if isinstance(y_pred, pd.Series):
+            y_pred.name = self._y_name
 
         return y_pred
 
-    def _predict_interval(self, fh, X=None, coverage=None):
-        """Compute prediction intervals for the forecasts.
 
-        Parameters
-        ----------
-        fh : ForecastingHorizon
-            The forecasting horizon with the steps ahead to predict.
-        X : pd.DataFrame, optional (default=None)
-            Exogenous variables.
-        coverage : list of float, optional (default=None)
-            Confidence levels for prediction intervals.
+def _predict_interval(self, fh, X=None, coverage=None):
+    """Compute prediction intervals for the forecasts.
 
-        Returns
-        -------
-        pred_int : pd.DataFrame
-            Prediction intervals.
-        """
-        pred_int_transformed = self.forecaster_.predict_interval(fh, X, coverage)
-        variance = self.forecaster_.predict_var(fh, X)
+    Parameters
+    ----------
+    fh : ForecastingHorizon
+        The forecasting horizon with the steps ahead to predict.
+    X : pd.DataFrame, optional (default=None)
+        Exogenous variables.
+    coverage : list of float, optional (default=None)
+        Confidence levels for prediction intervals.
 
-        lower_adjusted = self._apply_bias_adjustment(
-            pred_int_transformed["lower"], variance
+    Returns
+    -------
+    pred_int : pd.DataFrame
+        Prediction intervals.
+    """
+    pred_int_transformed = self.forecaster_.predict_interval(fh, X, coverage)
+    variance = self.forecaster_.predict_var(fh, X)
+
+    if isinstance(pred_int_transformed.index, pd.PeriodIndex):
+        pred_int_transformed.index = pred_int_transformed.index.to_timestamp()
+
+    adjusted_intervals = {}
+
+    for idx, cov, bound in pred_int_transformed.columns:
+        adjusted_value = self._apply_bias_adjustment(
+            pred_int_transformed[(idx, cov, bound)], variance
         )
-        upper_adjusted = self._apply_bias_adjustment(
-            pred_int_transformed["upper"], variance
-        )
 
-        return pd.concat([lower_adjusted, upper_adjusted], axis=1)
+        if isinstance(adjusted_value, np.ndarray):
+            adjusted_value = adjusted_value.flatten()
+
+        adjusted_intervals[(idx, cov, bound)] = adjusted_value
+
+    result = pd.DataFrame(adjusted_intervals, index=pred_int_transformed.index)
+
+    return result
 
     def _apply_bias_adjustment(self, y, variance):
         """Apply bias adjustment for BoxCox Transformations.
@@ -152,9 +168,9 @@ class BoxCoxBiasAdjustedForecaster(BaseForecaster):
         y_adjusted : pd.Series
             Bias-adjusted predictions or prediction intervals.
         """
-        lmbda = self.boxcox_transformer_.lmbda_
-        w = self.y_transformed
-        denominator = 2 * (lmbda * w + 1) ** 2
+        lmbda = self.boxcox_transformer_.lambda_
+
+        denominator = 2 * (lmbda * y + 1) ** 2
         adjustment = 1 + (variance * (1 - lmbda)) / denominator
 
         return inv_boxcox(y, lmbda) * adjustment
@@ -174,12 +190,10 @@ class BoxCoxBiasAdjustedForecaster(BaseForecaster):
             Parameters to create testing instances of the class.
         """
         from sktime.forecasting.naive import NaiveForecaster
-        from sktime.forecasting.trend import PolynomialTrendForecaster
 
-        forecaster1 = NaiveForecaster(strategy="mean")
-        forecaster2 = PolynomialTrendForecaster(degree=1)
+        forecaster = NaiveForecaster()
 
-        params1 = {"forecaster": forecaster1}
-        params2 = {"forecaster": forecaster2, "lmbda": 0.5}
+        params1 = {"forecaster": forecaster}
+        params2 = {"forecaster": forecaster, "lambda_fixed": 0.5}
 
         return [params1, params2]
