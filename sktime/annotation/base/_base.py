b0e2d943aa61e3f135e99936c1abe3b13f13d323
Franz Király <f.kiraly@ucl.ac.uk>
2024-11-07 00:09:00 +0100

    Update _base.py

--- sktime/annotation/base/_base.py
+++ sktime/annotation/base/_base.py
@@ -62,7 +62,8 @@ class BaseSeriesAnnotator(BaseEstimator):
     """
 
     _tags = {
-        "object_type": ["detector", "series-annotator"],  # type of object  # todo 0.37.0: remove series-annotator
+        "object_type": ["detector", "series-annotator"],  # type of object
+        # todo 0.37.0: remove series-annotator
         "learning_type": "None",  # Tag to determine test in test_all_annotators
         "task": "None",  # Tag to determine test in test_all_annotators
         #
