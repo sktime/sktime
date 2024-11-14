aa2faea20cc66824b65b29d7c4be5f6c36f631b8
julian fong <cyfongj@gmail.com>
2024-11-13 19:58:52 -0500

    update

--- sktime/forecasting/ttm.py
+++ sktime/forecasting/ttm.py
@@ -3,7 +3,7 @@
 
 __author__ = ["ajati", "wgifford", "vijaye12", "geetu040"]
 # ajati, wgifford, vijaye12 for ibm-granite code
-
+##
 import numpy as np
 import pandas as pd
 from skbase.utils.dependencies import _check_soft_dependencies
