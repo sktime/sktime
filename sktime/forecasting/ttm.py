aa85f214045457d9a4461a0a47f0a08b1693653d
julian fong <cyfongj@gmail.com>
2024-11-17 00:22:33 -0500

    removed error

--- sktime/forecasting/ttm.py
+++ sktime/forecasting/ttm.py
@@ -218,7 +218,6 @@ class TinyTimeMixerForecaster(_BaseGlobalForecaster):
         -------
         self : reference to self
         """
-        raise ValueError("test_error")
         if self.use_source_package:
             from tsfm_public.models.tinytimemixer import (
                 TinyTimeMixerConfig,
