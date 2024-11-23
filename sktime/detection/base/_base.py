0f652291f02fb2e78a6eca22d3ddd62f0aa0ae28
Franz Király <f.kiraly@ucl.ac.uk>
2024-11-23 15:42:21 +0100

    linting

--- sktime/detection/base/_base.py
+++ sktime/detection/base/_base.py
@@ -494,6 +494,7 @@ class BaseDetector(BaseEstimator):
         """
         raise NotImplementedError("abstract method")
 
+
     def _transform_scores(self, X):
         """Return scores for predicted labels on test/deployment data.
 
