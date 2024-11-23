sktime/sktime

commit 188bacc72c681081d8226812b8392fe505078ee1

[ENH] `EnsembleForecaster` option to specify multiple copies of same forecaster easily (#7424)
Resolves #7407. 

A common use case with ML estimators is to create an ensemble estimator
with N replicates of the same estimator. This PR facilitates this use
case.

Previously, an ensemble forecaster was created via EnsembleForecaster(
forecasters, ... ), with forecasters being a list of 2-tuples [(str1,
estimator1), ..., (strN, estimatorN)]. This is a bit tedious if many of
the estimators are identical.

This PR allows each tuple to be either of the form (str, estimator) (as
before) or (str, estimator, count). This latter form says to create
count replicates of the estimator.

sktime/forecasting/compose/_ensemble.py
@@ -278,8 +278,13 @@ class EnsembleForecaster(_HeterogenousEnsembleForecaster):
 
     Parameters
     ----------
-    forecasters : list of (str, estimator) tuples
+    forecasters : list of estimator, (str, estimator), or (str, estimator, count) tuples
         Estimators to apply to the input series.
+
+        * (str, estimator) tuples: the string is a name for the estimator.
+        * estimator without string will be assigned unique name based on class name
+        * (str, estimator, count) tuples: the estimator will be replicated count times.
+
     n_jobs : int or None, optional, default=None
         The number of jobs to run in parallel for fit. None means 1 unless
         in a joblib.parallel_backend context.
@@ -320,14 +325,48 @@ class EnsembleForecaster(_HeterogenousEnsembleForecaster):
         "scitype:y": "both",
     }
 
+    # for default get_params/set_params from _HeterogenousMetaEstimator
+    # _steps_attr points to the attribute of self
+    # which contains the heterogeneous set of estimators
+    # this must be an iterable of (name: str, estimator, ...) tuples for the default
+    _steps_attr = "_forecasters"
+
+    # if the estimator is fittable, _HeterogenousMetaEstimator also
+    # provides an override for get_fitted_params for params from the fitted estimators
+    # the fitted estimators should be in a different attribute, _steps_fitted_attr
+    # this must be an iterable of (name: str, estimator, ...) tuples for the default
+    _steps_fitted_attr = "forecasters_"
+
     def __init__(self, forecasters, n_jobs=None, aggfunc="mean", weights=None):
-        super().__init__(forecasters=forecasters, n_jobs=n_jobs)
         self.aggfunc = aggfunc
         self.weights = weights
+        super().__init__(forecasters=forecasters, n_jobs=n_jobs)
+
+        print("test")
+
+        fc = []
+        for forecaster in forecasters:
+            if len(forecaster) <= 2:
+                # Handle the (str, est) tuple
+                fc.append(forecaster)
+            elif len(forecaster) == 3:
+                # Handle the (str, est, num_replicates) tuple
+                name, estimator, num_replicates = forecaster
+                fc.extend([(name, estimator)] * num_replicates)
+            else:
+                msg = (
+                    "Error in EnsembleForecaster construction: "
+                    "forecasters argument must be as list of "
+                    "estimator, (str, estimator) or (str, estimator, count) tuples."
+                )
+                raise ValueError(msg)
+
+        self._forecasters = self._check_estimators(fc, clone_ests=False)
+        self.forecasters_ = self._check_estimators(fc, clone_ests=True)
 
         # the ensemble requires fh in fit
         # iff any of the component forecasters require fh in fit
-        self._anytagis_then_set("requires-fh-in-fit", True, False, self.forecasters)
+        self._anytagis_then_set("requires-fh-in-fit", True, False, self._forecasters)
 
     def _fit(self, y, X, fh):
         """Fit to training data.
@@ -397,13 +436,16 @@ class EnsembleForecaster(_HeterogenousEnsembleForecaster):
 
         # univariate case
         FORECASTER = NaiveForecaster()
-        params = [{"forecasters": [("f1", FORECASTER), ("f2", FORECASTER)]}]
+        params0 = {"forecasters": [("f1", FORECASTER), ("f2", FORECASTER)]}
 
         # test multivariate case, i.e., ensembling multiple variables at same time
         FORECASTER = DirectReductionForecaster.create_test_instance()
-        params = params + [{"forecasters": [("f1", FORECASTER), ("f2", FORECASTER)]}]
+        params1 = {"forecasters": [("f1", FORECASTER), ("f2", FORECASTER)]}
 
-        return params
+        # test with multiplicities
+        params2 = {"forecasters": [("f", FORECASTER, 2)]}
+
+        return [params0, params1, params2]
 
 
 def _aggregate(y, aggfunc, weights):

sktime/forecasting/online_learning/_online_ensemble.py
@@ -16,7 +16,14 @@ class OnlineEnsembleForecaster(EnsembleForecaster):
     Parameters
     ----------
     ensemble_algorithm : ensemble algorithm
-    forecasters : list of (str, estimator) tuples
+
+    forecasters : list of estimator, (str, estimator), or (str, estimator, count) tuples
+        Estimators to apply to the input series.
+
+        * (str, estimator) tuples: the string is a name for the estimator.
+        * estimator without string will be assigned unique name based on class name
+        * (str, estimator, count) tuples: the estimator will be replicated count times.
+
     n_jobs : int or None, optional (default=None)
         The number of jobs to run in parallel for fit. None means 1 unless
         in a joblib.parallel_backend context.
@@ -41,7 +48,7 @@ class OnlineEnsembleForecaster(EnsembleForecaster):
         self.n_jobs = n_jobs
         self.ensemble_algorithm = ensemble_algorithm
 
-        super(EnsembleForecaster, self).__init__(forecasters=forecasters, n_jobs=n_jobs)
+        super().__init__(forecasters=forecasters, n_jobs=n_jobs)
 
     def _fit(self, y, X, fh):
         """Fit to training data.

commit 9915adb0c6833be67b9b79707ebfb9cec0141c2f

[ENH] parity of `_HeterogenousMetaEstimator._check_estimator` with `skbase` and fixes in `EnsembleForecaster.__init__` (#7429)
This PR combines a refactor with a fix:

* brings `_HeterogeousMetaEstimator._check_estimator` to parity with
`scikit-base`
* fixes issues with `EnsembleForecaster.__init__` introduced in
https://github.com/sktime/sktime/pull/7424

The two are combined due to small footprint and dependency.

sktime/base/_meta.py
@@ -233,7 +233,9 @@ class _HeterogenousMetaEstimator:
         estimators,
         attr_name="steps",
         cls_type=None,
+        allow_dict=False,
         allow_mix=True,
+        allow_empty=False,
         clone_ests=True,
     ):
         """Check that estimators is a list of estimators or list of str/est tuples.
@@ -247,9 +249,13 @@ class _HeterogenousMetaEstimator:
             Name of checked attribute in error messages
         cls_type : class or tuple of class, optional. Default = BaseEstimator.
             class(es) that all estimators are checked to be an instance of
+        allow_dict : bool, default=False
+            Whether `objs` can be a dictionary mapping str names to objects.
         allow_mix : boolean, optional. Default = True.
             whether mix of estimator and (str, estimator) is allowed in `estimators`
-        clone_ests : boolean, optional. Default = True.
+        allow_empty : boolean, optional. Default = False
+            whether empty list of estimators is allowed
+        clone_ests : boolean, optional. Default = True
             whether estimators in return are cloned (True) or references (False).
 
         Returns
@@ -280,8 +286,11 @@ class _HeterogenousMetaEstimator:
 
         if (
             estimators is None
-            or len(estimators) == 0
-            or not isinstance(estimators, list)
+            or (not allow_empty and len(estimators) == 0)
+            or not (
+                isinstance(estimators, list)
+                or (allow_dict and isinstance(estimators, dict))
+            )
         ):
             raise TypeError(msg)
 
@@ -292,7 +301,15 @@ class _HeterogenousMetaEstimator:
 
             return is_est, is_tuple
 
-        if not all(any(is_est_is_tuple(x)) for x in estimators):
+        # We've already guarded against objs being dict when allow_dict is False
+        # So here we can just check dictionary elements
+        if isinstance(estimators, dict) and not all(
+            isinstance(name, str) and isinstance(obj, cls_type)
+            for name, obj in estimators.items()
+        ):
+            raise TypeError(msg)
+
+        elif not all(any(is_est_is_tuple(x)) for x in estimators):
             raise TypeError(msg)
 
         msg_no_mix = (

sktime/forecasting/compose/_ensemble.py
@@ -342,8 +342,6 @@ class EnsembleForecaster(_HeterogenousEnsembleForecaster):
         self.weights = weights
         super().__init__(forecasters=forecasters, n_jobs=n_jobs)
 
-        print("test")
-
         fc = []
         for forecaster in forecasters:
             if len(forecaster) <= 2:
@@ -361,8 +359,12 @@ class EnsembleForecaster(_HeterogenousEnsembleForecaster):
                 )
                 raise ValueError(msg)
 
-        self._forecasters = self._check_estimators(fc, clone_ests=False)
-        self.forecasters_ = self._check_estimators(fc, clone_ests=True)
+        self._forecasters = self._check_estimators(
+            fc, clone_ests=False, allow_empty=True
+        )
+        self.forecasters_ = self._check_estimators(
+            fc, clone_ests=True, allow_empty=True
+        )
 
         # the ensemble requires fh in fit
         # iff any of the component forecasters require fh in fit