2c8cb6e303de48b346fc4238dce7826681a3427e
Franz Király <f.kiraly@ucl.ac.uk>
2024-11-25 00:04:28 +0100

    simplify code, deduplicate

--- sktime/detection/dummy/_zero_cp.py
+++ sktime/detection/dummy/_zero_cp.py
@@ -1,11 +1,9 @@
 """Dummy change point detector which detects no change points."""
 
-import pandas as pd
-
-from sktime.detection.base import BaseDetector
+from sktime.detection.dummy._zero_an import ZeroAnomalies
 
 
-class ZeroChangePoints(BaseDetector):
+class ZeroChangePoints(ZeroAnomalies):
     """Dummy change point detector which detects no change points ever.
 
     Naive method that can serve as benchmarking pipeline or API test.
@@ -21,36 +19,5 @@ class ZeroChangePoints(BaseDetector):
     >>> d.fit_transform(y)
     """
 
-    _tags = {
-        "authors": ["fkiraly"],
-        "capability:multivariate": True,
-        "capability:missing_values": True,
-        "fit_is_empty": True,
-        "task": "change_point_detection",
-        "learning_type": "unsupervised",
-    }
-
-    def __init__(self):
-        super().__init__()
-
-    def _predict(self, X, y=None):
-        """Create labels on test/deployment data.
-
-        private _predict containing the core logic, called from predict
-
-        Parameters
-        ----------
-        X : pd.DataFrame
-            Time series subject to detection, which will be assigned labels or scores.
-
-        Returns
-        -------
-        y : pd.Series with RangeIndex
-            Labels for sequence ``X``, in sparse format.
-            Values are ``iloc`` references to indices of ``X``.
-
-            * If ``task`` is ``"anomaly_detection"`` or ``"change_point_detection"``,
-              the values are integer indices of the changepoints/anomalies.
-            * If ``task`` is "segmentation", the values are ``pd.Interval`` objects.
-        """
-        return pd.Series([], dtype="int64")
+    # same code except the tag
+    _tags = {"task": "change_point_detection"}
