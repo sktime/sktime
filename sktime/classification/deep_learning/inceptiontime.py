771acebbc4c702c5370abf0d9775816b954bb667
Muhammad Sakib Khan Inan <sakib.khaninan@gmail.com>
2024-12-10 17:27:54 +1100

    Added Note for InceptionTimeClassifier to solve Issue #7453

--- sktime/classification/deep_learning/inceptiontime.py
+++ sktime/classification/deep_learning/inceptiontime.py
@@ -43,8 +43,16 @@ class InceptionTimeClassifier(BaseDeepClassifier):
     Adapted from the implementation from Fawaz et. al
     https://github.com/hfawaz/InceptionTime/blob/master/classifiers/inception.py
 
+    The `InceptionTimeClassifier` here, is a single instance of InceptionTime model.
+    The original InceptionTime paper recommends to ensemble 5 instances
+    of InceptionTime model to match with the benchmark standards. To build an
+    ensemble of models following the original paper use the `BaggingClassifier` with
+    `n_estimators=5`, `bootstrap=False` using `InceptionTimeClassifier` as estimator.
+
     Examples
     --------
+    Single instance
+    ---------------
     >>> from sktime.classification.deep_learning import InceptionTimeClassifier
     >>> from sktime.datasets import load_unit_test  # doctest: +SKIP
     >>> X_train, y_train = load_unit_test(split="train")  # doctest: +SKIP
@@ -52,6 +60,20 @@ class InceptionTimeClassifier(BaseDeepClassifier):
     >>> clf = InceptionTimeClassifier()  # doctest: +SKIP
     >>> clf.fit(X_train, y_train)  # doctest: +SKIP
     InceptionTimeClassifier(...)
+
+    Ensemble of 5 instances
+    -----------------------
+    >>> from sktime.classification.ensemble import BaggingClassifier
+    >>> from sktime.classification.deep_learning import InceptionTimeClassifier
+    >>> from sktime.datasets import load_unit_test  # doctest: +SKIP
+    >>> X_train, y_train = load_unit_test(split="train")  # doctest: +SKIP
+    >>> X_test, y_test = load_unit_test(split="test")  # doctest: +SKIP
+    >>> clf = BaggingClassifier(
+            InceptionTimeClassifier(),
+    ...     n_estimators=5,
+    ...     bootstrap=False) # doctest: +SKIP  # doctest: +SKIP
+    >>> clf.fit(X_train, y_train)  # doctest: +SKIP
+    BaggingClassifier(...)
     """
 
     _tags = {
