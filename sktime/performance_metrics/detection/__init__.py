deb121bef932d3d8dba96b8220580150c3d36b3b
Franz Király <f.kiraly@ucl.ac.uk>
2024-12-11 23:48:34 +0100

    also hausdorff

--- sktime/performance_metrics/detection/__init__.py
+++ sktime/performance_metrics/detection/__init__.py
@@ -1,5 +1,6 @@
 """Metrics for detection tasks."""
 
 from sktime.performance_metrics.detection._chamfer import DirectedChamfer
+from sktime.performance_metrics.detection._hausdorff import DirectedHausdorff
 
-__all__ = ["DirectedChamfer"]
+__all__ = ["DirectedChamfer", "DirectedHausdorff"]
--- sktime/performance_metrics/detection/_hausdorff.py
+++ sktime/performance_metrics/detection/_hausdorff.py
@@ -1,4 +1,4 @@
-"""Directed Chamfer distance between two sets of points."""
+"""Directed Hausdorff distance between two sets of points."""
 
 import numpy as np
 import pandas as pd
@@ -7,29 +7,22 @@ from sktime.performance_metrics.detection._base import BaseDetectionMetric
 from sktime.performance_metrics.detection.utils import _find_closest_elements+
 
 
-class DirectedChamfer(BaseDetectionMetric):
-    r"""Directed Chamfer distance between event points.
+class DirectedHausdorff(BaseDetectionMetric):
+    r"""Directed Hausdorff distance between event points.
 
     For detected time points :math:`A = (a_1, a_2, \ldots, a_n)` and true time points
     :math:`B = (b_1, b_2, \ldots, b_m)`,
-    the directed (unnormalized) Chamfer distance is defined as:
+    the directed (unnormalized) Hausdorff distance is defined as:
 
     .. math::
 
-        d(A, B) = \sum_{i=1}^{n} \left| a_i - b'_i \right|
+        d(A, B) = \max_i \left| a_i - b'_i \right|
 
     where :math:`b'_i = \arg \min_{b\in B} |a_i - b|` is the closest
     element of :math:`B`, to :math:`a_i`.
 
     If ``X`` is provided, the time points are taken as the location indices in ``X``.
     Otherwise, it is assumed that ``X`` has a ``RangeIndex``.
-
-    Parameters
-    ----------
-    normalize : bool, default=False
-        If True, the Chamfer distance is normalized by the number of detected events,
-        i.e., divided by :math:`n`.
-        If unnormalized, too many detections will be penalized.
     """
 
     _tags = {
@@ -37,9 +30,6 @@ class DirectedChamfer(BaseDetectionMetric):
         "requires_X": False,
         "lower_is_better": True,
     }
-
-    def __init__(self, normalize=False):
-        self.normalize = normalize
 
     def _evaluate(self, y_true, y_pred, X=None):
         """Evaluate the desired metric on given inputs.
@@ -85,33 +75,6 @@ class DirectedChamfer(BaseDetectionMetric):
         y_true_closest = self._find_closest_elements(y_pred_locs, y_true_locs)
         y_true_closest = np.array(y_true_closest)
 
-        distance = np.sum(np.abs(y_true_closest - y_pred_locs))
-
-        if self.normalize:
-            distance /= len(y_pred_locs)
+        distance = np.max(np.abs(y_true_closest - y_pred_locs))
 
         return distance
-
-    @classmethod
-    def get_test_params(cls, parameter_set="default"):
-        """Return testing parameter settings for the estimator.
-
-        Parameters
-        ----------
-        parameter_set : str, default="default"
-            Name of the set of test parameters to return, for use in tests. If no
-            special parameters are defined for a value, will return ``"default"`` set.
-
-        Returns
-        -------
-        params : dict or list of dict, default={}
-            Parameters to create testing instances of the class.
-            Each dict are parameters to construct an "interesting" test instance, i.e.,
-            ``MyClass(**params)`` or ``MyClass(**params[i])`` creates a valid test
-            instance.
-            ``create_test_instance`` uses the first (or only) dictionary in ``params``.
-        """
-        param1 = {}
-        param2 = {"normalize": True}
-
-        return [param1, param2]
