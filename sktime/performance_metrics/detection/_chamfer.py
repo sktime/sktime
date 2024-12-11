462d129a422bf1f526fd1e9ab5535873119b9a9c
Franz Király <f.kiraly@ucl.ac.uk>
2024-12-11 23:52:46 +0100

    Update _chamfer.py

--- sktime/performance_metrics/detection/_chamfer.py
+++ sktime/performance_metrics/detection/_chamfer.py
@@ -29,7 +29,9 @@ class DirectedChamfer(BaseDetectionMetric):
     normalize : bool, default=False
         If True, the Chamfer distance is normalized by the number of detected events,
         i.e., divided by :math:`n`.
-        If unnormalized, too many detections will be penalized.
+        If unnormalized, making too many detections will be penalized, wherease
+        the normalized distance penalizes only the average distance to the
+        closest true events, regardless of the number of detections.
     """
 
     _tags = {
