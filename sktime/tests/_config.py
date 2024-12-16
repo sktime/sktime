ee691d3f400fb6ca730ab94f9081565fab1ef757
Adarsh2345 <130629672+Adarsh2345@users.noreply.github.com>
2024-12-16 21:05:17 +0530

    Removed `BaggingForecaster` from _config.py

--- sktime/tests/_config.py
+++ sktime/tests/_config.py
@@ -254,7 +254,6 @@ EXCLUDED_TESTS = {
 EXCLUDED_TESTS_BY_TEST = {
     "test_get_test_params_coverage": [
         "Arsenal",
-        "BaggingForecaster",
         "BOSSEnsemble",
         "BinarySegmentation",
         "CNTCClassifier",
