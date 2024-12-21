91f7fa54936573350cacfb080bee64da27fc572c
Franz Király <f.kiraly@ucl.ac.uk>
2024-12-21 20:09:17 +0100

    Update _lookup.py

--- sktime/registry/_lookup.py
+++ sktime/registry/_lookup.py
@@ -188,9 +188,10 @@ def all_estimators(
             return [obj]
         return obj
 
-    estimator_types = _coerce_to_list_of_str(estimator_types)
-    estimator_types = [x for y in estimator_types for x in _get_all_descendants(y)]
-    estimator_types = list(set(estimator_types))
+    if estimator_types is not None:
+        estimator_types = _coerce_to_list_of_str(estimator_types)
+        estimator_types = [x for y in estimator_types for x in _get_all_descendants(y)]
+        estimator_types = list(set(estimator_types))
 
     if estimator_types is not None and filter_tags is not None:
         if "object_type" in filter_tags:
