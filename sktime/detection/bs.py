1751418208f88035fb726675cdb83a4a7620961f
Alex-JG3 <49785867+Alex-JG3@users.noreply.github.com>
2024-12-29 20:24:36 +0000

    [ENH] `BinarySegmentation` fixes for new detection interface (#7504)
    #### Reference Issues/PRs
    
    Fixes #7411.
    
    #### What does this implement/fix? Explain your changes.
    
    Removes binary segmentation tests from the exclude list so they now run are part of the CI/CD pipeline.

--- sktime/detection/bs.py
+++ sktime/detection/bs.py
@@ -43,13 +43,13 @@ class BinarySegmentation(BaseDetector):
     >>> model = BinarySegmentation(threshold=1)
     >>> X = pd.Series([1, 1, 1, 1, 5, 5, 5, 5])
     >>> model.fit_predict(X)
-    0    3
-    dtype: int64
+       ilocs
+    0      3
     >>> X = pd.Series([1.1, 1.3, -1.4, -1.4, 5.5, 5.6])
     >>> model.fit_predict(X)
-    0    1
-    1    3
-    dtype: int64
+       ilocs
+    0      1
+    1      3
     """
 
     _tags = {
@@ -63,6 +63,7 @@ class BinarySegmentation(BaseDetector):
         "univariate-only": True,
         "task": "change_point_detection",
         "learning_type": "unsupervised",
+        "X_inner_mtype": "pd.Series",
     }
 
     def __init__(self, threshold, min_cp_distance=0, max_iter=10000):
@@ -202,4 +203,6 @@ class BinarySegmentation(BaseDetector):
             instance.
             ``create_test_instance`` uses the first (or only) dictionary in ``params``
         """
-        return {"threshold": 1}
+        params0 = {"threshold": 1}
+        params1 = {"threshold": 0.1, "min_cp_distance": 1, "max_iter": 100}
+        return [params0, params1]
