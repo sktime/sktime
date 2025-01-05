19df6d98e30fa1d79fff75b899fe60e200c95272
benheid <benheid@users.noreply.github.com>
2025-01-05 21:42:23 +0100

    Add get_test_params

--- sktime/performance_metrics/forecasting/_classes.py
+++ sktime/performance_metrics/forecasting/_classes.py
@@ -820,7 +820,29 @@ class BaseForecastingErrorMetric(BaseMetric):
 
         # else, we expect multioutput to be array-like
         return df.dot(multioutput)
+    
+    @classmethod
+    def get_test_params(cls, parameter_set="default"):
+        """Return testing parameter settings for the estimator.
 
+        Parameters
+        ----------
+        parameter_set : str, default="default"
+            Name of the set of test parameters to return, for use in tests. If no
+            special parameters are defined for a value, will return ``"default"`` set.
+
+        Returns
+        -------
+        params : dict or list of dict, default = {}
+            Parameters to create testing instances of the class
+            Each dict are parameters to construct an "interesting" test instance, i.e.,
+            ``MyClass(**params)`` or ``MyClass(**params[i])`` creates a valid test
+            instance.
+            ``create_test_instance`` uses the first (or only) dictionary in ``params``
+        """
+        params1 = {}
+        params2 = {"evaluate_by_timepoints": True}
+        return [params1, params2]
 
 class BaseForecastingErrorMetricFunc(BaseForecastingErrorMetric):
     """Adapter for numpy metrics."""
