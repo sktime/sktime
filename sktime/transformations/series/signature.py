721add57c2317f0096cc98ab45e64ab646a4f222
AdiTyaPal0710 <adityapal0710@gmail.com>
2025-01-13 15:48:19 +0530

    [ENH] improved signature transformer

--- sktime/transformations/series/signature.py
+++ sktime/transformations/series/signature.py
@@ -3,8 +3,8 @@
 import numpy as np
 import pandas as pd
 from itertools import product
+from joblib import Parallel,delayed
 from sktime.transformations.base import BaseTransformer
-
 
 class SignatureMoments(BaseTransformer):
     """Signature Moments Transformer for multivariate time series.
@@ -57,7 +57,7 @@ class SignatureMoments(BaseTransformer):
     >>> from sktime.transformations.series.signature import SignatureMoments
     >>> from sktime.datasets import load_airline
     >>> y = load_airline()
-    >>> transformer = SignatureMoments(degree=2, use_index=False)
+    >>> transformer = SignatureMoments(degree=2, use_index=True)
     >>> Xt = transformer.fit_transform(y)
     """
 
@@ -72,112 +72,7 @@ class SignatureMoments(BaseTransformer):
         "fit_is_empty": True,
     }
 
-    """
-    def __init__(self, degree=2, use_index=True, normalize_prod=False):
-        self.degree = degree
-        self.use_index = use_index
-        self.normalize_prod = normalize_prod
-        super().__init__()
-
-    
-    def _transform(self, X, y=None):
-        #Compute the signature features for the input time series.
-        n_timepoints, n_channels = X.shape
-
-        if self.use_index:
-            index = np.arange(n_timepoints).reshape(-1, 1)
-            X = np.concatenate((X, index), axis=1)
-            n_channels += 1
-
-        signature_matrix = []
-        instance_data = X.T
-        signature_row, feature_names = self._compute_signature(instance_data)
-        signature_matrix.append(signature_row)
-
-        if self.normalize_prod:
-            signature_matrix = np.power(signature_matrix, 1 / self.degree)
-
-        return pd.DataFrame(signature_matrix, columns=feature_names)
-
-    def _compute_signature(self, data):
-        #Compute signature features for a single instance.
-        n_channels, _ = data.shape
-
-        feature_names = []
-        signature_row = []
-        for length in range(1, self.degree + 1):
-            for indices in np.ndindex((n_channels,) * length):
-                element_mean = self._compute_mean_product(data, indices)
-                signature_row.append(element_mean)
-
-                feature_name = "".join(f"[{i}]" for i in indices)
-                feature_names.append(feature_name)
-
-        return signature_row, feature_names
-
-    def _compute_mean_product(self, data, indices):
-        #Compute mean product of the specified data dimensions.
-        length = len(indices)
-        ix = indices
-
-        if length == 1:
-            return np.mean(data[ix[0], :])
-        elif length == 2:
-            correlation_data = data[ix[0], :, np.newaxis] * data[ix[1], np.newaxis, :]
-            triu_indices = np.triu_indices(correlation_data.shape[1])
-            return np.mean(correlation_data[triu_indices])
-        elif length == 3:
-            correlation_data = (
-                data[ix[0], :, np.newaxis, np.newaxis]
-                * data[ix[1], np.newaxis, :, np.newaxis]
-                * data[ix[2], np.newaxis, np.newaxis, :]
-            )
-            depth = correlation_data.shape[0]
-
-            total_sum, total_len = 0, 0
-
-            for i in range(depth):
-                filtered_matrix = correlation_data[i][i:]
-
-                triu_indices = np.triu_indices(filtered_matrix.shape[0])
-
-                upper_triangular = filtered_matrix[triu_indices]
-
-                total_len += len(upper_triangular)
-                total_sum += np.sum(upper_triangular)
-
-            data_mean = total_sum / total_len
-
-            return data_mean
-        else:
-            raise NotImplementedError("Degree higher than 3 is not implemented.")
-    
-    @classmethod
-    def get_test_params(cls, parameter_set="default"):
-        #Return testing parameter settings for the estimator.
-
-        Parameters
-        ----------
-        parameter_set : str, default="default"
-            Name of the set of test parameters to return, for use in tests. If no
-            special parameters are defined for a value, will return ``"default"`` set.
-
-        Returns
-        -------
-        params : dict or list of dict, default = {}
-            Parameters to create testing instances of the class
-            Each dict are parameters to construct an "interesting" test instance, i.e.,
-            ``MyClass(**params)`` or ``MyClass(**params[i])`` creates a valid test
-            instance.
-            ``create_test_instance`` uses the first (or only) dictionary in ``params``
-        
-        params0 = {"degree": 1, "use_index": True}
-        params1 = {"degree": 3, "use_index": False}
-        params2 = {"degree": 2, "use_index": True, "normalize_prod": True}
-        return [params0, params1, params2]
-    """
-
-    def __init__(self, degree=2, use_index=False, use_smaller_equal=False):
+    def __init__(self, degree=2, use_index=False,use_smaller_equal=False):
         if degree < 1:
             raise ValueError("`degree` must be an integer >= 1.")
         self.degree = degree
@@ -198,9 +93,8 @@ class SignatureMoments(BaseTransformer):
             n_channels += 1
 
         features = {}
-        # Generate combinations (allowing duplicates) for each degree
         for degree in range(1, self.degree + 1):
-            combs = product(range(n_channels), repeat=degree)
+            combs = product(range(n_channels))
             for comb in combs:
                 column_name = "".join(map(str, comb))
                 features[column_name] = self._compute_signature(X, comb)
@@ -232,5 +126,3 @@ class SignatureMoments(BaseTransformer):
         params1 = {"degree": 2, "use_index": True, "use_smaller_equal": True}
         params2 = {"degree": 3, "use_index": False, "use_smaller_equal": True}
         return [params0, params1, params2]
-
-    
