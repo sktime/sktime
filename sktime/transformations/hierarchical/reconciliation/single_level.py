25c2559addb81d8bd189c9272771e682142ff1ad
felipeangelimvieira <felipeangelim@gmail.com>
2025-01-15 07:59:11 -0300

    Properly detect if input is hierarchical dataset, else behave like identity

--- sktime/transformations/hierarchical/reconciliation/single_level.py
+++ sktime/transformations/hierarchical/reconciliation/single_level.py
@@ -4,60 +4,16 @@ import pandas as pd
 
 from sktime.transformations.base import BaseTransformer
 from sktime.transformations.hierarchical.aggregate import Aggregator
+from sktime.transformations.hierarchical.reconciliation._utils import (
+    _is_hierarchical_dataframe,
+    filter_descendants,
+    get_bottom_level_idxs,
+    get_middle_level_aggregators,
+    get_total_level_idxs,
+    loc_series_idxs,
+)
 
 __all__ = ["TopdownShareReconciler", "BottomUpReconciler", "MiddleOutReconciler"]
-
-
-def loc_series_idxs(y, idxs):
-    return y.loc[y.index.droplevel(-1).isin(idxs)]
-
-
-def get_bottom_level_idxs(y):
-    idx = y.index
-    idx = idx.droplevel(-1).unique()
-    return idx[idx.get_level_values(-1) != "__total"]
-
-
-def get_bottom_series(y):
-    bottom_idx = get_bottom_level_idxs(y)
-    return loc_series_idxs(y, bottom_idx)
-
-
-def get_total_level_idxs(y):
-    nlevels = y.index.droplevel(-1).nlevels
-    return pd.Index([tuple(["__total"] * nlevels)])
-
-
-def get_total_series(y):
-    total_idx = get_total_level_idxs(y)
-    return loc_series_idxs(y, total_idx)
-
-
-def get_middle_level_series(y, middle_level):
-    idx = y.index
-    idx = idx.droplevel(-1).unique()
-    idx_middle_level = idx.get_level_values(middle_level)
-    idx_below_middle_level = idx.get_level_values(middle_level + 1)
-    is_middle_level_mask = (idx_below_middle_level == "__total") & (
-        idx_middle_level != "__total"
-    )
-    return idx[is_middle_level_mask]
-
-
-def split_middle_levels(y, middle_level):
-    """
-    Return two series: middle level and above, and middle level and below.
-    """
-    idx = y.index
-    idx = idx.droplevel(-1).unique()
-    idx_middle_level = idx.get_level_values(middle_level)
-    idx_below_middle_level = idx.get_level_values(middle_level + 1)
-    is_middle_or_above_mask = idx_below_middle_level == "__total"
-    is_middle_or_below_mask = idx_middle_level != "__total"
-
-    return loc_series_idxs(y, idx[is_middle_or_above_mask]), loc_series_idxs(
-        y, idx[is_middle_or_below_mask]
-    )
 
 
 _COMMON_TAGS = {
@@ -79,13 +35,15 @@ _COMMON_TAGS = {
         "pd_multiindex_hier",
     ],
     "y_inner_mtype": "None",  # which mtypes do _fit/_predict support for y?
-    "capability:inverse_transform": False,  # does transformer have inverse
-    "skip-inverse-transform": True,  # is inverse-transform skipped when called?
+    "capability:inverse_transform": True,  # does transformer have inverse
+    "skip-inverse-transform": False,  # is inverse-transform skipped when called?
     "univariate-only": False,  # can the transformer handle multivariate X?
     "handles-missing-data": False,  # can estimator handle missing data?
     "X-y-must-have-same-index": False,  # can estimator handle different X/y index?
     "fit_is_empty": False,  # is fit empty and can be skipped? Yes = True
     "transform-returns-same-time-index": False,
+    "hierarchical": True,
+    "hierarchical:reconciliation": True,
 }
 
 
@@ -115,7 +73,7 @@ class TopdownShareReconciler(BaseTransformer):
     _tags = _COMMON_TAGS
 
     def _fit(self, X, y):
-        self._no_hierarchy = X.index.nlevels == 1
+        self._no_hierarchy = not _is_hierarchical_dataframe(X)
 
         if self._no_hierarchy:
             return self
@@ -198,7 +156,7 @@ class BottomUpReconciler(BaseTransformer):
     _tags = _COMMON_TAGS
 
     def _fit(self, X, y):
-        self._no_hierarchy = X.index.nlevels == 1
+        self._no_hierarchy = not _is_hierarchical_dataframe(X)
 
         if self._no_hierarchy:
             return self
@@ -231,37 +189,6 @@ class BottomUpReconciler(BaseTransformer):
         return X
 
 
-from sktime.transformations.base import BaseTransformer
-
-# other imports as needed
-
-
-def get_middle_level_aggregators(X, middle_level):
-    """
-    Identify aggregator nodes at the specified middle_level, i.e.
-    those for which the next level is '__total'.
-
-    Example:
-      If middle_level=1, then for a node= (region, store, cat, dept),
-      we check node[middle_level+1] == '__total'.
-      The node itself must not be __total at middle_level
-      (otherwise it's above or not a well-defined middle node).
-    """
-    idx = X.index.droplevel(-1).unique()
-    if middle_level + 1 >= idx.nlevels:
-        # If the user picks a middle_level at the last level, there's no "next" level
-        return []
-
-    # The aggregator condition:
-    #   * the next level's value is '__total'
-    #   * the middle level's value != '__total'
-    next_level_vals = idx.get_level_values(middle_level + 1)
-    this_level_vals = idx.get_level_values(middle_level)
-
-    is_agg = (next_level_vals == "__total") & (this_level_vals != "__total")
-    return idx[is_agg]
-
-
 class MiddleOutReconciler(BaseTransformer):
     """
     Reconciliation using a middle-out approach.
@@ -276,6 +203,8 @@ class MiddleOutReconciler(BaseTransformer):
         The transformer to use for each subtree below the middle-level totals.
     """
 
+    _tags = _COMMON_TAGS
+
     def __init__(
         self,
         middle_level: int,
@@ -288,7 +217,7 @@ class MiddleOutReconciler(BaseTransformer):
         super().__init__()
 
     def _fit(self, X, y):
-        self._no_hierarchy = X.index.nlevels == 1
+        self._no_hierarchy = not _is_hierarchical_dataframe(X)
         if self._no_hierarchy:
             return self
 
@@ -417,31 +346,18 @@ class MiddleOutReconciler(BaseTransformer):
         _X = _X[~_X.index.duplicated(keep="first")]
         return _X
 
-
-def is_ancestor(agg_node, node):
-    """Returns True if agg_node is an ancestor of node."""
-    return all(a == b or a == "__total" for a, b in zip(agg_node, node))
-
-
-def filter_descendants(X, aggregator_node):
-    """
-    Returns a sub-DataFrame/Series of X containing only rows whose
-    droplevel(-1) is a descendant of 'aggregator_node' at the given 'middle_level'.
-
-    aggregator_node is a tuple like ('CA', 'CA_1', '__total', '__total')
-    or ('regionA', 'storeA', '__total', '__total'), etc.
-    """
-    # We'll operate on the "higher-level" portion of the index
-    # (i.e., the index after dropping the time or final level).
-    idx_upper = X.index.droplevel(-1)  # e.g. (region, store, cat, dept)
-    nodes = idx_upper.unique()
-
-    # We only want to keep those which are descendants of aggregator_node,
-    # i.e., aggregator_node is an ancestor of that node.
-    # Because aggregator_node is "at" middle_level,
-    # but it may also have __total at deeper levels.
-    descendant_mask = [is_ancestor(aggregator_node, n) for n in nodes]
-    descendant_nodes = nodes[descendant_mask]
-
-    # Now filter X by these nodes
-    return X.loc[idx_upper.isin(descendant_nodes)]
+    @classmethod
+    def get_test_params(cls, parameter_set="default"):
+        """Get test params."""
+        return [
+            {
+                "middle_level": 1,
+                "middle_top_reconciler": TopdownShareReconciler(),
+                "middle_bottom_reconciler": BottomUpReconciler(),
+            },
+            {
+                "middle_level": 2,
+                "middle_top_reconciler": TopdownShareReconciler(),
+                "middle_bottom_reconciler": BottomUpReconciler(),
+            },
+        ]
