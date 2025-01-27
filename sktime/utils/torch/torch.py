0cd2549fed8b8a8ef98717a0ea76015db2348824
Jigyasu <jigyasu@outlook.in>
2025-01-27 18:55:01 +0530

    [ENH] Util for Dummy Implementation of Soft Dependencies

---
+++ sktime/utils/torch/torch.py
@@ -0,0 +1,45 @@
+"""Dispatch torch functions to isolate torch."""
+
+from sktime.utils.dependencies import _check_soft_dependencies
+
+# exports torch if torch is present, otherwise provides a dummy implementation
+if _check_soft_dependencies("torch", severity="none"):
+    import torch
+
+else:
+
+    class torch:
+        """Dummy class if torch is unavailable."""
+
+        def __init__(self):
+            raise ImportError(
+                "Please install torch to use this functionality. "
+                "You can install it with `pip install torch`."
+            )
+
+        class nn:
+            """Dummy nn class if torch.nn is unavailable."""
+
+            def __init__(self):
+                raise ImportError(
+                    "Please install torch to use this functionality. "
+                    "You can install it with `pip install torch`."
+                )
+
+            class Module:
+                """Dummy nn.Module class if unavailable."""
+
+                def __init__(self, *args, **kwargs):
+                    raise ImportError(
+                        "Please install torch to use this functionality. "
+                        "You can install it with `pip install torch`."
+                    )
+
+        class Tensor:
+            """Dummy torch.Tensor class if unavailable."""
+
+            def __init__(self, *args, **kwargs):
+                raise ImportError(
+                    "Please install torch to use this functionality. "
+                    "You can install it with `pip install torch`."
+                )
