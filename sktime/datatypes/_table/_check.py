1f4bd06c8261418969b505a9f7da46339095b84b
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2025-01-28 15:51:58 +0530

    [DOC] Fixed `TablePolarsEager ` rendering issues related to recent merged PR #7666 (#7699)
    I fixed the rendering issues and fixed some from the base classes.

--- sktime/datatypes/_table/_check.py
+++ sktime/datatypes/_table/_check.py
@@ -685,23 +685,26 @@ def _check_list_of_dict_table(obj, return_metadata=False, var_name="obj"):
 
 
 class TablePolarsEager(ScitypeTable):
-    """
-    Data type: eager polars DataFrame based specification of data frame table.
+    """Data type: eager polars DataFrame based specification of data frame table.
 
     Name: ``"TablePolarsEager"``
+
     Short description:
         A specification for a data table backed by an eager Polars DataFrame,
         supporting both univariate and multivariate data.
+
     Long description:
         The ``"TablePolarsEager"`` :term:`mtype` is a concrete specification
         that implements the ``Table`` :term:`scitype`, representing a data table
         with an eager Polars DataFrame.
         An object ``obj: Polars DataFrame`` follows the specification iff:
+
         * structure convention: ``obj`` is a Polars DataFrame.
         * feature: the DataFrame can have multiple features (columns).
         * instances: rows of the DataFrame represent individual instances.
         * instance index: The index is implicit, with each row corresponding to
           a unique instance (zero-indexed by default).
+
     Capabilities:
         * supports multivariate data with multiple features.
         * can handle missing values (NaNs) and empty tables.
