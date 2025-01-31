38e24093ad9e105b3455c1aa5d88e353845f1386
Krish Bakshi <krishbakshi23@gmail.com>
2025-01-31 14:52:01 +0530

    Added get_test_params method to macnn.py

--- sktime/networks/macnn.py
+++ sktime/networks/macnn.py
@@ -179,3 +179,38 @@ class MACNNNetwork(BaseDeepNetwork):
         output_layer = keras.layers.GlobalAveragePooling1D()(x)
 
         return input_layer, output_layer
+
+    @classmethod
+    def get_test_params(cls, parameter_set="default"):
+        """Return testing parameter settings for the estimator.
+
+        Parameters
+        ----------
+        parameter_set : str, default="default"
+            Name of the set of test parameters to return, for use in tests. If no
+            special parameters are defined for a value, will return ``"default"`` set.
+            Reserved values for classifiers:
+                "results_comparison" - used for identity testing in some classifiers
+                    should contain parameter settings comparable to "TSC bakeoff"
+
+        Returns
+        -------
+        params : dict or list of dict, default = {}
+            Parameters to create testing instances of the class
+            Each dict are parameters to construct an "interesting" test instance, i.e.,
+            ``MyClass(**params)`` or ``MyClass(**params[i])`` creates a valid test
+            instance.
+            ``create_test_instance`` uses the first (or only) dictionary in ``params``
+        """
+        params1 = {}
+        params2 = {
+            "padding": "valid",
+            "pool_size": 2,
+            "strides": 1,
+            "repeats": 2,
+            "filter_sizes": (64, 128, 256),
+            "kernel_size": (3, 6, 12),
+            "reduction": 16,
+            "random_state": 0,
+        }
+        return [params1, params2]
