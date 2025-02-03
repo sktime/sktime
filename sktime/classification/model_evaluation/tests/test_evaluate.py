9ad4855836f4ea2e4ea36e0900c688ab990fa617
ksharma6 <ksharma6@tutanota.com>
2025-02-03 13:55:48 -0800

    first pass test_evaluate_common_configs

--- sktime/classification/model_evaluation/tests/test_evaluate.py
+++ sktime/classification/model_evaluation/tests/test_evaluate.py
@@ -8,19 +8,30 @@ tested with various configurations for correct output.
 
 __author__ = ["ksharma6"]
 
-__all__ = []
+__all__ = [
+    "test_evaluate_common_configs",
+]
 
 import numpy as np
 import pandas as pd
+import pytest
 from sklearn import metrics
+from sklearn.model_selection import KFold
+
+#from sktime.datasets import load_airline, load_longley
 
 from sktime.classification.base import BaseClassifier
-
-
+from sktime.classification.model_evaluation import evaluate
 from sktime.classification.model_evaluation._functions import (
     _check_scores,
     _get_column_order_and_datatype,
 )
+from sktime.classification.dummy import DummyClassifier
+from sktime.tests.test_switch import run_test_for_class
+
+from sktime.utils._testing.panel import make_classification_problem
+
+from sktime.utils.dependencies import _check_soft_dependencies
 from sktime.utils.parallel import _get_parallel_test_fixtures
 
 METRICS = [
@@ -59,3 +70,48 @@ def _check_evaluate_output(out, cv, y, scoring, return_data, return_model):
             isinstance(f, (BaseClassifier, type(None)))
             for f in out["fitted_classifier"].values
         )
+
+@pytest.mark.skipif(
+    not run_test_for_class(evaluate),
+    reason="run test only if softdeps are present and incrementally (if requested)",
+)
+
+@pytest.mark.parametrize("CV", KFold)
+@pytest.mark.parametrize("strategy", ["refit", "update", "no-update_params"])
+@pytest.mark.parametrize("scoring", METRICS)
+@pytest.mark.parametrize("backend", BACKENDS)
+def test_evaluate_common_configs(
+    CV, scoring, backend
+):
+    """Test evaluate common configs."""
+    # skip test for dask backend if dask is not installed
+    if backend == "dask" and not _check_soft_dependencies("dask", severity="none"):
+        return None
+    
+    X, y = make_classification_problem(n_timepoints=30)
+    classifier = DummyClassifier()
+    cv = CV(n_splits=3, shuffle=False)
+
+    out = evaluate(
+        classifier = classifier,
+        cv=KFold(n_splits=3, shuffle=False),
+        X=X,
+        y=y,
+        scoring=scoring,
+        **backend,
+    )
+    _check_evaluate_output(
+        out=out, cv=cv, y=y, scoring=scoring, return_data=False, return_model=False
+    )
+
+    # check scoring
+    actual = out.loc[:, f"test_{scoring.name}"]
+
+    n_splits = cv.get_n_splits(y)
+    expected = np.empty(n_splits)
+    for i, (train, test) in enumerate(cv.split(y)):
+        c = classifier.clone()
+        c.fit(y.iloc[train])
+        expected[i] = scoring(y.iloc[test], c.predict(), y_train=y.iloc[train])
+
+    np.testing.assert_array_equal(actual, expected)
