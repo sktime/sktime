b7ea6821ec16534ee307dd852dccc71db9559228
ksharma6 <ksharma6@tutanota.com>
2025-02-03 12:37:32 -0800

    updated _check_evaluate_output to include return_model

--- sktime/classification/model_evaluation/tests/test_evaluate.py
+++ sktime/classification/model_evaluation/tests/test_evaluate.py
@@ -14,6 +14,9 @@ import numpy as np
 import pandas as pd
 from sklearn import metrics
 
+from sktime.classification.base import BaseClassifier
+
+
 from sktime.classification.model_evaluation._functions import (
     _check_scores,
     _get_column_order_and_datatype,
@@ -28,11 +31,13 @@ METRICS = [
 BACKENDS = _get_parallel_test_fixtures("estimator")
 
 
-def _check_evaluate_output(out, cv, y, scoring, return_data):
+def _check_evaluate_output(out, cv, y, scoring, return_data, return_model):
     assert isinstance(out, pd.DataFrame)
-    # Check column names
+    # Check column names.
     scoring = _check_scores(scoring)
-    columns = _get_column_order_and_datatype(scoring, return_data)
+    columns = _get_column_order_and_datatype(
+        metric_types=scoring, return_data=return_data, return_model=return_model
+    )
     assert set(out.columns) == columns.keys(), "Columns are not identical"
 
     # Check number of rows against number of splits.
@@ -46,3 +51,11 @@ def _check_evaluate_output(out, cv, y, scoring, return_data):
     np.testing.assert_array_equal(
         out["cutoff"].to_numpy(), y.iloc[cv.get_cutoffs(y)].index.to_numpy()
     )
+
+    # Check fitted models
+    if return_model:
+        assert "fitted_classifier" in out.columns
+        assert all(
+            isinstance(f, (BaseClassifier, type(None)))
+            for f in out["fitted_classifier"].values
+        )
