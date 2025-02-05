9e8dd94eb5438066679d945a600913a5869f9341
Franz Király <f.kiraly@ucl.ac.uk>
2025-02-06 00:02:43 +0100

    Update _convert.py

--- sktime/datatypes/_proba/_convert.py
+++ sktime/datatypes/_proba/_convert.py
@@ -33,7 +33,7 @@ __all__ = ["convert_dict"]
 import numpy as np
 import pandas as pd
 
-from sktime.datatypes._proba._registry import MTYPE_LIST_PROBA
+MTYPE_LIST_PROBA = ["pred_interval", "pred_quantiles"]
 
 ##############################################################
 # methods to convert one machine type to another machine type
