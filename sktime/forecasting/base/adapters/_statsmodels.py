892530150b43f68612ea097e630b6c594083ddc3
Franz Király <f.kiraly@ucl.ac.uk>
2025-02-14 21:24:56 +0100

    Update _statsmodels.py

--- sktime/forecasting/base/adapters/_statsmodels.py
+++ sktime/forecasting/base/adapters/_statsmodels.py
@@ -56,7 +56,7 @@ class _StatsModelsAdapter(BaseForecaster):
         # save info needed for _predict: should these be saved to self._y_metadata?
         self._y_len = len(y)
         self._y_first_index = y.index[0]
-        self._y_name = y.name
+        self._y_was_series_without_name = hasattr(y, "name") and y.name is None
         self._set_cutoff_from_y(y)
 
         # statsmodels does not support the pd.Int64Index as required,
@@ -136,7 +136,8 @@ class _StatsModelsAdapter(BaseForecaster):
         y_pred = y_pred.iloc[fh_int]
         # ensure that name is not added nor removed
         # otherwise this may upset conversion to pd.DataFrame
-        y_pred.name = self._y_name
+        if isinstance(y_pred, pd.Series) and self._y_was_series_without_name:
+            y_pred.name = None
         return y_pred
 
     @staticmethod
