7641805f3a7b519255cb40eb7b95e0ffa60a0fef
Franz Király <f.kiraly@ucl.ac.uk>
2025-03-02 16:34:46 +0100

    Update impute.py

--- sktime/transformations/series/impute.py
+++ sktime/transformations/series/impute.py
@@ -100,7 +100,7 @@ class Imputer(BaseTransformer):
         "scitype:transform-output": "Series",
         # what scitype is returned: Primitives, Series, Panel
         "scitype:instancewise": True,  # is this an instance-wise transform?
-        "X_inner_mtype": ["pd.DataFrame"],
+        "X_inner_mtype": ["pd.DataFrame", "pd-multiindex", "pd_multiindex_hier"],
         # which mtypes do _fit/_predict support for X?
         "y_inner_mtype": "None",  # which mtypes do _fit/_predict support for y?
         "fit_is_empty": False,
