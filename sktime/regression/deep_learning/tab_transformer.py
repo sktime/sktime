c146679fe481f9e42df66e79e76845ba1c4878a8
Ankit-1204 <ankit2003upadhyay@gmail.com>
2025-03-04 19:01:10 +0530

    [ENH] Implement custom dataset, dataloader. remove preprocessing function.

--- sktime/regression/deep_learning/tab_transformer.py
+++ sktime/regression/deep_learning/tab_transformer.py
@@ -1,7 +1,5 @@
 """TabTransformer : Modeling Tabular Data using Contextual Embedding."""
 
-import numpy as np
-
 from sktime.forecasting.base.adapters._pytorch import BaseDeepNetworkPyTorch
 from sktime.utils.dependencies import _check_soft_dependencies
 
@@ -9,15 +7,47 @@ if _check_soft_dependencies("torch", severity="none"):
     import torch
     import torch.nn as nn
     from torch.nn import TransformerEncoder, TransformerEncoderLayer
+    from torch.utils.data import Dataset
 
+    DataSet = Dataset
     NNModule = nn.Module
 else:
 
     class NNModule:
         """Dummy class if torch is unavailable."""
 
+    class DataSet:
+        """Dummy class if torch is unavailable."""
+
 
 __author__ = ["Ankit-1204"]
+
+
+class TabDataset(DataSet):
+    """Implements Pytorch Dataset class for Training TabTransformer."""
+
+    def __init__(self, X, y, cat_idx) -> None:
+        self.X = torch.tensor(X)
+        self.y = torch.tensor(y)
+        if self.y.dim() == 1:
+            self.y = self.y.unsqueeze(1)
+
+        self.cat_idx = cat_idx
+        self._prepare_data()
+
+    def _prepare_data(self):
+        self.x_cat = self.X[:, self.cat_idx].long()
+        self.x_con = self.X[
+            :, [i for i in range(self.X.shape[1]) if i not in self.cat_idx]
+        ]
+
+    def __len__(self):
+        """Get length of the dataset."""
+        return len(self.X)
+
+    def __getitem__(self, index):
+        """Return data point."""
+        return self.x_cat[index], self.x_con[index], self.y[index]
 
 
 class Tab_Transformer(NNModule):
@@ -25,7 +55,7 @@ class Tab_Transformer(NNModule):
 
     Parameters
     ----------
-    num_cat_feat : list,
+    num_cat_class : list,
         array of unique classes for each categorical feature
     num_cont_features : int,
         number of continuos features
@@ -37,50 +67,42 @@ class Tab_Transformer(NNModule):
         number of attention heads
     output_dim : int,
         dimension for network output
-    task : string,
-        classification or regression task.
 
     """
 
     def __init__(
         self,
-        num_cat_feat,
+        num_cat_class,
         num_cont_features,
         embedding_dim,
         n_transformer_layer,
         n_heads,
         output_dim,
-        task="classification",
     ) -> None:
         super().__init__()
-        self.num_cat_feat = num_cat_feat
+        self.num_cat_class = num_cat_class
         self.num_cont_features = num_cont_features
         self.embedding_dim = embedding_dim
         self.n_transformer_layer = n_transformer_layer
         self.n_heads = n_heads
         self.output_dim = output_dim
-        self.task = task
         self.embedding = nn.ModuleList(
-            [nn.Embedding(cat, self.embedding_dim) for cat in self.num_cat_feat]
+            [nn.Embedding(cat, self.embedding_dim) for cat in self.num_cat_class]
         )
         self.transformer_layer = TransformerEncoderLayer(
-            d_model=self.embedding_dim, nhead=self.n_heads
+            d_model=self.embedding_dim, nhead=self.n_heads, batch_first=True
         )
         self.transformer = TransformerEncoder(
             self.transformer_layer, num_layers=self.n_transformer_layer
         )
         self.feed_forward = nn.Sequential(
             nn.Linear(
-                self.embedding_dim * len(self.num_cat_feat) + self.num_cont_features,
+                self.embedding_dim * len(self.num_cat_class) + self.num_cont_features,
                 128,
             ),
             nn.ReLU(),
             nn.Linear(128, self.output_dim),
         )
-        if self.task == "classification":
-            self.activation = nn.Softmax() if self.output_dim > 1 else nn.Sigmoid()
-        else:
-            self.activation = nn.Identity()
 
     def forward(self, x_cat, x_cont):
         """Implement forward for Tab Transformer."""
@@ -91,16 +113,16 @@ class Tab_Transformer(NNModule):
         context = context.view(context.size(0), -1)
         combined = torch.concat([context, x_cont], dim=1)
         feed = self.feed_forward(combined)
-        return self.activation(feed)
+        return feed
 
 
-class TabTransformer(BaseDeepNetworkPyTorch):
+class TabTransformerRegressor(BaseDeepNetworkPyTorch):
     r"""
     Tab Transformer for Tabular Data.
 
     Parameters
     ----------
-    num_cat_feat : list,
+    num_cat_class : list,
         array of unique classes for each categorical feature
     num_cont_features : int,
         number of continuos features
@@ -110,8 +132,6 @@ class TabTransformer(BaseDeepNetworkPyTorch):
         number of transformer layers
     n_heads : int,
         number of attention heads
-    output_dim : int,
-        dimension for network output
     task : string,
         classification or regression task.
 
@@ -124,13 +144,12 @@ class TabTransformer(BaseDeepNetworkPyTorch):
 
     def __init__(
         self,
-        num_cat_feat,
+        num_cat_class,
+        cat_idx,
         num_cont_features,
         embedding_dim,
         n_transformer_layer,
         n_heads,
-        output_dim,
-        task="classification",
         num_epochs=16,
         batch_size=8,
         lr=0.001,
@@ -139,13 +158,12 @@ class TabTransformer(BaseDeepNetworkPyTorch):
         optimizer=None,
         optimizer_kwargs=None,
     ):
-        self.num_cat_feat = num_cat_feat
+        self.num_cat_class = num_cat_class
+        self.cat_idx = cat_idx
         self.num_cont_features = num_cont_features
         self.embedding_dim = embedding_dim
         self.n_transformer_layer = n_transformer_layer
         self.n_heads = n_heads
-        self.output_dim = output_dim
-        self.task = task
         self.num_epochs = num_epochs
         self.batch_size = batch_size
         self.optimizer = optimizer
@@ -155,61 +173,60 @@ class TabTransformer(BaseDeepNetworkPyTorch):
         self.lr = lr
         super().__init__()
         if _check_soft_dependencies("torch", severity="none"):
-            pass
+            import torch
 
-    def _window_classification(self, X, y, window_size=3):
-        r"""Convert Timeseries into Tabular Format.
+            self.criterions = {
+                "MSE": torch.nn.MSELoss,
+                "L1": torch.nn.L1Loss,
+                "SmoothL1": torch.nn.SmoothL1Loss,
+                "Huber": torch.nn.HuberLoss,
+            }
 
-        Parameters
-        ----------
-        window_size : int, Optional (default=3)
-            Defines the size of the sliding window
-        X : 3d numpy array
-            shape : (n_instances, series_length, n_dimensions)
-        y : Required for Classification
+            self.optimizers = {
+                "Adadelta": torch.optim.Adadelta,
+                "Adagrad": torch.optim.Adagrad,
+                "Adam": torch.optim.Adam,
+                "AdamW": torch.optim.AdamW,
+                "SGD": torch.optim.SGD,
+            }
 
-        """
-        x_transf = []
-        y_transf = []
-        for sample_id in range(len(X)):
-            for i in range(X.shape[1] - window_size):
-                x_curr = X[sample_id, i : i + window_size, :].flatten()
-                y_transf.append(y[sample_id])
-                x_transf.append(x_curr)
-        x_transf = np.array(x_transf)
-        y_transf = np.array(y_transf)
+    def _build_network(self, y):
+        self.output_dim = y.shape[0]
+        self.network = Tab_Transformer(
+            self.num_cat_class,
+            self.num_cont_features,
+            self.embedding_dim,
+            self.n_transformer_layer,
+            self.n_heads,
+            self.output_dim,
+        )
 
-        return x_transf, y_transf
+    def build_pytorch_train_dataloader(self, X, y):
+        """Build PyTorch DataLoader for training."""
+        from torch.utils.data import DataLoader
 
-    def _window_regression(self, X, window_size=3):
-        r"""Convert Timeseries into Tabular Format.
-
-        Parameters
-        ----------
-        window_size : int, Optional (default=3)
-            Defines the size of the sliding window
-        X : 3d numpy array
-            shape : (n_instances, series_length, n_dimensions)
-
-        """
-        x_transf = []
-        y_transf = []
-        for sample_id in range(len(X)):
-            for i in range(X.shape[1] - window_size - 1):
-                x_transf.append(X[sample_id, i : i + window_size, :].flatten())
-                y_transf.append(X[sample_id, i + window_size, :])
-        x_transf = np.array(x_transf)
-        y_transf = np.array(y_transf)
-
-        return x_transf, y_transf
+        if self.custom_dataset_train:
+            if hasattr(self.custom_dataset_train, "build_dataset") and callable(
+                self.custom_dataset_train.build_dataset
+            ):
+                self.custom_dataset_train.build_dataset(y)
+                dataset = self.custom_dataset_train
+            else:
+                raise NotImplementedError(
+                    "Custom Dataset `build_dataset` method is not available. Please "
+                    f"refer to the {self.__class__.__name__}.build_dataset "
+                    "documentation."
+                )
+        else:
+            dataset = TabDataset(X=X, y=y, cat_idx=self.cat_idx)
+        return DataLoader(dataset=dataset, batch_size=self.batch_size)
 
     def _fit(self, X, y):
         r"""Fit the model on input.
 
         Parameters
         ----------
-        X : 2d numpy array
-            shape : (n_instances, series_length * n_dimensions)
+        X :
         y : 1d or 2d numpy array
             for classification, shape = (n_instances)
             for regression, shape = (n_instances, n_dimensions)
