9c6724e99c7fb3c62a60c894a46e0020c38a7beb
XinyuWuu <xinyu.wuxinyu@outlook.com>
2025-03-10 12:12:21 +0800

    ColumnEnsembleTransformer

--- sktime/datatypes/_convert_utils/_coerce.py
+++ sktime/datatypes/_convert_utils/_coerce.py
@@ -38,3 +38,35 @@ def _coerce_df_dtypes(obj):
         return obj
 
     return obj
+
+
+def _coerce_variable_name(obj: pd.Series | pd.DataFrame, prefix=""):
+    if isinstance(obj, pd.Series):
+        old_names = [obj.name]
+        obj.name = prefix + "var0"
+        new_names = [obj.name]
+    elif isinstance(obj, pd.DataFrame):
+        old_names = list(obj.columns)
+        obj.columns = [prefix + "var" + str(i) for i in range(len(obj.columns))]
+        new_names = list(obj.columns)
+    else:
+        return obj, [], []
+    return obj, old_names, new_names
+
+
+def _restore_variable_name(
+    obj: pd.Series | pd.DataFrame, old_names: list, new_names: list
+):
+    if isinstance(obj, pd.Series):
+        assert (
+            obj.name == new_names[0]
+        ), f"Series name: {obj.name} not match with new_names: {new_names}"
+        assert len(old_names) == 1, "len(old_names) must equal to 1 for series"
+        obj.name = old_names[0]
+    elif isinstance(obj, pd.DataFrame):
+        assert (
+            list(obj.columns) == new_names
+        ), f"DataFrame name(s): {obj.columns} not match with new_names: {new_names}"
+        obj.columns = old_names
+
+    return obj
