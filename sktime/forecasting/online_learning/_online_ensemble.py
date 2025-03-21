9c07bc56c43a3e391a22d2b179426694ec98ab17
Franz Király <f.kiraly@ucl.ac.uk>
2025-03-21 23:46:22 +0100

    update

--- sktime/forecasting/online_learning/_online_ensemble.py
+++ sktime/forecasting/online_learning/_online_ensemble.py
@@ -107,7 +107,7 @@ class OnlineEnsembleForecaster(EnsembleForecaster):
         if len(y) >= 1 and self.ensemble_algorithm is not None:
             self._fit_ensemble(y, X)
 
-        for forecaster in self.forecasters_:
+        for forecaster in self._get_forecaster_list():
             forecaster.update(y, X, update_params=update_params)
 
         return self
