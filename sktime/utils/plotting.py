24062b9abfa4c80056977d7ba0045a5986f7c186
Prahitha Movva <prahitha.movva03@gmail.com>
2025-03-25 12:33:21 -0400

    added examples

--- sktime/utils/plotting.py
+++ sktime/utils/plotting.py
@@ -181,7 +181,7 @@ def plot_series(
 def plot_interval(ax, interval_df):
     """Plot prediction intervals on an existing matplotlib axes.
 
-    This function overlays prediction intervals on an existing plot to visualize 
+    This function overlays prediction intervals on an existing plot to visualize
     forecast uncertainty.
 
     Parameters
@@ -195,6 +195,41 @@ def plot_interval(ax, interval_df):
     -------
     ax : matplotlib.axes.Axes
         The matplotlib axes with the prediction intervals added.
+
+    Examples
+    --------
+    >>> import pandas as pd
+    >>> import numpy as np
+    >>> import matplotlib.pyplot as plt
+    >>> from sktime.datasets import load_airline
+    >>> from sktime.forecasting.naive import NaiveForecaster
+    >>> from sktime.forecasting.model_selection import temporal_train_test_split
+    >>> from sktime.utils.plotting import plot_interval
+
+    >>> data = load_airline()
+    >>> y_train, y_test = temporal_train_test_split(data, test_size=12)
+
+    >>> forecaster = NaiveForecaster(strategy="last")
+    >>> forecaster.fit(y_train)
+
+    >>> fh = np.arange(1, 13)  # Forecasting for the next 12 months
+    >>> interval_df = forecaster.predict_interval(fh)
+
+    >>> y_train.index = y_train.index.to_timestamp()
+    >>> y_test.index = y_test.index.to_timestamp()
+    >>> interval_df.index = interval_df.index.to_timestamp()
+
+    >>> fig, ax = plt.subplots(figsize=(10, 5))
+    >>> ax.plot(y_train.index, y_train, label='Training Data', color='blue')  # doctest: +SKIP
+    >>> ax.plot(y_test.index, y_test, label='Actual Test Data', color='orange')  # doctest: +SKIP
+
+    >>> plot_interval(ax, interval_df)  # doctest: +SKIP
+
+    >>> ax.set_title('Predictions with Confidence Intervals')  # doctest: +SKIP
+    >>> ax.set_xlabel('Date')  # doctest: +SKIP
+    >>> ax.set_ylabel('Passengers')  # doctest: +SKIP
+    >>> plt.legend()  # doctest: +SKIP
+    >>> plt.show()  # doctest: +SKIP
     """
     import seaborn as sns
 
@@ -470,6 +505,14 @@ def plot_windows(cv, y, title="", ax=None):
         matplotlib figure object
     ax : matplotlib.axes.Axes
         matplotlib axes object with the figure
+
+    Examples
+    --------
+    >>> from sktime.split import ExpandingWindowSplitter
+    >>> from sktime.utils.plotting import plot_windows
+
+    >>> cv = ExpandingWindowSplitter(step_length=1, fh=fh, initial_window=24)
+    >>> plot_windows(cv, y.iloc[:50])  # doctest: +SKIP
     """
     from sktime.utils.dependencies import _check_soft_dependencies
 
@@ -593,6 +636,25 @@ def plot_calibration(y_true, y_pred, ax=None):
         matplotlib figure object
     ax : matplotlib.axes.Axes
         matplotlib axes object with the figure
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from sktime.datasets import load_airline
+    >>> from sktime.forecasting.theta import ThetaForecaster
+    >>> from sktime.utils.plotting import plot_calibration
+
+    >>> y_train = load_airline()[0:24]  # train on 24 months, 1949 and 1950
+    >>> y_test = load_airline()[24:36]  # ground truth for 12 months in 1951
+
+    >>> # try to forecast 12 months ahead, from y_train
+    >>> fh = np.arange(1, 13)
+
+    >>> forecaster = ThetaForecaster(sp=12)
+    >>> forecaster.fit(y_train, fh=fh)  # doctest: +SKIP
+
+    >>> pred_quantiles = forecaster.predict_quantiles(alpha=[0.1, 0.25, 0.5, 0.75, 0.9])
+    >>> plot_calibration(y_true=y_test.loc[pred_quantiles.index], y_pred=pred_quantiles)  # doctest: +SKIP
     """  # noqa: E501
     from sktime.utils.dependencies import _check_soft_dependencies
 
