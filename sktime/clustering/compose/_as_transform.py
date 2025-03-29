sktime/sktime

commit 39bedad0fd9211e30d3a1d880854db5ab2317eb2

[BUG] Handle hierarchical mtype in ClustererAsTransformer (#8013)
#### Reference Issues/PRs
Fixes #8012, fixes https://github.com/sktime/sktime/issues/8036
Solves #8005 

Depends on https://github.com/sktime/sktime/pull/8037

#### What does this implement/fix? Explain your changes.
* adds pd_multiindex_hier for `X_inner_mtype` in ClustererAsTransformer and handles it
* ensures the index of the transform output is correctly set

sktime/clustering/compose/_as_transform.py
@@ -1,7 +1,9 @@
 """Using a clusterer as transformation."""
 
 # copyright: sktime developers, BSD-3-Clause License (see LICENSE file)
-from sktime.datatypes import MTYPE_LIST_PANEL
+import numpy as np
+import pandas as pd
+
 from sktime.transformations.base import BaseTransformer
 
 __author__ = ["fkiraly"]
@@ -45,7 +47,7 @@ class ClustererAsTransformer(BaseTransformer):
     _tags = {
         # packaging info
         # --------------
-        "authors": ["fkiraly"],
+        "authors": ["fkiraly", "felipeangelimvieira"],
         # estimator type
         # --------------
         "scitype:transform-input": "Series",
@@ -54,7 +56,7 @@ class ClustererAsTransformer(BaseTransformer):
         "scitype:instancewise": False,  # is this an instance-wise transform?
         "capability:inverse_transform": False,  # can the transformer inverse transform?
         "univariate-only": False,  # can the transformer handle multivariate X?
-        "X_inner_mtype": MTYPE_LIST_PANEL,
+        "X_inner_mtype": ["pd-multiindex", "pd_multiindex_hier"],
         "y_inner_mtype": "None",  # which mtypes do _fit/_predict support for y?
         "requires_y": False,  # does y need to be passed in fit?
         "fit_is_empty": False,  # is fit empty and can be skipped? Yes = True
@@ -109,8 +111,38 @@ class ClustererAsTransformer(BaseTransformer):
         -------
         self: reference to self
         """
+        if isinstance(X, pd.DataFrame) and X.index.nlevels > 2:
+            self._idx_names = X.index.names
+            X, _ = self._map_hier_to_panel(X)
         self.clusterer_.fit(X)
         return self
+
+    def _map_hier_to_panel(self, X):
+        """Map hierarchical index to panel format."""
+        X = X.copy()
+        unique_series = X.index.droplevel(-1).unique()
+        new_index = np.arange(len(unique_series))
+        # Create a mapping
+        mapping = dict(zip(unique_series, new_index))
+        # Replace levels 0 up to -2 with new index
+        new_index = pd.MultiIndex.from_tuples(
+            [(mapping[x[:-1]], x[-1]) for x in X.index.to_list()],
+            names=["temp_index", X.index.names[-1]],
+        )
+        X.set_index(new_index, inplace=True)
+
+        return X, mapping
+
+    def _map_primitive_to_hier_idx(self, X, mapping):
+        """Convert primitive index to hierarchical index."""
+        X = X.copy()
+        mapping_inv = {v: k for k, v in mapping.items()}
+        new_index = pd.MultiIndex.from_tuples(
+            [mapping_inv[x] for x in X.index.to_list()],
+            names=self._idx_names[:-1],
+        )
+        X.set_index(new_index, inplace=True)
+        return X
 
     def _transform(self, X, y=None):
         """Transform X and return a transformed version.
@@ -129,7 +161,22 @@ class ClustererAsTransformer(BaseTransformer):
         -------
         transformed version of X
         """
-        return self.clusterer_.predict(X)
+        requires_index_mapping = isinstance(X, pd.DataFrame) and X.index.nlevels > 2
+        expected_index = X.index.droplevel(-1).unique()
+
+        if requires_index_mapping:  # map indices to flat index
+            X, mapping = self._map_hier_to_panel(X)
+
+        y_pred = self.clusterer_.predict(X)
+
+        # y_pred is a np.ndarray, we need to convert it to a pd.DataFrame
+        # which also has correct indices and column names
+        y_pred = pd.DataFrame(y_pred, expected_index, columns=["cluster"])
+
+        if requires_index_mapping:  # map flat index back to hierarchical index
+            y_pred = self._map_primitive_to_hier_idx(y_pred, mapping)
+
+        return y_pred
 
     @classmethod
     def get_test_params(cls, parameter_set="default"):

commit 1c30ef61825d4096e4bda153013ba08efcd42d0f

[BUG] Hierarchical data passed to `ClustererAsTransformer` fails (#8053)
#### Reference Issues/PRs

Fixes small bug introduced in #8013. See also #8012, #8036, #8005.


#### What does this implement/fix? Explain your changes.

`ClustererAsTransformer` fails when applied to hierarchical data:
```python
from sktime.utils._testing.hierarchical import _make_hierarchical
from sktime.clustering.compose._as_transform import ClustererAsTransformer
from sktime.clustering.dbscan import TimeSeriesDBSCAN
y_h  = _make_hierarchical()

model = ClustererAsTransformer(TimeSeriesDBSCAN.create_test_instance())
y_pred = model.fit_transform(y_h) # KeyError: ('h0_0', 'h1_0')
```

This was introduced in the clean up phase of #8013, and missed due to
hierarchical tests not running on transformers. The commit that
introduced the bug has been reversed.

Hierarchical tests have also been enabled for transformers.

sktime/clustering/compose/_as_transform.py
@@ -162,10 +162,11 @@ class ClustererAsTransformer(BaseTransformer):
         transformed version of X
         """
         requires_index_mapping = isinstance(X, pd.DataFrame) and X.index.nlevels > 2
-        expected_index = X.index.droplevel(-1).unique()
 
         if requires_index_mapping:  # map indices to flat index
             X, mapping = self._map_hier_to_panel(X)
+
+        expected_index = X.index.droplevel(-1).unique()
 
         y_pred = self.clusterer_.predict(X)
 

sktime/utils/_testing/scenarios_transformers.py
@@ -422,15 +422,25 @@ class TransformerFitTransformHierarchicalUnivariate(TransformerTestScenario):
     _tags = {
         "X_scitype": "Hierarchical",
         "X_univariate": True,
-        "is_enabled": False,
+        "is_enabled": True,
         "has_y": False,
     }
 
     @property
     def args(self):
         return {
-            "fit": {"X": _make_hierarchical(random_state=RAND_SEED)},
-            "transform": {"X": _make_hierarchical(random_state=RAND_SEED + 1)},
+            "fit": {
+                "X": _make_hierarchical(
+                    hierarchy_levels=(2, 2),
+                    random_state=RAND_SEED,
+                )
+            },
+            "transform": {
+                "X": _make_hierarchical(
+                    hierarchy_levels=(2, 2),
+                    random_state=RAND_SEED + 1,
+                )
+            },
         }
 
     default_method_sequence = ["fit", "transform"]
@@ -442,15 +452,27 @@ class TransformerFitTransformHierarchicalMultivariate(TransformerTestScenario):
     _tags = {
         "X_scitype": "Hierarchical",
         "X_univariate": False,
-        "is_enabled": False,
+        "is_enabled": True,
         "has_y": False,
     }
 
     @property
     def args(self):
-        X_trafo = {"X": _make_hierarchical(random_state=RAND_SEED + 1, n_columns=2)}
+        X_trafo = {
+            "X": _make_hierarchical(
+                hierarchy_levels=(2, 2),
+                random_state=RAND_SEED + 1,
+                n_columns=2,
+            )
+        }
         return {
-            "fit": {"X": _make_hierarchical(random_state=RAND_SEED, n_columns=2)},
+            "fit": {
+                "X": _make_hierarchical(
+                    hierarchy_levels=(2, 2),
+                    random_state=RAND_SEED,
+                    n_columns=2,
+                )
+            },
             "transform": X_trafo,
         }
 