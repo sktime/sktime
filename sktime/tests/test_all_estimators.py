sktime/sktime

commit 4ad63da518ded7cbc79b8785e5015443bdbc3304

[ENH] catch `test_fit_idempotent` `predict_proba` soft dependency error (#8076)
This PR adds test case handling for a case missed in
https://github.com/sktime/sktime/pull/8023

sktime/tests/test_all_estimators.py
@@ -1278,12 +1278,16 @@ class TestAllEstimators(BaseFixtureGenerator, QuickTester):
 
         # run fit plus method_nsc once, save results
         set_random_state(estimator)
-        results = scenario.run(
-            estimator,
-            method_sequence=["fit", method_nsc_arraylike],
-            return_all=True,
-            deepcopy_return=True,
-        )
+        if method_nsc_arraylike in ["predict_proba", "predict_var"]:
+            with ValidProbaErrors() as handler:
+                results = scenario.run(
+                    estimator,
+                    method_sequence=["fit", method_nsc_arraylike],
+                    return_all=True,
+                    deepcopy_return=True,
+                )
+            if handler.skipped:
+                return None
 
         estimator = results[0]
         set_random_state(estimator)

commit 97d34a7d6af2171742a3013e30f20b36d54c620c

[BUG] catch `test_fit_idempotent` `predict_proba` soft dependency error - secondary fix (#8080)
Fixes a bug accidentally introduced in
https://github.com/sktime/sktime/pull/8076

sktime/tests/test_all_estimators.py
@@ -1288,6 +1288,13 @@ class TestAllEstimators(BaseFixtureGenerator, QuickTester):
                 )
             if handler.skipped:
                 return None
+        else:
+            results = scenario.run(
+                estimator,
+                method_sequence=["fit", method_nsc_arraylike],
+                return_all=True,
+                deepcopy_return=True,
+            )
 
         estimator = results[0]
         set_random_state(estimator)