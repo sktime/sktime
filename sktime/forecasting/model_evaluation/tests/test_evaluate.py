674c9d13d00f0e5817614e6500684be2e32912eb
luca hoelper <l29265@eon.com>
2025-04-03 02:56:30 +0200

    Remove dependency check in tests

--- sktime/forecasting/model_evaluation/tests/test_evaluate.py
+++ sktime/forecasting/model_evaluation/tests/test_evaluate.py
@@ -171,12 +171,6 @@ def test_evaluate_common_configs(
     CV, fh, window_length, step_length, strategy, scoring, backend
 ):
     """Test evaluate common configs."""
-    # skip test for dask backend if dask is not installed
-    if backend == "dask" and not _check_soft_dependencies("dask", severity="none"):
-        return None
-    # also skip for ray backend if ray is not installed
-    if backend == "ray" and not _check_soft_dependencies("ray", severity="none"):
-        return None
 
     y = make_forecasting_problem(n_timepoints=30, index_type="int")
     forecaster = NaiveForecaster()
@@ -425,12 +419,6 @@ def test_evaluate_error_score(
 @pytest.mark.parametrize("backend", BACKENDS)
 def test_evaluate_hierarchical(backend):
     """Check that evaluate works with hierarchical data."""
-    # skip test for dask backend if dask is not installed
-    if backend == "dask" and not _check_soft_dependencies("dask", severity="none"):
-        return None
-    # also skip for ray backend if ray is not installed
-    if backend == "ray" and not _check_soft_dependencies("ray", severity="none"):
-        return None
 
     y = _make_hierarchical(
         random_state=0, hierarchy_levels=(2, 2), min_timepoints=12, max_timepoints=12
