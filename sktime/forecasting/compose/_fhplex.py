5b10b83faac3d6c3438d44dfa2d7b72d9a5b0ef7
Franz Király <f.kiraly@ucl.ac.uk>
2025-04-05 10:59:47 +0200

    fixes

--- sktime/forecasting/compose/_fhplex.py
+++ sktime/forecasting/compose/_fhplex.py
@@ -64,7 +64,7 @@ class FhPlexForecaster(BaseForecaster):
     >>> f = FhPlexForecaster(NaiveForecaster())
     >>> f.fit(y, fh=[1, 2, 3])
     FhPlexForecaster(...)
-    # get individual fitted forecasters
+    >>> # get individual fitted forecasters
     >>> f.forecasters_  # doctest: +SKIP
     {1: NaiveForecaster(), 2: NaiveForecaster(), 3: NaiveForecaster()}
     >>> fitted_params = f.get_fitted_params()  # or via get_fitted_params
