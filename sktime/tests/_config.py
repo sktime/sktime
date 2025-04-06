e92be1f03506763803b03247a83854eae3e3f678
Franz Király <f.kiraly@ucl.ac.uk>
2025-04-06 16:39:57 +0200

    Update _config.py

--- sktime/tests/_config.py
+++ sktime/tests/_config.py
@@ -278,7 +278,10 @@ EXCLUDED_TESTS = {
     "ARLagOrderSelector": [
         "test_doctest_examples",  # doctest fails, see #8129
     ],
-    "ESRNNForecaster": ["test_persistence_via_pickle"],  # pickling problem, see #8135
+    "ESRNNForecaster": [  # pickling problem, see #8135
+        "test_persistence_via_pickle",
+        "test_save_estimators_to_file",
+    ],
     "TSFreshClassifier": ["test_multiprocessing_idempotent"],  # see 8150
 }
 
