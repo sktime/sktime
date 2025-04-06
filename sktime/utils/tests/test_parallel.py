9c0c5ad7b5ee721a002038e74280b619e928f568
luca hoelper <l29265@eon.com>
2025-04-06 23:28:02 +0200

    [ENH] Add minimal tests for ray backend

---
+++ sktime/utils/tests/test_parallel.py
@@ -0,0 +1,45 @@
+import os
+
+from sktime.utils.parallel import parallelize
+
+
+def test_ray_leaves_params_invariant():
+    def trial_function(params, meta):
+        return params
+
+    backend = "ray"
+    backend_params = {
+        "mute_warnings": True,
+        "ray_remote_args": {"num_cpus": os.cpu_count() - 1},
+    }
+    # copy for later comparison
+    backup = backend_params.copy()
+
+    params = [1, 2, 3]
+    meta = {}
+
+    parallelize(trial_function, params, meta, backend, backend_params)
+
+    assert backup == backend_params
+
+
+def test_ray_adds_one_key_to_params():
+    def trial_function(params, meta):
+        return params
+
+    backend = "ray"
+    backend_params = {
+        "mute_warnings": True,
+    }
+    # copy for later comparison
+    backup = backend_params.copy()
+
+    params = [1, 2, 3]
+    meta = {}
+
+    parallelize(trial_function, params, meta, backend, backend_params)
+    assert len(backup.keys()) == len(backend_params) - 1
+    assert "ray_remote_args" in backend_params.keys()
+    # when the ray_remote_args key gets removed the two should be the same again
+    backend_params.pop("ray_remote_args")
+    assert backup == backend_params
