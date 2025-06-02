bc78d0f29bc7c49bd719a8fc4fd5c651c071728a
benheid <benheid@users.noreply.github.com>
2025-06-02 14:29:32 +0200

    fix linting

--- sktime/performance_metrics/forecasting/_weighted_average_metric.py
+++ sktime/performance_metrics/forecasting/_weighted_average_metric.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 from sktime.performance_metrics.forecasting._classes import (
     BaseForecastingErrorMetric,
     MeanAbsoluteError,
@@ -116,7 +117,9 @@ class WeightedAverageMetric(BaseForecastingErrorMetric):
         """
         total = 0.0
         weights = self.weights if self.normalize else [1.0] * len(self.metrics)
-        weights = np.array(weights) / sum(weights) if self.normalize else np.array(weights) 
+        weights = (
+            np.array(weights) / sum(weights) if self.normalize else np.array(weights)
+        )
         for metric, weight in zip(self.metrics, weights):
             total += weight * metric.evaluate(y_true, y_pred, **kwargs)
         return total
