a359ffd6991098c47ca06eb7bdfa424bf9d67ae3
Victor <victor.tingstrom@gmail.com>
2025-06-23 09:04:42 +0200

    Feature/hurdle model (#1)
    * Refactor demand forecasters to introduce a base class and improve structure
    
    * Add sampling methods for probability and demand in HurdleDemandForecaster
    
    * Enhance demand sampling method to incorporate features for improved regression
    
    * Handles predictions
    
    * Use masking instead to avoid slicing
    
    * Add support for time-varying probability and demand in sampling methods and make method more general
    
    * Adds out-of-sample parameter
    
    * Forgot to change parameter
    
    * Use expit instead
    
    * Adds support for forecasting and TODO
    
    * Removes the zero-inflated Poisson model in favor of the Hurdle-model
    
    * Rename variable
    
    * Uses RecursiveLinearTransform
    
    * Avoid nesting
    
    * Renames the site
    
    * WIP
    
    * bumps alpha
    
    * The offset is already added in the regressors
    
    * Adds docs to the the model
    
    * Adds predictions to notebook
    
    * Typo
    
    * Docs
    
    * Fix handling of overlapping indices in `_get_predict_data`
    
    * length is still original
    
    * rename of site
    
    * rename of site
    
    * adds todo
    
    * typo
    
    * That should do it?
    
    * Fixes construction of prediction frame
    
    * Updates NB
    
    ---------
    
    Co-authored-by: victor <baj>

--- sktime/forecasting/prob_intermittent_demand.py
+++ sktime/forecasting/prob_intermittent_demand.py
@@ -6,9 +6,14 @@ import numpy as np
 import numpyro.handlers
 import pandas as pd
 from numpyro.distributions import (
+    Bernoulli,
+    LogNormal,
     Normal,
-    ZeroInflatedPoisson,
+    Poisson,
+    TransformedDistribution,
+    TruncatedNormal,
 )
+from numpyro.distributions.transforms import RecursiveLinearTransform
 from prophetverse.sktime.base import BaseBayesianForecaster
 from skpro.distributions import Empirical
 from xarray import DataArray
@@ -18,133 +23,57 @@ from sktime.forecasting.base import ForecastingHorizon
 # TODO: think about priors, can we make them more informative?
 
 
-# TODO: replace truncated laplace with a more formalized approach
-class ZeroInflatedPoissonDemandForecaster(BaseBayesianForecaster):
-    r"""Probabilistic Intermittent Demand Forecaster.
-
-    Uses a Bayesian approach to forecast intermittent demand time series by modeling
-    the series as an Zero-Inflated Poisson (ZIP) process. The mathematical model is
-    given by
-    .. math::
-        y_t \sim ZIP(g_t, r_t)
-
-    where :math:`g_t` is the gate parameter and :math:`r_t` is the rate parameter. The
-    gate parameter determines the probability of observing a non-zero value, while the
-    rate parameter determines the expected value of the non-zero observations. The
-    rates and gates can be time-varying or constant, depending on the model
-    configuration. The general model structure is as follows:
-    .. math::
-        \logit{g_t} = \beta_{g, 0} + \beta_{g} \cdot X_t,
-        \log{r_t} = \beta_{r, 0} + \beta_{r} \cdot X_t
-
-    where :math:`X_t` are exogenous variables, and :math:`\beta` are regression
-    coefficients. TODO
-    """
-
-    _tags = {
-        "authors": ["tingiskhan", "felipeangleimvieira"],
-        "maintainers": ["tingiskhan"],
-        "python_version": None,
-        "python_dependencies": ["prophetverse"],
-        "object_type": "forecaster",
-        "scitype:y": "univariate",
-        "ignores-exogeneous-X": False,
-        "capability:insample": True,
-        "capability:pred_int": True,
-        "capability:pred_int:insample": True,
-        "capability:missing_values": True,
-        "y_inner_mtype": "pd.Series",
-        "X_inner_mtype": "pd.DataFrame",
-        "requires-fh-in-fit": False,
-        "X-y-must-have-same-index": True,
-        "enforce_index_type": None,
-        "fit_is_empty": False,
-        "capability:categorical_in_X": True,
-    }
-
-    def __init__(
-        self,
-        time_varying_gate: bool = False,
-        time_varying_rate: bool = False,
-        inference_engine=None,
-    ):
-        super().__init__(scale=1.0, inference_engine=inference_engine)
-
-        if time_varying_gate:
-            raise NotImplementedError(
-                "Time-varying gate parameters are not implemented yet."
-            )
-
-        if time_varying_rate:
-            raise NotImplementedError(
-                "Time-varying rate parameters are not implemented yet."
-            )
-
-        self.time_varying_gate = time_varying_gate
-        self.time_varying_rate = time_varying_rate
+class _BaseProbabilisticDemandForecaster(BaseBayesianForecaster):
+    """Base class for probabilistic intermittent demand forecasters."""
 
     def _get_fit_data(self, y: pd.DataFrame, X: pd.DataFrame, fh: ForecastingHorizon):
         return {
             "length": y.shape[0],
-            "y": jnp.array(y.values),
-            "X": jnp.array(X.values) if X is not None else None,
+            "y": y.values,
+            "X": X.values if X is not None else None,
             "mask": jnp.isfinite(y.values),
         }
 
     def _get_predict_data(self, X: pd.DataFrame, fh: ForecastingHorizon):
         # TODO: handle this better - only append if X is not in self._X
         if X is not None:
-            X = pd.concat([self._X, X], axis=0, verify_integrity=True)
+            temp = self._X.copy()
+            temp.update(X)
+
+            oos_index = X.index.difference(temp.index)
+            if oos_index.size > 0:
+                X = pd.concat([temp, X.loc[oos_index]], axis=0)
 
         index = fh.to_absolute_int(self._y.index[0], self._cutoff)
         oos = fh.to_out_of_sample(self.cutoff).to_numpy().size
 
         return {
-            "length": self._y.shape[0] + oos,
+            "length": self._y.shape[0],
             "y": None,
-            "X": jnp.array(X.values) if X is not None else None,
+            "X": X.values if X is not None else None,
             "oos": oos,
             "index": index.to_numpy(),
             "mask": True,
         }
 
-    def _sample_gate(self, length: int, X: np.ndarray) -> jnp.ndarray:
-        """Sample the gate parameter for the ZIP model."""
-        features = np.ones((length, 1))
+    def _predict_proba(self, marginal=True, **kwargs):
+        if self._is_vectorized:
+            return self._vectorize_predict_method("_predict_components", **kwargs)
 
-        if X is not None:
-            features = np.concatenate((features, X), axis=1)
+        predictive_samples = self._get_predictive_samples_dict(**kwargs)
+        y_hat = predictive_samples["obs"]
 
-        with numpyro.plate("factors", features.shape[-1]):
-            beta = numpyro.sample("beta", Normal())
+        index = kwargs["fh"].to_absolute(self.cutoff).to_numpy()
+        as_array = DataArray(y_hat, dims=["sample", "time"], coords={"time": index})
 
-        regressors = features @ beta
+        as_frame = as_array.to_dataframe(self._y_metadata["feature_names"][0])
 
-        # TODO: add support for time varying gate parameters
-        gate = jax.nn.sigmoid(regressors)
+        return Empirical(as_frame, time_indep=False)
 
-        return gate
-
-    def _sample_rate(
-        self,
-        length: int,
-        X: np.ndarray,
-    ) -> jnp.ndarray:
-        """Sample the log_rate parameter for the ZIP model."""
-        features = np.ones((length, 1))
-
-        if X is not None:
-            features = np.concatenate((features, X), axis=1)
-
-        with numpyro.plate("factors", features.shape[-1]):
-            beta = numpyro.sample("beta", Normal())
-
-        regressors = features @ beta
-
-        # TODO: add support for time varying gate parameters
-        rate = jnp.exp(regressors)
-
-        return rate
+    def _predict(self, fh, X):
+        # TODO: this is technically "wrong" since we should use median rather than mean
+        predictive_samples = self.predict_components(fh=fh, X=X)
+        return predictive_samples["obs"]
 
     def model(
         self,
@@ -177,71 +106,170 @@ class ZeroInflatedPoissonDemandForecaster(BaseBayesianForecaster):
         -------
             Nothing.
         """
-        with numpyro.handlers.scope(prefix="gate"):
-            gate = self._sample_gate(length, X)
+        raise NotImplementedError()
 
-        with numpyro.handlers.scope(prefix="rate"):
-            rate = self._sample_rate(length, X)
 
-        # NB: by indexing the gate and rate we can speed up posterior predictive
-        # sampling
+class HurdleDemandForecaster(_BaseProbabilisticDemandForecaster):
+    r"""Probabilistic Intermittent Demand Forecaster using a hurdle model.
+
+    The mathematics behind the model is the following:
+        .. math::
+            Y_t = \begin{cases}
+                \mathcal{P}(r_t) \text{ if } I_t = 1, \\
+                0 \text{ if } I_t = 0.
+            \end{cases}
+    where
+        .. math::
+            \log{r_t} = \beta_r \cdot X_t + \phi_r \log{r_{t - 1}} + \epsilon_{r, t}, \\
+            \sigma^{-1}(p_t) = \beta_p \cdot X_t + \phi_p \sigma^{-1}(p_{t - 1})
+                + \epsilon_{p, t}, \\
+            I_t \sim \mathcal{B}(p_t),
+    :math:`X` is the exogenous variables, and :math:`\sigma^{-1}` denotes the logit
+    function. The time varying component can be toggled on or off depending on the
+    value of the `time_varying_<probability|demand>` parameter.
+    """
+
+    _tags = {
+        "authors": ["tingiskhan", "felipeangleimvieira"],
+        "maintainers": ["tingiskhan"],
+        "python_version": None,
+        "python_dependencies": ["prophetverse"],
+        "object_type": "forecaster",
+        "scitype:y": "univariate",
+        "ignores-exogeneous-X": False,
+        "capability:insample": True,
+        "capability:pred_int": True,
+        "capability:pred_int:insample": True,
+        "capability:missing_values": True,
+        "y_inner_mtype": "pd.Series",
+        "X_inner_mtype": "pd.DataFrame",
+        "requires-fh-in-fit": False,
+        "X-y-must-have-same-index": True,
+        "enforce_index_type": None,
+        "fit_is_empty": False,
+        "capability:categorical_in_X": True,
+    }
+
+    def __init__(
+        self,
+        time_varying_probability: bool = False,
+        time_varying_demand: bool = False,
+        inference_engine=None,
+    ):
+        super().__init__(scale=1.0, inference_engine=inference_engine)
+
+        self.time_varying_probability = time_varying_probability
+        self.time_varying_demand = time_varying_demand
+
+    def _sample_parameters(
+        self, length: int, X: np.ndarray, time_regressor: bool = False, oos: int = 0
+    ) -> jnp.ndarray:
+        features = np.ones((length + oos, 1))
+
+        if X is not None:
+            features = np.concatenate((features, X), axis=1)
+
+        with numpyro.plate("factors", features.shape[-1]):
+            beta = numpyro.sample("beta", Normal())
+
+        regressors = features @ beta
+
+        if not time_regressor:
+            return regressors
+
+        sigma = numpyro.sample("sigma", LogNormal()) ** 0.5
+        reversion_speed = numpyro.sample("phi", TruncatedNormal(low=-1.0, high=1.0))
+
+        alpha = jnp.zeros((1, 1))
+
+        transition_matrix = reversion_speed.reshape((1, 1))
+        alpha = alpha.reshape((1, 1))
+
+        eps = Normal(loc=alpha, scale=sigma).expand((length, 1)).to_event(1)
+
+        time_varying_component = numpyro.sample(
+            "x",
+            TransformedDistribution(
+                eps, RecursiveLinearTransform(transition_matrix=transition_matrix)
+            ),
+        ).squeeze(1)
+
+        if oos > 0:
+            mean = jnp.eye(oos, 1) * time_varying_component[-1] + alpha
+            eps_oos = Normal(mean, sigma).to_event(1)
+
+            x_oos = numpyro.sample(
+                "x_oos",
+                TransformedDistribution(
+                    eps_oos,
+                    RecursiveLinearTransform(transition_matrix=transition_matrix),
+                ),
+            ).squeeze(1)
+
+            time_varying_component = jnp.concatenate(
+                (time_varying_component, x_oos), axis=0
+            )
+
+        return regressors + time_varying_component
+
+    def _sample_probability(
+        self, length: int, X: np.ndarray, oos: int = 0
+    ) -> jnp.ndarray:
+        logit_prob = self._sample_parameters(
+            length=length, X=X, time_regressor=self.time_varying_probability, oos=oos
+        )
+        prob = jax.scipy.special.expit(logit_prob)
+
+        return prob
+
+    def _sample_demand(self, length: int, X: np.ndarray, oos: int = 0) -> jnp.ndarray:
+        log_demand = self._sample_parameters(
+            length=length, time_regressor=self.time_varying_demand, X=X, oos=oos
+        )
+        demand = jnp.exp(log_demand)
+
+        return demand
+
+    def model(  # noqa: D102
+        self,
+        length: int,
+        y: jnp.ndarray,
+        X: np.ndarray,
+        mask: jnp.ndarray,
+        oos: int = 0,
+        index: np.array = None,
+    ):
+        with numpyro.handlers.scope(prefix="probability"):
+            prob = self._sample_probability(length, X, oos=oos)
+
+        with numpyro.handlers.scope(prefix="demand"):
+            demand = self._sample_demand(length, X, oos=oos)
+
         if index is not None:
-            gate = gate[index]
-            rate = rate[index]
+            prob = prob[index]
+            demand = demand[index]
+
+            events = None
+            events_mask = True
+            observed_demand = None
+        else:
+            events = y > 0.0
+            events_mask = events
+            observed_demand = y
 
         with numpyro.handlers.mask(mask=mask):
-            sampled_y = numpyro.sample(
-                "y:ignore", ZeroInflatedPoisson(1.0 - gate, rate), obs=y
+            sampled_events = numpyro.sample(
+                "events:ignore", Bernoulli(prob), obs=events
+            )
+
+        with numpyro.handlers.mask(mask=events_mask & mask):
+            sampled_demand = numpyro.sample(
+                "demand:ignore", Poisson(demand), obs=observed_demand
             )
 
         if oos == 0:
             return
 
-        numpyro.deterministic("observed", sampled_y)
-        numpyro.deterministic("rate", rate)
-        numpyro.deterministic("gate", gate)
+        numpyro.deterministic("obs", sampled_events * sampled_demand)
 
         return
-
-    def _predict(self, fh, X):
-        predictive_samples = self.predict_components(fh=fh, X=X)
-        return predictive_samples["observed"]
-
-    def predict_components(self, fh: ForecastingHorizon, X: pd.DataFrame = None):  # noqa: D102
-        if self._is_vectorized:
-            return self._vectorize_predict_method("predict_components", X=X, fh=fh)
-
-        fh_as_index = self.fh_to_index(fh)
-
-        X_inner = self._check_X(X=X)
-        predictive_samples_ = self._get_predictive_samples_dict(fh=fh, X=X_inner)
-
-        mapping = {
-            "observed": np.median,
-            "rate": np.mean,
-            "gate": np.mean,
-        }
-
-        out = pd.DataFrame(
-            data={
-                site: mapping[site](data, axis=0).flatten()
-                for site, data in predictive_samples_.items()
-            },
-            index=self.periodindex_to_multiindex(fh_as_index),
-        ).sort_index()
-
-        return self._inv_scale_y(out)
-
-    def _predict_proba(self, marginal=True, **kwargs):
-        if self._is_vectorized:
-            return self._vectorize_predict_method("_predict_components", **kwargs)
-
-        predictive_samples = self._get_predictive_samples_dict(**kwargs)
-        y_hat = predictive_samples["observed"]
-
-        index = kwargs["fh"].to_absolute(self.cutoff).to_numpy()
-        as_array = DataArray(y_hat, dims=["sample", "time"], coords={"time": index})
-
-        as_frame = as_array.to_dataframe(self._y_metadata["feature_names"][0])
-
-        return Empirical(as_frame, time_indep=False)
