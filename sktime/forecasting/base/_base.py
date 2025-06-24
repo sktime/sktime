d9cdcb1a1da3a15e7004d22575d626c1eb19a21e
Parasa V Prajwal <74557086+pvprajwal@users.noreply.github.com>
2025-06-24 12:22:43 +0530

    Fix `_predict_moving_cutoff` to correctly align exogenous features
    Fixes #8394
    
    Updated to use correct subset of exogenous features (`X_new`) in `update_predict_single` to prevent index misalignment

--- sktime/forecasting/base/_base.py
+++ sktime/forecasting/base/_base.py
@@ -2296,13 +2296,14 @@ class BaseForecaster(_PredictProbaMixin, BaseEstimator):
         # iterate over data
         for new_window, _ in cv.split(y):
             y_new = y.iloc[new_window]
+            X_new = X.iloc[new_window] if X is not None else X
 
             # we use `update_predict_single` here
             #  this updates the forecasting horizon
             y_pred = self_copy.update_predict_single(
                 y=y_new,
                 fh=fh,
-                X=X,
+                X=X_new,
                 update_params=update_params,
             )
             y_preds.append(y_pred)
