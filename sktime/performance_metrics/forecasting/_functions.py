9c4a618c1886f2a9a0c720a3055d9069b7983478
administrator <33958609+andretheronsa@users.noreply.github.com>
2025-06-26 09:44:31 +0200

    Port 1.6 sklearn _check_reg_targets

--- sktime/performance_metrics/forecasting/_functions.py
+++ sktime/performance_metrics/forecasting/_functions.py
@@ -13,7 +13,7 @@ from scipy.stats import gmean
 from sklearn.metrics import mean_absolute_error as _mean_absolute_error
 from sklearn.metrics import mean_squared_error as _mean_squared_error
 from sklearn.metrics import median_absolute_error as _median_absolute_error
-from sklearn.metrics._regression import _check_reg_targets
+from sktime.utils.sklearn import _check_reg_targets
 from sklearn.utils.stats import _weighted_percentile
 from sklearn.utils.validation import check_consistent_length
 
