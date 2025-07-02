93fad44d1623c30eaa5e48c2f4f105d80cf4278c
Franz Király <fkiraly@gcos.ai>
2025-07-02 19:11:53 +0200

    Update chronos_bolt.py

--- sktime/libs/chronos/chronos_bolt.py
+++ sktime/libs/chronos/chronos_bolt.py
@@ -31,7 +31,7 @@ logger = logging.getLogger(__file__)
 
 # workaround condition for the case where ModelOutput cannot be imported
 # using the mock class does not work here
-if ModelOutput.__class__.__name__ == "CommonMagicMeta"
+if ModelOutput.__class__.__name__ == "CommonMagicMeta":
 
     class ModelOutput:
         """Dummy model output if transformers is unavailable."""
