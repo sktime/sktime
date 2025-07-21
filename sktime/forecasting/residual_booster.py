0b3dbfd495b33735f91f1eada3661e7a38dc9eed
sanchaysingh <sanchay072@gmail.com>
2025-07-21 16:14:01 +0530

    Should run now

--- sktime/forecasting/residual_booster.py
+++ sktime/forecasting/residual_booster.py
@@ -15,7 +15,8 @@ from sktime.forecasting.base import BaseForecaster
 
 
 class ResidualBoostingForecaster(BaseForecaster):
-    """Residual boosting: add exogenous support to any base forecaster.
+    """
+    Residual boosting: add exogenous support to any base forecaster.
 
     Parameters
     ----------
@@ -23,16 +24,54 @@ class ResidualBoostingForecaster(BaseForecaster):
         Point-forecast model that may ignore X.
     residual_forecaster : sktime forecaster
         Model trained on the base model's in-sample residuals.
+
+
+    Example
+    -------
+    >>> from sktime.datasets import load_airline
+    >>> from sktime.forecasting.naive import NaiveForecaster
+    >>> from sktime.forecasting.arima import ARIMA
+    >>> from sktime.forecasting import ResidualBoostingForecaster
+    >>> y = load_airline()
+    >>> fh = [1, 2, 3]
+    >>> booster = ResidualBoostingForecaster(
+    ...     base_forecaster=ARIMA(order=(1, 0, 0)),
+    ...     residual_forecaster=NaiveForecaster(strategy="mean"),
+    ... )
+    >>> booster.fit(y, fh=fh)
+    >>> y_pred = booster.predict(fh)
+    >>> print(y_pred.head())
     """
 
     _tags = {
         "authors": ["Sanchay117"],
+        "capability:pred_int": False,
+        "capability:exogenous": False,
+        "capability:missing_values": False,
     }
 
     def __init__(self, base_forecaster, residual_forecaster):
         self.base_forecaster = base_forecaster
         self.residual_forecaster = residual_forecaster
         super().__init__()
+
+        exog = self.base_forecaster.get_tag(
+            "capability:exogenous"
+        ) or self.residual_forecaster.get_tag("capability:exogenous")
+
+        miss = self.base_forecaster.get_tag(
+            "capability:missing_values"
+        ) and self.residual_forecaster.get_tag("capability:missing_values")
+
+        pred_int = self.residual_forecaster.get_tag("capability:pred_int")
+
+        self.set_tags(
+            **{
+                "capability:exogenous": exog,
+                "capability:missing_values": miss,
+                "capability:pred_int": pred_int,
+            }
+        )
 
     def _fit(self, y, X=None, fh=None):
         """
@@ -43,23 +82,20 @@ class ResidualBoostingForecaster(BaseForecaster):
         2. Fit clone B of base_forecaster to X, y, with fh
         3. Fit clone of residual_forecaster to X, r
         """
-        # 1) base clone A for residual extraction
-        base_forecaster_copy = clone(self.base_forecaster)
-        base_forecaster_copy.fit(y=y, X=X, fh=None)
-        y_pred = base_forecaster_copy.predict_in_sample()
-        residuals = y - y_pred
+        # clone A: fit on (y,X) to obtain in-sample residuals
+        self.base_insample_ = clone(self.base_forecaster).fit(y, X, fh)
+        residuals = self.base_insample_.predict_residuals(y=y, X=X)
 
-        # 2) base clone B for future prediction
-        self.base_forecaster_copy_fh = clone(self.base_forecaster)
-        self.base_forecaster_copy_fh.fit(y=y, X=X, fh=fh)
+        # clone B: fit a fresh copy that knows the final fh
+        self.base_future_ = clone(self.base_forecaster).fit(y, X, fh)
 
-        # 3) residual forecaster on residuals
-        self.residual_forecaster_copy = clone(self.residual_forecaster)
-        self.residual_forecaster_copy.fit(y=residuals, X=X, fh=fh)
-
+        # clone C: fit residual model on errors
+        self.residual_forecaster_ = clone(self.residual_forecaster).fit(
+            residuals, X, fh
+        )
         return self
 
-    def _predict(self, fh, X=None):
+    def _predict(self, fh=None, X=None):
         """
         Forecast = base forecast + residual forecast.
 
@@ -67,8 +103,8 @@ class ResidualBoostingForecaster(BaseForecaster):
         2. Use residual_forecaster clone to obtain a prediction y_pred_resid
         3. Return y_pred_base + y_pred_resid
         """
-        y_base = self.base_forecaster_copy_fh.predict(fh=fh, X=X)
-        y_resid = self.residual_forecaster_copy.predict(fh=fh, X=X)
+        y_base = self.base_future_.predict(fh=fh, X=X)
+        y_resid = self.residual_forecaster_.predict(fh=fh, X=X)
         return y_base + y_resid
 
     @classmethod
@@ -86,14 +122,16 @@ class ResidualBoostingForecaster(BaseForecaster):
         params : dict or list of dict
             Parameters to create test instances of the estimator.
         """
+        from sktime.forecasting.arima import ARIMA
         from sktime.forecasting.naive import NaiveForecaster
 
         params1 = {
             "base_forecaster": NaiveForecaster(strategy="last"),
             "residual_forecaster": NaiveForecaster(strategy="mean"),
         }
+
         params2 = {
-            "base_forecaster": NaiveForecaster(strategy="drift"),
-            "residual_forecaster": NaiveForecaster(strategy="last"),
+            "base_forecaster": ARIMA(order=(1, 0, 0)),
+            "residual_forecaster": NaiveForecaster(strategy="mean"),
         }
         return [params1, params2]
