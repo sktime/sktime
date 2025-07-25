ff592d6588a2b6039a3bad104e6969d5a51e2615
Julian Fong <44014224+julian-fong@users.noreply.github.com>
2025-07-25 15:46:14 -0400

    Update install_osx_dependencies.sh

--- .github/scripts/install_osx_dependencies.sh
+++ .github/scripts/install_osx_dependencies.sh
@@ -4,17 +4,19 @@
 # It is called by test workflow in .github/workflows/
 # To build necessary system dependencies for MacOS, run:
 
-# check if os is MacOS using uname
 if [ "$(uname)" = "Darwin" ]; then
-    # install necessary dependencies
     echo "installing necessary dependencies..."
     brew install libomp
+
+    LIBOMP_PREFIX="$(brew --prefix libomp)"
+
     echo "Verifying libomp installation..."
-    ls /opt/homebrew/opt/libomp/lib/libomp.dylib
+    ls "$LIBOMP_PREFIX/lib/libomp.dylib"
+
     {
-        echo "DYLD_LIBRARY_PATH=/usr/local/opt/libomp/lib:\$DYLD_LIBRARY_PATH"
-        echo "LDFLAGS=-L/usr/local/opt/libomp/lib"
-        echo "CPPFLAGS=-I/usr/local/opt/libomp/include"
+        echo "DYLD_LIBRARY_PATH=$LIBOMP_PREFIX/lib:\$DYLD_LIBRARY_PATH"
+        echo "LDFLAGS=-L$LIBOMP_PREFIX/lib"
+        echo "CPPFLAGS=-I$LIBOMP_PREFIX/include"
     } >> "$GITHUB_ENV"
 else
     echo "This script is intended to run on macOS (Darwin)."
