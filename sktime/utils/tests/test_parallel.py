396639f9f16b799a1dac77ed088f1de8c0afa8a0
luca hoelper <l29265@eon.com>
2025-07-27 08:32:04 +0200

    Ray backend now makes a copy of backend params

--- sktime/utils/tests/test_parallel.py
+++ sktime/utils/tests/test_parallel.py
@@ -30,30 +30,3 @@ def test_ray_leaves_params_invariant():
     parallelize(trial_function, params, meta, backend, backend_params)
 
     assert backup == backend_params
-
-
-@pytest.mark.skipif(
-    not run_test_module_changed("sktime.utils.parallel")
-    or not _check_soft_dependencies("ray", severity="none"),
-    reason="Execute tests for iff anything in the module has changed",
-)
-def test_ray_adds_one_key_to_params():
-    def trial_function(params, meta):
-        return params
-
-    backend = "ray"
-    backend_params = {
-        "mute_warnings": True,
-    }
-    # copy for later comparison
-    backup = backend_params.copy()
-
-    params = [1, 2, 3]
-    meta = {}
-
-    parallelize(trial_function, params, meta, backend, backend_params)
-    assert len(backup.keys()) == len(backend_params) - 1
-    assert "ray_remote_args" in backend_params.keys()
-    # when the ray_remote_args key gets removed the two should be the same again
-    backend_params.pop("ray_remote_args")
-    assert backup == backend_params
