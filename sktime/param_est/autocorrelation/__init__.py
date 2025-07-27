9966a5b3cd60e50820f4f4eb9436eb3e1edc30aa
Franz Király <fkiraly@gcos.ai>
2025-07-27 13:26:05 +0200

    move to lag folder

--- sktime/param_est/autocorrelation/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Module for parameter estimators of autocorrelation tests."""
-
-__author__ = ["HarshvirSandhu"]
-__all__ = [
-    "AcorrLjungbox",
-]
-from sktime.param_est.autocorrelation._acorrljungbox import AcorrLjungbox
