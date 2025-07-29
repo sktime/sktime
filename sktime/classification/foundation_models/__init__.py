a416003d342ca77f3210b452004202fc5f262725
julian fong <cyfongj@gmail.com>
2025-07-29 11:00:03 -0400

    diagnostic

--- sktime/classification/foundation_models/__init__.py
+++ sktime/classification/foundation_models/__init__.py
@@ -4,4 +4,4 @@ __all__ = [
     "MomentFMClassifier",
 ]
 
-from sktime.classification.foundation_models.momentfm import MomentFMClassifier
+# from sktime.classification.foundation_models.momentfm import MomentFMClassifier
