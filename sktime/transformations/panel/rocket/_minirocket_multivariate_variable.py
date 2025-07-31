d0da87bea180bc19d62d4ee52816fca6e63cdb9e
IsaiasRT <isaiasruizt@gmail.com>
2025-07-30 21:59:09 -0400

    fixed some commit errors

--- sktime/transformations/panel/rocket/_minirocket_multivariate_variable.py
+++ sktime/transformations/panel/rocket/_minirocket_multivariate_variable.py
@@ -346,3 +346,36 @@ def _nested_dataframe_to_transposed2D_array_and_len_list(
         raise ValueError("X_new and lengths do not match. check input dimension")
 
     return X_2d_t, lengths
+
+
+@classmethod
+def get_test_params(cls, parameter_set="default"):
+    """Return testing parameter sets for the estimator.
+
+    Parameters
+    ----------
+    parameter_set : str, default="default"
+        Name of the set of test parameters to return, for use in tests. If no
+        special parameters are defined for a value, will return `"default"` set.
+
+    Returns
+    -------
+    params : dict or list of dict, default={}
+        Parameters to create testing instances of the class.
+        Each dict are parameters to construct an "interesting" test instance, i.e.,
+        `MyClass(**params)` or `MyClass(**params[i])` creates a valid test instance.
+        `create_test_instance` uses the first (or only) dictionary in `params`.
+    """
+    params1 = {
+        "num_kernels": 84,
+        "random_state": 0,
+    }
+
+    params2 = {
+        "num_kernels": 252,
+        "random_state": None,
+    }
+
+    params3 = {}
+
+    return [params1, params2, params3]
