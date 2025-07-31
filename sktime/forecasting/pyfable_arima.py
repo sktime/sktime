902608da3e986f4393e696a49dcae62ea32dd25f
ericjb <ericjberger@gmail.com>
2025-07-31 22:56:02 +0300

    convert python df column to be used for tsibble index to character

--- sktime/forecasting/pyfable_arima.py
+++ sktime/forecasting/pyfable_arima.py
@@ -187,6 +187,8 @@ class PyFableARIMA(BaseForecaster):
         if isinstance(Z.index, pd.PeriodIndex):
             Z[date_col_name] = Z.index.to_timestamp()
 
+        Z[date_col_name] = Z[date_col_name].dt.strftime("%Y-%m-%d")
+
         # Convert the pandas DataFrame to an R data frame
         with localconverter(robjects.default_converter + pandas2ri.converter):
             r_data = robjects.conversion.py2rpy(Z)
@@ -291,9 +293,9 @@ class PyFableARIMA(BaseForecaster):
         import rpy2.robjects as robjects
 
         fitted_values = robjects.r["fitted"](self._fit_auto_arima_)
-        fitted_values_series = robjects.pandas2ri.rpy2py(fitted_values)
-        fitted_values_series.index = self._fit_index_
-        series = fitted_values_series[".fitted"].squeeze()
+        fitted_values_df = robjects.pandas2ri.rpy2py(fitted_values)
+        fitted_values_df.index = self._fit_index_
+        series = fitted_values_df[".fitted"].squeeze()
         series.name = self._y.name
 
         return series
