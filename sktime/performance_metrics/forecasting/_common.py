3d0e305867f9d65ab28eedf689070329f72c493d
Franz Király <fkiraly@gcos.ai>
2025-08-05 13:27:03 +0200

    [DOC] correct and improve `_percentage_error` docstring and related docstrings (#8640)
    corrects and improves `_percentage_error` docstring and related
    docstrings

--- sktime/performance_metrics/forecasting/_common.py
+++ sktime/performance_metrics/forecasting/_common.py
@@ -4,21 +4,32 @@ import numpy as np
 
 
 def _relative_error(y_true, y_pred, y_pred_benchmark, eps=None):
-    """Relative error for observations to benchmark method.
+    r"""Relative error with respect to benchmark predictions.
+
+    For arrays ``y_true``, ``y_pred``, and ``y_pred_benchmark``,
+    writing :math:`y` for ``y_true``, writing :math:`\widehat{y}` for ``y_pred``,
+    and writing :math:`\widehat{y}_b` for ``y_pred_benchmark``,
+    this function calculates the element-wise relative error,
+    of :math:`\widehat{y}` with respect to the benchmark :math:`\widehat{y}_b`,
+    defined as
+
+    .. math::
+        \frac{y - \widehat{y}}{\widehat{y} - \widehat{y}_b}
+
+    where all operations are element-wise.
+
+    The denominator is replaced by ``eps`` if it is smaller than ``eps``, entry-wise.
 
     Parameters
     ----------
-    y_true : pandas Series, pandas DataFrame or NumPy array of
-            shape (fh,) or (fh, n_outputs) where fh is the forecasting horizon
+    y_true : array-like of ground truth values
         Ground truth (correct) target values.
 
-    y_pred : pandas Series, pandas DataFrame or NumPy array of
-            shape (fh,) or (fh, n_outputs) where fh is the forecasting horizon
-        Forecasted values.
+    y_pred : array-like of predicted values, must be same shape as y_true
+        Predicted values.
 
-    y_pred_benchmark : pd.Series, pd.DataFrame or np.array of shape (fh,) or \
-             (fh, n_outputs) where fh is the forecasting horizon, default=None
-        Forecasted values from benchmark method.
+    y_pred_benchmark : array-like of benchmark predictions, must be same shape as y_true
+        Benchmark predictions to compare against.
 
     eps : float, default=None
         Numerical epsilon used in denominator to avoid division by zero.
@@ -27,8 +38,8 @@ def _relative_error(y_true, y_pred, y_pred_benchmark, eps=None):
 
     Returns
     -------
-    relative_error : float
-        relative error
+    relative_error : np.ndarray, same shape as y_true and y_pred
+        The element-wise relative error.
 
     References
     ----------
@@ -47,23 +58,40 @@ def _relative_error(y_true, y_pred, y_pred_benchmark, eps=None):
 
 
 def _percentage_error(y_true, y_pred, symmetric=False, relative_to="y_true", eps=None):
-    """Percentage error.
+    r"""Percentage error.
+
+    For arrays ``y_true`` and ``y_pred``,
+    writing :math:`y` for ``y_true`` and :math:`\widehat{y}` for ``y_pred``,
+    this function calculates the element-wise percentage error, defined as
+
+    * :math:`\frac{|y - \widehat{y}|}{|y|}` if ``symmetric`` is ``False``,
+      and ``relative_to`` is ``'y_true'``,
+    * :math:`\frac{|y - \widehat{y}|}{|\widehat{y}|}` if ``symmetric`` is ``False``,
+      and ``relative_to`` is ``'y_pred'``,
+    * :math:`2 \frac{|y - \widehat{y}|}{|y| + |\widehat{y}|}`
+      if ``symmetric`` is ``True``,
+
+    where all operations are element-wise.
+
+    All denominators are replaced by ``eps`` if they are smaller than ``eps``,
+    entry-wise.
 
     Parameters
     ----------
-    y_true : pd.Series, pd.DataFrame or np.array of shape (fh,) or (fh, n_outputs) \
-             where fh is the forecasting horizon
+    y_true : array-like of ground truth values
         Ground truth (correct) target values.
 
-    y_pred : pd.Series, pd.DataFrame or np.array of shape (fh,) or (fh, n_outputs) \
-             where fh is the forecasting horizon
-        Forecasted values.
+    y_pred : array-like of predicted values, must be same shape as y_true
+        Predicted values.
 
     symmetric : bool, default = False
         Whether to calculate symmetric percentage error.
 
-    relative_to : bool, default = "y_true"
-        Whether to calculate percentage error by forecast.
+    relative_to : {'y_true', 'y_pred'}, default='y_true'
+        Determines the denominator of the percentage error.
+
+        * If ``'y_true'``, the denominator is the true values,
+        * If ``'y_pred'``, the denominator is the predicted values.
 
     eps : float, default=None
         Numerical epsilon used in denominator to avoid division by zero.
@@ -72,7 +100,8 @@ def _percentage_error(y_true, y_pred, symmetric=False, relative_to="y_true", eps
 
     Returns
     -------
-    percentage_error : float
+    percentage_error : np.ndarray, same shape as y_true and y_pred
+        The element-wise percentage error.
 
     References
     ----------
