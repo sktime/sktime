5f71e4c704b367a7b7d8eff252405a29e3970d67
Franz Király <fkiraly@gcos.ai>
2025-08-06 07:37:59 +0200

    Update cntc.py

--- sktime/classification/deep_learning/cntc.py
+++ sktime/classification/deep_learning/cntc.py
@@ -296,3 +296,35 @@ class CNTCClassifier(BaseDeepClassifier):
             probs = np.hstack([1 - probs, probs])
         probs = probs / probs.sum(axis=1, keepdims=1)
         return probs
+
+    @classmethod
+    def get_test_params(cls, parameter_set="default"):
+        """Return testing parameter settings for the estimator.
+
+        Parameters
+        ----------
+        parameter_set : str, default="default"
+            Name of the set of test parameters to return, for use in tests. If no
+            special parameters are defined for a value, will return ``"default"`` set.
+            For classifiers, a "default" set of parameters should be provided for
+            general testing, and a "results_comparison" set for comparing against
+            previously recorded results if the general set does not produce suitable
+            probabilities to compare against.
+
+        Returns
+        -------
+        params : dict or list of dict, default={}
+            Parameters to create testing instances of the class.
+            Each dict are parameters to construct an "interesting" test instance, i.e.,
+            ``MyClass(**params)`` or ``MyClass(**params[i])`` creates a valid test
+            instance.
+            ``create_test_instance`` uses the first (or only) dictionary in ``params``.
+        """
+        param0 = {"n_epochs": 10, "batch_size": 4}
+        param1 = {
+            "n_epochs": 10,
+            "batch_size": 4,
+            "rnn_size": 16,
+            "lstm_size": 16,
+        }
+        return [param0, param1]
