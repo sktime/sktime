7ab3f46d408660a5726982438f9d4a7ec539c09d
Orest Alickolli <oalickolli@gmail.com>
2025-08-11 22:18:49 -0500

    First working implementation of ElementWiseArithmeticOperator (not widely tested)

--- sktime/transformations/series/elementwise_operator.py
+++ sktime/transformations/series/elementwise_operator.py
@@ -45,290 +45,98 @@ __author__ = ["oresthes"]
 from sktime.transformations.base import BaseTransformer
 from sktime.base import _HeterogenousMetaEstimator
 
-# todo: add any necessary sktime internal imports here
+import pandas as pd
+import numpy as np
 
 
 class ElementWiseArithmeticOperator(BaseTransformer, _HeterogenousMetaEstimator):
-    """Custom transformer for element-wise arithmetic operations.
+    """Element-wise arithmetic operator for transformer outputs.
 
-    This transformer applies a specified arithmetic operation element-wise
-    to the input time series data. It can be used to perform operations such
-    as addition, subtraction, multiplication, or division on the input data.
+    Applies a user-supplied elementwise operation to the outputs of two or more transformers.
 
     Parameters
     ----------
-    parama : int
-        descriptive explanation of parama
-    paramb : string, optional (default='default')
-        descriptive explanation of paramb
-    paramc : boolean, optional (default=MyOtherEstimator(foo=42))
-        descriptive explanation of paramc
-    and so on
+    transformer_list : list of (str, transformer) tuples
+        List of transformers to apply to the input data.
+    operation : callable
+        Function to apply elementwise to the outputs of the transformers.
+        Should accept N arrays/Series/DataFrames and return a DataFrame or Series.
     """
 
-    # todo: fill out estimator tags here
-    #  tags are inherited from parent class if they are not set
-    #
-    # todo: define the transformer scitype by setting the tags
-    #   scitype:transform-input - the expected input scitype of X
-    #   scitype:transform-output - the output scitype that transform produces
-    #   scitype:transform-labels - whether y is used and if yes which scitype
-    #   scitype:instancewise - whether transform uses all samples or acts by instance
-    #
-    # todo: define internal types for X, y in _fit/_transform by setting the tags
-    #   X_inner_mtype - the internal mtype used for X in _fit and _transform
-    #   y_inner_mtype - if y is used, the internal mtype used for y; usually "None"
-    #   setting this guarantees that X, y passed to _fit, _transform are of above types
-    #   for possible mtypes see datatypes.MTYPE_REGISTER, or the datatypes tutorial
-    #
-    #  when scitype:transform-input is set to Panel:
-    #   X_inner_mtype must be changed to one or a list of sktime Panel mtypes
-    #  when scitype:transform-labels is set to Series or Panel:
-    #   y_inner_mtype must be changed to one or a list of compatible sktime mtypes
-    #  the other tags are "safe defaults" which can usually be left as-is
+    _steps_attr = "_transformer_list"
+    _steps_fitted_attr = "transformer_list_"
+
     _tags = {
-        # tags and full specifications are available in the tag API reference
-        # https://www.sktime.net/en/stable/api_reference/tags.html
-        # to list all valid tags with description, use sktime.registry.all_tags
-        #   all_tags(estimator_types="transformer", as_dataframe=True)
-        #
-        #
-        # behavioural tags: transformer type
-        # ----------------------------------
-        #
-        # scitype:transform-input, scitype:transform-output, scitype:transform-labels
-        # control the input/output type of transform, in terms of scitype
-        #
-        # scitype:transform-input, scitype:transform-output should be the
-        # simplest scitype that describes the mapping, taking into account vectorization
-        # a transform that produces Series when given Series, Panel when given Panel
-        #   should have both transform-input and transform-output as "Series"
-        # a transform that produces a tabular DataFrame (Table)
-        #   when given Series or Panel should have transform-input "Series"
-        #       and transform-output as "Primitives"
         "scitype:transform-input": "Series",
-        # valid values: "Series", "Panel"
         "scitype:transform-output": "Series",
-        # valid values: "Series", "Panel", "Primitives"
-        #
-        # scitype:instancewise = is fit_transform an instance-wise operation?
-        # instance-wise = only values of a given series instance are used to transform
-        #   that instance. Example: Fourier transform; non-example: series PCA
-        "scitype:instancewise": True,
-        #
-        # scitype:transform-labels types the y used in transform
-        #   if y is not used in transform, this should be "None"
         "scitype:transform-labels": "None",
-        # valid values: "None" (not needed), "Primitives", "Series", "Panel"
-        #
-        #
-        # behavioural tags: internal type
-        # ----------------------------------
-        #
-        # X_inner_mtype, y_inner_mtype control which format X/y appears in
-        # in the inner functions _fit, _transform, etc
+        "scitype:instancewise": True,
         "X_inner_mtype": "pd.DataFrame",
         "y_inner_mtype": "None",
-        # valid values: str and list of str
-        # if str, must be a valid mtype str, in sktime.datatypes.MTYPE_REGISTER
-        #   of scitype Series, Panel (panel data) or Hierarchical (hierarchical series)
-        #   y_inner_mtype can also be of scitype Table (one row/instance per series)
-        #   in that case, all inputs are converted to that one type
-        # if list of str, must be a list of valid str specifiers
-        #   in that case, X/y are passed through without conversion if on the list
-        #   if not on the list, converted to the first entry of the same scitype
-        #
-        # univariate-only controls whether internal X can be univariate/multivariate
-        # if True (only univariate), always applies vectorization over variables
         "univariate-only": False,
-        # valid values: True = inner _fit, _transform receive only univariate series
-        #   False = uni- and multivariate series are passed to inner methods
-        #
-        # requires_y = does y need to be passed in fit?
         "requires_y": False,
-        # valid values: False (no), True = exception is raised if no y is seen in _fit
-        #   y can be passed or not in _transform for either value of requires_y
-        #
-        #
-        # capability tags: properties of the estimator
-        # --------------------------------------------
-        #
-        # fit_is_empty = is fit empty and can be skipped?
-        "fit_is_empty": True,
-        # valid values: True = _fit is considered empty and skipped, False = No
-        # CAUTION: default is "True", i.e., _fit will be skipped even if implemented
-        #
-        # capability:inverse_transform = is inverse_transform implemented?
+        "fit_is_empty": False,
         "capability:inverse_transform": False,
-        # valid values: boolean True (yes), False (no)
-        # if True, _inverse_transform must be implemented
-        # if False, exception is raised if inverse_transform is called,
-        #   unless the skip-inverse-transform tag is set to True
-        #
-        # capability:unequal_length = can the transformer handle unequal length panels,
-        #   i.e., when passed unequal length instances in Panel or Hierarchical data
-        "capability:unequal_length": True,
-        # valid values: boolean True (yes), False (no)
-        # if False, may raise exception when passed unequal length Panel/Hierarchical
-        #
-        # handles-missing-data = can the transformer handle missing data (np or pd.NA)?
-        "capability:missing_values": False,  # can estimator handle missing data?
-        # valid values: boolean True (yes), False (no)
-        # if False, may raise exception when passed time series with missing values
-        #
-        # ownership and contribution tags
-        # -------------------------------
-        #
-        # author = author(s) of th estimator
-        # an author is anyone with significant contribution to the code at some point
-        "authors": ["author1", "author2"],
-        # valid values: str or list of str, should be GitHub handles
-        # this should follow best scientific contribution practices
-        # scope is the code, not the methodology (method is per paper citation)
-        # if interfacing a 3rd party estimator, ensure to give credit to the
-        # authors of the interfaced estimator
-        #
-        # maintainer = current maintainer(s) of the estimator
-        # per algorithm maintainer role, see governance document
-        # this is an "owner" type role, with rights and maintenance duties
-        # for 3rd party interfaces, the scope is the sktime class only
-        "maintainers": ["maintainer1", "maintainer2"],
-        # valid values: str or list of str, should be GitHub handles
-        # remove tag if maintained by sktime core team
+        "capability:unequal_length": False,
+        "capability:missing_values": False,
+        "authors": ["oresthes"],
+        "maintainers": ["oresthes"],
     }
 
-    # todo: add any hyper-parameters and components to constructor
-    def __init__(self, parama, paramb="default", paramc=None):
-        # todo: write any hyper-parameters to self
-        self.parama = parama
-        self.paramb = paramb
-        self.paramc = paramc
-        # IMPORTANT: the self.params should never be overwritten or mutated from now on
-        # for handling defaults etc, write to other attributes, e.g., self._parama
-
-        # leave this as is
+    def __init__(self, transformer_list, operation):
+        self.transformer_list = transformer_list
+        self.operation = operation
+        self.transformer_list_ = self._check_estimators(transformer_list, cls_type=BaseTransformer)
         super().__init__()
 
-        # todo: optional, parameter checking logic (if applicable) should happen here
-        # if writes derived values to self, should *not* overwrite self.parama etc
-        # instead, write to self._parama, self._newparam (starting with _)
+    @property
+    def _transformer_list(self):
+        return self._get_estimator_tuples(self.transformer_list, clone_ests=False)
 
-    # todo: implement this, mandatory (except in special case below)
+    @_transformer_list.setter
+    def _transformer_list(self, value):
+        self.transformer_list = value
+        self.transformer_list_ = self._check_estimators(value, cls_type=BaseTransformer)
+
     def _fit(self, X, y=None):
-        """Fit transformer to X and y.
+        self.transformer_list_ = self._check_estimators(self.transformer_list, cls_type=BaseTransformer)
+        for _, transformer in self.transformer_list_:
+            transformer.fit(X=X, y=y)
+        return self
 
-        private _fit containing the core logic, called from fit
-
-        Parameters
-        ----------
-        X : Series, Panel, or Hierarchical data, of mtype X_inner_mtype
-            if X_inner_mtype is list, _fit must support all types in it
-            Data to fit transform to
-        y : Series, Panel, or Hierarchical data, of mtype y_inner_mtype, default=None
-            Additional data, e.g., labels for transformation
-
-        Returns
-        -------
-        self: reference to self
-        """
-
-        # implement here
-        # X, y passed to this function are always of X_inner_mtype, y_inner_mtype
-        # IMPORTANT: avoid side effects to X, y
-        #
-        # any model parameters should be written to attributes ending in "_"
-        #  attributes set by the constructor must not be overwritten
-        #
-        # special case: if no fitting happens before transformation
-        #  then: delete _fit (don't implement)
-        #   set "fit_is_empty" tag to True
-        #
-        # Note: when interfacing a model that has fit, with parameters
-        #   that are not data (X, y) or data-like
-        #   but model parameters, *don't* add as arguments to fit, but treat as follows:
-        #   1. pass to constructor,  2. write to self in constructor,
-        #   3. read from self in _fit,  4. pass to interfaced_model.fit in _fit
-
-    # todo: implement this, mandatory
     def _transform(self, X, y=None):
-        """Transform X and return a transformed version.
+        transformers = self._get_estimator_list(self.transformer_list_)
+        Xt_list = [trafo.transform(X, y) for trafo in transformers]
 
-        private _transform containing core logic, called from transform
+        # Ensure all outputs have matching indexes and columns
+        first_index = Xt_list[0].index
+        first_columns = Xt_list[0].columns
+        for i, Xt in enumerate(Xt_list[1:], 1):
+            if not Xt.index.equals(first_index):
+                raise ValueError(f"Index mismatch between transformer 0 and {i}")
+            if not Xt.columns.equals(first_columns):
+                raise ValueError(f"Column mismatch between transformer 0 and {i}")
 
-        Parameters
-        ----------
-        X : Series, Panel, or Hierarchical data, of mtype X_inner_mtype
-            if X_inner_mtype is list, _transform must support all types in it
-            Data to be transformed
-        y : Series, Panel, or Hierarchical data, of mtype y_inner_mtype, default=None
-            Additional data, e.g., labels for transformation
+        # Apply the operation elementwise
+        # Stack into 3D array if possible, else apply operation column-wise
+        arrays = [Xt.values for Xt in Xt_list]
+        result = self.operation(*arrays)
+        # If result is ndarray, wrap as DataFrame
+        if isinstance(result, np.ndarray):
+            result = pd.DataFrame(result, index=first_index, columns=first_columns)
+        elif isinstance(result, pd.Series):
+            result = result.to_frame()
+        return result
 
-        Returns
-        -------
-        transformed version of X
-        """
-        # implement here
-        # X, y passed to this function are always of X_inner_mtype, y_inner_mtype
-        # IMPORTANT: avoid side effects to X, y
-        #
-        # if transform-output is "Primitives":
-        #  return should be pd.DataFrame, with as many rows as instances in input
-        #  if input is a single series, return should be single-row pd.DataFrame
-        # if transform-output is "Series":
-        #  return should be of same mtype as input, X_inner_mtype
-        #  if multiple X_inner_mtype are supported, ensure same input/output
-        # if transform-output is "Panel":
-        #  return a multi-indexed pd.DataFrame of Panel mtype pd_multiindex
-        #
-        # todo: add the return mtype/scitype to the docstring, e.g.,
-        #  Returns
-        #  -------
-        #  X_transformed : Series of mtype pd.DataFrame
-        #       transformed version of X
-
-    # todo: return default parameters, so that a test instance can be created
-    #   required for automated unit and integration testing of estimator
     @classmethod
     def get_test_params(cls, parameter_set="default"):
-        """Return testing parameter settings for the estimator.
-
-        Parameters
-        ----------
-        parameter_set : str, default="default"
-            Name of the set of test parameters to return, for use in tests. If no
-            special parameters are defined for a value, will return `"default"` set.
-            There are currently no reserved values for transformers.
-
-        Returns
-        -------
-        params : dict or list of dict, default = {}
-            Parameters to create testing instances of the class
-            Each dict are parameters to construct an "interesting" test instance, i.e.,
-            `MyClass(**params)` or `MyClass(**params[i])` creates a valid test instance.
-            `create_test_instance` uses the first (or only) dictionary in `params`
-        """
-
-        # todo: set the testing parameters for the estimators
-        # Testing parameters can be dictionary or list of dictionaries.
-        # Testing parameter choice should cover internal cases well.
-        #   for "simple" extension, ignore the parameter_set argument.
-        #
-        # A good parameter set should primarily satisfy two criteria,
-        #   1. Chosen set of parameters should have a low testing time,
-        #      ideally in the magnitude of few seconds for the entire test suite.
-        #       This is vital for the cases where default values result in
-        #       "big" models which not only increases test time but also
-        #       run into the risk of test workers crashing.
-        #   2. There should be a minimum two such parameter sets with different
-        #      sets of values to ensure a wide range of code coverage is provided.
-        #
-        # example 1: specify params as dictionary
-        # any number of params can be specified
-        # params = {"est": value0, "parama": value1, "paramb": value2}
-        #
-        # example 2: specify params as list of dictionary
-        # note: Only first dictionary will be used by create_test_instance
-        # params = [{"est": value1, "parama": value2},
-        #           {"est": value3, "parama": value4}]
-        #
-        # return params
+        from sktime.transformations.series.exponent import ExponentTransformer
+        # Example: ratio of two transformers
+        params = {
+            "transformer_list": [
+                ("t1", ExponentTransformer(power=2)),
+                ("t2", ExponentTransformer(power=1)),
+            ],
+            "operation": np.divide,
+        }
+        return [params]
