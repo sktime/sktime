16decdf6a75de29056e940e15cd752d3f94670c4
Franz Király <fkiraly@gcos.ai>
2025-08-16 15:26:30 +0200

    fh splitter

---
+++ sktime/split/fh.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3 -u
+# copyright: sktime developers, BSD-3-Clause License (see LICENSE file)
+"""Forecasting horizon based train test split utility function."""
+
+import numpy as np
+import pandas as pd
+from typing import Optional
+
+from sktime.split.base import BaseSplitter
+from sktime.utils.validation.forecasting import check_fh
+
+
+class ForecastingHorizonSplitter(BaseSplitter):
+    """Splitter that creates a single train/test split based on a forecasting horizon.
+
+    Handles both relative and absolute forecasting horizons.
+
+    Parameters
+    ----------
+    fh : ForecastingHorizon or compatible input
+        Forecasting horizon that defines the test set.
+        Must be all out-of-sample if relative.
+    """
+
+    _tags = {"split_hierarchical": False}
+
+    def __init__(self, fh):
+        self.fh = fh
+        super().__init__()
+
+    def _split(self, y: pd.Index):
+        """Return train/test indices based on forecasting horizon."""
+        fh = check_fh(self.fh, freq=y)
+        idx = fh.to_pandas()
+
+        if fh.is_relative:
+            if not fh.is_all_out_of_sample():
+                raise ValueError("`fh` must only contain out-of-sample values")
+
+            max_step = idx.max()
+            steps = fh.to_indexer()
+
+            train_ix = np.arange(len(y) - max_step)
+            test_ix = (np.arange(len(y) - max_step, len(y)))[steps]
+
+        else:
+            min_step, max_step = idx.min(), idx.max()
+
+            train_ix = np.where(y < min_step)[0]
+            test_ix = np.where((y >= min_step) & (y <= max_step))[0]
+
+        yield train_ix, test_ix
+
+    def get_n_splits(self, y: Optional[pd.Index] = None) -> int:
+        """Return number of splits (always 1)."""
+        return 1
+
+    @classmethod
+    def get_test_params(cls, parameter_set="default"):
+        """Return testing parameter settings for the splitter."""
+        import numpy as np
+        from sktime.forecasting.base import ForecastingHorizon
+
+        fh_rel = ForecastingHorizon([1, 2, 3], is_relative=True)
+        fh_abs = ForecastingHorizon([7, 8, 9], is_relative=False)
+
+        return [{"fh": fh_rel}, {"fh": fh_abs}]
