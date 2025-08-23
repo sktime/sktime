2b7407598a1d8932ea58123339e972c8f9b8deda
Franz Király <fkiraly@gcos.ai>
2025-08-23 17:05:44 +0200

    [MNT] remove `requires_cython` tag and separate VM handling in favour of `tests:vm` (#8674)
    This PR removes the `requires_cython` tag and related extra CI by
    replacing it with the more broad `tests:vm` tag for the two estimators
    affected, `MrSQM` and `MrSEQL`.
    
    Use of the `tests:vm` tag is more configurable, as it allows to install
    estimator specific dependencies.
    
    As the `requires_cython` tag was used only in `sktime` internal testing,
    no deprecation is required.

--- sktime/tests/test_all_estimators.py
+++ sktime/tests/test_all_estimators.py
@@ -29,7 +29,6 @@ from sktime.forecasting.base import BaseForecaster
 from sktime.registry import all_estimators, get_base_class_lookup, scitype
 from sktime.regression.deep_learning.base import BaseDeepRegressor
 from sktime.tests._config import (
-    CYTHON_ESTIMATORS,
     EXCLUDE_ESTIMATORS,
     EXCLUDED_TESTS,
     MATRIXDESIGN,
@@ -206,10 +205,7 @@ class BaseFixtureGenerator:
         # TODO(fangelim): refactor this _all_estimators
         # to make it possible to set custom tags to filter
         # as class attributes, similar to `estimator_type_filter`
-        if CYTHON_ESTIMATORS:
-            filter_tags = {"requires_cython": True, "tests:skip_all": False}
-        else:
-            filter_tags = {"tests:skip_all": False}
+        filter_tags = {"tests:skip_all": False}
 
         est_list = all_estimators(
             estimator_types=getattr(self, "estimator_type_filter", None),
