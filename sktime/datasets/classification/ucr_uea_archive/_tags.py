1cd76521aa4f87ae314af2ac11b97b99879d49ee
Jigyasu <jigyasu@outlook.in>
2025-09-09 18:42:51 +0530

    add tags for test

--- sktime/datasets/classification/ucr_uea_archive/_tags.py
+++ sktime/datasets/classification/ucr_uea_archive/_tags.py
@@ -1,1 +1,20 @@
 """Pre-calculated tags for UCR UEA datasets."""
+
+DATASET_TAGS = {
+    "Beef": {
+        "n_splits": 1,
+        "is_univariate": True,
+        "n_instances": 60,
+        "n_instances_train": 30,
+        "n_instances_test": 30,
+        "n_classes": 5,
+    },
+    "BeetleFly": {
+        "n_splits": 1,
+        "is_univariate": True,
+        "n_instances": 40,
+        "n_instances_train": 20,
+        "n_instances_test": 20,
+        "n_classes": 2,
+    },
+}
