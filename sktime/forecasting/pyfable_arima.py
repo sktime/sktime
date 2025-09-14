9ea44f5d22770e375bcfc61c430c6073edca1ff7
ericjb <ericjberger@gmail.com>
2025-09-14 16:03:51 +0300

    fix Python 3.9 issue flagged by CI

--- sktime/forecasting/pyfable_arima.py
+++ sktime/forecasting/pyfable_arima.py
@@ -5,6 +5,7 @@ __author__ = ["ericjb"]
 
 import io
 import sys
+from typing import Optional  # see _letter_from_index definition
 
 import numpy as np
 import pandas as pd
@@ -174,7 +175,7 @@ class PyFableARIMA(BaseForecaster):
         return is_arithmetic_progression
 
     @staticmethod
-    def _letter_from_index(idx) -> str | None:
+    def _letter_from_index(idx) -> Optional[str]:  # rhs was str|None
         """
         Map DatetimeIndex/PeriodIndex frequency to one of 'A','Q','M','W','D'.
 
