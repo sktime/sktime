f1ea2f973a5bdae442baa3cdc7e4bd7372a4e9f2
Franz Király <fkiraly@gcos.ai>
2025-09-18 00:08:43 +0200

    Update __init__.py

--- sktime/datasets/classification/ucr_uea_archive/__init__.py
+++ sktime/datasets/classification/ucr_uea_archive/__init__.py
@@ -1,7 +1,7 @@
 """TSC datasets from UCR UEA archive."""
 
 from sktime.datasets.classification.ucr_uea_archive._ucr_uea_datasets import (
-    UCRUEADataset
+    UCRUEADataset,
 )
 
 __all__ = ["UCRUEADataset"]
