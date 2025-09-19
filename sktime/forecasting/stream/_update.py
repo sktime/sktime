7e8273772a0f7d2454a408cc19a0a957b9f5864a
Franz Király <fkiraly@gcos.ai>
2025-09-19 10:57:31 +0200

    [ENH] fix `UpdateRefitsEvery` and `UpdateEvery` stream update compositors to work with newer `pandas` `DateOffset` (#8822)
    fixes `UpdateRefitsEvery` and `UpdateEvery` stream update compositors to
    work with newer `pandas` `DateOffset` like `MonthEnd`.

--- sktime/forecasting/stream/_update.py
+++ sktime/forecasting/stream/_update.py
@@ -14,28 +14,66 @@ from sktime.forecasting.base._delegate import _DelegatedForecaster
 class UpdateRefitsEvery(_DelegatedForecaster):
     """Refits periodically when update is called.
 
-    If update is called with update_params=True and refit_interval or more has
-        elapsed since the last fit, refits the forecaster instead (call to fit).
-            refitting is done on (potentially) all data seen so far.
-        refit_window controls the lookback window on which refitting is done
-            refit data is cutoff (inclusive) to cutoff minus refit_window (exclusive)
+    If update is called with ``update_params=True`` and ``refit_interval`` or more has
+    elapsed since the last ``fit``, refits the ``forecaster`` instead (call to ``fit``).
+
+    Refitting is done on (potentially) all data seen so far.
+
+    ``refit_window`` controls the lookback window on which refitting is done.
+    The refit is carried out on all data in the lookback window
+    ``cutoff`` (inclusive, end) to
+    ``cutoff`` minus ``refit_window`` (exclusive, start),
+    with a default lookback window of all data seen so far.
 
     Parameters
     ----------
+    forecaster : an sktime forecaster
+        the forecaster to be refit/updated regularly
+
     refit_interval : difference of sktime time indices (int or timedelta), optional
         interval that needs to elapse after which the first update defaults to fit
         default = 0, i.e., always refits, never updates
-        if index of y seen in fit is integer or y is index-free container type,
-            refit_interval must be int, and is interpreted as difference of int location
-        if index of y seen in fit is timestamp, must be int or pd.Timedelta
-            if pd.Timedelta, will be interpreted as time since last refit elapsed
-            if int, will be interpreted as number of time stamps seen since last refit
+
+        * if index of ``y`` seen in ``fit`` is integer or ``y`` is
+          index-free container type, ``refit_interval`` must be ``int``,
+          and is interpreted as difference of ``int`` location
+        * if index of ``y`` seen in ``fit`` is timestamp,
+          must be ``int`` or ``pd.Timedelta``
+
+            - if ``pd.Timedelta``, will be interpreted as time since last refit elapsed
+            - if int, will be interpreted as number of time stamps seen since last refit
+
     refit_window_size : difference of sktime time indices (int or timedelta), optional
-        length of the data window to refit to in case update calls fit
+        length of the data window to refit to in case update calls fit;
         default = inf, i.e., refits to entire training data seen so far
+
     refit_window_lag : difference of sktime indices (int or timedelta), optional
-        lag of the data window to refit to, w.r.t. cutoff, in case update calls fit
-        default = 0, i.e., refit window ends with and includes cutoff
+        lag of the data window to refit to, w.r.t. ``cutoff``,
+        in case ``update`` calls ``fit``;
+        default = 0, i.e., refit window ends with and includes ``cutoff``
+
+    Examples
+    --------
+    >>> from sktime.forecasting.trend import TrendForecaster
+    >>> from sktime.forecasting.stream import UpdateRefitsEvery
+    >>> from sktime.datasets import load_airline
+    >>> y = load_airline()
+    >>> y0 = y.iloc[:-20]
+    >>> y1 = y.iloc[-20:-10]
+    >>> y2 = y.iloc[-10:]
+    >>> forecaster = TrendForecaster()
+    >>> forecaster = UpdateRefitsEvery(forecaster, refit_interval=12)
+    >>> forecaster.fit(y0, fh=[1,2,3])
+    UpdateRefitsEvery(...)
+    >>> # predict etc could be called here
+    >>> # e.g., forecaster.predict()
+    >>>
+    >>> # first update, 10 < refit_interval = 12, so calls update
+    >>> forecaster.update(y1)
+    UpdateRefitsEvery(...)
+    >>> # second update, 20 >= refit_interval = 12, so calls fit
+    >>> forecaster.update(y2)
+    UpdateRefitsEvery(...)
     """
 
     # attribute for _DelegatedForecaster, which then delegates
@@ -150,20 +188,20 @@ class UpdateRefitsEvery(_DelegatedForecaster):
         # treat situation where indexing of y is in timedelta but differences are int
         #   in that case, interpret any integers as iloc index differences
         #   and replace integers with timedelta quantities before proceeding
-        if isinstance(time_since_last_fit, pd.Timedelta):
+        if _is_time_difference(time_since_last_fit):
             if isinstance(refit_window_lag, int):
                 lag = min(refit_window_lag, len(_y))
                 refit_window_lag = self.cutoff[0] - _y.index[-lag]
             if isinstance(refit_window_size, int):
                 _y_lag = get_window(_y, lag=refit_window_lag)
-                window_size = min(refit_window_size, len(_y_lag))
-                refit_window_size = _y_lag.index[-window_size]
+                window_size = min(refit_window_size + 1, len(_y_lag))
+                refit_window_size = self.cutoff[0] - _y_lag.index[-window_size]
             if isinstance(refit_interval, int):
-                index = min(refit_interval, len(_y))
+                index = min(refit_interval + 1, len(_y))
                 refit_interval = self.cutoff[0] - _y.index[-index]
         # case distinction based on whether the refit_interval period has elapsed
         #   if yes: call fit, on the specified window sub-set of all observed data
-        if time_since_last_fit >= refit_interval and update_params:
+        if _geq(time_since_last_fit, refit_interval) and update_params:
             if refit_window_size is not None or refit_window_lag != 0:
                 y_win = get_window(
                     _y, window_length=refit_window_size, lag=refit_window_lag
@@ -175,7 +213,7 @@ class UpdateRefitsEvery(_DelegatedForecaster):
                 y_win = _y
                 X_win = _X
             fh = self._fh
-            estimator.fit(y=y_win, X=X_win, fh=fh, update_params=update_params)
+            estimator.fit(y=y_win, X=X_win, fh=fh)
 
             # remember that we just fitted the estimator
             self.last_fit_cutoff_ = self.cutoff[0]
@@ -210,23 +248,53 @@ class UpdateRefitsEvery(_DelegatedForecaster):
 class UpdateEvery(_DelegatedForecaster):
     """Update only periodically when update is called.
 
-    If update is called, behaves like update_params=False, unless update_interval has
-        elapsed since the last "true" update = inner update with update_params=False.
-        update_window controls the lookback window on which refitting is done
-            refit data is cutoff (inclusive) to cutoff minus refit_window (exclusive)
+    If ``update`` is called, behaves like ``update_params=False``,
+    unless ``update_interval`` time has elapsed since the last "true" update,
+    i.e., call to ``forecaster.update`` with ``update_params=False``.
 
-    Caution: default value of update_interval means *no updates* after ``fit``.
+    ``update_interval`` controls the minimum time that needs to elapse.
+
+    Caution: default value of ``update_interval`` means *no updates* after ``fit``.
 
     Parameters
     ----------
     update_interval : difference of sktime time indices (int or timedelta), optional
         interval that needs to elapse until inner update call with update_params=True
         default = None = infinity, i.e., never updates
-        if index of y seen in fit is integer or y is index-free container type,
-            update_interval must be int, is interpreted as difference of int location
-        if index of y seen in fit is timestamp, must be int or pd.Timedelta
-            if pd.Timedelta, will be interpreted as time since last true update elapsed
-            if int, will be interpreted as number of time stamps seen since last update
+
+        * if index of ``y`` seen in ``fit`` is integer or ``y`` is
+          index-free container type, ``refit_interval`` must be ``int``,
+          and is interpreted as difference of ``int`` location
+        * if index of ``y`` seen in ``fit`` is timestamp,
+          must be ``int`` or ``pd.Timedelta``
+
+            - if ``pd.Timedelta``, will be interpreted as time since last refit elapsed
+            - if int, will be interpreted as number of time stamps seen since last refit
+
+    Examples
+    --------
+    >>> from sktime.forecasting.trend import TrendForecaster
+    >>> from sktime.forecasting.stream import UpdateEvery
+    >>> from sktime.datasets import load_airline
+    >>> y = load_airline()
+    >>> y0 = y.iloc[:-20]
+    >>> y1 = y.iloc[-20:-10]
+    >>> y2 = y.iloc[-10:]
+    >>> inner_forecaster = TrendForecaster()
+    >>> forecaster = UpdateEvery(inner_forecaster, update_interval=12)
+    >>> forecaster.fit(y0, fh=[1,2,3])
+    UpdateEvery(...)
+    >>> # predict etc could be called here
+    >>> # e.g., forecaster.predict()
+    >>>
+    >>> # first update, 10 < update_interval = 12, so calls update with
+    >>> # update_params=False
+    >>> forecaster.update(y1)
+    UpdateEvery(...)
+    >>> # second update, 20 >= update_interval = 12, so calls update with
+    >>> # update_params=True
+    >>> forecaster.update(y2)
+    UpdateEvery(...)
     """
 
     # attribute for _DelegatedForecaster, which then delegates
@@ -331,14 +399,14 @@ class UpdateEvery(_DelegatedForecaster):
         # treat situation where indexing of y is in timedelta but differences are int
         #   in that case, interpret any integers as iloc index differences
         #   and replace integers with timedelta quantities before proceeding
-        if isinstance(time_since_last_update, pd.Timedelta):
+        if _is_time_difference(time_since_last_update):
             if isinstance(update_interval, int):
                 index = min(update_interval, len(_y))
                 update_interval = self.cutoff[0] - _y.index[-index]
         # case distinction based on whether the update_interval period has elapsed
         # (None update_interval means infinite update_interval)
         #   if yes: call inner update with update_params=True, aka "true" update
-        if update_interval is not None and time_since_last_update >= update_interval:
+        if _geq(time_since_last_update, update_interval):
             estimator.update(y=y, X=X, update_params=update_params)
 
             # remember that we just updated the estimator
@@ -476,3 +544,57 @@ class DontUpdate(_DelegatedForecaster):
         forecaster = TrendForecaster.create_test_instance()
 
         return {"forecaster": forecaster}
+
+
+def _is_time_offset(obj):
+    """Check whether obj is a pd.DateOffset.
+
+    Parameters
+    ----------
+    obj : any Python object
+
+    Returns
+    -------
+    bool : whether obj is a pd.DateOffset
+    """
+    return hasattr(pd, "DateOffset") and isinstance(obj, pd.DateOffset)
+
+
+def _is_time_difference(obj):
+    """Check whether obj is a time difference (pd.Timedelta or pd.DateOffset).
+
+    Parameters
+    ----------
+    obj : any Python object
+
+    Returns
+    -------
+    bool : whether obj is a time difference
+    """
+    if isinstance(obj, pd.Timedelta):
+        return True
+    if _is_time_offset(obj):
+        return True
+    return False
+
+
+def _geq(a, b):
+    """Check whether time difference a is bigger equal than b.
+
+    b can be None, in which case return False (interpreted as -infinity).
+
+    Parameters
+    ----------
+    a : time difference (int or pd.Timedelta or pd.DateOffset)
+    b : time difference (int or pd.Timedelta or pd.DateOffset), or None
+
+    Returns
+    -------
+    bool : whether a > b
+    """
+    if b is None:
+        return False
+    if _is_time_offset(a) and _is_time_offset(b):
+        return a.n >= b.n
+    else:
+        return a >= b
