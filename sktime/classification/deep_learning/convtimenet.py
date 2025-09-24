92e9d1f87641b6386f9e850b416b80feee8b2639
Franz Király <fkiraly@gcos.ai>
2025-09-25 00:36:48 +0200

    Update convtimenet.py

--- sktime/classification/deep_learning/convtimenet.py
+++ sktime/classification/deep_learning/convtimenet.py
@@ -95,10 +95,18 @@ class ConvTimeNetClassifier(BaseDeepClassifierPytorch):
     """
 
     _tags = {
+        # packaging info
+        # --------------
         "authors": ["Tanuj-Taneja1"],
         "maintainers": ["Tanuj-Taneja1"],
         "tests:skip_by_name": ["test_fit_idempotent"],
         "python_dependencies": ["torch"],
+        # estimator type
+        # --------------
+        "capability:random_state": True,
+        "property:randomness": "derandomized",
+        # CI and testing
+        # --------------
         "tests:libs": [
             "sktime.networks.convtimenet._convtimenet",
             "sktime.networks.convtimenet._dlutils",
