89f6b995cc4d087dcad93bacce1dbdf0fbd22e0e
Franz Király <fkiraly@gcos.ai>
2025-09-25 20:39:34 +0200

    [ENH] more tag updates (#8851)
    Fixes a small number of leftover tags after more stringent testing.

--- sktime/clustering/k_shapes.py
+++ sktime/clustering/k_shapes.py
@@ -60,6 +60,8 @@ class TimeSeriesKShapes(_TslearnAdapter, BaseClusterer):
         "capability:out_of_sample": True,
         "capability:predict": True,
         "capability:predict_proba": False,
+        "capability:random_state": True,
+        "property:randomness": "derandomized",
     }
 
     # defines the name of the attribute containing the tslearn estimator
