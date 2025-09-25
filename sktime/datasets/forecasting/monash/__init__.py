59d648f574d68ce0c9209ca2fd64f0f8b312a6c0
Franz Király <fkiraly@gcos.ai>
2025-09-25 22:14:26 +0200

    Update __init__.py

--- sktime/datasets/forecasting/monash/__init__.py
+++ sktime/datasets/forecasting/monash/__init__.py
@@ -1,5 +1,5 @@
 """Monash forecasting datasets."""
 
-from sktime.datasets.forecasting.monash.forecasting_data import ForecastingData
+from sktime.datasets.forecasting.monash._forecasting_data import ForecastingData
 
 __all__ = ["ForecastingData"]
