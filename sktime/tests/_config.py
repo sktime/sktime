8bbc545cc95ae7646498ed072c341b9cb6d8936a
Aryan Dhanuka <a9936067905@gmail.com>
2025-10-04 10:19:35 +0530

    [ENH] remove TimeBinner from excluded tests

--- sktime/tests/_config.py
+++ sktime/tests/_config.py
@@ -308,7 +308,7 @@ EXCLUDED_TESTS_BY_TEST = {
         "TSFreshClassifier",
         "TapNetNetwork",
         "TemporalDictionaryEnsemble",
-        "TimeBinner",
+        # "TimeBinner",
         "TimeSeriesKMedoids",
         "TimeSeriesKernelKMeans",
         "TruncationTransformer",
