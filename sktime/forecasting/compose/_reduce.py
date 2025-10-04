c2362fed459fe3ff2cc1ca057f795d3dd558c3f2
sanskarmodi8 <sansyprog8@gmail.com>
2025-10-04 17:24:09 +0530

    corrected allow_nan tag access

--- sktime/forecasting/compose/_reduce.py
+++ sktime/forecasting/compose/_reduce.py
@@ -2400,9 +2400,7 @@ class RecursiveReductionForecaster(BaseForecaster, _ReducerMixin):
                 sklearn.__version__
             ) >= version.parse("1.6"):
                 tags = sklearn_get_tags(self.estimator)
-                self.set_tags(
-                    **{"capability:missing_values": tags.get("allow_nan", False)}
-                )
+                self.set_tags(**{"capability:missing_values": tags["allow_nan"]})
             else:
                 self.set_tags(**{"capability:missing_values": False})
 
