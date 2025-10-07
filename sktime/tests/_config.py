1abdd831be4963f17481d341eafbd3f8f0fbd25b
Luigi Giugliano <luigi3000@gmail.com>
2025-10-07 21:54:28 +0200

    [ENH] `BOSSEnsemble` second set of test parameters (#8922)
    #### Reference Issues/PRs
    #3429
    
    
    #### What does this implement/fix? Explain your changes.
    Adds "feature_selection" = "chi2" to first test parameter creating a
    second one

--- sktime/tests/_config.py
+++ sktime/tests/_config.py
@@ -223,7 +223,6 @@ EXCLUDED_TESTS = {
 # exclude tests but keyed by test name
 EXCLUDED_TESTS_BY_TEST = {
     "test_get_test_params_coverage": [
-        "BOSSEnsemble",
         "CAPA",
         "CNTCNetwork",
         "CanonicalIntervalForest",
