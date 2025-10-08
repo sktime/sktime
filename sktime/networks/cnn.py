53b0868c6dfbf093fff317d2fbdfd499a8131395
Gregor Ehrensperger <g@ehrensperger.dev>
2025-10-08 07:26:19 +0200

    doc: extend activation docstring of CNNNetwork

--- sktime/networks/cnn.py
+++ sktime/networks/cnn.py
@@ -20,8 +20,10 @@ class CNNNetwork(BaseDeepNetwork):
     n_conv_layers : int, default = 2
         the number of convolutional plus average pooling layers
     filter_sizes : array of int, shape = (n_conv_layers)
-    activation : string, default = sigmoid
-        keras activation function
+    activation : string, default = "sigmoid"
+        activation function used for hidden layers;
+        List of available keras activation functions:
+        https://keras.io/api/layers/activations/
     padding : string, default = "auto"
         Controls padding logic for the convolutional layers,
         i.e. whether ``'valid'`` and ``'same'`` are passed to the ``Conv1D`` layer.
