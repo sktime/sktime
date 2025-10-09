ac652fca72867d7ab02c972c9a4c374111bedc17
Astrael <szymon8999@wp.pl>
2025-10-09 11:58:44 +0200

    [ENH] get_test_params for WhiteNoiseAugmenter

--- sktime/tests/_config.py
+++ sktime/tests/_config.py
@@ -307,7 +307,6 @@ EXCLUDED_TESTS_BY_TEST = {
         "TruncationTransformer",
         "UnobservedComponents",
         "WEASEL",
-        "WhiteNoiseAugmenter",
         # The below estimators need to have their name removed from EXCLUDE_SOFT_DEPS
         # too after adding test parameters to them
         "BaggingForecaster",
