7435beddf4977115b22e882c55dae704a7247a75
Luigi Giugliano <luigi3000@gmail.com>
2025-10-17 23:47:43 +0200

    [BUG] fix `ColumnEnsembleClassifier` `remainder` handling (#8916)
    #### Reference Issues/PRs
    #3429 
    
    #### What does this implement/fix? Explain your changes.
    Fixes `ColumnEnsembleClassifier` `remainder` handling, and adds a second dict as return for `get_test_params`

--- sktime/tests/_config.py
+++ sktime/tests/_config.py
@@ -206,7 +206,6 @@ EXCLUDED_TESTS_BY_TEST = {
         "CircularBinarySegmentation",
         "ClaSPTransformer",
         "ClearSky",
-        "ColumnEnsembleClassifier",
         "ColumnwiseTransformer",
         "ContractableBOSS",
         "DOBIN",
