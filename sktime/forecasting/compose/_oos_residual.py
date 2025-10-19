59fe39e1f3ad79e98009f8bac34b5bf1c912dc72
geetu040 <raoarmaghanshakir040@gmail.com>
2025-10-19 11:40:12 +0500

    enable capability:pred_int

--- sktime/forecasting/compose/_oos_residual.py
+++ sktime/forecasting/compose/_oos_residual.py
@@ -19,14 +19,13 @@ class OosResidualsWrapper(BaseForecaster):
         "scitype:y": "univariate",
         "capability:exogenous": True,
         "capability:insample": True,
-        "capability:pred_int": False,
+        "capability:pred_int": True,
         "capability:pred_int:insample": True,
         "capability:missing_values": True,
         "capability:categorical_in_X": True,
         "y_inner_mtype": ["pd.DataFrame", "pd-multiindex", "pd_multiindex_hier"],
         "X_inner_mtype": ["pd.DataFrame", "pd-multiindex", "pd_multiindex_hier"],
         "requires-fh-in-fit": False,
-        "fit_is_empty": False,
     }
 
     def __init__(self, forecaster, cv=None):
@@ -40,7 +39,7 @@ class OosResidualsWrapper(BaseForecaster):
         self.set_tags(
             **{
                 "capability:exogenous": self.forecaster.get_tag("capability:exogenous"),
-                # "capability:pred_int": self.forecaster.get_tag("capability:pred_int"),
+                "capability:pred_int": self.forecaster.get_tag("capability:pred_int"),
                 "capability:missing_values": self.forecaster.get_tag(
                     "capability:missing_values"
                 ),
@@ -50,57 +49,76 @@ class OosResidualsWrapper(BaseForecaster):
             }
         )
 
-    def _fit(self, y, X, fh):
-        if fh is not None and not fh.is_all_in_sample(self.cutoff):
-            out_of_sample_fh = fh.to_out_of_sample(self.cutoff)
-        else:
-            out_of_sample_fh = None
+    def _split_fh(self, fh=None):
+        if fh is None:
+            return None, None
 
-        self._out_of_sample_forecaster = clone(self.forecaster)
-        self._out_of_sample_forecaster.fit(y=y, X=X, fh=out_of_sample_fh)
-
-    def _predict(self, fh, X):
-        """Predicts using the OosResidualsWrapper."""
-        _y = self._y
-        _X = self._X
-
-        # Split fh into in-sample and out-of-sample
-        out_of_sample_fh = (
-            fh.to_out_of_sample(self.cutoff)
-            if not fh.is_all_in_sample(self.cutoff)
-            else None
-        )
         in_sample_fh = (
             fh.to_in_sample(self.cutoff)
             if not fh.is_all_out_of_sample(self.cutoff)
             else None
         )
+        out_of_sample_fh = (
+            fh.to_out_of_sample(self.cutoff)
+            if not fh.is_all_in_sample(self.cutoff)
+            else None
+        )
+
+        return in_sample_fh, out_of_sample_fh
+
+    def _fit(self, y, X, fh):
+        _, out_of_sample_fh = self._split_fh(fh)
+
+        self._out_of_sample_forecaster = clone(self.forecaster)
+        self._out_of_sample_forecaster.fit(y=y, X=X, fh=out_of_sample_fh)
+
+    def _custom_predict(self, fh, X, method_name, **method_kwargs):
+        """Predicts using the OosResidualsWrapper."""
+        _y = self._y
+        _X = self._X
+        in_sample_fh, out_of_sample_fh = self._split_fh(fh)
 
         # Prepare CV
         cv = self.cv
         if cv is None and in_sample_fh is not None:
             from sktime.split import ExpandingWindowSplitter
+            from sktime.utils.warnings import warn
+
+            warn(
+                "No cross-validation splitter (`cv`) was provided to "
+                "`OosResidualsWrapper`. A default "
+                "`ExpandingWindowSplitter(initial_window=2)` has been "
+                "initialized. Ensure that the wrapped forecaster is "
+                "compatible with this splitter, or pass a suitable `cv` "
+                "object when constructing the wrapper.",
+                category=UserWarning,
+                obj=self,
+            )
 
             cv = ExpandingWindowSplitter(initial_window=2)
         elif cv is not None:
             cv = check_cv(cv)
 
         # Prepare placeholder for predictions
+        columns = self._get_columns(method=method_name, **method_kwargs)
         index = fh.to_absolute_index(self.cutoff)
         if isinstance(_y.index, pd.MultiIndex):
             index = pd.MultiIndex.from_product(
                 _y.index.levels[:-1] + [index], names=_y.index.names
             )
-        preds = pd.DataFrame(pd.NA, index=index, columns=_y.columns)
+        preds = pd.DataFrame(0.0, index=index, columns=columns)
 
         # Out-of-sample predictions
         if out_of_sample_fh is not None:
-            pred = self._out_of_sample_forecaster.predict(X=X, fh=out_of_sample_fh)
+            method = getattr(self._out_of_sample_forecaster, method_name)
+            pred = method(X=X, fh=out_of_sample_fh, **method_kwargs)
             preds.loc[pred.index] = pred.values
 
         # In-sample predictions using cross-validation
         if in_sample_fh is not None:
             self._in_sample_forecaster = clone(self.forecaster)
+            method = getattr(self._in_sample_forecaster, method_name)
+
             # fit on the first training window
             window, horizon = next(cv.split(_y))
             new_y = _y.iloc[window]
@@ -113,13 +131,21 @@ class OosResidualsWrapper(BaseForecaster):
                 new_X = _X.iloc[window] if _X is not None else None
                 new__X = _X.iloc[horizon] if _X is not None else None
 
-                self._in_sample_forecaster.update(y=new_y, X=new_X, update_params=False)
-                pred = self._in_sample_forecaster.predict(X=new__X)
+                self._in_sample_forecaster.update(y=new_y, X=new_X, update_params=True)
+                pred = method(X=new__X, **method_kwargs)
 
                 common_idx = pred.index.intersection(preds.index)
                 preds.loc[common_idx] = pred.loc[common_idx].values
 
         return preds
+
+    def _predict(self, fh, X):
+        return self._custom_predict(fh=fh, X=X, method_name="predict")
+
+    def _predict_interval(self, fh, X=None, coverage=0.9):
+        return self._custom_predict(
+            fh=fh, X=X, method_name="predict_interval", coverage=coverage
+        )
 
     @classmethod
     def get_test_params(cls, parameter_set="default"):
