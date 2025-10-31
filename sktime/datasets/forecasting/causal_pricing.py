1c8dc292f7c2f956d29075e11864d65eaf52b67c
XAheli <ahelipoddar2003@gmail.com>
2025-10-31 16:27:03 +0530

    fix: Make pgmpy soft dependency with conditional check in __init__

--- sktime/datasets/forecasting/causal_pricing.py
+++ sktime/datasets/forecasting/causal_pricing.py
@@ -671,6 +671,13 @@ class CausalPricing(_ForecastingDatasetFromLoader):
         self._tags["n_timepoints"] = n_timepoints
         self._tags["n_panels"] = n_series
 
+        if return_ground_truth:
+            from sktime.utils.dependencies import _check_soft_dependencies
+
+            _check_soft_dependencies(
+                "pgmpy", severity="error", obj=self.__class__.__name__
+            )
+
         super().__init__()
 
     def _split_into_y_and_X(self, loader_output):
