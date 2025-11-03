7b2e05ae9c70da226e1d2e375ec72d4cea704f42
Old Patrick <patrick@old>
2025-11-03 11:45:39 +0100

    changed param ssignment

--- sktime/param_est/tests/test_cointegration.py
+++ sktime/param_est/tests/test_cointegration.py
@@ -1,12 +1,15 @@
 
 from sktime.datasets import load_airline
 from sktime.param_est.cointegration import JohansenCointegration
-
+import pandas as pd
 
 
 X = load_airline()
+X2 = X.shift(1).bfill()
+df = pd.DataFrame({"X":X, "X2": X2})
+print(df)
 coint_est = JohansenCointegration()
-coint_est.fit(X)
+coint_est.fit(df)
 
 print(coint_est.trace_stat)
 print(coint_est.trace_stat_crit_vals)
