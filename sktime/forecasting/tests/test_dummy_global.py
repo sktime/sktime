63f72673e6159a636d10f2fe7052f9a8f8895e6e
Simon Blanke <simon.blanke@yahoo.com>
2025-11-08 18:32:02 +0100

    add test

--- sktime/forecasting/tests/test_dummy_global.py
+++ sktime/forecasting/tests/test_dummy_global.py
@@ -1,6 +1,6 @@
 """Tests for DummyGlobalForecaster."""
 
-__author__ = ["fkiraly"]
+__author__ = ["SimonBlanke"]
 
 import numpy as np
 import pandas as pd
@@ -21,10 +21,7 @@ class TestDummyGlobalForecaster:
             hierarchy_levels=(3,), min_timepoints=10, max_timepoints=10, n_columns=1
         )
 
-        # Initialize forecaster
         forecaster = DummyGlobalForecaster(strategy="mean")
-
-        # Pretrain
         forecaster.pretrain(y_panel)
 
         # Check that global mean was computed
@@ -35,8 +32,6 @@ class TestDummyGlobalForecaster:
         # Fit on a single series
         y_train = _make_series(n_columns=1, n_timepoints=20)
         forecaster.fit(y_train, fh=[1, 2, 3])
-
-        # Predict
         y_pred = forecaster.predict()
 
         # Check predictions
@@ -45,4 +40,28 @@ class TestDummyGlobalForecaster:
         # All predictions should be the global mean
         np.testing.assert_array_almost_equal(
             y_pred.values, np.repeat(forecaster.global_mean_, 3)
+        )
+
+    def test_pretrain_fit_predict_last_strategy(self):
+        """Test basic pretrain-fit-predict workflow with last strategy."""
+        # Create panel data for pretraining
+        y_panel = _make_hierarchical(
+            hierarchy_levels=(2,), min_timepoints=10, max_timepoints=10, n_columns=1
+        )
+
+        forecaster = DummyGlobalForecaster(strategy="last")
+        forecaster.pretrain(y_panel)
+
+        # Fit on a single series
+        y_train = _make_series(n_columns=1, n_timepoints=20)
+        forecaster.fit(y_train, fh=[1, 2, 3])
+        y_pred = forecaster.predict()
+
+        # Check predictions
+        assert len(y_pred) == 3
+        assert isinstance(y_pred, pd.Series)
+        # All predictions should be the last value
+        expected_value = y_train.iloc[-1]
+        np.testing.assert_array_almost_equal(
+            y_pred.values, np.repeat(expected_value, 3)
         )
