3e4df3f2ff085140b59aefdb8b96d49fa5bc0440
Nirbhai <nsceh1@gmail.com>
2025-11-19 03:03:55 +0530

    [BUG] Improve default activation_hidden in SimpleRNNClassifier, SimpleRNNRegressor & RNNNetwork (#9048)
    Fixes #9047 
    
    #### What does this implement/fix? Explain your changes.
    
    Changed default activation_hidden from linear to tanh in
    SimpleRNNClassifier, SimpleRNNRegressor & RNNNetwork.

--- sktime/regression/deep_learning/rnn.py
+++ sktime/regression/deep_learning/rnn.py
@@ -10,8 +10,14 @@ from sklearn.utils import check_random_state
 from sktime.networks.rnn import RNNNetwork
 from sktime.regression.deep_learning.base import BaseDeepRegressor
 from sktime.utils.dependencies import _check_dl_dependencies
+from sktime.utils.warnings import warn
 
 
+# TODO (release 0.41.0)
+# change the default value of 'activation_hidden' to "tanh"
+# update the docstring for activation_hidden from "linear" to "tanh"
+# and remove the usage of self._activation_hidden throughout the class
+# and replace it with self.activation_hidden
 class SimpleRNNRegressor(BaseDeepRegressor):
     """Simple recurrent neural network.
 
@@ -40,6 +46,8 @@ class SimpleRNNRegressor(BaseDeepRegressor):
     activation_hidden : string or a tf callable, default="linear"
         Activation function used in the hidden layers.
         List of available activation functions: https://keras.io/api/layers/activations/
+        Default value of activation_hidden will change to "tanh"
+        in version '0.41.0'.
     use_bias : boolean, default = True
         whether the layer uses a bias vector.
     optimizer : keras.optimizers object, default = RMSprop(lr=0.001)
@@ -73,6 +81,8 @@ class SimpleRNNRegressor(BaseDeepRegressor):
         ],
     }
 
+    # TODO (release 0.41.0)
+    # Change the default value of 'activation_hidden' to "tanh"
     def __init__(
         self,
         n_epochs=100,
@@ -85,7 +95,7 @@ class SimpleRNNRegressor(BaseDeepRegressor):
         loss="mean_squared_error",
         metrics=None,
         activation="linear",
-        activation_hidden="linear",
+        activation_hidden="changing_from_linear_to_tanh_in_0.41.0",
         use_bias=True,
         optimizer=None,
     ):
@@ -102,14 +112,37 @@ class SimpleRNNRegressor(BaseDeepRegressor):
         self.metrics = metrics
         self.activation = activation
         self.activation_hidden = activation_hidden
+
+        # TODO (release 0.41.0)
+        # After changing the default value of 'activation_hidden' to "tanh"
+        # remove the following 'if-else' check
+        # and remove the usage of self._activation_hidden throughout the class
+        # and replace it with self.activation_hidden
+        if activation_hidden == "changing_from_linear_to_tanh_in_0.41.0":
+            warn(
+                "in `SimpleRNNRegressor`, the default value of parameter "
+                "'activation_hidden' will change to 'tanh' in version '0.41.0'. "
+                "To keep current behaviour and to silence this warning, "
+                "set 'activation_hidden' to 'linear' explicitly.",
+                category=DeprecationWarning,
+                obj=self,
+            )
+            self._activation_hidden = "linear"
+        else:
+            self._activation_hidden = activation_hidden
         self.use_bias = use_bias
         self.optimizer = optimizer
 
         super().__init__()
 
+        # TODO (release 0.41.0)
+        # After changing the default value of 'activation_hidden' to "tanh"
+        # in the __init__ method,
+        # remove the usage of self._activation_hidden in the following lines
+        # and replace it with self.activation_hidden
         self.history = None
         self._network = RNNNetwork(
-            activation=self.activation_hidden,
+            activation=self._activation_hidden,
             random_state=random_state,
             units=units,
         )
