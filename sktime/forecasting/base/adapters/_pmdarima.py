000795ffd57c0d1b891aed20b13810f8589681e5
Franz Király <fkiraly@gcos.ai>
2025-11-22 17:21:47 +0100

    [MNT] 0.40.0 deprecations and change actions (#9080)
    Carries out deprecations and change actions scheduled for 0.40.0:
    
    * update `pmdarima` bounds after recent `numpy 2` compatibility update
    * bump deprecation/change checks scheduled for 0.40.0 to 0.41.0 or 1.0.0

--- sktime/forecasting/base/adapters/_pmdarima.py
+++ sktime/forecasting/base/adapters/_pmdarima.py
@@ -23,9 +23,7 @@ class _PmdArimaAdapter(BaseForecaster):
         "capability:pred_int:insample": True,
         "requires-fh-in-fit": False,
         "capability:missing_values": True,
-        # TODO 0.40.0: check if numpy 2 incompatiblity can be removed
-        # todo 0.40.0: check whether scipy<1.16 is still needed
-        "python_dependencies": ["pmdarima", "numpy<2", "scipy<1.16"],
+        "python_dependencies": ["pmdarima"],
     }
 
     def __init__(self):
