30a64d82468fed56d60769ea5d044bb745c0b0bb
EmanAbdelhaleem <emanabdelhaleem4@gmail.com>
2025-11-27 13:46:21 +0200

    added tests for hierarchical data with two levels

--- sktime/forecasting/tests/test_known_values.py
+++ sktime/forecasting/tests/test_known_values.py
@@ -52,7 +52,51 @@ def test_multiindex(fh, y_known) -> None:
         check_index_type=False,
     )
 
+@pytest.mark.skipif(
+    not run_test_for_class(ForecastKnownValues),
+    reason="run test only if softdeps are present and incrementally (if requested)",
+)
+@pytest.mark.parametrize("fh", TEST_OOS_FHS)
+@pytest.mark.parametrize(
+    "levels, time_points",
+    [
+        (["A", "B"], [1, 2, 3]),
+        (["AAA", "BBB", "CCC"], [1, 2]),
+        (["X"], [1, 2, 3, 4]),
+        ([1, 2, 3], [10, 20, 30]),
+    ],
+)
+def test_two_level_hierarchical(fh, levels, time_points) -> None:
+    """Test 2-level hierarchical y_known."""
+    index = pd.MultiIndex.from_product(
+        [levels, time_points],
+        names=["Level1", "Date"],
+    )
+    data = range(len(index))
+    
+    y_known = pd.DataFrame(data, index=index, columns=["Value"])
 
+    f = ForecastKnownValues(y_known=y_known)
+    f.fit(y_known)
+    y_pred = f.predict(fh)
+
+    # Create expected data
+    if not isinstance(fh, np.ndarray):
+        fh = np.array([fh])
+
+    expected_index = pd.MultiIndex.from_product(
+        [levels, fh + time_points[-1]],
+        names=["Level1", "Date"],
+    )
+    expected = pd.DataFrame(None, index=expected_index, columns=["Value"])
+
+    pd.testing.assert_frame_equal(
+        y_pred,
+        expected,
+        check_dtype=False,
+        check_index_type=False,
+    )
+    
 @pytest.mark.skipif(
     not run_test_for_class(ForecastKnownValues),
     reason="run test only if softdeps are present and incrementally (if requested)",
