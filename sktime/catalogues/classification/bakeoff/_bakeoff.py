3ad4e9c5c2c26e1ab96ffaee9d680d897583f6d0
Jigyasu <jigyasu@outlook.in>
2025-11-28 23:19:06 +0530

    [ENH] TSC Bake Off 2017 Catalogue

--- sktime/catalogues/classification/bakeoff/_bakeoff.py
+++ sktime/catalogues/classification/bakeoff/_bakeoff.py
@@ -1,7 +1,6 @@
 """TSC Bake off 2017 catalogue."""
 
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import KFold
 
 from sktime.catalogues.base import BaseCatalogue
 
@@ -14,11 +13,11 @@ class BakeOffCatalogue(BaseCatalogue):
         "maintainers": "jgyasu",
         "object_type": "catalogue",
         "catalogue_type": "mixed",
-        "n_items": 5,
-        "n_datasets": 2,
+        "n_items": 87,
+        "n_datasets": 85,
         "n_classifiers": 1,
         "n_metrics": 1,
-        "n_cv_splitters": 1,
+        "n_cv_splitters": 0,
         "info:name": "The great time series classification bake off",
         "info:source": "https://doi.org/10.1007/s10618-016-0483-9",
     }
@@ -26,17 +25,100 @@ class BakeOffCatalogue(BaseCatalogue):
     def _get(self):
         """Return a dict of items (datasets, forecasters, metrics)."""
         datasets = [
+            "MiddlePhalanxTW",
+            "TwoLeadECG",
+            "WormsTwoClass",
+            "Meat",
+            "MiddlePhalanxOutlineCorrect",
             "Beef",
             "ArrowHead",
+            "ECG5000",
+            "ScreenType",
+            "Wine",
+            "Adiac",
+            "Coffee",
+            "DistalPhalanxOutlineCorrect",
+            "OliveOil",
+            "Symbols",
+            "Lightning2",
+            "SonyAIBORobotSurface2",
+            "Computers",
+            "Haptics",
+            "UWaveGestureLibraryY",
+            "FacesUCR",
+            "SwedishLeaf",
+            "WordSynonyms",
+            "ProximalPhalanxTW",
+            "ProximalPhalanxOutlineAgeGroup",
+            "ShapeletSim",
+            "Herring",
+            "Car",
+            "CricketY",
+            "UWaveGestureLibraryZ",
+            "Phoneme",
+            "PhalangesOutlinesCorrect",
+            "HandOutlines",
+            "InlineSkate",
+            "SyntheticControl",
+            "Yoga",
+            "ProximalPhalanxOutlineCorrect",
+            "ToeSegmentation2",
+            "Ham",
+            "CricketZ",
+            "Trace",
+            "Worms",
+            "UWaveGestureLibraryAll",
+            "CricketX",
+            "DiatomSizeReduction",
+            "FordA",
+            "CinCECGTorso",
+            "MedicalImages",
+            "FaceAll",
+            "Plane",
+            "ToeSegmentation1",
+            "RefrigerationDevices",
+            "OSULeaf",
+            "MoteStrain",
+            "ShapesAll",
+            "CBF",
+            "ECG200",
+            "Lightning7",
+            "SonyAIBORobotSurface1",
+            "SmallKitchenAppliances",
+            "FordB",
+            "ECGFiveDays",
+            "TwoPatterns",
+            "DistalPhalanxOutlineAgeGroup",
+            "LargeKitchenAppliances",
+            "NonInvasiveFetalECGThorax2",
+            "Mallat",
+            "DistalPhalanxTW",
+            "ItalyPowerDemand",
+            "GunPoint",
+            "NonInvasiveFetalECGThorax1",
+            "Earthquakes",
+            "ChlorineConcentration",
+            "Wafer",
+            "Fish",
+            "ElectricDevices",
+            "StarlightCurves",
+            "InsectWingbeatSound",
+            "FiftyWords",
+            "BirdChicken",
+            "MiddlePhalanxOutlineAgeGroup",
+            "Strawberry",
+            "BeetleFly",
+            "UWaveGestureLibraryX",
+            "FaceFour",
         ]
 
         classifiers = [
-            "DummyClassifier()",
+            "ShapeletLearningClassifierTslearn()",
         ]
 
         metrics = [accuracy_score]
 
-        cv_splitters = [KFold(n_splits=3)]
+        cv_splitters = []
 
         all_objects = {
             "dataset": [f"UCRUEADataset('{dataset}')" for dataset in datasets],
