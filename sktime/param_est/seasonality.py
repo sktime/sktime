92bda7fcadef72686f2aaf5514d3af03f8c2c329
JATAYU000 <shrivaths44kunju@gmail.com>
2025-12-04 17:40:17 +0530

    Implemented periodogram functions

--- sktime/param_est/seasonality.py
+++ sktime/param_est/seasonality.py
@@ -466,19 +466,138 @@ class SeasonalityPeriodogram(BaseParamFitter):
         "scitype:X": "Series",
         "capability:missing_values": True,
         "capability:multivariate": False,
-        # todo 0.40.0: check whether scipy<1.16 is still needed
-        "python_dependencies": ["seasonal", "scipy<1.16"],
-        # CI and test flags
-        # -----------------
-        "tests:vm": True,  # tested on separate VM due to seasonal dependency
-        "tests:core": False,  # should tests be triggered by framework changes?
     }
 
     def __init__(self, min_period=4, max_period=None, thresh=0.10):
         self.min_period = min_period
         self.max_period = max_period
         self.thresh = thresh
+        self.MIN_FFT_CYCLES = 3.0
+        self.MAX_FFT_PERIOD = 512
         super().__init__()
+
+    def periodogram_peaks(self, data, min_period=4, max_period=None, thresh=0.90):
+        """Return a list of intervals containg high-scoring periods.
+
+        Use a robust periodogram to estimate ranges containing
+        high-scoring periodicities in possibly short, noisy data. Returns
+        each peak period along with its adjacent bracketing periods from
+        the FFT coefficient sequence.
+
+        Data should be detrended for sharpest results, but trended data
+        can be accommodated by lowering thresh (resulting in more
+        intervals being returned)
+
+
+        Parameters
+        ----------
+        data : ndarray
+            Data series, evenly spaced samples.
+        min_period : int
+            Disregard periods shorter than this number of samples.
+            Defaults to 4
+        max_period : int
+            Disregard periods longer than this number of samples.
+            Defaults to the smaller of len(data)/MIN_FFT_CYCLES or MAX_FFT_PERIOD
+        thresh : float (0..1)
+            Retain periods scoring above thresh*maxscore. Defaults to 0.9
+
+        Returns
+        -------
+        periods : array of quads, or None
+            Array of (period, power, period-, period+), maximizing period
+            and its score, and FFT periods bracketing the maximizing
+            period, returned in decreasing order of score
+
+        Notes
+        -----
+        This method is adapted from the standalone seasonal package.
+
+        References
+        ----------
+        .. [1] https://github.com/welch/seasonal
+
+        """
+        periods, power = self.periodogram(data, min_period, max_period)
+        if np.all(np.isclose(power, 0.0)):
+            return None  # DC
+        result = []
+        keep = power.max() * thresh
+        while True:
+            peak_i = power.argmax()
+            if power[peak_i] < keep:
+                break
+            min_period = periods[min(peak_i + 1, len(periods) - 1)]
+            max_period = periods[max(peak_i - 1, 0)]
+            result.append([periods[peak_i], power[peak_i], min_period, max_period])
+            power[peak_i] = 0
+        return result if len(result) else None
+
+    def periodogram(self, data, min_period=4, max_period=None):
+        """Score periodicities by their spectral power.
+
+        Produce a robust periodogram estimate for each possible periodicity
+        of the (possibly noisy) data.
+
+        **Important**: Periods are computed with respect to positional indices (iloc),
+        not label-based indices (loc).
+
+        Parameters
+        ----------
+        data : ndarray
+            Data series, having at least three periods of data.
+        min_period : int
+            Disregard periods shorter than this number of samples.
+            Defaults to 4
+        max_period : int
+            Disregard periods longer than this number of samples.
+            Defaults to the smaller of len(data)/MIN_FFT_CYCLES or MAX_FFT_PERIOD
+
+        Returns
+        -------
+        periods, power : ndarray, ndarray
+            Periods is an array of Fourier periods in descending order,
+            beginning with the first one greater than max_period.
+            Power is an array of spectral power values for the periods
+
+        Notes
+        -----
+        This uses Welch's method (no relation) of periodogram
+        averaging[1]_, which trades off frequency precision for better
+        noise resistance. We don't look for sharp period estimates from
+        it, as it uses the FFT, which evaluates at periods N, N/2, N/3, ...,
+        so that longer periods are sparsely sampled.
+
+        References
+        ----------
+        .. [1]: https://en.wikipedia.org/wiki/Welch%27s_method
+        .. [2]: https://github.com/welch/seasonal
+
+        """
+        import scipy.signal
+
+        data = np.asarray(data)
+        if max_period is None:
+            max_period = int(min(len(data) / self.MIN_FFT_CYCLES, self.MAX_FFT_PERIOD))
+        nperseg = min(max_period * 2, len(data) // 2)  # FFT window
+        freqs, power = scipy.signal.welch(
+            data, 1.0, scaling="spectrum", nperseg=nperseg
+        )
+        periods = np.array([int(round(1.0 / freq)) for freq in freqs[1:]])
+        power = power[1:]
+        # take the max among frequencies having the same integer part
+        idx = 1
+        while idx < len(periods):
+            if periods[idx] == periods[idx - 1]:
+                power[idx - 1] = max(power[idx - 1], power[idx])
+                periods, power = np.delete(periods, idx), np.delete(power, idx)
+            else:
+                idx += 1
+        power[periods == nperseg] = 0  # disregard the artifact at nperseg
+        min_i = len(periods[periods >= max_period]) - 1
+        max_i = len(periods[periods < min_period])
+        periods, power = periods[min_i:-max_i], power[min_i:-max_i]
+        return periods, power
 
     def _fit(self, X):
         """Fit estimator and estimate parameters.
@@ -497,9 +616,7 @@ class SeasonalityPeriodogram(BaseParamFitter):
         -------
         self : reference to self
         """
-        from seasonal.periodogram import periodogram_peaks
-
-        seasons = periodogram_peaks(
+        seasons = self.periodogram_peaks(
             X,
             min_period=self.min_period,
             max_period=self.max_period,
