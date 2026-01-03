9b735f16fcd4c05a1d3e86ad2b91676a7549a73a
Franz Király <fkiraly@gcos.ai>
2026-01-03 19:00:48 +0100

    Update vecm.py

--- sktime/forecasting/vecm.py
+++ sktime/forecasting/vecm.py
@@ -221,17 +221,16 @@ class VECM(_StatsModelsAdapter):
                 y_pred_insample if y_pred_insample is not None else y_pred_outsample
             )
 
-        index = fh.to_absolute_index(self.cutoff)
-        index.name = self._y.index.name
-        y_pred = pd.DataFrame(
-            y_pred[fh.to_indexer(self.cutoff), :],
-            index=index,
-            columns=self._y.columns,
-        )
-
         # invert the "only_1s" column if it was added during fit
         if self._y_metadata["n_features"] == 1:
             y_pred = y_pred.iloc[:, 0]
+
+        index = fh.get_expected_pred_idx(cutoff=self.cutoff)
+        y_pred = pd.DataFrame(
+            y_pred[fh.to_indexer(self.cutoff), :],
+            index=index,
+            columns=self._get_columns(),
+        )
 
         return y_pred
 
