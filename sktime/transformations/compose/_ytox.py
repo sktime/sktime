e7470a6d55133699e96dd947bd4ec3d0f022a3b2
Franz Király <fkiraly@gcos.ai>
2026-01-04 19:47:19 +0100

    linting

--- sktime/transformations/compose/_ytox.py
+++ sktime/transformations/compose/_ytox.py
@@ -124,7 +124,6 @@ class YtoX(BaseTransformer):
         "capability:multivariate": True,
         "X_inner_mtype": ["pd.DataFrame", "pd-multiindex", "pd_multiindex_hier"],
         "y_inner_mtype": ["pd.DataFrame", "pd-multiindex", "pd_multiindex_hier"],
-        "capability:multivariate": True,
         "fit_is_empty": True,
         "requires_X": False,
         "requires_y": True,
