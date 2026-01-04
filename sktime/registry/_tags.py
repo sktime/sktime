2094fe26f1ffb81336f6812ad600fa08f6e6421d
Franz Király <fkiraly@gcos.ai>
2026-01-04 23:05:38 +0100

    deprecation test

--- sktime/registry/_tags.py
+++ sktime/registry/_tags.py
@@ -3525,6 +3525,15 @@ ESTIMATOR_TAG_REGISTER = [
         "which scitypes does X internally support?",
     ),
     (
+        "scitype:y",  # -> capability:multivariate
+        # the scitype:y tag should be kept but for separate use,
+        # a list of the internal scitypes supported by the estimator
+        # or the base scitype of the target data
+        ["param_est", "metric"],
+        "str",
+        "which series type does the forecaster support? multivariate means >1 vars",
+    ),
+    (
         "scitype:instancewise",
         "transformer",
         "bool",
@@ -3717,15 +3726,6 @@ ESTIMATOR_TAG_REGISTER = [
         "forecaster",
         "bool",
         "deprecated tag for exogenous capability",
-    ),
-    (
-        "scitype:y",  # -> capability:multivariate
-        # the scitype:y tag should be kept but for separate use,
-        # a list of the internal scitypes supported by the estimator
-        # or the base scitype of the target data
-        "forecaster",
-        ("str", ["univariate", "multivariate", "both"]),
-        "which series type does the forecaster support? multivariate means >1 vars",
     ),
 ]
 
