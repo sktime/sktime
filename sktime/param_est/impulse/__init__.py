d39d6e6dcbfb8518de9f172e4824ab6d8285ae4b
Old Patrick <patrick@old>
2026-01-11 18:09:49 +0100

    fix whitespaces

--- sktime/param_est/impulse/__init__.py
+++ sktime/param_est/impulse/__init__.py
@@ -1,10 +1,10 @@
 """Module for parameter retrieval of impulse responses functions.
 
-This module provides functionality for calculating the impulse responses for 
-univariate and multivariate time-series methods, like ARIMA, VAR, VECM, 
-VARMAX and other. The main class ImpulseResponseFunction provides shall give 
-a unified approach for any time series supported by statsmodels. 
-Supported IRFs according to statsmodels (x marks a check done for sktime, 
+This module provides functionality for calculating the impulse responses for
+univariate and multivariate time-series methods, like ARIMA, VAR, VECM,
+VARMAX and other. The main class ImpulseResponseFunction provides shall give
+a unified approach for any time series supported by statsmodels.
+Supported IRFs according to statsmodels (x marks a check done for sktime,
 given the estimator exists in the library):
 - ARIMA
 - ARMA
