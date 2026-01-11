2420e3a1be1e5403304360c05b191bc91402e09c
Old Patrick <patrick@old>
2026-01-11 17:27:10 +0100

    checked IRF support from statsmodels

--- sktime/param_est/impulse/__init__.py
+++ sktime/param_est/impulse/__init__.py
@@ -1,9 +1,30 @@
 """Module for parameter retrieval of impulse responses functions
 
 This module provides functionality for calculating the impulse responses for 
-univariate and multivariate time-series methods, like ARIMA, VAR, VECM, VARMAX and other.  
+univariate and multivariate time-series methods, like ARIMA, VAR, VECM, 
+VARMAX and other.  
 
-The main class ImpulseResponseFunction provides shall give a unified approach for any time series uses
+The main class ImpulseResponseFunction provides shall give a unified approach 
+for any time series supported by statsmodels.
+
+Supported IRFs according to statsmodels 
+(x marks a check done for sktime, given the estimator exists in the library):
+
+- ARIMA
+- ARMA
+- DynamicFactor
+- DynamicFactorMQ
+- ExponentialSmoothing
+- KalmanFilter
+- KalmanSmoother
+- MLEModel
+- RecursiveLS
+- SARIMAX
+- SimulationSmoother
+- UnobservedComponents
+- VAR
+- VARMAX
+- VECM
 """
 
 __all__ = [
