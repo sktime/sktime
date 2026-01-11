800d36e86704d3120b75f82be0c9a68e8b98f3b1
Old Patrick <patrick@old>
2026-01-11 17:34:32 +0100

    missing dot

--- sktime/param_est/impulse/__init__.py
+++ sktime/param_est/impulse/__init__.py
@@ -1,4 +1,4 @@
-"""Module for parameter retrieval of impulse responses functions
+"""Module for parameter retrieval of impulse responses functions.
 
 This module provides functionality for calculating the impulse responses for 
 univariate and multivariate time-series methods, like ARIMA, VAR, VECM, 
