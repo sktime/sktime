a25f9f9e9792f3e2f2f3b2177ed18d0c8988b3a1
prashantpandeygit <prashantpandeyiuet@gmail.com>
2026-01-20 14:08:52 +0530

    lint

--- sktime/transformations/compose/__init__.py
+++ sktime/transformations/compose/__init__.py
@@ -9,8 +9,10 @@ from sktime.transformations.compose._column import (
 )
 from sktime.transformations.compose._featureunion import FeatureUnion
 from sktime.transformations.compose._fitintransform import FitInTransform
-from sktime.transformations.compose._grouped import GroupByCategoryTransformer
-from sktime.transformations.compose._grouped import TransformByLevel
+from sktime.transformations.compose._grouped import (
+    GroupByCategoryTransformer,
+    TransformByLevel,
+)
 from sktime.transformations.compose._id import Id
 from sktime.transformations.compose._invert import InvertTransform
 from sktime.transformations.compose._ixtox import IxToX
@@ -21,7 +23,15 @@ from sktime.transformations.compose._pipeline import TransformerPipeline
 from sktime.transformations.compose._transformif import TransformIf
 from sktime.transformations.compose._ytox import YtoX
 
-__author__ = ["fkiraly", "mloning", "miraep8", "aiwalter", "SveaMeyer13", "prashantpandeygit"]
+__author__ = [
+    "fkiraly",
+    "mloning",
+    "miraep8",
+    "aiwalter",
+    "SveaMeyer13",
+    "prashantpandeygit",
+]
+
 __all__ = [
     "ColumnwiseTransformer",
     "ColumnEnsembleTransformer",
