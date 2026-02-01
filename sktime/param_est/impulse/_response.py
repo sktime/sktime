291e7734bc23e971ed215c6f069669f379c7a531
Old Patrick <patrick@old>
2026-02-01 19:09:37 +0100

    pre-commit clean

--- sktime/param_est/impulse/_response.py
+++ sktime/param_est/impulse/_response.py
@@ -4,17 +4,18 @@ author = ["PBormann"]
 all = ["ImpulseResponseFunction"]
 
 import numpy as np
-from sktime.param_est.base import BaseParamFitter
-
-from statsmodels.tsa.statespace.varmax import VARMAX
 from statsmodels.tsa.statespace.dynamic_factor import DynamicFactor
 from statsmodels.tsa.statespace.dynamic_factor_mq import DynamicFactorMQ
+from statsmodels.tsa.statespace.varmax import VARMAX
+
+from sktime.param_est.base import BaseParamFitter
 
 MODEL_MAPPING = {
     "VARMAX": VARMAX,
     "DynamicFactor": DynamicFactor,
-    "DynamicFactorMQ": DynamicFactorMQ
+    "DynamicFactorMQ": DynamicFactorMQ,
 }
+
 
 class ImpulseResponseFunction(BaseParamFitter):
     """
@@ -27,15 +28,12 @@ class ImpulseResponseFunction(BaseParamFitter):
     ----------
     name : type, default=
 
-        * 
-       
+        *
+
 
     Attributes
     ----------
-    some :  np.ndarray of float 
-
-    Examples
-    --------
+    some :  np.ndarray of float
 
 
     Notes
@@ -54,19 +52,19 @@ class ImpulseResponseFunction(BaseParamFitter):
 
     def test():
         _tags = {
-        "X_inner_mtype": "np.ndarray",  # no support of pl.DataFrame
-        "capability:missing_values": False,
-        "capability:multivariate": True,
-        "capability:pairwise": True,
-        "authors": "PBormann",
-        "python_dependencies": "statsmodels",
-    }
+            "X_inner_mtype": "np.ndarray",  # no support of pl.DataFrame
+            "capability:missing_values": False,
+            "capability:multivariate": True,
+            "capability:pairwise": True,
+            "authors": "PBormann",
+            "python_dependencies": "statsmodels",
+        }
 
     def __init__(
         self,
-        model=None, #default fitted None
+        model=None,  # default fitted None
     ):
-        self.model = model #needs a previously fitted model
+        self.model = model  # needs a previously fitted model
 
         super().__init__()
 
@@ -76,11 +74,12 @@ class ImpulseResponseFunction(BaseParamFitter):
         Text from statsmodels:
         https://www.statsmodels.org/dev/generated/statsmodels.tsa.arima.model.ARIMA.impulse_responses.html
 
-        Responses for each endogenous variable due to the impulse given by the impulse argument. 
-        For a time-invariant model, the impulse responses are given for steps + 1 elements 
-        (this gives the “initial impulse” followed by steps responses for the important 
-        cases of VAR and SARIMAX models), while for time-varying models the impulse responses 
-        are only given for steps elements (to avoid having to unexpectedly provide updated 
+        Responses for each endogenous variable due to the impulse given by the
+        impulse argument. For a time-invariant model, the impulse responses are
+        given for steps + 1 elements (this gives the “initial impulse” followed
+        by steps responses for the important cases of VAR and SARIMAX models),
+        while for time-varying models the impulse responses are only given for
+        steps elements (to avoid having to unexpectedly provide updated
         time-varying matrices).
 
         Parameters
@@ -95,48 +94,41 @@ class ImpulseResponseFunction(BaseParamFitter):
         ImportedModel = MODEL_MAPPING[self.model.__class__.__name__]
         proxy_model = ImportedModel(X)
         return proxy_model.fit().impulse_responses()
-        
+
     def get_irf_from_sktime(self, steps=1, orthogonalized=False):
         model_name = self.model.__class__.__name__
         ImportedModel = MODEL_MAPPING[model_name]
 
-        sm_wrapper = self.model._fitted_forecaster  
+        sm_wrapper = self.model._fitted_forecaster
 
         k_vars = sm_wrapper.model.k_endog
         fitted_params = sm_wrapper.params
 
         dummy_data = np.zeros((10, k_vars))
 
-        if model_name == "VARMAX":               
+        if model_name == "VARMAX":
             p = sm_wrapper.model.k_ar
             q = sm_wrapper.model.k_ma
-            trend_type = sm_wrapper.model.trend 
+            trend_type = sm_wrapper.model.trend
 
-            dummy_model = ImportedModel(
-                dummy_data, 
-                order=(p, q), 
-                trend=trend_type 
-            )   
+            dummy_model = ImportedModel(dummy_data, order=(p, q), trend=trend_type)
         else:
             k_factors = sm_wrapper.model.k_factors
             factor_order = sm_wrapper.model.factor_order
             error_order = sm_wrapper.model.error_order
-            #error_var = sm_wrapper.model.error_var
-            #exog = sm_wrapper.model.exog
+            # error_var = sm_wrapper.model.error_var
+            # exog = sm_wrapper.model.exog
 
             dummy_model = ImportedModel(
                 endog=dummy_data,
-                k_factors=k_factors, 
+                k_factors=k_factors,
                 factor_order=factor_order,
                 error_order=error_order,
-                enforce_stationarity=False
+                enforce_stationarity=False,
             )
-        
 
         irf = dummy_model.impulse_responses(
-            params=fitted_params, 
-            steps=steps,
-            orthogonalized=orthogonalized
+            params=fitted_params, steps=steps, orthogonalized=orthogonalized
         )
 
         return irf
