b6891d9a20f17a81b77124b0974ba450382a1c40
Mohit25f101 <25f1001180@ds.study.iitm.ac.in>
2026-02-05 16:32:23 +0530

    Fix: remove trailing hash from tag

--- sktime/transformations/panel/segment.py
+++ sktime/transformations/panel/segment.py
@@ -208,7 +208,7 @@ class RandomIntervalSegmenter(_DelegatedTransformer):
         "scitype:instancewise": True,  # is this an instance-wise transform?
         "fit_is_empty": False,  # is fit empty and can be skipped? Yes = True
         "capability:unequal_length:removes": True,
-       "tests:skip_all": True,#
+       "tests:skip_all": True,
         # is transform result always guaranteed to be equal length (and series)?
     }
 
