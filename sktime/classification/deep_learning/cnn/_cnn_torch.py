2e74d9d15d37c28c3310e73311f4b70c19578991
Saba Siddique <sabasiddique001@gmail.com>
2026-02-11 19:05:01 +0500

    Merge remote fix/lstmfcn-docstring-params

---
+++ sktime/classification/deep_learning/cnn/_cnn_torch.py
@@ -0,0 +1,214 @@
+"""Time Convolutional Neural Network (CNN) for classification in PyTorch."""
+
+__authors__ = ["sabasiddique1"]
+__all__ = ["CNNClassifierTorch"]
+
+from collections.abc import Callable
+
+import numpy as np
+
+from sktime.classification.deep_learning.base import BaseDeepClassifierPytorch
+from sktime.networks.cnn import CNNNetworkTorch
+
+
+class CNNClassifierTorch(BaseDeepClassifierPytorch):
+    """Time Convolutional Neural Network in PyTorch for time series classification.
+
+    Zhao et al. 2017 uses sigmoid activation in the hidden layers.
+    To obtain same behaviour, set activation_hidden to "sigmoid" (default).
+
+    Adapted from the TensorFlow CNN classifier. Same defaults as the TensorFlow
+    implementation where applicable.
+
+    Parameters
+    ----------
+    kernel_size : int, default = 7
+        Length of the 1D convolution window.
+    avg_pool_size : int, default = 3
+        Size of the average pooling windows.
+    n_conv_layers : int, default = 2
+        Number of convolutional plus average pooling layers.
+    filter_sizes : array-like of int, default = None
+        Filter sizes per conv layer. If None, uses [6, 12].
+    activation_hidden : str, default = "sigmoid"
+        Activation for hidden conv layers. One of "relu", "sigmoid".
+    padding : str, default = "auto"
+        Padding logic: "auto" (same if series_length < 60 else valid), "valid", "same".
+    use_bias : bool, default = True
+        Whether conv and linear layers use a bias vector.
+    num_epochs : int, default = 100
+        Number of epochs to train the model.
+    batch_size : int, default = 16
+        Size of each mini-batch (same default as TF CNN).
+    optimizer : str or None or optimizer instance, default = "Adam"
+        Optimizer to use (TF CNN uses Adam lr=0.01).
+    optimizer_kwargs : dict or None, default = None
+        Additional keyword arguments for the optimizer.
+    lr : float, default = 0.01
+        Learning rate (same default as TF CNN).
+    criterion : str or None or loss instance, default = "CrossEntropyLoss"
+        Loss function for training.
+    criterion_kwargs : dict or None, default = None
+        Additional keyword arguments for the criterion.
+    callbacks : None or str or tuple of str, default = "ReduceLROnPlateau"
+        Learning rate schedulers supported as callbacks.
+    callback_kwargs : dict or None, default = None
+        Keyword arguments for the callbacks.
+    verbose : bool, default = False
+        Whether to print progress during training.
+    random_state : int, default = 0
+        Seed for reproducibility.
+
+    References
+    ----------
+    .. [1] Zhao et al. Convolutional neural networks for time series classification,
+    Journal of Systems Engineering and Electronics, 28(1):2017.
+
+    Examples
+    --------
+    >>> from sktime.classification.deep_learning.cnn import CNNClassifierTorch
+    >>> from sktime.datasets import load_unit_test
+    >>> X_train, y_train = load_unit_test(split="train")
+    >>> X_test, y_test = load_unit_test(split="test")
+    >>> clf = CNNClassifierTorch(n_epochs=20, batch_size=4)  # doctest: +SKIP
+    >>> clf.fit(X_train, y_train)  # doctest: +SKIP
+    CNNClassifierTorch(...)
+    """
+
+    _tags = {
+        "authors": ["sabasiddique1"],
+        "maintainers": ["sabasiddique1"],
+        "python_version": ">=3.9",
+        "python_dependencies": "torch",
+        "property:randomness": "stochastic",
+        "capability:random_state": True,
+    }
+
+    def __init__(
+        self: "CNNClassifierTorch",
+        kernel_size: int = 7,
+        avg_pool_size: int = 3,
+        n_conv_layers: int = 2,
+        filter_sizes: list | None = None,
+        activation_hidden: str = "sigmoid",
+        padding: str = "auto",
+        use_bias: bool = True,
+        num_epochs: int = 100,
+        batch_size: int = 16,
+        optimizer: str | None | Callable = "Adam",
+        optimizer_kwargs: dict | None = None,
+        lr: float = 0.01,
+        criterion: str | None | Callable = "CrossEntropyLoss",
+        criterion_kwargs: dict | None = None,
+        callbacks: None | str | tuple[str, ...] = "ReduceLROnPlateau",
+        callback_kwargs: dict | None = None,
+        verbose: bool = False,
+        random_state: int = 0,
+    ):
+        self.kernel_size = kernel_size
+        self.avg_pool_size = avg_pool_size
+        self.n_conv_layers = n_conv_layers
+        self.filter_sizes = filter_sizes
+        self.activation_hidden = activation_hidden
+        self.padding = padding
+        self.use_bias = use_bias
+        self.num_epochs = num_epochs
+        self.batch_size = batch_size
+        self.optimizer = optimizer
+        self.optimizer_kwargs = optimizer_kwargs
+        self.lr = lr
+        self.criterion = criterion
+        self.criterion_kwargs = criterion_kwargs
+        self.callbacks = callbacks
+        self.callback_kwargs = callback_kwargs
+        self.verbose = verbose
+        self.random_state = random_state
+        self.input_size = None
+        self.num_classes = None
+        self.series_length = None
+
+        super().__init__(
+            num_epochs=self.num_epochs,
+            batch_size=self.batch_size,
+            activation=None,
+            criterion=self.criterion,
+            criterion_kwargs=self.criterion_kwargs,
+            optimizer=self.optimizer,
+            optimizer_kwargs=self.optimizer_kwargs,
+            callbacks=self.callbacks,
+            callback_kwargs=self.callback_kwargs,
+            lr=self.lr,
+            verbose=self.verbose,
+            random_state=self.random_state,
+        )
+
+    def _build_network(self, X, y):
+        """Build the CNN network.
+
+        Parameters
+        ----------
+        X : numpy.ndarray
+            Input data, shape (n_instances, n_dims, series_length).
+        y : numpy.ndarray
+            Target labels.
+
+        Returns
+        -------
+        model : CNNNetworkTorch
+            The constructed CNN network.
+        """
+        if len(X.shape) != 3:
+            raise ValueError(
+                f"Expected 3D input X with shape (n_instances, n_dims, series_length), "
+                f"but got shape {X.shape}."
+            )
+        _, self.input_size, self.series_length = X.shape
+        self.num_classes = len(np.unique(y))
+        return CNNNetworkTorch(
+            input_size=self.input_size,
+            num_classes=self.num_classes,
+            kernel_size=self.kernel_size,
+            avg_pool_size=self.avg_pool_size,
+            n_conv_layers=self.n_conv_layers,
+            filter_sizes=self.filter_sizes,
+            activation=None,
+            activation_hidden=self.activation_hidden,
+            padding=self.padding,
+            series_length=self.series_length,
+            bias=self.use_bias,
+            random_state=self.random_state,
+        )
+
+    @classmethod
+    def get_test_params(cls, parameter_set="default"):
+        """Return testing parameter settings for the estimator.
+
+        Parameters
+        ----------
+        parameter_set : str, default="default"
+            Name of the set of test parameters to return, for use in tests.
+
+        Returns
+        -------
+        params : list of dict
+            Parameters to create testing instances of the class.
+        """
+        params1 = {}
+        params2 = {
+            "num_epochs": 10,
+            "batch_size": 4,
+            "avg_pool_size": 4,
+        }
+        params3 = {
+            "num_epochs": 12,
+            "batch_size": 6,
+            "kernel_size": 2,
+            "n_conv_layers": 1,
+            "padding": "valid",
+        }
+        params4 = {
+            "kernel_size": 5,
+            "padding": "same",
+            "num_epochs": 8,
+        }
+        return [params1, params2, params3, params4]
---
+++ sktime/networks/cnn/_cnn_tf.py
@@ -0,0 +1,197 @@
+"""Time Convolutional Neural Network (CNN) (minus the final output layer)."""
+
+from sktime.networks.base import BaseDeepNetwork
+from sktime.utils.dependencies import _check_dl_dependencies
+from sktime.utils.warnings import warn
+
+
+# TODO (release 0.41.0)
+# change the default value of 'activation' to "relu"
+# update the docstring for activation from "sigmoid" to "relu"
+# and remove the note about the change from the docstring.
+# Remove the usage of self._activation throughout the class
+# and replace it with self.activation
+class CNNNetwork(BaseDeepNetwork):
+    """Establish the network structure for a CNN.
+
+    Zhao et al. 2017 uses sigmoid activation in the hidden layers.
+    To obtain same behaviour as Zhao et al. 2017, set activation to "sigmoid".
+
+    Adapted from the implementation used in [1]_.
+
+    Parameters
+    ----------
+    kernel_size : int, default = 7
+        specifying the length of the 1D convolution window
+    avg_pool_size : int, default = 3
+        size of the average pooling windows
+    n_conv_layers : int, default = 2
+        the number of convolutional plus average pooling layers
+    filter_sizes : array of int, shape = (n_conv_layers)
+    activation : string, default = "sigmoid"
+        activation function used for hidden layers;
+        List of available keras activation functions:
+        https://keras.io/api/layers/activations/
+        Default value of activation will change to "relu"
+        in version '0.41.0'.
+    padding : string, default = "auto"
+        Controls padding logic for the convolutional layers,
+        i.e. whether ``'valid'`` and ``'same'`` are passed to the ``Conv1D`` layer.
+        - "auto": as per original implementation, ``"same"`` is passed if
+          ``input_shape[0] < 60`` in the input layer, and ``"valid"`` otherwise.
+        - "valid", "same", and other values are passed directly to ``Conv1D``
+    random_state    : int, default = 0
+        seed to any needed random actions
+
+    References
+    ----------
+    .. [1] Zhao et al. Convolutional neural networks for time series classification,
+    Journal of Systems Engineering and Electronics 28(1), 162--169, 2017
+    """
+
+    _tags = {
+        # packaging info
+        # --------------
+        "authors": ["hfawaz", "James-Large", "Withington", "TonyBagnall", "noxthot"],
+        "python_dependencies": "tensorflow",
+    }
+
+    # TODO (release 0.41.0)
+    # Change the default value of 'activation' to "relu"
+    def __init__(
+        self,
+        kernel_size=7,
+        avg_pool_size=3,
+        n_conv_layers=2,
+        filter_sizes=None,
+        activation="changing_from_sigmoid_to_relu_in_0.41.0",
+        padding="auto",
+        random_state=0,
+    ):
+        _check_dl_dependencies(severity="error")
+        self.random_state = random_state
+        self.padding = padding
+        self.kernel_size = kernel_size
+        self.avg_pool_size = avg_pool_size
+        self.n_conv_layers = n_conv_layers
+        self.filter_sizes = filter_sizes
+        if filter_sizes is None:
+            self._filter_sizes = [6, 12]
+        else:
+            self._filter_sizes = filter_sizes
+        self.activation = activation
+        # TODO (release 0.41.0)
+        # After changing the default value of 'activation' to "relu"
+        # in the __init__ method signature,
+        # Remove the following 'if-else' check
+        # Remove the usage of self._activation throughout the class
+        # and replace it with self.activation
+        if activation == "changing_from_sigmoid_to_relu_in_0.41.0":
+            warn(
+                "in `CNNNetwork`, the default value of parameter 'activation'"
+                " will change to 'relu' in version '0.41.0'. "
+                "To keep current behaviour and to silence this warning, "
+                "set 'activation' to 'sigmoid' explicitly.",
+                category=DeprecationWarning,
+                obj=self,
+            )
+            self._activation = "sigmoid"
+        else:
+            self._activation = activation
+
+        super().__init__()
+
+    def build_network(self, input_shape, **kwargs):
+        """Construct a network and return its input and output layers.
+
+        Arguments
+        ---------
+        input_shape : tuple
+            The shape of the data fed into the input layer
+
+        Returns
+        -------
+        input_layer : a keras layer
+        output_layer : a keras layer
+        """
+        from tensorflow import keras
+
+        fs = self._filter_sizes
+        nconv = self.n_conv_layers
+        padding = self.padding
+
+        # this is the condition from the original implementation
+        if padding == "auto":
+            if input_shape[0] < 60:
+                padding = "same"
+            else:
+                padding = "valid"
+
+        # Extends filter_sizes to match n_conv_layers length
+        filter_sizes = fs[:nconv] + [fs[-1]] * max(0, nconv - len(fs))
+
+        input_layer = keras.layers.Input(input_shape)
+
+        # TODO (release 0.41.0)
+        # After changing the default value of 'activation' to "relu"
+        # in the __init__ method signature,
+        # remove the usage of self._activation in the following lines
+        # and replace it with self.activation
+        conv = keras.layers.Conv1D(
+            filters=filter_sizes[0],
+            kernel_size=self.kernel_size,
+            padding=padding,
+            activation=self._activation,
+        )(input_layer)
+        conv = keras.layers.AveragePooling1D(pool_size=self.avg_pool_size)(conv)
+
+        for i in range(1, self.n_conv_layers):
+            # TODO (release 0.41.0)
+            # After changing the default value of 'activation' to "relu"
+            # in the __init__ method signature,
+            # remove the usage of self._activation in the following lines
+            # and replace it with self.activation
+            conv = keras.layers.Conv1D(
+                filters=filter_sizes[i],
+                kernel_size=self.kernel_size,
+                padding=padding,
+                activation=self._activation,
+            )(conv)
+            conv = keras.layers.AveragePooling1D(pool_size=self.avg_pool_size)(conv)
+
+        flatten_layer = keras.layers.Flatten()(conv)
+
+        return input_layer, flatten_layer
+
+    @classmethod
+    def get_test_params(cls, parameter_set="default"):
+        """Return testing parameter settings for the estimator.
+
+        Parameters
+        ----------
+        parameter_set : str, default="default"
+            Name of the set of test parameters to return, for use in tests. If no
+            special parameters are defined for a value, will return ``"default"`` set.
+            Reserved values for classifiers:
+                "results_comparison" - used for identity testing in some classifiers
+                    should contain parameter settings comparable to "TSC bakeoff"
+
+        Returns
+        -------
+        params : dict or list of dict, default = {}
+            Parameters to create testing instances of the class
+            Each dict are parameters to construct an "interesting" test instance, i.e.,
+            ``MyClass(**params)`` or ``MyClass(**params[i])`` creates a valid test
+            instance.
+            ``create_test_instance`` uses the first (or only) dictionary in ``params``
+        """
+        params1 = {}
+        params2 = {
+            "filter_sizes": [10],
+            "kernel_size": 3,
+            "avg_pool_size": 1,
+            "padding": "valid",
+            "n_conv_layers": 1,
+        }
+        params3 = {"kernel_size": 5, "padding": "same"}
+        return [params1, params2, params3]
