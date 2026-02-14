b10fb780eca56b21efe8bb61f876a9681980e38a
adan-shahid <adanshahid.student@gmail.com>
2026-02-14 08:32:16 +0500

    Revert "updated sktime/classification/deep_learning/base/_base_torch.py"
    This reverts commit b71c354646d6ba2ec48901629a5c93feb161d5eb.

--- sktime/classification/deep_learning/base/_base_torch.py
+++ sktime/classification/deep_learning/base/_base_torch.py
@@ -432,7 +432,7 @@ class BaseDeepClassifierPytorch(BaseClassifier):
                 optimizer_class = _safe_import(
                     f"torch.optim.{self._all_optimizers[self.optimizer.lower()]}"
                 )
-                if self.optimizer_kwargs:
+                if self.callback_kwargs:
                     return optimizer_class(
                         self.network.parameters(), lr=self.lr, **self.optimizer_kwargs
                     )
