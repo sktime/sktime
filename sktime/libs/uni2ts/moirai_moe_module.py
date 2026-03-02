e585d0c6b66c9f71029f47ab16c718e6ac237d6d
Priyanshu Harshbodhi <priyanshuqpwp@gmail.com>
2026-03-03 02:19:54 +0530

    fix wrong dependency name

--- sktime/libs/uni2ts/moirai_moe_module.py
+++ sktime/libs/uni2ts/moirai_moe_module.py
@@ -41,7 +41,7 @@ else:
         pass
 
 
-if _check_soft_dependencies("huggingface-hub", severity="none"):
+if _check_soft_dependencies("huggingface_hub", severity="none"):
     from huggingface_hub import PyTorchModelHubMixin
 else:
 
