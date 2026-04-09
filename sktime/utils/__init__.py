2477292aaf2e062be458bfe439383f4ac54b8b65
nensii21 <nancypatel016@gmail.com>
2026-04-09 20:08:01 +0530

    Add detection plotting wrappers to sktime.utils

--- sktime/utils/__init__.py
+++ sktime/utils/__init__.py
@@ -3,4 +3,15 @@
 from sktime.utils.estimator_checks import check_estimator
 from sktime.utils.plotting import plot_series, plot_windows
 
-__all__ = ["check_estimator", "plot_series", "plot_windows"]
+from .plotting_detection import (
+    plot_time_series_with_change_points,
+    plot_time_series_with_profiles,
+)
+
+__all__ = [
+    "check_estimator",
+    "plot_series",
+    "plot_windows",
+    "plot_time_series_with_change_points",
+    "plot_time_series_with_profiles",
+]
