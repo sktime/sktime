8a7383f54c283ad5ba61e6870bbabee7429e1fb3
nensii21 <nancypatel016@gmail.com>
2026-04-18 10:00:13 +0530

    Fix docs build by handling optional imports in plotting_detection

--- plotting_detection.py
+++ plotting_detection.py
@@ -1,12 +1,24 @@
 def plot_time_series_with_change_points(*args, **kwargs):
-    from sktime.detection.plotting.utils import (
-        plot_time_series_with_change_points,
-    )
+    try:
+        from sktime.detection.plotting.utils import (
+            plot_time_series_with_change_points,
+        )
+    except ImportError as e:
+        raise ImportError(
+            "plot_time_series_with_change_points requires optional detection dependencies."
+        ) from e
+
     return plot_time_series_with_change_points(*args, **kwargs)
 
 
 def plot_time_series_with_profiles(*args, **kwargs):
-    from sktime.detection.plotting.utils import (
-        plot_time_series_with_profiles,
-    )
+    try:
+        from sktime.detection.plotting.utils import (
+            plot_time_series_with_profiles,
+        )
+    except ImportError as e:
+        raise ImportError(
+            "plot_time_series_with_profiles requires optional detection dependencies."
+        ) from e
+
     return plot_time_series_with_profiles(*args, **kwargs)
