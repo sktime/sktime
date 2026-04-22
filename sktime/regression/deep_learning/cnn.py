eeed40467859bb9ebb17bb4b638c1c37d531a04d
Franz Király <fkiraly@gcos.ai>
2026-04-22 22:09:14 +0200

    cnn

--- sktime/regression/deep_learning/cnn.py
+++ sktime/regression/deep_learning/cnn.py
@@ -8,7 +8,6 @@ from sklearn.utils import check_random_state
 
 from sktime.networks.cnn import CNNNetwork
 from sktime.regression.deep_learning.base import BaseDeepRegressor
-from sktime.utils.dependencies import _check_dl_dependencies
 from sktime.utils.warnings import warn
 
 
