971538a855fd68c2ee68729cb8ee4583149b1275
hydrauluu <nurtaymusaev@gmail.com>
2026-04-29 00:27:33 +0600

    fix(TimesFMForecaster): remove outdated python_version upper bound

--- sktime/forecasting/timesfm_forecaster.py
+++ sktime/forecasting/timesfm_forecaster.py
@@ -151,7 +151,7 @@ class TimesFMForecaster(_BaseGlobalForecaster):
         "authors": ["rajatsen91", "geetu040"],
         # rajatsen91 for google-research/timesfm
         "maintainers": ["geetu040"],
-        "python_version": ">=3.10,<3.11",
+        "python_version": ">=3.10,<3.13",
         "python_dependencies": [
             "tensorflow",
             "einshape",
