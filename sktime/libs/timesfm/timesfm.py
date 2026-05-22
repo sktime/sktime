fe0e72dfbde056ff5a32efcf1e1b101bac442168
geetu040 <raoarmaghanshakir040@gmail.com>
2026-05-22 19:26:30 +0200

    fix einshape import

--- sktime/libs/timesfm/timesfm.py
+++ sktime/libs/timesfm/timesfm.py
@@ -22,7 +22,7 @@ from os import path
 
 from sktime.utils.dependencies import _safe_import
 
-es = _safe_import("sktime.utils.einshape")
+es = _safe_import("einshape")
 jax = _safe_import("jax")
 jnp = _safe_import("jax.numpy")
 
