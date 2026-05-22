eadb16d32098844d6704a9cfccb3a697e455f46e
Tobias Pitters <tobias.pitters@gmail.com>
2026-05-22 10:27:35 +0200

    remove bugs

--- bugs/imports/doctest_mstl.py
+++
@@ -1,7 +0,0 @@
-from sktime.datasets import load_airline
-from sktime.transformations.mstl import MSTL
-
-# bugs/imports/doctest_mstl.py
-X = load_airline()
-X.index = X.index.to_timestamp()
-mstl = MSTL(return_components=True)
--- bugs/imports/import_mstl.py
+++
@@ -1,2 +0,0 @@
-# import sktime.transformations.series.detrend.mstl
-# from sktime.transformations.series.detrend import mstl
