c862cb36d27793900c7c3b17896065da72e611a1
vedantag17 <vrsagrawal@gmail.com>
2026-05-27 12:45:19 +0530

    Added mantis classifier

--- sktime/classification/foundation_models/__init__.py
+++ sktime/classification/foundation_models/__init__.py
@@ -1,7 +1,9 @@
-"""Classification foudnation models."""
+"""Classification foundation models."""
 
 __all__ = [
+    "MantisClassifier",
     "MomentFMClassifier",
 ]
 
+from sktime.classification.foundation_models.mantis import MantisClassifier
 from sktime.classification.foundation_models.momentfm import MomentFMClassifier
