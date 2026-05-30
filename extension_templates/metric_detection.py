5bed62b73ca1b2fdd1fa8b0d1d2cc6da46b5d86d
Franz Király <fkiraly@gcos.ai>
2026-05-30 15:29:31 +0200

    extension templates

--- extension_templates/metric_detection.py
+++ extension_templates/metric_detection.py
@@ -103,7 +103,24 @@ class MyMetric(BaseDetectionMetric):
         super().__init__()
 
         # do not put anything else in __init__,
+        # use __dynamic_tags__ for dynamic tag setting
         # use __post_init__ for any further initialization logic
+
+    # todo: add if there is dynamic tag setting logic, otherwise delete this method
+    def __dynamic_tags__(self):
+        """Dynamic tag setter logic for setting tag values conditional on parameters.
+
+        This method should be used for setting dynamic tags only.
+        """
+        # todo: if tags of estimator depend on component tags, set these here
+        #  typically only needed if estimator is a composite
+        #  tags set here apply to the instance, and override the class tags
+        #
+        # example 1: conditional setting of a tag based on parameter foo
+        # if self.foo == 42:
+        #   self.set_tags(**{"capability:missing_values": True})
+        # example 2: cloning tags from component estimator component_estimator
+        #   self.clone_tags(self.component_estimator, ["capability:missing_values"])
 
     def __post_init__(self):
         """Post-init constructor logic, can be used by inheriting classes.
