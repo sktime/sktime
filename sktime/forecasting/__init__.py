b156f4d106252559f9fc2014462c333c6b994990
Franz Király <fkiraly@gcos.ai>
2026-06-07 11:21:40 +0200

    renames

--- sktime/forecasting/__init__.py
+++ sktime/forecasting/__init__.py
@@ -1,1 +1,33 @@
-"""Implements univariate and multivariate forecasting models."""
+"""Forecasting models."""
+
+
+# mypackage/__init__.py
+
+from importlib import import_module
+import warnings
+
+# alias dictionary to map old module names to new module names
+# if an old module name is queried, imports work, but
+# a deprecation warning is issued to update the import statement
+# old_name -> new_name
+_MODULE_ALIASES = {
+    "boxcox_bias_adjusted_forecaster": "boxcox_biasadj",
+    "conditional_invertible_neural_network": "cinn",
+    "hf_momentfm_forecaster": "momentfm",
+}
+
+
+def __getattr__(name):
+    if name in _MODULE_ALIASES:
+        new_name = _MODULE_ALIASES[name]
+
+        warnings.warn(
+            f"mypackage.{name} is deprecated and has been renamed to "
+            f"mypackage.{new_name}; please update your imports.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+
+        return import_module(f".{_MODULE_ALIASES[name]}", __name__)
+
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
