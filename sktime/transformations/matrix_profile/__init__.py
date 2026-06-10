92d47a90b99a8ad34c9b42be0585242517306652
Franz Király <fkiraly@gcos.ai>
2026-06-10 23:22:12 +0200

    Update __init__.py

--- sktime/transformations/matrix_profile/__init__.py
+++ sktime/transformations/matrix_profile/__init__.py
@@ -7,3 +7,5 @@ __all__ = [
 
 from sktime.transformations.matrix_profile._mp_features import MatrixProfileFeatures
 from sktime.transformations.matrix_profile._stumpy import MatrixProfileTransformer
+
+MatrixProfile = MatrixProfileFeatures  # for silent downwards compatibility
