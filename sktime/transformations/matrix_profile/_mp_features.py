54c965d5c43a27aa44e611e4352610a0aab1b215
Franz Király <fkiraly@gcos.ai>
2026-06-11 00:54:40 +0200

    matrixprofile

--- sktime/transformations/matrix_profile/_mp_features.py
+++ sktime/transformations/matrix_profile/_mp_features.py
@@ -246,3 +246,27 @@ class MatrixProfileFeatures(BaseTransformer):
         n_instances = X.shape[0]
         Xt = pd.DataFrame([_stomp_self(X[i], self.m) for i in range(n_instances)])
         return Xt
+
+    @classmethod
+    def get_test_params(cls, parameter_set="default"):
+        """Return testing parameter settings for the estimator.
+
+        Parameters
+        ----------
+        parameter_set : str, default="default"
+            Name of the set of test parameters to return, for use in tests. If no
+            special parameters are defined for a value, will return ``"default"`` set.
+            There are currently no reserved values for transformers.
+
+        Returns
+        -------
+        params : dict or list of dict, default = {}
+            Parameters to create testing instances of the class
+            Each dict are parameters to construct an "interesting" test instance, i.e.,
+            ``MyClass(**params)`` or ``MyClass(**params[i])`` creates a valid test
+            instance.
+            ``create_test_instance`` uses the first (or only) dictionary in ``params``
+        """
+        params0 = {}
+        params1 = {"m": 5}
+        return [params0, params1]
