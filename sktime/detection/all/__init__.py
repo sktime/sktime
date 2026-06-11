f853a61d6070171ce777bb8d35fae38824e681bc
Franz Király <fkiraly@gcos.ai>
2026-06-11 05:28:12 +0200

    clean

--- sktime/detection/all/__init__.py
+++ sktime/detection/all/__init__.py
@@ -1,8 +1,8 @@
-"""All time series classifiers."""
+"""All time series detectors."""
 
 from sktime.registry import all_estimators
 
-est_tuples = all_estimators(estimator_types="classifier", return_names=True)
+est_tuples = all_estimators(estimator_types="detector", return_names=True)
 est_names, ests = zip(*est_tuples)
 
 for i, x in enumerate(est_tuples):
