19243ac46816078df75a06129141d44739f5eaec
Franz Király <fkiraly@gcos.ai>
2026-06-11 05:57:20 +0200

    [ENH] clean up exports in `all` modules (#10380)
    cleans up exports in `all` modules, so that only estimators are exported
    from the same module.

--- sktime/performance_metrics/all/__init__.py
+++ sktime/performance_metrics/all/__init__.py
@@ -1,8 +1,9 @@
-"""All transformers in sktime."""
+#!/usr/bin/env python3 -u
+"""Import all performance metrics available in sktime."""
 
 from sktime.registry import all_estimators
 
-est_tuples = all_estimators(estimator_types="transformer", return_names=True)
+est_tuples = all_estimators(estimator_types="metric", return_names=True)
 est_names, ests = zip(*est_tuples)
 
 for i, x in enumerate(est_tuples):
