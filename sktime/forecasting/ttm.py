9e97588f33c2c0760bb4fd41641ef7f23f172ae2
geetu040 <raoarmaghanshakir040@gmail.com>
2026-06-15 09:34:10 +0200

    doc: remove multi-index example

--- sktime/forecasting/ttm.py
+++ sktime/forecasting/ttm.py
@@ -192,37 +192,6 @@ class TinyTimeMixerForecaster(_GlobalForecastingDeprecationMixin, BaseForecaster
     TinyTimeMixerForecaster(...)
     >>> y_pred = forecaster.predict()
 
-    >>> from sktime.forecasting.ttm import TinyTimeMixerForecaster
-    >>> from sktime.datasets import load_tecator
-    >>>
-    >>> # load multi-index dataset
-    >>> y = load_tecator(
-    ...     return_type="pd-multiindex",
-    ...     return_X_y=False
-    ... )
-    >>> y.drop(['class_val'], axis=1, inplace=True)
-    >>>
-    >>> # global forecasting on multi-index dataset
-    >>> forecaster = TinyTimeMixerForecaster(
-    ...     model_path=None,
-    ...     fit_strategy="full",
-    ...     config={
-    ...             "context_length": 8,
-    ...             "prediction_length": 2
-    ...     },
-    ...     training_args={
-    ...         "num_train_epochs": 1,
-    ...         "output_dir": "test_output",
-    ...         "per_device_train_batch_size": 32,
-    ...     },
-    ... )
-    >>>
-    >>> # model initialized with random weights due to None model_path
-    >>> # and trained with the full strategy.
-    >>> forecaster.fit(y, fh=[1, 2, 3])
-    TinyTimeMixerForecaster(...)
-    >>> y_pred = forecaster.predict()
-
     Example with exogenous variables:
 
     >>> from sktime.forecasting.ttm import TinyTimeMixerForecaster
