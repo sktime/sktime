f7934e540a712d0d2187a38b32dd1144b54843be
geetu040 <raoarmaghanshakir040@gmail.com>
2026-07-14 12:12:51 +0200

    migrate falcon tst to foundation base

--- sktime/forecasting/falcon_tst.py
+++ sktime/forecasting/falcon_tst.py
@@ -22,11 +22,11 @@ from warnings import warn
 import numpy as np
 import pandas as pd
 
-from sktime.forecasting.base import BaseForecaster
+from sktime.forecasting.foundation._base2 import BaseFoundationForecaster
 from sktime.utils.singleton import _multiton
 
 
-class FalconTSTForecaster(BaseForecaster):
+class FalconTSTForecaster(BaseFoundationForecaster):
     """Falcon-TST forecaster via Hugging Face ``transformers``.
 
     This forecaster wraps Falcon-TST prediction models [1]_, [2]_ from Hugging
@@ -156,13 +156,14 @@ class FalconTSTForecaster(BaseForecaster):
         quantization_config=None,
         revin=True,
     ):
-        self.model_path = model_path
-        self.config = config
         self.device_map = device_map
-        self.quantization_config = quantization_config
         self.revin = revin
-
-        super().__init__()
+        super().__init__(
+            model_path=model_path,
+            config=config,
+            device=device_map,
+            quantization_config=quantization_config,
+        )
 
     def _fit(self, y, X=None, fh=None):
         """Load the model and store history for zero-shot forecasting.
