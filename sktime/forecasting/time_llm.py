36d85f7ebed9d687f0f45e020dc2ad7f7a45031b
geetu040 <raoarmaghanshakir040@gmail.com>
2026-07-17 11:27:40 +0200

    Revert "migrate time llm to foundation base"
    This reverts commit 75b0f2123dc053fa324f393c4eefd88f3f4ca1b4.

--- sktime/forecasting/time_llm.py
+++ sktime/forecasting/time_llm.py
@@ -8,14 +8,14 @@ from types import SimpleNamespace
 
 import pandas as pd
 
-from sktime.forecasting.foundation._base2 import BaseFoundationForecaster
+from sktime.forecasting.base import BaseForecaster
 from sktime.utils.dependencies import _safe_import
 from sktime.utils.singleton import _multiton
 
 torch = _safe_import("torch")
 
 
-class TimeLLMForecaster(BaseFoundationForecaster):
+class TimeLLMForecaster(BaseForecaster):
     """
     Interface to the Time-LLM.
 
@@ -119,8 +119,10 @@ class TimeLLMForecaster(BaseFoundationForecaster):
         self.d_ff = d_ff
         self.n_heads = n_heads
         self.dropout = dropout
+        self.device = device
         self.prompt_domain = prompt_domain
-        super().__init__(device=device)
+
+        super().__init__()
 
     def _fit(self, y, X=None, fh=None):
         """Fit forecaster to training data.
