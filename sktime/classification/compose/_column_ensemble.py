6c8da83125f2c7402bc550a2172d8ed91b25f5e2
Franz Király <fkiraly@gcos.ai>
2026-07-26 16:17:34 +0200

    Update _column_ensemble.py

--- sktime/classification/compose/_column_ensemble.py
+++ sktime/classification/compose/_column_ensemble.py
@@ -291,6 +291,7 @@ class ColumnEnsembleClassifier(BaseColumnEnsembleClassifier):
             ``create_test_instance`` uses the first (or only) dictionary in ``params``.
         """
         from sktime.classification.dictionary_based import ContractableBOSS
+        from sktime.classification.dummy import DummyClassifier
         from sktime.classification.interval_based import CanonicalIntervalForest
         from sktime.classification.interval_based import (
             TimeSeriesForestClassifier as TSFC,
@@ -307,8 +308,8 @@ class ColumnEnsembleClassifier(BaseColumnEnsembleClassifier):
         else:
             param0 = {
                 "estimators": [
-                    ("tsf1", TSFC(n_estimators=2, min_interval=2), 0.3),
-                    ("tsf2", TSFC(n_estimators=4), 0.7),
+                    ("d1", DummyClassifier(strategy="most_frequent"), 0),
+                    ("d2", DummyClassifier(strategy="prior"), 0),
                 ]
             }
             param1 = {
@@ -319,7 +320,7 @@ class ColumnEnsembleClassifier(BaseColumnEnsembleClassifier):
             }
             param2 = {**param1, "remainder": TSFC(n_estimators=2)}
 
-            return [param1, param2]
+            return [param0, param1, param2]
 
 
 def _get_column(X, key):
