65c940a171c086c070056225b51c4295fd5d97a7
marrov <marc.rs@proton.me>
2026-07-30 14:35:46 +0200

    Removed stale imports of deleted classes

--- sktime/forecasting/base/__init__.py
+++ sktime/forecasting/base/__init__.py
@@ -5,14 +5,8 @@ __all__ = [
     "ForecastingHorizon",
     "BaseForecaster",
     "BaseProbaForecaster",
-    "_BaseGlobalForecaster",
-    "_GlobalForecastingDeprecationMixin",
 ]
 
 from sktime.forecasting.base._base import BaseForecaster
 from sktime.forecasting.base._base_proba import BaseProbaForecaster
-from sktime.forecasting.base._deprecation_global import (
-    _BaseGlobalForecaster,
-    _GlobalForecastingDeprecationMixin,
-)
 from sktime.forecasting.base._fh import ForecastingHorizon
