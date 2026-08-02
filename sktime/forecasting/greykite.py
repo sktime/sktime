14d054f8c6e83f398f13e75a47df8d295f395778
Faakhir30 <zahidfaakhir@gmail.com>
2026-08-02 18:55:13 +0500

    skip test_deepcopy_fitted on greykite
    Signed-off-by: Faakhir30 <zahidfaakhir@gmail.com>

--- sktime/forecasting/greykite.py
+++ sktime/forecasting/greykite.py
@@ -90,6 +90,7 @@ class GreykiteForecaster(BaseForecaster):
             "test_save_estimators_to_file",
             "test_update_predict_predicted_index",
             "test_deepcopy_fitted_predict",
+            "test_deepcopy_fitted",
         ],
         "tests:python_dependencies": ["prophet", "setuptools<82"],
     }
