f2cd03a9fb66f17463b662d10f1a6d0af7e29d68
Jigyasu <jigyasu@outlook.in>
2026-08-06 01:25:41 +0530

    Raise PTF version bound in the adapter

--- sktime/forecasting/model_evaluation/tests/test_evaluate.py
+++ sktime/forecasting/model_evaluation/tests/test_evaluate.py
@@ -214,10 +214,10 @@ def test_evaluate_common_configs(
 @pytest.mark.parametrize("backend", BACKENDS)
 def test_evaluate_global_mode(scoring, strategy, backend):
     """Check that evaluate works with hierarchical data."""
-    if backend["backend"] in ["multiprocessing", "threading", "loky"]:
-        # multiprocessing and threading backends fail wih refit
-        # for unknown reason
+    if backend["backend"] == "multiprocessing":
+        # multiprocessing will block the test due to unknown reason
         if strategy not in ["update", "no-update_params"]:
+            # if strategy in ["update","no-update_params"], it won't run parallelly
             return None
 
     hierarchy_levels = (4, 4)
