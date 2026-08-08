c1aaada0c834faec4e92a1282c5302dd11c8a974
Franz Király <fkiraly@gcos.ai>
2026-08-08 23:04:25 +0200

    skips

--- sktime/performance_metrics/forecasting/probabilistic/_classes.py
+++ sktime/performance_metrics/forecasting/probabilistic/_classes.py
@@ -863,6 +863,9 @@ class ConstraintViolation(_BaseProbaForecastingErrorMetric):
     _tags = {
         "scitype:y_pred": "pred_interval",
         "lower_is_better": True,
+        # CI and test flags
+        # -----------------
+        "tests:skip_by_name": ["test_class_has_doctest_example"],
     }
 
     def __init__(
@@ -1060,6 +1063,12 @@ class LogLoss(_BaseDistrForecastingMetric):
           the log-loss is computed per variable marginal, results in many scores per row
     """
 
+    _tags = {
+        # CI and test flags
+        # -----------------
+        "tests:skip_by_name": ["test_class_has_doctest_example"],
+    }
+
     def __init__(self, multioutput="uniform_average", multivariate=False):
         self.multivariate = multivariate
         super().__init__(multioutput=multioutput)
@@ -1120,6 +1129,12 @@ class SquaredDistrLoss(_BaseDistrForecastingMetric):
           the score is computed per variable marginal, results in many scores per row
     """
 
+    _tags = {
+        # CI and test flags
+        # -----------------
+        "tests:skip_by_name": ["test_class_has_doctest_example"],
+    }
+
     def __init__(self, multioutput="uniform_average", multivariate=False):
         self.multivariate = multivariate
         super().__init__(multioutput=multioutput)
@@ -1177,6 +1192,12 @@ class CRPS(_BaseDistrForecastingMetric):
         * if False, is univariate CRPS:
           the score is computed per variable marginal, results in many scores per row
     """  # noqa: E501
+
+    _tags = {
+        # CI and test flags
+        # -----------------
+        "tests:skip_by_name": ["test_class_has_doctest_example"],
+    }
 
     def __init__(self, multioutput="uniform_average", multivariate=False):
         self.multivariate = multivariate
