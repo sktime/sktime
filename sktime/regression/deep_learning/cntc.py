b93ed8180cb48b438dea85cadcc3ec288a18b3a9
Franz Király <fkiraly@gcos.ai>
2026-08-15 22:54:06 +0200

    regr

--- sktime/regression/deep_learning/cntc.py
+++ sktime/regression/deep_learning/cntc.py
@@ -86,11 +86,7 @@ class CNTCRegressor(BaseDeepRegressor):
         ],
         "maintainers": ["James-Large", "Withington", "AurumnPegasus", "nilesh05apr"],
         "python_dependencies": ["tensorflow"],
-        "tests:skip_by_name": [
-            "test_fit_idempotent",  # fails with `AssertionError`, see #3616
-            "test_persistence_via_pickle",  # fails with `AssertionError`, see #8059
-            "test_save_estimators_to_file",
-        ],
+        "tests:skip_by_name": ["test_class_has_doctest_example"],
         "tests:vm": True,  # isolated due to suspected memory leaks, see #8518
     }
 
