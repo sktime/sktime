b5f884c4492a5108bb79a829fd1446bbfd3b60b7
CloseChoice <tobias.pitters@gmail.com>
2026-08-16 21:46:30 +0200

    rename estimator_type to object_type

--- sktime/forecasting/tests/test_all_forecasters.py
+++ sktime/forecasting/tests/test_all_forecasters.py
@@ -275,21 +275,21 @@ class TestAllForecasters(ForecasterFixtureGenerator, QuickTester):
         with pytest.raises(TypeError, match=r"type"):
             object_instance.fit(y_train, X, fh=FH0)
 
-    def test_categorical_X_passes(self, estimator_instance):
+    def test_categorical_X_passes(self, object_instance):
         """Test that categorical X in supported forecasters pass.
 
         Only test with forecasters which do not ignore exogenous X and those that do
         support categorical natively.
         """
-        if estimator_instance.get_tag(
+        if object_instance.get_tag(
             "capability:exogenous"
-        ) and estimator_instance.get_tag("capability:categorical_in_X"):
+        ) and object_instance.get_tag("capability:categorical_in_X"):
             y_train = _make_series(n_timepoints=6, n_columns=2)
             X_train = pd.DataFrame(
                 {"col_0": ["a", "b", "c", "a", "b", "c"]}, index=y_train.index
             )
 
-            estimator_instance.fit(y_train, X_train, fh=FH0)
+            object_instance.fit(y_train, X_train, fh=FH0)
 
     def test_categorical_X_raises_error(self, object_instance):
         """Test that categorical X in not supported forecasters raises error.
