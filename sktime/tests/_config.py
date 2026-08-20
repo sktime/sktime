50a096b891576bbedcbbea1033ee3b81842b6959
Harshavardhan-28 <harshavardhan.khamkar@gmail.com>
2026-08-20 20:19:14 +0530

    Add second test parameter set for TimeSeriesKMedoids

--- sktime/tests/_config.py
+++ sktime/tests/_config.py
@@ -180,7 +180,6 @@ EXCLUDED_TESTS_BY_TEST = {
         "TSFreshClassifier",
         "TapNetNetwork",
         "TemporalDictionaryEnsemble",
-        "TimeSeriesKMedoids",
         "WEASEL",
         # The below estimators need to have their name removed from EXCLUDE_SOFT_DEPS
         # too after adding test parameters to them
