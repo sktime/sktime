c11548c08b3a1c69a71d0acc4a03ef234201f4f1
James Boardman <jamesboardman@live.co.uk>
2026-08-26 14:15:47 +0100

    Move WEASEL test skip config from tests._config to estimator tags with get_test_params_coverage fix

--- sktime/tests/_config.py
+++ sktime/tests/_config.py
@@ -98,7 +98,6 @@ EXCLUDED_TESTS = {
     "Pipeline": ["test_inheritance"],  # does not inherit from intermediate base classes
     # networks do not support negative fh
     "HFTransformersForecaster": ["test_predict_time_index_in_sample_full"],
-    "WEASEL": ["test_multiprocessing_idempotent"],  # see 5658
     # StatsForecastMSTL is failing in probabistic forecasts, see #5703, #5920
     "StatsForecastMSTL": ["test_pred_int_tag"],
     # KNeighborsTimeSeriesClassifierTslearn crashes in parallel mode
@@ -169,7 +168,6 @@ EXCLUDED_TESTS_BY_TEST = {
         "TSFreshClassifier",
         "TapNetNetwork",
         "TemporalDictionaryEnsemble",
-        "WEASEL",
         # The below estimators need to have their name removed from EXCLUDE_SOFT_DEPS
         # too after adding test parameters to them
         "BaggingForecaster",
