aab16f73e7e259daeff7c94f3fabedb39362f63c
Franz Király <fkiraly@gcos.ai>
2026-08-26 18:34:51 +0200

    Update _reduce.py

--- sktime/forecasting/compose/_reduce.py
+++ sktime/forecasting/compose/_reduce.py
@@ -334,6 +334,7 @@ class _Reducer(_BaseWindowForecaster):
         from sklearn.pipeline import make_pipeline
         from sklearn.tree import DecisionTreeRegressor
 
+        from sktime.regression.compose import RegressorPipeline
         from sktime.transformations.reduce import Tabularizer
 
         # naming convention is as follows:
@@ -343,7 +344,7 @@ class _Reducer(_BaseWindowForecaster):
         # which of these is the case, we check by checking substring in the class name
         est = LinearRegression()
         if "TimeSeries" in cls.__name__:
-            est = make_pipeline(Tabularizer(), est)
+            est = RegressorPipeline(est, [Tabularizer()])
 
         params = [{"estimator": est, "window_length": 3}]
 
