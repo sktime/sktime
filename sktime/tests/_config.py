3cbf08349fc4f92cba4e198f13bcaf47cf91eee3
Muhammad <mabdurrehman2006@gmail.com>
2026-09-02 02:55:08 +0100

    [ENH] move test skip config for SameLocSplitter to tags

--- sktime/tests/_config.py
+++ sktime/tests/_config.py
@@ -48,7 +48,6 @@ EXCLUDE_ESTIMATORS = [
     "LSTMFCNRegressor",
     # splitters excluded with undiagnosed failures, see #6194
     # these are temporarily skipped to allow merging of the base test framework
-    "SameLocSplitter",
     "Repeat",
     "CutoffFhSplitter",
     # sporadic timeouts, see #6344
