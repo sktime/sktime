9058f100b08ea1c4934b39b79910d95782d889ad
Franz Király <fkiraly@gcos.ai>
2026-09-04 17:26:08 +0200

    remove

--- sktime/libs/uni2ts/moirai2_forecast.py
+++ sktime/libs/uni2ts/moirai2_forecast.py
@@ -13,8 +13,6 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from __future__ import annotations
-
 import math
 import sys
 from contextlib import contextmanager
