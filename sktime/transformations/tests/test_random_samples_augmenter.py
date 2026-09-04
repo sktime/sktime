c12b2725a1bcc2b80d5700bb07fb2c1f9ed8e884
PiyushKumar74110 <piyushkumar74110@gmail.com>
2026-09-04 23:39:48 +0530

    Fix requested changes

--- sktime/transformations/tests/test_random_samples_augmenter.py
+++ sktime/transformations/tests/test_random_samples_augmenter.py
@@ -1,14 +1,19 @@
 import pytest
 
+from sktime.tests.test_switch import run_test_for_class
 from sktime.transformations.augmenter import RandomSamplesAugmenter
 
 
+@pytest.mark.skipif(
+    not run_test_for_class(RandomSamplesAugmenter),
+    reason="run test only if softdeps are present and incrementally (if requested)",
+)
 @pytest.mark.parametrize(
     "invalid_n, expected_type_regex",
     [
-        ("spam", r"<class 'str'>"),
-        (None, r"<class 'NoneType'>"),
-        ([], r"<class 'list'>"),
+        ("spam", r"\<class 'str'\>"),
+        (None, r"\<class 'NoneType'\>"),
+        ([], r"\<class 'list'\>"),
     ],
 )
 def test_random_samples_augmenter_invalid_n_raises_value_error(
