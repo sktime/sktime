5d9b8f8ef0c35a88734360634d2c49ee07abc91a
Franz Király <fkiraly@gcos.ai>
2026-09-14 23:24:43 +0200

    wrong test

--- sktime/transformations/tests/test_time_since.py
+++ sktime/transformations/tests/test_time_since.py
@@ -132,12 +132,7 @@ def test_fit_transform_datetime_monthly_idx_numeric_output(df_datetime_monthly_i
 
     Xt = transformer.fit_transform(df_datetime_monthly_idx)
     expected = pd.DataFrame(
-        data={
-            "time_since_2000-01-01 00:00:00": (
-                df_datetime_monthly_idx.index - df_datetime_monthly_idx.index[0]
-            )
-        },
-        index=df_datetime_monthly_idx.index,
+        data={"time_since_2000-01-01 00:00:00": [0, 1, 2, 3, 4]},
     )
     assert_frame_equal(Xt, expected)
 
@@ -155,9 +150,9 @@ def test_fit_transform_datetime_monthly_idx_datetime_output(df_datetime_monthly_
     Xt = transformer.fit_transform(df_datetime_monthly_idx)
     expected = pd.DataFrame(
         data={
-            "time_since_2000-01-01 00:00:00": [
-                pd.Timedelta(i, unit="D") for i in (0, 31, 60, 91, 121)
-            ]
+            "time_since_2000-01-01 00:00:00": (
+                df_datetime_monthly_idx.index - df_datetime_monthly_idx.index[0]
+            )
         },
         index=df_datetime_monthly_idx.index,
     )
