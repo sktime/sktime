45acda9d84855f1abbab569a65ba05cb7f1f281a
Franz Király <fkiraly@gcos.ai>
2026-09-15 00:14:24 +0200

    [ENH] make `TimeSince` tests `pandas 3` compatible (#11134)
    The `TimeSince` transformer will return a different datetime frequency
    when not explicitly passed, in `pandas 3`.
    
    The transformer is meant to be consistent with `pandas 3` behaviour, so
    there is no fix needed for the transformer; instead, the expected output
    of the transformation is calculated in the test directly, independent of
    `pandas` version.

--- sktime/transformations/tests/test_time_since.py
+++ sktime/transformations/tests/test_time_since.py
@@ -151,9 +151,9 @@ def test_fit_transform_datetime_monthly_idx_datetime_output(df_datetime_monthly_
     Xt = transformer.fit_transform(df_datetime_monthly_idx)
     expected = pd.DataFrame(
         data={
-            "time_since_2000-01-01 00:00:00": [
-                pd.Timedelta(i, unit="D") for i in (0, 31, 60, 91, 121)
-            ]
+            "time_since_2000-01-01 00:00:00": (
+                df_datetime_monthly_idx.index - df_datetime_monthly_idx.index[0]
+            )
         },
         index=df_datetime_monthly_idx.index,
     )
