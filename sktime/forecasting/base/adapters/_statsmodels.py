5fc8ab80a52924e48efac56c96a34dd44ec52d15
shyamsharmas124commits <2006sharma.shyam@gmail.com>
2026-09-15 10:56:49 +0530

    [BUG] Fix VARMAX and DynamicFactor statsmodels 0.15 compatibility (#11111)

--- sktime/forecasting/base/adapters/_statsmodels.py
+++ sktime/forecasting/base/adapters/_statsmodels.py
@@ -72,7 +72,9 @@ class _StatsModelsAdapter(BaseForecaster):
 
         # statsmodels does not support the pd.Int64Index as required,
         # so we coerce them here to pd.RangeIndex
-        if isinstance(y, pd.Series) and pd.api.types.is_integer_dtype(y.index):
+        if isinstance(y, (pd.Series, pd.DataFrame)) and pd.api.types.is_integer_dtype(
+            y.index
+        ):
             y, X = _coerce_int_to_range_index(y, X)
         self._fit_forecaster(y, X)
         return self
