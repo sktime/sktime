5aeac00907bd9dc9d021dd56a891f4fc90e06f68
Franz Király <fkiraly@gcos.ai>
2026-09-17 00:09:57 +0200

    Update _tsf.py

--- sktime/classification/interval_based/_tsf.py
+++ sktime/classification/interval_based/_tsf.py
@@ -117,7 +117,7 @@ class TimeSeriesForestClassifier(
         # CI and testing tags
         # -------------------
         "tests:vm": True,
-        "tests:libs": ["sktime.classification.interval_based._tsf"],
+        "tests:libs": ["sktime.base._panel.forest._tsf"],
         "tests:specific": ["sktime.classification.interval_based.tests.test_tsf"],
     }
 
