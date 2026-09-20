e6540105cbb9505a1231d38d9a20c4f125044ae7
Franz Király <fkiraly@gcos.ai>
2026-09-20 10:49:55 +0200

    Update _boss_pyts.py

--- sktime/classification/dictionary_based/_boss_pyts.py
+++ sktime/classification/dictionary_based/_boss_pyts.py
@@ -112,6 +112,7 @@ class BOSSVSClassifierPyts(_PytsAdapter, BaseClassifier):
         # CI and test flags
         # -----------------
         "tests:libs": ["sktime.base.adapters._pyts"],
+        "tests:skip_by_name": ["test_class_has_doctest_example"],
     }
 
     # defines the name of the attribute containing the pyts estimator
