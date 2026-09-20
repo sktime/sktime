9fd444daf6339301a5ede753f054c63ab9afe402
Franz Király <fkiraly@gcos.ai>
2026-09-20 10:36:12 +0200

    remove skip

--- sktime/split/cutoff.py
+++ sktime/split/cutoff.py
@@ -315,9 +315,6 @@ class CutoffFhSplitter(BaseSplitter):
         # CI and test flags
         # -----------------
         "tests:specific": ["sktime.split.tests.test_cutoff"],
-        # splitters excluded with undiagnosed failures, see #6194
-        # these are temporarily skipped to allow merging of the base test framework
-        "tests:skip_all": True,
     }
 
     def __init__(self, cutoff, fh=None):
