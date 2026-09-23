d12535b7dce8b9fb63ec7587f863ee434b1674eb
Yash Sangwan <yashsangwan00@gmail.com>
2026-09-24 02:59:38 +0530

    [ENH] dummy detector firing by time-from-start frequency

--- sktime/detection/dummy/__init__.py
+++ sktime/detection/dummy/__init__.py
@@ -4,6 +4,7 @@ from sktime.detection.dummy._dummy_pattern_an import DummyPatternAnomalies
 from sktime.detection.dummy._dummy_rate_an import DummyRateAnomalies
 from sktime.detection.dummy._dummy_regular_an import DummyRegularAnomalies
 from sktime.detection.dummy._dummy_regular_cp import DummyRegularChangePoints
+from sktime.detection.dummy._dummy_timefreq_an import DummyTimeFreqAnomalies
 from sktime.detection.dummy._zero_an import ZeroAnomalies
 from sktime.detection.dummy._zero_cp import ZeroChangePoints
 from sktime.detection.dummy._zero_seg import ZeroSegments
@@ -13,6 +14,7 @@ __all__ = [
     "DummyRateAnomalies",
     "DummyRegularAnomalies",
     "DummyRegularChangePoints",
+    "DummyTimeFreqAnomalies",
     "ZeroAnomalies",
     "ZeroChangePoints",
     "ZeroSegments",
